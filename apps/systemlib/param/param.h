14e60e9b4d04b4ac91a39a2158387c3c57474c83
px4dev <px4@purgatory.org>
2012-08-20 21:46:52 -0700

    Back out a change that snuck in from another branch.

--- apps/systemlib/param/param.h
+++ apps/systemlib/param/param.h
@@ -48,11 +48,36 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#include "drivers/drv_param.h"
-
 /** Maximum size of the parameter backing file */
 #define PARAM_FILE_MAXSIZE	4096
 
+/**
+ * Parameter types.
+ */
+typedef enum param_type_e {
+	/* globally-known parameter types */
+	PARAM_TYPE_INT32 = 0,
+	PARAM_TYPE_FLOAT,
+
+	/* structure parameters; size is encoded in the type value */
+	PARAM_TYPE_STRUCT = 100,
+	PARAM_TYPE_STRUCT_MAX = 16384 + PARAM_TYPE_STRUCT,
+
+	PARAM_TYPE_UNKNOWN = 0xffff
+} param_type_t;
+
+/**
+ * Parameter handle.
+ *
+ * Parameters are represented by parameter handles, which can
+ * be obtained by looking up (or creating?) parameters.
+ */
+typedef uintptr_t	param_t;
+
+/**
+ * Handle returned when a parameter cannot be found.
+ */
+#define PARAM_INVALID	((uintptr_t)0xffffffff)
 
 /**
  * Look up a parameter by name.
