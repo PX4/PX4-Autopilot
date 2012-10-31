8dcde7f8cd72e73ced0ea534a84257ef43210ab6
Lorenz Meier <lm@inf.ethz.ch>
2012-10-31 15:49:01 +0100

    prevent double-precision promotion where its not required

--- apps/systemlib/pid/pid.c
+++ apps/systemlib/pid/pid.c
@@ -155,8 +155,8 @@ __EXPORT float pid_calculate(PID_t *pid, float sp, float val, float val_dot, flo
 	// Calculate the error integral and check for saturation
 	i = pid->integral + (error * dt);
 
-	if (fabs((error * pid->kp) + (i * pid->ki) + (d * pid->kd)) > pid->limit ||
-	    fabs(i) > pid->intmax) {
+	if (fabsf((error * pid->kp) + (i * pid->ki) + (d * pid->kd)) > pid->limit ||
+	    fabsf(i) > pid->intmax) {
 		i = pid->integral;		// If saturated then do not update integral value
 		pid->saturated = 1;
 
