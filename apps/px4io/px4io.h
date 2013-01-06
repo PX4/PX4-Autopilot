a9e87c791ddedb4b820c99c79a830f157fb46e46
Lorenz Meier <lm@inf.ethz.ch>
2013-01-06 10:13:53 +0100

    Fixed header for sbus input

--- apps/px4io/px4io.h
+++ apps/px4io/px4io.h
@@ -198,8 +198,7 @@ extern void	controls_main(void);
 extern int	dsm_init(const char *device);
 extern bool	dsm_input(void);
 extern int	sbus_init(const char *device);
-extern bool	sbus_input(int fd, unsigned max_channels, uint16_t *channel_data, unsigned *channel_count,
-		uint64_t *receive_time, bool *updated);
+extern bool	sbus_input(void);
 
 /*
  * Assertion codes
