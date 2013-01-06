658a61c79fef1d8e92b851a282e2519f95bd65e0
jgoppert <james.goppert@gmail.com>
2013-01-06 16:56:01 -0500

    Added hil tools.

---
+++ Tools/hil/README.md
@@ -0,0 +1,28 @@
+# Description
+
+This branch currently has a switch to go between 
+sensors HIL and state HIL. This is located in apps/mavlink/mavlink.c.
+Currently the HIL mode is set to HIL_MODE_SENSORS.
+
+# Usage
+
+During the boot process you must type this or put it in your startup script:
+```
+kalman_demo start
+control_demo start
+```
+## QGroundControl
+
+The udp packets used by the python script called px4Start.sh are the same as the FlightGear xml descriptions in qgroundcontrol for the fixed wing.
+
+Currently I have not modified qgroundcontrol to have a new JSBSim mode, but you can disable the FlightGear process start and use this temporarily. This is currently done on my jsbsim branch of github.com/jgoppert/qgroundcontrol.
+
+# TODO:
+
+* Magnetometer measurement model doesn't depend on lat/lot yet.
+* Add noise.
+* Initialization routines for EKF.
+
+# Source
+
+This is currently a modification of the code for ardupilotmega SITL written by Andrew Tridgell.
---
+++ Tools/hil/pysim/aircraft.py
@@ -0,0 +1,50 @@
+import math, util, rotmat
+from rotmat import Vector3, Matrix3
+
+class Aircraft(object):
+    '''a basic aircraft class'''
+    def __init__(self):
+        self.home_latitude = 0
+        self.home_longitude = 0
+        self.home_altitude = 0
+        self.ground_level = 0
+        self.frame_height = 0.0
+
+        self.latitude  = self.home_latitude
+        self.longitude = self.home_longitude
+        self.altitude  = self.home_altitude
+
+        self.dcm = Matrix3()
+
+        # rotation rate in body frame
+        self.gyro = Vector3(0,0,0) # rad/s
+
+        self.velocity = Vector3(0, 0, 0) # m/s, North, East, Down
+        self.position = Vector3(0, 0, 0) # m North, East, Down
+        self.mass = 0.0
+        self.update_frequency = 50 # in Hz
+        self.gravity = 9.80665 # m/s/s
+        self.accelerometer = Vector3(0, 0, -self.gravity)
+
+        self.wind = util.Wind('0,0,0')
+
+    def on_ground(self, position=None):
+        '''return true if we are on the ground'''
+        if position is None:
+            position = self.position
+        return (-position.z) + self.home_altitude <= self.ground_level + self.frame_height
+
+    def update_position(self, delta_time):
+        '''update lat/lon/alt from position'''
+
+        bearing = math.degrees(math.atan2(self.position.y, self.position.x))
+        distance = math.sqrt(self.position.x**2 + self.position.y**2)
+
+        (self.latitude, self.longitude) = util.gps_newpos(self.home_latitude, self.home_longitude,
+                                                          bearing, distance)
+
+        self.altitude  = self.home_altitude - self.position.z
+
+        velocity_body = self.dcm.transposed() * self.velocity
+
+        self.accelerometer = self.accel_body.copy()
