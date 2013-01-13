958842bc318887c82cb3fb5fbf0eab3b2d131485
James Goppert <james.goppert@gmail.com>
2013-01-13 16:29:30 -0500

    Moved HIL to separate repo.

--- Tools/hil/scripts/px4MAVProxy.start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-./jsbsim/runsim.py \
-    --script=jsbsim/easystar_test \
-    --home=37.6166110,-122.4161053,25,0 \
-    --fgout=127.0.0.1:5503 \
-    --simout=127.0.0.1:5501 \
-    --simin=127.0.0.1:5502 \
-    --wind=0,0,0
