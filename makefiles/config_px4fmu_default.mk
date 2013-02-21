3494039d9000d211c122d73d5e7ac9cf9109dddb
px4dev <px4@purgatory.org>
2013-02-20 22:07:18 -0800

    Get a bit fancier with the builtin app specifications, so that we can generate them from apps as well as the config.

--- makefiles/config_px4fmu_default.mk
+++ makefiles/config_px4fmu_default.mk
@@ -6,4 +6,9 @@ CONFIG		 = px4fmu_default
 SRCS		 = $(PX4_BASE)/platforms/empty.c
 ROMFS_ROOT	 = $(PX4_BASE)/ROMFS/$(CONFIG)
 
+# Commands from the NuttX export archive
+#
+# Each entry here is <command>.<priority>.<stacksize>.<entrypoint>
+BUILTIN_COMMANDS = perf.SCHED_PRIORITY_DEFAULT.CONFIG_PTHREAD_STACK_DEFAULT.perf_main
+
 include $(PX4_MK_DIR)/firmware.mk
