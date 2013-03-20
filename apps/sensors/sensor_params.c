3f5e23071eccdc1cb1fa91200298724d03d382d2
Lorenz Meier <lm@inf.ethz.ch>
2013-03-20 23:47:16 +0100

    Switched to USB NSH

--- apps/sensors/sensor_params.c
+++ apps/sensors/sensor_params.c
@@ -153,6 +153,7 @@ PARAM_DEFINE_FLOAT(RC14_DZ, 0.0f);
 PARAM_DEFINE_INT32(RC_TYPE, 1); /** 1 = FUTABA, 2 = Spektrum, 3 = Graupner HoTT, 4 = Turnigy 9x */
 
 /* default is conversion factor for the PX4IO / PX4IOAR board, the factor for PX4FMU standalone is different */
+/* 0.00459228514 for FMU standalone */
 PARAM_DEFINE_FLOAT(BAT_V_SCALING, (3.3f * 52.0f / 5.0f / 4095.0f));
 
 PARAM_DEFINE_INT32(RC_MAP_ROLL, 1);
