b1de6c0eaf26b9bb05075ffa18e1eb32d3cae75b
Lorenz Meier <lm@inf.ethz.ch>
2013-04-28 10:38:45 +0200

    Excluded examples from default build

--- makefiles/config_px4fmu_default.mk
+++ makefiles/config_px4fmu_default.mk
@@ -88,18 +88,18 @@ MODULES		+= modules/uORB
 #
 # Demo apps
 #
-MODULES		+= examples/math_demo
+#MODULES		+= examples/math_demo
 # Tutorial code from
 # https://pixhawk.ethz.ch/px4/dev/hello_sky
-MODULES		+= examples/px4_simple_app
+#MODULES		+= examples/px4_simple_app
 
 # Tutorial code from
 # https://pixhawk.ethz.ch/px4/dev/daemon
-MODULES		+= examples/px4_daemon_app
+#MODULES		+= examples/px4_daemon_app
 
 # Tutorial code from
 # https://pixhawk.ethz.ch/px4/dev/debug_values
-MODULES		+= examples/px4_mavlink_debug
+#MODULES		+= examples/px4_mavlink_debug
 
 #
 # Transitional support - add commands from the NuttX export archive.
