8c6abe717db1f50c5d4b89f18e27f89608a9a1b8
Lorenz Meier <lm@inf.ethz.ch>
2013-05-05 11:24:31 +0200

    Moved BLCTRL driver to new world

--- src/drivers/mkblctrl/module.mk
+++ src/drivers/mkblctrl/module.mk
@@ -1,6 +1,6 @@
 ############################################################################
 #
-#   Copyright (C) 2012 PX4 Development Team. All rights reserved.
+#   Copyright (c) 2012,2013 PX4 Development Team. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -35,10 +35,8 @@
 # Interface driver for the Mikrokopter BLCtrl
 #
 
-APPNAME		 = mkblctrl
-PRIORITY	 = SCHED_PRIORITY_DEFAULT
-STACKSIZE	 = 2048
+MODULE_COMMAND		= mkblctrl
 
-INCLUDES	 = $(TOPDIR)/arch/arm/src/stm32 $(TOPDIR)/arch/arm/src/common
+SRCS				= mkblctrl.cpp
 
-include $(APPDIR)/mk/app.mk
+INCLUDE_DIRS		+= $(TOPDIR)/arch/arm/src/stm32 $(TOPDIR)/arch/arm/src/common
