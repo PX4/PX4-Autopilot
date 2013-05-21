2db1422fa7cb400f45eddfdae622c7f84c883acb
Duncan Greer <duncangreer@gmail.com>
2013-05-21 19:16:49 +1000

    Revert "change default rc channel mapping"
    This reverts commit fa403956ed6ef1a345ee06c25500c937a78dd7cd.

--- src/modules/uORB/topics/rc_channels.h
+++ src/modules/uORB/topics/rc_channels.h
@@ -64,9 +64,9 @@
  */
 enum RC_CHANNELS_FUNCTION
 {
-  THROTTLE = 2,
-  ROLL     = 0,
-  PITCH    = 1,
+  THROTTLE = 0,
+  ROLL     = 1,
+  PITCH    = 2,
   YAW      = 3,
   OVERRIDE = 4,
   AUTO_MODE = 5,
