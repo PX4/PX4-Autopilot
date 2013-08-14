9505654f9103c8965891991514ea690b3e6aea25
Anton Babushkin <anton.babushkin@me.com>
2013-08-14 17:57:01 +0200

    commander/px4_custom_mode.h added

---
+++ src/modules/commander/px4_custom_mode.h
@@ -0,0 +1,18 @@
+/*
+ * px4_custom_mode.h
+ *
+ *  Created on: 09.08.2013
+ *      Author: ton
+ */
+
+#ifndef PX4_CUSTOM_MODE_H_
+#define PX4_CUSTOM_MODE_H_
+
+enum PX4_CUSTOM_MODE {
+	PX4_CUSTOM_MODE_MANUAL = 1,
+	PX4_CUSTOM_MODE_SEATBELT,
+	PX4_CUSTOM_MODE_EASY,
+	PX4_CUSTOM_MODE_AUTO,
+};
+
+#endif /* PX4_CUSTOM_MODE_H_ */
