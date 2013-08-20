95260d453592903bfcd3dba9379db033738d5b89
Lorenz Meier <lm@inf.ethz.ch>
2013-08-20 09:36:26 +0200

    Fixed NSH terminal init

--- src/systemcmds/nshterm/nshterm.c
+++ src/systemcmds/nshterm/nshterm.c
@@ -62,7 +62,7 @@ nshterm_main(int argc, char *argv[])
     }
     uint8_t retries = 0;
     int fd = -1;
-    while (retries < 5) {
+    while (retries < 50) {
         /* the retries are to cope with the behaviour of /dev/ttyACM0 */
         /* which may not be ready immediately. */
         fd = open(argv[1], O_RDWR);
