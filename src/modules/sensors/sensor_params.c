a1c4c0fd787401f8ed332fa974a88a74ae079383
Lorenz Meier <lm@inf.ethz.ch>
2013-09-01 00:09:59 +0200

    Comments on scaling factors

--- src/modules/sensors/sensor_params.c
+++ src/modules/sensors/sensor_params.c
@@ -159,6 +159,8 @@ PARAM_DEFINE_INT32(RC_RL1_DSM_VCC, 0); /* Relay 1 controls DSM VCC */
 PARAM_DEFINE_INT32(RC_DSM_BIND, 0); /* 0 = Idle, 1 = Start DSM2 bind, 2 = Start DSMX bind */
 
 /* default is conversion factor for the PX4IO / PX4IOAR board, the factor for PX4FMU standalone is different */
+/* PX4IOAR: 0.00838095238 */
+/* FMU standalone: 1/(10 / (47+10)) * (3.3 / 4095) = 0.00459340659 */
 PARAM_DEFINE_FLOAT(BAT_V_SCALING, (3.3f * 52.0f / 5.0f / 4095.0f));
 
 PARAM_DEFINE_INT32(RC_MAP_ROLL, 1);
