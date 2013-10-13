5671df0af4e258ef0a83377cdbd50e59734aa00b
Lorenz Meier <lm@inf.ethz.ch>
2013-10-13 11:44:42 +0200

    Improved mixer tests

--- Tools/tests-host/mixer_test.cpp
+++ Tools/tests-host/mixer_test.cpp
@@ -1,12 +1,12 @@
 #include <systemlib/mixer/mixer.h>
 #include <systemlib/err.h>
-
-extern int test_mixer(int argc, char *argv[]);
+#include "../../src/systemcmds/tests/tests.h"
 
 int main(int argc, char *argv[]) {
 	warnx("Host execution started");
 
-	char* args[] = {argv[0], "../../ROMFS/px4fmu_common/mixers/IO_pass.mix"};
+	char* args[] = {argv[0], "../../ROMFS/px4fmu_common/mixers/IO_pass.mix",
+				 "../../ROMFS/px4fmu_common/mixers/FMU_quad_w.mix"};
 
-	test_mixer(2, args);
+	test_mixer(3, args);
 }
