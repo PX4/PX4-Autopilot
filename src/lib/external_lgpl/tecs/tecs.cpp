947207e68da08b7cd14e7da0b44d000b1c435b11
Thomas Gubler <thomasgubler@student.ethz.ch>
2013-10-31 12:15:14 +0100

    change calc of hgt_rate_dem to use the filtered height setpoint

--- src/lib/external_lgpl/tecs/tecs.cpp
+++ src/lib/external_lgpl/tecs/tecs.cpp
@@ -231,7 +231,7 @@ void TECS::_update_height_demand(float demand, float state)
 	_hgt_dem_adj = 0.05f * demand + 0.95f * _hgt_dem_adj_last;
 	_hgt_dem_adj_last = _hgt_dem_adj;
 
-	_hgt_rate_dem = (demand-state)*0.1f; //xxx: parameter
+	_hgt_rate_dem = (_hgt_dem_adj-state)*0.1f; //xxx: parameter
 	// Limit height rate of change
 	if (_hgt_rate_dem > _maxClimbRate) {
 		_hgt_rate_dem = _maxClimbRate;
