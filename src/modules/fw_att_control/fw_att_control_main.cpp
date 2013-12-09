2761d47be2792a3e28e92bfd60407a0aaa243106
Thomas Gubler <thomasgubler@gmail.com>
2013-12-09 09:53:51 +0100

    disable printf

--- src/modules/fw_att_control/fw_att_control_main.cpp
+++ src/modules/fw_att_control/fw_att_control_main.cpp
@@ -820,10 +820,10 @@ FixedwingAttitudeControl::task_main()
 			if (_actuators_1_pub > 0) {
 				/* publish the attitude setpoint */
 				orb_publish(ORB_ID(actuator_controls_1), _actuators_1_pub, &_actuators_airframe);
-				warnx("%.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f",
-						(double)_actuators_airframe.control[0], (double)_actuators_airframe.control[1], (double)_actuators_airframe.control[2],
-						(double)_actuators_airframe.control[3], (double)_actuators_airframe.control[4], (double)_actuators_airframe.control[5],
-						(double)_actuators_airframe.control[6], (double)_actuators_airframe.control[7]);
+//				warnx("%.2f %.2f %.2f %.2f %.2f %.2f %.2f %.2f",
+//						(double)_actuators_airframe.control[0], (double)_actuators_airframe.control[1], (double)_actuators_airframe.control[2],
+//						(double)_actuators_airframe.control[3], (double)_actuators_airframe.control[4], (double)_actuators_airframe.control[5],
+//						(double)_actuators_airframe.control[6], (double)_actuators_airframe.control[7]);
 
 			} else {
 				/* advertise and publish */
