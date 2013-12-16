43582cdb6843cdcfdc8f0deba1439b6c3fe7e417
Lorenz Meier <lm@inf.ethz.ch>
2013-12-16 09:04:03 +0100

    Re-introduced AQ MAVLink files since we have had them around before

---
+++ mavlink/include/mavlink/v1.0/autoquad/version.h
@@ -0,0 +1,12 @@
+/** @file
+ *	@brief MAVLink comm protocol built from autoquad.xml
+ *	@see http://pixhawk.ethz.ch/software/mavlink
+ */
+#ifndef MAVLINK_VERSION_H
+#define MAVLINK_VERSION_H
+
+#define MAVLINK_BUILD_DATE "Mon Dec 16 09:03:20 2013"
+#define MAVLINK_WIRE_PROTOCOL_VERSION "1.0"
+#define MAVLINK_MAX_DIALECT_PAYLOAD_SIZE 254
+ 
+#endif // MAVLINK_VERSION_H
