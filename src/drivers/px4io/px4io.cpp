0f0dc5ba068d24fb8b339acc8ef850f5f6ea9e47
Lorenz Meier <lm@inf.ethz.ch>
2013-12-21 12:45:04 +0100

    Allowed custom battery scaling on IO

--- src/drivers/px4io/px4io.cpp
+++ src/drivers/px4io/px4io.cpp
@@ -896,8 +896,21 @@ PX4IO::task_main()
 
 				/* re-upload RC input config as it may have changed */
 				io_set_rc_config();
-			}
-		}
+
+				/* re-set the battery scaling */
+				int32_t voltage_scaling_val;
+				param_t voltage_scaling_param;
+
+				/* see if bind parameter has been set, and reset it to -1 */
+				param_get(voltage_scaling_param = param_find("BAT_V_SCALE_IO"), &voltage_scaling_val);
+
+				/* send channel config to IO */
+				uint16_t scaling = voltage_scaling_val;
+				int pret = io_reg_set(PX4IO_PAGE_SETUP, PX4IO_P_SETUP_VBATT_SCALE, &scaling, 1);
+
+				if (pret != OK) {
+					log("voltage scaling upload failed");
+				}
 
 		perf_end(_perf_update);
 	}
