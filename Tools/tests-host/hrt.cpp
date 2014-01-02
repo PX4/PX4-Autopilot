eb47a164cb44ddab70a2639ddf4f68519eadd4df
Lorenz Meier <lm@inf.ethz.ch>
2014-01-02 01:18:12 +0100

    Added missing HRT functionality, cleanup

--- Tools/tests-host/hrt.cpp
+++ Tools/tests-host/hrt.cpp
@@ -3,11 +3,14 @@
 #include <drivers/drv_hrt.h>
 #include <stdio.h>
 
-uint64_t hrt_absolute_time()
-{
+hrt_abstime hrt_absolute_time() {
     struct timeval te; 
     gettimeofday(&te, NULL); // get current time
-    unsigned long long us = static_cast<uint64_t>(te.tv_sec) * 1e6 + te.tv_usec; // caculate us
-    printf("us: %lld\n", us);
+    hrt_abstime us = static_cast<uint64_t>(te.tv_sec) * 1e6 + te.tv_usec; // caculate us
     return us;
+}
+
+hrt_abstime hrt_elapsed_time(const volatile hrt_abstime *then) {
+	// not thread safe
+    return hrt_absolute_time() - *then;
 }
