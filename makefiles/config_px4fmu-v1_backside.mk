1e0f292566b2a80557a2727e99b74990c75c90d9
Lorenz Meier <lm@inf.ethz.ch>
2014-01-14 08:57:32 +0100

    Disabling EEPROM and old RAMTRON driver

--- makefiles/config_px4fmu-v1_backside.mk
+++ makefiles/config_px4fmu-v1_backside.mk
@@ -38,8 +38,7 @@ MODULES		+= modules/sensors
 #
 # System commands
 #
-MODULES		+= systemcmds/eeprom
-MODULES		+= systemcmds/ramtron
+MODULES		+= systemcmds/mtd
 MODULES		+= systemcmds/bl_update
 MODULES		+= systemcmds/boardinfo
 MODULES		+= systemcmds/i2c
