dfaa5a0c7c81f432d64cfffdaaf3d683c01f10b4
Darryl Taylor <darryl.c.taylor@gmail.com>
2014-01-28 09:57:47 +0800

    Reduced stack from 2048 to 1024. Top reports stack usage at 812 under flight conditions.

--- src/drivers/px4io/px4io.cpp
+++ src/drivers/px4io/px4io.cpp
@@ -756,7 +756,7 @@ PX4IO::init()
 	}
 
 	/* start the IO interface task */
-	_task = task_create("px4io", SCHED_PRIORITY_ACTUATOR_OUTPUTS, 2048, (main_t)&PX4IO::task_main_trampoline, nullptr);
+	_task = task_create("px4io", SCHED_PRIORITY_ACTUATOR_OUTPUTS, 1024, (main_t)&PX4IO::task_main_trampoline, nullptr);
 
 	if (_task < 0) {
 		debug("task start failed: %d", errno);
