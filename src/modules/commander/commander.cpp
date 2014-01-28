0488d5b41c5c91658494242f778c7b9de99bcca4
Darryl Taylor <darryl.c.taylor@gmail.com>
2014-01-28 10:43:33 +0800

    Reduced commander main task stack size to 2088. The high-water-mark measured at 1668 after calibration and flight. 25% safety margin, but commander is fairly complex. There are surely untested code paths here, but each is relatively shallow.

--- src/modules/commander/commander.cpp
+++ src/modules/commander/commander.cpp
@@ -237,7 +237,7 @@ int commander_main(int argc, char *argv[])
 		daemon_task = task_spawn_cmd("commander",
 					     SCHED_DEFAULT,
 					     SCHED_PRIORITY_MAX - 40,
-					     3000,
+					     2088,
 					     commander_thread_main,
 					     (argv) ? (const char **)&argv[2] : (const char **)NULL);
 
