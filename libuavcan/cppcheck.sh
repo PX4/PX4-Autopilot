690e0257dcf5e5271e6b590808eb2776afbe82fa
Pavel Kirienko <pavel.kirienko@gmail.com>
2014-02-06 00:08:18 +0400

    cppcheck: removed flag --inconclusive as it was producing some stupid false positives

--- libuavcan/cppcheck.sh
+++ libuavcan/cppcheck.sh
@@ -6,5 +6,5 @@
 
 # TODO: with future versions of cppcheck, add --library=glibc
 cppcheck . --error-exitcode=1 --quiet --enable=all --platform=unix64 --std=c99 --std=c++11 \
-           --inconclusive --inline-suppr --force --template=gcc \
+           --inline-suppr --force --template=gcc \
            -Iinclude $@
