694054dbc71cb2da4be97ebf9e84579b068918d7
px4dev <px4@purgatory.org>
2014-02-09 17:23:46 -0800

    Working, but too slow. Tighten some things up and add perf counters; average read is ~25ms, worst case nearly 60.

--- src/drivers/zromdisk/drv_zromdisk.cpp
+++ src/drivers/zromdisk/drv_zromdisk.cpp
@@ -57,11 +57,13 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include <assert.h>
 #include <debug.h>
 
 #include <drivers/device/device.h>
+#include <systemlib/perf_counter.h>
 
 #ifdef CONFIG_FS_WRITABLE
 # define REGISTER_FUNCTION ramdisk_register
@@ -94,15 +96,24 @@ public:
 	virtual int	geometry(struct inode *inode, struct geometry *g);
 
 private:
-	struct State {
-		unsigned	N;
-		unsigned	F;
-		unsigned	THRESHOLD;
+	struct State 
+	{
 		unsigned	discard;
 		unsigned	resid;
 		const unsigned char *src;
 		unsigned char	*dst;
 		unsigned char	*text_buf;
+
+		int		_getc()      { return *src++; }
+		void		_putc(int c) 
+		{
+			if (discard > 0) {
+				discard--;
+			} else if (resid > 0) {
+				*dst++ = c;
+				resid--;
+			}
+		}
 	};
 
 	static const unsigned	_magic = 'Z';
@@ -110,9 +121,11 @@ private:
 	const ZRDHeader		*const _hdr;
 	const unsigned		_sectorsize;
 	const unsigned		_image_size;
+	perf_counter_t		_read_perf;
+	perf_counter_t		_seq;
+	State			*_s;
+	unsigned		_next;
 
-	int			_getc(State &s);
-	int			_putc(State &s, int c);
 	void			_lzss_decode(State &s);
 };
 
@@ -121,13 +134,19 @@ ZROMDisk::ZROMDisk(const char *devname, const unsigned char *image, unsigned ima
 	BDev("zromdisk", devname),
 	_hdr(reinterpret_cast<const ZRDHeader *>(image)),
 	_sectorsize(sectorsize),
-	_image_size(image_size)
+	_image_size(image_size),
+	_read_perf(perf_alloc(PC_ELAPSED, "zromdisk read")),
+	_seq(perf_alloc(PC_COUNT, "zromdisk sequential")),
+	_s(nullptr),
+	_next(0xffffffff)
 {
 	log("@%p ss%u", image, sectorsize);
 }
 
 ZROMDisk::~ZROMDisk()
 {
+	if (_read_perf != nullptr)
+		perf_free(_read_perf);
 }
 
 int
@@ -145,41 +164,60 @@ ZROMDisk::open(struct inode *inode)
 ssize_t
 ZROMDisk::read(struct inode *inode, unsigned char *buffer, size_t start_sector, size_t nsectors)
 {
+	ssize_t result;
 	State s;
 
+	lock();
+	_s = &s;
+	perf_begin(_read_perf);
+
+	if (start_sector == _next)
+		perf_count(_seq);
+	_next = start_sector + nsectors;
+
 	/* read entirely outside device */
-	if ((start_sector * _sectorsize) > _hdr->uncomp_len)
-		return 0;
+	if ((start_sector * _sectorsize) > _hdr->uncomp_len) {
+		result = 0;
+		goto out;
+	}
 
 	/* read overlaps end of device - shorten it up */
 	if (((start_sector + nsectors) * _sectorsize) > _hdr->uncomp_len) {
 		nsectors = (_hdr->uncomp_len / _sectorsize) - start_sector;
 	}
 
-	s.N = 1 << _hdr->log_n;
-	s.F = _hdr->f;
-	s.THRESHOLD = _hdr->threshold;
 	s.discard = start_sector * _sectorsize;
 	s.resid = nsectors * _sectorsize;
 	s.src = &_hdr->data[0];
 	s.dst = buffer;
 
-	unsigned buffer_size = s.N + s.F - 1;
-	s.text_buf = (unsigned char *)malloc(buffer_size);
+	{
+		unsigned buffer_size = (1 << _hdr->log_n) + _hdr->f - 1;
+		s.text_buf = (unsigned char *)malloc(buffer_size);
 
-	if (s.text_buf == nullptr) {
-		log("can't get %u bytes", buffer_size);
-		return -ENOMEM;
+		if (s.text_buf == nullptr) {
+			log("can't get %u bytes", buffer_size);
+			result = -ENOMEM;
+			goto out;
+		}
 	}
 
 	_lzss_decode(s);
+
 	free(s.text_buf);
 
 	if (s.resid > 0) {
 		log("short read by %u", s.resid);
-		return -EIO;
+		result = -EIO;
+	} else {
+		result = nsectors;
 	}
-	return nsectors;
+
+out:
+	perf_end(_read_perf);
+	unlock();
+
+	return result;
 }
 
 int
@@ -203,79 +241,42 @@ ZROMDisk::_lzss_decode(State &s)
 {
 	int  i, j, k, r, c;
 	unsigned int  flags;
+	unsigned N = (1 << _hdr->log_n);
+	unsigned F = _hdr->f;
+	unsigned THRESHOLD = _hdr->threshold;
 
-	for (i = 0; (unsigned)i < s.N - s.F; i++) 
-		s.text_buf[i] = ' ';
-
-	r = s.N - s.F;
+	r = N - F;
+	memset(s.text_buf, ' ', r);
 	flags = 0;
 
-	for (; ;) {
+	while (s.resid > 0) {
 		if (((flags >>= 1) & 256) == 0) {
-			if ((c = _getc(s)) == -1)
-				break;
-
+			c = s._getc();
 			flags = c | 0xff00;             /* uses higher byte cleverly */
 		}                                       /* to count eight */
 
 		if (flags & 1) {
-			if ((c = _getc(s)) == -1)
-				break;
-
-			if (_putc(s, c) == -1)
-				break;
-
+			c = s._getc();
+			s._putc(c);
 			s.text_buf[r++] = c;
-			r &= (s.N - 1);
+			r &= (N - 1);
 
 		} else {
-			if ((i = _getc(s)) == -1)
-				break;
-
-			if ((j = _getc(s)) == -1) 
-				break;
+			i = s._getc();
+			j = s._getc();
 
 			i |= ((j & 0xf0) << 4);
-			j = (j & 0x0f) + s.THRESHOLD;
+			j = (j & 0x0f) + THRESHOLD;
 
 			for (k = 0; k <= j; k++) {
-				c = s.text_buf[(i + k) & (s.N - 1)];
+				c = s.text_buf[(i + k) & (N - 1)];
 
-				if (_putc(s, c) == -1)
-					break;
-
+				s._putc(c);
 				s.text_buf[r++] = c;
-				r &= (s.N - 1);
+				r &= (N - 1);
 			}
 		}
 	}
-}
-
-int
-ZROMDisk::_getc(State &s)
-{
-	/* avoid running (too far) off the end of the source data */
-	if (s.src > &_hdr->data[_image_size])
-		return -1;
-
-	return *(s.src++);
-}
-
-int
-ZROMDisk::_putc(State &s, int c)
-{
-	if (s.discard > 0) {
-		s.discard--;
-
-	} else if (s.resid < 1) {
-		return -1;
-
-	} else {
-		*(s.dst++) = c;
-		s.resid--;
-	}
-
-	return 0;
 }
 
 ZROMDisk *zrd;
