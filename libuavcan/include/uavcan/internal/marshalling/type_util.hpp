a24cbcc4930014190336dd38b97133db8bb231c3
Pavel Kirienko <pavel.kirienko@gmail.com>
2014-02-24 21:46:18 +0400

    Simplified StorageType<>

--- libuavcan/include/uavcan/internal/marshalling/type_util.hpp
+++ libuavcan/include/uavcan/internal/marshalling/type_util.hpp
@@ -18,22 +18,13 @@ template <>
 struct IntegerBitLen<0> { enum { Result = 0 }; };
 
 
-// TODO: fix
 template <typename T, typename Enable = void>
-struct StorageTypeImpl { typedef T Type; };
+struct StorageType { typedef T Type; };
 
 template <typename T>
-struct StorageTypeImpl<T, typename EnableIfType<typename T::StorageType>::Type>
+struct StorageType<T, typename EnableIfType<typename T::StorageType>::Type>
 {
     typedef typename T::StorageType Type;
-};
-
-template <typename T>
-class StorageType : public T
-{
-    StorageType();
-public:
-    typedef typename StorageTypeImpl<T>::Type Type;
 };
 
 }
