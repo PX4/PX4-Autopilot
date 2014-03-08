008efff42ea0fec64173941bd8a746ec0e7ecfa4
Lorenz Meier <lm@inf.ethz.ch>
2014-03-08 20:00:06 +0100

    Added support for driver rangefinder

--- makefiles/config_px4fmu-v2_default.mk
+++ makefiles/config_px4fmu-v2_default.mk
@@ -27,6 +27,7 @@ MODULES		+= drivers/l3gd20
 MODULES		+= drivers/hmc5883
 MODULES		+= drivers/ms5611
 MODULES		+= drivers/mb12xx
+MODULES		+= drivers/sf0x
 MODULES		+= drivers/gps
 MODULES		+= drivers/hil
 MODULES		+= drivers/hott/hott_telemetry
