a977ff2b6295bd4bac614a854ecc35e1b9b2d75c
Thomas Gubler <thomasgubler@gmail.com>
2014-03-12 00:20:27 +0100

    fw att control: add timestamp dependency in airspeed check

--- src/modules/fw_att_control/fw_att_control_main.cpp
+++ src/modules/fw_att_control/fw_att_control_main.cpp
@@ -632,7 +632,8 @@ FixedwingAttitudeControl::task_main()
 
 				/* if airspeed is smaller than min, the sensor is not giving good readings */
 				if ((_airspeed.indicated_airspeed_m_s < 0.5f * _parameters.airspeed_min) ||
-				    !isfinite(_airspeed.indicated_airspeed_m_s)) {
+				    !isfinite(_airspeed.indicated_airspeed_m_s) ||
+				    (float)hrt_elapsed_time(&_airspeed.timestamp) > 1e6f) {
 					airspeed = _parameters.airspeed_trim;
 
 				} else {
