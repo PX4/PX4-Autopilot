6ad511a9bef12b9eecef4e61f11d64671ab3f537
Pavel Kirienko <pavel.kirienko@gmail.com>
2014-03-17 22:19:52 +0400

    Multithreaded mode for cppcheck

--- libuavcan/cppcheck.sh
+++ libuavcan/cppcheck.sh
@@ -4,7 +4,14 @@
 # For Debian based: apt-get install cppcheck
 #
 
+num_cores=$(grep -c ^processor /proc/cpuinfo)
+if [ -z "$num_cores" ]; then
+    echo "Hey, it looks like we're not on Linux. Please fix this script to add support for this OS."
+    num_cores=4
+fi
+echo "Number of threads for cppcheck: $num_cores"
+
 # TODO: with future versions of cppcheck, add --library=glibc
 cppcheck . --error-exitcode=1 --quiet --enable=all --platform=unix64 --std=c99 --std=c++11 \
-           --inline-suppr --force --template=gcc \
+           --inline-suppr --force --template=gcc -j$num_cores \
            -Iinclude $@
