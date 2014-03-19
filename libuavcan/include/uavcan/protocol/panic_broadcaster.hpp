ce9c0b6dbe92c365d337e42435acb04b65364f21
Pavel Kirienko <pavel.kirienko@gmail.com>
2014-03-19 12:25:49 +0400

    PanicBroadcaster

---
+++ libuavcan/include/uavcan/protocol/panic_broadcaster.hpp
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2014 Pavel Kirienko <pavel.kirienko@gmail.com>
+ */
+
+#pragma once
+
+#include <uavcan/node/publisher.hpp>
+#include <uavcan/node/timer.hpp>
+#include <uavcan/protocol/Panic.hpp>
+
+namespace uavcan
+{
+
+class PanicBroadcaster : private Timer
+{
+    Publisher<protocol::Panic> pub_;
+    protocol::Panic msg_;
+
+    void publishOnce();
+
+    void handleTimerEvent(const TimerEvent&);
+
+public:
+    PanicBroadcaster(INode& node)
+    : Timer(node)
+    , pub_(node)
+    {
+        pub_.setTxTimeout(MonotonicDuration::fromMSec(protocol::Panic::BROADCASTING_INTERVAL_MS - 10));
+    }
+
+    void panic(const char* short_reason);
+
+    void dontPanic();    // Where's my towel
+
+    bool isPanicking() const;
+
+    const typename protocol::Panic::FieldTypes::reason_text& getReason() const;
+};
+
+}
---
+++ libuavcan/src/protocol/panic_broadcaster.cpp
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2014 Pavel Kirienko <pavel.kirienko@gmail.com>
+ */
+
+#include <uavcan/protocol/panic_broadcaster.hpp>
+#include <uavcan/debug.hpp>
+
+namespace uavcan
+{
+
+void PanicBroadcaster::publishOnce()
+{
+    const int res = pub_.broadcast(msg_);
+    if (res < 0)
+    {
+        pub_.getNode().registerInternalFailure("Panic broadcasting failure");
+    }
+}
+
+void PanicBroadcaster::handleTimerEvent(const TimerEvent&)
+{
+    publishOnce();
+}
+
+void PanicBroadcaster::panic(const char* short_reason)
+{
+    msg_.reason_text.clear();
+    const char* p = short_reason;
+    while (p && *p)
+    {
+        if (msg_.reason_text.size() == msg_.reason_text.capacity())
+            break;
+        msg_.reason_text.push_back(*p);
+        p++;
+    }
+
+    UAVCAN_TRACE("PanicBroadcaster", "Panicking with reason '%s'", getReason().c_str());
+
+    publishOnce();
+    startPeriodic(MonotonicDuration::fromMSec(protocol::Panic::BROADCASTING_INTERVAL_MS));
+}
+
+void PanicBroadcaster::dontPanic()
+{
+    stop();
+    msg_.reason_text.clear();
+}
+
+bool PanicBroadcaster::isPanicking() const
+{
+    return isRunning();
+}
+
+const typename protocol::Panic::FieldTypes::reason_text& PanicBroadcaster::getReason() const
+{
+    return msg_.reason_text;
+}
+
+}
