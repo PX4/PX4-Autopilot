ec94ebb1f4b0582e575831885ac4bafc1b6f4b85
Pavel Kirienko <pavel.kirienko@gmail.com>
2014-03-23 14:41:17 +0400

    stdint.hpp - typedef instead of using declaration - typedefs are safer

--- libuavcan/include/uavcan/stdint.hpp
+++ libuavcan/include/uavcan/stdint.hpp
@@ -17,15 +17,15 @@
 namespace uavcan
 {
 
-using std::uint8_t;
-using std::uint16_t;
-using std::uint32_t;
-using std::uint64_t;
+typedef std::uint8_t uint8_t;
+typedef std::uint16_t uint16_t;
+typedef std::uint32_t uint32_t;
+typedef std::uint64_t uint64_t;
 
-using std::int8_t;
-using std::int16_t;
-using std::int32_t;
-using std::int64_t;
+typedef std::int8_t int8_t;
+typedef std::int16_t int16_t;
+typedef std::int32_t int32_t;
+typedef std::int64_t int64_t;
 
 }
 
