4ddf4f6727d3583a725dc19ca87e06ba0ad28cd0
Andrew Tridgell <tridge@samba.org>
2014-03-25 11:01:57 +1100

    tests: disable mathlib tests for APM
    APM does not link in mathlib

--- src/systemcmds/tests/tests_main.c
+++ src/systemcmds/tests/tests_main.c
@@ -110,7 +110,9 @@ const struct {
 	{"conv",		test_conv,	OPT_NOJIGTEST | OPT_NOALLTEST},
 	{"mount",		test_mount,	OPT_NOJIGTEST | OPT_NOALLTEST},
 	{"mtd",			test_mtd,	0},
+#ifndef ARDUPILOT_BUILD
 	{"mathlib",		test_mathlib,	0},
+#endif
 	{"help",		test_help,	OPT_NOALLTEST | OPT_NOHELP | OPT_NOJIGTEST},
 	{NULL,			NULL, 		0}
 };
