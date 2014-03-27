0d526bddca9210bed75cfd26458a48ac87453c9e
Thomas Gubler <thomasgubler@gmail.com>
2014-03-27 21:47:34 +0100

    fw_pos_control: whitespace in module.mk

--- src/modules/fw_pos_control_l1/module.mk
+++ src/modules/fw_pos_control_l1/module.mk
@@ -40,5 +40,5 @@ MODULE_COMMAND	= fw_pos_control_l1
 SRCS		= fw_pos_control_l1_main.cpp \
 		  fw_pos_control_l1_params.c \
 		  landingslope.cpp \
-		  mtecs/mTecs.cpp\
+		  mtecs/mTecs.cpp \
 		  mtecs/mTecs_params.c
