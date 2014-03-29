77d46cb91f3bff5c0e871c8847d729d4c6f31806
Pavel Kirienko <pavel.kirienko@gmail.com>
2014-03-29 12:35:52 +0400

    pyuavcan setup.py

---
+++ pyuavcan/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+args = dict(
+    name='pyuavcan',
+    version='0.1',
+    description='UAVCAN for Python',
+    packages=['pyuavcan', 'pyuavcan.dsdl'],
+    author='Pavel Kirienko',
+    author_email='pavel.kirienko@gmail.com',
+    url='http://uavcan.org',
+    license='MIT'
+)
+
+setup(**args)
