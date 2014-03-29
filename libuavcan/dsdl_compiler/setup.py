18e4d919c1ae83b4097f09f2786afa6fafdc068b
Pavel Kirienko <pavel.kirienko@gmail.com>
2014-03-29 14:54:26 +0400

    uavcan_dsdlc renamed to libuavcan_dsdlc

--- libuavcan/dsdl_compiler/setup.py
+++ libuavcan/dsdl_compiler/setup.py
@@ -3,12 +3,12 @@
 from distutils.core import setup
 
 args = dict(
-    name='uavcan_dsdlc',
+    name='libuavcan_dsdl_compiler',
     version='0.1',
     description='UAVCAN DSDL compiler for libuavcan',
     packages=['libuavcan_dsdl_compiler'],
     package_data={'libuavcan_dsdl_compiler': ['data_type_template.tmpl']},
-    scripts=['uavcan_dsdlc'],
+    scripts=['libuavcan_dsdlc'],
     requires=['mako', 'pyuavcan'],
     author='Pavel Kirienko',
     author_email='pavel.kirienko@gmail.com',
