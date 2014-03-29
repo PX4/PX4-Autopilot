21152e00db30d00b529b3843b2cbed7116ea4eb3
Pavel Kirienko <pavel.kirienko@gmail.com>
2014-03-29 15:31:06 +0400

    Renamed dsdlc target, library made static, added linux drivers project (empty yet)

---
+++ libuavcan_drivers/linux/test/log_subscriber.cpp
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2014 Pavel Kirienko <pavel.kirienko@gmail.com>
+ */
+
+#include <iostream>
+#include <uavcan/uavcan.hpp>
+#include <uavcan_linux/socketcan.hpp>
+#include <uavcan_linux/clock.hpp>
+
+int main()
+{
+    std::cout << "Hello owl." << std::endl;
+    return 0;
+}
