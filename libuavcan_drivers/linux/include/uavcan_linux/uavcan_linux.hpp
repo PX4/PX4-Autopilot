3df6f958f7faa7936a447f6a57af5e3e122b1c4b
Pavel Kirienko <pavel.kirienko@gmail.com>
2014-03-30 18:58:30 +0400

    Added clock test, superheader uavcan_linux.hpp

---
+++ libuavcan_drivers/linux/include/uavcan_linux/uavcan_linux.hpp
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2014 Pavel Kirienko <pavel.kirienko@gmail.com>
+ */
+
+#pragma once
+
+#include <uavcan/uavcan.hpp>
+
+#include <uavcan_linux/clock.hpp>
+#include <uavcan_linux/socketcan.hpp>
