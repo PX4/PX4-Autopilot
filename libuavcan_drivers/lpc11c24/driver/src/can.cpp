091356c1ba8ba815dbe89851d27f50c7bd7f5f63
Pavel Kirienko <pavel.kirienko@gmail.com>
2014-04-16 19:15:52 +0400

    LPC11C24: Reduced RX queue len

--- libuavcan_drivers/lpc11c24/driver/src/can.cpp
+++ libuavcan_drivers/lpc11c24/driver/src/can.cpp
@@ -11,7 +11,7 @@
  * The default value should be OK for any use case.
  */
 #ifndef UAVCAN_LPC11C24_RX_QUEUE_LEN
-# define UAVCAN_LPC11C24_RX_QUEUE_LEN   10
+# define UAVCAN_LPC11C24_RX_QUEUE_LEN   8
 #endif
 
 #if UAVCAN_LPC11C24_RX_QUEUE_LEN > 254
