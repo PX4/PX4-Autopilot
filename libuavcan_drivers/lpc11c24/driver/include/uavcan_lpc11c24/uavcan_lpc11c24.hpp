2a03b21aba75fcae380b0970d1c9de42e1a1354b
Pavel Kirienko <pavel.kirienko@gmail.com>
2014-04-16 13:09:03 +0400

    LPC11C24 superheader

---
+++ libuavcan_drivers/lpc11c24/driver/include/uavcan_lpc11c24/uavcan_lpc11c24.hpp
@@ -0,0 +1,9 @@
+/*
+ * Copyright (C) 2014 Pavel Kirienko <pavel.kirienko@gmail.com>
+ */
+
+#pragma once
+
+#include <uavcan/uavcan.hpp>
+#include <uavcan_lpc11c24/can.hpp>
+#include <uavcan_lpc11c24/clock.hpp>
