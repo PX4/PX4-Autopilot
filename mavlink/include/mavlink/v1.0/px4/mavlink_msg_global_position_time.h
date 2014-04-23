98f177057345ba9698e0428f8e8d17affa8fdd19
Anton Babushkin <anton.babushkin@me.com>
2014-04-23 22:11:34 +0200

    mavlink headers updated

--- mavlink/include/mavlink/v1.0/px4/mavlink_msg_global_position_time.h
+++ mavlink/include/mavlink/v1.0/px4/mavlink_msg_global_position_time.h
@@ -1,6 +1,6 @@
 // MESSAGE GLOBAL_POSITION_TIME PACKING
 
-#define MAVLINK_MSG_ID_GLOBAL_POSITION_TIME 126
+#define MAVLINK_MSG_ID_GLOBAL_POSITION_TIME 127
 
 typedef struct __mavlink_global_position_time_t
 {
@@ -15,10 +15,10 @@ typedef struct __mavlink_global_position_time_t
 } mavlink_global_position_time_t;
 
 #define MAVLINK_MSG_ID_GLOBAL_POSITION_TIME_LEN 34
-#define MAVLINK_MSG_ID_126_LEN 34
+#define MAVLINK_MSG_ID_127_LEN 34
 
 #define MAVLINK_MSG_ID_GLOBAL_POSITION_TIME_CRC 237
-#define MAVLINK_MSG_ID_126_CRC 237
+#define MAVLINK_MSG_ID_127_CRC 237
 
 
 
@@ -222,6 +222,52 @@ static inline void mavlink_msg_global_position_time_send(mavlink_channel_t chan,
 #endif
 #endif
 }
+
+#if MAVLINK_MSG_ID_GLOBAL_POSITION_TIME_LEN <= MAVLINK_MAX_PAYLOAD_LEN
+/*
+  This varient of _send() can be used to save stack space by re-using
+  memory from the receive buffer.  The caller provides a
+  mavlink_message_t which is the size of a full mavlink message. This
+  is usually the receive buffer for the channel, and allows a reply to an
+  incoming message with minimum stack space usage.
+ */
+static inline void mavlink_msg_global_position_time_send_buf(mavlink_message_t *msgbuf, mavlink_channel_t chan,  uint64_t time, int32_t lat, int32_t lon, float alt, float vx, float vy, float vz, uint16_t hdg)
+{
+#if MAVLINK_NEED_BYTE_SWAP || !MAVLINK_ALIGNED_FIELDS
+	char *buf = (char *)msgbuf;
+	_mav_put_uint64_t(buf, 0, time);
+	_mav_put_int32_t(buf, 8, lat);
+	_mav_put_int32_t(buf, 12, lon);
+	_mav_put_float(buf, 16, alt);
+	_mav_put_float(buf, 20, vx);
+	_mav_put_float(buf, 24, vy);
+	_mav_put_float(buf, 28, vz);
+	_mav_put_uint16_t(buf, 32, hdg);
+
+#if MAVLINK_CRC_EXTRA
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_GLOBAL_POSITION_TIME, buf, MAVLINK_MSG_ID_GLOBAL_POSITION_TIME_LEN, MAVLINK_MSG_ID_GLOBAL_POSITION_TIME_CRC);
+#else
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_GLOBAL_POSITION_TIME, buf, MAVLINK_MSG_ID_GLOBAL_POSITION_TIME_LEN);
+#endif
+#else
+	mavlink_global_position_time_t *packet = (mavlink_global_position_time_t *)msgbuf;
+	packet->time = time;
+	packet->lat = lat;
+	packet->lon = lon;
+	packet->alt = alt;
+	packet->vx = vx;
+	packet->vy = vy;
+	packet->vz = vz;
+	packet->hdg = hdg;
+
+#if MAVLINK_CRC_EXTRA
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_GLOBAL_POSITION_TIME, (const char *)packet, MAVLINK_MSG_ID_GLOBAL_POSITION_TIME_LEN, MAVLINK_MSG_ID_GLOBAL_POSITION_TIME_CRC);
+#else
+    _mav_finalize_message_chan_send(chan, MAVLINK_MSG_ID_GLOBAL_POSITION_TIME, (const char *)packet, MAVLINK_MSG_ID_GLOBAL_POSITION_TIME_LEN);
+#endif
+#endif
+}
+#endif
 
 #endif
 
