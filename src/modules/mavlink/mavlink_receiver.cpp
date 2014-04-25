7e1ea35571149a9d766cc7122738f206f1ed3427
Thomas Gubler <thomasgubler@gmail.com>
2014-04-25 09:43:19 +0200

    mavlink receiver: in hil use map_projection_timestamp for hil_local_pos.ref_timestamp

--- src/modules/mavlink/mavlink_receiver.cpp
+++ src/modules/mavlink/mavlink_receiver.cpp
@@ -795,7 +795,7 @@ MavlinkReceiver::handle_message_hil_state_quaternion(mavlink_message_t *msg)
 		if (!_hil_local_proj_inited) {
 			_hil_local_proj_inited = true;
 			_hil_local_alt0 = hil_state.alt / 1000.0f;
-			hil_local_pos.ref_timestamp = timestamp;
+			hil_local_pos.ref_timestamp = map_projection_timestamp();
 			hil_local_pos.ref_lat = lat;
 			hil_local_pos.ref_lon = lon;
 			hil_local_pos.ref_alt = _hil_local_alt0;
