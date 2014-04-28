9c81ab113e73c8862cc3f4e81411cb69dbec14ee
Lorenz Meier <lm@inf.ethz.ch>
2014-04-28 20:42:46 +0200

    Updated outo-test

--- Tools/tests-host/autodeclination_test.cpp
+++ Tools/tests-host/autodeclination_test.cpp
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+#include <stdlib.h> 
 #include <unistd.h>
 #include <string.h>
 #include <systemlib/mixer/mixer.h>
@@ -7,6 +8,7 @@
 #include <drivers/drv_hrt.h>
 #include <px4iofirmware/px4io.h>
 #include "../../src/systemcmds/tests/tests.h"
+#include <geo/geo.h>
 
 int main(int argc, char *argv[]) {
 	warnx("autodeclination test started");
@@ -14,6 +16,13 @@ int main(int argc, char *argv[]) {
 	if (argc < 3)
 		errx(1, "Need lat/lon!");
 
-	
+	char* p_end;
+
+	float lat = strtod(argv[1], &p_end);
+	float lon = strtod(argv[2], &p_end);
+
+	float declination = get_mag_declination(lat, lon);
+
+	printf("lat: %f lon: %f, dec: %f\n", lat, lon, declination);
 
 }
