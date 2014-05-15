3f9028b728e64104aa21adfd1f0c3b16a80ba016
Lorenz Meier <lm@inf.ethz.ch>
2014-05-15 07:29:43 +0200

    mc_att_control: Reduce stack mildly by 50 bytes

--- src/modules/mc_att_control/mc_att_control_main.cpp
+++ src/modules/mc_att_control/mc_att_control_main.cpp
@@ -818,7 +818,7 @@ MulticopterAttitudeControl::start()
 	_control_task = task_spawn_cmd("mc_att_control",
 				       SCHED_DEFAULT,
 				       SCHED_PRIORITY_MAX - 5,
-				       2048,
+				       2000,
 				       (main_t)&MulticopterAttitudeControl::task_main_trampoline,
 				       nullptr);
 
