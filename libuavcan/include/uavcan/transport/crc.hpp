95cdeff49ea681c2d8fb9cb0743e930ee4806d61
Pavel Kirienko <pavel.kirienko@gmail.com>
2014-06-16 11:22:21 +0400

    Explicit cast in transport CRC computer

--- libuavcan/include/uavcan/transport/crc.hpp
+++ libuavcan/include/uavcan/transport/crc.hpp
@@ -42,7 +42,7 @@ public:
 #if UAVCAN_TINY
     void add(uint8_t byte)
     {
-        value_ ^= byte << 8;
+        value_ ^= static_cast<uint16_t>(byte) << 8;
         for (uint8_t bit = 8; bit > 0; --bit)
         {
             if (value_ & 0x8000)
