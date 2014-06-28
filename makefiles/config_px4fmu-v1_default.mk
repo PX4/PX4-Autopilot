e78d496e921e9c50a3efc48a0a5875be1616c788
Lorenz Meier <lm@inf.ethz.ch>
2014-06-28 12:51:22 +0200

    Enable new lookup lib

--- makefiles/config_px4fmu-v1_default.mk
+++ makefiles/config_px4fmu-v1_default.mk
@@ -113,6 +113,7 @@ MODULES		+= lib/mathlib/math/filter
 MODULES		+= lib/ecl
 MODULES		+= lib/external_lgpl
 MODULES		+= lib/geo
+MODULES		+= lib/geo_lookup
 MODULES		+= lib/conversion
 MODULES		+= lib/launchdetection
 
