b060cf7e0de69b6d27ef1d815b0b7f7a93b23608
Pavel Kirienko <pavel.kirienko@gmail.com>
2014-07-11 19:13:41 +0400

    Refined pyuavcan docs

--- pyuavcan/pyuavcan/dsdl/__init__.py
+++ pyuavcan/pyuavcan/dsdl/__init__.py
@@ -4,6 +4,7 @@
 
 '''
 This module implements a fully compliant UAVCAN DSDL parser.
+Please read the specs at http://uavcan.org.
 '''
 
 from .parser import Parser, parse_namespaces, \
@@ -11,3 +12,8 @@ from .parser import Parser, parse_namespaces, \
                     Attribute, Field, Constant
 
 from .common import DsdlException
+
+__all__ = ['Parser', 'parse_namespaces',
+           'Type', 'PrimitiveType', 'ArrayType', 'CompoundType',
+           'Attribute', 'Field', 'Constant',
+           'DsdlException']
