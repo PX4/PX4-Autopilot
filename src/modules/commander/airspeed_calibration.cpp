56ad0c708d2695515489802676cc295f040b3606
Lorenz Meier <lm@inf.ethz.ch>
2014-07-13 22:53:50 +0200

    Fixed compile error

--- src/modules/commander/airspeed_calibration.cpp
+++ src/modules/commander/airspeed_calibration.cpp
@@ -162,7 +162,7 @@ int do_airspeed_calibration(int mavlink_fd)
 		return ERROR;
 	}
 
-	mavlink_log_critical(mavlink_fd, "Stored offset of %d Pa", (int)calibrated_pa);
+	mavlink_log_critical(mavlink_fd, "Stored offset of %d Pa", (int)diff_pres_offset);
 
 	/* wait 500 ms to ensure parameter propagated through the system */
 	usleep(500 * 1000);
