6ba63c41ada0a96763b8483e84943ff3a0f43814
Lorenz Meier <lm@inf.ethz.ch>
2014-10-11 00:26:20 +0200

    Support three GPS modules

--- src/modules/uORB/topics/vehicle_gps_position.h
+++ src/modules/uORB/topics/vehicle_gps_position.h
@@ -91,5 +91,6 @@ struct vehicle_gps_position_s {
 /* register this as object request broker structure */
 ORB_DECLARE(vehicle_gps_position_0);
 ORB_DECLARE(vehicle_gps_position_1);
+ORB_DECLARE(vehicle_gps_position_2);
 
 #endif
