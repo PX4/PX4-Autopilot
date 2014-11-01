91f170cf3d55d7a9b2afce646fc33ac73bb52873
James Goppert <james.goppert@gmail.com>
2014-11-01 18:45:40 -0400

    Encoder logging support.

--- src/modules/sdlog2/sdlog2_messages.h
+++ src/modules/sdlog2/sdlog2_messages.h
@@ -407,6 +407,16 @@ struct log_VISN_s {
 	float qw;
 };
 
+/* --- ENCODERS - ENCODER DATA --- */
+#define LOG_ENCODERS_MSG 39
+struct log_ENCODERS_s {
+	int64_t counts_0;
+	float velocity_0;
+	int64_t counts_1;
+	float velocity_1;
+};
+
+
 /********** SYSTEM MESSAGES, ID > 0x80 **********/
 
 /* --- TIME - TIME STAMP --- */
@@ -471,6 +481,7 @@ static const struct log_format_s log_formats[] = {
 	LOG_FORMAT(GS1B, "BBBBBBBBBBBBBBBB",	"s0,s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15"),
 	LOG_FORMAT(TECS, "fffffffffffffB",	"ASP,AF,FSP,F,FF,AsSP,AsF,AsDSP,AsD,TERSP,TER,EDRSP,EDR,M"),
 	LOG_FORMAT(WIND, "ffff",	"X,Y,CovX,CovY"),
+	LOG_FORMAT(ENCODERS, "qfqf",	"counts_0, velocity_0, counts_1, velocity_1"),
 
 	/* system-level messages, ID >= 0x80 */
 	/* FMT: don't write format of format message, it's useless */
