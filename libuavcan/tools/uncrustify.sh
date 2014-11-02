f97b21c89ed01136224648d91e656dae32477be2
Pavel Kirienko <pavel.kirienko@gmail.com>
2014-11-02 22:19:17 +0300

    uncrustify config moved to https://github.com/Zubax/zubax_style_guide

--- libuavcan/tools/uncrustify.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#files="$files $(find include -name '*.hpp')"
-#files="$files $(find src -name '*.cpp')"
-#files="$files $(find test -name '*.cpp')"
-#files="$files $(find test -name '*.hpp')"
-
-uncrustify --replace --no-backup -c uncrustify.cfg $files
