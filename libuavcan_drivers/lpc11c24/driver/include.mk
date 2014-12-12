eda4f4b879863199fb879d5b9146efb368531d5b
Pavel Kirienko <pavel.kirienko@gmail.com>
2014-12-12 17:45:54 +0300

    With 2074a3e1455826351612a5bdfe9e391744afa12d

--- libuavcan_drivers/lpc11c24/driver/include.mk
+++ libuavcan_drivers/lpc11c24/driver/include.mk
@@ -4,6 +4,6 @@
 
 LIBUAVCAN_LPC11C24_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
-LIBUAVCAN_LPC11C24_SRC := $(shell find $(LIBUAVCAN_LPC11C24_DIR)/src/ -type f -name '*.cpp')
+LIBUAVCAN_LPC11C24_SRC := $(shell find $(LIBUAVCAN_LPC11C24_DIR)/src -type f -name '*.cpp')
 
 LIBUAVCAN_LPC11C24_INC := $(LIBUAVCAN_LPC11C24_DIR)/include/
