55112fd0169b941a0d6fab21d4a11ddfcdc0c577
Lorenz Meier <lm@inf.ethz.ch>
2014-12-20 18:51:52 +0100

    Improve mixer test, no firm checks yet

--- unittests/mixer_test.cpp
+++ unittests/mixer_test.cpp
@@ -3,12 +3,17 @@
 #include "../../src/systemcmds/tests/tests.h"
 
 int main(int argc, char *argv[]) {
+
+	int ret;
+
 	warnx("Host execution started");
 
-	char* args[] = {argv[0], "../../ROMFS/px4fmu_common/mixers/IO_pass.mix",
-				 "../../ROMFS/px4fmu_common/mixers/FMU_quad_w.mix"};
+	char* args[] = {argv[0], "../ROMFS/px4fmu_common/mixers/IO_pass.mix",
+				 "../ROMFS/px4fmu_common/mixers/FMU_quad_w.mix"};
 
-	test_mixer(3, args);
+	if (ret = test_mixer(3, args));
 
 	test_conv(1, args);
+
+	return 0;
 }
