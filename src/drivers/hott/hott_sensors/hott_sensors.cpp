06aa3fceee670ca510cea3d148634c0daf3c7911
Lorenz Meier <lm@inf.ethz.ch>
2015-01-04 12:05:59 +0100

    Hott driver: Feedback cleanup

--- src/drivers/hott/hott_sensors/hott_sensors.cpp
+++ src/drivers/hott/hott_sensors/hott_sensors.cpp
@@ -157,7 +157,7 @@ hott_sensors_thread_main(int argc, char *argv[])
 	/* enable UART, writes potentially an empty buffer, but multiplexing is disabled */
 	const int uart = open_uart(device);
 	if (uart < 0) {
-		errx(1, "Failed opening HoTT UART, exiting.");
+		errx(1, "Open fail, exiting.");
 		thread_running = false;
 	}
 
