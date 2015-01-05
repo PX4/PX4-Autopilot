18bf501992afa8b561822615104c3868091429b9
Anton Matosov <anton.matosov@gmail.com>
2015-01-05 21:59:56 +0200

    Added output of the generated multi_tables to the terminal as well as to the file Changed compiler C++0x enabling flag to check if it is a cuase Updated copyrights

--- src/modules/systemlib/mixer/multi_tables.mk
+++ src/modules/systemlib/mixer/multi_tables.mk
@@ -1,6 +1,6 @@
 ############################################################################
 #
-#   Copyright (c) 2012, 2013 PX4 Development Team. All rights reserved.
+#   Copyright (c) 2014 Anton Matosov <anton.matosov@gmail.com>. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -38,4 +38,6 @@ SELF_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 $(SELF_DIR)mixer_multirotor.cpp : $(SELF_DIR)mixer_multirotor.generated.h
 
 $(SELF_DIR)mixer_multirotor.generated.h : $(SELF_DIR)multi_tables
-	$(SELF_DIR)multi_tables> $(SELF_DIR)mixer_multirotor.generated.h
+	$(SELF_DIR)multi_tables > $(SELF_DIR)mixer_multirotor.generated.h
+	$(SELF_DIR)multi_tables
+	
