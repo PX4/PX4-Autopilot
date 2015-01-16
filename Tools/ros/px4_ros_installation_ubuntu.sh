43f3e3ac789d7af15b41ba5f857cc8596b70999d
Andreas Antener <antener_a@gmx.ch>
2015-01-16 18:21:06 +0100

    adding drcsim binary installation

--- Tools/ros/px4_ros_installation_ubuntu.sh
+++ Tools/ros/px4_ros_installation_ubuntu.sh
@@ -2,14 +2,17 @@
 
 # main ROS Setup
 # following http://wiki.ros.org/indigo/Installation/Ubuntu
+# also adding dependencies for gazebo http://gazebosim.org/tutorials?tut=drcsim_install
 # run this file with . ./px4_ros_setup_ubuntu.sh
 
 ## add ROS repository
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo sh -c 'echo "deb http://packages.osrfoundation.org/drc/ubuntu trusty main" > /etc/apt/sources.list.d/drc-latest.list'
 
 ## add key
 wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | \
     sudo apt-key add -
+wget http://packages.osrfoundation.org/drc.key -O - | sudo apt-key add -
 
 ## Install main ROS pacakges
 sudo apt-get update
@@ -26,4 +29,5 @@ sudo apt-get install python-rosinstall
 
 # additional dependencies necessary
 sudo apt-get install ros-indigo-octomap-msgs
+sudo apt-get install drcsim
 
