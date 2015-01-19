fa4d09018527676d3803582a432df8e25c8e5010
Lorenz Meier <lm@inf.ethz.ch>
2015-01-19 20:18:44 +0100

    Hotfix: Fix EKF estimator and remove debug output which should not have been on master anyway

--- src/modules/ekf_att_pos_estimator/ekf_att_pos_estimator_main.cpp
+++ src/modules/ekf_att_pos_estimator/ekf_att_pos_estimator_main.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2013, 2014 PX4 Development Team. All rights reserved.
+ *   Copyright (c) 2013-2015 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -1517,12 +1517,6 @@ FixedwingEstimator::task_main()
 						_global_pos.epv = _gps.epv;
 
 						_global_pos.timestamp = _local_pos.timestamp;
-
-						// FIXME: usurp terrain alt field for baro_gps_offset
-						_global_pos.terrain_alt = _baro_gps_offset;
-						_global_pos.terrain_alt_valid = true;
-						_global_pos.eph = _baro_alt_filt;
-						_global_pos.epv = _gps_alt_filt;
 
 						/* lazily publish the global position only once available */
 						if (_global_pos_pub > 0) {
