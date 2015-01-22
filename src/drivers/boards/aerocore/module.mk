c273adf79e087e36ee1eb4a95b30b6d1f4d3c40c
David Sidrane <david_s5@nscdg.com>
2015-01-22 12:28:18 -1000

    Added Support for C++ static initializers for all boards. Including compile time checking for proper C++ configuration and not calling up_cxxinitialize redundantly

--- src/drivers/boards/aerocore/module.mk
+++ src/drivers/boards/aerocore/module.mk
@@ -7,4 +7,10 @@ SRCS		 = aerocore_init.c \
 		   aerocore_spi.c \
 		   aerocore_led.c
 
+ENABLE_CXXINITIALIZE=$(call check_nuttx_config ,"CONFIG_HAVE_CXX 1", $(NUTTX_CONFIG_H))
+ENABLE_CXXINITIALIZE+=$(call check_nuttx_config ,"CONFIG_HAVE_CXXINITIALIZE 1", $(NUTTX_CONFIG_H))
+ifeq ("$(ENABLE_CXXINITIALIZE)",$(nuttx_config_2true))
+SRCS		 +=	   ../../../modules/systemlib/up_cxxinitialize.c
+endif
+
 MAXOPTIMIZATION	 = -Os
