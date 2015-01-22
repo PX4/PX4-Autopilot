eddf288965f8241200c7e6ad6eef398fce32462b
David Sidrane <david_s5@nscdg.com>
2015-01-22 12:28:18 -1000

    Added Support for C++ static initializers for all boards. Including compile time checking for proper C++ configuration and not calling up_cxxinitialize redundantly

--- src/drivers/boards/px4io-v1/module.mk
+++ src/drivers/boards/px4io-v1/module.mk
@@ -5,4 +5,10 @@
 SRCS	= px4io_init.c \
 	  px4io_pwm_servo.c
 
+ENABLE_CXXINITIALIZE=$(call check_nuttx_config ,"CONFIG_HAVE_CXX 1", $(NUTTX_CONFIG_H))
+ENABLE_CXXINITIALIZE+=$(call check_nuttx_config ,"CONFIG_HAVE_CXXINITIALIZE 1", $(NUTTX_CONFIG_H))
+ifeq ("$(ENABLE_CXXINITIALIZE)",$(nuttx_config_2true))
+SRCS		 +=	   ../../../modules/systemlib/up_cxxinitialize.c
+endif
+
 MAXOPTIMIZATION	 = -Os
