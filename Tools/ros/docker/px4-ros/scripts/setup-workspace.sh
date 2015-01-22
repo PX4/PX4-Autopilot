353c230db5927b3e060bbfde9a0aa34e27abbe35
Andreas Antener <antener_a@gmx.ch>
2015-01-22 17:31:47 +0100

    restructured things a little bit

--- Tools/ros/docker/px4-ros/scripts/setup-workspace.sh
+++ Tools/ros/docker/px4-ros/scripts/setup-workspace.sh
@@ -1,28 +1,43 @@
 #!/bin/sh
 #
-# Fetch source repositories
+# Create workspace at current location and fetch source repositories
 #
 
+WDIR=`pwd`
+WORKSPACE=$WDIR/catkin_ws
+
+# Setup workspace
+mkdir -p $WORKSPACE/src
+cd $WORKSPACE/src
+catkin_init_workspace
+cd $WORKSPACE
+catkin_make
+echo "source $WORKSPACE/devel/setup.bash" >> ~/.bashrc
+
 # PX4 Firmware
+cd $WORKSPACE/src
 git clone https://github.com/PX4/Firmware.git \
 	&& cd Firmware \
-	&& git checkout ros \
-	&& cd ..
+	&& git checkout ros
 
 # euroc simulator
+cd $WORKSPACE/src
 git clone https://github.com/PX4/euroc_simulator.git \
 	&& cd euroc_simulator \
-	&& git checkout px4_nodes \
-	&& cd ..
+	&& git checkout px4_nodes
 
 # mav comm
+cd $WORKSPACE/src
 git clone https://github.com/PX4/mav_comm.git
 
 # glog catkin
+cd $WORKSPACE/src
 git clone https://github.com/ethz-asl/glog_catkin.git
 
 # catkin simple
+cd $WORKSPACE/src
 git clone https://github.com/catkin/catkin_simple.git
 
+cd $WORKSPACE
 echo "Execute catkin_make to compile all the sources."
 
