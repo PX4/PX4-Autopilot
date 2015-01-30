b7f36376078ac8a1ebca667a8ead908dd0dbd4de
Lorenz Meier <lm@inf.ethz.ch>
2015-01-30 10:16:51 +0100

    Fix compiler warning on prototypes

--- src/drivers/boards/aerocore/aerocore_init.c
+++ src/drivers/boards/aerocore/aerocore_init.c
@@ -111,7 +111,7 @@ __END_DECLS
  ****************************************************************************/
 
 #if defined(CONFIG_HAVE_CXX) && defined(CONFIG_HAVE_CXXINITIALIZE)
-__EXPORT extern void up_cxxinitialize();
+__EXPORT extern void up_cxxinitialize(void);
 #endif
 
 #if defined(CONFIG_FAT_DMAMEMORY)
