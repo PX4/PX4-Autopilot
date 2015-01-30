6a17ef8150697209939ae29b5c3b6d29479c479e
Lorenz Meier <lm@inf.ethz.ch>
2015-01-30 15:02:19 +0100

    Simplify v2 config

--- src/drivers/boards/px4fmu-v2/module.mk
+++ src/drivers/boards/px4fmu-v2/module.mk
@@ -7,13 +7,7 @@ SRCS		 = px4fmu_can.c \
 		   px4fmu_pwm_servo.c \
 		   px4fmu_spi.c \
 		   px4fmu_usb.c \
-		   px4fmu2_led.c
-
-
-ENABLE_CXXINITIALIZE=$(call check_nuttx_config ,"CONFIG_HAVE_CXX 1", $(NUTTX_CONFIG_H))
-ENABLE_CXXINITIALIZE+=$(call check_nuttx_config ,"CONFIG_HAVE_CXXINITIALIZE 1", $(NUTTX_CONFIG_H))
-ifeq ("$(ENABLE_CXXINITIALIZE)",$(nuttx_config_2true))
-SRCS		 +=	   ../../../modules/systemlib/up_cxxinitialize.c
-endif
+		   px4fmu2_led.c \
+		   ../../../modules/systemlib/up_cxxinitialize.c
 
 MAXOPTIMIZATION	 = -Os
