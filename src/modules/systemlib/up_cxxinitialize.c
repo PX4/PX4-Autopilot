3656ba81af9d35bbddc11f92f318bf3d2b8e186f
Lorenz Meier <lm@inf.ethz.ch>
2015-01-30 09:35:46 +0100

    Add export to up_cxxinitialize to make it globally visible

--- src/modules/systemlib/up_cxxinitialize.c
+++ src/modules/systemlib/up_cxxinitialize.c
@@ -126,7 +126,7 @@ extern uint32_t _etext;
  *
  ***************************************************************************/
 
-void up_cxxinitialize(void)
+__EXPORT void up_cxxinitialize(void)
 {
   initializer_t *initp;
 
