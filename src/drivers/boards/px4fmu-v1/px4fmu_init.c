32e2c05e46ccb0f7eddf07f02ef3f90913385b81
Lorenz Meier <lm@inf.ethz.ch>
2015-01-30 09:59:50 +0100

    FMUv1: board init

--- src/drivers/boards/px4fmu-v1/px4fmu_init.c
+++ src/drivers/boards/px4fmu-v1/px4fmu_init.c
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2012, 2013 PX4 Development Team. All rights reserved.
+ *   Copyright (c) 2012-2015 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -112,6 +112,10 @@ __END_DECLS
 /****************************************************************************
  * Public Functions
  ****************************************************************************/
+
+#if defined(CONFIG_HAVE_CXX) && defined(CONFIG_HAVE_CXXINITIALIZE)
+__EXPORT extern void up_cxxinitialize();
+#endif
 
 /************************************************************************************
  * Name: stm32_boardinitialize
