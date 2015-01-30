6a269873dcb298b2e53ce0584922d6731c5ebfc4
Lorenz Meier <lm@inf.ethz.ch>
2015-01-30 15:00:11 +0100

    Simplify aerocore config

--- src/drivers/boards/aerocore/module.mk
+++ src/drivers/boards/aerocore/module.mk
@@ -5,12 +5,7 @@
 SRCS		 = aerocore_init.c \
 		   aerocore_pwm_servo.c \
 		   aerocore_spi.c \
-		   aerocore_led.c
-
-ENABLE_CXXINITIALIZE=$(call check_nuttx_config ,"CONFIG_HAVE_CXX 1", $(NUTTX_CONFIG_H))
-ENABLE_CXXINITIALIZE+=$(call check_nuttx_config ,"CONFIG_HAVE_CXXINITIALIZE 1", $(NUTTX_CONFIG_H))
-ifeq ("$(ENABLE_CXXINITIALIZE)",$(nuttx_config_2true))
-SRCS		 +=	   ../../../modules/systemlib/up_cxxinitialize.c
-endif
+		   aerocore_led.c \
+		   ../../../modules/systemlib/up_cxxinitialize.c
 
 MAXOPTIMIZATION	 = -Os
