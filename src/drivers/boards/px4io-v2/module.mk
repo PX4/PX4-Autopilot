fbd2ae1c347da65c5e54b0ca4401e0b1d45a509f
Lorenz Meier <lm@inf.ethz.ch>
2015-01-30 15:04:47 +0100

    IOv2: simplify config

--- src/drivers/boards/px4io-v2/module.mk
+++ src/drivers/boards/px4io-v2/module.mk
@@ -3,12 +3,7 @@
 #
 
 SRCS		= px4iov2_init.c \
-		  px4iov2_pwm_servo.c
-
-ENABLE_CXXINITIALIZE=$(call check_nuttx_config ,"CONFIG_HAVE_CXX 1", $(NUTTX_CONFIG_H))
-ENABLE_CXXINITIALIZE+=$(call check_nuttx_config ,"CONFIG_HAVE_CXXINITIALIZE 1", $(NUTTX_CONFIG_H))
-ifeq ("$(ENABLE_CXXINITIALIZE)",$(nuttx_config_2true))
-SRCS		 +=	   ../../../modules/systemlib/up_cxxinitialize.c
-endif
+		  px4iov2_pwm_servo.c \
+		  ../../../modules/systemlib/up_cxxinitialize.c
 
 MAXOPTIMIZATION	 = -Os
