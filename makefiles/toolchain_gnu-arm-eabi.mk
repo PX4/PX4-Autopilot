a9a7ef843cf6f38f5d44a2d0243b0b2e1137b646
Lorenz Meier <lm@inf.ethz.ch>
2015-01-31 01:13:05 +0100

    Allow GCC 4.7.4 in check

--- makefiles/toolchain_gnu-arm-eabi.mk
+++ makefiles/toolchain_gnu-arm-eabi.mk
@@ -1,5 +1,5 @@
 #
-#   Copyright (C) 2012-2014 PX4 Development Team. All rights reserved.
+#   Copyright (c) 2012-2015 PX4 Development Team. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -50,7 +50,7 @@ OBJDUMP			 = $(CROSSDEV)objdump
 
 # Check if the right version of the toolchain is available
 #
-CROSSDEV_VER_SUPPORTED	 = 4.8.4 4.9.3
+CROSSDEV_VER_SUPPORTED	 = 4.7.4 4.7.5 4.7.6 4.8.4 4.9.3
 CROSSDEV_VER_FOUND	 = $(shell $(CC) -dumpversion)
 
 ifeq (,$(findstring $(CROSSDEV_VER_FOUND), $(CROSSDEV_VER_SUPPORTED)))
