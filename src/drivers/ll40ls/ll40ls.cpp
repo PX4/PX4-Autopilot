2f4d820063b7712a3acae8f5336439ebbd43fe80
Lorenz Meier <lm@inf.ethz.ch>
2015-02-03 13:47:08 +0100

    LL40S: Move to 0 based index

--- src/drivers/ll40ls/ll40ls.cpp
+++ src/drivers/ll40ls/ll40ls.cpp
@@ -235,7 +235,7 @@ LL40LS::~LL40LS()
 	}
 
 	if (_class_instance != -1) {
-		unregister_class_devname(RANGE_FINDER_DEVICE_PATH, _class_instance);
+		unregister_class_devname(RANGE_FINDER_BASE_DEVICE_PATH, _class_instance);
 	}
 
 	// free perf counters
@@ -261,7 +261,7 @@ LL40LS::init()
 		goto out;
 	}
 
-	_class_instance = register_class_devname(RANGE_FINDER_DEVICE_PATH);
+	_class_instance = register_class_devname(RANGE_FINDER_BASE_DEVICE_PATH);
 
 	if (_class_instance == CLASS_DEVICE_PRIMARY) {
 		/* get a publish handle on the range finder topic */
