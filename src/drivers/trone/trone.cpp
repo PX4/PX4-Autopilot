bd6945ba70dc287054a51eb9cc9a9430c041910d
Lorenz Meier <lm@inf.ethz.ch>
2015-02-03 13:49:45 +0100

    TeraRange One: move to 0 based index

--- src/drivers/trone/trone.cpp
+++ src/drivers/trone/trone.cpp
@@ -258,7 +258,7 @@ TRONE::~TRONE()
 	}
 
 	if (_class_instance != -1) {
-		unregister_class_devname(RANGE_FINDER_DEVICE_PATH, _class_instance);
+		unregister_class_devname(RANGE_FINDER_BASE_DEVICE_PATH, _class_instance);
 	}
 
 	// free perf counters
@@ -284,7 +284,7 @@ TRONE::init()
 		goto out;
 	}
 
-	_class_instance = register_class_devname(RANGE_FINDER_DEVICE_PATH);
+	_class_instance = register_class_devname(RANGE_FINDER_BASE_DEVICE_PATH);
 
 	if (_class_instance == CLASS_DEVICE_PRIMARY) {
 		/* get a publish handle on the range finder topic */
