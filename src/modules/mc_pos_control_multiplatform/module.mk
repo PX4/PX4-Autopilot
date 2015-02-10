b269e5d060e81696ced6af6303e27c936bbffbed
Lorenz Meier <lm@inf.ethz.ch>
2015-02-10 14:52:05 +0100

    multiplatform mc pos control: Allow enough frame size

--- src/modules/mc_pos_control_multiplatform/module.mk
+++ src/modules/mc_pos_control_multiplatform/module.mk
@@ -1,6 +1,6 @@
 ############################################################################
 #
-#   Copyright (c) 2013 PX4 Development Team. All rights reserved.
+#   Copyright (c) 2013-2015 PX4 Development Team. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -42,4 +42,4 @@ SRCS		= mc_pos_control_main.cpp \
 		  mc_pos_control.cpp \
 		  mc_pos_control_params.c
 
-EXTRACXXFLAGS = -Wframe-larger-than=1040
+EXTRACXXFLAGS = -Wframe-larger-than=1200
