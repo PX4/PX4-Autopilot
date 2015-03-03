66c49d0c58c68399f61c1d46a92d583aa3615f6b
Lorenz Meier <lm@inf.ethz.ch>
2015-03-03 03:13:31 +0100

    Disable estimator apps defaulting to off since they allocate RAM statically

--- makefiles/config_px4fmu-v1_default.mk
+++ makefiles/config_px4fmu-v1_default.mk
@@ -67,9 +67,9 @@ MODULES 	+= modules/land_detector
 #
 # Estimation modules (EKF / other filters)
 #
-MODULES		+= modules/attitude_estimator_ekf
+#MODULES		+= modules/attitude_estimator_ekf
 MODULES		+= modules/ekf_att_pos_estimator
-MODULES		+= modules/position_estimator_inav
+#MODULES		+= modules/position_estimator_inav
 
 #
 # Vehicle Control
