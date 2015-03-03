662ec3f62f6c271d7746204888c2d55e3c3bd86e
Daniel Agar <daniel@agar.ca>
2015-03-03 11:47:27 -0500

    examples fix code style

--- src/examples/publisher/publisher_example.h
+++ src/examples/publisher/publisher_example.h
@@ -40,7 +40,8 @@
 #pragma once
 #include <px4.h>
 
-class PublisherExample {
+class PublisherExample
+{
 public:
 	PublisherExample();
 
@@ -49,7 +50,7 @@ public:
 	int main();
 protected:
 	px4::NodeHandle _n;
-	px4::Publisher<px4::px4_rc_channels> * _rc_channels_pub;
-	px4::Publisher<px4::px4_vehicle_attitude> * _v_att_pub;
-	px4::Publisher<px4::px4_parameter_update> * _parameter_update_pub;
+	px4::Publisher<px4::px4_rc_channels> *_rc_channels_pub;
+	px4::Publisher<px4::px4_vehicle_attitude> *_v_att_pub;
+	px4::Publisher<px4::px4_parameter_update> *_parameter_update_pub;
 };
