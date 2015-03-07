61437a5587b20e7e3c79fd1ab91e945dc0c316fb
Lorenz Meier <lm@inf.ethz.ch>
2015-03-07 14:07:41 +0100

    MAVLink app: Do no allocate memory statically, but only on execution on stack.

--- src/modules/mavlink/mavlink_receiver.h
+++ src/modules/mavlink/mavlink_receiver.h
@@ -182,6 +182,8 @@ private:
 	bool _hil_local_proj_inited;
 	float _hil_local_alt0;
 	struct map_projection_reference_s _hil_local_proj_ref;
+	struct offboard_control_mode_s _offboard_control_mode;
+	struct vehicle_rates_setpoint_s _rates_sp;
 	double _time_offset_avg_alpha;
 	uint64_t _time_offset;
 
