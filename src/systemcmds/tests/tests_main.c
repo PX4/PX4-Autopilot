b80908d95677fa927152ed0cf6e49e27e1780543
Lorenz Meier <lm@inf.ethz.ch>
2015-03-20 00:05:32 +0100

    Disable eigen test until fixed

--- src/systemcmds/tests/tests_main.c
+++ src/systemcmds/tests/tests_main.c
@@ -112,7 +112,7 @@ const struct {
 #ifndef TESTS_MATHLIB_DISABLE
 	{"mathlib",		test_mathlib,	0},
 #endif
-	{"eigen",		test_eigen,	0},
+	{"eigen",		test_eigen,	OPT_NOALLTEST},
 	{"help",		test_help,	OPT_NOALLTEST | OPT_NOHELP | OPT_NOJIGTEST},
 	{NULL,			NULL, 		0}
 };
