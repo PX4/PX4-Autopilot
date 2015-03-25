7554eb515a9b87658e860de3ceee552beff610ee
Lorenz Meier <lm@inf.ethz.ch>
2015-03-25 01:02:29 -0700

    Fix ROS install instructions

--- Tools/ros/px4_workspace_setup.sh
+++ Tools/ros/px4_workspace_setup.sh
@@ -10,9 +10,6 @@ git clone https://github.com/PX4/Firmware.git
 
 # euroc simulator
 git clone https://github.com/PX4/euroc_simulator.git
-cd euroc_simulator
-git checkout px4_nodes
-cd ..
 
 # mav comm
 git clone https://github.com/PX4/mav_comm.git
