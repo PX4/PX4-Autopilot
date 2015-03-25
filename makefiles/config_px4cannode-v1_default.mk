7cb0369e6fa102e64029bed6a3783da18ce2136e
David Sidrane <david_s5@nscdg.com>
2015-03-25 11:24:43 -1000

    Builds and runs - needs testing

--- makefiles/config_px4cannode-v1_default.mk
+++ makefiles/config_px4cannode-v1_default.mk
@@ -16,13 +16,18 @@ MODULES		+= drivers/boards/px4cannode-v1
 MODULES		+= systemcmds/reboot
 MODULES		+= systemcmds/top
 MODULES		+= systemcmds/config
-MODULES		+= systemcmds/nshterm
 MODULES		+= systemcmds/ver
+
+#
+# General system control
+#
+MODULES		+= modules/uavcannode
 
 #
 # Library modules
 #
 MODULES		+= modules/systemlib
+
 
 
 #
@@ -31,7 +36,26 @@ MODULES		+= modules/systemlib
 #MODULES 	+= modules/unit_test
 #MODULES 	+= modules/commander/commander_tests
 
+# Generate parameter XML file
+GEN_PARAM_XML = 1
+
+
 #
 # Demo apps
 #
+
+#
+# Transitional support - add commands from the NuttX export archive.
+#
+# In general, these should move to modules over time.
+#
+# Each entry here is <command>.<priority>.<stacksize>.<entrypoint> but we use a helper macro
+# to make the table a bit more readable.
+#
+define _B
+	$(strip $1).$(or $(strip $2),SCHED_PRIORITY_DEFAULT).$(or $(strip $3),CONFIG_PTHREAD_STACK_DEFAULT).$(strip $4)
+endef
+
+BUILTIN_COMMANDS := \
+    $(call _B, null, , 60, null_main) 
 
