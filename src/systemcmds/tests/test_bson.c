32e790110e766c0f6fbd5c439d15f2649df2c542
Lorenz Meier <lm@inf.ethz.ch>
2015-03-28 19:51:15 +0100

    Tests: Fix code style on system tests

--- src/systemcmds/tests/test_bson.c
+++ src/systemcmds/tests/test_bson.c
@@ -58,18 +58,29 @@ static const uint8_t sample_data[256] = {0};
 static int
 encode(bson_encoder_t encoder)
 {
-	if (bson_encoder_append_bool(encoder, "bool1", sample_bool) != 0)
+	if (bson_encoder_append_bool(encoder, "bool1", sample_bool) != 0) {
 		warnx("FAIL: encoder: append bool failed");
-	if (bson_encoder_append_int(encoder, "int1", sample_small_int) != 0)
+	}
+
+	if (bson_encoder_append_int(encoder, "int1", sample_small_int) != 0) {
 		warnx("FAIL: encoder: append int failed");
-	if (bson_encoder_append_int(encoder, "int2", sample_big_int) != 0)
+	}
+
+	if (bson_encoder_append_int(encoder, "int2", sample_big_int) != 0) {
 		warnx("FAIL: encoder: append int failed");
-	if (bson_encoder_append_double(encoder, "double1", sample_double) != 0)
+	}
+
+	if (bson_encoder_append_double(encoder, "double1", sample_double) != 0) {
 		warnx("FAIL: encoder: append double failed");
-	if (bson_encoder_append_string(encoder, "string1", sample_string) != 0)
+	}
+
+	if (bson_encoder_append_string(encoder, "string1", sample_string) != 0) {
 		warnx("FAIL: encoder: append string failed");
-	if (bson_encoder_append_binary(encoder, "data1", BSON_BIN_BINARY, sizeof(sample_data), sample_data) != 0)
+	}
+
+	if (bson_encoder_append_binary(encoder, "data1", BSON_BIN_BINARY, sizeof(sample_data), sample_data) != 0) {
 		warnx("FAIL: encoder: append data failed");
+	}
 
 	bson_encoder_fini(encoder);
 
@@ -86,51 +97,63 @@ decode_callback(bson_decoder_t decoder, void *private, bson_node_t node)
 			warnx("FAIL: decoder: bool1 type %d, expected %d", node->type, BSON_BOOL);
 			return 1;
 		}
+
 		if (node->b != sample_bool) {
-			warnx("FAIL: decoder: bool1 value %s, expected %s", 
-				(node->b ? "true" : "false"),
-				(sample_bool ? "true" : "false"));
+			warnx("FAIL: decoder: bool1 value %s, expected %s",
+			      (node->b ? "true" : "false"),
+			      (sample_bool ? "true" : "false"));
 			return 1;
 		}
+
 		warnx("PASS: decoder: bool1");
 		return 1;
 	}
+
 	if (!strcmp(node->name, "int1")) {
 		if (node->type != BSON_INT32) {
 			warnx("FAIL: decoder: int1 type %d, expected %d", node->type, BSON_INT32);
 			return 1;
 		}
+
 		if (node->i != sample_small_int) {
 			warnx("FAIL: decoder: int1 value %lld, expected %d", node->i, sample_small_int);
 			return 1;
 		}
+
 		warnx("PASS: decoder: int1");
 		return 1;
 	}
+
 	if (!strcmp(node->name, "int2")) {
 		if (node->type != BSON_INT64) {
 			warnx("FAIL: decoder: int2 type %d, expected %d", node->type, BSON_INT64);
 			return 1;
 		}
+
 		if (node->i != sample_big_int) {
 			warnx("FAIL: decoder: int2 value %lld, expected %lld", node->i, sample_big_int);
 			return 1;
 		}
+
 		warnx("PASS: decoder: int2");
 		return 1;
 	}
+
 	if (!strcmp(node->name, "double1")) {
 		if (node->type != BSON_DOUBLE) {
 			warnx("FAIL: decoder: double1 type %d, expected %d", node->type, BSON_DOUBLE);
 			return 1;
 		}
+
 		if (fabs(node->d - sample_double) > 1e-12) {
 			warnx("FAIL: decoder: double1 value %f, expected %f", node->d, sample_double);
 			return 1;
 		}
+
 		warnx("PASS: decoder: double1");
 		return 1;
 	}
+
 	if (!strcmp(node->name, "string1")) {
 		if (node->type != BSON_STRING) {
 			warnx("FAIL: decoder: string1 type %d, expected %d", node->type, BSON_STRING);
@@ -150,21 +173,26 @@ decode_callback(bson_decoder_t decoder, void *private, bson_node_t node)
 			warnx("FAIL: decoder: string1 copy failed");
 			return 1;
 		}
+
 		if (bson_decoder_data_pending(decoder) != 0) {
 			warnx("FAIL: decoder: string1 copy did not exhaust all data");
 			return 1;
 		}
+
 		if (sbuf[len - 1] != '\0') {
 			warnx("FAIL: decoder: string1 not 0-terminated");
 			return 1;
 		}
+
 		if (strcmp(sbuf, sample_string)) {
 			warnx("FAIL: decoder: string1 value '%s', expected '%s'", sbuf, sample_string);
 			return 1;
 		}
+
 		warnx("PASS: decoder: string1");
 		return 1;
 	}
+
 	if (!strcmp(node->name, "data1")) {
 		if (node->type != BSON_BINDATA) {
 			warnx("FAIL: decoder: data1 type %d, expected %d", node->type, BSON_BINDATA);
@@ -177,7 +205,7 @@ decode_callback(bson_decoder_t decoder, void *private, bson_node_t node)
 			warnx("FAIL: decoder: data1 length %d, expected %d", len, sizeof(sample_data));
 			return 1;
 		}
-		
+
 		if (node->subtype != BSON_BIN_BINARY) {
 			warnx("FAIL: decoder: data1 subtype %d, expected %d", node->subtype, BSON_BIN_BINARY);
 			return 1;
@@ -189,20 +217,25 @@ decode_callback(bson_decoder_t decoder, void *private, bson_node_t node)
 			warnx("FAIL: decoder: data1 copy failed");
 			return 1;
 		}
+
 		if (bson_decoder_data_pending(decoder) != 0) {
 			warnx("FAIL: decoder: data1 copy did not exhaust all data");
 			return 1;
 		}
+
 		if (memcmp(sample_data, dbuf, len)) {
 			warnx("FAIL: decoder: data1 compare fail");
 			return 1;
 		}
+
 		warnx("PASS: decoder: data1");
 		return 1;
 	}
 
-	if (node->type != BSON_EOO)
+	if (node->type != BSON_EOO) {
 		warnx("FAIL: decoder: unexpected node name '%s'", node->name);
+	}
+
 	return 1;
 }
 
@@ -225,19 +258,28 @@ test_bson(int argc, char *argv[])
 	int len;
 
 	/* encode data to a memory buffer */
-	if (bson_encoder_init_buf(&encoder, NULL, 0))
+	if (bson_encoder_init_buf(&encoder, NULL, 0)) {
 		errx(1, "FAIL: bson_encoder_init_buf");
+	}
+
 	encode(&encoder);
 	len = bson_encoder_buf_size(&encoder);
-	if (len <= 0)
+
+	if (len <= 0) {
 		errx(1, "FAIL: bson_encoder_buf_len");
+	}
+
 	buf = bson_encoder_buf_data(&encoder);
-	if (buf == NULL)
+
+	if (buf == NULL) {
 		errx(1, "FAIL: bson_encoder_buf_data");
+	}
 
 	/* now test-decode it */
-	if (bson_decoder_init_buf(&decoder, buf, len, decode_callback, NULL))
+	if (bson_decoder_init_buf(&decoder, buf, len, decode_callback, NULL)) {
 		errx(1, "FAIL: bson_decoder_init_buf");
+	}
+
 	decode(&decoder);
 	free(buf);
 
--- src/systemcmds/tests/test_mixer.cpp
+++ src/systemcmds/tests/test_mixer.cpp
@@ -83,8 +83,9 @@ int test_mixer(int argc, char *argv[])
 
 	const char *filename = "/etc/mixers/IO_pass.mix";
 
-	if (argc > 1)
+	if (argc > 1) {
 		filename = argv[1];
+	}
 
 	warnx("loading: %s", filename);
 
@@ -108,8 +109,10 @@ int test_mixer(int argc, char *argv[])
 	unsigned xx = loaded;
 	mixer_group.load_from_buf(&buf[0], xx);
 	warnx("complete buffer load: loaded %u mixers", mixer_group.count());
-	if (mixer_group.count() != 8)
+
+	if (mixer_group.count() != 8) {
 		return 1;
+	}
 
 	unsigned empty_load = 2;
 	char empty_buf[2];
@@ -118,8 +121,10 @@ int test_mixer(int argc, char *argv[])
 	mixer_group.reset();
 	mixer_group.load_from_buf(&empty_buf[0], empty_load);
 	warnx("empty buffer load: loaded %u mixers, used: %u", mixer_group.count(), empty_load);
-	if (empty_load != 0)
+
+	if (empty_load != 0) {
 		return 1;
+	}
 
 	/* FIRST mark the mixer as invalid */
 	/* THEN actually delete it */
@@ -155,8 +160,9 @@ int test_mixer(int argc, char *argv[])
 			warnx("used %u", mixer_text_length - resid);
 
 			/* copy any leftover text to the base of the buffer for re-use */
-			if (resid > 0)
+			if (resid > 0) {
 				memcpy(&mixer_text[0], &mixer_text[mixer_text_length - resid], resid);
+			}
 
 			mixer_text_length = resid;
 		}
@@ -166,11 +172,12 @@ int test_mixer(int argc, char *argv[])
 
 	warnx("chunked load: loaded %u mixers", mixer_group.count());
 
-	if (mixer_group.count() != 8)
+	if (mixer_group.count() != 8) {
 		return 1;
+	}
 
 	/* execute the mixer */
-	
+
 	float	outputs[output_max];
 	unsigned mixed;
 	const int jmax = 5;
@@ -193,30 +200,31 @@ int test_mixer(int argc, char *argv[])
 	unsigned sleepcount = 0;
 
 	while (hrt_elapsed_time(&starttime) < INIT_TIME_US + RAMP_TIME_US) {
-		
+
 		/* mix */
 		mixed = mixer_group.mix(&outputs[0], output_max);
 
-		pwm_limit_calc(should_arm, mixed, r_page_servo_disarmed, r_page_servo_control_min, r_page_servo_control_max, outputs, r_page_servos, &pwm_limit);
+		pwm_limit_calc(should_arm, mixed, r_page_servo_disarmed, r_page_servo_control_min, r_page_servo_control_max, outputs,
+			       r_page_servos, &pwm_limit);
 
 		//warnx("mixed %d outputs (max %d), values:", mixed, output_max);
-		for (unsigned i = 0; i < mixed; i++)
-		{
+		for (unsigned i = 0; i < mixed; i++) {
 			/* check mixed outputs to be zero during init phase */
 			if (hrt_elapsed_time(&starttime) < INIT_TIME_US &&
-				r_page_servos[i] != r_page_servo_disarmed[i]) {
+			    r_page_servos[i] != r_page_servo_disarmed[i]) {
 				warnx("disarmed servo value mismatch");
 				return 1;
 			}
 
 			if (hrt_elapsed_time(&starttime) >= INIT_TIME_US &&
-				r_page_servos[i] + 1 <= r_page_servo_disarmed[i]) {
+			    r_page_servos[i] + 1 <= r_page_servo_disarmed[i]) {
 				warnx("ramp servo value mismatch");
 				return 1;
 			}
 
 			//printf("\t %d: %8.4f limited: %8.4f, servo: %d\n", i, (double)outputs_unlimited[i], (double)outputs[i], (int)r_page_servos[i]);
 		}
+
 		usleep(sleep_quantum_us);
 		sleepcount++;
 
@@ -225,6 +233,7 @@ int test_mixer(int argc, char *argv[])
 			fflush(stdout);
 		}
 	}
+
 	printf("\n");
 
 	warnx("ARMING TEST: NORMAL OPERATION");
@@ -232,7 +241,7 @@ int test_mixer(int argc, char *argv[])
 	for (int j = -jmax; j <= jmax; j++) {
 
 		for (unsigned i = 0; i < output_max; i++) {
-			actuator_controls[i] = j/10.0f + 0.1f * i;
+			actuator_controls[i] = j / 10.0f + 0.1f * i;
 			r_page_servo_disarmed[i] = PWM_LOWEST_MIN;
 			r_page_servo_control_min[i] = PWM_DEFAULT_MIN;
 			r_page_servo_control_max[i] = PWM_DEFAULT_MAX;
@@ -241,12 +250,14 @@ int test_mixer(int argc, char *argv[])
 		/* mix */
 		mixed = mixer_group.mix(&outputs[0], output_max);
 
-		pwm_limit_calc(should_arm, mixed, r_page_servo_disarmed, r_page_servo_control_min, r_page_servo_control_max, outputs, r_page_servos, &pwm_limit);
+		pwm_limit_calc(should_arm, mixed, r_page_servo_disarmed, r_page_servo_control_min, r_page_servo_control_max, outputs,
+			       r_page_servos, &pwm_limit);
 
 		warnx("mixed %d outputs (max %d)", mixed, output_max);
-		for (unsigned i = 0; i < mixed; i++)
-		{
+
+		for (unsigned i = 0; i < mixed; i++) {
 			servo_predicted[i] = 1500 + outputs[i] * (r_page_servo_control_max[i] - r_page_servo_control_min[i]) / 2.0f;
+
 			if (fabsf(servo_predicted[i] - r_page_servos[i]) > 2) {
 				printf("\t %d: %8.4f predicted: %d, servo: %d\n", i, (double)outputs[i], servo_predicted[i], (int)r_page_servos[i]);
 				warnx("mixer violated predicted value");
@@ -262,15 +273,15 @@ int test_mixer(int argc, char *argv[])
 	should_arm = false;
 
 	while (hrt_elapsed_time(&starttime) < 600000) {
-		
+
 		/* mix */
 		mixed = mixer_group.mix(&outputs[0], output_max);
 
-		pwm_limit_calc(should_arm, mixed, r_page_servo_disarmed, r_page_servo_control_min, r_page_servo_control_max, outputs, r_page_servos, &pwm_limit);
+		pwm_limit_calc(should_arm, mixed, r_page_servo_disarmed, r_page_servo_control_min, r_page_servo_control_max, outputs,
+			       r_page_servos, &pwm_limit);
 
 		//warnx("mixed %d outputs (max %d), values:", mixed, output_max);
-		for (unsigned i = 0; i < mixed; i++)
-		{
+		for (unsigned i = 0; i < mixed; i++) {
 			/* check mixed outputs to be zero during init phase */
 			if (r_page_servos[i] != r_page_servo_disarmed[i]) {
 				warnx("disarmed servo value mismatch");
@@ -279,6 +290,7 @@ int test_mixer(int argc, char *argv[])
 
 			//printf("\t %d: %8.4f limited: %8.4f, servo: %d\n", i, outputs_unlimited[i], outputs[i], (int)r_page_servos[i]);
 		}
+
 		usleep(sleep_quantum_us);
 		sleepcount++;
 
@@ -287,6 +299,7 @@ int test_mixer(int argc, char *argv[])
 			fflush(stdout);
 		}
 	}
+
 	printf("\n");
 
 	warnx("ARMING TEST: REARMING: STARTING RAMP");
@@ -296,30 +309,30 @@ int test_mixer(int argc, char *argv[])
 	should_arm = true;
 
 	while (hrt_elapsed_time(&starttime) < 600000 + RAMP_TIME_US) {
-		
+
 		/* mix */
 		mixed = mixer_group.mix(&outputs[0], output_max);
 
-		pwm_limit_calc(should_arm, mixed, r_page_servo_disarmed, r_page_servo_control_min, r_page_servo_control_max, outputs, r_page_servos, &pwm_limit);
+		pwm_limit_calc(should_arm, mixed, r_page_servo_disarmed, r_page_servo_control_min, r_page_servo_control_max, outputs,
+			       r_page_servos, &pwm_limit);
 
 		//warnx("mixed %d outputs (max %d), values:", mixed, output_max);
-		for (unsigned i = 0; i < mixed; i++)
-		{
+		for (unsigned i = 0; i < mixed; i++) {
 			/* predict value */
 			servo_predicted[i] = 1500 + outputs[i] * (r_page_servo_control_max[i] - r_page_servo_control_min[i]) / 2.0f;
 
 			/* check ramp */
 
 			if (hrt_elapsed_time(&starttime) < RAMP_TIME_US &&
-				(r_page_servos[i] + 1 <= r_page_servo_disarmed[i] ||
-					r_page_servos[i] > servo_predicted[i])) {
+			    (r_page_servos[i] + 1 <= r_page_servo_disarmed[i] ||
+			     r_page_servos[i] > servo_predicted[i])) {
 				warnx("ramp servo value mismatch");
 				return 1;
 			}
 
 			/* check post ramp phase */
 			if (hrt_elapsed_time(&starttime) > RAMP_TIME_US &&
-				fabsf(servo_predicted[i] - r_page_servos[i]) > 2) {
+			    fabsf(servo_predicted[i] - r_page_servos[i]) > 2) {
 				printf("\t %d: %8.4f predicted: %d, servo: %d\n", i, (double)outputs[i], servo_predicted[i], (int)r_page_servos[i]);
 				warnx("mixer violated predicted value");
 				return 1;
@@ -327,6 +340,7 @@ int test_mixer(int argc, char *argv[])
 
 			//printf("\t %d: %8.4f limited: %8.4f, servo: %d\n", i, outputs_unlimited[i], outputs[i], (int)r_page_servos[i]);
 		}
+
 		usleep(sleep_quantum_us);
 		sleepcount++;
 
@@ -335,15 +349,18 @@ int test_mixer(int argc, char *argv[])
 			fflush(stdout);
 		}
 	}
+
 	printf("\n");
 
 	/* load multirotor at once test */
 	mixer_group.reset();
 
-	if (argc > 2)
+	if (argc > 2) {
 		filename = argv[2];
-	else
+
+	} else {
 		filename = "/etc/mixers/quad_w.main.mix";
+	}
 
 	load_mixer_file(filename, &buf[0], sizeof(buf));
 	loaded = strlen(buf);
@@ -353,6 +370,7 @@ int test_mixer(int argc, char *argv[])
 	unsigned mc_loaded = loaded;
 	mixer_group.load_from_buf(&buf[0], mc_loaded);
 	warnx("complete buffer load: loaded %u mixers", mixer_group.count());
+
 	if (mixer_group.count() != 5) {
 		warnx("FAIL: Quad W mixer load failed");
 		return 1;
@@ -368,11 +386,13 @@ mixer_callback(uintptr_t handle,
 	       uint8_t control_index,
 	       float &control)
 {
-	if (control_group != 0)
+	if (control_group != 0) {
 		return -1;
+	}
 
-	if (control_index > (sizeof(actuator_controls) / sizeof(actuator_controls[0])))
+	if (control_index > (sizeof(actuator_controls) / sizeof(actuator_controls[0]))) {
 		return -1;
+	}
 
 	control = actuator_controls[control_index];
 
