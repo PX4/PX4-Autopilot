e6ff5818a3016ae2f25e247e25aa67984df386d5
Pavel Kirienko <pavel.kirienko@gmail.com>
2015-04-21 20:47:13 +0300

    libuavcan test: string comparison fix

--- libuavcan/test/dsdl_test/dsdl_test.cpp
+++ libuavcan/test/dsdl_test/dsdl_test.cpp
@@ -54,11 +54,11 @@ TEST(Dsdl, EmptyServices)
 TEST(Dsdl, Signature)
 {
     ASSERT_EQ(0xe74617107a34aa9c, root_ns_a::EmptyService::getDataTypeSignature().get());
-    ASSERT_EQ("root_ns_a.EmptyService", root_ns_a::EmptyService::getDataTypeFullName());
+    ASSERT_STREQ("root_ns_a.EmptyService", root_ns_a::EmptyService::getDataTypeFullName());
     ASSERT_EQ(uavcan::DataTypeKindService, root_ns_a::EmptyService::DataTypeKind);
 
     ASSERT_EQ(0x99604d7066e0d713, root_ns_a::NestedMessage::getDataTypeSignature().get());  // Computed manually
-    ASSERT_EQ("root_ns_a.NestedMessage", root_ns_a::NestedMessage::getDataTypeFullName());
+    ASSERT_STREQ("root_ns_a.NestedMessage", root_ns_a::NestedMessage::getDataTypeFullName());
     ASSERT_EQ(uavcan::DataTypeKindMessage, root_ns_a::NestedMessage::DataTypeKind);
 }
 
