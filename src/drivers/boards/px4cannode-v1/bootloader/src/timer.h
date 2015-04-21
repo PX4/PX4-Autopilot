254aa39e2bab1a9ce3eab105e44290b389ba002c
David Sidrane <david_s5@nscdg.com>
2015-04-20 17:56:16 -1000

    Documantation pass

--- src/drivers/boards/px4cannode-v1/bootloader/src/timer.h
+++ src/drivers/boards/px4cannode-v1/bootloader/src/timer.h
@@ -1,8 +1,58 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2015 PX4 Development Team. All rights reserved.
+ *       Author: Ben Dyer <ben_dyer@mac.com>
+ *               Pavel Kirienko <pavel.kirienko@zubax.com>
+ *               David Sidrane <david_s5@nscdg.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
 #pragma once
 
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
 
 #include "stm32.h"
 #include "nvic.h"
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+#define TIMER_HRT_CYCLES_PER_US (STM32_HCLK_FREQUENCY/1000000)
+#define TIMER_HRT_CYCLES_PER_MS (STM32_HCLK_FREQUENCY/1000)
+
+/****************************************************************************
+ * Public Type Definitions
+ ****************************************************************************/
 
 typedef enum {
         //! Specifies a one-shot timer. After notification timer is discarded.
@@ -16,6 +66,7 @@ typedef enum {
 
 
 } bl_timer_modes_t;
+
 
 typedef uint8_t bl_timer_id;
 typedef uint32_t time_ms_t;
@@ -31,8 +82,16 @@ typedef struct {
 
 } bl_timer_cb_t;
 
+
+/****************************************************************************
+ * Global Variables
+ ****************************************************************************/
+
 extern bl_timer_cb_t null_cb;
 
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
 
 void timer_init(void);
 bl_timer_id timer_allocate(bl_timer_modes_t mode, time_ms_t msfromnow, bl_timer_cb_t *fc);
@@ -50,8 +109,6 @@ static inline int timer_ref_expired(time_ref_t ref)
 }
 
 
-#define TIMER_HRT_CYCLES_PER_US (STM32_HCLK_FREQUENCY/1000000)
-#define TIMER_HRT_CYCLES_PER_MS (STM32_HCLK_FREQUENCY/1000)
 
 static inline time_hrt_cycles_t timer_hrt_read(void)
 {
