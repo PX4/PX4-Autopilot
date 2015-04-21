422322fe0ec143d7606539959a3645dd117a2764
David Sidrane <david_s5@nscdg.com>
2015-04-21 05:10:39 -1000

    moved app_config data to bord_config.h

--- src/drivers/boards/px4cannode-v1/bootloader/can/driver.c
+++ src/drivers/boards/px4cannode-v1/bootloader/can/driver.c
@@ -39,7 +39,7 @@
  ****************************************************************************/
 
 #include <nuttx/config.h>
-#include "app_config.h"
+#include "board_config.h"
 
 #include <stdint.h>
 #include <stdlib.h>
