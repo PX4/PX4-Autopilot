5b91f172e397ec57420869680e35653f2f844edb
Mark Charlebois <charlebm@gmail.com>
2015-04-27 09:52:28 -0700

    topic_listener: moved generated file to Build dir
    Moved the generated topic_listener.cpp to the Build tree so it is
    cleaned when make clean is called.
    
    Signed-off-by: Mark Charlebois <charlebm@gmail.com>

--- src/systemcmds/topic_listener/module.mk
+++ src/systemcmds/topic_listener/module.mk
@@ -35,8 +35,8 @@
 # Build the topic listener tool.
 #
 
-$(PX4_BASE)/src/systemcmds/topic_listener/topic_listener.cpp : $(PX4_BASE)/Tools/generate_listener.py
-	$(PX4_BASE)/Tools/generate_listener.py $(PX4_BASE) > $(PX4_BASE)/src/systemcmds/topic_listener/$@
+./topic_listener.cpp : $(PX4_BASE)/Tools/generate_listener.py
+	$(PX4_BASE)/Tools/generate_listener.py $(PX4_BASE) > $@
 
 MODULE_COMMAND	 = listener
 SRCS		 = topic_listener.cpp
