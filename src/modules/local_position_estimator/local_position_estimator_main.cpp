ed5bd63a9d970ab047fe2fb5c511e21e4a47f7f9
James Goppert <james.goppert@gmail.com>
2015-05-06 05:52:24 -0400

    Increased stack size.

--- src/modules/local_position_estimator/local_position_estimator_main.cpp
+++ src/modules/local_position_estimator/local_position_estimator_main.cpp
@@ -106,10 +106,10 @@ int local_position_estimator_main(int argc, char *argv[])
 
 		thread_should_exit = false;
 
-		deamon_task = task_spawn_cmd("local_position_estimator",
+		deamon_task = task_spawn_cmd("local_position_est",
 					 SCHED_DEFAULT,
 					 SCHED_PRIORITY_MAX - 10,
-					 6100,
+					 7500,
 					 local_position_estimator_thread_main,
 					 (argv) ? (char * const *)&argv[2] : (char * const *)NULL);
 		exit(0);
