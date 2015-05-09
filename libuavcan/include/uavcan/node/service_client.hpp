567d16764069d8db8534e2a9707cb4132cf95c1c
Pavel Kirienko <pavel.kirienko@gmail.com>
2015-05-09 16:21:50 +0300

    Updated default service timeout

--- libuavcan/include/uavcan/node/service_client.hpp
+++ libuavcan/include/uavcan/node/service_client.hpp
@@ -110,7 +110,7 @@ public:
     /**
      * It's not recommended to override default timeouts.
      */
-    static MonotonicDuration getDefaultRequestTimeout() { return MonotonicDuration::fromMSec(1000); }
+    static MonotonicDuration getDefaultRequestTimeout() { return MonotonicDuration::fromMSec(500); }
     static MonotonicDuration getMinRequestTimeout() { return MonotonicDuration::fromMSec(10); }
     static MonotonicDuration getMaxRequestTimeout() { return MonotonicDuration::fromMSec(60000); }
 
