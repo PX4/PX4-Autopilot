a548d8311ce9c72562327bc92cd02b7ee2f38f2c
Pavel Kirienko <pavel.kirienko@gmail.com>
2015-05-13 16:39:22 +0300

    Testing framework: added emulateSingleFrameBroadcastTransfer()

--- libuavcan/test/protocol/helpers.hpp
+++ libuavcan/test/protocol/helpers.hpp
@@ -103,3 +103,31 @@ struct BackgroundSpinner : uavcan::TimerBase
         spinning_node.spin(uavcan::MonotonicDuration::fromMSec(1));
     }
 };
+
+template <typename MessageType>
+static inline void emulateSingleFrameBroadcastTransfer(CanDriverMock& can, uavcan::NodeID node_id,
+                                                       const MessageType& message, uavcan::TransferID tid)
+{
+    uavcan::StaticTransferBuffer<100> buffer;
+    uavcan::BitStream bitstream(buffer);
+    uavcan::ScalarCodec codec(bitstream);
+
+    // Manual message publication
+    ASSERT_LT(0, MessageType::encode(message, codec));
+    ASSERT_GE(8, buffer.getMaxWritePos());
+
+    // DataTypeID data_type_id, TransferType transfer_type, NodeID src_node_id, NodeID dst_node_id,
+    // uint_fast8_t frame_index, TransferID transfer_id, bool last_frame
+    uavcan::Frame frame(MessageType::DefaultDataTypeID, uavcan::TransferTypeMessageBroadcast,
+                        node_id, uavcan::NodeID::Broadcast, 0, tid, true);
+
+    ASSERT_EQ(buffer.getMaxWritePos(), frame.setPayload(buffer.getRawPtr(), buffer.getMaxWritePos()));
+
+    uavcan::CanFrame can_frame;
+    ASSERT_TRUE(frame.compile(can_frame));
+
+    for (uint8_t i = 0; i < can.getNumIfaces(); i++)
+    {
+        can.ifaces.at(i).pushRx(can_frame);
+    }
+}
