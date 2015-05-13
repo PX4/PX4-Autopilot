700693706b3b6ea9828152e9d22ea2ac51bc66df
David Sidrane <david_s5@nscdg.com>
2015-05-12 15:27:15 -1000

    Added -v1 designation to s2740vc hw

--- makefiles/config_s2740vc-v1_bootloader.mk
+++ makefiles/config_s2740vc-v1_bootloader.mk
@@ -5,7 +5,7 @@
 #
 # Board support modules
 #
-MODULES		+= drivers/boards/s2740vc/bootloader
+MODULES		+= drivers/boards/s2740vc-v1/bootloader
 
 INCLUDE_DIRS += $(PX4_MODULE_SRC)$(MODULES) $(PX4_BOOTLOADER_BASE)include $(PX4_MODULE_SRC)/modules/systemlib
 
