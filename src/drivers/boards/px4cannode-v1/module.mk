37eb1a4b9c7f858c82c5151d756e3ede6ee5c75f
David Sidrane <david_s5@nscdg.com>
2015-05-16 14:29:03 -1000

    Keep the build byproducts out of the source tree

--- src/drivers/boards/px4cannode-v1/module.mk
+++ src/drivers/boards/px4cannode-v1/module.mk
@@ -12,8 +12,15 @@ SRCS   = \
 		   px4cannode_spi.c \
 		   ../../../drivers/device/cdev.cpp \
 		   ../../../drivers/device/device.cpp \
-		   ../../../modules/systemlib/up_cxxinitialize.c \
-		   $(ABS_BOOTLOADER_SRC)common/boot_app_shared.c \
-		   $(ABS_BOOTLOADER_SRC)util/crc.c
+		   ../../../modules/systemlib/up_cxxinitialize.c 
+
+BOOTLOADER_SRC =  \
+			$(ABS_BOOTLOADER_SRC)common/boot_app_shared.c \
+ 			$(ABS_BOOTLOADER_SRC)util/crc.c
+		   
+# Use the relitive path to keep the genrated files in the BUILD_DIR
+
+SRCS += $(subst  $(PX4_MODULE_SRC),../../../,$(BOOTLOADER_SRC))
+		   
 
 MAXOPTIMIZATION	 = -Os
