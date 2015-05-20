80c7719b42307a2716b6a58848af6e8f61a4b788
TSC21 <n.marques21@hotmail.com>
2015-05-20 12:51:20 +0100

    distance_sensor: typo correction on msg.type

--- src/modules/mavlink/mavlink_messages.cpp
+++ src/modules/mavlink/mavlink_messages.cpp
@@ -2234,7 +2234,7 @@ protected:
 
 			switch (dist_sensor.type) {
 			case MAV_DISTANCE_SENSOR_ULTRASOUND:
-				msg.type = MAV_DISTANCE_SENSOR_LASER;
+				msg.type = MAV_DISTANCE_SENSOR_ULTRASOUND;
 				break;
 
 			case MAV_DISTANCE_SENSOR_LASER:
