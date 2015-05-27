82f4484355ed9156ae8a72cd35f035ab5ea1bce7
David Sidrane <david_s5@nscdg.com>
2015-05-27 06:16:23 -1000

    Increased stack size

--- src/modules/uavcan/uavcan_main.hpp
+++ src/modules/uavcan/uavcan_main.hpp
@@ -97,7 +97,7 @@ class UavcanNode : public device::CDev
          */
 
 	static constexpr unsigned RxQueueLenPerIface = FramePerMSecond * PollTimeoutMs; // At
-	static constexpr unsigned StackSize          = 2800;
+	static constexpr unsigned StackSize          = 3400;
 
 public:
 	typedef uavcan::Node<MemPoolSize> Node;
