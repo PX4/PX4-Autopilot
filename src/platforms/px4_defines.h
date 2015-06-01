6bc0d4d03b1ed31bd09439ac5107f67d6ae4122d
Roman <romanbapst@gmail.com>
2015-06-01 11:13:31 +0200

    add definition of MAX_RAND

--- src/platforms/px4_defines.h
+++ src/platforms/px4_defines.h
@@ -146,6 +146,8 @@ __END_DECLS
 #define OK 0
 #define ERROR -1
 
+#define MAX_RAND 32767
+
 #if defined(__PX4_QURT)
 #define M_PI			3.14159265358979323846
 #define M_PI_2			1.57079632679489661923
@@ -208,6 +210,7 @@ __END_DECLS
 #define isinf(x) false
 #define fminf(x, y) ((x) > (y) ? y : x)
 #endif
+
 
 #endif
 
