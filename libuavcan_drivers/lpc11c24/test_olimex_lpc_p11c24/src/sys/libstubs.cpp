1f1679c75d989420350e69339d48b53203c5e874
Pavel Kirienko <pavel.kirienko@gmail.com>
2015-06-03 15:02:22 +0300

    LPC11C24 - removed useless libstdc++ stubs

--- libuavcan_drivers/lpc11c24/test_olimex_lpc_p11c24/src/sys/libstubs.cpp
+++ libuavcan_drivers/lpc11c24/test_olimex_lpc_p11c24/src/sys/libstubs.cpp
@@ -35,50 +35,6 @@ void operator delete[](void*)
     std::abort();
 }
 
-/*
- * stdlibc++ workaround.
- * Default implementations will throw, which causes code size explosion.
- * These definitions override the ones defined in the stdlibc+++.
- */
-namespace std
-{
-
-void __throw_bad_exception() { std::abort(); }
-
-void __throw_bad_alloc() { std::abort(); }
-
-void __throw_bad_cast() { std::abort(); }
-
-void __throw_bad_typeid() { std::abort(); }
-
-void __throw_logic_error(const char*) { std::abort(); }
-
-void __throw_domain_error(const char*) { std::abort(); }
-
-void __throw_invalid_argument(const char*) { std::abort(); }
-
-void __throw_length_error(const char*) { std::abort(); }
-
-void __throw_out_of_range(const char*) { std::abort(); }
-
-void __throw_runtime_error(const char*) { std::abort(); }
-
-void __throw_range_error(const char*) { std::abort(); }
-
-void __throw_overflow_error(const char*) { std::abort(); }
-
-void __throw_underflow_error(const char*) { std::abort(); }
-
-void __throw_ios_failure(const char*) { std::abort(); }
-
-void __throw_system_error(int) { std::abort(); }
-
-void __throw_future_error(int) { std::abort(); }
-
-void __throw_bad_function_call() { std::abort(); }
-
-}
-
 namespace __gnu_cxx
 {
 
