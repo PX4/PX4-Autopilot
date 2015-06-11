03f34b12cc1d42a2e5c01c10a78429610027c0ad
David Sidrane <david_s5@nscdg.com>
2015-06-10 14:12:21 -1000

    Format change board_xxxx.mk

--- makefiles/nuttx/board_aerocore.mk
+++ makefiles/nuttx/board_aerocore.mk
@@ -6,6 +6,6 @@
 # Configure the toolchain
 #
 CONFIG_ARCH			 = CORTEXM4F
-CONFIG_BOARD			 = AEROCORE
+CONFIG_BOARD		 = AEROCORE
 
 include $(PX4_MK_DIR)/nuttx/toolchain_gnu-arm-eabi.mk
--- makefiles/nuttx/board_mavstation.mk
+++ makefiles/nuttx/board_mavstation.mk
@@ -6,6 +6,6 @@
 # Configure the toolchain
 #
 CONFIG_ARCH			 = CORTEXM3
-CONFIG_BOARD			 = MAVSTATION
+CONFIG_BOARD		 = MAVSTATION
 
 include $(PX4_MK_DIR)/toolchain_gnu-arm-eabi.mk
