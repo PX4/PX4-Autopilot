8d85c34accf94cf9c6a2eb6884bd5368028ad93f
David Sidrane <david_s5@nscdg.com>
2015-06-10 18:52:18 -1000

    s2740vc-v1 using px4 platform

--- src/drivers/boards/s2740vc-v1/module.mk
+++ src/drivers/boards/s2740vc-v1/module.mk
@@ -4,8 +4,16 @@
 
 SRCS   = \
 		   s2740vc_can.c \
-		   s2740vc_init.c \
+		   s2740vc_init.c
+
+ifeq ($(PX4_TARGET_OS),nuttx)
+SRCS +=		  \
 		   ../../../drivers/device/cdev.cpp \
-		   ../../../drivers/device/device.cpp
+		   ../../../drivers/device/device_nuttx.cpp
+else
+SRCS +=		  \
+		   ../../../drivers/device/device_posix.cpp \
+		   ../../../drivers/device/vdev.cpp
+endif
 
 MAXOPTIMIZATION	 = -Os
