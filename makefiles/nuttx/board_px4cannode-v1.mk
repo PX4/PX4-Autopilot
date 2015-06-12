52345b4f6e43daab1eebf64e395da2e4e26ec778
David Sidrane <david_s5@nscdg.com>
2015-06-12 10:16:41 -1000

    Removed extra slashes

--- makefiles/nuttx/board_px4cannode-v1.mk
+++ makefiles/nuttx/board_px4cannode-v1.mk
@@ -9,4 +9,4 @@ CONFIG_ARCH			 = CORTEXM3
 CONFIG_BOARD		 = PX4CANNODE_V1
 
 WUSEPACKED = -Wno-packed
-include $(PX4_MK_DIR)/toolchain_gnu-arm-eabi.mk
+include $(PX4_MK_DIR)nuttx/toolchain_gnu-arm-eabi.mk
--- makefiles/nuttx/board_s2740vc-v1.mk
+++ makefiles/nuttx/board_s2740vc-v1.mk
@@ -9,4 +9,4 @@ CONFIG_ARCH			 = CORTEXM4
 CONFIG_BOARD		 = S2740VC_V1
 
 WUSEPACKED = -Wno-packed
-include $(PX4_MK_DIR)/toolchain_gnu-arm-eabi.mk
+include $(PX4_MK_DIR)nuttx/toolchain_gnu-arm-eabi.mk
