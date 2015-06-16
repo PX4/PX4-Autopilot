26d2589e97ee2a759fa7ad7f89d0be0afc4cffc3
Mark Charlebois <charlebm@gmail.com>
2015-06-15 17:23:04 -0700

    makefile cleanup
    Moved nuttx specific make rules to files in makefiles/nuttx.
    
    All target specific makefiles are in their target sub directories.
    
    To minimize file duplication, targets that share rules include a
    common file. For example the posix and posix-arm targets both use
    makefiles/posix/posix_elf.mk
    
    Signed-off-by: Mark Charlebois <charlebm@gmail.com>

--- makefiles/nuttx/nuttx_romfs.mk
+++ makefiles/nuttx/nuttx_romfs.mk
@@ -29,10 +29,6 @@
 # POSSIBILITY OF SUCH DAMAGE.
 #
 
-#
-# Makefile for PX4 Nuttx based firmware images.
-#
-
 ################################################################################
 # ROMFS generation
 ################################################################################
@@ -157,63 +153,4 @@ SRCS			+= $(BUILTIN_CSRC)
 EXTRA_CLEANS		+= $(BUILTIN_CSRC)
 
 endif
-
-################################################################################
-# Build rules
-################################################################################
-
-#
-# What we're going to build.
-#
-PRODUCT_BUNDLE		 = $(WORK_DIR)firmware.px4
-PRODUCT_BIN		 = $(WORK_DIR)firmware.bin
-PRODUCT_ELF		 = $(WORK_DIR)firmware.elf
-PRODUCT_PARAMXML = $(WORK_DIR)/parameters.xml
-
-.PHONY:			firmware
-firmware:		$(PRODUCT_BUNDLE)
-
-#
-# Built product rules
-#
-
-$(PRODUCT_BUNDLE):	$(PRODUCT_BIN)
-	@$(ECHO) %% Generating $@
-ifdef GEN_PARAM_XML
-	python $(PX4_BASE)/Tools/px_process_params.py --src-path $(PX4_BASE)/src --board CONFIG_ARCH_BOARD_$(CONFIG_BOARD) --xml
-	$(Q) $(MKFW) --prototype $(IMAGE_DIR)/$(BOARD).prototype \
-		--git_identity $(PX4_BASE) \
-		--parameter_xml $(PRODUCT_PARAMXML) \
-		--image $< > $@
-else
-	$(Q) $(MKFW) --prototype $(IMAGE_DIR)/$(BOARD).prototype \
-		--git_identity $(PX4_BASE) \
-		--image $< > $@
-endif
-
-$(PRODUCT_BIN):		$(PRODUCT_ELF)
-	$(call SYM_TO_BIN,$<,$@)
-
-$(PRODUCT_ELF):		$(OBJS) $(MODULE_OBJS) $(LIBRARY_LIBS) $(GLOBAL_DEPS) $(LINK_DEPS) $(MODULE_MKFILES)
-	$(call LINK,$@,$(OBJS) $(MODULE_OBJS) $(LIBRARY_LIBS))
-
-#
-# Utility rules
-#
-
-.PHONY: upload
-upload:	$(PRODUCT_BUNDLE) $(PRODUCT_BIN)
-	$(Q) $(MAKE) -f $(PX4_MK_DIR)/upload.mk \
-		METHOD=serial \
-		CONFIG=$(CONFIG) \
-		BOARD=$(BOARD) \
-		BUNDLE=$(PRODUCT_BUNDLE) \
-		BIN=$(PRODUCT_BIN)
-
-.PHONY: clean
-clean:			$(MODULE_CLEANS)
-	@$(ECHO) %% cleaning
-	$(Q) $(REMOVE) $(PRODUCT_BUNDLE) $(PRODUCT_BIN) $(PRODUCT_ELF)
-	$(Q) $(REMOVE) $(OBJS) $(DEP_INCLUDES) $(EXTRA_CLEANS)
-	$(Q) $(RMDIR) $(NUTTX_EXPORT_DIR)
 
