8c4c79a2ce976ea108d9eda434e2687acc47c0e8
Lorenz Meier <lm@inf.ethz.ch>
2015-07-02 16:36:40 +0200

    param library: Separate it into its own library

--- src/lib/param/module.mk
+++ src/lib/param/module.mk
@@ -32,34 +32,10 @@
 ############################################################################
 
 #
-# System utility library
+# param library
 #
 
-SRCS		 = \
-		   perf_counter.c \
-		   conversions.c \
-		   cpuload.c \
-		   pid/pid.c \
-		   airspeed.c \
-		   system_params.c \
-		   mavlink_log.c \
-		   rc_check.c \
-		   otp.c \
-		   board_serial.c \
-		   pwm_limit/pwm_limit.c \
-		   mcu_version.c \
-		   bson/tinybson.c \
-		   circuit_breaker.cpp \
-		   circuit_breaker_params.c
-
-ifeq ($(PX4_TARGET_OS),nuttx)
-SRCS		+= err.c \
-		   up_cxxinitialize.c 
-endif
-
-ifneq ($(PX4_TARGET_OS),qurt)
-SRCS 		+= hx_stream.c 
-endif
+SRCS		 = param.c
 
 MAXOPTIMIZATION	 = -Os
 
--- src/modules/systemlib/system_params.c
+++ src/modules/systemlib/system_params.c
@@ -38,7 +38,7 @@
  */
 
 #include <px4_config.h>
-#include <systemlib/param/param.h>
+#include <param/param.h>
 
 /**
  * Auto-start script index.
