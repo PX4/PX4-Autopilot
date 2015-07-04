4bf92ffc7fc08f33aafa7076b3a4c73cf61ee7c0
Lorenz Meier <lm@inf.ethz.ch>
2015-07-04 12:05:43 +0200

    POSIX: Change warn to info

--- src/modules/systemlib/param/param.h
+++ src/modules/systemlib/param/param.h
@@ -2,7 +2,7 @@
 #ifndef _PARAM_DEPRECATED_H
 #define _PARAM_DEPRECATED_H
 
-#include <param/param.h>
-#warning This header is deprecated. Please include now <param/param.h> instead of the old #include <param/param.h>
+#include <lib/param/param.h>
+#info This header is deprecated. Please include now <param/param.h> instead of the old #include <param/param.h>
 
 #endif
