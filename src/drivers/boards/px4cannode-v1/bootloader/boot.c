2d4c6dbee778b1aadf7503fd28bdd0357d4c0e41
David Sidrane <david_s5@nscdg.com>
2015-07-07 11:01:16 -1000

    More srhinkage

--- src/drivers/boards/px4cannode-v1/bootloader/boot.c
+++ src/drivers/boards/px4cannode-v1/bootloader/boot.c
@@ -189,34 +189,34 @@ size_t board_get_hardware_version(uavcan_HardwareVersion_t *hw_version)
  ****************************************************************************/
 #define led(n, code, r , g , b, h) {.red = (r),.green = (g), .blue = (b),.hz = (h)}
 
-typedef struct led_t {
-	uint8_t red;
-	uint8_t green;
-	uint8_t blue;
+typedef struct packed_struct led_t {
+	uint16_t red    :       5;
+	uint16_t green  :       6;
+	uint16_t blue   :       5;
 	uint8_t hz;
 } led_t;
 
 static const  led_t i2l[] = {
 
-	led(0, off,                            0   , 0   , 0   , 0),
-	led(1, reset,                          10  , 255 , 255 , 255),
-	led(2, autobaud_start,                 0   , 255 , 0   , 1),
-	led(3, autobaud_end,                   0   , 255 , 0   , 2),
-	led(4, allocation_start,               0   , 0   , 255 , 2),
-	led(5, allocation_end,                 0   , 255 , 255 , 3),
-	led(6, fw_update_start,                100 , 255 , 255 , 3),
-	led(7, fw_update_erase_fail,           100 , 255 , 100 , 3),
-	led(8, fw_update_invalid_response,     255 , 0   , 0   , 1),
-	led(9, fw_update_timeout,              255 , 0   , 0   , 2),
-	led(a, fw_update_invalid_crc,          255 , 0   , 0   , 4),
-	led(b, jump_to_app,                    0   , 255 , 0   , 10),
+	led(0, off,                            0   , 0  , 0   , 0),
+	led(1, reset,                          10  , 63 , 31 , 255),
+	led(2, autobaud_start,                 0   , 63 , 0   , 1),
+	led(3, autobaud_end,                   0   , 63 , 0   , 2),
+	led(4, allocation_start,               0   , 0  , 31  , 2),
+	led(5, allocation_end,                 0   , 63 , 31  , 3),
+	led(6, fw_update_start,                15  , 63 , 31  , 3),
+	led(7, fw_update_erase_fail,           15  , 63 , 15  , 3),
+	led(8, fw_update_invalid_response,     31  , 0   , 0  , 1),
+	led(9, fw_update_timeout,              31  , 0   , 0  , 2),
+	led(a, fw_update_invalid_crc,          31  , 0   , 0  , 4),
+	led(b, jump_to_app,                    0   , 63 , 0   , 10),
 
 };
 
 void board_indicate(uiindication_t indication)
 {
-	rgb_led(i2l[indication].red,
-		i2l[indication].green,
-		i2l[indication].blue,
+	rgb_led(i2l[indication].red << 3,
+		i2l[indication].green << 2,
+		i2l[indication].blue << 3,
 		i2l[indication].hz);
 }
