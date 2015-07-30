b046caa97b4c3aa2446786539abe89c320adc2dc
Mark Charlebois <charlebm@gmail.com>
2015-07-30 16:48:10 -0700

    QuRT: build configuration changes
    Added DSPAL header paths to toolchain_hexagon.mk.
    
    Made changes to build configuraion for QuRT to support HIL testing.
    
    Signed-off-by: Mark Charlebois <charlebm@gmail.com>

--- makefiles/qurt/config_qurt_muorb_test.mk
+++ makefiles/qurt/config_qurt_muorb_test.mk
@@ -7,17 +7,17 @@
 #
 MODULES		+= drivers/device
 #MODULES		+= drivers/blinkm
-MODULES		+= drivers/hil
-MODULES		+= drivers/led
-MODULES		+= drivers/rgbled
-MODULES		+= modules/sensors
+#MODULES		+= drivers/hil
+#MODULES		+= drivers/led
+#MODULES		+= drivers/rgbled
+#MODULES		+= modules/sensors
 #MODULES		+= drivers/ms5611
 
 #
 # System commands
 #
-MODULES	+= systemcmds/param
-MODULES += systemcmds/mixer
+#MODULES	+= systemcmds/param
+#MODULES += systemcmds/mixer
 
 #
 # General system control
@@ -28,24 +28,24 @@ MODULES += systemcmds/mixer
 # Estimation modules (EKF/ SO3 / other filters)
 #
 #MODULES		+= modules/attitude_estimator_ekf
-MODULES		+= modules/ekf_att_pos_estimator
+#MODULES		+= modules/ekf_att_pos_estimator
 
 #
 # Vehicle Control
 #
-MODULES		+= modules/mc_att_control
-MODULES		+= modules/mc_pos_control
+#MODULES		+= modules/mc_att_control
+#MODULES		+= modules/mc_pos_control
 
 #
 # Library modules
 #
 MODULES		+= modules/systemlib
-MODULES		+= modules/systemlib/mixer
+#MODULES		+= modules/systemlib/mixer
 MODULES		+= modules/uORB
 #MODULES		+= modules/dataman
 #MODULES		+= modules/sdlog2
-MODULES		+= modules/simulator
-MODULES		+= modules/commander
+#MODULES		+= modules/simulator
+#MODULES		+= modules/commander
 
 #
 # Libraries
@@ -61,10 +61,10 @@ MODULES		+= lib/conversion
 #
 MODULES		+= platforms/qurt/px4_layer
 MODULES		+= platforms/posix/work_queue
-MODULES		+= platforms/posix/drivers/accelsim
-MODULES		+= platforms/posix/drivers/gyrosim
-MODULES		+= platforms/posix/drivers/adcsim
-MODULES		+= platforms/posix/drivers/barosim
+#MODULES		+= platforms/posix/drivers/accelsim
+#MODULES		+= platforms/posix/drivers/gyrosim
+#MODULES		+= platforms/posix/drivers/adcsim
+#MODULES		+= platforms/posix/drivers/barosim
 
 #
 # Unit tests
