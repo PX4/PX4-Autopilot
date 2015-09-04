6c28ef83bf2185f87998eba74d243bef505f3a78
Mark Charlebois <charlebm@gmail.com>
2015-09-04 16:21:19 -0700

    Fixes for posix-arm toolchain file
    Removed incorrect nuttx checks (there should be no nuttx dependency).
    Removed unsupported ARM targets. The posix build has not been tested on
    CortexM targets.
    
    Signed-off-by: Mark Charlebois <charlebm@gmail.com>

--- makefiles/posix-arm/toolchain_gnu-arm-linux-gnueabihf.mk
+++ makefiles/posix-arm/toolchain_gnu-arm-linux-gnueabihf.mk
@@ -76,37 +76,6 @@ ARCHCPUFLAGS_CORTEXA8    = -mtune=cortex-a8 \
                            -mfloat-abi=hard \
                            -mfpu=neon
  
-ARCHCPUFLAGS_CORTEXM4F	 = -mcpu=cortex-m4 \
-			   -mthumb \
-			   -march=armv7e-m \
-			   -mfpu=fpv4-sp-d16 \
-			   -mfloat-abi=hard
-
-ARCHCPUFLAGS_CORTEXM4	 = -mcpu=cortex-m4 \
-			   -mthumb \
-			   -march=armv7e-m \
-			   -mfloat-abi=soft
-
-ARCHCPUFLAGS_CORTEXM3	 = -mcpu=cortex-m3 \
-			   -mthumb \
-			   -march=armv7-m \
-			   -mfloat-abi=soft
-
-# Enabling stack checks if OS was build with them
-#
-TEST_FILE_STACKCHECK=$(WORK_DIR)nuttx-export/include/nuttx/config.h
-TEST_VALUE_STACKCHECK=CONFIG_ARMV7M_STACKCHECK\ 1
-ENABLE_STACK_CHECKS=$(shell $(GREP) -q "$(TEST_VALUE_STACKCHECK)" $(TEST_FILE_STACKCHECK); echo $$?;)
-ifeq ("$(ENABLE_STACK_CHECKS)","0")
-ARCHINSTRUMENTATIONDEFINES_CORTEXM4F = -finstrument-functions -ffixed-r10
-ARCHINSTRUMENTATIONDEFINES_CORTEXM4  = -finstrument-functions -ffixed-r10
-ARCHINSTRUMENTATIONDEFINES_CORTEXM3  =
-else
-ARCHINSTRUMENTATIONDEFINES_CORTEXM4F =
-ARCHINSTRUMENTATIONDEFINES_CORTEXM4  =
-ARCHINSTRUMENTATIONDEFINES_CORTEXM3  =
-endif
-
 # Pick the right set of flags for the architecture.
 #
 ARCHCPUFLAGS		 = $(ARCHCPUFLAGS_$(CONFIG_ARCH))
@@ -120,13 +89,13 @@ ifeq ($(CONFIG_BOARD),)
 $(error Board config does not define CONFIG_BOARD)
 endif
 ARCHDEFINES		+= -DCONFIG_ARCH_BOARD_$(CONFIG_BOARD) \
-                   -D__PX4_LINUX -D__PX4_POSIX \
-			       -Dnoreturn_function= \
-			       -I$(PX4_BASE)/src/modules/systemlib \
-			       -I$(PX4_BASE)/src/lib/eigen \
-			       -I$(PX4_BASE)/src/platforms/posix/include \
-			       -I$(PX4_BASE)/mavlink/include/mavlink \
-			       -Wno-error=shadow
+	                   -D__PX4_LINUX -D__PX4_POSIX \
+			   -Dnoreturn_function= \
+			   -I$(PX4_BASE)/src/modules/systemlib \
+			   -I$(PX4_BASE)/src/lib/eigen \
+			   -I$(PX4_BASE)/src/platforms/posix/include \
+			   -I$(PX4_BASE)/mavlink/include/mavlink \
+			   -Wno-error=shadow
 
 # optimisation flags
 #
