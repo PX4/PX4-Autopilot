ebfae73108af109bff893906bde082f16a0f8ddf
Lorenz Meier <lm@inf.ethz.ch>
2015-09-05 22:06:15 +0200

    MS5611 code style fix

--- src/drivers/ms5611/ms5611.h
+++ src/drivers/ms5611/ms5611.h
@@ -83,4 +83,4 @@ extern bool crc4(uint16_t *n_prom);
 extern device::Device *MS5611_spi_interface(ms5611::prom_u &prom_buf, uint8_t busnum) __attribute__((weak));
 extern device::Device *MS5611_i2c_interface(ms5611::prom_u &prom_buf, uint8_t busnum) __attribute__((weak));
 extern device::Device *MS5611_sim_interface(ms5611::prom_u &prom_buf, uint8_t busnum) __attribute__((weak));
-typedef device::Device* (*MS5611_constructor)(ms5611::prom_u &prom_buf, uint8_t busnum);
+typedef device::Device *(*MS5611_constructor)(ms5611::prom_u &prom_buf, uint8_t busnum);
