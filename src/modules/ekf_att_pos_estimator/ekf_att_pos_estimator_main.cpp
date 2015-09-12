d222f6e664823090263144daa932e03d9e8e28b1
Lorenz Meier <lm@inf.ethz.ch>
2015-09-12 10:52:07 +0200

    EKF: Remove unused error variable

--- src/modules/ekf_att_pos_estimator/ekf_att_pos_estimator_main.cpp
+++ src/modules/ekf_att_pos_estimator/ekf_att_pos_estimator_main.cpp
@@ -105,12 +105,6 @@ uint64_t getMicros()
 namespace estimator
 {
 
-/* oddly, ERROR is not defined for c++ */
-#ifdef ERROR
-# undef ERROR
-#endif
-static const int ERROR = -1;
-
 AttitudePositionEstimatorEKF	*g_estimator = nullptr;
 }
 
