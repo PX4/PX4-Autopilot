e1b4239562e886dd75188b2530772bf61adc900e
James Goppert <james.goppert@gmail.com>
2015-09-12 05:57:59 -0400

    Corrected typo in prebuild.

--- cmake/nuttx/px4_impl_nuttx.cmake
+++ cmake/nuttx/px4_impl_nuttx.cmake
@@ -467,7 +467,7 @@ function(px4_os_prebuild_targets)
 			ARGN ${ARGN})
 	add_custom_target(${OUT})
 	px4_nuttx_add_export(OUT nuttx_export_${board}
-		CONFIG ${board}
+		CONFIG ${BOARD}
 		THREADS ${THREADS}
 		DEPENDS git_nuttx)
 	add_dependencies(${OUT} nuttx_export_${board})
