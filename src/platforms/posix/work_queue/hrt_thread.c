3a2e064c92bef40c4806f93f5aa808bc89a9c9cf
Mark Charlebois <charlebm@gmail.com>
2015-09-16 15:58:15 -0700

    Fixed error due to set but unused variable
    Signed-off-by: Mark Charlebois <charlebm@gmail.com>

--- src/platforms/posix/work_queue/hrt_thread.c
+++ src/platforms/posix/work_queue/hrt_thread.c
@@ -117,7 +117,6 @@ static void hrt_work_process()
 	uint64_t elapsed;
 	uint32_t remaining;
 	uint32_t next;
-	int ret;
 
 	/* Then process queued work.  We need to keep interrupts disabled while
 	 * we process items in the work list.
@@ -207,8 +206,7 @@ static void hrt_work_process()
 
 	/* might sleep less if a signal received and new item was queued */
 	//PX4_INFO("Sleeping for %u usec", next);
-	ret = usleep(next);
-	//PX4_INFO("WOKE UP %d", ret);
+	usleep(next);
 }
 
 /****************************************************************************
