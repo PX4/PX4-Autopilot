672a6d8f9b6701d71cfdf9bdb198c09642649356
Lorenz Meier <lm@inf.ethz.ch>
2015-09-20 12:54:22 +0200

    Add debug targets to SITL run

--- Tools/sitl_run.sh
+++ Tools/sitl_run.sh
@@ -4,4 +4,12 @@ cd build_posix_sitl_simple/src/firmware/posix
 mkdir -p rootfs/fs/microsd
 mkdir -p rootfs/eeprom
 touch rootfs/eeprom/parameters
-./mainapp ../../../../$1
+if [ "$2" == "lldb" ]
+then
+	lldb -- mainapp ../../../../$1
+elif [ "$2" == "gdb" ]
+then
+	gdb -- mainapp ../../../../$1
+else
+	./mainapp ../../../../$1
+fi
