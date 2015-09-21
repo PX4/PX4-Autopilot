1ab1046a1212579fc24b5df65703028569984b58
againagainst <again.against@gmail.com>
2015-09-22 01:03:17 +0300

    Fixed wrong uORB struct fields init order

--- src/platforms/posix/drivers/airspeedsim/airspeedsim.cpp
+++ src/platforms/posix/drivers/airspeedsim/airspeedsim.cpp
@@ -357,12 +357,11 @@ Airspeed::update_status()
 {
 	if (_sensor_ok != _last_published_sensor_ok) {
 		/* notify about state change */
-		struct subsystem_info_s info = {
-			subsystem_info_s::SUBSYSTEM_TYPE_DIFFPRESSURE,
-			true,
-			true,
-			_sensor_ok
-		};
+		struct subsystem_info_s info = {};
+		info.present = true;
+		info.enabled = true;
+		info.ok = _sensor_ok;
+		info.subsystem_type = subsystem_info_s::SUBSYSTEM_TYPE_DIFFPRESSURE;
 
 		if (_subsys_pub != nullptr) {
 			orb_publish(ORB_ID(subsystem_info), _subsys_pub, &info);
