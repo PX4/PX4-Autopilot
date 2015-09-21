e1606cd6db99eea8dcdf04013a257719eeb0d724
tumbili <bapstr@ethz.ch>
2015-09-21 12:46:02 +0200

    enable quaternion based attitude control for fixed wings

--- src/lib/ecl/attitude_fw/ecl_controller.cpp
+++ src/lib/ecl/attitude_fw/ecl_controller.cpp
@@ -113,6 +113,11 @@ void ECL_Controller::set_max_rate(float max_rate)
 	_max_rate = max_rate;
 }
 
+void ECL_Controller::set_rate_setpoint(float rate_setpoint)
+{
+	_rate_setpoint = rate_setpoint;
+}
+
 float ECL_Controller::get_rate_error()
 {
 	return _rate_error;
