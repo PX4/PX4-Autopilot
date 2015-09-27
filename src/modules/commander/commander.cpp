aa40f1fd9734e29bd741d9a0fb38ce907c8e22dc
Lorenz Meier <lm@inf.ethz.ch>
2015-09-27 18:57:51 +0200

    Commander: More detailed status

--- src/modules/commander/commander.cpp
+++ src/modules/commander/commander.cpp
@@ -408,7 +408,8 @@ void usage(const char *reason)
 void print_status()
 {
 	warnx("type: %s", (status.is_rotary_wing) ? "symmetric motion" : "forward motion");
-	warnx("usb powered: %s", (status.usb_connected) ? "yes" : "no");
+	warnx("power: USB: %s, BRICK: %s", (status.usb_connected) ? "[OK]" : "[NO]",
+		(status.condition_power_input_valid) ? " [OK]" : "[NO]");
 	warnx("avionics rail: %6.2f V", (double)status.avionics_power_rail_voltage);
 	warnx("home: lat = %.7f, lon = %.7f, alt = %.2f ", _home.lat, _home.lon, (double)_home.alt);
 	warnx("home: x = %.7f, y = %.7f, z = %.2f ", (double)_home.x, (double)_home.y, (double)_home.z);
