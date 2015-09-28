c5e66e41f13ffaaae9b437b577c872100daa91c8
Mark Charlebois <charlebm@gmail.com>
2015-09-28 10:49:48 -0700

    qurt: Must use latest version of Eigen
    The latest version of Eigen is required to build with the Hexagon
    7.4 toolchain. Only certain C++11 features are supported and the
    latest version if Eigen provides these tests. A patch is required
    to add support for the Hexagon compiler.
    
    These changes force the sync and then update of eigen and allow it
    to be patched for qurt.
    
    The eigen-3.2 submodule was removed as it is no longer needed by the
    qurt build with the updated toolchain.
    
    Signed-off-by: Mark Charlebois <charlebm@gmail.com>

--- cmake/common/px4_base.cmake
+++ cmake/common/px4_base.cmake
@@ -139,7 +139,7 @@ function(px4_add_git_submodule)
 	add_custom_command(OUTPUT ${CMAKE_BINARY_DIR}/git_${NAME}.stamp
 		WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
 		COMMAND git submodule init ${PATH}
-		COMMAND git submodule update ${PATH}
+		COMMAND git submodule update -f ${PATH}
 		COMMAND touch ${CMAKE_BINARY_DIR}/git_${NAME}.stamp
 		)
 	add_custom_target(${TARGET}
