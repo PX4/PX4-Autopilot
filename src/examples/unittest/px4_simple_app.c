30e64806dbb0d1baf8bc07cf1e57e0f391219ccc
Lorenz Meier <lm@inf.ethz.ch>
2015-10-05 18:48:40 +0100

    Add example unittest and unittest platform

---
+++ src/examples/unittest/px4_simple_app.c
@@ -0,0 +1,121 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2012-2015 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/**
+ * @file px4_simple_app.c
+ * Minimal application example for PX4 autopilot
+ *
+ * @author Example User <mail@example.com>
+ */
+
+#include <px4_config.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <poll.h>
+#include <string.h>
+
+#include <uORB/uORB.h>
+#include <uORB/topics/sensor_combined.h>
+#include <uORB/topics/vehicle_attitude.h>
+
+__EXPORT int px4_simple_app_main(int argc, char *argv[]);
+
+int px4_simple_app_main(int argc, char *argv[])
+{
+	printf("Hello Sky!\n");
+
+	/* subscribe to sensor_combined topic */
+	int sensor_sub_fd = orb_subscribe(ORB_ID(sensor_combined));
+	orb_set_interval(sensor_sub_fd, 1000);
+
+	/* advertise attitude topic */
+	struct vehicle_attitude_s att;
+	memset(&att, 0, sizeof(att));
+	orb_advert_t att_pub = orb_advertise(ORB_ID(vehicle_attitude), &att);
+
+	/* one could wait for multiple topics with this technique, just using one here */
+	struct pollfd fds[] = {
+		{ .fd = sensor_sub_fd,   .events = POLLIN },
+		/* there could be more file descriptors here, in the form like:
+		 * { .fd = other_sub_fd,   .events = POLLIN },
+		 */
+	};
+
+	int error_counter = 0;
+
+	for (int i = 0; i < 5; i++) {
+		/* wait for sensor update of 1 file descriptor for 1000 ms (1 second) */
+		int poll_ret = poll(fds, 1, 1000);
+
+		/* handle the poll result */
+		if (poll_ret == 0) {
+			/* this means none of our providers is giving us data */
+			printf("[px4_simple_app] Got no data within a second\n");
+
+		} else if (poll_ret < 0) {
+			/* this is seriously bad - should be an emergency */
+			if (error_counter < 10 || error_counter % 50 == 0) {
+				/* use a counter to prevent flooding (and slowing us down) */
+				printf("[px4_simple_app] ERROR return value from poll(): %d\n"
+				       , poll_ret);
+			}
+
+			error_counter++;
+
+		} else {
+
+			if (fds[0].revents & POLLIN) {
+				/* obtained data for the first file descriptor */
+				struct sensor_combined_s raw;
+				/* copy sensors raw data into local buffer */
+				orb_copy(ORB_ID(sensor_combined), sensor_sub_fd, &raw);
+				printf("[px4_simple_app] Accelerometer:\t%8.4f\t%8.4f\t%8.4f\n",
+				       (double)raw.accelerometer_m_s2[0],
+				       (double)raw.accelerometer_m_s2[1],
+				       (double)raw.accelerometer_m_s2[2]);
+
+				/* set att and publish this information for other apps */
+				att.roll = raw.accelerometer_m_s2[0];
+				att.pitch = raw.accelerometer_m_s2[1];
+				att.yaw = raw.accelerometer_m_s2[2];
+				orb_publish(ORB_ID(vehicle_attitude), att_pub, &att);
+			}
+
+			/* there could be more file descriptors here, in the form like:
+			 * if (fds[1..n].revents & POLLIN) {}
+			 */
+		}
+	}
+
+	return 0;
+}
---
+++ src/platforms/unittests/main.cpp
@@ -0,0 +1,230 @@
+/****************************************************************************
+ *
+ *   Copyright (C) 2015 Mark Charlebois. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+/**
+ * @file main.cpp
+ * Basic shell to execute builtin "apps"
+ *
+ * @author Mark Charlebois <charlebm@gmail.com>
+ */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <signal.h>
+
+namespace px4
+{
+void init_once(void);
+}
+
+using namespace std;
+
+typedef int (*px4_main_t)(int argc, char *argv[]);
+
+#include "apps.h"
+#include "px4_middleware.h"
+
+static bool _ExitFlag = false;
+extern "C" {
+	void _SigIntHandler(int sig_num);
+	void _SigIntHandler(int sig_num)
+	{
+		cout.flush();
+		cout << endl << "Exiting.." << endl;
+		cout.flush();
+		_exit(0);
+	}
+}
+
+static void print_prompt()
+{
+	cout.flush();
+	cout << "pxh> ";
+	cout.flush();
+}
+
+static void run_cmd(const vector<string> &appargs, bool exit_on_fail)
+{
+	// command is appargs[0]
+	string command = appargs[0];
+
+	if (apps.find(command) != apps.end()) {
+		const char *arg[appargs.size() + 2];
+
+		unsigned int i = 0;
+
+		while (i < appargs.size() && appargs[i] != "") {
+			arg[i] = (char *)appargs[i].c_str();
+			++i;
+		}
+
+		arg[i] = (char *)0;
+		int retval = apps[command](i, (char **)arg);
+
+		if (exit_on_fail && retval) {
+			exit(retval);
+		}
+		usleep(65000);
+
+	} else if (command.compare("help") == 0) {
+		list_builtins();
+
+	} else if (command.length() == 0) {
+		// Do nothing
+
+	} else {
+		cout << "Invalid command: " << command << "\ntype 'help' for a list of commands" << endl;
+
+	}
+	print_prompt();
+}
+
+static void usage()
+{
+
+	cout << "./mainapp [-d] [startup_config] -h" << std::endl;
+	cout << "   -d            - Optional flag to run the app in daemon mode and does not take listen for user input." <<
+	     std::endl;
+	cout << "                   This is needed if mainapp is intended to be run as a upstart job on linux" << std::endl;
+	cout << "<startup_config> - config file for starting/stopping px4 modules" << std::endl;
+	cout << "   -h            - help/usage information" << std::endl;
+}
+
+static void process_line(string &line, bool exit_on_fail)
+{
+	vector<string> appargs(8);
+
+	stringstream(line) >> appargs[0] >> appargs[1] >> appargs[2] >> appargs[3] >> appargs[4] >> appargs[5] >> appargs[6] >>
+			   appargs[7];
+	run_cmd(appargs, exit_on_fail);
+}
+
+int main(int argc, char **argv)
+{
+	bool daemon_mode = false;
+	signal(SIGINT, _SigIntHandler);
+
+	int index = 1;
+	bool error_detected = false;
+	char *commands_file = nullptr;
+
+	while (index < argc) {
+		if (argv[index][0] == '-') {
+			// the arg starts with -
+			if (strcmp(argv[index], "-d") == 0) {
+				daemon_mode = true;
+
+			} else if (strcmp(argv[index], "-h") == 0) {
+				usage();
+				return 0;
+
+			} else {
+				PX4_WARN("Unknown/unhandled parameter: %s", argv[index]);
+				return 1;
+			}
+
+		} else {
+			// this is an argument that does not have '-' prefix; treat it like a file name
+			ifstream infile(argv[index]);
+
+			if (infile.good()) {
+				infile.close();
+				commands_file = argv[index];
+
+			} else {
+				PX4_WARN("Error opening file: %s", argv[index]);
+				error_detected = true;
+				break;
+			}
+		}
+
+		++index;
+	}
+
+	if (!error_detected) {
+		px4::init_once();
+
+		px4::init(argc, argv, "mainapp");
+
+		//if commandfile is present, process the commands from the file
+		if (commands_file != nullptr) {
+			ifstream infile(commands_file);
+
+			if (infile.is_open()) {
+				for (string line; getline(infile, line, '\n');) {
+					process_line(line, false);
+				}
+
+			} else {
+				PX4_WARN("Error opening file: %s", commands_file);
+			}
+		}
+
+		if (!daemon_mode) {
+			string mystr;
+
+			print_prompt();
+
+			while (!_ExitFlag) {
+
+				struct pollfd fds;
+				int ret;
+				fds.fd = 0; /* stdin */
+				fds.events = POLLIN;
+				ret = poll(&fds, 1, 100);
+
+				if (ret > 0) {
+					getline(cin, mystr);
+					process_line(mystr, !daemon_mode);
+					mystr = "";
+				}
+			}
+
+		} else {
+			while (!_ExitFlag) {
+				usleep(100000);
+			}
+		}
+
+		if (px4_task_is_running("muorb")) {
+			// sending muorb stop is needed if it is running to exit cleanly
+			vector<string> muorb_stop_cmd = { "muorb", "stop" };
+			run_cmd(muorb_stop_cmd, !daemon_mode);
+		}
+
+		vector<string> shutdown_cmd = { "shutdown" };
+		run_cmd(shutdown_cmd, true);
+	}
+}
