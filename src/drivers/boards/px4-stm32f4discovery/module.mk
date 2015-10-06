fa5071b3c5257c0d73160e850b6d1829bdd5cbc5
Lorenz Meier <lm@inf.ethz.ch>
2015-10-06 11:23:20 +0200

    Remove module.mk files to prevent confusion of adopters upgrading

--- src/drivers/boards/px4-stm32f4discovery/module.mk
+++
@@ -1,7 +0,0 @@
-#
-# Board-specific startup code for the PX4-STM32F4Discovery
-#
-
-SRCS		 = px4discovery_init.c \
-		   px4discovery_usb.c \
-		   px4discovery_led.c
--- src/drivers/irlock/module.mk
+++
@@ -1,9 +0,0 @@
-#
-# Makefile to build the IRLock driver.
-#
-
-MODULE_COMMAND	= irlock
-
-SRCS		= irlock.cpp
-
-MAXOPTIMIZATION	 = -Os
