3dffcc007b9d614dc4f106fef5574dab59aeb8d3
Pavel Kirienko <pavel.kirienko@gmail.com>
2015-10-10 17:09:52 +0300

    STM32 demo - bxCAN GPIO initialization fixed

--- libuavcan_drivers/stm32/test_stm32f107/src/sys/board.h
+++ libuavcan_drivers/stm32/test_stm32f107/src/sys/board.h
@@ -20,6 +20,8 @@
 #define GPIO_PORT_LED    GPIOB
 #define GPIO_PIN_LED     9
 
+// GPIOD 10 is configured as OUTPUT, it is used as board reboot monitor.
+
 /*
  * I/O ports initial setup, this configuration is established soon after reset
  * in the initialization code.
@@ -48,7 +50,7 @@
 #define VAL_GPIOACRH            0x88888888      // 15..8
 #define VAL_GPIOAODR            0x00000000
 
-#define VAL_GPIOBCRL            0x8B488888
+#define VAL_GPIOBCRL            0x84488888      // CAN2 TX initialized as INPUT, it must be configured later!
 #define VAL_GPIOBCRH            0x88888828
 #define VAL_GPIOBODR            0x00000000
 
@@ -56,9 +58,9 @@
 #define VAL_GPIOCCRH            0x88888888
 #define VAL_GPIOCODR            0x00000000
 
-#define VAL_GPIODCRL            0x88b888B4
-#define VAL_GPIODCRH            0x88888888
-#define VAL_GPIODODR            0x00000000
+#define VAL_GPIODCRL            0x88b88844      // CAN1 TX initialized as INPUT, it must be configured later!
+#define VAL_GPIODCRH            0x88888288
+#define VAL_GPIODODR            ((1 << 10))
 
 #define VAL_GPIOECRL            0x88888888
 #define VAL_GPIOECRH            0x88888888
