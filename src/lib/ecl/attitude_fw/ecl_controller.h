c040250624e80bc77ba2f50e3fcca573575a5715
Andreas Antener <antener_a@gmx.ch>
2015-10-11 16:52:09 +0200

    more correct groundspeed scaling for wheel controller

--- src/lib/ecl/attitude_fw/ecl_controller.h
+++ src/lib/ecl/attitude_fw/ecl_controller.h
@@ -75,7 +75,8 @@ struct ECL_ControlData {
 	float airspeed_max;
 	float airspeed;
 	float scaler;
-	float ground_speed;
+	float groundspeed;
+	float groundspeed_scaler;
 	bool lock_integrator;
 };
 
