b081c5c33b53e7bc80780ae507d77b9de10f60ef
tumbili <bapstr@ethz.ch>
2015-10-23 14:00:24 +0200

    added signal handler for floating point exceptions

--- src/platforms/posix/main.cpp
+++ src/platforms/posix/main.cpp
@@ -66,6 +66,13 @@ extern "C" {
 		cout.flush();
 		_exit(0);
 	}
+	void _SigFpeHandler(int sig_num);
+	void _SigFpeHandler(int sig_num)
+	{
+		cout.flush();
+		cout << endl << "floating point exception" << endl;
+		cout.flush();
+	}
 }
 
 static void print_prompt()
@@ -137,6 +144,7 @@ int main(int argc, char **argv)
 {
 	bool daemon_mode = false;
 	signal(SIGINT, _SigIntHandler);
+	signal(SIGFPE, _SigFpeHandler);
 
 	int index = 1;
 	bool error_detected = false;
