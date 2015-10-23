483cb1115711d252cb8e7b3bf06acf06734fce9c
Nate Weibley <nate.weibley@prioria.com>
2015-10-23 11:18:09 -0400

    Fix comparison error in RC+GPS triggered FTS failsafe check
    This bug would cause loss of RC + loss of GPS to trigger a FTS when flying in non-manual modes with a good data link

--- src/modules/commander/commander.cpp
+++ src/modules/commander/commander.cpp
@@ -2320,7 +2320,7 @@ int commander_thread_main(int argc, char *argv[])
 			if ((status.main_state ==vehicle_status_s::MAIN_STATE_ACRO ||
 			     status.main_state ==vehicle_status_s::MAIN_STATE_RATTITUDE ||
 			     status.main_state ==vehicle_status_s::MAIN_STATE_MANUAL ||
-			     status.main_state !=vehicle_status_s::MAIN_STATE_STAB ||
+			     status.main_state ==vehicle_status_s::MAIN_STATE_STAB ||
 			     status.main_state ==vehicle_status_s::MAIN_STATE_ALTCTL ||
 			     status.main_state ==vehicle_status_s::MAIN_STATE_POSCTL) &&
 			    ((status.rc_signal_lost && status.gps_failure) ||
