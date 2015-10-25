6b52f55f57745a1729274e535d401c199d2afa47
jgoppert <james.goppert@gmail.com>
2015-10-25 10:58:34 -0400

    Resurrect controllib testing.

--- src/modules/controllib/controllib_test_main.cpp
+++ src/modules/controllib/controllib_test_main.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (C) 2012 PX4 Development Team. All rights reserved.
+ *   Copyright (c) 2013-2015 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,28 +32,20 @@
  ****************************************************************************/
 
 /**
- * @file test.hpp
+ * @file controllib.cpp
+ * Unit testing for controllib.
  *
- * Controller library code
+ * @author James Goppert <james.goppert@gmail.com>
  */
 
-#pragma once
+#include "blocks.hpp"
 
-//#include <assert.h>
-//#include <time.h>
-//#include <stdlib.h>
+extern "C" __EXPORT int controllib_test_main(int argc, char *argv[]);
 
-bool equal(float a, float b, float eps = 1e-5);
-
-bool greater_than(float a, float b);
-
-bool less_than(float a, float b);
-
-bool greater_than_or_equal(float a, float b);
-
-bool less_than_or_equal(float a, float b);
-
-void float2SigExp(
-	const float &num,
-	float &sig,
-	int &exp);
+int controllib_test_main(int argc, char *argv[])
+{
+	(void)argc;
+	(void)argv;
+	control::basicBlocksTest();
+	return 0;
+}
