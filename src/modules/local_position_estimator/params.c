56c692254b9d1e1915e14f77dfc162fef3296dff
jgoppert <james.goppert@gmail.com>
2015-10-27 10:08:06 -0400

    Added gps delay compensation to lpe.

--- src/modules/local_position_estimator/params.c
+++ src/modules/local_position_estimator/params.c
@@ -135,7 +135,15 @@ PARAM_DEFINE_FLOAT(LPE_GPS_VZ, 0.237f);
  */
 PARAM_DEFINE_FLOAT(LPE_EPH_MAX, 3.0f);
 
-
+/**
+ * GPS delay
+ *
+ * @group Local Position Estimator
+ * @unit seconds
+ * @min 0.0
+ * @max 1.0
+ */
+PARAM_DEFINE_FLOAT(LPE_GPS_DELAY, 0.3f);
 
 /**
  * Vision xy standard deviation.
