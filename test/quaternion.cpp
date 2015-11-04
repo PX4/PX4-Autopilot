b842f8d8a530251d6df0d123c0274993854e1062
jgoppert <james.goppert@gmail.com>
2015-11-03 20:17:34 -0500

    Added travis.

--- test/quaternion.cpp
+++ test/quaternion.cpp
@@ -1,12 +1,17 @@
 #include "Quaternion.hpp"
-#include <assert.h>
-#include <stdio.h>
+#include <cassert>
+#include <cstdio>
 
 using namespace matrix;
 
 int main()
 {
-    Quatf p(1, 0, 0, 0);
+    Quatf p(1, 2, 3, 4);
+	assert(p(0) == 1);
+	assert(p(1) == 2);
+	assert(p(2) == 3);
+	assert(p(3) == 100);
+
     Quatf q(0, 1, 0, 0);
     Quatf r = p*q;
     Dcmf dcm = Dcmf(p);
