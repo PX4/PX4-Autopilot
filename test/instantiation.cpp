88def6b54efc246b636c813c8bdd5a31194c186e
jgoppert <james.goppert@gmail.com>
2015-11-04 03:37:33 -0500

    Made instantiation actual test program.

--- test/instantiation.cpp
+++ test/instantiation.cpp
@@ -7,11 +7,17 @@
 #include <Scalar.hpp>
 #include <Quaternion.hpp>
 
-namespace matrix {
+using namespace matrix;
 
 template class Vector<float, 2>;
 template class Euler<float>;
 template class Scalar<float>;
 template class Matrix<float, 3, 3>;
 
+int main() {
+	Vector<float, 2> v;
+	Euler<float> e;
+	Scalar<float> s;
+	Matrix<float, 3, 3> m;
+	return 0;
 };
