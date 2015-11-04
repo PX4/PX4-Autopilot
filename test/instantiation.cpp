4bf4611087cbf3e59388c6d4fed0ae41c435510d
jgoppert <james.goppert@gmail.com>
2015-11-04 10:54:46 -0500

    Work on coverage.

--- test/instantiation.cpp
+++ test/instantiation.cpp
@@ -7,17 +7,11 @@
 #include <Scalar.hpp>
 #include <Quaternion.hpp>
 
-using namespace matrix;
+namespace matrix {
 
 template class Vector<float, 2>;
 template class Euler<float>;
 template class Scalar<float>;
 template class Matrix<float, 3, 3>;
 
-int main() {
-	Vector<float, 2> v;
-	Euler<float> e;
-	Scalar<float> s;
-	Matrix<float, 3, 3> m;
-	return 0;
 };
