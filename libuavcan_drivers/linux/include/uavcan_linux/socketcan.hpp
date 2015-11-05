50ce241ba0e35d65f2d46c31b86f1ee2fd321e90
Pavel Kirienko <pavel.kirienko@gmail.com>
2015-11-05 12:01:29 +0300

    SocketCAN: default max_frames_in_socket_tx_queue reduced to 2

--- libuavcan_drivers/linux/include/uavcan_linux/socketcan.hpp
+++ libuavcan_drivers/linux/include/uavcan_linux/socketcan.hpp
@@ -321,7 +321,7 @@ public:
      *
      * @ref max_frames_in_socket_tx_queue       See a note in the class comment.
      */
-    SocketCanIface(const SystemClock& clock, int socket_fd, int max_frames_in_socket_tx_queue = 3)
+    SocketCanIface(const SystemClock& clock, int socket_fd, int max_frames_in_socket_tx_queue = 2)
         : clock_(clock)
         , fd_(socket_fd)
         , max_frames_in_socket_tx_queue_(max_frames_in_socket_tx_queue)
