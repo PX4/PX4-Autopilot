5f407f539ecd6137feca815b7e9dc1c0a11db619
Lorenz Meier <lm@inf.ethz.ch>
2015-11-07 16:27:32 +0100

    Sensors: Fix wrong define leading to incorrect battery voltage scaler picked for FMUv1. Found by @xn365

--- src/modules/sensors/sensors.cpp
+++ src/modules/sensors/sensors.cpp
@@ -850,7 +850,7 @@ Sensors::parameters_update()
 		_parameters.battery_voltage_scaling = 0.0082f;
 #elif CONFIG_ARCH_BOARD_AEROCORE
 		_parameters.battery_voltage_scaling = 0.0063f;
-#elif CONFIG_ARCH_BOARD_PX4FMU_V2
+#elif CONFIG_ARCH_BOARD_PX4FMU_V1
 		_parameters.battery_voltage_scaling = 0.00459340659f;
 #else
 		/* ensure a missing default trips a low voltage lockdown */
