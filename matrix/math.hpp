222e42f9342e4ab50ea6a14fccc5051b45acd6d6
Lorenz Meier <lm@inf.ethz.ch>
2015-11-07 09:43:28 +0100

    Re-add convenience header

---
+++ matrix/math.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "Matrix.hpp"
+#include "SquareMatrix.hpp"
+#include "Vector.hpp"
+#include "Vector3.hpp"
+#include "Euler.hpp"
+#include "Dcm.hpp"
+#include "Scalar.hpp"
+#include "Quaternion.hpp"
