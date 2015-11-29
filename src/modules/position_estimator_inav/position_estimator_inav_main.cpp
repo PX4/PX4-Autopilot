3515e6ae918d1e29a656ead9e99ba79f9704957d
Lorenz Meier <lm@inf.ethz.ch>
2015-11-29 17:38:26 +0100

    INAV: Increase stack size as needed

--- src/modules/position_estimator_inav/position_estimator_inav_main.cpp
+++ src/modules/position_estimator_inav/position_estimator_inav_main.cpp
@@ -154,7 +154,7 @@ int position_estimator_inav_main(int argc, char *argv[])
 
 		thread_should_exit = false;
 		position_estimator_inav_task = px4_task_spawn_cmd("position_estimator_inav",
-					       SCHED_DEFAULT, SCHED_PRIORITY_MAX - 5, 5000,
+					       SCHED_DEFAULT, SCHED_PRIORITY_MAX - 5, 5300,
 					       position_estimator_inav_thread_main,
 					       (argv && argc > 2) ? (char * const *) &argv[2] : (char * const *) NULL);
 		return 0;
