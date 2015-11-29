a4aa844151b07c993d814ab9024ffabe40a7a52f
Lorenz Meier <lm@inf.ethz.ch>
2015-11-29 17:06:54 +0100

    FMU driver: Slightly increase run interval to save load

--- src/drivers/px4fmu/fmu.cpp
+++ src/drivers/px4fmu/fmu.cpp
@@ -94,7 +94,7 @@
  * This is the analog to FMU_INPUT_DROP_LIMIT_US on the IO side
  */
 
-#define CONTROL_INPUT_DROP_LIMIT_US		1500
+#define CONTROL_INPUT_DROP_LIMIT_US		1800
 #define NAN_VALUE	(0.0f/0.0f)
 
 class PX4FMU : public device::CDev
