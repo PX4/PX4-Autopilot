754aa6e94f6633e5a4695f3db064c9ebedd47b0a
Lorenz Meier <lm@inf.ethz.ch>
2015-12-01 13:42:06 +0100

    Work thread: Add missing header for Linux

--- src/platforms/posix/work_queue/work_thread.c
+++ src/platforms/posix/work_queue/work_thread.c
@@ -45,6 +45,10 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <queue.h>
+#ifdef __PX4_LINUX
+#define _GNU_SOURCE
+#endif
+#include <pthread.h>
 #include <px4_workqueue.h>
 #include <drivers/drv_hrt.h>
 #include "work_lock.h"
