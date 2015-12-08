b09cee24c80d2e512315cc13300e26cd1260a572
Mark Whitehorn <kd0aij@gmail.com>
2015-12-08 16:52:39 -0700

    clean up LidarLite PWM dropout logic and add comments

--- src/drivers/ll40ls/LidarLitePWM.cpp
+++ src/drivers/ll40ls/LidarLitePWM.cpp
@@ -205,25 +205,30 @@ int LidarLitePWM::measure()
 
 	// Normal reporting interval for LidarLite in PWM mode is 50msec
 	if (pulse_interval > 60*1000UL) {
+		// missed pulse, frequently associated with invalid pulsewidth measurements
 		printf("LL_pwm dropout length: %d\n", pulse_interval);
 		_pulseCount = 0;
 	} else {
+		// wait for 4 successive pulses at 50msec intervals before accepting pulsewidth
 		if (_pulseCount < 4) {
 			_pulseCount++;
-			if (_pulseCount < 4) {
-				printf("_pulseCount: %d, distance: %6.3f\n", _pulseCount, (double)_range.current_distance);
-				// report an invalid range of 0m, to avoid glitches on takeoff
-				_range.current_distance = 0.0f;
+			// hold last valid range reported to avoid glitches
+			printf("_pulseCount: %d, distance: %6.3f, forcing to _lastDistance\n", _pulseCount, (double)_range.current_distance);
+			_range.current_distance = _lastDistance;
+			if (_pulseCount >= 4) {
+				// next pulse will be accepted as valid, if received within 60msec
+				printf("pulse train detected\n");
 			}
 		} else {
+			// valid pulse train, reset lastDistance
 			_lastDistance = _range.current_distance;
 		}
 	}
 	// check for range discontinuity
-	float deltaRange = _lastDistance - _range.current_distance;
+	float deltaRange = _range.current_distance - _lastDistance;
 	if (fabsf(deltaRange) > 1.0f) {
 		printf("LL range discontinuity: %6.3f\n", (double)deltaRange);
-		printf("holding last distance: %6.3f\n", (double)_lastDistance);
+		printf("   holding last distance: %6.3f\n", (double)_lastDistance);
 		_range.current_distance = _lastDistance;
 	}
 
