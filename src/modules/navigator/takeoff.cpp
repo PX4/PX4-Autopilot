d142711e64e00573017e538b935e7dc6d285bcd3
Andreas Antener <antener_a@gmx.ch>
2015-12-20 17:41:47 +0100

    proper setpoint handling on takeoff, switch to loiter sp when takeoff finished

--- src/modules/navigator/takeoff.cpp
+++ src/modules/navigator/takeoff.cpp
@@ -97,5 +97,11 @@ Takeoff::on_active()
 	if (is_mission_item_reached() && !_navigator->get_mission_result()->finished) {
 		_navigator->get_mission_result()->finished = true;
 		_navigator->set_mission_result_updated();
+
+		/* set loiter item so position controllers stop doing takeoff logic */
+		set_loiter_item(&_mission_item);
+		struct position_setpoint_triplet_s *pos_sp_triplet = _navigator->get_position_setpoint_triplet();
+		mission_item_to_position_setpoint(&_mission_item, &pos_sp_triplet->current);
+		_navigator->set_position_setpoint_triplet_updated();
 	}
 }
