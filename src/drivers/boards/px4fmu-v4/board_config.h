a5bebbd659b8dfeb9d15bed7d54ed49f1148b239
Lorenz Meier <lm@inf.ethz.ch>
2016-01-01 20:32:14 +0100

    FMUv4: Add baro SPI bus define

--- src/drivers/boards/px4fmu-v4/board_config.h
+++ src/drivers/boards/px4fmu-v4/board_config.h
@@ -120,6 +120,7 @@ __BEGIN_DECLS
 
 #define PX4_SPI_BUS_SENSORS	1
 #define PX4_SPI_BUS_RAMTRON	2
+#define PX4_SPI_BUS_BARO	PX4_SPI_BUS_RAMTRON
 
 /* Use these in place of the spi_dev_e enumeration to select a specific SPI device on SPI1 */
 #define PX4_SPIDEV_GYRO			1
@@ -133,6 +134,9 @@ __BEGIN_DECLS
  * use 3 for the barometer to differentiate
  */
 #define PX4_SPIDEV_BARO			3
+#if (PX4_SPIDEV_BARO == SPIDEV_FLASH)
+ #error PX4_SPIDEV_BARO must not be equal to SPIDEV_FLASH as they share the same bus
+#endif
 
 /* I2C busses */
 #define PX4_I2C_BUS_EXPANSION	1
