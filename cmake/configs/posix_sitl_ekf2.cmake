dc11b8a7efa8a18de2b2c8a68f70c6849775bcf5
Lorenz Meier <lm@inf.ethz.ch>
2016-01-01 12:30:52 +0100

    EKF2 sitl target: start normal app selection

--- cmake/configs/posix_sitl_ekf2.cmake
+++ cmake/configs/posix_sitl_ekf2.cmake
@@ -32,18 +32,24 @@ set(config_module_list
 	modules/sensors
 	modules/simulator
 	modules/mavlink
-	modules/dataman
 	modules/attitude_estimator_ekf
-	#modules/attitude_estimator_q
+	modules/attitude_estimator_q
 	modules/ekf_att_pos_estimator
+	modules/ekf2
 	modules/position_estimator_inav
+	modules/navigator
+	modules/vtol_att_control
 	modules/mc_pos_control
 	modules/mc_att_control
-	modules/navigator
+	modules/mc_pos_control_multiplatform
+	modules/mc_att_control_multiplatform
+	modules/land_detector
+	modules/fw_att_control
+	modules/fw_pos_control_l1
+	modules/dataman
+	modules/sdlog2
 	modules/commander
-	modules/vtol_att_control
 	modules/controllib
-	modules/ekf2
 	lib/mathlib
 	lib/mathlib/math/filter
 	lib/conversion
@@ -54,6 +60,8 @@ set(config_module_list
 	lib/launchdetection
 	lib/terrain_estimation
 	lib/runway_takeoff
+	lib/tailsitter_recovery
+	examples/px4_simple_app
 	)
 
 set(config_extra_builtin_cmds
