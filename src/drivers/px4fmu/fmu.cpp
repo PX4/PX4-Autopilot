3f61e582a43428585df14f22abae5557a06c910a
Mark Whitehorn <kd0aij@gmail.com>
2016-01-08 17:41:51 -0700

    remove some debug code

--- src/drivers/px4fmu/fmu.cpp
+++ src/drivers/px4fmu/fmu.cpp
@@ -638,6 +638,8 @@ PX4FMU::cycle()
 
 		update_pwm_rev_mask();
 
+		_rcs_fd = :: open(RC_SERIAL_PORT, O_RDWR | O_NONBLOCK);
+
 		_initialized = true;
 	}
 
@@ -947,100 +949,6 @@ PX4FMU::cycle()
 	case RC_SCAN_SUMD:
 		break;
 	}
-#endif
-
-#define DSM_SERIAL_PORTxx
-#ifdef DSM_SERIAL_PORT
-	uint8_t n_bytes = 0;
-	uint8_t *bytes;
-	bool dsm_11_bit;
-	uint16_t raw_rc_values[input_rc_s::RC_INPUT_MAX_CHANNELS];
-	uint16_t raw_rc_count;
-
-	//			/* for PixRacer R07, this signal is active low */
-	//			/* for PixRacer R12, this signal is active high */
-	stm32_gpiowrite(GPIO_SBUS_INV, 1);
-
-	bool dsm_updated = dsm_input(_dsm_fd, raw_rc_values, &raw_rc_count, &dsm_11_bit, &n_bytes, &bytes,
-			input_rc_s::RC_INPUT_MAX_CHANNELS);
-
-	if (dsm_updated) {
-
-		warnx("%llu, %u, %u, %u, %u, %u\n", hrt_absolute_time(), dsm_updated,
-				raw_rc_values[0], raw_rc_values[1], raw_rc_values[2],
-				raw_rc_values[3]);
-
-//		if (dsm_11_bit) {
-//			raw_rc_flags |= PX4IO_P_RAW_RC_FLAGS_RC_DSM11;
-//
-//		} else {
-//			raw_rc_flags &= ~PX4IO_P_RAW_RC_FLAGS_RC_DSM11;
-//		}
-//
-//		raw_rc_flags &= ~(PX4IO_P_RAW_RC_FLAGS_FRAME_DROP);
-//		raw_rc_flags &= ~(PX4IO_P_RAW_RC_FLAGS_FAILSAFE);
-
-		// we have a new SBUS frame. Publish it.
-		_rc_in.channel_count = raw_rc_count;
-
-		if (_rc_in.channel_count > input_rc_s::RC_INPUT_MAX_CHANNELS) {
-			_rc_in.channel_count = input_rc_s::RC_INPUT_MAX_CHANNELS;
-		}
-
-		for (uint8_t i = 0; i < _rc_in.channel_count; i++) {
-			_rc_in.values[i] = raw_rc_values[i];
-		}
-
-		_rc_in.timestamp_publication = hrt_absolute_time();
-		_rc_in.timestamp_last_signal = _rc_in.timestamp_publication;
-
-		_rc_in.rc_ppm_frame_length = 0;
-//		_rc_in.rssi = (!sbus_frame_drop) ? RC_INPUT_RSSI_MAX : (RC_INPUT_RSSI_MAX / 2);
-//		_rc_in.rc_failsafe = sbus_failsafe;
-		_rc_in.rc_lost = false;
-//		_rc_in.rc_lost_frame_count = sbus_dropped_frames();
-		_rc_in.rc_total_frame_count = 0;
-
-		rc_updated = true;
-	}
-
-#endif
-
-#ifdef SBUS_SERIAL_PORT
-	bool sbus_failsafe, sbus_frame_drop;
-	uint16_t raw_rc_values[input_rc_s::RC_INPUT_MAX_CHANNELS];
-	uint16_t raw_rc_count;
-
-	// read port
-	bool sbus_updated = sbus_input(_rcs_fd, &raw_rc_values[0], &raw_rc_count,
-				       &sbus_failsafe, &sbus_frame_drop,
-				       input_rc_s::RC_INPUT_MAX_CHANNELS);
-
-	if (sbus_updated) {
-		// we have a new SBUS frame. Publish it.
-		_rc_in.channel_count = raw_rc_count;
-
-		if (_rc_in.channel_count > input_rc_s::RC_INPUT_MAX_CHANNELS) {
-			_rc_in.channel_count = input_rc_s::RC_INPUT_MAX_CHANNELS;
-		}
-
-		for (uint8_t i = 0; i < _rc_in.channel_count; i++) {
-			_rc_in.values[i] = raw_rc_values[i];
-		}
-
-		_rc_in.timestamp_publication = hrt_absolute_time();
-		_rc_in.timestamp_last_signal = _rc_in.timestamp_publication;
-
-		_rc_in.rc_ppm_frame_length = 0;
-		_rc_in.rssi = (!sbus_frame_drop) ? RC_INPUT_RSSI_MAX : (RC_INPUT_RSSI_MAX / 2);
-		_rc_in.rc_failsafe = sbus_failsafe;
-		_rc_in.rc_lost = false;
-		_rc_in.rc_lost_frame_count = sbus_dropped_frames();
-		_rc_in.rc_total_frame_count = 0;
-
-		rc_updated = true;
-	}
-
 #endif
 
 #ifdef HRT_PPM_CHANNEL
