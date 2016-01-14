05b23a8b5487ca1f729b322f2ef5a22b74deacdc
Andreas Antener <antener_a@gmx.ch>
2016-01-14 14:25:22 +0100

    disable pos/vel control during mission in transitions

--- src/modules/commander/commander.cpp
+++ src/modules/commander/commander.cpp
@@ -3018,8 +3018,8 @@ set_control_mode()
 		control_mode.flag_control_attitude_enabled = true;
 		control_mode.flag_control_altitude_enabled = true;
 		control_mode.flag_control_climb_rate_enabled = true;
-		control_mode.flag_control_position_enabled = true;
-		control_mode.flag_control_velocity_enabled = true;
+		control_mode.flag_control_position_enabled = !status.in_transition_mode;
+		control_mode.flag_control_velocity_enabled = !status.in_transition_mode;
 		control_mode.flag_control_termination_enabled = false;
 		break;
 
