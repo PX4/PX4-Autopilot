1668d8e5e6e11adf677b25908b824c9627ae356a
Mark Whitehorn <kd0aij@gmail.com>
2016-01-14 22:00:15 -0700

    make sure GPIO_RC_OUT is configured at all times
    DSM bind pulse sequence starting high, low pulses, ending high

--- src/drivers/px4fmu/fmu.cpp
+++ src/drivers/px4fmu/fmu.cpp
@@ -226,7 +226,6 @@ private:
 			unsigned frame_drops, int rssi);
 	void dsm_bind_ioctl(int dsmMode);
 	void set_rc_scan_state(RC_SCAN _rc_scan_state);
-	void rc_io_invert();
 	void rc_io_invert(bool invert);
 };
 
@@ -683,10 +682,10 @@ void PX4FMU::set_rc_scan_state(RC_SCAN newState)
 void PX4FMU::rc_io_invert(bool invert)
 {
 	INVERT_RC_INPUT(invert);
-
-	if (!invert) {
-		// set FMU_RC_OUTPUT high to pull RC_INPUT up
-		stm32_gpiowrite(GPIO_RC_OUT, 1);
+	if (invert) {
+		stm32_gpiowrite(GPIO_RC_OUT, 1);    /* set RC_OUTPUT to pull RC input down */
+	} else {
+		stm32_gpiowrite(GPIO_RC_OUT, 0);    /* set RC_OUTPUT to pull RC input down */
 	}
 }
 #endif
@@ -716,6 +715,8 @@ PX4FMU::cycle()
 		sbus_config(_rcs_fd, false);
 		// disable CPPM input by mapping it away from the timer capture input
 		stm32_configgpio(GPIO_PPM_IN & ~(GPIO_AF_MASK | GPIO_PUPD_MASK));
+		stm32_configgpio(GPIO_RC_OUT);
+		rc_io_invert(true);
 #endif
 
 		_initialized = true;
@@ -1614,7 +1615,7 @@ PX4FMU::pwm_ioctl(file *filp, int cmd, unsigned long arg)
 		    arg == DSMX8_BIND_PULSES) {
 
 			dsm_bind(DSM_CMD_BIND_POWER_DOWN, 0);
-			usleep(500000);
+			usleep(1000000);
 
 			dsm_bind(DSM_CMD_BIND_SET_RX_OUT, 0);
 
@@ -2109,8 +2110,11 @@ PX4FMU::dsm_bind_ioctl(int dsmMode)
 	if (!_armed.armed) {
 //      mavlink_log_info(_mavlink_fd, "[FMU] binding DSM%s RX", (dsmMode == 0) ? "2" : ((dsmMode == 1) ? "-X" : "-X8"));
 		warnx("[FMU] binding DSM%s RX", (dsmMode == 0) ? "2" : ((dsmMode == 1) ? "-X" : "-X8"));
+		rc_io_invert(true);
+		SPEKTRUM_RX_HIGH(false);
 		int ret = ioctl(nullptr, DSM_BIND_START,
 				(dsmMode == 0) ? DSM2_BIND_PULSES : ((dsmMode == 1) ? DSMX_BIND_PULSES : DSMX8_BIND_PULSES));
+		SPEKTRUM_RX_HIGH(false);
 
 		if (ret) {
 //            mavlink_log_critical(_mavlink_fd, "binding failed.");
