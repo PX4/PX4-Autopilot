cbde246f0a02afb771cccc5e61faed5f7115e22a
Lorenz Meier <lm@inf.ethz.ch>
2016-01-20 10:08:40 +0100

    FMUv4: Remove non-existent airspeed sensing channel

--- src/drivers/boards/px4fmu-v4/board_config.h
+++ src/drivers/boards/px4fmu-v4/board_config.h
@@ -154,7 +154,7 @@ __BEGIN_DECLS
  *
  * These are the channel numbers of the ADCs of the microcontroller that can be used by the Px4 Firmware in the adc driver
  */
-#define ADC_CHANNELS (1 << 2) | (1 << 3) | (1 << 4) | (1 << 10) | (1 << 11) | (1 << 12) | (1 << 13) | (1 << 14) | (1 << 15)
+#define ADC_CHANNELS (1 << 2) | (1 << 3) | (1 << 4) | (1 << 10) | (1 << 11) | (1 << 12) | (1 << 13) | (1 << 14)
 
 // ADC defines to be used in sensors.cpp to read from a particular channel
 #define ADC_BATTERY_VOLTAGE_CHANNEL		2
