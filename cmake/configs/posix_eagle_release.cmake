e47273f10bfb8c40bdd51d8662c715ceb6a4f727
Mark Charlebois <charlebm@gmail.com>
2016-01-20 21:09:37 -0800

    Provide impelmentation of px4muorb_KraitRpcWrapper
    The Terminate function is not properly implemented yet.
    
    Signed-off-by: Mark Charlebois <charlebm@gmail.com>

--- cmake/configs/posix_eagle_release.cmake
+++ cmake/configs/posix_eagle_release.cmake
@@ -1,24 +1,11 @@
 include(posix/px4_impl_posix)
 
-# Use build stubs unless explicitly set not to
-if("${DSPAL_STUBS_ENABLE}" STREQUAL "")
-	set(DSPAL_STUBS_ENABLE "1")
-endif()
-
-if ("${QRL_SDK_DIR}" STREQUAL "")
-	set(QRL_SDK_DIR /opt/qrlsdk)
-endif()
-
-set(CMAKE_PROGRAM_PATH 
-	"${QRL_SDK_DIR}/gcc-linaro-4.8-2015.06-x86_64_arm-linux-gnueabihf/bin"
-	${CMAKE_PROGRAM_PATH}
-	)
-
 set(CMAKE_TOOLCHAIN_FILE ${CMAKE_SOURCE_DIR}/cmake/toolchains/Toolchain-arm-linux-gnueabihf.cmake)
 
 set(CONFIG_SHMEM "1")
 
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -DENABLE_SHMEM")
+include(${CMAKE_SOURCE_DIR}/cmake/cmake_hexagon/qurt_app.cmake)
 
 set(config_module_list
 	drivers/device
