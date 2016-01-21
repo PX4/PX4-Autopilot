76875ac723f729a6a5f94860d57402a29f0531c9
Mark Charlebois <charlebm@gmail.com>
2016-01-20 21:47:49 -0800

    More bad merge fixes
    Signed-off-by: Mark Charlebois <charlebm@gmail.com>

--- src/platforms/posix/tests/vcdev_test/vcdevtest_example.cpp
+++ src/platforms/posix/tests/vcdev_test/vcdevtest_example.cpp
@@ -321,12 +321,16 @@ int VCDevExample::main()
 				 (char *const *)NULL);
 
 	ret = 0;
+
 	PX4_INFO("TEST: BLOCKING POLL ---------------");
 
 	if (do_poll(fd, -1, 3, 0)) {
 		ret = 1;
 		goto fail2;
+	}
+
 	PX4_INFO("TEST: ZERO TIMEOUT POLL -----------");
+
 	if(do_poll(fd, 0, 3, 0)) {
 		ret = 1;
 		goto fail2;
