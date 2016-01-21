7a7a01ef259723470b5c7e339a261d86f62ee611
Mark Charlebois <charlebm@gmail.com>
2016-01-20 21:09:37 -0800

    Provide impelmentation of px4muorb_KraitRpcWrapper
    The Terminate function is not properly implemented yet.
    
    Signed-off-by: Mark Charlebois <charlebm@gmail.com>

--- cmake/toolchains/Toolchain-arm-linux-gnueabihf.cmake
+++ cmake/toolchains/Toolchain-arm-linux-gnueabihf.cmake
@@ -37,8 +37,6 @@ set(CMAKE_SYSTEM_VERSION 1)
 
 # specify the cross compiler
 find_program(C_COMPILER arm-linux-gnueabihf-gcc
-	PATHS ${HEXAGON_SDK_ROOT}/gcc-linaro-arm-linux-gnueabihf-4.8-2013.08_linux/bin
-	NO_DEFAULT_PATH
 	)
 if(NOT C_COMPILER)
 	message(FATAL_ERROR "could not find arm-linux-gnueabihf-gcc compiler")
@@ -46,8 +44,6 @@ endif()
 cmake_force_c_compiler(${C_COMPILER} GNU)
 
 find_program(CXX_COMPILER arm-linux-gnueabihf-g++
-	PATHS ${HEXAGON_SDK_ROOT}/gcc-linaro-arm-linux-gnueabihf-4.8-2013.08_linux/bin
-	NO_DEFAULT_PATH
 	)
 if(NOT CXX_COMPILER)
 	message(FATAL_ERROR "could not find arm-linux-gnueabihf-g++ compiler")
@@ -58,8 +54,6 @@ cmake_force_cxx_compiler(${CXX_COMPILER} GNU)
 foreach(tool objcopy nm ld)
 	string(TOUPPER ${tool} TOOL)
 	find_program(${TOOL} arm-linux-gnueabihf-${tool}
-		PATHS ${HEXAGON_SDK_ROOT}/gcc-linaro-arm-linux-gnueabihf-4.8-2013.08_linux/bin
-		NO_DEFAULT_PATH
 		)
 	if(NOT ${TOOL})
 		message(FATAL_ERROR "could not find arm-linux-gnueabihf-${tool}")
