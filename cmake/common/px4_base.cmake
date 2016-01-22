e18d5ea784d0412e86e6467a70140145681e0de1
Mark Charlebois <charlebm@gmail.com>
2016-01-21 21:09:12 -0800

    Added -fPIC flag for only qurt build
    Signed-off-by: Mark Charlebois <charlebm@gmail.com>

--- cmake/common/px4_base.cmake
+++ cmake/common/px4_base.cmake
@@ -536,13 +536,18 @@ function(px4_add_common_flags)
 
 	set(max_optimization -Os)
 
+	set(PIC_FLAG)
+	if ("${OS}" STREQUAL "qurt")
+		set(PIC_FLAG -fPIC)
+	endif()
+
 	set(optimization_flags
 		-fno-strict-aliasing
 		-fomit-frame-pointer
 		-funsafe-math-optimizations
 		-ffunction-sections
 		-fdata-sections
-		-fPIC
+		${PIC_FLAG}
 		)
 
 	if (NOT ${CMAKE_C_COMPILER_ID} MATCHES ".*Clang.*")
