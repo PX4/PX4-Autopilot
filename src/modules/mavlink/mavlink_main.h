8167f729adeca942de6cd82aa7815ec3e319aa93
Lorenz Meier <lm@inf.ethz.ch>
2016-01-31 22:04:39 +0100

    MAVLink app: Fix NuttX compilation error

--- src/modules/mavlink/mavlink_main.h
+++ src/modules/mavlink/mavlink_main.h
@@ -333,17 +333,19 @@ public:
 
 	unsigned		get_system_type() { return _system_type; }
 
-	Protocol 		get_protocol() { return _protocol; };
+	Protocol 		get_protocol() { return _protocol; }
 
 	unsigned short		get_network_port() { return _network_port; }
 
 	int 			get_socket_fd () { return _socket_fd; };
 #ifdef __PX4_POSIX
-	struct sockaddr_in *	get_client_source_address() {return &_src_addr;};
+	struct sockaddr_in *	get_client_source_address() { return &_src_addr; }
 
-	void			set_client_source_initialized() { _src_addr_initialized = true; };
+	void			set_client_source_initialized() { _src_addr_initialized = true; }
 
-	bool			get_client_source_initialized() { return _src_addr_initialized; };
+	bool			get_client_source_initialized() { return _src_addr_initialized; }
+#else
+	bool			get_client_source_initialized() { return true; }
 #endif
 
 	uint64_t		get_start_time() { return _mavlink_start_time; }
