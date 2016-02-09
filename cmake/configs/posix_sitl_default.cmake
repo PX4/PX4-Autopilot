463df4333d7a6ac87e5b35ca4c33b927fb803ba4
Mark Charlebois <charlebm@gmail.com>
2016-02-09 00:33:28 -0800

    Fixed SITL build (and clang posix build)
    Signed-off-by: Mark Charlebois <charlebm@gmail.com>

--- cmake/configs/posix_sitl_default.cmake
+++ cmake/configs/posix_sitl_default.cmake
@@ -18,7 +18,6 @@ set(config_module_list
 	platforms/posix/drivers/gyrosim
 	platforms/posix/drivers/rgbledsim
 	platforms/posix/drivers/ledsim
-	platforms/posix/drivers/df_imu
 	systemcmds/param
 	systemcmds/mixer
 	systemcmds/ver
