546cc72a87170c3a6209eb1c03a7b222278911b7
Julian Oes <julian@oes.ch>
2016-02-10 15:23:33 +0100

    sensors: header comment changes

--- src/modules/sensors/sensors.cpp
+++ src/modules/sensors/sensors.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2012-2015 PX4 Development Team. All rights reserved.
+ *   Copyright (c) 2012-2016 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -41,7 +41,7 @@
  * well instead of relying on the sensor_combined topic.
  *
  * @author Lorenz Meier <lorenz@px4.io>
- * @author Julian Oes <julian@px4.io>
+ * @author Julian Oes <julian@oes.ch>
  * @author Thomas Gubler <thomas@px4.io>
  * @author Anton Babushkin <anton@px4.io>
  */
