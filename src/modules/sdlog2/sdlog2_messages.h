e695af2100b66177b20eba8a7f854068b5d896f0
Andreas Bircher <bircher@gmx.ch>
2016-02-22 13:40:13 +0100

    fixing the logging of trigger information

--- src/modules/sdlog2/sdlog2_messages.h
+++ src/modules/sdlog2/sdlog2_messages.h
@@ -511,7 +511,6 @@ struct log_CTS_s {
 /* --- CAMERA TRIGGER --- */
 #define LOG_CAMT_MSG 48
 struct log_CAMT_s {
-	uint64_t timestamp;
 	uint32_t seq;
 };
 
@@ -592,7 +591,7 @@ static const struct log_format_s log_formats[] = {
 	LOG_FORMAT(ENCD, "qfqf",	"cnt0,vel0,cnt1,vel1"),
 	LOG_FORMAT(TSYN, "Q", 		"TimeOffset"),
 	LOG_FORMAT(MACS, "fff", "RRint,PRint,YRint"),
-	LOG_FORMAT(CAMT, "if", "timestamp,seq"),
+	LOG_FORMAT(CAMT, "I", "seq"),
 
 	/* system-level messages, ID >= 0x80 */
 	/* FMT: don't write format of format message, it's useless */
