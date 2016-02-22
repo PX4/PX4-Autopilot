31beabd7b92780bb2f3ab23034cb5286df29fde1
Julian Oes <julian@oes.ch>
2016-02-22 12:44:43 +0100

    VtolLandDetector: only comment fixes

--- src/modules/land_detector/VtolLandDetector.cpp
+++ src/modules/land_detector/VtolLandDetector.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2013-2015 PX4 Development Team. All rights reserved.
+ *   Copyright (c) 2013-2016 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -33,7 +33,7 @@
 
 /**
  * @file VtolLandDetector.cpp
- * Land detection algorithm for vtol
+ * Land detection algorithm for VTOL
  *
  * @author Roman Bapst <bapstroma@gmail.com>
  */
