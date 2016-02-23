6613335937a8402ebd8799883a7ae183c07f82ee
mcsauder <mcsauder@greypointcorp.com>
2016-02-23 16:15:52 -0700

    Added constexpr back from const var type.

--- EKF/ekf.h
+++ EKF/ekf.h
@@ -98,7 +98,7 @@ public:
 private:
 
 	static const uint8_t _k_num_states = 24;
-	static const float _k_earth_rate = 0.000072921f;
+	static constexpr float _k_earth_rate = 0.000072921f;
 
 	stateSample _state;		// state struct of the ekf running at the delayed time horizon
 
