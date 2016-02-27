473eea154f3f8c7026c74e3b973c7092fc60d80c
Lorenz Meier <lm@inf.ethz.ch>
2016-02-27 12:12:26 +0100

    MC position control: Hold position more aggressively

--- src/modules/mc_pos_control/mc_pos_control_params.c
+++ src/modules/mc_pos_control/mc_pos_control_params.c
@@ -356,7 +356,7 @@ PARAM_DEFINE_FLOAT(MPC_HOLD_XY_DZ, 0.1f);
  * @decimal 2
  * @group Multicopter Position Control
  */
-PARAM_DEFINE_FLOAT(MPC_HOLD_MAX_XY, 0.5f);
+PARAM_DEFINE_FLOAT(MPC_HOLD_MAX_XY, 0.8f);
 
 /**
  * Maximum vertical velocity for which position hold is enabled (use 0 to disable check)
@@ -366,7 +366,7 @@ PARAM_DEFINE_FLOAT(MPC_HOLD_MAX_XY, 0.5f);
  * @decimal 2
  * @group Multicopter Position Control
  */
-PARAM_DEFINE_FLOAT(MPC_HOLD_MAX_Z, 0.5f);
+PARAM_DEFINE_FLOAT(MPC_HOLD_MAX_Z, 0.6f);
 
 /**
  * Low pass filter cut freq. for numerical velocity derivative
@@ -387,4 +387,4 @@ PARAM_DEFINE_FLOAT(MPC_VELD_LP, 5.0f);
  * @decimal 2
  * @group Multicopter Position Control
  */
-PARAM_DEFINE_FLOAT(MPC_ACC_HOR_MAX, 4.0f);
+PARAM_DEFINE_FLOAT(MPC_ACC_HOR_MAX, 6.0f);
