2fc8fcebd39bf017281c5ea9654b11e959881eb1
Andreas Antener <antener_a@gmx.ch>
2016-03-04 22:46:42 +0100

    added param meta data for COM_OFFB_RTL

--- src/modules/commander/commander_params.c
+++ src/modules/commander/commander_params.c
@@ -488,6 +488,9 @@ PARAM_DEFINE_INT32(COM_FLTMODE6, 0);
  * it will wait in LOITER before entering RTL. A value of 0 will make it go straight to RTL.
  *
  * @group Commander
+ * @unit second
  * @min -1
+ * @max 60
+ * @increment 1
  */
 PARAM_DEFINE_FLOAT(COM_OFFB_RTL, -1.0f);
