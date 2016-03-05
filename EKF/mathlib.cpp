f9be23933b2b3dcffa2fdcc07feecde25c783567
mcsauder <mcsauder@greypointcorp.com>
2016-03-05 01:58:58 -0700

    Correct deg to rad conversion inversion.  Add additional functionality to mathlib to allow standalone compile without Limits.cpp and Limits.hpp files from PX4.

--- EKF/mathlib.cpp
+++ EKF/mathlib.cpp
@@ -42,16 +42,35 @@
 
 #ifdef POSIX_SHARED
 
-float math::constrain(float &val, float min, float max)
+
+namespace math
+{
+
+float min(float val1, float val2)
+{
+	return (val1 < val2) ? val1 : val2;
+}
+
+float max(float val1, float val2)
+{
+	return (val1 > val2) ? val1 : val2;
+}
+
+float constrain(float &val, float min, float max)
 {
 	return (val < min) ? min : ((val > max) ? max : val);
 }
-float math::radians(float degrees)
+
+float radians(float degrees)
 {
 	return (degrees / 180.0f) * M_PI_F;
 }
-float math::degrees(float radians)
+
+float degrees(float radians)
 {
 	return (radians * 180.0f) / M_PI_F;
 }
+
+}
+
 #endif
