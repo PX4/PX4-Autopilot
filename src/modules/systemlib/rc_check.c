17b86d97e2a11a800367ac42ff081125f1979a7d
Lorenz Meier <lm@inf.ethz.ch>
2016-03-06 18:05:45 +0100

    System lib format fixes

--- src/modules/systemlib/rc_check.c
+++ src/modules/systemlib/rc_check.c
@@ -62,9 +62,9 @@ int rc_calibration_check(int mavlink_fd, bool report_fail)
 	unsigned map_fail_count = 0;
 
 	const char *rc_map_mandatory[] = {	/*"RC_MAP_MODE_SW",*/
-						/* needs discussion if this should be mandatory "RC_MAP_POSCTL_SW"*/
-						0 /* end marker */
-					 };
+		/* needs discussion if this should be mandatory "RC_MAP_POSCTL_SW"*/
+		0 /* end marker */
+	};
 
 	unsigned j = 0;
 
