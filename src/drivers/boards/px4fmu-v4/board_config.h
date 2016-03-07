7061bc804a03316cd1a186b9cfefbeb66e4fb85b
Robert Dickenson <robert.dickenson@gmail.com>
2016-03-07 19:19:09 +1100

    Remove file with only whitespace change as not required in PR

--- src/drivers/boards/px4fmu-v4/board_config.h
+++ src/drivers/boards/px4fmu-v4/board_config.h
@@ -123,8 +123,8 @@ __BEGIN_DECLS
 #define PX4_SPI_BUS_BARO	PX4_SPI_BUS_RAMTRON
 
 /* Use these in place of the spi_dev_e enumeration to select a specific SPI device on SPI1 */
-#define PX4_SPIDEV_GYRO 		1
-#define PX4_SPIDEV_ACCEL_MAG		2
+#define PX4_SPIDEV_GYRO			1
+#define PX4_SPIDEV_ACCEL_MAG	2
 #define PX4_SPIDEV_MPU			4
 #define PX4_SPIDEV_HMC			5
 #define PX4_SPIDEV_ICM			6
