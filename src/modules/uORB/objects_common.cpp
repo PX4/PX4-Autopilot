c87e8a6ddab75ee1dd9c3d185e8e1fa325a2c509
Julian Oes <julian@oes.ch>
2016-03-07 22:32:02 +0100

    sensors: first part of a calibration refactor
    This adds uORB messages to publish calibration data by sensors which is
    then consumed by the sensors. Currently this is only used on Snapdragon
    and guarded by QURT ifdefs.

--- src/modules/uORB/objects_common.cpp
+++ src/modules/uORB/objects_common.cpp
@@ -279,3 +279,12 @@ ORB_DEFINE(ekf2_innovations, struct ekf2_innovations_s);
 
 #include "topics/ekf2_replay.h"
 ORB_DEFINE(ekf2_replay, struct ekf2_replay_s);
+
+#include "topics/gyro_calibration.h"
+ORB_DEFINE(gyro_calibration, struct gyro_calibration_s);
+
+#include "topics/accel_calibration.h"
+ORB_DEFINE(accel_calibration, struct accel_calibration_s);
+
+#include "topics/mag_calibration.h"
+ORB_DEFINE(mag_calibration, struct mag_calibration_s);
