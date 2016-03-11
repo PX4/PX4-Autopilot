99eafaabcbf423c94251d410dcf2dae8a7e89b58
Julian Oes <julian@oes.ch>
2016-03-11 15:08:59 -0800

    df_hmc5883_wrapper: get the var swapping right

--- src/platforms/posix/drivers/df_hmc5883_wrapper/df_hmc5883_wrapper.cpp
+++ src/platforms/posix/drivers/df_hmc5883_wrapper/df_hmc5883_wrapper.cpp
@@ -171,8 +171,9 @@ int DfHmc9250Wrapper::_publish(struct mag_sensor_data &data)
 	/* The standard external mag by 3DR has x pointing to the
 	 * right, y pointing backwards, and z down, therefore switch x
 	 * and y and invert y. */
+	const float tmp = data.field_x_ga;
 	data.field_x_ga = -data.field_y_ga;
-	data.field_y_ga = data.field_x_ga;
+	data.field_y_ga = tmp;
 
 	// TODO: remove these (or get the values)
 	mag_report.x_raw = NAN;
