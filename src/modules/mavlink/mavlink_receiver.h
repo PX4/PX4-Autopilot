0a645a8cb5b797c7302bb39487d7534d4e32240b
Jimmy Johnson <catch22@fastmail.net>
2016-03-13 07:46:38 -0700

    fixing code style and enter/exit radius logic

--- src/modules/mavlink/mavlink_receiver.h
+++ src/modules/mavlink/mavlink_receiver.h
@@ -199,7 +199,7 @@ private:
 	orb_advert_t _manual_pub;
 	orb_advert_t _land_detector_pub;
 	orb_advert_t _time_offset_pub;
-	orb_advert_t _follow_me_pub;
+	orb_advert_t _follow_target_pub;
 	int _control_mode_sub;
 	int _hil_frames;
 	uint64_t _old_timestamp;
