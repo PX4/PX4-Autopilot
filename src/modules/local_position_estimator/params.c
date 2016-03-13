70b0333d193f6edb60c8fe1d8b9428fd4c3b3bcd
Daniel Agar <daniel@agar.ca>
2016-03-13 14:43:26 -0400

    local_position_estimator param @unit

--- src/modules/local_position_estimator/params.c
+++ src/modules/local_position_estimator/params.c
@@ -6,20 +6,16 @@
 /**
  * Enable local position estimator.
  *
+ * @unit boolean
  * @group Local Position Estimator
- * @min 0
- * @max 1
- * @decimal 0
  */
 PARAM_DEFINE_INT32(LPE_ENABLED, 1);
 
 /**
  * Enable accelerometer integration for prediction.
  *
+ * @unit boolean
  * @group Local Position Estimator
- * @min 0
- * @max 1
- * @decimal 0
  */
 PARAM_DEFINE_INT32(LPE_INTEGRATE, 1);
 
@@ -49,6 +45,7 @@ PARAM_DEFINE_FLOAT(LPE_FLW_XY, 0.01f);
  * Optical flow minimum quality threshold
  *
  * @group Local Position Estimator
+ * @unit
  * @min 0
  * @max 255
  * @decimal 0
@@ -109,7 +106,7 @@ PARAM_DEFINE_FLOAT(LPE_LDR_OFF_Z, 0.00f);
  * should be 0.0464
  *
  * @group Local Position Estimator
- * @unit m/s^2
+ * @unit m/s/s
  * @min 0.00001
  * @max 2
  * @decimal 4
@@ -122,7 +119,7 @@ PARAM_DEFINE_FLOAT(LPE_ACC_XY, 0.0454f);
  * (see Accel x comments)
  *
  * @group Local Position Estimator
- * @unit m/s^2
+ * @unit m/s/s
  * @min 0.00001
  * @max 2
  * @decimal 4
@@ -195,8 +192,6 @@ PARAM_DEFINE_FLOAT(LPE_GPS_VZ, 0.25f);
  */
 PARAM_DEFINE_FLOAT(LPE_EPH_MAX, 3.0f);
 
-
-
 /**
  * Vision xy standard deviation.
  *
@@ -225,6 +220,7 @@ PARAM_DEFINE_FLOAT(LPE_VIS_Z, 0.5f);
  * Set to the appropriate key (328754) to disable vision input.
  *
  * @group Local Position Estimator
+ * @unit
  * @min 0
  * @max 1
  * @decimal 0
@@ -246,7 +242,7 @@ PARAM_DEFINE_FLOAT(LPE_VIC_P, 0.05f);
  * Position propagation process noise power (variance*sampling rate).
  *
  * @group Local Position Estimator
- * @unit (m/s^2)-s
+ * @unit (m/s/s)-s
  * @min 0
  * @max 1
  * @decimal 8
