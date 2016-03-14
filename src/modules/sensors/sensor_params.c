5c810eef11aa78f9321bf1c4836eea3b55fe1172
Lorenz Meier <lm@inf.ethz.ch>
2016-03-14 09:21:18 +0100

    Set reboot required param for flow sensor

--- src/modules/sensors/sensor_params.c
+++ src/modules/sensors/sensor_params.c
@@ -783,6 +783,8 @@ PARAM_DEFINE_INT32(SENS_BOARD_ROT, 0);
  * @value 6 Yaw 270°
  * @value 7 Yaw 315°
  *
+ * @reboot_required true
+ *
  * @group Sensor Calibration
  */
 PARAM_DEFINE_INT32(SENS_FLOW_ROT, 0);
