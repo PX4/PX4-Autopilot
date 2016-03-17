213cdf1a9139488f156721a72feded53d839d964
Julian Oes <julian@oes.ch>
2016-03-17 11:01:45 +0000

    mavlink: send out parameters faster over UDP

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2012-2015 PX4 Development Team. All rights reserved.
+ *   Copyright (c) 2012-2016 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -35,7 +35,7 @@
  * MAVLink 1.0 protocol implementation.
  *
  * @author Lorenz Meier <lm@inf.ethz.ch>
- * @author Julian Oes <joes@student.ethz.ch>
+ * @author Julian Oes <julian@oes.ch>
  * @author Anton Babushkin <anton.babushkin@me.com>
  */
 
@@ -1869,6 +1869,11 @@ Mavlink::task_main(int argc, char *argv[])
 	/* if the protocol is serial, we send the system version blindly */
 	if (get_protocol() == SERIAL) {
 		send_autopilot_capabilites();
+	}
+
+	/* send parameters faster over UDP */
+	if (get_protocol() == UDP) {
+		configure_stream("PARAM_VALUE", 100000.0f);
 	}
 
 	while (!_task_should_exit) {
