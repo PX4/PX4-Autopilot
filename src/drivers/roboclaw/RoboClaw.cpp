6be6499086656e59193676dfac8d69d17cfd849f
Daniel Agar <daniel@agar.ca>
2016-03-20 15:28:55 -0400

    uORB topics autogeneration

--- src/drivers/roboclaw/RoboClaw.cpp
+++ src/drivers/roboclaw/RoboClaw.cpp
@@ -54,7 +54,6 @@
 #include <mavlink/mavlink_log.h>
 
 #include <uORB/Publication.hpp>
-#include <uORB/topics/actuator_controls_0.h>
 #include <uORB/topics/debug_key_value.h>
 #include <drivers/drv_hrt.h>
 
--- src/examples/fixedwing_control/main.c
+++ src/examples/fixedwing_control/main.c
@@ -61,10 +61,6 @@
 #include <uORB/topics/vehicle_attitude_setpoint.h>
 #include <uORB/topics/manual_control_setpoint.h>
 #include <uORB/topics/actuator_controls.h>
-#include <uORB/topics/actuator_controls_0.h>
-#include <uORB/topics/actuator_controls_1.h>
-#include <uORB/topics/actuator_controls_2.h>
-#include <uORB/topics/actuator_controls_3.h>
 #include <uORB/topics/vehicle_rates_setpoint.h>
 #include <uORB/topics/vehicle_global_position.h>
 #include <uORB/topics/parameter_update.h>
