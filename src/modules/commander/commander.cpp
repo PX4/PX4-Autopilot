f718c25dbed900d0e44e60d37247fbf3257ce1a5
Julian Oes <julian@oes.ch>
2016-03-27 10:49:30 +0200

    commander: remove QURT workaround
    Timestamps didn't exist when commander was run on the Linux side.
    However, commander is now always run on the QURT side, so the hack is no
    longer needed.

--- src/modules/commander/commander.cpp
+++ src/modules/commander/commander.cpp
@@ -2257,13 +2257,8 @@ int commander_thread_main(int argc, char *argv[])
 		}
 
 		/* RC input check */
-#ifndef __PX4_QURT
 		if (!status.rc_input_blocked && sp_man.timestamp != 0 &&
 		    (hrt_absolute_time() < sp_man.timestamp + (uint64_t)(rc_loss_timeout * 1e6f))) {
-#else
-		// HACK: remove old data check due to timestamp issue in QURT
-		if (!status.rc_input_blocked && sp_man.timestamp != 0) {
-#endif
 			/* handle the case where RC signal was regained */
 			if (!status.rc_signal_found_once) {
 				status.rc_signal_found_once = true;
