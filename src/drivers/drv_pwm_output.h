58e44eb1db4b4be026fa22787a7fcfafb97f5c1e
Andrew Tridgell <andrew@tridgell.net>
2016-04-15 09:26:04 +1000

    drv_pwm: added new PWM_SERVO_SET_MODE ioctl

--- src/drivers/drv_pwm_output.h
+++ src/drivers/drv_pwm_output.h
@@ -241,6 +241,23 @@ struct pwm_output_rc_config {
 /** setup OVERRIDE_IMMEDIATE behaviour on FMU fail */
 #define PWM_SERVO_SET_OVERRIDE_IMMEDIATE	_PX4_IOC(_PWM_SERVO_BASE, 30)
 
+/** set SBUS output frame rate in Hz */
+#define PWM_SERVO_SET_SBUS_RATE			_PX4_IOC(_PWM_SERVO_BASE, 31)
+
+/** set auxillary output mode. These correspond to enum Mode in px4fmu/fmu.cpp */
+#define PWM_SERVO_MODE_NONE			0
+#define PWM_SERVO_MODE_2PWM			1
+#define PWM_SERVO_MODE_2PWM2CAP			2
+#define PWM_SERVO_MODE_3PWM			3
+#define PWM_SERVO_MODE_3PWM1CAP			4
+#define PWM_SERVO_MODE_4PWM			5
+#define PWM_SERVO_MODE_6PWM			6
+#define PWM_SERVO_MODE_8PWM			7
+#define PWM_SERVO_MODE_4CAP			8
+#define PWM_SERVO_MODE_5CAP			9
+#define PWM_SERVO_MODE_6CAP		       10
+#define PWM_SERVO_SET_MODE			_PX4_IOC(_PWM_SERVO_BASE, 32)
+
 /*
  *
  *
