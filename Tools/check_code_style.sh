8a9d6d76fb9803e9ceceeec42196133596b49ac1
Daniel Agar <daniel@agar.ca>
2016-04-24 19:25:31 -0400

    check_code_style don't create tmp files

--- Tools/check_code_style.sh
+++ Tools/check_code_style.sh
@@ -6,12 +6,11 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 if [ -f "$file" ];
 then
-  ${DIR}/fix_code_style.sh --quiet < $file > $file.pretty
-  diffsize=$(diff -y --suppress-common-lines $file $file.pretty | wc -l)
-  rm -f $file.pretty
-  if [ $diffsize -ne 0 ]; then
-    echo $file 'bad formatting, please run "./Tools/fix_code_style.sh' $file'"'
-    exit 1
-  fi
+	${DIR}/fix_code_style.sh --dry-run $file | grep --quiet Formatted
+	if [[ $? -eq 0 ]]
+	then
+		echo $file 'bad formatting, please run "./Tools/fix_code_style.sh' $file'"'
+		exit 1
+	fi
 fi
 
