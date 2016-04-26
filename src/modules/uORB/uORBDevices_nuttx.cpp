ef95576af6de747f89d90fe94d6066a2ee59b592
Beat Küng <beat-kueng@gmx.net>
2016-04-26 09:55:02 +0200

    orb: move SmartLock into global space and use it also for NuttX

--- src/modules/uORB/uORBDevices_nuttx.cpp
+++ src/modules/uORB/uORBDevices_nuttx.cpp
@@ -40,6 +40,7 @@
 #include "uORBUtils.hpp"
 #include "uORBManager.hpp"
 #include "uORBCommunicator.hpp"
+#include <px4_sem.hpp>
 #include <stdlib.h>
 
 uORB::ORBMap uORB::DeviceMaster::_node_map;
@@ -579,9 +580,6 @@ uORB::DeviceMaster::ioctl(struct file *filp, int cmd, unsigned long arg)
 				return ret;
 			}
 
-			/* ensure that only one advertiser runs through this critical section */
-			lock();
-
 			ret = ERROR;
 
 			/* try for topic groups */
@@ -595,10 +593,11 @@ uORB::DeviceMaster::ioctl(struct file *filp, int cmd, unsigned long arg)
 				group_tries = *adv->instance;
 
 				if (group_tries >= max_group_tries) {
-					unlock();
 					return -ENOMEM;
 				}
 			}
+
+			SmartLock smart_lock(_lock);
 
 			do {
 				/* if path is modifyable change try index */
@@ -612,7 +611,6 @@ uORB::DeviceMaster::ioctl(struct file *filp, int cmd, unsigned long arg)
 				objname = strdup(meta->o_name);
 
 				if (objname == nullptr) {
-					unlock();
 					return -ENOMEM;
 				}
 
@@ -620,7 +618,6 @@ uORB::DeviceMaster::ioctl(struct file *filp, int cmd, unsigned long arg)
 				devpath = strdup(nodepath);
 
 				if (devpath == nullptr) {
-					unlock();
 					free((void *)objname);
 					return -ENOMEM;
 				}
@@ -630,7 +627,6 @@ uORB::DeviceMaster::ioctl(struct file *filp, int cmd, unsigned long arg)
 
 				/* if we didn't get a device, that's bad */
 				if (node == nullptr) {
-					unlock();
 					free((void *)objname);
 					free((void *)devpath);
 					return -ENOMEM;
@@ -673,9 +669,6 @@ uORB::DeviceMaster::ioctl(struct file *filp, int cmd, unsigned long arg)
 			if (ret != PX4_OK && group_tries >= max_group_tries) {
 				ret = -ENOMEM;
 			}
-
-			/* the file handle for the driver has been created, unlock */
-			unlock();
 
 			return ret;
 		}
