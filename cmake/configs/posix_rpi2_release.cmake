529460d5736287e90f3a4970d5431b672140b5c4
Kabir Mohammed <mhkabir98@gmail.com>
2016-04-29 09:39:23 +0530

    Fix RPi2 build system

--- cmake/configs/posix_rpi2_release.cmake
+++ cmake/configs/posix_rpi2_release.cmake
@@ -1,59 +1,109 @@
 include(posix/px4_impl_posix)
 
-if ("${RPI_TOOLCHAIN_DIR}" STREQUAL "")
-	set(RPI_TOOLCHAIN_DIR /opt/rpi_toolchain)
-endif()
+set(CMAKE_TOOLCHAIN_FILE ${CMAKE_SOURCE_DIR}/cmake/toolchains/Toolchain-arm-linux-gnueabihf-raspbian.cmake)
 
 set(CMAKE_PROGRAM_PATH
 	"${RPI_TOOLCHAIN_DIR}/gcc-linaro-arm-linux-gnueabihf-raspbian/bin"
 	${CMAKE_PROGRAM_PATH}
 	)
 
-set(CMAKE_TOOLCHAIN_FILE ${CMAKE_SOURCE_DIR}/cmake/toolchains/Toolchain-arm-linux-gnueabihf.cmake)
+# This definition allows to differentiate if this just the usual POSIX build
+# or if it is for the RPi.
+add_definitions(
+	-D__PX4_POSIX_RPI2
+	)
 
 set(config_module_list
+	#
+	# Board support modules
+	#
 	drivers/device
-	platforms/common
-	platforms/posix/px4_layer
-	platforms/posix/work_queue
+	modules/sensors
+	platforms/posix/drivers/df_mpu9250_wrapper
+	platforms/posix/drivers/df_bmp280_wrapper
+	platforms/posix/drivers/df_hmc5883_wrapper
+	platforms/posix/drivers/df_trone_wrapper
+	platforms/posix/drivers/df_isl29501_wrapper
+
+	#
+	# System commands
+	#
 	systemcmds/param
 	systemcmds/mixer
 	systemcmds/ver
 	systemcmds/esc_calib
-	systemcmds/reboot
 	systemcmds/topic_listener
 	systemcmds/perf
-	modules/uORB
-	modules/param
-	modules/systemlib
-	modules/systemlib/mixer
-	modules/sensors
-	modules/mavlink
+
+	#
+	# Estimation modules (EKF/ SO3 / other filters)
+	#
+	#modules/attitude_estimator_ekf
+	modules/ekf_att_pos_estimator
 	modules/attitude_estimator_q
 	modules/position_estimator_inav
-	modules/navigator
-	modules/vtol_att_control
-	modules/mc_pos_control
+	modules/local_position_estimator
+	modules/ekf2
+
+	#
+	# Vehicle Control
+	#
 	modules/mc_att_control
-	modules/land_detector
+	modules/mc_pos_control
 	modules/fw_att_control
-	modules/fw_pos_control_l1
-	modules/dataman
+	modules/fw_pos_control_l1	
+	modules/vtol_att_control
+
+	#
+	# Library modules
+	#
 	modules/sdlog2
 	modules/logger
 	modules/commander
 	modules/load_mon
+	modules/param
+	modules/systemlib
+	modules/systemlib/mixer
+	modules/uORB
+	modules/dataman
+	modules/land_detector
+	modules/navigator
+	modules/mavlink
+
+	#
+	# PX4 drivers
+	#
+
+	#
+	# Libraries
+	#
 	lib/controllib
 	lib/mathlib
 	lib/mathlib/math/filter
-	lib/conversion
-	lib/ecl
-	lib/external_lgpl
 	lib/geo
+	lib/ecl
 	lib/geo_lookup
 	lib/launchdetection
+	lib/external_lgpl
+	lib/conversion
 	lib/terrain_estimation
 	lib/runway_takeoff
 	lib/tailsitter_recovery
 	lib/DriverFramework/framework
-)
+
+	#
+	# POSIX
+	#
+	platforms/common
+	platforms/posix/px4_layer
+	platforms/posix/work_queue
+
+	)
+
+set(config_df_driver_list
+	mpu9250
+	bmp280
+	hmc5883
+	trone
+	isl29501
+	)
