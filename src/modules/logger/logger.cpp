a1b0e30b59984db55bbca4e6c5668308a05c78d9
Beat Küng <beat-kueng@gmx.net>
2016-05-02 14:52:16 +0200

    fix Logger::add_topic: 0 is a valid file descriptor

--- src/modules/logger/logger.cpp
+++ src/modules/logger/logger.cpp
@@ -333,7 +333,7 @@ int Logger::add_topic(const char *name, unsigned interval = 0)
 		}
 	}
 
-	if ((fd > 0) && (interval != 0)) {
+	if (fd >= 0 && interval != 0) {
 		orb_set_interval(fd, interval);
 	}
 
