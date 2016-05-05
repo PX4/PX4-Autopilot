805ef9fff1b2e8b3d936b2c044e9c82f41d7c2e8
Julian Oes <julian@oes.ch>
2016-05-05 09:11:59 +0200

    cmake: added some comments about eagle/excelsior

--- cmake/configs/posix_eagle_default.cmake
+++ cmake/configs/posix_eagle_default.cmake
@@ -1,7 +1,10 @@
+# The Eagle board is the first generation Snapdragon Flight board by Qualcomm.
+#
+# This cmake config builds for POSIX, so the part of the flight stack running
+# on the Linux side of the Snapdragon.
 include(configs/posix_sdflight_default)
 
-# This definition allows to differentiate if this just the usual POSIX build
-# or if it is for the Snapdragon.
+# This definition allows to differentiate the specific board.
 add_definitions(
 	-D__PX4_POSIX_EAGLE
-	)
+)
