fa762b623b098ec6c59e6e9334f0b31136e9eb08
David Sidrane <david_s5@nscdg.com>
2016-05-05 14:07:30 -1000

    Removed module to fit in flash

--- cmake/configs/nuttx_px4fmu-v2_default.cmake
+++ cmake/configs/nuttx_px4fmu-v2_default.cmake
@@ -48,8 +48,8 @@ set(config_module_list
 	drivers/pwm_input
 	drivers/camera_trigger
 	drivers/bst
-	drivers/snapdragon_rc_pwm
-	drivers/lis3mdl
+##	drivers/snapdragon_rc_pwm
+##	drivers/lis3mdl
 
 	#
 	# System commands
