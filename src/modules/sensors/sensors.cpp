ebaca071f61ea363dd91e97f7e9b4833466747ef
Lorenz Meier <lm@inf.ethz.ch>
2016-05-06 18:14:34 +0200

    Battery charge estimation: Factor in voltage drop for idle props

--- src/modules/sensors/sensors.cpp
+++ src/modules/sensors/sensors.cpp
@@ -1705,7 +1705,7 @@ Sensors::adc_poll(struct sensor_combined_s &raw)
 				actuator_controls_s ctrl;
 				orb_copy(ORB_ID(actuator_controls_0), _actuator_ctrl_0_sub, &ctrl);
 				_battery.updateBatteryStatus(t, bat_voltage_v, bat_current_a, ctrl.control[actuator_controls_s::INDEX_THROTTLE],
-							     &_battery_status);
+							     _armed, &_battery_status);
 
 				/* announce the battery status if needed, just publish else */
 				if (_battery_pub != nullptr) {
