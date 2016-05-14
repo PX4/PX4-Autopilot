1775435ee5c2af661368f70d1476f8275d188a0a
Lorenz Meier <lm@inf.ethz.ch>
2016-05-14 16:03:34 +0200

    v2 test config: Disable a couple unnecessary parts

--- cmake/configs/nuttx_px4fmu-v2_test.cmake
+++ cmake/configs/nuttx_px4fmu-v2_test.cmake
@@ -24,12 +24,12 @@ set(config_module_list
 	drivers/hmc5883
 	drivers/ms5611
 	#drivers/mb12xx
-	drivers/srf02
-	drivers/sf0x
-	drivers/ll40ls
-	drivers/trone
+	#drivers/srf02
+	#drivers/sf0x
+	#drivers/ll40ls
+	#drivers/trone
 	drivers/gps
-	drivers/pwm_out_sim
+	#drivers/pwm_out_sim
 	#drivers/hott
 	#drivers/hott/hott_telemetry
 	#drivers/hott/hott_sensors
@@ -45,8 +45,8 @@ set(config_module_list
 	drivers/gimbal
 	drivers/pwm_input
 	drivers/camera_trigger
-	drivers/bst
-	drivers/snapdragon_rc_pwm
+	#drivers/bst
+	#drivers/snapdragon_rc_pwm
 	#drivers/lis3mdl
 	#drivers/bmi160
 
