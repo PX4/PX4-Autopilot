a4e7ccab8f65135a9c7ba0f901543a7751d9fef5
bharathr <bharathr.mail@gmail.com>
2016-05-19 13:14:35 -0700

    Fixed the parsing of commander arguments for non-QURT builds
    Conflicts:
    	src/modules/commander/commander.cpp

--- src/modules/commander/commander.cpp
+++ src/modules/commander/commander.cpp
@@ -1157,11 +1157,10 @@ int commander_thread_main(int argc, char *argv[])
 #endif
 
 	if (argc > 2) {
-		if (!strcmp(argv[3],"-hil")) {
+		if (!strcmp(argv[2],"-hil")) {
 			startup_in_hil = true;
 		} else {
-			PX4_ERR("Argument %s not supported.", argv[3]);
-			PX4_ERR("COMMANDER NOT STARTED");
+			PX4_ERR("Argument %s not supported.", argv[2]);
 			thread_should_exit = true;
 		}
 	}
