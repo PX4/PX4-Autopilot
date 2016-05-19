6056d36464e43c22c664f687c29b2082efc7c807
bharathr <bharathr.mail@gmail.com>
2016-05-19 01:15:31 -0700

    Restricted the previous commit to __PX4_QURT only

--- src/modules/commander/commander.cpp
+++ src/modules/commander/commander.cpp
@@ -1159,7 +1159,9 @@ int commander_thread_main(int argc, char *argv[])
 #endif
 
 	if (argc > 2) {
+#ifdef __PX4_QURT
 		if (!strcmp(argv[2],"-hil")) {
+#endif
 			startup_in_hil = true;
 		} else {
 			PX4_ERR("Argument %s not supported.", argv[3]);
