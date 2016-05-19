dd6109671b95a197508014470e6da4b0b1b89eea
FantasyJXF <fantasyvsangel@qq.com>
2016-05-19 13:06:27 +0800

    my branch HUI

--- src/modules/attitude_estimator_q/attitude_estimator_q_params.c
+++ src/modules/attitude_estimator_q/attitude_estimator_q_params.c
@@ -88,7 +88,7 @@ PARAM_DEFINE_FLOAT(ATT_W_GYRO_BIAS, 0.1f);
  * GPS coordinates of the vehicle.
  *
  * @group Attitude Q estimator
- * @unit deg
+ * @unit degrees
  * @decimal 2
  */
 PARAM_DEFINE_FLOAT(ATT_MAG_DECL, 0.0f);
@@ -97,7 +97,8 @@ PARAM_DEFINE_FLOAT(ATT_MAG_DECL, 0.0f);
  * Enable automatic GPS based declination compensation
  *
  * @group Attitude Q estimator
- * @unit boolean
+ * @min 0
+ * @max 1
  */
 PARAM_DEFINE_INT32(ATT_MAG_DECL_A, 1);
 
@@ -107,10 +108,6 @@ PARAM_DEFINE_INT32(ATT_MAG_DECL_A, 1);
  * Set to 2 to use heading from motion capture.
  *
  * @group Attitude Q estimator
- * @unit enum
- * @value 0 none
- * @value 1 vision
- * @value 2 motion capture
  * @min 0
  * @max 2
  */
@@ -121,17 +118,18 @@ PARAM_DEFINE_INT32(ATT_EXT_HDG_M, 0);
  * velocity.
  *
  * @group Attitude Q estimator
- * @unit boolean
+ * @min 1
+ * @max 2
  */
-PARAM_DEFINE_INT32(ATT_ACC_COMP, 1);
+PARAM_DEFINE_INT32(ATT_ACC_COMP, 2);
 
 /**
  * Gyro bias limit
  *
  * @group Attitude Q estimator
- * @unit rad/s
  * @min 0
  * @max 2
+ * @unit rad/s
  * @decimal 3
  */
 PARAM_DEFINE_FLOAT(ATT_BIAS_MAX, 0.05f);
