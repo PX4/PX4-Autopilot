141b7a6a949f9f86ac58614762e453bd29d9629c
Mark Whitehorn <kd0aij@gmail.com>
2016-05-24 15:17:53 -0600

    add WIFI_TX_MODE parameter

--- src/drivers/px4fmu/fmu.cpp
+++ src/drivers/px4fmu/fmu.cpp
@@ -164,6 +164,9 @@ private:
 	bool _rc_scan_locked = false;
 	bool _report_lock = true;
 
+	int32_t _wifi_tx_mode = 1;
+	param_t _wifi_tx_param = PARAM_INVALID;
+
 	hrt_abstime _cycle_timestamp = 0;
 	hrt_abstime _last_safety_check = 0;
 
@@ -269,6 +272,7 @@ private:
 	void rc_io_invert();
 	void rc_io_invert(bool invert);
 	void safety_check_button(void);
+	void setWIFIstate(int32_t mode, bool armed);
 };
 
 const PX4FMU::GPIOConfig PX4FMU::_gpio_tab[] = {
@@ -929,6 +933,29 @@ PX4FMU::update_pwm_out_state(bool on)
 	up_pwm_servo_arm(on);
 }
 
+void PX4FMU::setWIFIstate(int32_t wifi_mode, bool armed)
+{
+	switch (wifi_mode) {
+	case 0: /* always off */
+		WIFI_TX(0);
+		break;
+
+	case 1: /* off when armed */
+		if (armed) {
+			WIFI_TX(0);
+
+		} else {
+			WIFI_TX(1);
+		}
+
+		break;
+
+	case 2: /* always on */
+		WIFI_TX(1);
+		break;
+	}
+}
+
 void
 PX4FMU::cycle()
 {
@@ -944,6 +971,15 @@ PX4FMU::cycle()
 		pwm_limit_init(&_pwm_limit);
 
 		update_pwm_rev_mask();
+
+		/* read wifi TX control parameter and init the TX enable pin */
+		_wifi_tx_param = param_find("WIFI_TX_MODE");
+		param_get(_wifi_tx_param, &_wifi_tx_mode);
+
+		if (_wifi_tx_mode != 0) {
+			/* WIFI should be on at boot time */
+			WIFI_TX(1);
+		}
 
 #ifdef RC_SERIAL_PORT
 		// dsm_init sets some file static variables and returns a file descriptor
@@ -1161,6 +1197,9 @@ PX4FMU::cycle()
 		/* update the armed status and check that we're not locked down */
 		_throttle_armed = _safety_off && _armed.armed && !_armed.lockdown;
 
+		/* update WIFI TX state */
+		setWIFIstate(_wifi_tx_mode, _armed.armed);
+
 		/* update PWM status if armed or if disarmed PWM values are set */
 		bool pwm_on = _armed.armed || _num_disarmed_set > 0;
 
@@ -1174,6 +1213,7 @@ PX4FMU::cycle()
 	orb_check(_param_sub, &updated);
 
 	if (updated) {
+		/* TODO: since pupdate is never used, these 2 lines must be present to achieve a side-effect? */
 		parameter_update_s pupdate;
 		orb_copy(ORB_ID(parameter_update), _param_sub, &pupdate);
 
@@ -1190,6 +1230,10 @@ PX4FMU::cycle()
 			dsm_bind_val = -1;
 			param_set(dsm_bind_param, &dsm_bind_val);
 		}
+
+		/* check for update to WIFI control parameter */
+		param_get(_wifi_tx_param, &_wifi_tx_mode);
+		setWIFIstate(_wifi_tx_mode, _armed.armed);
 	}
 
 	/* update ADC sampling */
@@ -1262,6 +1306,7 @@ PX4FMU::cycle()
 				if (rc_updated) {
 					// we have a new SBUS frame. Publish it.
 					_rc_in.input_source = input_rc_s::RC_INPUT_SOURCE_PX4FMU_SBUS;
+
 					fill_rc_in(raw_rc_count, raw_rc_values, _cycle_timestamp,
 						   sbus_frame_drop, sbus_failsafe, frame_drops);
 					_rc_scan_locked = true;
@@ -2444,7 +2489,7 @@ PX4FMU::peripheral_reset(int ms)
 	stm32_gpiowrite(GPIO_PERIPH_3V3_EN, 0);
 
 	bool last = stm32_gpioread(GPIO_SPEKTRUM_PWR_EN);
-	/* Keep Spektum on to discharge rail*/
+	/* Keep Spektrum on to discharge rail*/
 	stm32_gpiowrite(GPIO_SPEKTRUM_PWR_EN, 1);
 
 	/* wait for the peripheral rail to reach GND */
