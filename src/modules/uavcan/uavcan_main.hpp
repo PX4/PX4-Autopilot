8400b56269b13865b1c67f74e7f725260f3b159d
Pavel Kirienko <pavel.kirienko@gmail.com>
2016-05-26 21:00:10 +0300

    Fixed stack overflow in UAVCAN process (#4643)
    * Increased uavcan stack size; the old value of 1800 was insufficient
    
    * Removed a misleading warning message from uavcan servers initialization

--- src/modules/uavcan/uavcan_main.hpp
+++ src/modules/uavcan/uavcan_main.hpp
@@ -99,7 +99,7 @@ class UavcanNode : public device::CDev
 	 */
 
 	static constexpr unsigned RxQueueLenPerIface	= FramePerMSecond * PollTimeoutMs; // At
-	static constexpr unsigned StackSize		= 1800;
+	static constexpr unsigned StackSize		= 2400;
 
 public:
 	typedef uavcan_stm32::CanInitHelper<RxQueueLenPerIface> CanInitHelper;
