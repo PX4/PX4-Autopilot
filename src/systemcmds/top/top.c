b412980f90e984509bfa8b5dfd14dffa7899f475
Lorenz Meier <lm@inf.ethz.ch>
2016-05-28 12:10:19 +0200

    Enable top in SITL

--- src/systemcmds/top/top.c
+++ src/systemcmds/top/top.c
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2012, 2013, 2015 PX4 Development Team. All rights reserved.
+ *   Copyright (c) 2012-2016 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -96,7 +96,7 @@ top_main(int argc, char *argv[])
 				case 0x1b: // esc
 				case 'c':
 				case 'q':
-					return OK;
+					return 0;
 					/* not reached */
 				}
 			}
@@ -107,5 +107,5 @@ top_main(int argc, char *argv[])
 		curr_time = hrt_absolute_time();
 	}
 
-	return OK;
+	return 0;
 }
