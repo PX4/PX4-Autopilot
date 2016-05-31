492052f7cfeef16097c4af9a27cb8143da82fc4e
David Sidrane <david_s5@nscdg.com>
2016-05-30 15:08:36 -1000

    Track upstream change that changed CONFIG_ADC (the os driver) to CONFIG_STM32_ADC (the arch driver) to allow just the lower half driver to be included - ang broke thye PX4 Build do to an upstream error

--- src/drivers/boards/mavstation/mavstation_init.c
+++ src/drivers/boards/mavstation/mavstation_init.c
@@ -134,33 +134,33 @@ __EXPORT void stm32_boardinitialize(void)
 	stm32_configgpio(GPIO_PWM4);
 }
 
- /****************************************************************************
-  * Name: board_app_initialize
-  *
-  * Description:
-  *   Perform application specific initialization.  This function is never
-  *   called directly from application code, but only indirectly via the
-  *   (non-standard) boardctl() interface using the command BOARDIOC_INIT.
-  *
-  * Input Parameters:
-  *   arg - The boardctl() argument is passed to the board_app_initialize()
-  *         implementation without modification.  The argument has no
-  *         meaning to NuttX; the meaning of the argument is a contract
-  *         between the board-specific initalization logic and the the
-  *         matching application logic.  The value cold be such things as a
-  *         mode enumeration value, a set of DIP switch switch settings, a
-  *         pointer to configuration data read from a file or serial FLASH,
-  *         or whatever you would like to do with it.  Every implementation
-  *         should accept zero/NULL as a default configuration.
-  *
-  * Returned Value:
-  *   Zero (OK) is returned on success; a negated errno value is returned on
-  *   any failure to indicate the nature of the failure.
-  *
-  ****************************************************************************/
+/****************************************************************************
+ * Name: board_app_initialize
+ *
+ * Description:
+ *   Perform application specific initialization.  This function is never
+ *   called directly from application code, but only indirectly via the
+ *   (non-standard) boardctl() interface using the command BOARDIOC_INIT.
+ *
+ * Input Parameters:
+ *   arg - The boardctl() argument is passed to the board_app_initialize()
+ *         implementation without modification.  The argument has no
+ *         meaning to NuttX; the meaning of the argument is a contract
+ *         between the board-specific initalization logic and the the
+ *         matching application logic.  The value cold be such things as a
+ *         mode enumeration value, a set of DIP switch switch settings, a
+ *         pointer to configuration data read from a file or serial FLASH,
+ *         or whatever you would like to do with it.  Every implementation
+ *         should accept zero/NULL as a default configuration.
+ *
+ * Returned Value:
+ *   Zero (OK) is returned on success; a negated errno value is returned on
+ *   any failure to indicate the nature of the failure.
+ *
+ ****************************************************************************/
 
- __EXPORT int board_app_initialize(uintptr_t arg)
- {
+__EXPORT int board_app_initialize(uintptr_t arg)
+{
 #if defined(CONFIG_HAVE_CXX) && defined(CONFIG_HAVE_CXXINITIALIZE)
 
 	/* run C++ ctors before we go any further */
--- src/drivers/boards/px4-stm32f4discovery/px4discovery_init.c
+++ src/drivers/boards/px4-stm32f4discovery/px4discovery_init.c
@@ -120,30 +120,30 @@ stm32_boardinitialize(void)
 	board_autoled_initialize();
 }
 
- /****************************************************************************
-  * Name: board_app_initialize
-  *
-  * Description:
-  *   Perform application specific initialization.  This function is never
-  *   called directly from application code, but only indirectly via the
-  *   (non-standard) boardctl() interface using the command BOARDIOC_INIT.
-  *
-  * Input Parameters:
-  *   arg - The boardctl() argument is passed to the board_app_initialize()
-  *         implementation without modification.  The argument has no
-  *         meaning to NuttX; the meaning of the argument is a contract
-  *         between the board-specific initalization logic and the the
-  *         matching application logic.  The value cold be such things as a
-  *         mode enumeration value, a set of DIP switch switch settings, a
-  *         pointer to configuration data read from a file or serial FLASH,
-  *         or whatever you would like to do with it.  Every implementation
-  *         should accept zero/NULL as a default configuration.
-  *
-  * Returned Value:
-  *   Zero (OK) is returned on success; a negated errno value is returned on
-  *   any failure to indicate the nature of the failure.
-  *
-  ****************************************************************************/
+/****************************************************************************
+ * Name: board_app_initialize
+ *
+ * Description:
+ *   Perform application specific initialization.  This function is never
+ *   called directly from application code, but only indirectly via the
+ *   (non-standard) boardctl() interface using the command BOARDIOC_INIT.
+ *
+ * Input Parameters:
+ *   arg - The boardctl() argument is passed to the board_app_initialize()
+ *         implementation without modification.  The argument has no
+ *         meaning to NuttX; the meaning of the argument is a contract
+ *         between the board-specific initalization logic and the the
+ *         matching application logic.  The value cold be such things as a
+ *         mode enumeration value, a set of DIP switch switch settings, a
+ *         pointer to configuration data read from a file or serial FLASH,
+ *         or whatever you would like to do with it.  Every implementation
+ *         should accept zero/NULL as a default configuration.
+ *
+ * Returned Value:
+ *   Zero (OK) is returned on success; a negated errno value is returned on
+ *   any failure to indicate the nature of the failure.
+ *
+ ****************************************************************************/
 
 __EXPORT int board_app_initialize(uintptr_t arg)
 {
