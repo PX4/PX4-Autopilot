adffb859627779712b62da118f0a402e6c2f16e7
Andreas Bircher <bircher@gmx.ch>
2016-06-10 14:54:12 +0200

    adding arming check before setting PWM

--- src/drivers/camera_trigger/interfaces/src/pwm.h
+++ src/drivers/camera_trigger/interfaces/src/pwm.h
@@ -6,20 +6,23 @@
  */
 #pragma once
 
+#include <systemlib/param/param.h>
+
+#include <uORB/topics/vehicle_status.h>
 #include "camera_interface.h"
 
 // TODO(birchera): check if this is the right device and addresses
-#define PWM_DEVICE_PATH /dev/pwm_output0
-#define PWM_CAMERA_BASE		0x2a00
-#define PWM_CAMERA_SET(_pin)	_PX4_IOC(_PWM_CAMERA_BASE, 0x30 + _pin)
+#define PWM_DEVICE_PATH			"/dev/pwm_output0"
+#define PWM_CAMERA_BASE			0x2a00
+#define PWM_CAMERA_SET(_pin)	_PX4_IOC(PWM_CAMERA_BASE, 0x30 + _pin)
 // PWM levels of the interface to seagull MAP converter to
 // Multiport (http://www.seagulluav.com/manuals/Seagull_MAP2-Manual.pdf)
-#define PWM_CAMERA_DISARMED 90 // TODO(birchera): check here value
-#define PWM_CAMERA_ON 1100
-#define PWM_CAMERA_AUTOFOCUS_SHOOT 1300
-#define PWM_CAMERA_NEUTRAL 1500
-#define PWM_CAMERA_INSTANT_SHOOT 1700
-#define PWM_CAMERA_OFF 1900
+#define PWM_CAMERA_DISARMED			90 // TODO(birchera): check here value
+#define PWM_CAMERA_ON				1100
+#define PWM_CAMERA_AUTOFOCUS_SHOOT	1300
+#define PWM_CAMERA_NEUTRAL			1500
+#define PWM_CAMERA_INSTANT_SHOOT	1700
+#define PWM_CAMERA_OFF				1900
 
 
 class CameraInterfacePWM : public CameraInterface
@@ -37,6 +40,9 @@ public:
 
 	int _pins[6];
 private:
+
+	int _vehicle_status_sub;
+	struct vehicle_status_s _vehicle_status;
 
 	param_t _p_pin;
 	const char *_pwm_dev;
