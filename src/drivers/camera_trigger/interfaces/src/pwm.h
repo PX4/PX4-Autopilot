46ec1e6b95a23f2f3577ff47f96e22c5c47e013c
Andreas Bircher <bircher@gmx.ch>
2016-06-16 15:40:01 +0200

    fixing cherry-picking divergences

--- src/drivers/camera_trigger/interfaces/src/pwm.h
+++ src/drivers/camera_trigger/interfaces/src/pwm.h
@@ -11,20 +11,6 @@
 #include <uORB/topics/vehicle_status.h>
 #include "camera_interface.h"
 
-// TODO(birchera): check if this is the right device and addresses
-#define PWM_DEVICE_PATH			"/dev/pwm_output0"
-#define PWM_CAMERA_BASE			0x2a00
-#define PWM_CAMERA_SET(_pin)	_PX4_IOC(PWM_CAMERA_BASE, 0x30 + _pin)
-// PWM levels of the interface to seagull MAP converter to
-// Multiport (http://www.seagulluav.com/manuals/Seagull_MAP2-Manual.pdf)
-#define PWM_CAMERA_DISARMED			90 // TODO(birchera): check here value
-#define PWM_CAMERA_ON				1100
-#define PWM_CAMERA_AUTOFOCUS_SHOOT	1300
-#define PWM_CAMERA_NEUTRAL			1500
-#define PWM_CAMERA_INSTANT_SHOOT	1700
-#define PWM_CAMERA_OFF				1900
-
-
 class CameraInterfacePWM : public CameraInterface
 {
 public:
