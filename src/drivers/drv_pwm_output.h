forscience1337kaju/PX4Firmware

Add ioctl() to allow select channels to ignore the state of the safety switch

src/drivers/drv_pwm_output.h
@@ -244,6 +244,10 @@ struct pwm_output_rc_config {
 /** set SBUS output frame rate in Hz */
 #define PWM_SERVO_SET_SBUS_RATE			_PX4_IOC(_PWM_SERVO_BASE, 31)
 
+/** surfaces to move regardless of the arm state */
+#define PWM_SERVO_IGNORE_ARMING			_PX4_IOC(_PWM_SERVO_BASE, 32)
+
+/** set auxillary output mode. These correspond to enum Mode in px4fmu/fmu.cpp */
 /** set auxillary output mode. These correspond to enum Mode in px4fmu/fmu.cpp */
 #define PWM_SERVO_MODE_NONE			0
 #define PWM_SERVO_MODE_2PWM			1

src/modules/px4iofirmware/protocol.h
@@ -241,6 +241,7 @@ enum {							/* DSM bind states */
 #define PX4IO_P_SETUP_TRIM_YAW			18	/**< Yaw trim, in actuator units */
 
 #define PX4IO_P_SETUP_SBUS_RATE			19	/* frame rate of SBUS1 output in Hz */
+#define PX4IO_P_SETUP_IGNORE_ARMING		20	/* bitmask of surfaces to ignore the arm status */
 
 /* autopilot control values, -10000..10000 */
 #define PX4IO_PAGE_CONTROLS			51	/**< actuator control groups, one after the other, 8 wide */

PX4/PX4-Autopilot

src/drivers/drv_pwm_output.h
@@ -244,6 +244,10 @@ struct pwm_output_rc_config {
 /** set SBUS output frame rate in Hz */
 #define PWM_SERVO_SET_SBUS_RATE			_PX4_IOC(_PWM_SERVO_BASE, 31)
 
+/** surfaces to move regardless of the safety state */
+#define PWM_SERVO_IGNORE_SAFETY			_PX4_IOC(_PWM_SERVO_BASE, 32)
+
+/** set auxillary output mode. These correspond to enum Mode in px4fmu/fmu.cpp */
 /** set auxillary output mode. These correspond to enum Mode in px4fmu/fmu.cpp */
 #define PWM_SERVO_MODE_NONE			0
 #define PWM_SERVO_MODE_2PWM			1

src/modules/px4iofirmware/protocol.h
@@ -241,6 +241,7 @@ enum {							/* DSM bind states */
 #define PX4IO_P_SETUP_TRIM_YAW			18	/**< Yaw trim, in actuator units */
 
 #define PX4IO_P_SETUP_SBUS_RATE			19	/* frame rate of SBUS1 output in Hz */
+#define PX4IO_P_SETUP_IGNORE_SAFETY		20	/* bitmask of surfaces to ignore the safety status */
 
 /* autopilot control values, -10000..10000 */
 #define PX4IO_PAGE_CONTROLS			51	/**< actuator control groups, one after the other, 8 wide */