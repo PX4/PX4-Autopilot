08f708808d194d6fec96af12db45568e78bd7b6d
flatlevel <geoff@skyworksas.com>
2016-06-22 17:35:09 -0700

    Change board i2c address for RGBLED

--- src/drivers/boards/luci-v1/board_config.h
+++ src/drivers/boards/luci-v1/board_config.h
@@ -146,7 +146,7 @@ __BEGIN_DECLS
  *
  * Note that these are unshifted addresses.
  */
-#define PX4_I2C_OBDEV_LED	0x55
+#define PX4_I2C_OBDEV_LED	0x62
 #define PX4_I2C_OBDEV_HMC5883	0x1e
 #define PX4_I2C_OBDEV_LIS3MDL	0x1e
 
