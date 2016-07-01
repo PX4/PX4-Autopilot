e43625cfc1e941c64b377b769c1e9aa6064e6197
Lorenz Meier <lm@inf.ethz.ch>
2016-07-01 18:08:32 +0200

    ROMFS: Clear out SITL mixers from deployed image, delete unused mixers, move test mixers to test config

--- unittests/mixer_test.cpp
+++ unittests/mixer_test.cpp
@@ -7,6 +7,6 @@
 
 TEST(MixerTest, Mixer)
 {
-	const char *args[] = {"empty", "../ROMFS/px4fmu_common/mixers/IO_pass.mix", "../ROMFS/px4fmu_common/mixers/quad_test.mix"};
+	const char *args[] = {"empty", "../ROMFS/px4fmu_test/mixers/IO_pass.mix", "../ROMFS/px4fmu_test/mixers/quad_test.mix"};
 	ASSERT_EQ(test_mixer(3, (char **)args), 0) << "IO_pass.mix failed";
 }
