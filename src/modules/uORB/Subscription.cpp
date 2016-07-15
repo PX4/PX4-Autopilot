ee58f0d11dd96624a2c3d4ffedda9eb1a1754a71
Beat Küng <beat-kueng@gmx.net>
2016-07-15 23:59:52 +0200

    encoders.msg: remove this topic, it's never published (#5074)

--- src/modules/uORB/Subscription.cpp
+++ src/modules/uORB/Subscription.cpp
@@ -45,7 +45,6 @@
 #include "topics/hil_sensor.h"
 #include "topics/vehicle_attitude.h"
 #include "topics/vehicle_global_position.h"
-#include "topics/encoders.h"
 #include "topics/position_setpoint_triplet.h"
 #include "topics/vehicle_status.h"
 #include "topics/manual_control_setpoint.h"
@@ -162,7 +161,6 @@ template class __EXPORT Subscription<sensor_combined_s>;
 template class __EXPORT Subscription<hil_sensor_s>;
 template class __EXPORT Subscription<vehicle_attitude_s>;
 template class __EXPORT Subscription<vehicle_global_position_s>;
-template class __EXPORT Subscription<encoders_s>;
 template class __EXPORT Subscription<position_setpoint_triplet_s>;
 template class __EXPORT Subscription<vehicle_status_s>;
 template class __EXPORT Subscription<manual_control_setpoint_s>;
