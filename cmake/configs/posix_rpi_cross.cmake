a8d3b15d73f28adac122acc6b92d5201012b9adc
Hidenori <hidenori.kobayashi@gmail.com>
2016-07-19 17:12:09 -0400

    RPi: add clang & clang-check support

--- cmake/configs/posix_rpi_cross.cmake
+++ cmake/configs/posix_rpi_cross.cmake
@@ -1,6 +1,11 @@
 include(configs/posix_rpi_common)
 
-set(CMAKE_TOOLCHAIN_FILE ${CMAKE_SOURCE_DIR}/cmake/toolchains/Toolchain-arm-linux-gnueabihf-raspbian.cmake)
+if("$ENV{RPI_USE_CLANG}" STREQUAL "1")
+	set(CMAKE_TOOLCHAIN_FILE ${CMAKE_SOURCE_DIR}/cmake/toolchains/Toolchain-arm-linux-clang-gnueabihf-raspbian.cmake)
+else()
+	set(CMAKE_TOOLCHAIN_FILE ${CMAKE_SOURCE_DIR}/cmake/toolchains/Toolchain-arm-linux-gnueabihf-raspbian.cmake)
+endif()
+
 
 set(CMAKE_PROGRAM_PATH
 	"${RPI_TOOLCHAIN_DIR}/gcc-linaro-arm-linux-gnueabihf-raspbian/bin"
