437221bec2d93791f2fff82d5dc57250fe892ba4
James Goppert <jgoppert@users.noreply.github.com>
2016-08-02 05:02:01 -0400

    Make LPE default estimator. (#4483)

--- src/modules/systemlib/system_params.c
+++ src/modules/systemlib/system_params.c
@@ -104,7 +104,7 @@ PARAM_DEFINE_INT32(SYS_RESTART_TYPE, 2);
  * @reboot_required true
  * @group System
  */
-PARAM_DEFINE_INT32(SYS_MC_EST_GROUP, 0);
+PARAM_DEFINE_INT32(SYS_MC_EST_GROUP, 1);
 
 /**
  * TELEM2 as companion computer link
