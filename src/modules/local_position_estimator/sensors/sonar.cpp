6ac79aa55f677553ba368e05502bec6533c7818d
James Goppert <james.goppert@gmail.com>
2016-08-05 06:52:35 -0400

    Formatting.

--- src/modules/local_position_estimator/sensors/sonar.cpp
+++ src/modules/local_position_estimator/sensors/sonar.cpp
@@ -35,9 +35,9 @@ void BlockLocalPositionEstimator::sonarInit()
 
 		} else {
 			PX4_INFO("[lpe] sonar init "
-						     "mean %d cm std %d cm",
-						     int(100 * _sonarStats.getMean()(0)),
-						     int(100 * _sonarStats.getStdDev()(0)));
+				 "mean %d cm std %d cm",
+				 int(100 * _sonarStats.getMean()(0)),
+				 int(100 * _sonarStats.getStdDev()(0)));
 			_sonarInitialized = true;
 			_sonarFault = FAULT_NONE;
 		}
