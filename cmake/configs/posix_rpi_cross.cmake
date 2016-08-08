211c2b9ca6e6dcb2bc4357099f992989c27327ba
Hidenori <hidenori.kobayashi@gmail.com>
2016-08-08 09:21:31 -0400

    Rename clang toolchain file

--- cmake/configs/posix_rpi_cross.cmake
+++ cmake/configs/posix_rpi_cross.cmake
@@ -1,7 +1,7 @@
 include(configs/posix_rpi_common)
 
 if("$ENV{RPI_USE_CLANG}" STREQUAL "1")
-	set(CMAKE_TOOLCHAIN_FILE ${CMAKE_SOURCE_DIR}/cmake/toolchains/Toolchain-arm-linux-clang-gnueabihf-raspbian.cmake)
+	set(CMAKE_TOOLCHAIN_FILE ${CMAKE_SOURCE_DIR}/cmake/toolchains/Toolchain-arm-linux-gnueabihf-raspbian-clang.cmake)
 else()
 	set(CMAKE_TOOLCHAIN_FILE ${CMAKE_SOURCE_DIR}/cmake/toolchains/Toolchain-arm-linux-gnueabihf-raspbian.cmake)
 endif()
