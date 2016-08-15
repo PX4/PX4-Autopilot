259293bd8b0557b47aacbb3ddef6c956ad5ba8be
Lorenz Meier <lorenz@px4.io>
2016-08-15 13:07:45 +0200

    Use less memory for ESC driver

--- src/drivers/tap_esc/tap_esc.cpp
+++ src/drivers/tap_esc/tap_esc.cpp
@@ -1082,7 +1082,7 @@ void start()
 	_task_handle = px4_task_spawn_cmd("tap_esc_main",
 					  SCHED_DEFAULT,
 					  SCHED_PRIORITY_MAX,
-					  1200,
+					  1000,
 					  (px4_main_t)&task_main_trampoline,
 					  nullptr);
 
