39ce001c41249f5b8725686fff707df22e75f0ce
Lorenz Meier <lorenz@px4.io>
2016-08-15 13:08:14 +0200

    MAVLink: use only the memory it needs

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -2325,7 +2325,7 @@ Mavlink::start(int argc, char *argv[])
 	px4_task_spawn_cmd(buf,
 			   SCHED_DEFAULT,
 			   SCHED_PRIORITY_DEFAULT,
-			   2800,
+			   2400,
 			   (px4_main_t)&Mavlink::start_helper,
 			   (char *const *)argv);
 
