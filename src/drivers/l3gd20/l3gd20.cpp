cf25bf1c08519867dff36256cc0f5a4955567e60
Henry Zhang <zhanghui629@gmail.com>
2016-08-17 15:37:19 +0800

    MindPX: Remove hardcode for sensors rotation.

--- src/drivers/l3gd20/l3gd20.cpp
+++ src/drivers/l3gd20/l3gd20.cpp
@@ -1045,18 +1045,6 @@ L3GD20::measure()
 
 	report.z_raw = raw_report.z;
 
-#if defined(CONFIG_ARCH_BOARD_MINDPX_V2)
-	int16_t tx = -report.y_raw;
-	int16_t ty = -report.x_raw;
-	int16_t tz = -report.z_raw;
-	report.x_raw = tx;
-	report.y_raw = ty;
-	report.z_raw = tz;
-#endif
-
-
-
-
 	report.temperature_raw = raw_report.temp;
 
 	float xraw_f = report.x_raw;
