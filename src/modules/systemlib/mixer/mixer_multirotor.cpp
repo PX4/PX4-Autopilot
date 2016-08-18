ae00abe468b2bd8e4de7e68f4a8f324f943c9283
Lorenz Meier <lm@inf.ethz.ch>
2016-08-18 15:44:51 +0200

    Fix typo

--- src/modules/systemlib/mixer/mixer_multirotor.cpp
+++ src/modules/systemlib/mixer/mixer_multirotor.cpp
@@ -172,7 +172,7 @@ MultirotorMixer::from_text(Mixer::ControlCallback control_cb, uintptr_t cb_handl
 		geometry = MultirotorGeometry::HEX_COX;
 
 	} else if (!strcmp(geomname, "6t")) {
-		geometry = MultirotorGeometry::HEX_t;
+		geometry = MultirotorGeometry::HEX_T;
 
 	} else if (!strcmp(geomname, "8+")) {
 		geometry = MultirotorGeometry::OCTA_PLUS;
