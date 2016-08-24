14ed65f0e41ee497199c104a98036d4642605368
Julian Oes <julian@oes.ch>
2016-08-24 17:07:14 +0200

    sitl_run.sh: use ps instead of jps (#5376)
    The tool jps does not seem to be available on macos Java.

--- Tools/sitl_run.sh
+++ Tools/sitl_run.sh
@@ -44,7 +44,7 @@ fi
 # be running from last time
 pkill gazebo
 pkill px4
-jmavsim_pid=`jps | grep Simulator | cut -d" " -f1`
+jmavsim_pid=`ps aux | grep java | grep Simulator | cut -d" " -f1`
 if [ -n "$jmavsim_pid" ]
 then
 	kill $jmavsim_pid
