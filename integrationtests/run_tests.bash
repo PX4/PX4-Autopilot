64426a4f9cd1bd2860212e52dfcb08312af0fa0a
Lorenz Meier <lm@inf.ethz.ch>
2016-08-28 01:25:27 +0200

    Exit early on test failures

--- integrationtests/run_tests.bash
+++ integrationtests/run_tests.bash
@@ -83,8 +83,8 @@ echo -e "ROS_LOG_DIR\t\t: $ROS_LOG_DIR"
 echo -e "PX4_LOG_DIR\t\t: $PX4_LOG_DIR"
 echo -e "TEST_RESULT_TARGET_DIR\t: $TEST_RESULT_TARGET_DIR"
 
-# don't exit on error anymore from here on (because single tests or exports might fail)
-set +e
+# Exit on first error as we cannot afford to wait forever
+set -e
 echo "=====> run tests"
 rostest px4 mavros_posix_tests_iris.launch
 rostest px4 mavros_posix_tests_standard_vtol.launch
