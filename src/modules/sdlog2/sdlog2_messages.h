1c20cebd367b38e59daf1043e720223a2c008e49
Deafro <dlear002@odu.edu>
2016-09-16 16:32:31 -0400

    Update sdlog2_messages.h

--- src/modules/sdlog2/sdlog2_messages.h
+++ src/modules/sdlog2/sdlog2_messages.h
@@ -649,7 +649,7 @@ struct log_PARM_s {
 
 /* --- AOA,SS - ANGLE OF ATTACK AND SIDESLIP --- */ // ADDED BY DONALD LEAR
 #define LOG_AOASS_MSG 120
-struct log_AOAS_s {
+struct log_AOASS_s {
 	float aoa_v;     //# Angle-of-Attack voltage in volts.  Range 0-3.3 Volts
 	float ss_v;      //# Sideslip voltage.  Range 0 - 3.3 Volts
 }
@@ -726,7 +726,7 @@ static const struct log_format_s log_formats[] = {
 	LOG_FORMAT(TIME, "Q", "StartTime"),
 	LOG_FORMAT(VER, "NZ", "Arch,FwGit"),
 	LOG_FORMAT(PARM, "Nf", "Name,Value")
-	LOG_FORMAT(AOAS, "ff", "AOA,SS") // ADDED BY DONALD LEAR
+	LOG_FORMAT(AOASS, "ff", "AOA,SS") // ADDED BY DONALD LEAR
 };
 
 static const unsigned log_formats_num = sizeof(log_formats) / sizeof(log_formats[0]);
