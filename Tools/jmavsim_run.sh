0ee4ac3a361fbbf5288d122aeb5379a57e3208b0
Beat Küng <beat-kueng@gmx.net>
2016-09-19 20:09:12 +0200

    jmavsim_run.sh: add optional -p <port> argument

--- Tools/jmavsim_run.sh
+++ Tools/jmavsim_run.sh
@@ -3,6 +3,19 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPT_DIR/jMAVSim"
 
+udp_port=14560
+while getopts ":p:" opt; do
+	case $opt in
+		p)
+			udp_port=$OPTARG
+			;;
+		\?)
+			echo "Invalid option: -$OPTARG" >&2
+			exit 1
+			;;
+	esac
+done
+
 ant create_run_jar copy_res
 cd out/production
-java -Djava.ext.dirs= -jar jmavsim_run.jar -udp 127.0.0.1:14560
+java -Djava.ext.dirs= -jar jmavsim_run.jar -udp 127.0.0.1:$udp_port
