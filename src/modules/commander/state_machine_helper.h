50b8ed0a89d2a6022cc518df65fa6bc57375f393
Beat Küng <beat-kueng@gmx.net>
2016-09-20 18:47:33 +0200

    commander: initialize gps & baro as failure state
    This avoids error messages on startup.

--- src/modules/commander/state_machine_helper.h
+++ src/modules/commander/state_machine_helper.h
@@ -92,6 +92,7 @@ struct status_flags_s {
 	bool gps_failure;				// Set to true if a gps failure is detected
 	bool gps_failure_cmd;				// Set to true if a gps failure mode is commanded
 	bool barometer_failure;				// Set to true if a barometer failure is detected
+	bool ever_had_barometer_data;			// Set to true if ever had valid barometer data before
 };
 
 bool is_safe(const struct vehicle_status_s *current_state, const struct safety_s *safety, const struct actuator_armed_s *armed);
