979381fcfae726f9643f220ffd947ae0642f58c0
David Sidrane <david_s5@nscdg.com>
2016-09-22 11:06:05 -1000

    Support passing defines from top level config makefile

--- cmake/common/px4_base.cmake
+++ cmake/common/px4_base.cmake
@@ -809,9 +809,14 @@ function(px4_add_common_flags)
 
 	string(TOUPPER ${BOARD} board_upper)
 	string(REPLACE "-" "_" board_config ${board_upper})
+	set (added_target_definitions)
+	if (NOT ${target_definitions})
+	    px4_prepend_string(OUT added_target_definitions STR "-D" LIST ${target_definitions})
+	endif()
 	set(added_definitions
 		-DCONFIG_ARCH_BOARD_${board_config}
 		-D__STDC_FORMAT_MACROS
+		${added_target_definitions}
 		)
 
 	if (NOT (APPLE AND (${CMAKE_C_COMPILER_ID} MATCHES ".*Clang.*")))
