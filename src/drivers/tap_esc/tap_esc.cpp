a227d53e007e26c358ef9c175a5a6109827198ab
Lucas De Marchi <lucas.demarchi@intel.com>
2016-09-27 16:36:59 -0300

    tap_esc: fix output order for quadrotor

--- src/drivers/tap_esc/tap_esc.cpp
+++ src/drivers/tap_esc/tap_esc.cpp
@@ -711,6 +711,13 @@ TAP_ESC::cycle()
 			motor_out[6] = RPMSTOPPED;
 			motor_out[7] = RPMSTOPPED;
 
+		} else if (num_outputs == 4) {
+
+			motor_out[0] = _outputs.output[2];
+			motor_out[2] = _outputs.output[0];
+			motor_out[1] = _outputs.output[1];
+			motor_out[3] = _outputs.output[3];
+
 		} else {
 
 			// Use the system defaults
