714e783446f6a3a8e291b87ce3b0f92c975c63f8
Andrew Tridgell <andrew@tridgell.net>
2016-10-14 17:55:08 +1100

    px4io: added SRXL support

--- src/drivers/px4io/px4io.cpp
+++ src/drivers/px4io/px4io.cpp
@@ -1854,6 +1854,9 @@ PX4IO::io_publish_raw_rc()
 	} else if (_status & PX4IO_P_STATUS_FLAGS_RC_ST24) {
 		rc_val.input_source = input_rc_s::RC_INPUT_SOURCE_PX4IO_ST24;
 
+	} else if (_status & PX4IO_P_STATUS_FLAGS_RC_SRXL) {
+		rc_val.input_source = input_rc_s::RC_INPUT_SOURCE_PX4IO_SRXL;
+
 	} else {
 		rc_val.input_source = input_rc_s::RC_INPUT_SOURCE_UNKNOWN;
 
@@ -2196,6 +2199,7 @@ PX4IO::print_status(bool extended_status)
 	       ((flags & PX4IO_P_STATUS_FLAGS_RC_DSM)   ? " DSM" : ""),
 	       ((flags & PX4IO_P_STATUS_FLAGS_RC_ST24)   ? " ST24" : ""),
 	       ((flags & PX4IO_P_STATUS_FLAGS_RC_SBUS)  ? " SBUS" : ""),
+	       ((flags & PX4IO_P_STATUS_FLAGS_RC_SRXL)  ? " SRXL" : ""),
 	       ((flags & PX4IO_P_STATUS_FLAGS_FMU_OK)   ? " FMU_OK" : " FMU_FAIL"),
 	       ((flags & PX4IO_P_STATUS_FLAGS_RAW_PWM)  ? " RAW_PWM_PASSTHROUGH" : ""),
 	       ((flags & PX4IO_P_STATUS_FLAGS_MIXER_OK) ? " MIXER_OK" : " MIXER_FAIL"),
@@ -2830,6 +2834,9 @@ PX4IO::ioctl(file *filep, int cmd, unsigned long arg)
 
 			} else if (status & PX4IO_P_STATUS_FLAGS_RC_ST24) {
 				rc_val->input_source = input_rc_s::RC_INPUT_SOURCE_PX4IO_ST24;
+
+			} else if (status & PX4IO_P_STATUS_FLAGS_RC_SRXL) {
+				rc_val->input_source = input_rc_s::RC_INPUT_SOURCE_PX4IO_SRXL;
 
 			} else {
 				rc_val->input_source = input_rc_s::RC_INPUT_SOURCE_UNKNOWN;
