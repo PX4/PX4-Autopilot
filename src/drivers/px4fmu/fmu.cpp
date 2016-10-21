PX4/PX4-Autopilot

commit 78b0d1a01f2f3ebfd33d005d9da5908c72e4843b

tap: add landing gear capability
This configures the PWM output for the landing gear.

src/drivers/px4fmu/fmu.cpp
@@ -116,6 +116,7 @@ class PX4FMU : public device::CDev
 public:
 	enum Mode {
 		MODE_NONE,
+		MODE_1PWM,
 		MODE_2PWM,
 		MODE_2PWM2CAP,
 		MODE_3PWM,
@@ -529,6 +530,15 @@ PX4FMU::set_mode(Mode mode)
 	 * are presented on the output pins.
 	 */
 	switch (mode) {
+	case MODE_1PWM:
+		/* default output rates */
+		_pwm_default_rate = 50;
+		_pwm_alt_rate = 50;
+		_pwm_alt_rate_channels = 0;
+		_pwm_mask = 0x1;
+		_pwm_initialized = false;
+		break;
+
 	case MODE_2PWM2CAP:	// v1 multi-port with flow control lines as PWM
 		up_input_capture_set(2, Rising, 0, NULL, NULL);
 		up_input_capture_set(3, Rising, 0, NULL, NULL);
@@ -1041,6 +1051,10 @@ PX4FMU::cycle()
 			size_t num_outputs;
 
 			switch (_mode) {
+			case MODE_1PWM:
+				num_outputs = 1;
+				break;
+
 			case MODE_2PWM:
 			case MODE_2PWM2CAP:
 				num_outputs = 2;
@@ -1588,6 +1602,7 @@ PX4FMU::ioctl(file *filp, int cmd, unsigned long arg)
 
 	/* if we are in valid PWM mode, try it as a PWM ioctl as well */
 	switch (_mode) {
+	case MODE_1PWM:
 	case MODE_2PWM:
 	case MODE_3PWM:
 	case MODE_4PWM:
@@ -2002,6 +2017,10 @@ PX4FMU::pwm_ioctl(file *filp, int cmd, unsigned long arg)
 			*(unsigned *)arg = 2;
 			break;
 
+		case MODE_1PWM:
+			*(unsigned *)arg = 1;
+			break;
+
 		default:
 			ret = -EINVAL;
 			break;
@@ -2020,6 +2039,10 @@ PX4FMU::pwm_ioctl(file *filp, int cmd, unsigned long arg)
 			switch (arg) {
 			case 0:
 				set_mode(MODE_NONE);
+				break;
+
+			case 1:
+				set_mode(MODE_1PWM);
 				break;
 
 			case 2:
@@ -2060,6 +2083,10 @@ PX4FMU::pwm_ioctl(file *filp, int cmd, unsigned long arg)
 			switch (arg) {
 			case PWM_SERVO_MODE_NONE:
 				ret = set_mode(MODE_NONE);
+				break;
+
+			case PWM_SERVO_MODE_1PWM:
+				ret = set_mode(MODE_1PWM);
 				break;
 
 			case PWM_SERVO_MODE_2PWM:
@@ -2616,6 +2643,7 @@ enum PortMode {
 	PORT_PWM4,
 	PORT_PWM3,
 	PORT_PWM2,
+	PORT_PWM1,
 	PORT_PWM3CAP1,
 	PORT_PWM2CAP2,
 	PORT_CAPTURE,
@@ -2639,6 +2667,12 @@ fmu_new_mode(PortMode new_mode)
 		break;
 
 	case PORT_FULL_PWM:
+
+	case PORT_PWM1:
+		/* select 2-pin PWM mode */
+		servo_mode = PX4FMU::MODE_1PWM;
+		break;
+
 #if defined(BOARD_HAS_PWM) && BOARD_HAS_PWM == 4
 		/* select 4-pin PWM mode */
 		servo_mode = PX4FMU::MODE_4PWM;
@@ -3092,7 +3126,14 @@ fmu_main(int argc, char *argv[])
 	} else if (!strcmp(verb, "mode_pwm")) {
 		new_mode = PORT_FULL_PWM;
 
-#if defined(BOARD_HAS_PWM) && BOARD_HAS_PWM >= 6
+#if defined(BOARD_HAS_PWM)
+
+	} else if (!strcmp(verb, "mode_pwm1")) {
+		new_mode = PORT_PWM1;
+
+
+#elif defined(BOARD_HAS_PWM) && BOARD_HAS_PWM >= 6
+
 
 	} else if (!strcmp(verb, "mode_pwm4")) {
 		new_mode = PORT_PWM4;

ROMFS/px4fmu_common/mixers/mount_legs.aux.mix
@@ -23,5 +23,5 @@ S: 0 7      0  20000 -10000 -10000  10000
 # mixer for right leg
 M: 1
 O:      10000  10000      0 -10000  10000
-S: 0 8      0  20000 -10000 -10000  10000
+S: 0 7      0  20000 -10000 -10000  10000
 

ROMFS/tap_common/init.d/rcS
@@ -331,9 +331,19 @@ unset OUTPUT_MODE
 #
 # Start the RC input driver
 #
-if fmu mode_rcin
+if fmu mode_pwm1
 then
 fi
+
+#
+# Use 400 Hz PWM output for landing gear (frequency also affects RGB LED)
+#
+pwm rate -c 1 -r 400
+
+#
+# Load the gear mixer onto fmu
+#
+mixer load /dev/px4fmu /etc/mixers/gear.mix
 
 #
 # Start the navigator

ROMFS/tap_common/mixers/gear.mix
@@ -1,0 +1,7 @@
+#
+# This maps actuator_controls_0[7] to the first and only PWM output
+#
+
+M: 1
+O:      10000  10000      0 -10000  10000
+S: 0 7  10000  10000      0 -10000  10000

src/drivers/boards/tap-v1/board_config.h
@@ -195,10 +195,18 @@ __BEGIN_DECLS
 #define GPIO_TIM3_CH4OUT	GPIO_TIM3_CH4OUT_1
 #define DIRECT_PWM_OUTPUT_CHANNELS	4
 
+#define GPIO_TIM3_CH1IN		GPIO_TIM3_CH1IN_1
+#define GPIO_TIM3_CH2IN		GPIO_TIM3_CH2IN_1
+#define GPIO_TIM3_CH3IN		GPIO_TIM3_CH3IN_1
+#define GPIO_TIM3_CH4IN		GPIO_TIM3_CH4IN_1
+#define DIRECT_INPUT_TIMER_CHANNELS  4
+
 #define BOARD_HAS_LED_PWM
 #define LED_TIM3_CH1OUT  GPIO_TIM3_CH1OUT
 #define LED_TIM3_CH2OUT  GPIO_TIM3_CH2OUT
 #define LED_TIM3_CH3OUT  GPIO_TIM3_CH3OUT
+
+#define BOARD_PWM_DRIVE_ACTIVE_LOW 1
 
 
 /* USB OTG FS

src/drivers/boards/tap-v1/tap_timer_config.c
@@ -58,7 +58,7 @@ __EXPORT const io_timers_t io_timers[MAX_IO_TIMERS] = {
 		.clock_bit = RCC_APB1ENR_TIM3EN,
 		.clock_freq = STM32_APB1_TIM3_CLKIN,
 		.first_channel_index = 0,
-		.last_channel_index = 3,
+		.last_channel_index = 0,
 		.handler = io_timer_handler0,
 		.vectorno =  STM32_IRQ_TIM3,
 	}
@@ -66,32 +66,8 @@ __EXPORT const io_timers_t io_timers[MAX_IO_TIMERS] = {
 
 __EXPORT const timer_io_channels_t timer_io_channels[MAX_TIMER_IO_CHANNELS] = {
 	{
-		.gpio_out = GPIO_TIM3_CH1OUT,
-		.gpio_in = 0,
-		.timer_index = 0,
-		.timer_channel = 1,
-		.ccr_offset = STM32_GTIM_CCR1_OFFSET,
-		.masks  = GTIM_SR_CC1IF | GTIM_SR_CC1OF
-	},
-	{
-		.gpio_out = GPIO_TIM3_CH2OUT,
-		.gpio_in = 0,
-		.timer_index = 0,
-		.timer_channel = 2,
-		.ccr_offset = STM32_GTIM_CCR2_OFFSET,
-		.masks  = GTIM_SR_CC2IF | GTIM_SR_CC2OF
-	},
-	{
-		.gpio_out = GPIO_TIM3_CH3OUT,
-		.gpio_in = 0,
-		.timer_index = 0,
-		.timer_channel = 3,
-		.ccr_offset = STM32_GTIM_CCR3_OFFSET,
-		.masks  = GTIM_SR_CC3IF | GTIM_SR_CC3OF
-	},
-	{
 		.gpio_out = GPIO_TIM3_CH4OUT,
-		.gpio_in = 0,
+		.gpio_in = GPIO_TIM3_CH4IN,
 		.timer_index = 0,
 		.timer_channel = 4,
 		.ccr_offset = STM32_GTIM_CCR4_OFFSET,

src/drivers/drv_pwm_output.h
@@ -261,16 +261,17 @@ struct pwm_output_rc_config {
 
 /** set auxillary output mode. These correspond to enum Mode in px4fmu/fmu.cpp */
 #define PWM_SERVO_MODE_NONE			0
-#define PWM_SERVO_MODE_2PWM			1
-#define PWM_SERVO_MODE_2PWM2CAP			2
-#define PWM_SERVO_MODE_3PWM			3
-#define PWM_SERVO_MODE_3PWM1CAP			4
-#define PWM_SERVO_MODE_4PWM			5
-#define PWM_SERVO_MODE_6PWM			6
-#define PWM_SERVO_MODE_8PWM			7
-#define PWM_SERVO_MODE_4CAP			8
-#define PWM_SERVO_MODE_5CAP			9
-#define PWM_SERVO_MODE_6CAP		       10
+#define PWM_SERVO_MODE_1PWM			1
+#define PWM_SERVO_MODE_2PWM			2
+#define PWM_SERVO_MODE_2PWM2CAP			3
+#define PWM_SERVO_MODE_3PWM			4
+#define PWM_SERVO_MODE_3PWM1CAP			5
+#define PWM_SERVO_MODE_4PWM			6
+#define PWM_SERVO_MODE_6PWM			7
+#define PWM_SERVO_MODE_8PWM			8
+#define PWM_SERVO_MODE_4CAP			9
+#define PWM_SERVO_MODE_5CAP		       10
+#define PWM_SERVO_MODE_6CAP		       11
 #define PWM_SERVO_SET_MODE			_PX4_IOC(_PWM_SERVO_BASE, 32)
 
 /*

src/drivers/stm32/drv_io_timer.c
@@ -394,7 +394,7 @@ static int timer_set_rate(unsigned timer, unsigned rate)
 }
 
 
-static int io_timer_init_timer(unsigned timer)
+int io_timer_init_timer(unsigned timer)
 {
 	/* Do this only once per timer */
 
@@ -708,6 +708,10 @@ int io_timer_set_ccr(unsigned channel, uint16_t value)
 		} else {
 
 			/* configure the channel */
+#ifdef BOARD_PWM_DRIVE_ACTIVE_LOW
+			unsigned period = rARR(channels_timer(channel));
+			value = period - value;
+#endif
 
 			if (value > 0) {
 				value--;
@@ -727,6 +731,12 @@ uint16_t io_channel_get_ccr(unsigned channel)
 	if (io_timer_validate_channel_index(channel) == 0 &&
 	    io_timer_get_channel_mode(channel) == IOTimerChanMode_PWMOut) {
 		value = REG(channels_timer(channel), timer_io_channels[channel].ccr_offset) + 1;
+
+#ifdef BOARD_PWM_DRIVE_ACTIVE_LOW
+		unsigned period = rARR(channels_timer(channel));
+		value = period - value;
+#endif
+
 	}
 
 	return value;

commit 578f787a98172da7896047ee8da59ab5fdb86dfe

fmu: fix typo introduced in 78b0d1a which adds PWM1 mode

src/drivers/px4fmu/fmu.cpp
@@ -2668,11 +2668,6 @@ fmu_new_mode(PortMode new_mode)
 
 	case PORT_FULL_PWM:
 
-	case PORT_PWM1:
-		/* select 2-pin PWM mode */
-		servo_mode = PX4FMU::MODE_1PWM;
-		break;
-
 #if defined(BOARD_HAS_PWM) && BOARD_HAS_PWM == 4
 		/* select 4-pin PWM mode */
 		servo_mode = PX4FMU::MODE_4PWM;
@@ -2683,6 +2678,11 @@ fmu_new_mode(PortMode new_mode)
 #if defined(BOARD_HAS_PWM) && BOARD_HAS_PWM == 8
 		servo_mode = PX4FMU::MODE_8PWM;
 #endif
+		break;
+
+	case PORT_PWM1:
+		/* select 2-pin PWM mode */
+		servo_mode = PX4FMU::MODE_1PWM;
 		break;
 
 #if defined(BOARD_HAS_PWM) && BOARD_HAS_PWM >= 6