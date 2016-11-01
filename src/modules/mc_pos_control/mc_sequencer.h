3255bcd415de318acdca9984149aba81ec31d1f8
Mark Whitehorn <kd0aij@gmail.com>
2016-11-01 09:23:46 -0600

    switch to quaternion rotation chaining
    and cleanup/eliminate dead and commented code

--- src/modules/mc_pos_control/mc_sequencer.h
+++ src/modules/mc_pos_control/mc_sequencer.h
@@ -11,31 +11,6 @@
 #include <mathlib/mathlib.h>
 #include <platforms/px4_defines.h>
 
-#undef JUST_FLIPS
-//#define JUST_FLIPS
-
-#ifdef JUST_FLIPS
-/*
- * Perform a 360 degree roll or pitch flip starting and ending at current attitude
- *
- * Inputs:
- * ctrl_state: for current attitude quaternion
- * manual: sequence trigger switch
- *
- * Outputs:
- * att_sp: thrust and rotation angle setpoints
- * R_sp: attitude setpoint
- * rollRate, pitchRate, yawRate: rotation rate commands
- */
-void flip_sequence(
-	struct control_state_s &ctrl_state,
-	struct vehicle_attitude_setpoint_s &att_sp,
-	struct manual_control_setpoint_s &manual,
-	matrix::Dcmf &R_sp,
-	float &rollRate, float &pitchRate, float &yawRate);
-
-#else
-
 /*
  * Perform a programmed sequence with specified target attitudes
  *
@@ -53,7 +28,7 @@ void prog_sequence(
 	struct control_state_s &ctrl_state,
 	struct vehicle_attitude_setpoint_s &att_sp,
 	struct manual_control_setpoint_s &manual,
-	matrix::Dcmf &R_sp,
+	matrix::Quatf &R_sp,
 	float &rollRate, float &pitchRate, float &yawRate);
 
 enum Seq_state {
@@ -78,7 +53,5 @@ struct sequence {
 	const int N_entries;
 	const struct seq_entry_s *entries;
 };
-
-#endif
 
 #endif
