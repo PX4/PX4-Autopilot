39cc75b4a0c45bc251a4a46ec7766296649d8856
Sander Smeets <sander@droneslab.com>
2016-11-04 00:59:49 +0100

    Rebase of vtol_land_weathervane patch

--- src/modules/navigator/mission_block.cpp
+++ src/modules/navigator/mission_block.cpp
@@ -488,7 +488,8 @@ MissionBlock::mission_item_to_position_setpoint(const struct mission_item_s *ite
 				_navigator->get_loiter_radius();
 	sp->loiter_direction = (item->loiter_radius > 0) ? 1 : -1;
 	sp->acceptance_radius = item->acceptance_radius;
-	sp->disable_mc_yaw_control = false;
+	sp->disable_mc_yaw_control = item->disable_mc_yaw;
+
 	sp->cruising_speed = _navigator->get_cruising_speed();
 	sp->cruising_throttle = _navigator->get_cruising_throttle();
 
