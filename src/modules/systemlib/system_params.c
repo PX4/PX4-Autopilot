85cf68e74160fd86f588629c0cbeb9c6aaaf2506
Beat Küng <beat-kueng@gmx.net>
2016-11-07 15:10:03 +0100

    startup scripts: remove INAV, start LPE if INAV selected

--- src/modules/systemlib/system_params.c
+++ src/modules/systemlib/system_params.c
@@ -95,11 +95,10 @@ PARAM_DEFINE_INT32(SYS_RESTART_TYPE, 2);
  *
  * Set the group of estimators used for multicopters and vtols
  *
- * @value 0 position_estimator_inav, attitude_estimator_q
  * @value 1 local_position_estimator, attitude_estimator_q
  * @value 2 ekf2
  *
- * @min 0
+ * @min 1
  * @max 2
  * @reboot_required true
  * @group System
