f772fc2d021245b9ae274c3205e9d502d5cf1ec0
Andreas Antener <antener_a@gmx.ch>
2016-11-14 00:15:20 +0100

    Airspeed: preflight check for bad offset, fixed calls to preflight checks (vtol & airspeed)

--- src/modules/commander/PreflightCheck.h
+++ src/modules/commander/PreflightCheck.h
@@ -39,6 +39,8 @@
  * @author Johan Jansen <jnsn.johan@gmail.com>
  */
 
+#include <drivers/drv_hrt.h>
+
 #pragma once
 
 namespace Commander
@@ -67,7 +69,8 @@ namespace Commander
 *   true if the GNSS receiver should be checked
 **/
 bool preflightCheck(orb_advert_t *mavlink_log_pub, bool checkMag, bool checkAcc,
-    bool checkGyro, bool checkBaro, bool checkAirspeed, bool checkRC, bool checkGNSS, bool checkDynamic, bool isVTOL, bool reportFailures = false);
+    bool checkGyro, bool checkBaro, bool checkAirspeed, bool checkRC, bool checkGNSS,
+    bool checkDynamic, bool isVTOL, bool reportFailures, bool prearm, hrt_abstime time_since_boot);
 
 const unsigned max_mandatory_gyro_count = 1;
 const unsigned max_optional_gyro_count = 3;
