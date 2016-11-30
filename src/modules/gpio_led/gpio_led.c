780a83d7101ad2432b1a0a5ed9fee1e6f9fdf0d9
David Sidrane <david_s5@nscdg.com>
2016-11-29 16:36:51 -1000

    System Changes for px4fmuv4pro

--- src/modules/gpio_led/gpio_led.c
+++ src/modules/gpio_led/gpio_led.c
@@ -93,10 +93,11 @@ int gpio_led_main(int argc, char *argv[])
 		    );
 #endif
 
-#if defined(CONFIG_ARCH_BOARD_AUAV_X21)  || \
-	defined(CONFIG_ARCH_BOARD_PX4FMU_V2) || \
-	defined(CONFIG_ARCH_BOARD_PX4FMU_V4) || \
-	defined(CONFIG_ARCH_BOARD_MINDPX_V2) || \
+#if defined(CONFIG_ARCH_BOARD_AUAV_X21)          || \
+	defined(CONFIG_ARCH_BOARD_PX4FMU_V2)         || \
+	defined(CONFIG_ARCH_BOARD_PX4FMU_V4)         || \
+	defined(CONFIG_ARCH_BOARD_PX4FMU_V4PRO)      || \
+	defined(CONFIG_ARCH_BOARD_MINDPX_V2)         || \
 	defined(CONFIG_ARCH_BOARD_PX4NUCLEOF767ZI_V1)
 		errx(1, "usage: gpio_led {start|stop} [-p <n>]\n"
 		     "\t-p <n>\tUse specified AUX OUT pin number (default: 1)"
@@ -119,10 +120,12 @@ int gpio_led_main(int argc, char *argv[])
 #ifdef CONFIG_ARCH_BOARD_PX4FMU_V1
 			char *pin_name = "PX4FMU GPIO_EXT1";
 #endif
-#if defined(CONFIG_ARCH_BOARD_AUAV_X21)  || \
-	defined(CONFIG_ARCH_BOARD_PX4FMU_V2) || \
-	defined(CONFIG_ARCH_BOARD_PX4FMU_V4) || \
-	defined(CONFIG_ARCH_BOARD_MINDPX_V2) || \
+#if defined(CONFIG_ARCH_BOARD_AUAV_X21)          || \
+	defined(CONFIG_ARCH_BOARD_PX4FMU_V2)         || \
+	defined(CONFIG_ARCH_BOARD_PX4FMU_V4)         || \
+	defined(CONFIG_ARCH_BOARD_MINDPX_V2)         || \
+	defined(CONFIG_ARCH_BOARD_PX4FMU_V4PRO)      || \
+	defined(CONFIG_ARCH_BOARD_MINDPX_V2)         || \
 	defined(CONFIG_ARCH_BOARD_PX4NUCLEOF767ZI_V1)
 			char pin_name[] = "AUX OUT 1";
 #endif
@@ -166,10 +169,13 @@ int gpio_led_main(int argc, char *argv[])
 					}
 
 #endif
-#if defined(CONFIG_ARCH_BOARD_AUAV_X21)  || \
-	defined(CONFIG_ARCH_BOARD_PX4FMU_V2) || \
-	defined(CONFIG_ARCH_BOARD_PX4FMU_V4) || \
-	defined(CONFIG_ARCH_BOARD_MINDPX_V2) || \
+
+#if defined(CONFIG_ARCH_BOARD_AUAV_X21)          || \
+	defined(CONFIG_ARCH_BOARD_PX4FMU_V2)         || \
+	defined(CONFIG_ARCH_BOARD_PX4FMU_V4)         || \
+	defined(CONFIG_ARCH_BOARD_MINDPX_V2)         || \
+	defined(CONFIG_ARCH_BOARD_PX4FMU_V4PRO)      || \
+	defined(CONFIG_ARCH_BOARD_MINDPX_V2)         || \
 	defined(CONFIG_ARCH_BOARD_PX4NUCLEOF767ZI_V1)
 					unsigned int n = strtoul(argv[3], NULL, 10);
 
