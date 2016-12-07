5ffb147d7efda9815a9e0fa876daada1153951f6
David Sidrane <david_s5@nscdg.com>
2016-12-07 10:07:43 -1000

    mindpx-v2:Removed stm32.h arch/board/board.h and cruft

--- src/drivers/boards/mindpx-v2/board_config.h
+++ src/drivers/boards/mindpx-v2/board_config.h
@@ -47,9 +47,6 @@
 #include <nuttx/compiler.h>
 #include <stdint.h>
 
-#include <stm32.h>
-#include <arch/board/board.h>
-
 /****************************************************************************************************
  * Definitions
  ****************************************************************************************************/
@@ -357,24 +354,6 @@ extern void stm32_usbinitialize(void);
 
 #define board_peripheral_reset(ms)
 
-/****************************************************************************
- * Name: nsh_archinitialize
- *
- * Description:
- *   Perform architecture specific initialization for NSH.
- *
- *   CONFIG_NSH_ARCHINIT=y :
- *     Called from the NSH library
- *
- *   CONFIG_BOARD_INITIALIZE=y, CONFIG_NSH_LIBRARY=y, &&
- *   CONFIG_NSH_ARCHINIT=n :
- *     Called from board_initialize().
- *
- ****************************************************************************/
-
-#ifdef CONFIG_NSH_LIBRARY
-int nsh_archinitialize(void);
-#endif
 
 #include "../common/board_common.h"
 
