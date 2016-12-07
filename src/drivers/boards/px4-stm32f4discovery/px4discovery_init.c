56cc8d4a6f5891c3925865682cf46e920644ae7e
David Sidrane <david_s5@nscdg.com>
2016-12-07 10:08:18 -1000

    px4-stm32f4discovery:Removed stm32.h arch/board/board.h and cruft

--- src/drivers/boards/px4-stm32f4discovery/px4discovery_init.c
+++ src/drivers/boards/px4-stm32f4discovery/px4discovery_init.c
@@ -35,10 +35,10 @@
  * @file px4discovery_init.c
  *
  * PX4-stm32f4discovery specific early startup code.  This file implements the
- * nsh_archinitialize() function that is called early by nsh during startup.
+ * board_app_initialize() function that is called early by nsh during startup.
  *
  * Code here is run before the rcS script is invoked; it should start required
- * subsystems and perform board-specific initialisation.
+ * subsystems and perform board-specific initialization.
  */
 
 /****************************************************************************
