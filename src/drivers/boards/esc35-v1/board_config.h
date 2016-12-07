7fa1c42bda21dbef95979231073b55ffacd886c2
David Sidrane <david_s5@nscdg.com>
2016-12-07 10:06:45 -1000

    esc35-v1:Removed stm32.h arch/board/board.h and cruft

--- src/drivers/boards/esc35-v1/board_config.h
+++ src/drivers/boards/esc35-v1/board_config.h
@@ -47,12 +47,6 @@
 #include <nuttx/compiler.h>
 #include <stdint.h>
 
-__BEGIN_DECLS
-
-/* these headers are not C++ safe */
-#include <stm32.h>
-#include <arch/board/board.h>
-
 /************************************************************************************
  * Definitions
  ************************************************************************************/
@@ -142,6 +136,8 @@ __BEGIN_DECLS
 
 #define BOARD_NAME "ESC35_V1"
 
+__BEGIN_DECLS
+
 /************************************************************************************
  * Public Types
  ************************************************************************************/
@@ -193,24 +189,6 @@ void stm32_usbinitialize(void);
 
 void stm32_usb_set_pwr_callback(xcpt_t pwr_changed_handler);
 
-/************************************************************************************
- * Name: nsh_archinitialize
- *
- * Description:
- *   Perform architecture specific initialization for NSH.
- *
- *   CONFIG_NSH_ARCHINIT=y :
- *     Called from the NSH library
- *
- *   CONFIG_BOARD_INITIALIZE=y, CONFIG_NSH_LIBRARY=y, &&
- *   CONFIG_NSH_ARCHINIT=n :
- *     Called from board_initialize().
- *
- ************************************************************************************/
-
-#ifdef CONFIG_NSH_LIBRARY
-int nsh_archinitialize(void);
-#endif
 
 /****************************************************************************
  * Name: stm32_led_initialize
