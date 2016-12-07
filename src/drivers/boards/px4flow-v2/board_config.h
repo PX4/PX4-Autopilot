1eb0938c7d2bc3e129bf5ee850070e2860d08a21
David Sidrane <david_s5@nscdg.com>
2016-12-07 10:11:03 -1000

    px4flow-v2:Removed stm32.h arch/board/board.h and cruft

--- src/drivers/boards/px4flow-v2/board_config.h
+++ src/drivers/boards/px4flow-v2/board_config.h
@@ -47,12 +47,6 @@
 #include <nuttx/compiler.h>
 #include <stdint.h>
 
-__BEGIN_DECLS
-
-/* these headers are not C++ safe */
-#include <stm32.h>
-#include <arch/board/board.h>
-
 /************************************************************************************
  * Definitions
  ************************************************************************************/
@@ -246,6 +240,8 @@ __BEGIN_DECLS
 
 #define BOARD_NAME "PX4FLOW_V2"
 
+__BEGIN_DECLS
+
 /************************************************************************************
  * Public Types
  ************************************************************************************/
@@ -296,25 +292,6 @@ void stm32_usbinitialize(void);
  ************************************************************************************/
 
 void stm32_usb_set_pwr_callback(xcpt_t pwr_changed_handler);
-
-/************************************************************************************
- * Name: nsh_archinitialize
- *
- * Description:
- *   Perform architecture specific initialization for NSH.
- *
- *   CONFIG_NSH_ARCHINIT=y :
- *     Called from the NSH library
- *
- *   CONFIG_BOARD_INITIALIZE=y, CONFIG_NSH_LIBRARY=y, &&
- *   CONFIG_NSH_ARCHINIT=n :
- *     Called from board_initialize().
- *
- ************************************************************************************/
-
-#ifdef CONFIG_NSH_LIBRARY
-int nsh_archinitialize(void);
-#endif
 
 /****************************************************************************
  * Name: stm32_led_initialize
--- src/drivers/boards/px4flow-v2/px4flow_init.c
+++ src/drivers/boards/px4flow-v2/px4flow_init.c
@@ -35,7 +35,7 @@
  * @file px4flow_init.c
  *
  * PX4FLOW-specific early startup code.  This file implements the
- * nsh_archinitialize() function that is called early by nsh during startup.
+ * board_app_initialize() function that is called early by nsh during startup.
  *
  * Code here is run before the rcS script is invoked; it should start required
  * subsystems and perform board-specific initialization.
@@ -149,11 +149,7 @@ __EXPORT void board_initialize(void)
  ****************************************************************************/
 
 
-#ifdef CONFIG_NSH_LIBRARY
-__EXPORT int nsh_archinitialize(void)
-#else
 __EXPORT int board_app_initialize(uintptr_t arg)
-#endif
 {
 	int result = OK;
 
