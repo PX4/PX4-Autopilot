382b3b9baca221b149398703e999d2276a43e0c7
David Sidrane <david_s5@nscdg.com>
2016-12-08 15:44:08 -1000

    Disable LPE to fit px4fmu-v2_test

--- cmake/configs/nuttx_px4fmu-v2_test.cmake
+++ cmake/configs/nuttx_px4fmu-v2_test.cmake
@@ -102,7 +102,7 @@ set(config_module_list
 	#
 	modules/attitude_estimator_q
 	#modules/position_estimator_inav
-	modules/local_position_estimator
+##TO FIT modules/local_position_estimator
 	modules/ekf2
 
 	#
