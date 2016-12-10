aa694307f827801655030e8c69242f294b3f0a6f
Andrew Tridgell <andrew@tridgell.net>
2016-12-10 19:16:19 +1100

    added FMUv3 board file for make build

---
+++ makefiles/nuttx/board_px4fmu-v3.mk
@@ -0,0 +1,11 @@
+#
+# Board-specific definitions for the PX4FMUv2
+#
+
+#
+# Configure the toolchain
+#
+CONFIG_ARCH			 = CORTEXM4F
+CONFIG_BOARD			 = PX4FMU_V3
+
+include $(PX4_MK_DIR)/toolchain_gnu-arm-eabi.mk
