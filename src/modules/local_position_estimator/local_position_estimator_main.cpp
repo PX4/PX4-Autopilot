df613361b93f9e17465383fde3b0aba4df47cf0d
Lorenz Meier <lm@inf.ethz.ch>
2016-12-17 12:39:30 +0100

    LPE: Increase stack to allow enough safe margin

--- src/modules/local_position_estimator/local_position_estimator_main.cpp
+++ src/modules/local_position_estimator/local_position_estimator_main.cpp
@@ -113,7 +113,7 @@ int local_position_estimator_main(int argc, char *argv[])
 		deamon_task = px4_task_spawn_cmd("lp_estimator",
 						 SCHED_DEFAULT,
 						 SCHED_PRIORITY_MAX - 5,
-						 13000,
+						 13500,
 						 local_position_estimator_thread_main,
 						 (argv && argc > 2) ? (char *const *) &argv[2] : (char *const *) NULL);
 		return 0;
