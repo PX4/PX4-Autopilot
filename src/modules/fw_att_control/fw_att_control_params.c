7fd5aae8347d019657c6bf4d057e7d119a31ecd7
Sander Smeets <sander@droneslab.com>
2016-12-20 01:35:45 +0100

    Apply battery voltage throttle scaling to FW (ported from #5778)

--- src/modules/fw_att_control/fw_att_control_params.c
+++ src/modules/fw_att_control/fw_att_control_params.c
@@ -561,3 +561,17 @@ PARAM_DEFINE_FLOAT(FW_MAN_P_SC, 1.0f);
  * @group FW Attitude Control
  */
 PARAM_DEFINE_FLOAT(FW_MAN_Y_SC, 1.0f);
+
+/**
+ * Whether to scale throttle by battery power level
+ *
+ * This compensates for voltage drop of the battery over time by attempting to
+ * normalize performance across the operating range of the battery. The fixed wing
+ * should constantly behave as if it was fully charged with reduced max thrust
+ * at lower battery percentages. i.e. if cruise speed is at 0.5 throttle at 100% battery,
+ * it will still be 0.5 at 60% battery.
+ *
+ * @boolean
+ * @group FW Attitude Control
+ */
+PARAM_DEFINE_INT32(FW_BAT_SCALE_EN, 0);
