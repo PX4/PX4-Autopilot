64449883928182ca5abb516b91cf9818bd74bb18
Lorenz Meier <lorenz@px4.io>
2016-12-25 18:28:28 +0100

    MAVLink app: Send messages on average at a more correct rate and send on first call
    This patch fixes two issues:
      * It sends the message on the first call, making sure that the first update gets sent out.
      * It improves the rate scheduling. In an experiment with 0.5, 50 and 250 Hz all rates were correct within 0.3% of the intended rate.

--- src/modules/mavlink/mavlink_main.h
+++ src/modules/mavlink/mavlink_main.h
@@ -428,8 +428,10 @@ public:
 
 	void			set_logging_enabled(bool logging) { _logging_enabled = logging; }
 
-	int			get_data_rate() { return _datarate; }
+	int				get_data_rate() { return _datarate; }
 	void			set_data_rate(int rate) { if (rate > 0) { _datarate = rate; } }
+
+	uint64_t		get_main_loop_delay() { return _main_loop_delay; }
 
 	/** get the Mavlink shell. Create a new one if there isn't one. It is *always* created via MavlinkReceiver thread.
 	 *  Returns nullptr if shell cannot be created */
--- src/modules/mavlink/mavlink_stream.h
+++ src/modules/mavlink/mavlink_stream.h
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2014 PX4 Development Team. All rights reserved.
+ *   Copyright (c) 2014-2016 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -98,7 +98,7 @@ public:
 
 protected:
 	Mavlink     *_mavlink;
-	unsigned int _interval;
+	unsigned int _interval;		//<<< if set to zero = unlimited rate
 
 #ifndef __PX4_QURT
 	virtual void send(const hrt_abstime t) = 0;
