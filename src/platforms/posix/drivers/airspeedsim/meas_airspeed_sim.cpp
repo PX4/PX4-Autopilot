58e5a738afe3b7a3ea9b87ffe70b8a110b775273
Lorenz Meier <lorenz@px4.io>
2017-01-03 09:28:35 +0100

    Airspeed sim: Fix reset function

--- src/platforms/posix/drivers/airspeedsim/meas_airspeed_sim.cpp
+++ src/platforms/posix/drivers/airspeedsim/meas_airspeed_sim.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2013, 2014 PX4 Development Team. All rights reserved.
+ *   Copyright (c) 2013, 2014, 2017 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -33,7 +33,7 @@
 
 /**
  * @file meas_airspeed_sim.cpp
- * @author Lorenz Meier
+ * @author Lorenz Meier <lorenz@px4.io>
  * @author Sarthak Kaingade
  * @author Simon Wilks
  * @author Thomas Gubler
@@ -473,13 +473,17 @@ reset()
 
 	if (ioctl(fd, SENSORIOCRESET, 0) < 0) {
 		PX4_ERR("driver reset failed");
+		close(fd);
 		return 1;
 	}
 
 	if (ioctl(fd, SENSORIOCSPOLLRATE, SENSOR_POLLRATE_DEFAULT) < 0) {
 		PX4_ERR("driver poll restart failed");
+		close(fd);
 		return 1;
 	}
+
+	close(fd);
 
 	return 0;
 }
