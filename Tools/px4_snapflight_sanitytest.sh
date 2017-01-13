fdca163e669d805996b4624e99146f0be2c31d2b
bharathr <bharathr.mail@gmail.com>
2017-01-13 12:58:31 -0800

    Fixed the snapdragon flight sanity test script (default and legacy driver modes)

--- Tools/px4_snapflight_sanitytest.sh
+++ Tools/px4_snapflight_sanitytest.sh
@@ -143,16 +143,16 @@ installpx4() {
    # Copy binaries to the target
    if [ $mode == 0 ]; then
       # copy default binaries
-      adb push $workspace/build_qurt_eagle_legacy_driver_default/src/firmware/qurt/libpx4.so /usr/share/data/adsp
-      adb push $workspace/build_qurt_eagle_legacy_driver_default/src/firmware/qurt/libpx4muorb_skel.so /usr/share/data/adsp
-      adb push $workspace/build_posix_eagle_legacy_driver_default/src/firmware/posix/px4 /home/linaro
+      adb push $workspace/build_qurt_eagle_default/src/firmware/qurt/libpx4.so /usr/share/data/adsp
+      adb push $workspace/build_qurt_eagle_default/src/firmware/qurt/libpx4muorb_skel.so /usr/share/data/adsp
+      adb push $workspace/build_posix_eagle_default/src/firmware/posix/px4 /home/linaro
       adb push $workspace/posix-configs/eagle/flight/px4.config /usr/share/data/adsp
       adb push $workspace/posix-configs/eagle/flight/mainapp.config /home/linaro
    else
       # copy legacy binaries
-      adb push $workspace/build_qurt_eagle_default/src/firmware/qurt/libpx4.so /usr/share/data/adsp
-      adb push $workspace/build_qurt_eagle_default/src/firmware/qurt/libpx4muorb_skel.so /usr/share/data/adsp
-      adb push $workspace/build_posix_eagle_legacy_driver_default/src/firmware/posix/px4 /home/linaro   
+      adb push $workspace/build_qurt_eagle_legacy/src/firmware/qurt/libpx4.so /usr/share/data/adsp
+      adb push $workspace/build_qurt_eagle_legacy/src/firmware/qurt/libpx4muorb_skel.so /usr/share/data/adsp
+      adb push $workspace/build_posix_eagle_legacy/src/firmware/posix/px4 /home/linaro
       adb push $workspace/posix-configs/eagle/200qx/px4.config /usr/share/data/adsp
       adb push $workspace/posix-configs/eagle/200qx/mainapp.config /home/linaro
    fi
