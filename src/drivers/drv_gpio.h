bbbf8a9138c46c140ae9163fb23d87dd285feacc
David Sidrane <david_s5@nscdg.com>
2017-01-18 13:55:08 -1000

    crazyflie removed unused GPIO defines

--- src/drivers/drv_gpio.h
+++ src/drivers/drv_gpio.h
@@ -72,19 +72,6 @@
 # define PX4FMU_DEVICE_PATH	"/dev/px4fmu"
 #endif
 
-#ifdef CONFIG_ARCH_BOARD_CRAZYFLIE
-
-# define GPIO_SERVO_1			(1<<0)		/**< servo 1 output */
-# define GPIO_SERVO_2			(1<<1)		/**< servo 2 output */
-# define GPIO_SERVO_3			(1<<2)		/**< servo 3 output */
-# define GPIO_SERVO_4			(1<<3)		/**< servo 4 output */
-
-/**
- * Device paths for things that support the GPIO ioctl protocol.
- */
-# define PX4FMU_DEVICE_PATH	"/dev/px4fmu"
-
-#endif
 
 #ifdef CONFIG_ARCH_BOARD_PX4IO_V1
 /* no GPIO driver on the PX4IOv1 board */
@@ -109,7 +96,6 @@
 
 #if	!defined(BOARD_HAS_FMU_GPIO)                     && \
 	!defined(CONFIG_ARCH_BOARD_AEROFC_V1)            && \
-	!defined(CONFIG_ARCH_BOARD_CRAZYFLIE)            && \
 	!defined(CONFIG_ARCH_BOARD_PX4_STM32F4DISCOVERY) && \
 	!defined(CONFIG_ARCH_BOARD_PX4IO_V1)             && \
 	!defined(CONFIG_ARCH_BOARD_PX4IO_V2)             && \
