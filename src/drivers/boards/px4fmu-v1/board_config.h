90515196cd4fca98e6b9716747b3cc7de496ec3a
David Sidrane <david_s5@nscdg.com>
2017-01-18 12:30:26 -1000

    PX4FMUV1: moved GPIO defines to px4fmu-v1/board_config.h

--- src/drivers/boards/px4fmu-v1/board_config.h
+++ src/drivers/boards/px4fmu-v1/board_config.h
@@ -212,6 +212,22 @@
 		{GPIO_GPIO6_INPUT, GPIO_GPIO6_OUTPUT, GPIO_CAN2_TX_2}, \
 		{GPIO_GPIO7_INPUT, GPIO_GPIO7_OUTPUT, GPIO_CAN2_RX_2}, }
 
+/*
+ * GPIO numbers.
+ *
+ * For shared pins, alternate function 1 selects the non-GPIO mode
+ * (USART2, CAN2, etc.)
+ */
+#define GPIO_EXT_1     (1<<0)  /**< high-power GPIO 1 */
+#define GPIO_EXT_2     (1<<1)  /**< high-power GPIO 1 */
+#define GPIO_MULTI_1   (1<<2)  /**< USART2 CTS */
+#define GPIO_MULTI_2   (1<<3)  /**< USART2 RTS */
+#define GPIO_MULTI_3   (1<<4)  /**< USART2 TX */
+#define GPIO_MULTI_4   (1<<5)  /**< USART2 RX */
+#define GPIO_CAN_TX    (1<<6)  /**< CAN2 TX */
+#define GPIO_CAN_RX    (1<<7)  /**< CAN2 RX */
+
+
 /* BOARD_HAS_MULTI_PURPOSE_GPIO defined because the board
  * has alternate uses for GPIO as noted in that the third
  * column above has entries.
