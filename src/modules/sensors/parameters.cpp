ec56be18a477220aa079a494d8956d6ebd6ef212
David Sidrane <david_s5@nscdg.com>
2017-01-19 12:05:02 -1000

    FMUV2 Moved magic numbers for battery V div and A per V to board_config.h

--- src/modules/sensors/parameters.cpp
+++ src/modules/sensors/parameters.cpp
@@ -419,7 +419,7 @@ int update_parameters(const ParameterHandles &parameter_handles, Parameters &par
 
 		parameters.battery_v_div = BOARD_BATTERY1_V_DIV;
 
-#if defined (CONFIG_ARCH_BOARD_PX4FMU_V2) || defined ( CONFIG_ARCH_BOARD_MINDPX_V2 )
+#if defined ( CONFIG_ARCH_BOARD_MINDPX_V2 )
 		parameters.battery_v_div = 10.177939394f;
 #elif defined (CONFIG_ARCH_BOARD_AEROCORE)
 		parameters.battery_v_div = 7.8196363636f;
@@ -438,7 +438,7 @@ int update_parameters(const ParameterHandles &parameter_handles, Parameters &par
 
 		parameters.battery_a_per_v = BOARD_BATTERY1_A_PER_V;
 
-#if defined (CONFIG_ARCH_BOARD_PX4FMU_V2) || defined (CONFIG_ARCH_BOARD_MINDPX_V2) || defined (CONFIG_ARCH_BOARD_AEROCORE) || defined (CONFIG_ARCH_BOARD_PX4FMU_V1)
+#if defined (CONFIG_ARCH_BOARD_MINDPX_V2) || defined (CONFIG_ARCH_BOARD_AEROCORE) || defined (CONFIG_ARCH_BOARD_PX4FMU_V1)
 		/* current scaling for 3DR power brick */
 		parameters.battery_a_per_v = 15.391030303f;
 #endif
