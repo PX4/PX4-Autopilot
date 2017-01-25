93f2cf077fa199da2b00d3655e75b865b339fc72
David Sidrane <david_s5@nscdg.com>
2017-01-24 15:47:06 -1000

    posix derrived targes use BOARD_OVERRIDE_UUID

--- src/platforms/posix/include/system_config.h
+++ src/platforms/posix/include/system_config.h
@@ -9,7 +9,18 @@
 
 #define PX4_I2C_OBDEV_LED	0x55
 
-#define STM32_SYSMEM_UID "SIMULATIONID"
+#define BOARD_OVERRIDE_UUID "SIMULATIONID"
+#define SIM_FORMATED_UUID "000000010000000200000003"
+#define PX4_CPU_UUID_BYTE_LENGTH 12
+#define PX4_CPU_UUID_WORD32_LENGTH 3
+#define PX4_CPU_UUID_WORD32_LEGACY_FORMAT_SIZE  (PX4_CPU_UUID_WORD32_LENGTH-1+(2*PX4_CPU_UUID_BYTE_LENGTH))
+
+typedef unsigned char raw_uuid_byte_t[PX4_CPU_UUID_BYTE_LENGTH];
+typedef unsigned int raw_uuid_uint32_t[PX4_CPU_UUID_WORD32_LENGTH];
+
+#define board_get_uuid_raw32(id, null) do {id[0]=0;id[1]=1;id[2]=2;} while(0)
+#define board_get_uuid_formated32(format_buffer, size, format, seperator, optional_reorder) do { strcpy(format_buffer, SIM_FORMATED_UUID); } while(0)
+
 
 #define CONFIG_NFILE_STREAMS 1
 #define CONFIG_SCHED_WORKQUEUE 1
