380819dfc5164627b9b7149497e0aa9496ae7083
Daniel Agar <daniel@agar.ca>
2017-01-28 16:24:15 -0500

    astyle src/modules/uavcannode

--- src/modules/uavcannode/uavcannode_main.cpp
+++ src/modules/uavcannode/uavcannode_main.cpp
@@ -103,7 +103,7 @@ boot_app_shared_section app_descriptor_t AppDescriptor = {
 	.vcs_commit = 0,
 	.major_version = APP_VERSION_MAJOR,
 	.minor_version = APP_VERSION_MINOR,
-	.reserved = {0xff , 0xff , 0xff , 0xff , 0xff , 0xff }
+	.reserved = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff }
 };
 
 /*
@@ -288,7 +288,7 @@ void UavcanNode::cb_beginfirmware_update(const uavcan::ReceivedDataStructure<Uav
 			shared.bus_speed = active_bitrate;
 			shared.node_id = _node.getNodeID().get();
 			bootloader_app_shared_write(&shared, App);
-			rgb_led(255, 128 , 0 , 5);
+			rgb_led(255, 128, 0, 5);
 			_reset_timer.setCallback(cb_reboot);
 			_reset_timer.startOneShotWithDelay(uavcan::MonotonicDuration::fromMSec(1000));
 			rsp.error = rsp.ERROR_OK;
