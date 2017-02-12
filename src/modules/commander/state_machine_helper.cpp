68c45a853b53799bb62cc71e71e2f2248809ddbd
Daniel Agar <daniel@agar.ca>
2017-02-12 12:20:21 -0500

    commander tests allow FW ACRO

--- src/modules/commander/state_machine_helper.cpp
+++ src/modules/commander/state_machine_helper.cpp
@@ -376,9 +376,6 @@ main_state_transition(struct vehicle_status_s *status, main_state_t new_main_sta
 	switch (new_main_state) {
 	case commander_state_s::MAIN_STATE_MANUAL:
 	case commander_state_s::MAIN_STATE_STAB:
-		ret = TRANSITION_CHANGED;
-		break;
-
 	case commander_state_s::MAIN_STATE_ACRO:
 		ret = TRANSITION_CHANGED;
 		break;
