1a81b64d490129d0980f26bb4f922687d86d7ead
Beat Küng <beat-kueng@gmx.net>
2017-02-17 14:20:00 +0100

    VEHICLE_CMD_PREFLIGHT_CALIBRATION: update temperature_calibration param definition

--- src/modules/commander/commander.cpp
+++ src/modules/commander/commander.cpp
@@ -4037,7 +4037,9 @@ void *commander_low_prio_loop(void *arg)
 						answer_command(cmd, vehicle_command_s::VEHICLE_CMD_RESULT_ACCEPTED, command_ack_pub, command_ack);
 						calib_ret = do_gyro_calibration(&mavlink_log_pub);
 
-					} else if ((int)(cmd.param1) == 3 || (int)(cmd.param5) == 3 || (int)(cmd.param7) == 3) {
+					} else if ((int)(cmd.param1) == vehicle_command_s::PREFLIGHT_CALIBRATION_TEMPERATURE_CALIBRATION ||
+							(int)(cmd.param5) == vehicle_command_s::PREFLIGHT_CALIBRATION_TEMPERATURE_CALIBRATION ||
+							(int)(cmd.param7) == vehicle_command_s::PREFLIGHT_CALIBRATION_TEMPERATURE_CALIBRATION) {
 						/* temperature calibration: handled in events module */
 						break;
 
