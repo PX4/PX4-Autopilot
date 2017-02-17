4573faa338314d500f7430582cf6aa767f7b70c8
Sergej Scheiermann <sergej.scheiermann@gmail.com>
2017-02-17 15:54:14 +0100

    bmi055 added to start-up script

--- src/platforms/px4_spi.h
+++ src/platforms/px4_spi.h
@@ -15,8 +15,7 @@ enum spi_dev_e {
 	SPIDEV_EXPANDER,    /* Select SPI I/O expander device */
 	SPIDEV_MUX,         /* Select SPI multiplexer device */
 	SPIDEV_AUDIO_DATA,  /* Select SPI audio codec device data port */
-	SPIDEV_AUDIO_CTRL,  /* Select SPI audio codec device control port */
-	SPIDEV_BMI055_GYR /* Select SPI BMI055 Gyroscope */
+	SPIDEV_AUDIO_CTRL  /* Select SPI audio codec device control port */
 };
 
 /* Certain SPI devices may required different clocking modes */
