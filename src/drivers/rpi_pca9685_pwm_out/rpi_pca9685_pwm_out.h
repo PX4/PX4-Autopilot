c89351c45374cdc624d4a33143e2b6d54b27b2a8
crossa <421395590@qq.com>
2017-02-24 13:11:54 +0800

    Add PWM output driver with raspberry pi+pca9685 module

---
+++ src/drivers/rpi_pca9685_pwm_out/rpi_pca9685_pwm_out.h
@@ -0,0 +1,123 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2015-2016 zhangfan. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+#include <stdint.h>
+
+#include <px4_tasks.h>
+#include <px4_getopt.h>
+#include <px4_posix.h>
+#include <errno.h>
+#include <cmath>	// NAN
+
+#include <uORB/uORB.h>
+#include <uORB/topics/actuator_controls.h>
+#include <uORB/topics/actuator_outputs.h>
+#include <uORB/topics/actuator_armed.h>
+#include <drivers/drv_hrt.h>
+#include <drivers/drv_mixer.h>
+#include <systemlib/mixer/mixer.h>
+#include <systemlib/mixer/mixer_load.h>
+#include <systemlib/param/param.h>
+#include <systemlib/pwm_limit/pwm_limit.h>
+#include "PCA9685.h"
+
+namespace rpi_pca9685_pwm_out
+{
+static px4_task_t _task_handle = -1;
+volatile bool _task_should_exit = false;
+static bool _is_running = false;
+
+static const int NUM_PWM = 4;
+static char _device[64] = "/sys/class/pwm/pwmchip0";
+//static int _pwm_fd[NUM_PWM];
+
+static const int FREQUENCY_PWM = 400;
+static char _mixer_filename[64] = "ROMFS/px4fmu_common/mixers/quad_x.main.mix";
+
+// subscriptions
+int _controls_subs[actuator_controls_s::NUM_ACTUATOR_CONTROL_GROUPS];
+int _armed_sub;
+
+// publications
+orb_advert_t _outputs_pub = nullptr;
+orb_advert_t _rc_pub = nullptr;
+
+// topic structures
+actuator_controls_s _controls[actuator_controls_s::NUM_ACTUATOR_CONTROL_GROUPS];
+orb_id_t _controls_topics[actuator_controls_s::NUM_ACTUATOR_CONTROL_GROUPS];
+actuator_outputs_s _outputs;
+actuator_armed_s _armed;
+
+// polling
+uint8_t _poll_fds_num = 0;
+px4_pollfd_struct_t _poll_fds[actuator_controls_s::NUM_ACTUATOR_CONTROL_GROUPS];
+
+// control groups related
+uint32_t _groups_required = 0;
+uint32_t _groups_subscribed = 0;
+
+pwm_limit_t _pwm_limit;
+
+// esc parameters
+int32_t _pwm_disarmed;
+int32_t _pwm_min;
+int32_t _pwm_max;
+
+//PCA9685 device
+::PCA9685 pwm;
+
+MixerGroup *_mixer_group = nullptr;
+
+void usage();
+
+void start();
+
+void stop();
+
+int pwm_initialize();
+
+void pwm_deinitialize();
+
+void send_outputs_pwm(const uint16_t *pwm);
+
+void task_main_trampoline(int argc, char *argv[]);
+
+void subscribe();
+
+void task_main(int argc, char *argv[]);
+
+/* mixer initialization */
+int initialize_mixer(const char *mixer_filename);
+int mixer_control_callback(uintptr_t handle, uint8_t control_group,
+			   uint8_t control_index, float &input);
+}
+extern "C" __EXPORT int rpi_pca9685_pwm_out_main(int, char **);
