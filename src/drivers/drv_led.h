43b98d9a2f0d85a3e3c9bebaec6b5ca8cd49443b
Beat Küng <beat-kueng@gmx.net>
2017-02-24 10:04:08 +0100

    lib/led: add led class with led_control uorb topic
    This is the new interface to control external LED's (user-facing).
    Features:
    - Supports maximum N Leds (where the board can define N)
    - on/off/blink M times
    - Different priorities
    - Allows setting a single led or multiple at once

---
+++ src/drivers/drv_led.h
@@ -0,0 +1,61 @@
+/****************************************************************************
+ *
+ *   Copyright (C) 2012-2017 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/**
+ * @file drv_led.h
+ *
+ * Led device API to control the external LED(s) via uORB interface
+ */
+
+#pragma once
+
+
+#include <uORB/topics/led_control.h>
+
+#include <board_config.h>
+
+// allow the board to override the number (or maxiumum number) of LED's it has
+#ifndef BOARD_MAX_LEDS
+#define BOARD_MAX_LEDS 4
+#endif
+
+#if BOARD_MAX_LEDS > 8 // because led_mask is uint8_t
+#error "BOARD_MAX_LEDS too large. You need to change the led_mask type in the led_control uorb topic (and where it's used)"
+#endif
+
+
+// set the queue size to the number of LED's, so that each led can be controlled individually
+static const int LED_UORB_QUEUE_LENGTH = BOARD_MAX_LEDS;
+
+
+#define RGBLED0_DEVICE_PATH "/dev/rgbled0"
