b2daaf4cb8eaacd1ebcd729148950d9967ca1525
Sander Smeets <sander@droneslab.com>
2017-02-25 13:51:28 +0100

    Disable quadchute angles by default

--- src/modules/vtol_att_control/vtol_att_control_params.c
+++ src/modules/vtol_att_control/vtol_att_control_params.c
@@ -317,7 +317,7 @@ PARAM_DEFINE_FLOAT(VT_FW_MIN_ALT, 0.0f);
  * @max 180
  * @group VTOL Attitude Control
  */
-PARAM_DEFINE_INT32(VT_FW_QC_P, 55);
+PARAM_DEFINE_INT32(VT_FW_QC_P, 0);
 
 /**
  * QuadChute Max Roll
@@ -328,7 +328,7 @@ PARAM_DEFINE_INT32(VT_FW_QC_P, 55);
  * @max 180
  * @group VTOL Attitude Control
  */
-PARAM_DEFINE_INT32(VT_FW_QC_R, 55);
+PARAM_DEFINE_INT32(VT_FW_QC_R, 0);
 
 /**
  * Airspeed less front transition time (open loop)
