daf668e6873827a092d5705af661d96bc8f637a5
Lorenz Meier <lm@inf.ethz.ch>
2017-03-06 20:42:01 +0100

    Fixed wing land detector: Fix RTL logic with a temporary altitude limit workaround

--- src/modules/land_detector/FixedwingLandDetector.cpp
+++ src/modules/land_detector/FixedwingLandDetector.cpp
@@ -94,8 +94,10 @@ void FixedwingLandDetector::_update_params()
 
 float FixedwingLandDetector::_get_max_altitude()
 {
-	//ToDo
-	return 0.0f;
+	// TODO
+	// This means no altitude limit as the limit
+	// is always current position plus 1000 meters
+	return -_controlState.z_pos + 1000;
 }
 
 bool FixedwingLandDetector::_get_freefall_state()
