8ce878455d89e78edd8f85ab5b243a8e1f0c60d6
priseborough <p_riseborough@live.com.au>
2017-03-08 10:57:05 +1100

    position_estimator_inav: publish placeholder values for velocity accuracy

--- src/modules/position_estimator_inav/position_estimator_inav_main.cpp
+++ src/modules/position_estimator_inav/position_estimator_inav_main.cpp
@@ -1342,6 +1342,9 @@ int position_estimator_inav_thread_main(int argc, char *argv[])
 			local_pos.dist_bottom_valid = dist_bottom_valid;
 			local_pos.eph = eph;
 			local_pos.epv = epv;
+			// TODO provide calculated values for these
+			local_pos.evh = 0.0f;
+			local_pos.evv = 0.0f;
 
 			if (local_pos.dist_bottom_valid) {
 				local_pos.dist_bottom = dist_ground;
@@ -1372,6 +1375,10 @@ int position_estimator_inav_thread_main(int argc, char *argv[])
 
 				global_pos.eph = eph;
 				global_pos.epv = epv;
+
+				// TODO provide calculated values for these
+				global_pos.evh = 0.0f;
+				global_pos.evv = 0.0f;
 
 				if (terrain_estimator.is_valid()) {
 					global_pos.terrain_alt = global_pos.alt - terrain_estimator.get_distance_to_ground();
