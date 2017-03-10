dab4798006f164348f63b54469680618ddbf4c07
Cla Mattia Galliard <clag@student.ethz.ch>
2017-03-10 10:09:22 +0100

    added application fw_att_control_q for cla mattias Bachelorthesis

--- cmake/configs/nuttx_px4fmu-v2_default.cmake
+++ cmake/configs/nuttx_px4fmu-v2_default.cmake
@@ -104,8 +104,9 @@ set(config_module_list
 	#
 	# Vehicle Control
 	#
-	modules/fw_pos_control_l1
-	modules/fw_att_control
+        modules/fw_pos_control_l1
+        modules/fw_att_control
+        modules/fw_att_control_q
 	modules/mc_att_control
 	modules/mc_pos_control
 	modules/vtol_att_control
