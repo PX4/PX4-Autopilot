2522e464e8397373b567e7c2801bfedeb03d0c69
Francisco Ferreira <OXINARF@users.noreply.github.com>
2017-03-21 16:36:35 +0000

    upload: support FMUv3

--- makefiles/upload.mk
+++ makefiles/upload.mk
@@ -30,6 +30,9 @@ upload-serial-px4fmu-v1:	$(BUNDLE) $(UPLOADER)
 upload-serial-px4fmu-v2:	$(BUNDLE) $(UPLOADER)
 	$(Q) $(PYTHON) -u $(UPLOADER) --port $(SERIAL_PORTS) $(BUNDLE)
 
+upload-serial-px4fmu-v3:	$(BUNDLE) $(UPLOADER)
+	$(Q) $(PYTHON) -u $(UPLOADER) --port $(SERIAL_PORTS) $(BUNDLE)
+
 upload-serial-px4fmu-v4:	$(BUNDLE) $(UPLOADER)
 	$(Q) $(PYTHON) -u $(UPLOADER) --port $(SERIAL_PORTS) $(BUNDLE)
 
