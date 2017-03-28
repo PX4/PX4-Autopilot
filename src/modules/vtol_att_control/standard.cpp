66d67d3a22c84f43e349b4ec0d580b4241b5f4ca
Sander Smeets <sander@droneslab.com>
2017-03-28 02:45:07 +0200

    Full weight on FW actuators

--- src/modules/vtol_att_control/standard.cpp
+++ src/modules/vtol_att_control/standard.cpp
@@ -459,13 +459,13 @@ void Standard::fill_actuator_outputs()
 
 	//roll
 	_actuators_out_1->control[actuator_controls_s::INDEX_ROLL] =
-		-_actuators_fw_in->control[actuator_controls_s::INDEX_ROLL] * (1 - _mc_roll_weight);
+		-_actuators_fw_in->control[actuator_controls_s::INDEX_ROLL];
 	//pitch
 	_actuators_out_1->control[actuator_controls_s::INDEX_PITCH] =
-		(_actuators_fw_in->control[actuator_controls_s::INDEX_PITCH] + _params->fw_pitch_trim) * (1 - _mc_pitch_weight);
+		_actuators_fw_in->control[actuator_controls_s::INDEX_PITCH] + _params->fw_pitch_trim;
 	// yaw
 	_actuators_out_1->control[actuator_controls_s::INDEX_YAW] =
-		_actuators_fw_in->control[actuator_controls_s::INDEX_YAW] * (1 - _mc_yaw_weight);
+		_actuators_fw_in->control[actuator_controls_s::INDEX_YAW];
 
 	// set the fixed wing throttle control
 	if (_vtol_schedule.flight_mode == FW_MODE && _armed->armed) {
