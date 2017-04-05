lucasdemarchi/Firmware

mpu9250: add support to MPU6500
MPU9250 is mostly an MPU6500 with a mag (AK8963) in the same package.
Support driving MPU6500 with the MPU9250 driver. The id of the driver
isn't set differently since this way it allows to force a recalibration.

Ideally MPU9250 driver could even not exist and the support for these
sensors be merged back in the MPU6000 that's more complete. This is an
intermediate step in that direction.

src/drivers/mpu9250/mpu9250_spi.cpp
@@ -265,8 +265,28 @@ int
 MPU9250_SPI::probe()
 {
 	uint8_t whoami = 0;
-	uint8_t expected = MPU_WHOAMI_9250;
-	return (read(MPUREG_WHOAMI, &whoami, 1) == OK && (whoami == expected)) ? 0 : -EIO;
+
+	warnx("probe!");
+	int ret = read(MPUREG_WHOAMI, &whoami, 1);
+
+	if (ret != OK) {
+		return -EIO;
+	}
+
+	warnx("probe!");
+
+	switch (whoami) {
+	case MPU_WHOAMI_9250:
+	case MPU_WHOAMI_6500:
+		ret = 0;
+		break;
+
+	default:
+		warnx("probe failed! %u", whoami);
+		ret = -EIO;
+	}
+
+	return ret;
 }
 
 #endif // PX4_SPIDEV_MPU

PX4/PX4-Autopilot

src/drivers/mpu9250/mpu9250_spi.cpp
@@ -265,8 +265,25 @@ int
 MPU9250_SPI::probe()
 {
 	uint8_t whoami = 0;
-	uint8_t expected = MPU_WHOAMI_9250;
-	return (read(MPUREG_WHOAMI, &whoami, 1) == OK && (whoami == expected)) ? 0 : -EIO;
+
+	int ret = read(MPUREG_WHOAMI, &whoami, 1);
+
+	if (ret != OK) {
+		return -EIO;
+	}
+
+	switch (whoami) {
+	case MPU_WHOAMI_9250:
+	case MPU_WHOAMI_6500:
+		ret = 0;
+		break;
+
+	default:
+		PX4_WARN("probe failed! %u", whoami);
+		ret = -EIO;
+	}
+
+	return ret;
 }
 
 #endif // PX4_SPIDEV_MPU