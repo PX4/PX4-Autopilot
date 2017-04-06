648c471f63b4ed0b397f8ceb8e9d08179d19d5ad
Pavel Kirienko <pavel.kirienko@gmail.com>
2017-04-07 01:28:00 +0300

    More test fixes

--- libuavcan/test/protocol/dynamic_node_id_server/node_discoverer.cpp
+++ libuavcan/test/protocol/dynamic_node_id_server/node_discoverer.cpp
@@ -153,7 +153,7 @@ TEST(dynamic_node_id_server_NodeDiscoverer, Basic)
     node_status.uptime_sec += 5U;
     ASSERT_LE(0, node_status_pub.broadcast(node_status));
 
-    nodes.spinBoth(uavcan::MonotonicDuration::fromMSec(1150));
+    nodes.spinBoth(uavcan::MonotonicDuration::fromMSec(1250));
 
     ASSERT_EQ(1, tracer.countEvents(TraceDiscoveryNewNodeFound));
     ASSERT_EQ(1, tracer.countEvents(TraceDiscoveryTimerStart));
@@ -256,7 +256,7 @@ TEST(dynamic_node_id_server_NodeDiscoverer, RestartAndMaxAttempts)
     /*
      * Waiting for timeout
      */
-    nodes.spinBoth(uavcan::MonotonicDuration::fromMSec(1600));
+    nodes.spinBoth(uavcan::MonotonicDuration::fromMSec(3100));
 
     ASSERT_EQ(1, tracer.countEvents(TraceDiscoveryNewNodeFound));
     ASSERT_EQ(1, tracer.countEvents(TraceDiscoveryTimerStart));
