85b074f8d84c8be30d6d93ed5e00d850292f1fae
Lorenz Meier <lorenz@px4.io>
2017-04-09 23:16:50 +0200

    Navigator: Fix RTL state sequence

--- src/modules/navigator/rtl.h
+++ src/modules/navigator/rtl.h
@@ -94,8 +94,6 @@ private:
 		RTL_STATE_LANDED,
 	} _rtl_state;
 
-	bool _rtl_start_lock;
-
 	control::BlockParamFloat _param_return_alt;
 	control::BlockParamFloat _param_min_loiter_alt;
 	control::BlockParamFloat _param_descend_alt;
