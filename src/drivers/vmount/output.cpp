7c0c97ce037507c44335cd50210e719b91c44658
Julian Oes <julian@oes.ch>
2017-04-11 12:13:38 +0200

    vmount: added commented output printf

--- src/drivers/vmount/output.cpp
+++ src/drivers/vmount/output.cpp
@@ -92,6 +92,11 @@ void OutputBase::publish()
 		mount_orientation.attitude_euler_angle[i] = _angle_outputs[i];
 	}
 
+	//PX4_INFO("roll: %.2f, pitch: %.2f, yaw: %.2f",
+	//		(double)_angle_outputs[0],
+	//		(double)_angle_outputs[1],
+	//		(double)_angle_outputs[2]);
+
 	orb_publish_auto(ORB_ID(mount_orientation), &_mount_orientation_pub, &mount_orientation, &instance, ORB_PRIO_DEFAULT);
 }
 
