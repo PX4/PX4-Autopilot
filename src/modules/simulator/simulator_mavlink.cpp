a9ec1a35bc83b84e9663af161f173f98fed4b250
Mohamed Abdelkader Zahana <mohamedashraf123@gmail.com>
2017-04-13 09:27:21 +0300

    divide lat/lon received from HIL_STATE_QUATERNION msg by 1E7

--- src/modules/simulator/simulator_mavlink.cpp
+++ src/modules/simulator/simulator_mavlink.cpp
@@ -404,9 +404,9 @@ void Simulator::handle_message(mavlink_message_t *msg, bool publish)
 			hil_gpos.timestamp = timestamp;
 
 			hil_gpos.time_utc_usec = timestamp;
-			hil_gpos.lat = hil_state.lat;
-			hil_gpos.lon = hil_state.lon;
-			hil_gpos.alt = hil_state.alt / 1000.0f;
+			hil_gpos.lat = hil_state.lat / 10000000.0f;//1E7
+			hil_gpos.lon = hil_state.lon / 10000000.0f;//1E7
+			hil_gpos.alt = hil_state.alt / 1000.0f;//1E3
 
 			hil_gpos.vel_n = hil_state.vx / 100.0f;
 			hil_gpos.vel_e = hil_state.vy / 100.0f;
