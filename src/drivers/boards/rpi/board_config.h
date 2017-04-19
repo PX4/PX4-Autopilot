643a3ba2a72778cd751ad1b7aba104f350a3a103
Beat Küng <beat-kueng@gmx.net>
2017-04-19 07:04:40 +0200

    board_config: clean up board_config.h on POSIX targets

--- src/drivers/boards/rpi/board_config.h
+++ src/drivers/boards/rpi/board_config.h
@@ -39,6 +39,8 @@
 
 #pragma once
 
+#define BOARD_OVERRIDE_UUID "RPIID       " // must be of length 12 (PX4_CPU_UUID_BYTE_LENGTH)
+#define BOARD_OVERRIDE_MFGUID BOARD_OVERRIDE_UUID
 
 #define BOARD_NAME "RPI"
 #define BOARD_BATTERY1_V_DIV   (10.177939394f)
