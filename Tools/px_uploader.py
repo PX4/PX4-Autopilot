9530001886a420b692b8c1e279ea86c0dbdc6bdb
Julian Oes <julian@oes.ch>
2017-04-20 14:40:43 +0200

    px_uploader.py: properly loop through all baudrates
    In case when the baudrate change failed, we should still try that
    baudrate again in the next iteration.

--- Tools/px_uploader.py
+++ Tools/px_uploader.py
@@ -542,16 +542,14 @@ class uploader(object):
         self.port.close()
 
     def __next_baud_flightstack(self):
-        self.baudrate_flightstack_idx = self.baudrate_flightstack_idx + 1
-        if self.baudrate_flightstack_idx >= len(self.baudrate_flightstack):
+        if self.baudrate_flightstack_idx + 1 >= len(self.baudrate_flightstack):
             return False
-
         try:
-            self.port.baudrate = self.baudrate_flightstack[self.baudrate_flightstack_idx]
+            self.port.baudrate = self.baudrate_flightstack[self.baudrate_flightstack_idx + 1]
+            self.baudrate_flightstack_idx = self.baudrate_flightstack_idx + 1
         except serial.SerialException:
             # Sometimes _configure_port fails
             time.sleep(0.04)
-            pass
 
         return True
 
