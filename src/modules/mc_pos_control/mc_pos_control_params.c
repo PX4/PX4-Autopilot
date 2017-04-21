a7be9854d9373e08d9e3a99138170ae8777c0684
Dennis Mannhart <dennis.mannhart@gmail.com>
2017-04-21 18:08:05 +0200

    mc_pos_control: remove scaling in auto and remove limit_velocity function; update auto logic

--- src/modules/mc_pos_control/mc_pos_control_params.c
+++ src/modules/mc_pos_control/mc_pos_control_params.c
@@ -229,7 +229,7 @@ PARAM_DEFINE_FLOAT(MPC_XY_VEL_I, 0.02f);
 PARAM_DEFINE_FLOAT(MPC_XY_VEL_D, 0.01f);
 
 /**
- * Nominal horizontal velocity in mission
+ * Maximum horizontal velocity in mission
  *
  * Normal horizontal velocity in AUTO modes (includes
  * also RTL / hold / etc.) and endpoint for
@@ -245,7 +245,23 @@ PARAM_DEFINE_FLOAT(MPC_XY_VEL_D, 0.01f);
 PARAM_DEFINE_FLOAT(MPC_XY_CRUISE, 5.0f);
 
 /**
- * Nominal horizontal velocity for manual controlled mode
+ * Minimum horizontal velocity in mission
+ *
+ * Normal horizontal velocity in AUTO modes (includes
+ * also RTL / hold / etc.) and endpoint for
+ * position stabilized mode (POSCTRL).
+ *
+ * @unit m/s
+ * @min 0.5
+ * @max 8.0
+ * @increment 1
+ * @decimal 2
+ * @group Multicopter Position Control
+ */
+PARAM_DEFINE_FLOAT(MPC_CRUISE_MIN, 1.0f);
+
+/**
+ * Maximum horizontal velocity setpoint for manual controlled mode
  *
  * @unit m/s
  * @min 3.0
