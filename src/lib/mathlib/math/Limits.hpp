7429955d9c24cd1c176c6fa2ab66e8fb03a42052
Mohammed Kabir <kabir@uasys.io>
2017-04-22 15:24:29 +0200

    mathlib : switch min/max to constexpr to match std::min/max

--- src/lib/mathlib/math/Limits.hpp
+++ src/lib/mathlib/math/Limits.hpp
@@ -56,19 +56,19 @@ namespace math
 {
 
 template<typename _Tp>
-inline const _Tp &min(const _Tp &a, const _Tp &b)
+inline constexpr const _Tp &min(const _Tp &a, const _Tp &b)
 {
 	return (a < b) ? a : b;
 }
 
 template<typename _Tp>
-inline const _Tp &max(const _Tp &a, const _Tp &b)
+inline constexpr const _Tp &max(const _Tp &a, const _Tp &b)
 {
 	return (a > b) ? a : b;
 }
 
 template<typename _Tp>
-inline const _Tp &constrain(const _Tp &val, const _Tp &min_val, const _Tp &max_val)
+inline constexpr const _Tp &constrain(const _Tp &val, const _Tp &min_val, const _Tp &max_val)
 {
 	return (val < min_val) ? min_val : ((val > max_val) ? max_val : val);
 }
