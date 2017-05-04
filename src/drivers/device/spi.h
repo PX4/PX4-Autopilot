5137d1c60a29a5e35d6431076d6a7e2928dee1ab
David Sidrane <david_s5@nscdg.com>
2017-05-03 15:42:20 -1000

    device:spi_dev_e is now uint32_t in NuttX
       Using new type of uint32_t for spi device

--- src/drivers/device/spi.h
+++ src/drivers/device/spi.h
@@ -71,7 +71,7 @@ protected:
 	SPI(const char *name,
 	    const char *devname,
 	    int bus,
-	    enum spi_dev_e device,
+	    uint32_t device,
 	    enum spi_mode_e mode,
 	    uint32_t frequency,
 	    int irq = 0);
@@ -138,7 +138,7 @@ protected:
 	LockMode	locking_mode;	/**< selected locking mode */
 
 private:
-	enum spi_dev_e		_device;
+	uint32_t			_device;
 	enum spi_mode_e		_mode;
 	uint32_t		_frequency;
 	struct spi_dev_s	*_dev;
