b1670e7e6c03185c68ce5dd4f3b9c54baf5ecc5f
Lorenz Meier <lorenz@px4.io>
2017-06-02 18:28:48 +0200

    BMI160 driver: Minor code style fix

--- src/drivers/bmi160/bmi160.cpp
+++ src/drivers/bmi160/bmi160.cpp
@@ -1290,7 +1290,7 @@ BMI160::measure()
 	grb.temperature = _last_temperature;
 
 	/* return device ID */
-	grb.device_id = _gyro->_device_id.devid;;
+	grb.device_id = _gyro->_device_id.devid;
 
 	_accel_reports->force(&arb);
 	_gyro_reports->force(&grb);
