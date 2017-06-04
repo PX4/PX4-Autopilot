d9dd1b231d0ddde39c5eb56737fda0c265f38ace
Lorenz Meier <lorenz@px4.io>
2017-06-04 10:16:19 +0200

    UAVCAN server: Use correct MAVLink header version

--- src/modules/uavcan/uavcan_servers.cpp
+++ src/modules/uavcan/uavcan_servers.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2014 PX4 Development Team. All rights reserved.
+ *   Copyright (c) 2014-2017 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -62,7 +62,7 @@
 #include <uORB/topics/uavcan_parameter_request.h>
 #include <uORB/topics/uavcan_parameter_value.h>
 
-#include <v1.0/common/mavlink.h>
+#include <v2.0/common/mavlink.h>
 
 
 /**
