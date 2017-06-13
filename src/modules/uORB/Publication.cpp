550aee4da36d7556f65c318797f702db47c7fdea
Julien Lecoeur <julien.lecoeur@epfl.ch>
2017-06-13 09:53:07 +0200

    Remove explicit template instantiation of Publication<T>

--- src/modules/uORB/Publication.cpp
+++ src/modules/uORB/Publication.cpp
@@ -107,20 +107,4 @@ PublicationNode::PublicationNode(const struct orb_metadata *meta,
 	if (list != nullptr) { list->add(this); }
 }
 
-// explicit template instantiation
-template class __EXPORT Publication<actuator_controls_s>;
-template class __EXPORT Publication<actuator_direct_s>;
-template class __EXPORT Publication<actuator_outputs_s>;
-template class __EXPORT Publication<debug_key_value_s>;
-template class __EXPORT Publication<ekf2_innovations_s>;
-template class __EXPORT Publication<filtered_bottom_flow_s>;
-template class __EXPORT Publication<rc_channels_s>;
-template class __EXPORT Publication<tecs_status_s>;
-template class __EXPORT Publication<vehicle_attitude_s>;
-template class __EXPORT Publication<vehicle_attitude_setpoint_s>;
-template class __EXPORT Publication<vehicle_global_position_s>;
-template class __EXPORT Publication<vehicle_global_velocity_setpoint_s>;
-template class __EXPORT Publication<vehicle_local_position_s>;
-template class __EXPORT Publication<vehicle_rates_setpoint_s>;
-
 }
