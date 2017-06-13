f8f8566ee8c31157cb687e34e339a9b16fe0030b
zhangfan <421395590@qq.com>
2017-06-13 17:38:43 +0800

    修正通用工具链配置文件

--- cmake/toolchains/Toolchain-cross-generic.cmake
+++ cmake/toolchains/Toolchain-cross-generic.cmake
@@ -16,20 +16,21 @@
 
 include(CMakeForceCompiler)
 
-#if ("$ENV{RPI_TOOLCHAIN_DIR}" STREQUAL "")
-#        message(FATAL_ERROR "RPI_TOOLCHAIN_DIR not set")
-#else()
-#        set(RPI_TOOLCHAIN_DIR $ENV{RPI_TOOLCHAIN_DIR})
-#endif()
+if ("$ENV{TOOLCHAIN_BIN_DIR}" STREQUAL "")
+        message(FATAL_ERROR "TOOLCHAIN_BIN_DIR not set")
+else()
+        set(TOOLCHAIN_BIN_DIR $ENV{TOOLCHAIN_BIN_DIR})
+endif()
 
-#if ("$ENV{CROSS_COMPILE_PREFIX}" STREQUAL "")
-#        message(FATAL_ERROR "CROSS_COMPILE_PREFIX not set")
-#else()
-#        set(CROSS_COMPILE_PREFIX $ENV{CROSS_COMPILE_PREFIX})
-#endif()
+if ("$ENV{CROSS_COMPILE}" STREQUAL "")
+        message(FATAL_ERROR "CROSS_COMPILE not set")
+else()
+        set(CROSS_COMPILE $ENV{CROSS_COMPILE})
+endif()
 
-# 此处定义CROSS_COMPILE
-set(CROSS_COMPILE $ENV{CROSS_COMPILE})
+
+
+
 
 # this one is important
 set(CMAKE_SYSTEM_NAME Generic)
@@ -38,14 +39,21 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_VERSION 1)
 
 # specify the cross compiler
-find_program(C_COMPILER ${CROSS_COMPILE}-gcc)
+find_program(C_COMPILER ${CROSS_COMPILE}-gcc
+	PATHS ${TOOLCHAIN_BIN_DIR}/bin
+	NO_DEFAULT_PATH
+	)
 
 if(NOT C_COMPILER)
 	message(FATAL_ERROR "could not find ${CROSS_COMPILE}-gcc compiler")
 endif()
 cmake_force_c_compiler(${C_COMPILER} GNU)
 
-find_program(CXX_COMPILER  ${CROSS_COMPILE}-g++)
+find_program(CXX_COMPILER  ${CROSS_COMPILE}-g++
+	PATHS ${TOOLCHAIN_BIN_DIR}/bin
+	NO_DEFAULT_PATH
+	)
+
 if(NOT CXX_COMPILER)
 	message(FATAL_ERROR "could not find ${CROSS_COMPILE}-g++ compiler")
 endif()
@@ -54,7 +62,10 @@ cmake_force_cxx_compiler(${CXX_COMPILER} GNU)
 # compiler tools
 foreach(tool objcopy nm ld)
 	string(TOUPPER ${tool} TOOL)
-	find_program(${TOOL} ${CROSS_COMPILE}-${tool})
+	find_program(${TOOL} ${CROSS_COMPILE}-${tool}
+		PATHS ${TOOLCHAIN_BIN_DIR}/bin
+		NO_DEFAULT_PATH
+		)
 	if(NOT ${TOOL})
 		message(FATAL_ERROR "could not find ${CROSS_COMPILE}-${tool}")
 	endif()
@@ -68,10 +79,6 @@ foreach(tool echo grep rm mkdir nm cp touch make unzip)
 		message(FATAL_ERROR "could not find ${TOOL}")
 	endif()
 endforeach()
-
-#add_definitions(
-#	-D __DF_RPI
-#)
 
 set(LINKER_FLAGS "-Wl,-gc-sections")
 set(CMAKE_EXE_LINKER_FLAGS ${LINKER_FLAGS})
