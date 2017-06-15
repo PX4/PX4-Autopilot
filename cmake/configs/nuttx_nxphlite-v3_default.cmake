2de54da6482526914059aa4b6d9096dfff33c391
David Sidrane <david_s5@nscdg.com>
2017-06-14 16:18:14 -1000

    nxphlite-v3:Updates for latest master

--- cmake/configs/nuttx_nxphlite-v3_default.cmake
+++ cmake/configs/nuttx_nxphlite-v3_default.cmake
@@ -82,7 +82,7 @@ set(config_module_list
 ### NOT Portable YET drivers/test_ppm
 	modules/commander/commander_tests
 	modules/mc_pos_control/mc_pos_control_tests
-	modules/controllib_test
+	lib/controllib/controllib_test
 	modules/mavlink/mavlink_tests
 	modules/unit_test
 	modules/uORB/uORB_tests
@@ -99,6 +99,7 @@ set(config_module_list
 	modules/gpio_led
 ##NO CAN YET	modules/uavcan
 	modules/land_detector
+	modules/camera_feedback
 
 	#
 	# Estimation modules (EKF/ SO3 / other filters)
@@ -111,9 +112,10 @@ set(config_module_list
 	#
 	# Vehicle Control
 	#
-	# modules/segway # XXX Needs GCC 4.7 fix
-	modules/fw_pos_control_l1
 	modules/fw_att_control
+	modules/fw_pos_control_l1
+	modules/gnd_att_control
+	modules/gnd_pos_control
 	modules/mc_att_control
 	modules/mc_pos_control
 	modules/vtol_att_control
@@ -127,7 +129,7 @@ set(config_module_list
 	#
 	# Library modules
 	#
-	modules/param
+	modules/systemlib/param
 	modules/systemlib
 	modules/systemlib/mixer
 	modules/uORB
