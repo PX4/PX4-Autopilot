4349f49610e6ba5ae4e15e31f3d114a257628f17
David Sidrane <david_s5@nscdg.com>
2017-06-27 07:04:17 -1000

    PX4 System:Expunge the nuttx adc structure from the system
       This PR is preliminary ground work for FMUv5.
    
       PX4 does not use the NuttX adc driver. But used the same format
       for the data returned by the nuttx ADC driver.
    
       There was a fixme:in src/platforms/px4_adc.h "this needs to be
       a px4_adc_msg_s type" With this PR the need for
       src/platforms/px4_adc.h goes away as the driver drv_adc.h now
       describes the px4_adc_msg_t.

--- src/platforms/posix/include/system_config.h
+++ src/platforms/posix/include/system_config.h
@@ -39,6 +39,11 @@
 
 #pragma once
 
+#define ADC_BATTERY_VOLTAGE_CHANNEL     10
+#define ADC_BATTERY_CURRENT_CHANNEL     ((uint8_t)(-1))
+#define ADC_AIRSPEED_VOLTAGE_CHANNEL    11
+#define ADCSIM0_DEVICE_PATH	"/dev/adc0"
+
 /*
  * I2C busses
  */
