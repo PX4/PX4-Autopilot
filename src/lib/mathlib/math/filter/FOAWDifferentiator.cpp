78cd7e37e3d48fa3117ccc822a166ab158072096
Lorenz Meier <lorenz@px4.io>
2017-07-11 10:57:51 +0200

    FOAW: Fix code style issues

--- src/lib/mathlib/math/filter/FOAWDifferentiator.cpp
+++ src/lib/mathlib/math/filter/FOAWDifferentiator.cpp
@@ -2,7 +2,7 @@
 
 /****************************************************************************
  *
- *   Copyright (C) 2012 PX4 Development Team. All rights reserved.
+ *   Copyright (c) 2017 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -35,9 +35,9 @@
 
 
 /*
- * @file	FOAWDifferentiator.cpp
- * @brief	A class to implement a first order adaptive windowing differentiator 
- * Author: Mathieu Bresciani <brescianimathieu@gmail.com>
+ * @file    FOAWDifferentiator.cpp
+ * @brief   A class to implement a first order adaptive windowing differentiator
+ * @author Mathieu Bresciani <brescianimathieu@gmail.com>
  * From:  Discrete-Time Adaptive Windowing for Velocity Estimation
  * Farrokh Janabi-Sharifi, Vincent Hayward, and Chung-Shin J. Chen
  */
@@ -48,201 +48,197 @@
 
 namespace math
 {
-    FOAWDifferentiator::FOAWDifferentiator(float dt, float noise_level)
-    {
-        set_noise_level(noise_level);
-        set_sample_time(dt);
-        reset();
-    }
 
-    FOAWDifferentiator::~FOAWDifferentiator()
-    {
+FOAWDifferentiator::FOAWDifferentiator(float dt, float noise_level)
+{
+	set_noise_level(noise_level);
+	set_sample_time(dt);
+	reset();
+}
 
-    }
+FOAWDifferentiator::~FOAWDifferentiator()
+{
 
-    void FOAWDifferentiator::set_noise_level(float noise_level)
-    {
-        _delta = noise_level;
-    }
+}
 
-    void FOAWDifferentiator::set_sample_time(float dt)
-    {
-        _dt = dt;
-    }
+void FOAWDifferentiator::set_noise_level(float noise_level)
+{
+	_delta = noise_level;
+}
 
-    uint8_t FOAWDifferentiator::get_last_window_size(void)
-    {
-        return _last_window_size;
-    }
+void FOAWDifferentiator::set_sample_time(float dt)
+{
+	_dt = dt;
+}
 
-   void FOAWDifferentiator::reset(void) 
-    {
-        memset(&_buffer, 0, sizeof(_buffer));
-        _nb_samples = 0;
-        _last_window_size = 0;
-    }
-    
-    void FOAWDifferentiator::add_sample(float sample)
-    {
-        if (_nb_samples <= _max_window_size) {
-            _nb_samples++;
-        }
-        else{
-            shift_buffer();
-            _nb_samples = _max_window_size+1;
-        }
+uint8_t FOAWDifferentiator::get_last_window_size()
+{
+	return _last_window_size;
+}
 
-        _buffer[_nb_samples-1] = sample;
-    }
+void FOAWDifferentiator::reset()
+{
+	memset(&_buffer, 0, sizeof(_buffer));
+	_nb_samples = 0;
+	_last_window_size = 0;
+}
 
-    void FOAWDifferentiator::shift_buffer(void)
-    {
-        for (int i = 0; i < (_nb_samples-1); i++) {
-           _buffer[i] = _buffer[i+1]; 
-        }
+void FOAWDifferentiator::add_sample(float sample)
+{
+	if (_nb_samples <= _max_window_size) {
+		_nb_samples++;
 
-    }
- 
-    void FOAWDifferentiator::end_fit_FOAW(uint8_t window_size)
-    {
-        float d_amplitude;
-        float d_time;
-        uint8_t last_sample_pos;
+	} else {
+		shift_buffer();
+		_nb_samples = _max_window_size + 1;
+	}
 
-        last_sample_pos = _nb_samples - 1;
+	_buffer[_nb_samples - 1] = sample;
+}
 
-        d_amplitude = _buffer[last_sample_pos] - _buffer[last_sample_pos-window_size];
-        d_time  = window_size * _dt;
-        fit_val.a = d_amplitude / d_time;
-        fit_val.b = _buffer[last_sample_pos];
+void FOAWDifferentiator::shift_buffer()
+{
+	for (int i = 0; i < (_nb_samples - 1); i++) {
+		_buffer[i] = _buffer[i + 1];
+	}
+}
 
-        return;
-    }
+void FOAWDifferentiator::end_fit_FOAW(uint8_t window_size)
+{
+	float d_amplitude;
+	float d_time;
+	uint8_t last_sample_pos;
 
-    void FOAWDifferentiator::best_fit_FOAW(uint8_t window_size)
-    {
-        float sum1;
-        float sum2;
-        float den;
-        float y_mean;
-        uint8_t last_sample_pos;
-        uint8_t i;
+	last_sample_pos = _nb_samples - 1;
 
-        sum1 = 0.0f;
-        sum2 = 0.0f;
-        den = 0.0f;
-        y_mean = 0.0f;
-        fit_val.a = 0.0f;
-        fit_val.b = 0.0f;
+	d_amplitude = _buffer[last_sample_pos] - _buffer[last_sample_pos - window_size];
+	d_time  = window_size * _dt;
+	fit_val.a = d_amplitude / d_time;
+	fit_val.b = _buffer[last_sample_pos];
+}
 
-        last_sample_pos = _nb_samples - 1;
+void FOAWDifferentiator::best_fit_FOAW(uint8_t window_size)
+{
+	float sum1;
+	float sum2;
+	float den;
+	float y_mean;
+	uint8_t last_sample_pos;
+	uint8_t i;
 
-        // First order least squares fit of all the points inside the window
-        for (i = 0; i <= window_size; i++) {
-           sum1 += _buffer[last_sample_pos - i];
-           sum2 += _buffer[last_sample_pos - i]*i;
-        }
+	sum1 = 0.0f;
+	sum2 = 0.0f;
+	den = 0.0f;
+	y_mean = 0.0f;
+	fit_val.a = 0.0f;
+	fit_val.b = 0.0f;
 
-        y_mean = sum1/(window_size+1);
-        sum1 *= window_size;
-        sum2 *= 2;
+	last_sample_pos = _nb_samples - 1;
 
-        den = _dt*window_size*(window_size+1)*(window_size+2)/6.0f;
+	// First order least squares fit of all the points inside the window
+	for (i = 0; i <= window_size; i++) {
+		sum1 += _buffer[last_sample_pos - i];
+		sum2 += _buffer[last_sample_pos - i] * i;
+	}
 
-        // Prevents division by zero
-        if (den < 0.0001f && den > -0.0001f) {
-            fit_val.a = 0.0f;
-        }
-        else{
-            fit_val.a = (sum1 - sum2)/den;
-        }
+	y_mean = sum1 / (window_size + 1);
+	sum1 *= window_size;
+	sum2 *= 2;
 
-        fit_val.b = y_mean + fit_val.a*window_size*_dt/2.0f;
+	den = _dt * window_size * (window_size + 1) * (window_size + 2) / 6.0f;
 
-        return;
-    }
+	// Prevents division by zero
+	if (den < 0.0001f && den > -0.0001f) {
+		fit_val.a = 0.0f;
 
-    // TODO; Add a way to be able to select the method you prefer (End-fit is faster but less accurate)
-    // Performs the Best-fit-R algorithm
-    float FOAWDifferentiator::fit(void)
-    {
-        uint8_t window_size;
-        uint8_t j;
-        uint8_t last_sample_pos;
-        uint8_t pass;
-        float pos;
-        float result;
-        float slope;
+	} else {
+		fit_val.a = (sum1 - sum2) / den;
+	}
 
-        last_sample_pos = _nb_samples - 1;
-        pass = 0;
-        pos = 0.0f;
-        result = 0.0f;
-        slope = 0.0f;
-        window_size = 1;
+	fit_val.b = y_mean + fit_val.a * window_size * _dt / 2.0f;
+}
 
-        //end_fit_FOAW(window_size); 
-        best_fit_FOAW(window_size);
-        slope = fit_val.a;
-        result = slope;
-        _last_window_size = window_size;
+// TODO; Add a way to be able to select the method you prefer (End-fit is faster but less accurate)
+// Performs the Best-fit-R algorithm
+float FOAWDifferentiator::fit()
+{
+	uint8_t window_size;
+	uint8_t j;
+	uint8_t last_sample_pos;
+	uint8_t pass;
+	float pos;
+	float result;
+	float slope;
 
-        if (last_sample_pos == 0) {
-            return 0.0f;
-        }
+	last_sample_pos = _nb_samples - 1;
+	pass = 0;
+	pos = 0.0f;
+	result = 0.0f;
+	slope = 0.0f;
+	window_size = 1;
 
-        for (window_size = 2; window_size <= (_nb_samples-1); window_size++) {
-            //end_fit_FOAW(window_size); 
-            best_fit_FOAW(window_size);
-            slope = fit_val.a;
+	//end_fit_FOAW(window_size);
+	best_fit_FOAW(window_size);
+	slope = fit_val.a;
+	result = slope;
+	_last_window_size = window_size;
 
-            // Check if all the values are around the fit +/- delta
-            for (j = 1; j < window_size; j++) {
-                // Compute a point on the slope
-                pos = fit_val.b - slope*j*_dt;
+	if (last_sample_pos == 0) {
+		return 0.0f;
+	}
 
-                // Compute min and max bounds
-                float max_bound = pos + _delta;
-                float min_bound = pos - _delta;
-                // Select sample to check
-                float sample_to_check = _buffer[last_sample_pos-j];
+	for (window_size = 2; window_size <= (_nb_samples - 1); window_size++) {
+		//end_fit_FOAW(window_size);
+		best_fit_FOAW(window_size);
+		slope = fit_val.a;
 
-                // Pass the test if the sample is inside the boundaries
-                if (sample_to_check <= max_bound  && sample_to_check >= min_bound) {
-                    pass++;
-                }
-                else {
-                    break;
-                }
-            }
+		// Check if all the values are around the fit +/- delta
+		for (j = 1; j < window_size; j++) {
+			// Compute a point on the slope
+			pos = fit_val.b - slope * j * _dt;
 
-            // If all the values inside the windw are inside the boundaries, accept the new slope and continue with a bigger window if possible
-            if (pass == (window_size-1)) {
-                result = slope;
-                pass = 0;
-                _last_window_size = window_size;
-            }
-            // Otherwise (at least one sample is outside) we keep the previous slope
-            else {
-                break;
-            }
+			// Compute min and max bounds
+			float max_bound = pos + _delta;
+			float min_bound = pos - _delta;
+			// Select sample to check
+			float sample_to_check = _buffer[last_sample_pos - j];
 
-        }
-        return result;
-    }
+			// Pass the test if the sample is inside the boundaries
+			if (sample_to_check <= max_bound  && sample_to_check >= min_bound) {
+				pass++;
+
+			} else {
+				break;
+			}
+		}
+
+		// If all the values inside the windw are inside the boundaries, accept the new slope and continue with a bigger window if possible
+		if (pass == (window_size - 1)) {
+			result = slope;
+			pass = 0;
+			_last_window_size = window_size;
+		}
+
+		// Otherwise (at least one sample is outside) we keep the previous slope
+		else {
+			break;
+		}
+
+	}
+
+	return result;
+}
 
 
-    float FOAWDifferentiator::apply(float sample)
-    {
-        float derivative;
+float FOAWDifferentiator::apply(float sample)
+{
+	float derivative;
 
-        add_sample(sample);
-        derivative = fit();
+	add_sample(sample);
+	derivative = fit();
 
-        return derivative;
-    }
-
-
+	return derivative;
+}
 
 } // namespace math
 
--- src/lib/mathlib/math/filter/FOAWDifferentiator.hpp
+++ src/lib/mathlib/math/filter/FOAWDifferentiator.hpp
@@ -2,7 +2,7 @@
 
 /****************************************************************************
  *
- *   Copyright (C) 2012 PX4 Development Team. All rights reserved.
+ *   Copyright (c) 2017 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -33,9 +33,9 @@
  *
  ****************************************************************************/
 /*
- * @file	FOAWDifferentiator.hpp
- * @brief	A class to implement a first order adaptive windowing differentiator 
- * Author: Mathieu Bresciani <brescianimathieu@gmail.com>
+ * @file    FOAWDifferentiator.hpp
+ * @brief   A class to implement a first order adaptive windowing differentiator
+ * @author Mathieu Bresciani <brescianimathieu@gmail.com>
  * From:  Discrete-Time Adaptive Windowing for Velocity Estimation
  * Farrokh Janabi-Sharifi, Vincent Hayward, and Chung-Shin J. Chen
  */
@@ -47,87 +47,87 @@ namespace math
 class __EXPORT FOAWDifferentiator
 {
 public:
-    // Constructor
-    FOAWDifferentiator(float sample_time, float noise_level); 
+	// Constructor
+	FOAWDifferentiator(float sample_time, float noise_level);
 
-    // Destructor
-    ~FOAWDifferentiator();
+	// Destructor
+	~FOAWDifferentiator();
 
-    /**
-     * Change the main parameter of the differenciator
-     * A small value will make the differenciator more agressive and close to a raw first order derivative
-     * A large value will force the differenciator to use more samples (max 15) to compute the
-     * least squares slope (smoother output, but less reactive).
-     */
-    void set_noise_level(float delta);
+	/**
+	 * Change the main parameter of the differenciator
+	 * A small value will make the differenciator more agressive and close to a raw first order derivative
+	 * A large value will force the differenciator to use more samples (max 15) to compute the
+	 * least squares slope (smoother output, but less reactive).
+	 */
+	void set_noise_level(float delta);
 
-    /**
-     * Set the elapsed time between two measurements
-     */
-    void set_sample_time(float dt);
+	/**
+	 * Set the elapsed time between two measurements
+	 */
+	void set_sample_time(float dt);
 
-    /**
-     * Add a new raw value to the filter
-     *
-     * @return Retrieve the filtered result
-     */
-    float apply(float sample);
+	/**
+	 * Add a new raw value to the filter
+	 *
+	 * @return Retrieve the filtered result
+	 */
+	float apply(float sample);
 
-    /**
-     * @return Retrieve the noise level parameter (_delta) 
-     */
-    float get_noise_level(void); 
+	/**
+	 * @return Retrieve the noise level parameter (_delta)
+	 */
+	float get_noise_level();
 
-    /**
-     * @return Retrieve the size of the window used to compute the derivative
-     */
-    uint8_t get_last_window_size(void);
+	/**
+	 * @return Retrieve the size of the window used to compute the derivative
+	 */
+	uint8_t get_last_window_size();
 
-    /**
-     * Reset the filter
-     */
-    void reset(void);
+	/**
+	 * Reset the filter
+	 */
+	void reset();
 
 private:
 
-    /*
-     * Handles the paramaters of a first order curve
-     * f = a*x + b
-     */
-    struct fit_params{
-        float a;
-        float b;
-    }fit_val;
+	/*
+	 * Handles the paramaters of a first order curve
+	 * f = a*x + b
+	 */
+	struct fit_params {
+		float a;
+		float b;
+	} fit_val;
 
-    /*
-     * Moves all the samples in the buffer one step forward
-     * The oldest sample if thrown away
-     */
-    void shift_buffer(void);
+	/*
+	 * Moves all the samples in the buffer one step forward
+	 * The oldest sample if thrown away
+	 */
+	void shift_buffer();
 
-    /*
-     * Add a new sample at the beginning of the buffer
-     */
-    void add_sample(float sample);
+	/*
+	 * Add a new sample at the beginning of the buffer
+	 */
+	void add_sample(float sample);
 
-    /*
-     * Performs a fit using only the first and last values
-     */
-    void end_fit_FOAW(uint8_t window_size);
+	/*
+	 * Performs a fit using only the first and last values
+	 */
+	void end_fit_FOAW(uint8_t window_size);
 
-    /*
-     * Permorms a fit based on a least squares estimate using all the samples inside the window
-     */
-    void best_fit_FOAW(uint8_t window_size);
-    float fit(void);
+	/*
+	 * Permorms a fit based on a least squares estimate using all the samples inside the window
+	 */
+	void best_fit_FOAW(uint8_t window_size);
+	float fit();
 
 
-    float           _dt;                            // Sample time
-    float           _delta;                         // Noise level parameter 
-    float           _buffer[15];                    // Buffer of samples
-    uint8_t         _nb_samples;                    // Number of samples inside the buffer
-    uint8_t         _last_window_size;              // Size of the window used to compute the derivative
-    static const uint8_t   _max_window_size = 14;   // Maximum possible size of the window
+	float           _dt;                            // Sample time
+	float           _delta;                         // Noise level parameter
+	float           _buffer[15];                    // Buffer of samples
+	uint8_t         _nb_samples;                    // Number of samples inside the buffer
+	uint8_t         _last_window_size;              // Size of the window used to compute the derivative
+	static const uint8_t   _max_window_size = 14;   // Maximum possible size of the window
 };
 
 } // namespace math
