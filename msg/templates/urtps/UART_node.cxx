eProsima/Firmware

UART_node: make 'seq' header info internal
This information is useful for debug, but no need for the caller to
worry about it.

msg/templates/urtps/UART_node.cxx
@@ -240,11 +240,12 @@ int16_t UART_node::readFromUART(char* topic_ID, char out_buffer[], uint32_t buff
 }
 
 
-int16_t UART_node::writeToUART(const char topic_ID, char buffer[], uint32_t length, uint8_t seq)
+int16_t UART_node::writeToUART(const char topic_ID, char buffer[], uint32_t length)
 {
     static struct Header header {
         .marker = {'>','>','>'}
     };
+    static uint8_t seq = 0;
 
     if (m_uart_filestream == -1) return 2;
 
@@ -254,7 +255,7 @@ int16_t UART_node::writeToUART(const char topic_ID, char buffer[], uint32_t leng
 
     int ret = 0;
     header.topic_ID = topic_ID;
-    header.seq = seq;
+    header.seq = seq++;
     header.payload_len = length;
     header.crc_h = (crc >> 8) & 0xff;
     header.crc_l = crc & 0xff;

msg/templates/urtps/UART_node.h
@@ -27,7 +27,7 @@ public:
     int init_uart(const char * uart_name, uint32_t baudrate);
     uint8_t close_uart();
     int16_t readFromUART(char* topic_ID, char out_buffer[], uint32_t max_size);
-    int16_t writeToUART(const char topic_ID, char buffer[], uint32_t length, uint8_t seq);
+    int16_t writeToUART(const char topic_ID, char buffer[], uint32_t length);
 
 protected:
     uint16_t crc16_byte(uint16_t crc, const uint8_t data);

PX4/PX4-Autopilot

msg/templates/urtps/UART_node.cxx
@@ -244,11 +244,12 @@ int16_t UART_node::readFromUART(char* topic_ID, char out_buffer[], size_t buffer
     return ret;
 }
 
-int16_t UART_node::writeToUART(const char topic_ID, char buffer[], uint16_t length, uint8_t seq)
+int16_t UART_node::writeToUART(const char topic_ID, char buffer[], uint16_t length)
 {
     static struct Header header {
         .marker = {'>','>','>'}
     };
+    static uint8_t seq = 0;
 
     if (m_uart_filestream == -1) return 2;
 
@@ -258,7 +259,7 @@ int16_t UART_node::writeToUART(const char topic_ID, char buffer[], uint16_t leng
 
     int ret = 0;
     header.topic_ID = topic_ID;
-    header.seq = seq;
+    header.seq = seq++;
     header.payload_len_h = (length >> 8) & 0xff;
     header.payload_len_l = length & 0xff;
     header.crc_h = (crc >> 8) & 0xff;

msg/templates/urtps/UART_node.h
@@ -27,7 +27,7 @@ public:
     int init_uart(const char * uart_name, uint32_t baudrate);
     uint8_t close_uart();
     int16_t readFromUART(char* topic_ID, char out_buffer[], size_t buffer_size);
-    int16_t writeToUART(const char topic_ID, char buffer[], uint16_t length, uint8_t seq);
+    int16_t writeToUART(const char topic_ID, char buffer[], uint16_t length);
 
 protected:
     uint16_t crc16_byte(uint16_t crc, const uint8_t data);