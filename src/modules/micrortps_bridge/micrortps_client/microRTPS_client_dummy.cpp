d797c08d8dbcc3201c79dac913cccf9a3a4296ed
Mark Charlebois <charlebm@gmail.com>
2017-07-31 14:20:02 -0700

    Fixed copyright on microRTPS_client_dummy.cpp
    Signed-off-by: Mark Charlebois <charlebm@gmail.com>

--- src/modules/micrortps_bridge/micrortps_client/microRTPS_client_dummy.cpp
+++ src/modules/micrortps_bridge/micrortps_client/microRTPS_client_dummy.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- * Copyright 2017 Proyectos y Sistemas de Mantenimiento SL (eProsima).
+ * Copyright 2017 Mark Charlebois.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
