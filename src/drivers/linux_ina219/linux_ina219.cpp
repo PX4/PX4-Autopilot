546c6116b2d9cfecd93decd9cc5a43c60ad2be43
crossa <421395590@qq.com>
2017-08-02 01:16:41 +0800

    修改数据类型

--- src/drivers/linux_ina219/linux_ina219.cpp
+++ src/drivers/linux_ina219/linux_ina219.cpp
@@ -59,8 +59,8 @@ void linux_ina219::usage() {
 //---------------------------------------------------------------------------------------------//
 void linux_ina219::running(int argc,char**argv) {
 	PX4_INFO("持续测量");
-	float v;
-	float sv;
+	double v;
+	double sv;
 	//float a;
 	//a = linux_ina219::ina219->getCurrentMa();
 	//a = 1.0;
