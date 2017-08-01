de02acc9d03486828538de98e53ce70c19cc0841
crossa <421395590@qq.com>
2017-08-01 23:07:29 +0800

    再次提交

--- src/drivers/linux_ina219/linux_ina219.cpp
+++ src/drivers/linux_ina219/linux_ina219.cpp
@@ -63,7 +63,7 @@ void linux_ina219::running(int argc,char**argv) {
 	//float a;
 	//a = linux_ina219::ina219->getCurrentMa();
 	//a = 1.0;
-	v = linux_ina219::ina219->getCurrent();
+	v = linux_ina219::ina219->getBusVoltage();
 	linux_ina219::__battery_status_data.timestamp = hrt_absolute_time(); // required for logger
 	linux_ina219::__battery_status_data.voltage_v = v;
 	linux_ina219::__battery_status_data.voltage_filtered_v = v;
