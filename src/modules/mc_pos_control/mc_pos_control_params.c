bf5e81a34f0f3db0ef68b402e95a13ff7a7e14cf
Dennis Mannhart <dennis.mannhart@gmail.com>
2017-08-07 14:17:41 +0200

    mc_pos_control param: acceleration parameters adjustment and addition

--- src/modules/mc_pos_control/mc_pos_control_params.c
+++ src/modules/mc_pos_control/mc_pos_control_params.c
@@ -402,7 +402,7 @@ PARAM_DEFINE_FLOAT(MPC_HOLD_MAX_Z, 0.6f);
 PARAM_DEFINE_FLOAT(MPC_VELD_LP, 5.0f);
 
 /**
- * Maximum horizonal acceleration in velocity controlled modes
+ * Maximum horizontal acceleration for auto mode and maximum deceleration for manual mode
  *
  * @unit m/s/s
  * @min 2.0
@@ -411,10 +411,10 @@ PARAM_DEFINE_FLOAT(MPC_VELD_LP, 5.0f);
  * @decimal 2
  * @group Multicopter Position Control
  */
-PARAM_DEFINE_FLOAT(MPC_ACC_HOR_MAX, 5.0f);
+PARAM_DEFINE_FLOAT(MPC_ACC_HOR_MAX, 10.0f);
 
 /**
- * Maximum horizonal braking deceleration in velocity controlled modes
+ * Maximum horizontal manual acceleration
  *
  * @unit m/s/s
  * @min 2.0
@@ -423,7 +423,7 @@ PARAM_DEFINE_FLOAT(MPC_ACC_HOR_MAX, 5.0f);
  * @decimal 2
  * @group Multicopter Position Control
  */
-PARAM_DEFINE_FLOAT(MPC_DEC_HOR_MAX, 10.0f);
+PARAM_DEFINE_FLOAT(MPC_ACC_HOR_MAN, 5.0f);
 
 /**
  * Maximum vertical acceleration in velocity controlled modes upward
@@ -435,7 +435,7 @@ PARAM_DEFINE_FLOAT(MPC_DEC_HOR_MAX, 10.0f);
  * @decimal 2
  * @group Multicopter Position Control
  */
-PARAM_DEFINE_FLOAT(MPC_ACC_UP_MAX, 5.0f);
+PARAM_DEFINE_FLOAT(MPC_ACC_UP_MAX, 10.0f);
 
 /**
  * Maximum vertical acceleration in velocity controlled modes down
