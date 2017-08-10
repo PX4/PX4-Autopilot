5782e5c5fd6c55bfa50e22e2e6fe3d51028d7eb9
David Sidrane <david_s5@nscdg.com>
2017-08-10 09:23:38 -1000

    uavcan:Removed PX4FMUv1 support

--- src/modules/uavcan/uavcan_main.cpp
+++ src/modules/uavcan/uavcan_main.cpp
@@ -168,10 +168,7 @@ int UavcanNode::getHardwareVersion(uavcan::protocol::HardwareVersion &hwver)
 	int rv = -1;
 
 	if (UavcanNode::instance()) {
-		if (!std::strncmp(px4_board_name(), "PX4FMU_V1", 9)) {
-			hwver.major = 1;
-
-		} else if (!std::strncmp(px4_board_name(), "PX4FMU_V2", 9)) {
+		if (!std::strncmp(px4_board_name(), "PX4FMU_V2", 9)) {
 			hwver.major = 2;
 
 		} else {
