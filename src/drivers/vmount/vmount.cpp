037c0e4b1870e55c9c20aa1269954351dcc7e870
Nicolas de Palezieux <ndepal@gmail.com>
2017-08-11 11:53:32 +0200

    vmount: reduce stack size to 1900 as recommended by @bkueng (maximum used memory observed: 1552)

--- src/drivers/vmount/vmount.cpp
+++ src/drivers/vmount/vmount.cpp
@@ -444,7 +444,7 @@ int vmount_main(int argc, char *argv[])
 		int vmount_task = px4_task_spawn_cmd("vmount",
 						     SCHED_DEFAULT,
 						     SCHED_PRIORITY_DEFAULT + 40,
-						     2000,
+						     1900,
 						     vmount_thread_main,
 						     (char *const *)argv + 1);
 
