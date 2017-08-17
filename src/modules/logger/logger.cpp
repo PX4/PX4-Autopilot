e63da5860e25f0a2ed6cf038816ac0b8debcd60a
Daniel Agar <daniel@agar.ca>
2017-08-17 09:40:35 -0400

    uORB cleanup Subscription/Publication c++

--- src/modules/logger/logger.cpp
+++ src/modules/logger/logger.cpp
@@ -911,8 +911,7 @@ void Logger::run()
 
 			/* Check if parameters have changed */
 			// this needs to change to a timestamped record to record a history of parameter changes
-			if (parameter_update_sub.check_updated()) {
-				parameter_update_sub.update();
+			if (parameter_update_sub.update()) {
 				write_changed_parameters();
 			}
 
