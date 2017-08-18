25ccb6740ec7ee842bd7564a719e0793e8f58602
Mark Charlebois <charlebm@gmail.com>
2017-08-18 15:25:23 -0700

    Removed support for astyle 2.05.1
    There are bugs in version 2.05.1 (default version in Ubuntu 16.04)
    
    Signed-off-by: Mark Charlebois <charlebm@gmail.com>

--- Tools/check_code_style_all.sh
+++ Tools/check_code_style_all.sh
@@ -2,10 +2,9 @@
 set -eu
 
 # Check for the latest astyle version
-ASTYLE_VER_REQUIRED_1="Artistic Style Version 2.05.1"
-ASTYLE_VER_REQUIRED_2="Artistic Style Version 2.06"
-ASTYLE_VER_REQUIRED_3="Artistic Style Version 3.0"
-ASTYLE_VER_REQUIRED_4="Artistic Style Version 3.0.1"
+ASTYLE_VER_REQUIRED_1="Artistic Style Version 2.06"
+ASTYLE_VER_REQUIRED_2="Artistic Style Version 3.0"
+ASTYLE_VER_REQUIRED_3="Artistic Style Version 3.0.1"
 
 astyle_ver() {
 	echo "PX4 requires at least ${ASTYLE_VER_REQUIRED_1}"
@@ -24,11 +23,10 @@ else
 
 	if [ "$ASTYLE_VER" != "$ASTYLE_VER_REQUIRED_1" -a \
 	     "$ASTYLE_VER" != "$ASTYLE_VER_REQUIRED_2" -a \
-	     "$ASTYLE_VER" != "$ASTYLE_VER_REQUIRED_3" -a \
-	     "$ASTYLE_VER" != "$ASTYLE_VER_REQUIRED_4" ];
+	     "$ASTYLE_VER" != "$ASTYLE_VER_REQUIRED_3" ];
 	then
 	    echo "Error: you're using ${ASTYLE_VER}"
-	    echo "but should be using ${ASTYLE_VER_REQUIRED} instead"
+	    echo "but should be using ${ASTYLE_VER_REQUIRED_1} or later instead"
 	    exit 1
 	fi
 fi
