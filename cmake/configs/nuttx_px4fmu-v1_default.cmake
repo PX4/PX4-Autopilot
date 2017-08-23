fd6499baca56ae78b2b3cbc9e10adbdb539d8caa
Daniel Agar <daniel@agar.ca>
2017-08-22 22:32:47 -0400

    cmake simplify uavcan linking

--- cmake/configs/nuttx_px4fmu-v1_default.cmake
+++ cmake/configs/nuttx_px4fmu-v1_default.cmake
@@ -174,12 +174,6 @@ set(config_io_board
 	px4io-v1
 	)
 
-set(config_extra_libs
-	)
-
-set(config_io_extra_libs
-	)
-
 add_custom_target(sercon)
 set_target_properties(sercon PROPERTIES
 	PRIORITY "SCHED_PRIORITY_DEFAULT"
--- cmake/configs/nuttx_px4fmu-v2_default.cmake
+++ cmake/configs/nuttx_px4fmu-v2_default.cmake
@@ -204,14 +204,6 @@ set(config_io_board
 	px4io-v2
 	)
 
-#set(config_extra_libs
-#	uavcan
-#	uavcan_stm32_driver
-#	)
-
-set(config_io_extra_libs
-	)
-
 add_custom_target(sercon)
 set_target_properties(sercon PROPERTIES
 	PRIORITY "SCHED_PRIORITY_DEFAULT"
