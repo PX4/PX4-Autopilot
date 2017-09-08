963e2d23d8316646d78bd8aef3fd6153f0e8815f
David Sidrane <david_s5@nscdg.com>
2017-09-07 14:03:27 -1000

    sim:Added BOARD_OVERRIDE_PX4_GUID

--- platforms/qurt/include/board_config.h
+++ platforms/qurt/include/board_config.h
@@ -7,6 +7,10 @@
 #define PX4_I2C_BUS_EXPANSION	3
 #define PX4_I2C_BUS_LED		3
 
-#define BOARD_OVERRIDE_UUID "SYSMEMID"
+#define BOARD_OVERRIDE_UUID "QURT        " // must be of length 12 (PX4_CPU_UUID_BYTE_LENGTH)
+#define BOARD_OVERRIDE_PX4_GUID "00000000" BOARD_OVERRIDE_UUID // must be of length 18 (PX4_GUID_BYTE_LENGTH)
+
+#define BOARD_HAS_NO_RESET
+#define BOARD_HAS_NO_BOOTLOADER
 
 #include "../common/board_common.h"
