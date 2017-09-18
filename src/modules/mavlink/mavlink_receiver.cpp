faf05a1ff41d70f95024d88675c8febfa0b6a1ab
TSC21 <n.marques21@hotmail.com>
2017-09-18 21:55:32 +0100

    mavlink_receiver: heartbeat: accept heartbeat of MAV_TYPE_ONBOARD_CONTROLLER

--- src/modules/mavlink/mavlink_receiver.cpp
+++ src/modules/mavlink/mavlink_receiver.cpp
@@ -1854,9 +1854,8 @@ MavlinkReceiver::handle_message_heartbeat(mavlink_message_t *msg)
 		mavlink_heartbeat_t hb;
 		mavlink_msg_heartbeat_decode(msg, &hb);
 
-		/* ignore own heartbeats, accept only heartbeats from GCS */
-		if (msg->sysid != mavlink_system.sysid && hb.type == MAV_TYPE_GCS) {
-
+		/* ignore own heartbeats, accept only heartbeats from GCS or Onboard computers */
+		if (msg->sysid != mavlink_system.sysid && (hb.type == MAV_TYPE_GCS || hb.type == MAV_TYPE_ONBOARD_CONTROLLER)) {
 			struct telemetry_status_s &tstatus = _mavlink->get_rx_status();
 
 			/* set heartbeat time and topic time and publish -
