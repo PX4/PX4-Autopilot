9ad0ca24f701d8d478b7709cd154efc4b725bec1
José Roberto de Souza <jose.souza@intel.com>
2017-09-19 14:24:55 -0700

    cmake: AeroFC: Enable UAVCAN

--- cmake/configs/nuttx_aerofc-v1_default.cmake
+++ cmake/configs/nuttx_aerofc-v1_default.cmake
@@ -2,7 +2,7 @@ include(nuttx/px4_impl_nuttx)
 
 px4_nuttx_configure(HWCLASS m4 CONFIG nsh ROMFS y ROMFSROOT px4fmu_common)
 
-set(config_uavcan_num_ifaces 2)
+set(config_uavcan_num_ifaces 1)
 
 set(config_module_list
 	#
@@ -46,6 +46,7 @@ set(config_module_list
 	modules/load_mon
 	modules/navigator
 	modules/mavlink
+	modules/uavcan
 	modules/land_detector
 
 	#
