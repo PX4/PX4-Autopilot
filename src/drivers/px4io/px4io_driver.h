4e6ca271e73ee799e6f74e49b23a2c13f23707cf
Daniel Agar <daniel@agar.ca>
2017-09-20 00:04:23 -0400

    more px4fmu-v1 cleanup (#7981)

--- src/drivers/px4io/px4io_driver.h
+++ src/drivers/px4io/px4io_driver.h
@@ -41,10 +41,6 @@
 
 #include <board_config.h>
 
-#ifdef PX4_I2C_OBDEV_PX4IO
-device::Device	*PX4IO_i2c_interface();
-#endif
-
 #ifdef PX4IO_SERIAL_BASE
 device::Device	*PX4IO_serial_interface();
 #endif
