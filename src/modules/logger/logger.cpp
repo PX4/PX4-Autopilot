37cb7642128ea379726659e4fedc46b539933aae
Daniel Agar <daniel@agar.ca>
2017-09-20 00:37:00 -0400

    logger add telemetry_status

--- src/modules/logger/logger.cpp
+++ src/modules/logger/logger.cpp
@@ -557,6 +557,7 @@ void Logger::add_common_topics()
 	add_topic("system_power", 500);
 	add_topic("tecs_status", 200);
 	add_topic("telemetry_status");
+	add_topic("telemetry_status");
 	add_topic("vehicle_attitude", 30);
 	add_topic("vehicle_attitude_setpoint", 100);
 	add_topic("vehicle_command");
