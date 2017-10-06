180b2cd13c0fc3e9b01f3930fec582d756d36e35
Daniel Agar <daniel@agar.ca>
2017-10-06 19:16:45 -0400

    sensors param_find SYS_AUTOCONFIG/SYS_AUTOSTART for QGC

--- src/modules/sensors/parameters.cpp
+++ src/modules/sensors/parameters.cpp
@@ -178,6 +178,9 @@ void initialize_parameter_handles(ParameterHandles &parameter_handles)
 	(void)param_find("CAL_MAG_SIDES");
 
 	(void)param_find("RC_CHAN_CNT");
+
+	(void)param_find("SYS_AUTOCONFIG");
+	(void)param_find("SYS_AUTOSTART");
 }
 
 int update_parameters(const ParameterHandles &parameter_handles, Parameters &parameters)
