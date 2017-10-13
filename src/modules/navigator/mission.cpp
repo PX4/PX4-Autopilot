virenbajaj/Firmware

add new navigator_item struct (will replace mission_item)

src/modules/navigator/mission.cpp
@@ -512,7 +512,9 @@ Mission::set_mission_items()
 
 	/*********************************** handle mission item *********************************************/
 
-	// convert mission item to local navigation item
+	// convert mission_item to local navigation_item
+	//TODO use _navigation_item instead of _mission_item
+	_navigator->mission_item_to_navigation_item(&_navigation_item, &_mission_item);
 	_navigator->global_to_local(&_mission_item);
 
 	/* handle position mission items */

src/modules/navigator/navigator.h
@@ -235,6 +235,8 @@ public:
 
 	void 		global_to_local(struct mission_item_s *item);
 
+	void 		mission_item_to_navigation_item(struct navigator_item_s *nav_item, struct mission_item_s *mission_item);
+
 private:
 
 	bool		_task_should_exit{false};	/**< if true, sensor task should exit */

PX4/PX4-Autopilot

src/modules/navigator/mission.cpp
@@ -540,7 +540,9 @@ Mission::set_mission_items()
 
 	/*********************************** handle mission item *********************************************/
 
-	// convert mission item to local navigation item
+	// convert mission_item to local navigator_item
+	//TODO use _navigator_item instead of _mission_item
+	_navigator->mission_item_to_navigator_item(&_navigator_item, &_mission_item);
 	_navigator->global_to_local(&_mission_item);
 
 	/* handle position mission items */

src/modules/navigator/navigator.h
@@ -237,6 +237,8 @@ public:
 
 	void 		global_to_local(struct mission_item_s *item);
 
+	void 		mission_item_to_navigator_item(struct navigator_item_s *nav_item, struct mission_item_s *mission_item);
+
 private:
 
 	bool		_task_should_exit{false};	/**< if true, sensor task should exit */