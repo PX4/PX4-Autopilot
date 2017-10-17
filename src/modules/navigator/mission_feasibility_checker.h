787931f04fbd08e9526588f1abada76066585a19
Julian Oes <julian@oes.ch>
2017-10-17 14:12:26 -0400

    navigator: check distances between waypoints
    Instead of just checking whether the first waypoint is too far away from
    home it makes sense to also check between waypoints.
    This can prevent
    - flyaways due to user errors, or
    - catch the corner case where a takeoff waypoint is added to a mission
      and therefore the first waypoint is not too far away, however, the
      subsequent waypoints are still too far away.

--- src/modules/navigator/mission_feasibility_checker.h
+++ src/modules/navigator/mission_feasibility_checker.h
@@ -62,8 +62,9 @@ private:
 
 	bool checkMissionItemValidity(dm_item_t dm_current, size_t nMissionItems, bool condition_landed);
 
-	bool check_dist_1wp(dm_item_t dm_current, size_t nMissionItems, double curr_lat, double curr_lon,
-			    float dist_first_wp, bool &warning_issued);
+	bool checkDistanceToFirstWaypoint(dm_item_t dm_current, size_t nMissionItems, float max_distance, bool &warning_issued);
+	bool checkDistancesBetweenWaypoints(dm_item_t dm_current, size_t nMissionItems, float max_distance,
+					    bool &warning_issued);
 
 	/* Checks specific to fixedwing airframes */
 	bool checkFixedwing(dm_item_t dm_current, size_t nMissionItems, fw_pos_ctrl_status_s *fw_pos_ctrl_status,
@@ -88,7 +89,9 @@ public:
 	/*
 	 * Returns true if mission is feasible and false otherwise
 	 */
-	bool checkMissionFeasible(const mission_s &mission, float max_waypoint_distance, bool land_start_req);
+	bool checkMissionFeasible(const mission_s &mission,
+				  float max_distance_to_1st_waypoint, float max_distance_between_waypoints,
+				  bool land_start_req);
 
 };
 
