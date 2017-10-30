090b6680752ccb348e082d5e46a5e3d3dd95c3c0
davidaroyer <dave@aerotenna.com>
2017-10-30 11:14:41 -0500

    cmake: add missing vehicle control modules to ocpoc

--- cmake/configs/posix_ocpoc_cross.cmake
+++ cmake/configs/posix_ocpoc_cross.cmake
@@ -49,6 +49,9 @@ set(config_module_list
 	#
 	modules/mc_att_control
 	modules/mc_pos_control
+	modules/vtol_att_control
+	modules/fw_att_control
+	modules/fw_pos_control_l1
 
 	#
 	# Library modules
