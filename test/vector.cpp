3bd94fcd6f65f2d55c3bbce410657eeba5577336
Matthias Grob <maetugr@gmail.com>
2017-11-05 12:14:37 +0100

    Test vector: structured & commented, added normalize and unit_or_zero tests, removed duplicate data preparation

--- test/vector.cpp
+++ test/vector.cpp
@@ -6,25 +6,34 @@ using namespace matrix;
 
 int main()
 {
+    // test data
     float data1[] = {1,2,3,4,5};
     float data2[] = {6,7,8,9,10};
     Vector<float, 5> v1(data1);
-    TEST(isEqualF(v1.norm(), 7.416198487095663f));
-    TEST(isEqualF(v1.norm(), v1.length()));
     Vector<float, 5> v2(data2);
-    TEST(isEqualF(v1.dot(v2), 130.0f));
-    v2.normalize();
+
+    // copy constructor
     Vector<float, 5> v3(v2);
     TEST(isEqual(v2, v3));
+
+    // norm, dot product
+    TEST(isEqualF(v1.norm(), 7.416198487095663f));
+    TEST(isEqualF(v1.norm(), v1.length()));
+    TEST(isEqualF(v1.dot(v2), 130.0f));
+    TEST(isEqualF(v1.dot(v2), v1 * v2));
+
+    // unit, unit_zero, normalize
+    TEST(isEqualF(v2.unit().norm(), 1.f));
+    TEST(isEqualF(v2.unit_or_zero().norm(), 1.f));
+    TEST(isEqualF(Vector<float, 5>().unit_or_zero().norm(), 0.f));
+    v2.normalize();
+    TEST(isEqualF(v2.norm(), 1.f));
+
+    // power
     float data1_sq[] = {1,4,9,16,25};
     Vector<float, 5> v4(data1_sq);
     TEST(isEqual(v1, v4.pow(0.5)));
 
-    // dot product operator
-    v1 = Vector<float, 5>(data1);
-    v2 = Vector<float, 5>(data2);
-    float dprod = v1 * v2;
-    TEST(isEqualF(dprod, 130.0f));
     return 0;
 }
 
