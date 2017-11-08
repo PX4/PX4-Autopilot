e193240e980b9ea7f4c42f57a7abce25a803e516
Matthias Grob <maetugr@gmail.com>
2017-11-08 22:31:14 +0100

    FlightTask: fix CI: static field requiring definition in cpp file, errors with clang linker (CI) but not with GCC

--- src/lib/FlightTasks/tasks/FlightTask.cpp
+++ src/lib/FlightTasks/tasks/FlightTask.cpp
@@ -1,10 +1,12 @@
 #include "FlightTask.hpp"
 #include <mathlib/mathlib.h>
 
+constexpr uint64_t FlightTask::_timeout;
+
 int FlightTask::update()
 {
 	_time = hrt_elapsed_time(&_starting_time_stamp) / 1e6f;
-	_deltatime  = math::min((int)hrt_elapsed_time(&_last_time_stamp), _timeout) / 1e6f;
+	_deltatime  = math::min(hrt_elapsed_time(&_last_time_stamp), _timeout) / 1e6f;
 	_last_time_stamp = hrt_absolute_time();
 	updateSubscriptions();
 	_evaluate_vehicle_position();
