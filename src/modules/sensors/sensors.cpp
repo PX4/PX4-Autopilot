e071b1a60fef06791a1822b74725f2ae8d0ebc47
Roman <bapstr@ethz.ch>
2017-11-12 12:32:41 -0500

    sensors: subscribe to attitude and local position topic
    Signed-off-by: Roman <bapstr@ethz.ch>

--- src/modules/sensors/sensors.cpp
+++ src/modules/sensors/sensors.cpp
@@ -721,6 +721,14 @@ Sensors::run()
 
 	_actuator_ctrl_0_sub = orb_subscribe(ORB_ID(actuator_controls_0));
 
+	_vehicle_attitude_sub = orb_subscribe(ORB_ID(vehicle_attitude));
+
+	_vehicle_local_position_sub = orb_subscribe(ORB_ID(vehicle_local_position));
+
+	for (int b = 0; b < BOARD_NUMBER_BRICKS; b++) {
+		_battery[b].reset(&_battery_status[b]);
+	}
+
 	/* get a set of initial values */
 	_voted_sensors_update.sensors_poll(raw);
 
