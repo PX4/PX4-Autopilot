ea223d7fa27770631cee2683abe6679d920a81c8
Daniel Agar <daniel@agar.ca>
2017-11-21 20:22:48 -0500

    cmake handle git submodule depdencies

--- src/firmware/posix/sitl_target.cmake
+++ src/firmware/posix/sitl_target.cmake
@@ -1,6 +1,6 @@
 
 function(px4_add_sitl_app)
-px4_parse_function_args(NAME px4_add_sitl_app
+	px4_parse_function_args(NAME px4_add_sitl_app
 			ONE_VALUE APP_NAME MAIN_SRC UPLOAD_NAME
 			REQUIRED APP_NAME MAIN_SRC
 			ARGN ${ARGN}
@@ -56,10 +56,13 @@ add_custom_target(run_config
 			${config_sitl_model}
 			${PX4_SOURCE_DIR}
 			${PX4_BINARY_DIR}
-			WORKING_DIRECTORY ${SITL_WORKING_DIR}
-			USES_TERMINAL
+		WORKING_DIRECTORY ${SITL_WORKING_DIR}
+		USES_TERMINAL
 		DEPENDS px4 logs_symlink
 		)
+
+px4_add_git_submodule(TARGET git_gazebo PATH "${PX4_SOURCE_DIR}/Tools/sitl_gazebo")
+px4_add_git_submodule(TARGET git_jmavsim PATH "${PX4_SOURCE_DIR}/Tools/jMAVSim")
 
 # Add support for external project building
 include(ExternalProject)
@@ -70,13 +73,16 @@ ExternalProject_Add(sitl_gazebo
 	CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
 	BINARY_DIR ${PX4_BINARY_DIR}/build_gazebo
 	INSTALL_COMMAND ""
+	DEPENDS
+		git_gazebo
 	)
 set_target_properties(sitl_gazebo PROPERTIES EXCLUDE_FROM_ALL TRUE)
 
 ExternalProject_Add_Step(sitl_gazebo forceconfigure
 	DEPENDEES update
 	DEPENDERS configure
-	ALWAYS 1)
+	ALWAYS 1
+	)
 
 # create targets for each viewer/model/debugger combination
 set(viewers none jmavsim gazebo replay)
@@ -123,9 +129,10 @@ foreach(viewer ${viewers})
 						${model}
 						${PX4_SOURCE_DIR}
 						${PX4_BINARY_DIR}
-						WORKING_DIRECTORY ${SITL_WORKING_DIR}
-						USES_TERMINAL
-					DEPENDS logs_symlink
+					WORKING_DIRECTORY ${SITL_WORKING_DIR}
+					USES_TERMINAL
+					DEPENDS
+						logs_symlink
 					)
 			list(APPEND all_posix_vmd_make_targets ${_targ_name})
 			if (viewer STREQUAL "gazebo")
@@ -133,6 +140,8 @@ foreach(viewer ${viewers})
 				if (viewer STREQUAL "gazebo")
 					add_dependencies(${_targ_name} px4_${model})
 				endif()
+			elseif(viewer STREQUAL "jmavsim")
+				add_dependencies(${_targ_name} git_jmavsim)
 			endif()
 		endforeach()
 	endforeach()
