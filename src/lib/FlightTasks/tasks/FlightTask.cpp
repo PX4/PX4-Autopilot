b6001a1d1a28485cc7352f9ad68d3460b34404df
Matthias Grob <maetugr@gmail.com>
2017-11-28 18:17:39 +0100

    FlightTasks: small refactor for review comments

--- src/lib/FlightTasks/tasks/FlightTask.cpp
+++ src/lib/FlightTasks/tasks/FlightTask.cpp
@@ -26,8 +26,7 @@ int FlightTask::update()
 	_time = (_time_stamp_current - _time_stamp_activate) / 1e6f;
 	_deltatime  = math::min((_time_stamp_current - _time_stamp_last), _timeout) / 1e6f;
 	_time_stamp_last = _time_stamp_current;
-	int ret = _evaluate_vehicle_position();
-	return ret;
+	return _evaluate_vehicle_position();
 }
 
 int FlightTask::_evaluate_vehicle_position()
