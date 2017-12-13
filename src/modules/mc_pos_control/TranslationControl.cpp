6e83eb1b3f8ddb21e2875507045d3994ac7a4090
Dennis Mannhart <dennis.mannhart@gmail.com>
2017-12-13 18:37:29 +0100

    TranslationControl: new class that generates thrust vector from state and setpoints

---
+++ src/modules/mc_pos_control/TranslationControl.cpp
@@ -0,0 +1,66 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2017 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/**
+ * @file TranslationControl.cpp
+ *
+ */
+
+#include "TranslationControl.hpp"
+#include <float.h>
+#include <mathlib/mathlib.h>
+
+using namespace matrix;
+
+using Data = matrix::Vector3f;
+
+TranslationControl::TranslationControl()
+{
+
+};
+
+void TranslationControl::updateState(struct vehicle_local_position_s state)
+{
+	_pos = Data(&state.x);
+	_vel = Data(&state.vx);
+	_pos.print();
+}
+
+void TranslationControl::updateSetpoint(struct vehicle_local_position_setpoint_s setpoint)
+{
+	_pos_sp = Data(&setpoint.x);
+	_vel_sp = Data(&setpoint.vx);
+	_acc_sp = Data(&setpoint.acc_x);
+	_yaw_sp = setpoint.yaw;
+	_yawspeed_sp = setpoint.yawspeed;
+}
