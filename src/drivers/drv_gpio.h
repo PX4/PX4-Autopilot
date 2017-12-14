125d2da62069d3d7c0c515c39722cc90d6f78a2a
Daniel Agar <daniel@agar.ca>
2017-12-14 12:20:52 -0500

    delete unused GPIO_SET_OUTPUT_HIGH

--- src/drivers/drv_gpio.h
+++ src/drivers/drv_gpio.h
@@ -79,7 +79,4 @@
 
 #define GPIO_PERIPHERAL_RAIL_RESET	GPIOC(14)
 
-/** configure the board GPIOs in (arg) as outputs, initially high */
-#define GPIO_SET_OUTPUT_HIGH	GPIOC(16)
-
 #endif /* _DRV_GPIO_H */
