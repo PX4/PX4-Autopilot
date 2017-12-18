5c8b22b43fabd42766031ecaac9130531a85e367
CarlOlsson <carlolsson.co@gmail.com>
2017-12-18 15:36:28 +0100

    ekf: return beta test ratio in get_innovation_test_status
    Signed-off-by: CarlOlsson <carlolsson.co@gmail.com>

--- EKF/ekf.h
+++ EKF/ekf.h
@@ -224,7 +224,7 @@ public:
 	// Innovation Test Ratios - these are the ratio of the innovation to the acceptance threshold.
 	// A value > 1 indicates that the sensor measurement has exceeded the maximum acceptable level and has been rejected by the EKF
 	// Where a measurement type is a vector quantity, eg magnetoemter, GPS position, etc, the maximum value is returned.
-	void get_innovation_test_status(uint16_t *status, float *mag, float *vel, float *pos, float *hgt, float *tas, float *hagl);
+	void get_innovation_test_status(uint16_t *status, float *mag, float *vel, float *pos, float *hgt, float *tas, float *hagl, float *beta);
 
 	// return a bitmask integer that describes which state estimates can be used for flight control
 	void get_ekf_soln_status(uint16_t *status);
