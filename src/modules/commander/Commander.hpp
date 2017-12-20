294fbc46a94de6027e5c1f161a8406bc530153bc
Daniel Agar <daniel@agar.ca>
2017-12-20 15:39:20 -0500

    commander initial class structure

---
+++ src/modules/commander/Commander.hpp
@@ -0,0 +1,102 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2017 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#ifndef COMMANDER_HPP_
+#define COMMANDER_HPP_
+
+#include <controllib/blocks.hpp>
+#include <px4_module.h>
+
+// publications
+#include <uORB/topics/actuator_armed.h>
+#include <uORB/topics/home_position.h>
+#include <uORB/topics/vehicle_command_ack.h>
+#include <uORB/topics/vehicle_control_mode.h>
+#include <uORB/topics/vehicle_status.h>
+#include <uORB/topics/vehicle_status_flags.h>
+#include <uORB/Publication.hpp>
+
+// subscriptions
+#include <uORB/topics/geofence_result.h>
+#include <uORB/topics/safety.h>
+#include <uORB/topics/vehicle_attitude.h>
+#include <uORB/topics/vehicle_command.h>
+#include <uORB/topics/vehicle_global_position.h>
+#include <uORB/topics/vehicle_local_position.h>
+#include <uORB/Subscription.hpp>
+
+using control::BlockParamFloat;
+using control::BlockParamInt;
+using uORB::Publication;
+using uORB::Subscription;
+
+class Commander : public control::SuperBlock, public ModuleBase<Commander>
+{
+public:
+	Commander() :
+		SuperBlock(nullptr, "COM")
+	{
+		updateParams();
+	}
+
+	/** @see ModuleBase */
+	static int task_spawn(int argc, char *argv[]);
+
+	/** @see ModuleBase */
+	static Commander *instantiate(int argc, char *argv[]);
+
+	/** @see ModuleBase */
+	static int custom_command(int argc, char *argv[]);
+
+	/** @see ModuleBase */
+	static int print_usage(const char *reason = nullptr);
+
+	/** @see ModuleBase::run() */
+	void run() override;
+
+	void enable_hil();
+
+private:
+
+	bool handle_command(vehicle_status_s *status, const safety_s *safety, vehicle_command_s *cmd,
+			    actuator_armed_s *armed, home_position_s *home, vehicle_global_position_s *global_pos,
+			    vehicle_local_position_s *local_pos, vehicle_attitude_s *attitude, orb_advert_t *home_pub,
+			    orb_advert_t *command_ack_pub, bool *changed);
+
+	bool set_home_position(orb_advert_t &homePub, home_position_s &home,
+				const vehicle_local_position_s &localPosition, const vehicle_global_position_s &globalPosition,
+				const vehicle_attitude_s &attitude, bool set_alt_only_to_lpos_ref);
+
+};
+
+#endif /* COMMANDER_HPP_ */
