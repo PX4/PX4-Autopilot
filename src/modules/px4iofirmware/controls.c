7a52b69960974439880b0a6b86717bf75daa84aa
Lorenz Meier <lorenz@px4.io>
2017-12-31 12:16:33 +0100

    IO Firmware: Document override behavior for manual override.

--- src/modules/px4iofirmware/controls.c
+++ src/modules/px4iofirmware/controls.c
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2012-2014 PX4 Development Team. All rights reserved.
+ *   Copyright (c) 2012-2017 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -35,6 +35,8 @@
  * @file controls.c
  *
  * R/C inputs and servo outputs.
+ *
+ * @author Lorenz Meier <lorenz@px4.io>
  */
 
 #include <px4_config.h>
@@ -492,11 +494,16 @@ controls_tick()
 	 *
 	 * Firstly, manual override must be enabled, RC input available and a mixer loaded.
 	 */
-	if ((r_setup_arming & PX4IO_P_SETUP_ARMING_MANUAL_OVERRIDE_OK) &&
-	    (r_status_flags & PX4IO_P_STATUS_FLAGS_RC_OK) &&
-	    !(r_raw_rc_flags & PX4IO_P_RAW_RC_FLAGS_FAILSAFE) &&
-	    !(r_setup_arming & PX4IO_P_SETUP_ARMING_RC_HANDLING_DISABLED) &&
-	    (r_status_flags & PX4IO_P_STATUS_FLAGS_MIXER_OK)) {
+	if (/* condition 1: Override is always allowed */
+		(r_setup_arming & PX4IO_P_SETUP_ARMING_MANUAL_OVERRIDE_OK) &&
+		/* condition 2: We have valid RC control inputs from the user */
+		(r_status_flags & PX4IO_P_STATUS_FLAGS_RC_OK) &&
+		/* condition 3: The system didn't go already into failsafe mode with fixed outputs */
+		!(r_raw_rc_flags & PX4IO_P_RAW_RC_FLAGS_FAILSAFE) &&
+		/* condition 4: RC handling wasn't generally disabled */
+		!(r_setup_arming & PX4IO_P_SETUP_ARMING_RC_HANDLING_DISABLED) &&
+		/* condition 5: We have a valid mixer to map RC inputs to actuator outputs */
+		(r_status_flags & PX4IO_P_STATUS_FLAGS_MIXER_OK)) {
 
 		bool override = false;
 
