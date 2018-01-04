7ae3884944bc82cbdede9cf054062de732467c73
Nicolas de Palezieux <ndepal@gmail.com>
2018-01-04 10:15:53 +0100

    heading towards ROI: do not require setting the MIS_YAWMODE param to enable multicopters yawing towards ROIs
    ROI yaw control: configure yawing capability of mount in vmount parameters.
    If configured that mount has no yaw control, vehicle will yaw towards ROI, irrespective of MIS_YAWMODE.
    Vehicle will behave according to MIS_YAWMODE when there is no ROI.

--- src/modules/navigator/mission_params.c
+++ src/modules/navigator/mission_params.c
@@ -130,7 +130,6 @@ PARAM_DEFINE_INT32(MIS_ALTMODE, 1);
  * @value 1 Heading towards waypoint
  * @value 2 Heading towards home
  * @value 3 Heading away from home
- * @value 4 Heading towards ROI
  * @group Mission
  */
 PARAM_DEFINE_INT32(MIS_YAWMODE, 1);
