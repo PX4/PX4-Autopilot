447aa134db76354e6cd342481d4847075128f5de
stmoon <munhoney@gmail.com>
2018-01-06 20:34:58 +0900

    change mavlink version (1.0->2.0) for simulation

--- src/modules/simulator/simulator.h
+++ src/modules/simulator/simulator.h
@@ -59,8 +59,8 @@
 #include <uORB/uORB.h>
 #include <uORB/topics/optical_flow.h>
 #include <uORB/topics/distance_sensor.h>
-#include <v1.0/mavlink_types.h>
-#include <v1.0/common/mavlink.h>
+#include <v2.0/mavlink_types.h>
+#include <v2.0/common/mavlink.h>
 #include <geo/geo.h>
 namespace simulator
 {
@@ -392,7 +392,8 @@ private:
 	void pollForMAVLinkMessages(bool publish, int udp_port);
 
 	void pack_actuator_message(mavlink_hil_actuator_controls_t &actuator_msg, unsigned index);
-	void send_mavlink_message(const uint8_t msgid, const void *msg, uint8_t component_ID);
+    //void send_mavlink_message(const uint8_t msgid, const void *msg, uint8_t component_ID)
+	void send_mavlink_message(const mavlink_message_t &aMsg);
 	void update_sensors(mavlink_hil_sensor_t *imu);
 	void update_gps(mavlink_hil_gps_t *gps_sim);
 	void parameters_update(bool force);
