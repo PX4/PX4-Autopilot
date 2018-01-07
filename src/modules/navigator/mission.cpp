f5cf3cd9e2263bbcbfc8b455a4b5085976d646af
Lorenz Meier <lorenz@px4.io>
2018-01-07 22:59:31 +0100

    Navigator: Move mission param into mission params
    This is important for the compile scope.

--- src/modules/navigator/mission.cpp
+++ src/modules/navigator/mission.cpp
@@ -64,7 +64,8 @@ Mission::Mission(Navigator *navigator, const char *name) :
 	_param_dist_1wp(this, "MIS_DIST_1WP", false),
 	_param_dist_between_wps(this, "MIS_DIST_WPS", false),
 	_param_altmode(this, "MIS_ALTMODE", false),
-	_param_yawmode(this, "MIS_YAWMODE", false)
+	_param_yawmode(this, "MIS_YAWMODE", false),
+	_param_mnt_yaw_ctl(this, "MIS_MNT_YAW_CTL", false)
 {
 }
 
