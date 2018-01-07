7888acf0878f9048e4007dd40b78c64a75a4c55e
Lorenz Meier <lorenz@px4.io>
2018-01-07 16:35:38 +0100

    Reboot command: Documentation fix
    Pure comment edit

--- src/systemcmds/reboot/reboot.c
+++ src/systemcmds/reboot/reboot.c
@@ -1,7 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (C) 2012 PX4 Development Team. All rights reserved.
- *   Author: @author Lorenz Meier <lm@inf.ethz.ch>
+ *   Copyright (c) 2012-2018 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -35,6 +34,8 @@
 /**
  * @file reboot.c
  * Tool similar to UNIX reboot command
+ *
+ * @author Lorenz Meier <lorenz@px4.io>
  */
 
 #include <px4_config.h>
