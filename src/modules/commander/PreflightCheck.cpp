b6a1dba224d58cf65aa8e505aa9eb4a07d31c6cd
Daniel Agar <daniel@agar.ca>
2018-01-07 17:57:16 -0500

    commander ekf2Check respect reportFailures
     - fixes #8621

--- src/modules/commander/PreflightCheck.cpp
+++ src/modules/commander/PreflightCheck.cpp
@@ -795,7 +795,7 @@ bool preflightCheck(orb_advert_t *mavlink_log_pub, bool checkSensors, bool check
 		// don't report ekf failures for the first 10 seconds to allow time for the filter to start
 		bool report_ekf_fail = (time_since_boot > 10 * 1000000);
 
-		if (!ekf2Check(mavlink_log_pub, true, (report_ekf_fail && !failed), checkGNSS)) {
+		if (!ekf2Check(mavlink_log_pub, true, reportFailures && report_ekf_fail && !failed, checkGNSS)) {
 			failed = true;
 		}
 	}
