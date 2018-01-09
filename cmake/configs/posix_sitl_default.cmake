ee2dcaf5ed5a02aa677130fd04b106603376e505
belaidH <34583606+belaidH@users.noreply.github.com>
2018-01-09 15:19:51 +0100

    Update posix_sitl_default.cmake

--- cmake/configs/posix_sitl_default.cmake
+++ cmake/configs/posix_sitl_default.cmake
@@ -1,20 +1,20 @@
-include(posix/px4_impl_posix)
 
 set(CMAKE_TOOLCHAIN_FILE ${PX4_SOURCE_DIR}/cmake/toolchains/Toolchain-native.cmake)
 
 set(config_module_list
+	drivers/airspeed
 	drivers/boards
 	drivers/camera_trigger
 	drivers/device
-	drivers/gps
-	drivers/pwm_out_sim
-	drivers/vmount
-	drivers/linux_gpio
-	drivers/airspeed
 	drivers/ets_airspeed
+	drivers/gps
+	drivers/linux_gpio
+	drivers/ll40ls
 	drivers/ms4525_airspeed
 	drivers/ms5525_airspeed
+	drivers/pwm_out_sim
 	drivers/sdp3x_airspeed
+	drivers/vmount
 
 	modules/sensors
 	platforms/posix/drivers/accelsim
