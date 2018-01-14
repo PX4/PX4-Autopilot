01868f173bcb8d6a7125dc7f58d9cd18f1d64d20
Nicolas de Palezieux <ndepal@gmail.com>
2018-01-14 23:23:33 +0530

    logger : add support for logging precision landing messages

--- src/modules/logger/logger.cpp
+++ src/modules/logger/logger.cpp
@@ -593,6 +593,7 @@ void Logger::add_default_topics()
 	add_topic("estimator_status", 200);
 	add_topic("home_position");
 	add_topic("input_rc", 200);
+	add_topic("landing_target_pose");
 	add_topic("manual_control_setpoint", 200);
 	add_topic("mission");
 	add_topic("mission_result");
