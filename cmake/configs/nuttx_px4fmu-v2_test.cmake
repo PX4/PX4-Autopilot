0e3574c44f47653a8ad60611257dc1c659f95e71
Daniel Agar <daniel@agar.ca>
2018-01-15 15:18:46 -0500

    hott automatically include hott_sensors and hott_telemetry

--- cmake/configs/nuttx_px4fmu-v2_test.cmake
+++ cmake/configs/nuttx_px4fmu-v2_test.cmake
@@ -27,8 +27,6 @@ set(config_module_list
 	drivers/gps
 	#drivers/pwm_out_sim
 	#drivers/hott
-	#drivers/hott/hott_telemetry
-	#drivers/hott/hott_sensors
 	drivers/blinkm
 	drivers/airspeed
 	drivers/barometer
