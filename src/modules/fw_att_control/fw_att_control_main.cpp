f581b00dac18a4599724280508b7b1210557ff34
Daniel Agar <daniel@agar.ca>
2018-02-06 23:59:50 -0500

    FW controllers initialize vtol_type to a non-valid value
     - tailsitter is vtol type 0

--- src/modules/fw_att_control/fw_att_control_main.cpp
+++ src/modules/fw_att_control/fw_att_control_main.cpp
@@ -456,6 +456,9 @@ FixedwingAttitudeControl::FixedwingAttitudeControl() :
 
 	_parameter_handles.bat_scale_en = param_find("FW_BAT_SCALE_EN");
 
+	// initialize to invalid VTOL type
+	_parameters.vtol_type = -1;
+
 	/* fetch initial parameter values */
 	parameters_update();
 }
