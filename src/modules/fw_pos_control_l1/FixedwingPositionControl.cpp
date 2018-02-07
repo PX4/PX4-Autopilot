8b63dbecde587317cc3399f663e7590df233f9eb
Daniel Agar <daniel@agar.ca>
2018-02-06 23:59:50 -0500

    FW controllers initialize vtol_type to a non-valid value
     - tailsitter is vtol type 0

--- src/modules/fw_pos_control_l1/FixedwingPositionControl.cpp
+++ src/modules/fw_pos_control_l1/FixedwingPositionControl.cpp
@@ -94,6 +94,9 @@ FixedwingPositionControl::FixedwingPositionControl() :
 	_parameter_handles.heightrate_ff =			param_find("FW_T_HRATE_FF");
 	_parameter_handles.speedrate_p =			param_find("FW_T_SRATE_P");
 
+	// initialize to invalid vtol type
+	_parameters.vtol_type = -1;
+
 	/* fetch initial parameter values */
 	parameters_update();
 }
