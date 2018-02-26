7b5266b54e7495eb067e6376231f0aec7052ecfc
Matthias Grob <maetugr@gmail.com>
2018-02-26 13:40:59 +0100

    FlightTasks: made a FlightTask inherit from SuperBlock to have Block::Subscription s FlightTask Manual: subscription made setpoint conversion according to vehicle attitude work

--- src/lib/FlightTasks/FlightTasks.hpp
+++ src/lib/FlightTasks/FlightTasks.hpp
@@ -48,7 +48,10 @@
 class FlightTasks
 {
 public:
-	FlightTasks() {};
+	FlightTasks(control::SuperBlock *parent) :
+		Manual(parent, "MAN"),
+		Orbit(parent, "ORB")
+	{};
 	~FlightTasks() {};
 
 	/**
