d1dca4e74c0c22fa58d23e5bbf58d029ee1c5b19
Daniel Agar <daniel@agar.ca>
2018-02-28 18:07:49 -0500

    fw_pos_ctrl_l1 move takeoff to control_takeoff()

--- src/modules/fw_pos_control_l1/FixedwingPositionControl.hpp
+++ src/modules/fw_pos_control_l1/FixedwingPositionControl.hpp
@@ -439,7 +439,8 @@ private:
 
 	bool		control_position(const math::Vector<2> &curr_pos, const math::Vector<2> &ground_speed,
 					 const position_setpoint_s &pos_sp_prev, const position_setpoint_s &pos_sp_curr);
-
+	void		control_takeoff(const math::Vector<2> &curr_pos, const math::Vector<2> &ground_speed,
+					const position_setpoint_s &pos_sp_prev, const position_setpoint_s &pos_sp_curr);
 	void		control_landing(const math::Vector<2> &curr_pos, const math::Vector<2> &ground_speed,
 					const position_setpoint_s &pos_sp_prev, const position_setpoint_s &pos_sp_curr);
 
