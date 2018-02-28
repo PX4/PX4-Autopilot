57be487ed09732e1a7f057339391480805df2585
Matthias Grob <maetugr@gmail.com>
2018-02-28 09:30:20 +0100

    FlightTasks: replace setpoint setters with members
    I realized that instead of using the setpoint setters inline
    in real world tasks everyone started to have its own setpoint
    member variable and only call the setter in the end for all the
    privatly generate setpoints. This makes the setter useless and
    therefore I switched to member setpoints in the architecture.
    They bring more felxibility which is obviously needed but also
    less structure which is the price to pay.

--- src/lib/FlightTasks/FlightTasks.cpp
+++ src/lib/FlightTasks/FlightTasks.cpp
@@ -15,7 +15,7 @@ bool FlightTasks::update()
 	return false;
 }
 
-const vehicle_local_position_setpoint_s &FlightTasks::getPositionSetpoint()
+const vehicle_local_position_setpoint_s FlightTasks::getPositionSetpoint()
 {
 	if (isAnyTaskActive()) {
 		return _current_task->getPositionSetpoint();
