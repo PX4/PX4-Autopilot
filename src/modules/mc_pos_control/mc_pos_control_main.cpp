1916cb5414ede110a7548b3e79e0bdb607005bc9
Dennis Mannhart <dennis@yuneecresearch.com>
2018-03-09 07:55:09 +0100

    mc_pos_control: only use flighttask if rotary wing

--- src/modules/mc_pos_control/mc_pos_control_main.cpp
+++ src/modules/mc_pos_control/mc_pos_control_main.cpp
@@ -3077,6 +3077,7 @@ MulticopterPositionControl::task_main()
 			_flight_tasks.switchTask(FlightTaskIndex::None);
 		}
 
+
 		if (_test_flight_tasks.get() && _flight_tasks.isAnyTaskActive()) {
 
 			_flight_tasks.update();
