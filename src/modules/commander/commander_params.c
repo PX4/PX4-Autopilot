a9313bed64bbeda9675ccc38e09cf36c9acbd23f
Daniel Agar <daniel@agar.ca>
2018-03-15 01:39:26 -0400

    commander add COM_POS_FS_PROB and COM_POS_FS_DELAY metadata limit

--- src/modules/commander/commander_params.c
+++ src/modules/commander/commander_params.c
@@ -685,6 +685,8 @@ PARAM_DEFINE_INT32(COM_ARM_AUTH, 256010);
  * @unit sec
  * @reboot_required true
  * @group Commander
+ * @min 1
+ * @max 100
  */
 PARAM_DEFINE_INT32(COM_POS_FS_DELAY, 1);
 
@@ -700,6 +702,8 @@ PARAM_DEFINE_INT32(COM_POS_FS_DELAY, 1);
  * @unit sec
  * @reboot_required true
  * @group Commander
+ * @min 1
+ * @max 100
  */
 PARAM_DEFINE_INT32(COM_POS_FS_PROB, 30);
 
