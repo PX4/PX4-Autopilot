df53dce6e106ee06c7332212c88a0a1a7dcc4596
Roman <bapstroman@gmail.com>
2018-03-23 11:04:09 +0100

    vtol_type.h: initialise flag_idle_mc correctly
    Signed-off-by: Roman <bapstroman@gmail.com>

--- src/modules/vtol_att_control/vtol_type.h
+++ src/modules/vtol_att_control/vtol_type.h
@@ -184,7 +184,7 @@ protected:
 
 	struct Params 					*_params;
 
-	bool flag_idle_mc = true;		//false = "idle is set for fixed wing mode"; true = "idle is set for multicopter mode"
+	bool flag_idle_mc = false;		//false = "idle is set for fixed wing mode"; true = "idle is set for multicopter mode"
 
 	bool _pusher_active = false;
 	float _mc_roll_weight = 1.0f;	// weight for multicopter attitude controller roll output
