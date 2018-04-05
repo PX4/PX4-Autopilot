a779e7934cad0e844dae1d593ec48fd312bf01d4
Dennis Mannhart <dennis@px4.io>
2018-04-05 16:31:03 +0200

    FlightTaskOffboard: first commit similar in logic to legacy code
    FlightTaskOffboard: ensure supported offboard setpoints

--- src/lib/FlightTasks/FlightTasks.cpp
+++ src/lib/FlightTasks/FlightTasks.cpp
@@ -90,6 +90,10 @@ int FlightTasks::switchTask(FlightTaskIndex new_task_index)
 		_current_task = new (&_task_union.autoFollowMe) FlightTaskAutoFollowMe();
 		break;
 
+	case FlightTaskIndex::Offboard:
+		_current_task = new (&_task_union.offboard) FlightTaskOffboard();
+		break;
+
 	default:
 		/* invalid task */
 		return -1;
