b98de09fd30bd649674c688acafe8b29b3be3825
Daniel Agar <daniel@agar.ca>
2018-04-04 20:48:44 -0400

    commander preflight checks pass status and status_flags messages

--- src/modules/commander/PreflightCheck.h
+++ src/modules/commander/PreflightCheck.h
@@ -40,6 +40,8 @@
  */
 
 #include <drivers/drv_hrt.h>
+#include <uORB/topics/vehicle_status.h>
+#include <uORB/topics/vehicle_status_flags.h>
 
 #pragma once
 
@@ -70,8 +72,9 @@ namespace Preflight
 * @param checkPower
 *   true if the system power should be checked
 **/
-bool preflightCheck(orb_advert_t *mavlink_log_pub, bool checkSensors, bool checkAirspeed, bool checkRC, bool checkGNSS,
-		    bool checkDynamic, bool checkPower, bool isVTOL, bool reportFailures, bool prearm, const hrt_abstime &time_since_boot);
+bool preflightCheck(orb_advert_t *mavlink_log_pub, const vehicle_status_s &status,
+		    const vehicle_status_flags_s &status_flags, bool checkGNSS, bool reportFailures, bool prearm,
+		    const hrt_abstime &time_since_boot);
 
 static constexpr unsigned max_mandatory_gyro_count = 1;
 static constexpr unsigned max_optional_gyro_count = 3;
