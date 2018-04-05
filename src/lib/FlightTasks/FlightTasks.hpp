1099dd8c7d57880b099f72b480ca1d14b4d5c648
Dennis Mannhart <dennis@px4.io>
2018-04-05 16:31:03 +0200

    FlightTaskOffboard: first commit similar in logic to legacy code
    FlightTaskOffboard: ensure supported offboard setpoints

--- src/lib/FlightTasks/FlightTasks.hpp
+++ src/lib/FlightTasks/FlightTasks.hpp
@@ -51,6 +51,7 @@
 #include "tasks/FlightTaskAutoFollowMe.hpp"
 #include "tasks/FlightTaskOrbit.hpp"
 #include "tasks/FlightTaskSport.hpp"
+#include "tasks/FlightTaskOffboard.hpp"
 
 #include "SubscriptionArray.hpp"
 
@@ -67,6 +68,7 @@ enum class FlightTaskIndex : int {
 	Sport,
 	AutoLine,
 	AutoFollowMe,
+	Offboard,
 
 	Count // number of tasks
 };
@@ -151,6 +153,7 @@ private:
 		FlightTaskSport sport;
 		FlightTaskAutoLine autoLine;
 		FlightTaskAutoFollowMe autoFollowMe;
+		FlightTaskOffboard offboard;
 	} _task_union; /**< storage for the currently active task */
 
 	FlightTask *_current_task = nullptr;
