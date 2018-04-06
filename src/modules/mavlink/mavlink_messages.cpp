2b258167e2f7f46f370e61acb0f8cdfe9f7d090f
Oleg Kalachev <okalachev@gmail.com>
2018-04-07 01:48:04 +0300

    Add new mode AUTO_PRECLAND

--- src/modules/mavlink/mavlink_messages.cpp
+++ src/modules/mavlink/mavlink_messages.cpp
@@ -207,6 +207,12 @@ void get_mavlink_navigation_mode(const struct vehicle_status_s *const status, ui
 		custom_mode->sub_mode = PX4_CUSTOM_SUB_MODE_AUTO_FOLLOW_TARGET;
 		break;
 
+	case vehicle_status_s::NAVIGATION_STATE_AUTO_PRECLAND:
+		*mavlink_base_mode |= auto_mode_flags;
+		custom_mode.main_mode = PX4_CUSTOM_MAIN_MODE_AUTO;
+		custom_mode.sub_mode = PX4_CUSTOM_SUB_MODE_AUTO_PRECLAND;
+		break;
+
 	case vehicle_status_s::NAVIGATION_STATE_AUTO_RTL:
 
 	/* fallthrough */
