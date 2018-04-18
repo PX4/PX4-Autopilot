3cc392ef0a9da47cafc43ca6821de48c7fbc1101
Roman <bapstroman@gmail.com>
2018-04-18 20:46:01 +0200

    tailsitter: use forward transition throttle for climb-rate controlled mode
    - the mc pos controller will decrease throttle during the transition
    and thus the vehicle will not pick up enough airspeed to complete the
    transition
    
    Signed-off-by: Roman <bapstroman@gmail.com>

--- src/modules/vtol_att_control/tailsitter.cpp
+++ src/modules/vtol_att_control/tailsitter.cpp
@@ -198,8 +198,6 @@ void Tailsitter::update_transition_state()
 		_v_att_sp->pitch_body = math::constrain(_v_att_sp->pitch_body, PITCH_TRANSITION_FRONT_P1 - 0.2f,
 							_pitch_transition_start);
 
-		_v_att_sp->thrust = _mc_virtual_att_sp->thrust;
-
 		// disable mc yaw control once the plane has picked up speed
 		if (_airspeed->indicated_airspeed_m_s > ARSP_YAW_CTRL_DISABLE) {
 			_mc_yaw_weight = 0.0f;
@@ -222,15 +220,19 @@ void Tailsitter::update_transition_state()
 					time_since_trans_start / _params->back_trans_duration;
 		_v_att_sp->pitch_body = math::constrain(_v_att_sp->pitch_body, -2.0f, PITCH_TRANSITION_BACK + 0.2f);
 
-
-		_v_att_sp->thrust = _mc_virtual_att_sp->thrust;
-
 		// keep yaw disabled
 		_mc_yaw_weight = 0.0f;
 
 		// smoothly move control weight to MC
 		_mc_roll_weight = _mc_pitch_weight = time_since_trans_start / _params->back_trans_duration;
 
+	}
+
+	if (_v_control_mode->flag_control_climb_rate_enabled) {
+		_v_att_sp->thrust = _params->front_trans_throttle;
+
+	} else {
+		_v_att_sp->thrust = _mc_virtual_att_sp->thrust;
 	}
 
 	_mc_roll_weight = math::constrain(_mc_roll_weight, 0.0f, 1.0f);
