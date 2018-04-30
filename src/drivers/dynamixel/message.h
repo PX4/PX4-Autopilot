56a966404f259bb8197a22c5b3a4eea984ab596e
Zachary Taylor <zacharyjeremytaylor@gmail.com>
2018-04-30 13:14:46 +0200

    auto format code

--- src/drivers/dynamixel/message.h
+++ src/drivers/dynamixel/message.h
@@ -68,18 +68,18 @@
 
 #define MESSAGE_HEADER_START_BYTE 0
 #define MESSAGE_RESERVED_START_BYTE \
-  (MESSAGE_HEADER_START_BYTE + 3 * sizeof(uint8_t))
+	(MESSAGE_HEADER_START_BYTE + 3 * sizeof(uint8_t))
 #define MESSAGE_TARGET_START_BYTE \
-  (MESSAGE_RESERVED_START_BYTE + sizeof(uint8_t))
+	(MESSAGE_RESERVED_START_BYTE + sizeof(uint8_t))
 #define MESSAGE_SIZE_START_BYTE (MESSAGE_TARGET_START_BYTE + sizeof(uint8_t))
 #define MESSAGE_INSTRUCTION_START_BYTE \
-  (MESSAGE_SIZE_START_BYTE + sizeof(uint16_t))
+	(MESSAGE_SIZE_START_BYTE + sizeof(uint16_t))
 #define MESSAGE_ADDRESS_START_BYTE \
-  (MESSAGE_INSTRUCTION_START_BYTE + sizeof(uint8_t))
+	(MESSAGE_INSTRUCTION_START_BYTE + sizeof(uint8_t))
 #define MESSAGE_DATA_LENGTH_START_BYTE \
-  (MESSAGE_ADDRESS_START_BYTE + sizeof(uint16_t))
+	(MESSAGE_ADDRESS_START_BYTE + sizeof(uint16_t))
 #define MESSAGE_BODY_START_BYTE \
-  (MESSAGE_DATA_LENGTH_START_BYTE + sizeof(uint16_t))
+	(MESSAGE_DATA_LENGTH_START_BYTE + sizeof(uint16_t))
 
 #define PACKET_HEADER_SIZE sizeof(uint8_t)
 #define READ_PACKET_SIZE PACKET_HEADER_SIZE
@@ -89,40 +89,40 @@
 
 #define BASE_MESSAGE_SIZE (MESSAGE_BODY_START_BYTE + sizeof(uint16_t))
 #define READ_MESSAGE_SIZE \
-  (BASE_MESSAGE_SIZE + NUM_DYNAMIXELS * READ_PACKET_SIZE)
+	(BASE_MESSAGE_SIZE + NUM_DYNAMIXELS * READ_PACKET_SIZE)
 #define WRITE_32_BIT_MESSAGE_SIZE \
-  (BASE_MESSAGE_SIZE + NUM_DYNAMIXELS * WRITE_32_BIT_PACKET_SIZE)
+	(BASE_MESSAGE_SIZE + NUM_DYNAMIXELS * WRITE_32_BIT_PACKET_SIZE)
 #define WRITE_16_BIT_MESSAGE_SIZE \
-  (BASE_MESSAGE_SIZE + NUM_DYNAMIXELS * WRITE_16_BIT_PACKET_SIZE)
+	(BASE_MESSAGE_SIZE + NUM_DYNAMIXELS * WRITE_16_BIT_PACKET_SIZE)
 #define WRITE_8_BIT_MESSAGE_SIZE \
-  (BASE_MESSAGE_SIZE + NUM_DYNAMIXELS * WRITE_8_BIT_PACKET_SIZE)
+	(BASE_MESSAGE_SIZE + NUM_DYNAMIXELS * WRITE_8_BIT_PACKET_SIZE)
 #define STATUS_MESSAGE_SIZE 15
 
-uint16_t calc_crc(uint8_t* data_blk_ptr, uint16_t data_blk_size);
-void add_message_header(uint8_t* msg);
-void add_message_reserved_byte(uint8_t* msg);
-void add_message_broadcast_target(uint8_t* msg);
-void add_message_instruction(uint8_t instruction, uint8_t* msg);
-void add_message_address(uint16_t message_address, uint8_t* msg);
-void add_message_data_length(uint16_t data_length, uint8_t* msg);
-void add_message_read_data_packet(uint8_t target_id, uint8_t* msg);
+uint16_t calc_crc(uint8_t *data_blk_ptr, uint16_t data_blk_size);
+void add_message_header(uint8_t *msg);
+void add_message_reserved_byte(uint8_t *msg);
+void add_message_broadcast_target(uint8_t *msg);
+void add_message_instruction(uint8_t instruction, uint8_t *msg);
+void add_message_address(uint16_t message_address, uint8_t *msg);
+void add_message_data_length(uint16_t data_length, uint8_t *msg);
+void add_message_read_data_packet(uint8_t target_id, uint8_t *msg);
 void add_message_32_bit_write_data_packet(uint8_t target_id, int32_t data,
-                                          uint8_t* msg);
+		uint8_t *msg);
 void add_message_16_bit_write_data_packet(uint8_t target_id, uint16_t data,
-                                          uint8_t* msg);
+		uint8_t *msg);
 void add_message_8_bit_write_data_packet(uint8_t target_id, uint8_t data,
-                                         uint8_t* msg);
-void add_message_size_and_crc(uint16_t packet_size, uint8_t* msg);
+		uint8_t *msg);
+void add_message_size_and_crc(uint16_t packet_size, uint8_t *msg);
 int calc_message_size(uint16_t packet_size);
 
-void build_read_message(uint16_t read_address, uint8_t* msg);
-void build_write_32_bit_message(uint16_t write_address, int32_t* data,
-                                uint8_t* msg);
-void build_write_16_bit_message(uint16_t write_address, uint16_t* data,
-                                uint8_t* msg);
-void build_write_8_bit_message(uint16_t write_address, uint8_t* data,
-                               uint8_t* msg);
+void build_read_message(uint16_t read_address, uint8_t *msg);
+void build_write_32_bit_message(uint16_t write_address, int32_t *data,
+				uint8_t *msg);
+void build_write_16_bit_message(uint16_t write_address, uint16_t *data,
+				uint8_t *msg);
+void build_write_8_bit_message(uint16_t write_address, uint8_t *data,
+			       uint8_t *msg);
 
-uint8_t parse_status_messages(uint8_t* msg, uint8_t* ids, int32_t* data);
+uint8_t parse_status_messages(uint8_t *msg, uint8_t *ids, int32_t *data);
 
 #endif //_DYNAMIXEL_MESSAGE_H
