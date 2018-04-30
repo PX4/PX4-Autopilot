0606cf9a4d4bb32d211778933e969c543e76fff0
Zachary Taylor <zacharyjeremytaylor@gmail.com>
2018-04-30 13:07:30 +0200

    improved comments

--- src/drivers/dynamixel/dynamixel_position.c
+++ src/drivers/dynamixel/dynamixel_position.c
@@ -1,3 +1,48 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2012-2016 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/**
+ * @file dynamixel_position.c
+ * Dynamixel position driver.
+ *
+ * @author Zachary Taylor	<ztaylor@ethz.ch>
+ *
+ * A simple controller for working with dynamixel servos that use protocal 2.0.
+ * The servos are set into extended position mode and can be given an angle between -512*PI and 512*PI. 
+ * The angle is given via actuator control group 2, unlike most other actuators its value is not normalized.
+ * All feedback from the servos is disabled to allow higher throughput when using the ttl servos.
+ */
+
 #include <drivers/drv_hrt.h>
 #include <math.h>
 #include <poll.h>
@@ -14,31 +59,64 @@
 
 #include "message.h"
 
-#define DEFAULT_UART "/dev/ttyS1"  // Serial1
+#define DEFAULT_UART "/dev/ttyS1"  /* Serial1 */
 #define DEFAULT_BAUDRATE B115200
 #define DEFAULT_IGNORE_DISARM true
-#define ANGLE_RES 651.898646904f  // 4096 / (2*pi)
+#define ANGLE_RES 651.898646904f  /* 4096 / (2*pi) */
 
 static int _thread_should_exit = false; /**< Deamon exit flag */
 static int _thread_running = false;     /**< Deamon status flag */
 static int _deamon_task;                /**< Handle of deamon task / thread */
 static const char _daemon_name[] = "dynamixel_position";
 static const char _commandline_usage[] =
+    "sends values (interpreted as angles in radians) in Control Group #2 to up "
+    "to 8 dynamixels\n"
     "usage: dynamixel_position start|status|stop [-d <device> -b <baudrate> -u "
     "<update_rate_hz> -i <ignore_disarm>]";
 
-void open_uart(const char *device, const speed_t baudrate);
-bool write_uart(const uint8_t *msg, int msg_length);
-bool set_angles(const float32 *angles);
-bool enable(void);
-bool disable(void);
-void update_armed_status(int armed_sub, bool ignore_disarm);
-void get_commandline_values(int argc, char *argv[], char **device,
-                            speed_t *baudrate, bool *ignore_disarm);
-int dynamixel_position_thread_main(int argc, char *argv[]);
-
 struct termios _uart_config_original;
 int _uart = 0;
+
+/**
+ * Sets uart parameters and opens it.
+ */
+void open_uart(const char *device, const speed_t baudrate);
+
+/**
+ * Writes a given message to the uart.
+ */
+bool write_uart(const uint8_t *msg, int msg_length);
+
+/**
+ * Sends the specified angles to the dynamixels.
+ */
+bool set_angles(const float32 *angles);
+
+/**
+ * Sets up and enables torque for the dynamixels.
+ */
+bool enable(void);
+
+/**
+ * Disables torque in the dynamixels.
+ */
+bool disable(void);
+
+/**
+ * Enables/disables the dynamixels, in response to the armed status of the system.
+ */
+void update_armed_status(int armed_sub, bool ignore_disarm);
+
+/**
+ * Sets parameters specified on the command line.
+ */
+void get_commandline_values(int argc, char *argv[], char **device,
+                            speed_t *baudrate, bool *ignore_disarm);
+
+/**
+ * Main loop that passes specified angles to the dynamixels.
+ */
+int dynamixel_position_thread_main(int argc, char *argv[]);
 
 void open_uart(const char *device, const speed_t baudrate) {
   /* open uart */
@@ -108,25 +186,25 @@ bool enable(void) {
 
   uint8_t msg[WRITE_8_BIT_MESSAGE_SIZE];
 
-  // disable msg status feedback
+  /* disable msg status feedback */
   build_write_8_bit_message(RETURN_LEVEL_SET_ADDRESS, disable_flags, msg);
   if (!write_uart(msg, WRITE_8_BIT_MESSAGE_SIZE)) {
     return false;
   }
 
-  // set to extended position control mode
+  /* set to extended position control mode */
   build_write_8_bit_message(OPERATING_MODE_ADDRESS, control_flags, msg);
   if (!write_uart(msg, WRITE_8_BIT_MESSAGE_SIZE)) {
     return false;
   }
 
-  // enable servo
+  /* enable servo */
   build_write_8_bit_message(ENABLE_ADDRESS, enable_flags, msg);
   if (!write_uart(msg, WRITE_8_BIT_MESSAGE_SIZE)) {
     return false;
   }
 
-  // turn on led to get some feedback
+  /* turn on led to get some feedback */
   build_write_8_bit_message(LED_ADDRESS, enable_flags, msg);
   if (!write_uart(msg, WRITE_8_BIT_MESSAGE_SIZE)) {
     return false;
@@ -143,13 +221,13 @@ bool disable(void) {
 
   uint8_t msg[WRITE_8_BIT_MESSAGE_SIZE];
 
-  // disable servo
+  /* disable servo */
   build_write_8_bit_message(ENABLE_ADDRESS, disable_flags, msg);
   if (!write_uart(msg, WRITE_8_BIT_MESSAGE_SIZE)) {
     return false;
   }
 
-  // turn off led to get some feedback
+  /* turn off led to get some feedback */
   build_write_8_bit_message(LED_ADDRESS, disable_flags, msg);
   if (!write_uart(msg, WRITE_8_BIT_MESSAGE_SIZE)) {
     return false;
@@ -250,11 +328,10 @@ int dynamixel_position_thread_main(int argc, char *argv[]) {
   int sub_fd = orb_subscribe(ORB_ID(actuator_controls_2));
   int armed_sub = orb_subscribe(ORB_ID(actuator_armed));
 
-  // limit the update rate to something the uart can easily handle
-  if(baudrate == B57600){
+  /* limit the update rate to something the uart can easily handle */
+  if (baudrate == B57600) {
     orb_set_interval(sub_fd, 20);
-  }
-  else{
+  } else {
     orb_set_interval(sub_fd, 10);
   }
 
--- src/drivers/dynamixel/message.h
+++ src/drivers/dynamixel/message.h
@@ -1,3 +1,36 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2012-2016 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
 #ifndef _DYNAMIXEL_MESSAGE_H
 #define _DYNAMIXEL_MESSAGE_H
 
@@ -80,17 +113,13 @@ void add_message_16_bit_write_data_packet(uint8_t target_id, uint16_t data,
 void add_message_8_bit_write_data_packet(uint8_t target_id, uint8_t data,
                                          uint8_t* msg);
 void add_message_size_and_crc(uint16_t packet_size, uint8_t* msg);
-
 int calc_message_size(uint16_t packet_size);
 
 void build_read_message(uint16_t read_address, uint8_t* msg);
-
 void build_write_32_bit_message(uint16_t write_address, int32_t* data,
                                 uint8_t* msg);
-
 void build_write_16_bit_message(uint16_t write_address, uint16_t* data,
                                 uint8_t* msg);
-
 void build_write_8_bit_message(uint16_t write_address, uint8_t* data,
                                uint8_t* msg);
 
