57bf5cd018e60df40f020860fa91bee33e79f5ab
Anthony Lamping <lamping.ap@gmail.com>
2018-05-02 14:51:05 -0400

    tools: cleanup setup for gazebo env vars

--- Tools/setup_gazebo.bash
+++ Tools/setup_gazebo.bash
@@ -7,10 +7,8 @@
 #
 # License: according to LICENSE.md in the root directory of the PX4 Firmware repository
 
-if [ "$#" != 2 ]
-then
-    echo usage: source setup_gazebo.bash src_dir build_dir
-    echo ""
+if [ "$#" != 2 ]; then
+    echo -e "usage: source setup_gazebo.bash src_dir build_dir\n"
     return 1
 fi
 
@@ -18,11 +16,10 @@ SRC_DIR=$1
 BUILD_DIR=$2
 
 # setup Gazebo env and update package path
-export GAZEBO_PLUGIN_PATH=${BUILD_DIR}/build_gazebo:${GAZEBO_PLUGIN_PATH}
-export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:${SRC_DIR}/Tools/sitl_gazebo/models
-# Disabling the remote model download seems only necessary with Gazebo 6
-#export GAZEBO_MODEL_DATABASE_URI=""
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${SRC_DIR}/Tools/sitl_gazebo/Build/msgs/:${BUILD_DIR}/build_gazebo
+export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:${BUILD_DIR}/build_gazebo
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:${SRC_DIR}/Tools/sitl_gazebo/models
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${BUILD_DIR}/build_gazebo
+
 echo -e "GAZEBO_PLUGIN_PATH $GAZEBO_PLUGIN_PATH"
 echo -e "GAZEBO_MODEL_PATH $GAZEBO_MODEL_PATH"
 echo -e "LD_LIBRARY_PATH $LD_LIBRARY_PATH"
