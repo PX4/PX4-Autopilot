6f840f9d7ac9c8881da8d12ac75ee4ce0d9ef11f
Paul Riseborough <p_riseborough@live.com.au>
2018-05-16 12:14:08 +1000

    EKF: Improve detection of indoor flight condition

--- EKF/ekf.h
+++ EKF/ekf.h
@@ -326,6 +326,7 @@ private:
 	bool _mag_use_inhibit{false};		///< true when magnetomer use is being inhibited
 	bool _mag_inhibit_yaw_reset_req{false};	///< true when magnetomer inhibit has been active for long enough to require a yaw reset when conditons improve.
 	float _last_static_yaw{0.0f};		///< last yaw angle recorded when on ground motion checks were passing (rad)
+	bool _vehicle_at_rest_prev{false};	///< true when the vehicle was at rest the previous time the status was checked
 
 	float P[_k_num_states][_k_num_states] {};	///< state covariance matrix
 
