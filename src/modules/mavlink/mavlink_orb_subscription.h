e61120319151371d6878fd6ef548555eb29ed8fe
Daniel Agar <daniel@agar.ca>
2018-05-17 15:20:26 -0400

    Mavlink set last sent timestamp to space out initial publication
     - remove QURT defines no longer required

--- src/modules/mavlink/mavlink_orb_subscription.h
+++ src/modules/mavlink/mavlink_orb_subscription.h
@@ -48,7 +48,7 @@
 class MavlinkOrbSubscription
 {
 public:
-	MavlinkOrbSubscription *next;	///< pointer to next subscription in list
+	MavlinkOrbSubscription *next{nullptr};	///< pointer to next subscription in list
 
 	MavlinkOrbSubscription(const orb_id_t topic, int instance);
 	~MavlinkOrbSubscription();
@@ -97,11 +97,15 @@ public:
 
 private:
 	const orb_id_t _topic;		///< topic metadata
-	int _fd;			///< subscription handle
 	const uint8_t _instance;		///< get topic instance
-	bool _published;		///< topic was ever published
-	bool _subscribe_from_beginning; ///< we need to subscribe from the beginning, e.g. for vehicle_command_acks
-	hrt_abstime _last_pub_check;	///< when we checked last
+
+	int _fd{-1};			///< subscription handle
+
+	bool _published{false};		///< topic was ever published
+
+	bool _subscribe_from_beginning{false}; ///< we need to subscribe from the beginning, e.g. for vehicle_command_acks
+
+	hrt_abstime _last_pub_check{0};	///< when we checked last
 
 	/* do not allow copying this class */
 	MavlinkOrbSubscription(const MavlinkOrbSubscription &);
