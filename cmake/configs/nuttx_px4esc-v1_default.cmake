7d17ea37c71ab9b35997c626b815b6c257e5132a
Daniel Agar <daniel@agar.ca>
2018-05-30 19:58:19 -0700

    Jenkins archive nuttx bin file

--- cmake/configs/nuttx_px4esc-v1_default.cmake
+++ cmake/configs/nuttx_px4esc-v1_default.cmake
@@ -27,7 +27,7 @@ add_definitions(
 )
 
 px4_nuttx_make_uavcan_bootloadable(BOARD ${BOARD}
-	BIN ${PX4_BINARY_DIR}/platforms/nuttx/px4esc-v1.bin
+	BIN ${PX4_BINARY_DIR}/px4esc-v1.bin
 	HWNAME ${uavcanblid_name}
 	HW_MAJOR ${uavcanblid_hw_version_major}
 	HW_MINOR ${uavcanblid_hw_version_minor}
