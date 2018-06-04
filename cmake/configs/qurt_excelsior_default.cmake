c2a7fd54261c206d5de083fe4cfece15f9113f45
Roman <bapstroman@gmail.com>
2018-06-04 16:52:28 +0200

    excelsior: support building for qurt
    Signed-off-by: Roman <bapstroman@gmail.com>

--- cmake/configs/qurt_excelsior_default.cmake
+++ cmake/configs/qurt_excelsior_default.cmake
@@ -6,6 +6,13 @@
 # The config between different QURT builds is shared.
 include(configs/qurt_sdflight_default)
 
+# Get $QC_SOC_TARGET from environment if existing.
+if (DEFINED ENV{QC_SOC_TARGET})
+	set(QC_SOC_TARGET $ENV{QC_SOC_TARGET})
+else()
+	set(QC_SOC_TARGET "APQ8096")
+endif()
+
 # This definition allows to differentiate the specific board.
 add_definitions(
 	-D__PX4_QURT_EXCELSIOR
