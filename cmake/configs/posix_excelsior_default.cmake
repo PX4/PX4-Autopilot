5e22f08e01f9c200fdc49b82fbf28ed55e8ad321
Roman <bapstroman@gmail.com>
2018-06-04 11:19:53 +0200

    support for the posix side of APQ8096
    Signed-off-by: Roman <bapstroman@gmail.com>

--- cmake/configs/posix_excelsior_default.cmake
+++ cmake/configs/posix_excelsior_default.cmake
@@ -3,7 +3,17 @@
 # This cmake config builds for POSIX, so the part of the flight stack running
 # on the Linux side of the Snapdragon.
 include(configs/posix_sdflight_default)
-set(CMAKE_TOOLCHAIN_FILE ${PX4_SOURCE_DIR}/cmake/cmake_hexagon/toolchain/Toolchain-arm-oemllib32-linux-gnueabi.cmake)
+
+set(DISABLE_PARAMS_MODULE_SCOPING TRUE)
+
+# Get $QC_SOC_TARGET from environment if existing.
+if (DEFINED ENV{QC_SOC_TARGET})
+	set(QC_SOC_TARGET $ENV{QC_SOC_TARGET})
+else()
+	set(QC_SOC_TARGET "APQ8096")
+endif()
+
+set(CONFIG_SHMEM "1")
 
 # This definition allows to differentiate the specific board.
 add_definitions(
