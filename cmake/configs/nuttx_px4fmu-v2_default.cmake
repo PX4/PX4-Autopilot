de9b18f4039d7bc6292912ba8373d5b6aa7dc36c
zarathustra <jin_wu_uestc@hotmail.com>
2018-06-13 16:46:46 +0800

    New adding of codes for high-precision jitter-rejection AMOV-IMU

--- cmake/configs/nuttx_px4fmu-v2_default.cmake
+++ cmake/configs/nuttx_px4fmu-v2_default.cmake
@@ -32,7 +32,7 @@ set(config_module_list
 	#drivers/mkblctrl
         drivers/imu/mpu6000
         drivers/imu/amov_imu
-        #drivers/imu/mpu9250
+        drivers/imu/mpu9250
 	#drivers/oreoled
 	#drivers/protocol_splitter
 	drivers/pwm_input
@@ -49,13 +49,13 @@ set(config_module_list
 
 	# distance sensors
         #drivers/distance_sensor/ll40ls
-	#drivers/distance_sensor/mb12xx
-        #drivers/distance_sensor/sf0x
-        #drivers/distance_sensor/sf1xx
-        #drivers/distance_sensor/srf02
-        #drivers/distance_sensor/teraranger
-        #drivers/distance_sensor/tfmini
-	#drivers/distance_sensor/ulanding
+        drivers/distance_sensor/mb12xx
+        drivers/distance_sensor/sf0x
+        drivers/distance_sensor/sf1xx
+        drivers/distance_sensor/srf02
+        drivers/distance_sensor/teraranger
+        drivers/distance_sensor/tfmini
+        #drivers/distance_sensor/ulanding
 	modules/sensors
 
 	#
