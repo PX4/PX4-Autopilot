78d8061aaf642d2c6e9fff992585e98175175060
johannes <johannes@auterion.com>
2018-06-15 17:06:38 +0200

    ecl-ekf tools: set exit code to -1 for failed analysis, pipe args
    - process_log_data: exit with code -1 if whole system analysis fails
    - batch_process_logdata and process_log_data: pipe sensor safety
    margin argument to the arguments of the superseeding scripts
    - reduce minimum flight length for analysis to 50 samples

--- Tools/ecl_ekf/process_logdata_ekf.py
+++ Tools/ecl_ekf/process_logdata_ekf.py
@@ -3,11 +3,11 @@
 from __future__ import print_function
 
 import argparse
-import os
+import os, sys
 
 from pyulog import *
 
-from analyse_logdata_ekf import *
+from analyse_logdata_ekf import analyse_ekf
 
 """
 Performs a health assessment on the ecl EKF navigation estimator data contained in a an ULog file
@@ -21,6 +21,9 @@ parser.add_argument('--no-plots', action='store_true',
                     help='Whether to only analyse and not plot the summaries for developers.')
 parser.add_argument('--check-level-thresholds', type=str, default=None,
                     help='The csv file of fail and warning test thresholds for analysis.')
+parser.add_argument('--no-sensor-safety-margin', action='store_true',
+                    help='Whether to not cut-off 5s after take-off and 5s before landing '
+                         '(for certain sensors that might be influence by proximity to ground).')
 
 def is_valid_directory(parser, arg):
     if os.path.isdir(arg):
@@ -73,15 +76,8 @@ print('Using test criteria loaded from {:s}'.format(check_level_dict_filename))
 # perform the ekf analysis
 test_results = analyse_ekf(
     estimator_status_data, ekf2_innovations_data, sensor_preflight_data,
-    check_levels, plot=not args.no_plots, output_plot_filename=args.filename + ".pdf")
-
-# print master test status to console
-if (test_results['master_status'][0] == 'Pass'):
-    print('No anomalies detected')
-elif (test_results['master_status'][0] == 'Warning'):
-    print('Minor anomalies detected')
-elif (test_results['master_status'][0] == 'Fail'):
-    print('Major anomalies detected')
+    check_levels, plot=not args.no_plots, output_plot_filename=args.filename + ".pdf",
+    late_start_early_ending=not args.no_sensor_safety_margin)
 
 # write metadata to a .csv file
 with open(args.filename + ".mdat.csv", "w") as file:
@@ -99,3 +95,12 @@ print('Test results written to {:s}.mdat.csv'.format(args.filename))
 
 if not args.no_plots:
     print('Plots saved to {:s}.pdf'.format(args.filename))
+
+# print master test status to console
+if (test_results['master_status'][0] == 'Pass'):
+    print('No anomalies detected')
+elif (test_results['master_status'][0] == 'Warning'):
+    print('Minor anomalies detected')
+elif (test_results['master_status'][0] == 'Fail'):
+    print('Major anomalies detected')
+    sys.exit(-1)
