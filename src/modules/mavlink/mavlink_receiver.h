3999c066e0bb0fcbb55d51aada4bcf9a8c3d0bc5
TSC21 <n.marques21@hotmail.com>
2018-06-24 16:21:08 +0100

    mavlink_receiver: set URT_SIZE as static const

--- src/modules/mavlink/mavlink_receiver.h
+++ src/modules/mavlink/mavlink_receiver.h
@@ -264,7 +264,7 @@ private:
 	param_t _p_bat_crit_thr;
 	param_t _p_bat_low_thr;
 
-	const size_t URT_SIZE = 21;
+	static const size_t URT_SIZE = 21;
 
 	MavlinkReceiver(const MavlinkReceiver &) = delete;
 	MavlinkReceiver operator=(const MavlinkReceiver &) = delete;
