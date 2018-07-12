a2063230f8d1a3ba99fee03df91ff1f60d5f6370
Martina <martina@rivizzigno.it>
2018-07-12 21:23:16 +0200

    add script to initialize catkin workspace and build PX4 avoidance local planner

---
+++ test/rostest_avoidance_run.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+PX4_SRC_DIR=${DIR}/..
+
+source /opt/ros/kinetic/setup.bash
+mkdir ~/catkin_ws/src
+cd ~/catkin_ws
+git clone --depth=1 https://github.com/PX4/avoidance.git src/
+
+catkin init
+catkin build local_planner --cmake-args -DCMAKE_BUILD_TYPE=Release
+source ${PX4_SRC_DIR}/Tools/setup_gazebo.bash ${PX4_SRC_DIR} ${PX4_SRC_DIR}/build/posix_sitl_default
+
+source ~/catkin_ws/devel/setup.bash 2>/dev/null
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:${PX4_SRC_DIR}:${PX4_SRC_DIR}/Tools/sitl_gazebo
+
+rostest px4 "$@"
