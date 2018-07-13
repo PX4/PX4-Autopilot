b6d01c36afab77aa2309e8ff7fa9c5cb23ae0e80
David Sidrane <david_s5@nscdg.com>
2018-07-13 11:38:57 -0700

    px4fmu-v5:Support RX TX swap

--- src/drivers/boards/px4fmu-v5/init.c
+++ src/drivers/boards/px4fmu-v5/init.c
@@ -130,7 +130,14 @@ __EXPORT void board_rc_input(bool invert_on, uint32_t uxart_base)
 	putreg32(regval, STM32_USART_CR1_OFFSET + uxart_base);
 
 	if (invert_on) {
+#if defined(BOARD_HAS_RX_TX_SWAP) &&	RC_SERIAL_PORT_IS_SWAPED == 1
+
+		/* This is only tuned on */
+
+		cr2 |= (USART_CR2_RXINV | USART_CR2_TXINV | USART_CR2_SWAP);
+#else
 		cr2 |= (USART_CR2_RXINV | USART_CR2_TXINV);
+#endif
 
 	} else {
 		cr2 &= ~(USART_CR2_RXINV | USART_CR2_TXINV);
