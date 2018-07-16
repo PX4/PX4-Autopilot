49a361b90e88f5dd0c953fe4da27fcbeca3f0600
Daniel Agar <daniel@agar.ca>
2018-07-15 20:44:17 -0400

    ekf2 ekf_gps_position publish remove unnecessary param check

--- src/modules/ekf2/ekf2_main.cpp
+++ src/modules/ekf2/ekf2_main.cpp
@@ -1016,27 +1016,25 @@ void Ekf2::run()
 			_ekf.setGpsData(_gps_output[_gps_select_index].time_usec, &_gps_output[_gps_select_index]);
 
 			// log blended solution as a third GPS instance
-			if (_gps_blend_mask.get() > 0) {
-				ekf_gps_position_s gps;
-				gps.timestamp = _gps_output[_gps_select_index].time_usec;
-				gps.lat = _gps_output[_gps_select_index].lat;
-				gps.lon = _gps_output[_gps_select_index].lon;
-				gps.alt = _gps_output[_gps_select_index].alt;
-				gps.fix_type = _gps_output[_gps_select_index].fix_type;
-				gps.eph = _gps_output[_gps_select_index].eph;
-				gps.epv = _gps_output[_gps_select_index].epv;
-				gps.s_variance_m_s = _gps_output[_gps_select_index].sacc;
-				gps.vel_m_s = _gps_output[_gps_select_index].vel_m_s;
-				gps.vel_n_m_s = _gps_output[_gps_select_index].vel_ned[0];
-				gps.vel_e_m_s = _gps_output[_gps_select_index].vel_ned[1];
-				gps.vel_d_m_s = _gps_output[_gps_select_index].vel_ned[2];
-				gps.vel_ned_valid = _gps_output[_gps_select_index].vel_ned_valid;
-				gps.satellites_used = _gps_output[_gps_select_index].nsats;
-				gps.selected = _gps_select_index;
+			ekf_gps_position_s gps;
+			gps.timestamp = _gps_output[_gps_select_index].time_usec;
+			gps.lat = _gps_output[_gps_select_index].lat;
+			gps.lon = _gps_output[_gps_select_index].lon;
+			gps.alt = _gps_output[_gps_select_index].alt;
+			gps.fix_type = _gps_output[_gps_select_index].fix_type;
+			gps.eph = _gps_output[_gps_select_index].eph;
+			gps.epv = _gps_output[_gps_select_index].epv;
+			gps.s_variance_m_s = _gps_output[_gps_select_index].sacc;
+			gps.vel_m_s = _gps_output[_gps_select_index].vel_m_s;
+			gps.vel_n_m_s = _gps_output[_gps_select_index].vel_ned[0];
+			gps.vel_e_m_s = _gps_output[_gps_select_index].vel_ned[1];
+			gps.vel_d_m_s = _gps_output[_gps_select_index].vel_ned[2];
+			gps.vel_ned_valid = _gps_output[_gps_select_index].vel_ned_valid;
+			gps.satellites_used = _gps_output[_gps_select_index].nsats;
+			gps.selected = _gps_select_index;
 
-				// Publish to the EKF blended GPS topic
-				orb_publish_auto(ORB_ID(ekf_gps_position), &_blended_gps_pub, &gps, &_gps_orb_instance, ORB_PRIO_LOW);
-			}
+			// Publish to the EKF blended GPS topic
+			orb_publish_auto(ORB_ID(ekf_gps_position), &_blended_gps_pub, &gps, &_gps_orb_instance, ORB_PRIO_LOW);
 		}
 
 		bool airspeed_updated = false;
