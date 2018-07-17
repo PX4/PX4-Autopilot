ae9cd86580ca7c128c8883e2fa329a0c68300201
Martina <martina@rivizzigno.it>
2018-07-17 11:51:46 +0200

    mc_pos_control: TEMP in missions run flight task AutoSmooth instead of AutoLine

--- src/modules/mc_pos_control/mc_pos_control_main.cpp
+++ src/modules/mc_pos_control/mc_pos_control_main.cpp
@@ -712,7 +712,7 @@ MulticopterPositionControl::start_flight_task()
 
 	} else if (_control_mode.flag_control_auto_enabled) {
 		// Auto relate maneuvers
-		int error = _flight_tasks.switchTask(FlightTaskIndex::AutoLine);
+		int error = _flight_tasks.switchTask(FlightTaskIndex::AutoSmooth);
 
 		if (error != 0) {
 			PX4_WARN("Auto activation failed with error: %s", _flight_tasks.errorToString(error));
