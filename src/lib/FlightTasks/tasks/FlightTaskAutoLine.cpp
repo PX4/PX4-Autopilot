d85481be6d631aeee5011ba27d82b4dc9c368d97
Martina <martina@rivizzigno.it>
2018-07-19 16:35:45 +0200

    Restructure FlightTaskAutoLine: -add FlightTaskAutoMapper that handles the different types of waypoint and generates setpoints for all types except of position and loiter - FlightTaskAutoLine generates the setpoint types position and loiter if the flight between waypoint is a straight line

--- src/lib/FlightTasks/tasks/FlightTaskAutoLine.cpp
+++ src/lib/FlightTasks/tasks/FlightTaskAutoLine.cpp
@@ -45,108 +45,9 @@ using namespace matrix;
 
 bool FlightTaskAutoLine::activate()
 {
-	bool ret = FlightTaskAuto::activate();
-	_reset();
-	return ret;
+	return FlightTaskAutoMapper::activate();
 }
 
-bool FlightTaskAutoLine::update()
-{
-	// always reset constraints because they might change depending on the type
-	_setDefaultConstraints();
-
-	_updateAltitudeAboveGround();
-
-	bool follow_line = _type == WaypointType::loiter || _type == WaypointType::position;
-	bool follow_line_prev = _type_previous == WaypointType::loiter || _type_previous == WaypointType::position;
-
-	// 1st time that vehicle starts to follow line. Reset all setpoints to current vehicle state.
-	if (follow_line && !follow_line_prev) {
-		_reset();
-	}
-
-	// The only time a thrust set-point is sent out is during
-	// idle. Hence, reset thrust set-point to NAN in case the
-	// vehicle exits idle.
-
-	if (_type_previous == WaypointType::idle) {
-		_thrust_setpoint = Vector3f(NAN, NAN, NAN);
-	}
-
-	if (_type == WaypointType::idle) {
-		_generateIdleSetpoints();
-
-	} else if (_type == WaypointType::land) {
-		_generateLandSetpoints();
-
-	} else if (follow_line) {
-		_generateSetpoints();
-
-	} else if (_type == WaypointType::takeoff) {
-		_generateTakeoffSetpoints();
-
-	} else if (_type == WaypointType::velocity) {
-		_generateVelocitySetpoints();
-	}
-
-	// update previous type
-	_type_previous = _type;
-
-	return true;
-}
-
-void FlightTaskAutoLine::_reset()
-{
-	// Set setpoints equal current state.
-	_velocity_setpoint = _velocity;
-	_position_setpoint = _position;
-	_yaw_setpoint = _yaw;
-	_destination = _position;
-	_origin = _position;
-	_speed_at_target = 0.0f;
-}
-
-void FlightTaskAutoLine::_generateIdleSetpoints()
-{
-	// Send zero thrust setpoint */
-	_position_setpoint = Vector3f(NAN, NAN, NAN); // Don't require any position/velocity setpoints
-	_velocity_setpoint = Vector3f(NAN, NAN, NAN);
-	_thrust_setpoint.zero();
-}
-
-void FlightTaskAutoLine::_generateLandSetpoints()
-{
-	// Keep xy-position and go down with landspeed. */
-	_position_setpoint = Vector3f(_target(0), _target(1), NAN);
-	_velocity_setpoint = Vector3f(Vector3f(NAN, NAN, MPC_LAND_SPEED.get()));
-	// set constraints
-	_constraints.tilt = MPC_TILTMAX_LND.get();
-	_constraints.speed_down = MPC_LAND_SPEED.get();
-	_constraints.landing_gear = vehicle_constraints_s::GEAR_DOWN;
-}
-
-void FlightTaskAutoLine::_generateTakeoffSetpoints()
-{
-	// Takeoff is completely defined by target position. */
-	_position_setpoint = _target;
-	_velocity_setpoint = Vector3f(NAN, NAN, NAN);
-
-	// limit vertical speed during takeoff
-	_constraints.speed_up = math::gradual(_alt_above_ground, MPC_LAND_ALT2.get(),
-					      MPC_LAND_ALT1.get(), MPC_TKO_SPEED.get(), _constraints.speed_up);
-
-	_constraints.landing_gear = vehicle_constraints_s::GEAR_DOWN;
-
-}
-
-void FlightTaskAutoLine::_generateVelocitySetpoints()
-{
-	// TODO: Remove velocity force logic from navigator, since
-	// navigator should only send out waypoints.
-	_position_setpoint = Vector3f(NAN, NAN, _position(2));
-	Vector2f vel_sp_xy = Vector2f(&_velocity(0)).unit_or_zero() * _mc_cruise_speed;
-	_velocity_setpoint = Vector3f(vel_sp_xy(0), vel_sp_xy(1), NAN);
-}
 
 void FlightTaskAutoLine::_generateSetpoints()
 {
@@ -517,31 +418,8 @@ float FlightTaskAutoLine::_getVelocityFromAngle(const float angle)
 	return math::constrain(speed_close, min_cruise_speed, _mc_cruise_speed);
 }
 
-void FlightTaskAutoLine::_updateAltitudeAboveGround()
-{
-	// Altitude above ground is by default just the negation of the current local position in D-direction.
-	_alt_above_ground = -_position(2);
-
-	if (PX4_ISFINITE(_dist_to_bottom)) {
-		// We have a valid distance to ground measurement
-		_alt_above_ground = _dist_to_bottom;
-
-	} else if (_sub_home_position->get().valid_alt) {
-		// if home position is set, then altitude above ground is relative to the home position
-		_alt_above_ground = -_position(2) + _sub_home_position->get().z;
-	}
-}
-
 bool FlightTaskAutoLine::_highEnoughForLandingGear()
 {
 	// return true if altitude is above two meters
 	return _alt_above_ground > 2.0f;
-}
-
-void FlightTaskAutoLine::updateParams()
-{
-	FlightTaskAuto::updateParams();
-
-	// make sure that alt1 is above alt2
-	MPC_LAND_ALT1.set(math::max(MPC_LAND_ALT1.get(), MPC_LAND_ALT2.get()));
 }
