74e4619042f6e916420853bc55e01a650b3dc077
Beat Küng <beat-kueng@gmx.net>
2018-07-23 17:51:25 +0200

    rc lib: cleanup, move structs out of common_rc.h

--- src/lib/rc/common_rc.h
+++ src/lib/rc/common_rc.h
@@ -7,19 +7,8 @@
 #include "sbus.h"
 #include "st24.h"
 #include "sumd.h"
-#include "dsm.h"
 
 #pragma pack(push, 1)
-typedef   uint8_t dsm_frame_t[DSM_BUFFER_SIZE]; /**< DSM dsm frame receive buffer */
-typedef   uint8_t dsm_buf_t[DSM_FRAME_SIZE * 2]; // Define working buffer
-
-typedef  struct dsm_decode_t {
-	dsm_frame_t frame;
-	dsm_buf_t buf;
-} dsm_decode_t;
-
-typedef   uint8_t sbus_frame_t[SBUS_FRAME_SIZE + (SBUS_FRAME_SIZE / 2)];
-
 typedef  struct rc_decode_buf_ {
 	union {
 		dsm_decode_t dsm;
