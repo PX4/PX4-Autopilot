6f663821bf0279c09a4050cffb7fd890efb53960
Anthony Lamping <lamping.ap@gmail.com>
2018-07-23 18:30:17 -0400

    test: avoidance: add to GAZEBO_MODEL_PATH and fix clone path

--- test/rostest_avoidance_run.sh
+++ test/rostest_avoidance_run.sh
@@ -6,13 +6,14 @@ PX4_SRC_DIR=${DIR}/..
 source /opt/ros/kinetic/setup.bash
 mkdir -p ${PX4_SRC_DIR}/catkin_ws/src
 cd ${PX4_SRC_DIR}/catkin_ws/
-git clone --depth=1 https://github.com/PX4/avoidance.git src/
+git clone --depth=1 https://github.com/PX4/avoidance.git src/avoidance/
 
 catkin init
 catkin build local_planner --cmake-args -DCMAKE_BUILD_TYPE=Release
 
 source ${PX4_SRC_DIR}/catkin_ws/devel/setup.bash
 export CATKIN_SETUP_UTIL_ARGS=--extend
+export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:${PX4_SRC_DIR}/catkin_ws/src/avoidance/sim/models
 
 source $DIR/rostest_px4_run.sh "$@"
 
