04dbd407234a4da39430d37bd8893bd6d3ac35a0
Beat Küng <beat-kueng@gmx.net>
2018-07-23 18:52:08 +0200

    rc lib: add CRSF Crossfire protocol (RC and Telemetry)

--- src/lib/rc/common_rc.h
+++ src/lib/rc/common_rc.h
@@ -3,6 +3,7 @@
 
 #include <stdint.h>
 
+#include "crsf.h"
 #include "dsm.h"
 #include "sbus.h"
 #include "st24.h"
@@ -11,6 +12,7 @@
 #pragma pack(push, 1)
 typedef  struct rc_decode_buf_ {
 	union {
+		crsf_frame_t crsf_frame;
 		dsm_decode_t dsm;
 		sbus_frame_t sbus_frame;
 		ReceiverFcPacket _strxpacket;
