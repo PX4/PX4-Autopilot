1ca363e71dd09b6d160ea129f8153405b4949a9c
David Sidrane <david_s5@nscdg.com>
2018-07-25 15:04:09 -0700

    aerocore2:Board does CAN GPIO init

--- src/drivers/boards/aerocore2/init.c
+++ src/drivers/boards/aerocore2/init.c
@@ -150,6 +150,9 @@ __EXPORT void board_peripheral_reset(int ms)
 __EXPORT void
 stm32_boardinitialize(void)
 {
+	/* configure LEDs */
+	board_autoled_initialize();
+
 	/* configure ADC pins */
 
 	stm32_configgpio(GPIO_ADC1_IN10);	/* used by battery sense */
@@ -163,11 +166,14 @@ stm32_boardinitialize(void)
 	/* configure spektrum power controller gpio */
 	stm32_configgpio(GPIO_SPEKTRUM_PWR_EN);
 
+	/* configure CAN interface */
+
+	stm32_configgpio(GPIO_CAN1_RX);
+	stm32_configgpio(GPIO_CAN1_TX);
+
 	/* configure SPI interfaces */
 	stm32_spiinitialize();
 
-	/* configure LEDs */
-	board_autoled_initialize();
 }
 
 /****************************************************************************
