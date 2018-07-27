3d3855e87bbfb29b3d32f584b819d7caddd6cca1
Daniel Agar <daniel@agar.ca>
2018-07-27 17:36:04 -0400

    AV-X configure default serial on UART5 (J116)

--- src/drivers/boards/av-x-v1/board_config.h
+++ src/drivers/boards/av-x-v1/board_config.h
@@ -314,10 +314,14 @@
 #define HRT_TIMER		     5  /* use timer5 for the HRT */
 #define HRT_TIMER_CHANNEL    1  /* use capture/compare channel 3 */
 
-//#define RC_UXART_BASE        STM32_USART6_BASE /* NOT FMUv5 test HW ONLY*/
-//#define RC_SERIAL_PORT       "/dev/ttyS4"
+#define RC_UXART_BASE                      STM32_UART5_BASE
+#define RC_SERIAL_PORT                     "/dev/ttyS4"
+#define BOARD_HAS_SINGLE_WIRE              1 /* HW is capable of Single Wire */
+#define BOARD_HAS_SINGLE_WIRE_ON_TX        1 /* HW default is wired as Single Wire On TX pin */
+#define BOARD_HAS_RX_TX_SWAP               1 /* HW Can swap TX and RX */
+#define RC_SERIAL_PORT_IS_SWAPED           0 /* Board wired with RC's TX is on cpu RX */
 
-#define GPS_DEFAULT_UART_PORT "/dev/ttyS5" /* UART1 on FMUv5 */
+#define GPS_DEFAULT_UART_PORT "/dev/ttyS5" /* UART7 */
 
 /* Power switch controls ******************************************************/
 
