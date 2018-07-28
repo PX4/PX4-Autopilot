36ff088551ebcaa784b0f4ecb1f79e4327e45a76
Daniel Agar <daniel@agar.ca>
2018-07-28 09:18:27 -0400

    commander params fix COM_OF_LOSS_T unit

--- src/modules/commander/commander_params.c
+++ src/modules/commander/commander_params.c
@@ -339,7 +339,7 @@ PARAM_DEFINE_INT32(COM_LOW_BAT_ACT, 0);
  * See COM_OBL_ACT and COM_OBL_RC_ACT to configure action.
  *
  * @group Commander
- * @unit second
+ * @unit s
  * @min 0
  * @max 60
  * @increment 1
