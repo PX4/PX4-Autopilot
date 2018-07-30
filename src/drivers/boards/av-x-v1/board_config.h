7b92d0c0db88dbedd8e8ead60a5f094059d24f1b
Daniel Agar <daniel@agar.ca>
2018-07-30 15:25:13 -0400

    AV-X enable GPS for rev C board

--- src/drivers/boards/av-x-v1/board_config.h
+++ src/drivers/boards/av-x-v1/board_config.h
@@ -321,7 +321,7 @@
 #define BOARD_HAS_RX_TX_SWAP               1 /* HW Can swap TX and RX */
 #define RC_SERIAL_PORT_IS_SWAPED           0 /* Board wired with RC's TX is on cpu RX */
 
-#define GPS_DEFAULT_UART_PORT "/dev/ttyS5" /* UART7 */
+#define GPS_DEFAULT_UART_PORT "/dev/ttyS6" /* UART7 */
 
 /* Power switch controls ******************************************************/
 
