453ca9c59666ff9d28add971842b67d2b8a120cb
Daniel Agar <daniel@agar.ca>
2018-08-04 16:48:47 -0400

    Jenkins px4fmu-v2 hardware test

--- Jenkinsfile
+++ Jenkinsfile
@@ -2,7 +2,7 @@ pipeline {
   agent none
   stages {
 
- stage('Analysis') {
+    stage('Analysis') {
 
       parallel {
 
--- platforms/posix/cmake/px4_impl_os.cmake
+++ platforms/posix/cmake/px4_impl_os.cmake
@@ -429,7 +429,6 @@ endfunction()
 #
 #	Input:
 #		BOARD 		: board
-#		THREADS 	: number of threads for building
 #
 #	Output:
 #		OUT	: the target list
@@ -440,7 +439,7 @@ endfunction()
 function(px4_os_prebuild_targets)
 	px4_parse_function_args(
 			NAME px4_os_prebuild_targets
-			ONE_VALUE OUT BOARD THREADS
+			ONE_VALUE OUT BOARD
 			REQUIRED OUT BOARD
 			ARGN ${ARGN})
 
