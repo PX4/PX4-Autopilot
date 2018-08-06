03b81736c650ffa8a435806f72ca7e17f8c1be1a
Alessandro Simovic <alessandro@yuneecresearch.com>
2018-08-06 11:24:24 +0200

    use double LAT/LONG fields instead of float params for storing ROI settings

--- src/modules/navigator/mission_block.cpp
+++ src/modules/navigator/mission_block.cpp
@@ -452,13 +452,15 @@ MissionBlock::issue_command(const mission_item_s &item)
 		vcmd.param2 = item.params[1];
 		vcmd.param3 = item.params[2];
 		vcmd.param4 = item.params[3];
-		vcmd.param5 = (double)item.params[4];
-		vcmd.param6 = (double)item.params[5];
 
 		if (item.nav_cmd == NAV_CMD_DO_SET_ROI_LOCATION && item.altitude_is_relative) {
+			vcmd.param5 = item.lat;
+			vcmd.param6 = item.lon;
 			vcmd.param7 = item.params[6] + _navigator->get_home_position()->alt;
 
 		} else {
+			vcmd.param5 = item.params[4];
+			vcmd.param6 = item.params[5];
 			vcmd.param7 = item.params[6];
 		}
 
