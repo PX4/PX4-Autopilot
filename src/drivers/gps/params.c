8c2e29a38b375f73f65cd736b76e640f22b0fa8f
Beat Küng <beat-kueng@gmx.net>
2018-08-06 14:02:07 +0200

    gps: add SER_GPS1_BAUD param to configure the baudrate
    The default is auto-detect, but some GPS devices might not like that, such
    as the Trimble MB-Two.

--- src/drivers/gps/params.c
+++ src/drivers/gps/params.c
@@ -85,3 +85,22 @@ PARAM_DEFINE_INT32(GPS_UBX_DYNMODEL, 7);
  * @group GPS
  */
 PARAM_DEFINE_FLOAT(GPS_YAW_OFFSET, 0.f);
+
+
+/**
+ * GPS Baudrate
+ *
+ * Configure the Baudrate for the GPS Serial Port. In most cases this can be set to Auto.
+ *
+ * The Trimble MB-Two GPS does not support auto-detection and uses a baudrate of 115200.
+ *
+ * @value 0 Auto
+ * @value 9600 9600 8N1
+ * @value 19200 19200 8N1
+ * @value 38400 38400 8N1
+ * @value 57600 57600 8N1
+ * @value 115200 115200 8N1
+ * @reboot_required true
+ * @group GPS
+ */
+PARAM_DEFINE_INT32(SER_GPS1_BAUD, 0);
