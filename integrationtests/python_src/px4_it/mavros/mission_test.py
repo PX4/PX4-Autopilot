a328cff115d3871e54a5a7ac3f9b755de3da5240
Beat Küng <beat-kueng@gmx.net>
2018-08-06 14:13:00 +0200

    fixes for integration and unit tests
    Move ros logs dir so that .ulg files are not in the same directory
    (mission_test.py:: get_last_log() fails otherwise)

--- integrationtests/python_src/px4_it/mavros/mission_test.py
+++ integrationtests/python_src/px4_it/mavros/mission_test.py
@@ -60,8 +60,10 @@ def get_last_log():
     try:
         log_path = os.environ['PX4_LOG_DIR']
     except KeyError:
-        log_path = os.path.join(os.environ['HOME'],
-                                '.ros/rootfs/fs/microsd/log')
+        try:
+            log_path = os.path.join(os.environ['ROS_HOME'], 'log')
+        except KeyError:
+            log_path = os.path.join(os.environ['HOME'], '.ros/log')
     last_log_dir = sorted(glob.glob(os.path.join(log_path, '*')))[-1]
     last_log = sorted(glob.glob(os.path.join(last_log_dir, '*.ulg')))[-1]
     return last_log
