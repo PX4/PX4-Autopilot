9551dcb49714c6783b449774f81af4287ade05d5
Daniel Agar <daniel@agar.ca>
2018-08-07 10:06:52 -0400

    logger increase log_writer_file stack 1072 -> 1150

--- src/modules/logger/log_writer_file.cpp
+++ src/modules/logger/log_writer_file.cpp
@@ -176,7 +176,7 @@ int LogWriterFile::thread_start()
 	param.sched_priority = SCHED_PRIORITY_DEFAULT - 40;
 	(void)pthread_attr_setschedparam(&thr_attr, &param);
 
-	pthread_attr_setstacksize(&thr_attr, PX4_STACK_ADJUSTED(1072));
+	pthread_attr_setstacksize(&thr_attr, PX4_STACK_ADJUSTED(1150));
 
 	int ret = pthread_create(&_thread, &thr_attr, &LogWriterFile::run_helper, this);
 	pthread_attr_destroy(&thr_attr);
