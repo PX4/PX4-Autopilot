ab7f68f2adb6a9473d086621d433daee8c428d02
Beat Küng <beat-kueng@gmx.net>
2018-08-10 18:49:56 +0200

    ekf2 replay: refactor to use unified rcS scripts

--- Tools/sitl_run.sh
+++ Tools/sitl_run.sh
@@ -36,19 +36,6 @@ then
 	model="iris"
 fi
 
-# check replay mode
-if [ "$replay_mode" == "ekf2" ]
-then
-	model="iris_replay"
-	# create the publisher rules
-	publisher_rules_file="$rootfs/orb_publisher.rules"
-	cat <<EOF > "$publisher_rules_file"
-restrict_topics: sensor_combined, vehicle_gps_position, vehicle_land_detected
-module: replay
-ignore_others: false
-EOF
-fi
-
 if [ "$#" -lt 7 ]
 then
 	echo usage: sitl_run.sh sitl_bin rcS_path debugger program model src_path build_path
