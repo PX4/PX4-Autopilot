ad0c7bec5e6ef07231044e42174d4a2114c84439
Beat Küng <beat-kueng@gmx.net>
2018-08-10 19:22:41 +0200

    SITL: handle 'make posix_sitl_shell' via unified rcS
    The command is now: 'make posix none_shell'
    which means: don't start a simulator and run a minimal shell only.

--- platforms/posix/cmake/sitl_target.cmake
+++ platforms/posix/cmake/sitl_target.cmake
@@ -45,9 +45,9 @@ ExternalProject_Add_Step(sitl_gazebo forceconfigure
 	)
 
 # create targets for each viewer/model/debugger combination
-set(viewers none jmavsim gazebo replay)
+set(viewers none jmavsim gazebo)
 set(debuggers none ide gdb lldb ddd valgrind callgrind)
-set(models none iris iris_opt_flow iris_vision iris_rplidar iris_irlock standard_vtol plane solo tailsitter typhoon_h480 rover hippocampus tiltrotor)
+set(models none shell iris iris_opt_flow iris_vision iris_rplidar iris_irlock standard_vtol plane solo tailsitter typhoon_h480 rover hippocampus tiltrotor)
 set(all_posix_vmd_make_targets)
 foreach(viewer ${viewers})
 	foreach(debugger ${debuggers})
