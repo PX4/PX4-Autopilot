1fb70460520408519df2bb3966ae9a03c4c2ea43
David Sidrane <david_s5@nscdg.com>
2018-08-17 10:36:37 -0700

    stm32 tone_alarm: Remove stale documentation & cleanup
        Since tune control came in this driver does not have a
        have a test based interface. The module Documentation
        has been moved to tune_control
    
        minor cleanup and assert on workque delay of 0

--- src/drivers/stm32/tone_alarm/tone_alarm.cpp
+++ src/drivers/stm32/tone_alarm/tone_alarm.cpp
@@ -476,16 +476,11 @@ void ToneAlarm::next_note()
 
 	if (updated) {
 		orb_copy(ORB_ID(tune_control), _tune_control_sub, &_tune);
-
-		if (_tunes.set_control(_tune) == 0) {
-			_play_tone = true;
-
-		} else {
-			_play_tone = false;
-		}
+		_play_tone = _tunes.set_control(_tune) == 0;
 	}
 
-	unsigned frequency = 0, duration = 0;
+	unsigned frequency = 0;
+	unsigned duration = 0;
 
 	if (_play_tone) {
 		_play_tone = false;
@@ -516,6 +511,7 @@ void ToneAlarm::next_note()
 	}
 
 	// and arrange a callback when the note should stop
+	assert(duration != 0);
 	work_queue(HPWORK, &_work, (worker_t)&ToneAlarm::next_trampoline, this, USEC2TICK(duration));
 }
 
