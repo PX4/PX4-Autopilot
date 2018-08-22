d7c338b4b41e02ea0d79e5d8dec7549175002de8
zengjg <zengjgkeynes@163.com>
2018-08-22 12:59:21 +0800

    the loiter yaw should remain NAN  when it is not multirotor

--- src/modules/navigator/mission_block.cpp
+++ src/modules/navigator/mission_block.cpp
@@ -601,7 +601,11 @@ MissionBlock::set_loiter_item(struct mission_item_s *item, float min_clearance)
 		}
 
 		item->altitude_is_relative = false;
-		item->yaw = _navigator->get_global_position()->yaw;
+		if(_navigator->get_vstatus()->is_rotary_wing){
+			item->yaw = _navigator->get_global_position()->yaw;
+		} else {
+			item->yaw = NAN;
+		}
 		item->loiter_radius = _navigator->get_loiter_radius();
 		item->acceptance_radius = _navigator->get_acceptance_radius();
 		item->time_inside = 0.0f;
