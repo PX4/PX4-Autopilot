c1cab08ba485b0d17d206c4b956adee33c4f2560
Daniel Agar <daniel@agar.ca>
2018-08-30 11:48:55 -0400

    ECL_Controller base needs virtual destructor

--- attitude_fw/ecl_controller.h
+++ attitude_fw/ecl_controller.h
@@ -76,7 +76,7 @@ class ECL_Controller
 {
 public:
 	ECL_Controller(const char *name);
-	~ECL_Controller() = default;
+	virtual ~ECL_Controller() = default;
 
 	virtual float control_attitude(const struct ECL_ControlData &ctl_data) = 0;
 	virtual float control_euler_rate(const struct ECL_ControlData &ctl_data) = 0;
