e88a839ed21b518b594bc8c0cbb5e35043db63e4
Daniel Agar <daniel@agar.ca>
2018-09-02 12:59:00 -0400

    add px4fmu-v{3,4,5} stackcheck configs

---
+++ cmake/configs/nuttx_px4fmu-v4_stackcheck.cmake
@@ -0,0 +1,116 @@
+
+px4_nuttx_configure(HWCLASS m4 CONFIG stackcheck ROMFS y ROMFSROOT px4fmu_common)
+
+set(config_uavcan_num_ifaces 1)
+
+set(config_module_list
+	#
+	# Board support modules
+	#
+	drivers/barometer
+	drivers/differential_pressure
+	drivers/distance_sensor
+	drivers/magnetometer
+	drivers/telemetry
+	drivers/imu
+
+	drivers/batt_smbus
+	drivers/blinkm
+	drivers/camera_trigger
+	drivers/gps
+	drivers/irlock
+	drivers/mkblctrl
+	drivers/oreoled
+	drivers/pca9685
+	drivers/pwm_input
+	drivers/pwm_out_sim
+	drivers/px4flow
+	drivers/px4fmu
+	drivers/rgbled
+	drivers/rc_input
+	drivers/stm32
+	drivers/stm32/adc
+	drivers/stm32/tone_alarm
+	drivers/tap_esc
+	drivers/vmount
+	modules/sensors
+
+	#
+	# System commands
+	#
+	systemcmds/bl_update
+	systemcmds/config
+	systemcmds/dumpfile
+	systemcmds/esc_calib
+	systemcmds/hardfault_log
+	systemcmds/led_control
+	systemcmds/mixer
+	systemcmds/motor_ramp
+	systemcmds/mtd
+	systemcmds/nshterm
+	systemcmds/param
+	systemcmds/perf
+	systemcmds/pwm
+	systemcmds/reboot
+	systemcmds/sd_bench
+	systemcmds/top
+	systemcmds/topic_listener
+	systemcmds/tune_control
+	systemcmds/ver
+
+	#
+	# Testing
+	#
+	drivers/distance_sensor/sf0x/sf0x_tests
+	drivers/test_ppm
+	#lib/rc/rc_tests
+	modules/commander/commander_tests
+	lib/controllib/controllib_test
+	modules/mavlink/mavlink_tests
+	modules/uORB/uORB_tests
+	systemcmds/tests
+
+	#
+	# General system control
+	#
+	modules/camera_feedback
+	modules/commander
+	modules/events
+	modules/gpio_led
+	modules/land_detector
+	modules/load_mon
+	modules/mavlink
+	modules/navigator
+	#modules/uavcan
+
+	#
+	# Estimation modules
+	#
+	modules/attitude_estimator_q
+	modules/ekf2
+	modules/landing_target_estimator
+	modules/local_position_estimator
+	modules/position_estimator_inav
+	modules/wind_estimator
+
+	#
+	# Vehicle Control
+	#
+	modules/fw_att_control
+	modules/fw_pos_control_l1
+	modules/gnd_att_control
+	modules/gnd_pos_control
+	modules/mc_att_control
+	modules/mc_pos_control
+	modules/vtol_att_control
+
+	#
+	# Logging
+	#
+	modules/logger
+
+	#
+	# Library modules
+	#
+	modules/dataman
+)
