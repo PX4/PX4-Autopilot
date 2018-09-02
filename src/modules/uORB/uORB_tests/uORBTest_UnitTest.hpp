f30d4add275017bf7e2ee0bbceb311d7ffba1f95
Daniel Agar <daniel@agar.ca>
2018-09-02 15:08:28 -0400

    uORB tests increase stack sizes

--- src/modules/uORB/uORB_tests/uORBTest_UnitTest.hpp
+++ src/modules/uORB/uORB_tests/uORBTest_UnitTest.hpp
@@ -147,7 +147,7 @@ int uORBTest::UnitTest::latency_test(orb_id_t T, bool print)
 	int pubsub_task = px4_task_spawn_cmd("uorb_latency",
 					     SCHED_DEFAULT,
 					     SCHED_PRIORITY_MAX,
-					     1700,
+					     2000,
 					     (px4_main_t)&uORBTest::UnitTest::pubsubtest_threadEntry,
 					     args);
 
