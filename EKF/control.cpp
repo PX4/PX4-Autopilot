eb7ff54315787d484a0fd6c0b2ec7bc9409d60a2
Kabir Mohammed <mhkabir98@gmail.com>
2018-09-03 00:26:43 -0400

    EKF : Fix divergence when optical flow is not fused for a long time (#503)
    * terrain_estimator : guard against case where latest range sample is newer than IMU sample
    
    * EKF : control : correct detection of no optical flow fusion over a time period

--- EKF/control.cpp
+++ EKF/control.cpp
@@ -420,7 +420,7 @@ void Ekf::controlOpticalFlowFusion()
 			       _control_status.flags.opt_flow = false;
 			       _time_last_of_fuse = 0;
 
-			} else if (_time_last_imu - _flow_sample_delayed.time_us > (uint64_t)_params.no_gps_timeout_max) {
+			} else if (_time_last_imu - _time_last_of_fuse > (uint64_t)_params.no_gps_timeout_max) {
 				_control_status.flags.opt_flow = false;
 
 			}
