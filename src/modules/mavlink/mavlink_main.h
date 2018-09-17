renzed5a/Firmware

mavlink temporary workarounds for dronekit: added parameters to disable hash check and heartbeat forwarding - hash check disabling: for systems where a companion link forwards messages to QGC (e.g. via LTE) parameter streaming will be stopped as soon as QGC sends the request to stop the stream. If the companion side for some reason still requires the stream to be active we need to disable QGC from stopping the stream. Normally dronekit should be responsible for filtering out the request from QGC but apparently this is not possible.
- disable heartbeat forwarding: dronekit does seem to get confused if
heartbeats from another system other than the autopilot get forwarded to it.
Example: Sending messages from QGC via the autopilot to dronekit running on
companion computer.

Signed-off-by: Roman <bapstroman@gmail.com>

src/modules/mavlink/mavlink_main.h
@@ -487,6 +487,10 @@ public:
 
 	bool ftp_enabled() const { return _ftp_on; }
 
+	bool hash_check_enabled() { return _param_hash_check_enabled.get(); }
+
+	bool forward_heartbeats_enabled() { return _param_heartbeat_forwarding_enabled.get(); }
+
 	struct ping_statistics_s {
 		uint64_t last_ping_time;
 		uint32_t last_ping_seq;
@@ -629,7 +633,9 @@ private:
 		(ParamInt<px4::params::MAV_TYPE>) _param_system_type,
 		(ParamBool<px4::params::MAV_USEHILGPS>) _param_use_hil_gps,
 		(ParamBool<px4::params::MAV_FWDEXTSP>) _param_forward_externalsp,
-		(ParamInt<px4::params::MAV_BROADCAST>) _param_broadcast_mode
+		(ParamInt<px4::params::MAV_BROADCAST>) _param_broadcast_mode,
+		(ParamInt<px4::params::MAV_EN_HASH_CHK>) _param_hash_check_enabled,
+		(ParamInt<px4::params::MAV_EN_HB_FORW>) _param_heartbeat_forwarding_enabled
 	)
 
 	perf_counter_t		_loop_perf;			/**< loop performance counter */

PX4/PX4-Autopilot

src/modules/mavlink/mavlink_main.h
@@ -487,6 +487,10 @@ public:
 
 	bool ftp_enabled() const { return _ftp_on; }
 
+	bool hash_check_enabled() { return _param_hash_check_enabled.get(); }
+
+	bool forward_heartbeats_enabled() { return _param_heartbeat_forwarding_enabled.get(); }
+
 	struct ping_statistics_s {
 		uint64_t last_ping_time;
 		uint32_t last_ping_seq;
@@ -629,7 +633,9 @@ private:
 		(ParamInt<px4::params::MAV_TYPE>) _param_system_type,
 		(ParamBool<px4::params::MAV_USEHILGPS>) _param_use_hil_gps,
 		(ParamBool<px4::params::MAV_FWDEXTSP>) _param_forward_externalsp,
-		(ParamInt<px4::params::MAV_BROADCAST>) _param_broadcast_mode
+		(ParamInt<px4::params::MAV_BROADCAST>) _param_broadcast_mode,
+		(ParamBool<px4::params::MAV_HASH_CHK_EN>) _param_hash_check_enabled,
+		(ParamBool<px4::params::MAV_HB_FORW_EN>) _param_heartbeat_forwarding_enabled
 	)
 
 	perf_counter_t		_loop_perf;			/**< loop performance counter */
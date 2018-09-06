1d74a04eceeb3357817034de44e2a5cd6308c1f6
Roman <bapstroman@gmail.com>
2018-09-06 11:43:28 +0200

    mavlink temporary workarounds for dronekit: added parameters to disable hash check and heartbeat forwarding - hash check disabling: for systems where a companion link forwards messages to QGC (e.g. via LTE) parameter streaming will be stopped as soon as QGC sends the request to stop the stream. If the companion side for some reason still requires the stream to be active we need to disable QGC from stopping the stream. Normally dronekit should be responsible for filtering out the request from QGC but apparently this is not possible.
    - disable heartbeat forwarding: dronekit does seem to get confused if
    heartbeats from another system other than the autopilot get forwarded to it.
    Example: Sending messages from QGC via the autopilot to dronekit running on
    companion computer.
    
    Signed-off-by: Roman <bapstroman@gmail.com>

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -546,7 +546,8 @@ Mavlink::forward_message(const mavlink_message_t *msg, Mavlink *self)
 			// Broadcast or addressing this system and not trying to talk
 			// to the autopilot component -> pass on to other components
 			if ((target_system_id == 0 || target_system_id == self->get_system_id())
-			    && (target_component_id == 0 || target_component_id != self->get_component_id())) {
+			    && (target_component_id == 0 || target_component_id != self->get_component_id())
+			    && !(!self->forward_heartbeats_enabled() && msg->msgid == MAVLINK_MSG_ID_HEARTBEAT)) {
 
 				inst->pass_message(msg);
 			}
