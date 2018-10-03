viktorRau/PX4_Firmware_adjusted

sim:Use Non Arch specific Board identity API

src/drivers/boards/sim/board_config.h
@@ -7,8 +7,7 @@
 
 #define PX4_I2C_OBDEV_LED	0x55
 
-#define BOARD_OVERRIDE_UUID "SIMULATIONID"
-#define BOARD_OVERRIDE_PX4_GUID "00000000" BOARD_OVERRIDE_UUID // must be of length 18 (PX4_GUID_BYTE_LENGTH)
-
+#define BOARD_OVERRIDE_UUID "SIMULATIONID0000" // must be of length 16
+#define PX4_SOC_ARCH_ID 0x1001
 #define BOARD_HAS_NO_RESET
 #define BOARD_HAS_NO_BOOTLOADER

PX4/PX4-Autopilot

platforms/qurt/include/board_config.h
@@ -7,8 +7,8 @@
 #define PX4_I2C_BUS_EXPANSION	3
 #define PX4_I2C_BUS_LED		3
 
-#define BOARD_OVERRIDE_UUID "SYSMEMID0000   " // must be of length 16
-#define PX4_SOC_ARCH_ID 0x1000
+#define BOARD_OVERRIDE_UUID "SYSTEMID0000    " // must be of length 16
+#define PX4_SOC_ARCH_ID 0x1001
 
 #define BOARD_HAS_NO_RESET
 #define BOARD_HAS_NO_BOOTLOADER
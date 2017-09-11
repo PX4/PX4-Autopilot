73552bdd64869082b3c079cd4916f093fa784744
David Sidrane <david_s5@nscdg.com>
2017-09-11 13:32:13 -1000

    sim:Use Non Arch specific Board identity API

--- platforms/qurt/include/board_config.h
+++ platforms/qurt/include/board_config.h
@@ -7,8 +7,8 @@
 #define PX4_I2C_BUS_EXPANSION	3
 #define PX4_I2C_BUS_LED		3
 
-#define BOARD_OVERRIDE_UUID "SYSMEMID0000   " // must be of length 16
-#define PX4_SOC_ARCH_ID 0x1000
+#define BOARD_OVERRIDE_UUID "SYSTEMID0000    " // must be of length 16
+#define PX4_SOC_ARCH_ID 0x1001
 
 #define BOARD_HAS_NO_RESET
 #define BOARD_HAS_NO_BOOTLOADER
