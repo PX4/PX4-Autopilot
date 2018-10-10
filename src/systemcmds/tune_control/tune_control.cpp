9e6c4e61a6876e4ecdc297a82be70993685661b4
Hamish Willee <hamishwillee@gmail.com>
2018-10-10 14:48:11 -0700

    Fix jenkins build error in tune_control

--- src/systemcmds/tune_control/tune_control.cpp
+++ src/systemcmds/tune_control/tune_control.cpp
@@ -89,7 +89,7 @@ $ tune_control play -t 2
         PRINT_MODULE_USAGE_PARAM_INT('f', 0, 0, 22, "Frequency of tone in Hz (0-22kHz)", true);
 	PRINT_MODULE_USAGE_PARAM_INT('d', 1, 1, 21, "Duration of tone in us", true);
 	PRINT_MODULE_USAGE_PARAM_INT('s', 40, 0, 100, "Strength of tone (0-100)", true);
-	PRINT_MODULE_USAGE_PARAM_STRING('m', nullptr, "<string> - e.g. \"MFT200e8a8a\"",
+	PRINT_MODULE_USAGE_PARAM_STRING('m', nullptr,  R"(<string> - e.g. "MFT200e8a8a")",
 					 "Melody in string form", true);
 	PRINT_MODULE_USAGE_COMMAND_DESCR("libtest","Test library");
 	PRINT_MODULE_USAGE_COMMAND_DESCR("stop","Stop playback (use for repeated tunes)");
