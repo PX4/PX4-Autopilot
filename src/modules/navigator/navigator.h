86673ecfcb4c1af3c63bd5d46d0e395ac2c4ee7b
Beat Küng <beat-kueng@gmx.net>
2018-10-23 12:02:56 +0200

    navigator: remove dependency on vtol module by using the C param API
    This is the quickest way to achieve the desired result.
    Long-term we might do something else.

--- src/modules/navigator/navigator.h
+++ src/modules/navigator/navigator.h
@@ -283,8 +283,8 @@ public:
 	float		get_yaw_timeout() const { return _param_yaw_timeout.get(); }
 	float		get_yaw_threshold() const { return _param_yaw_err.get(); }
 
-	float		get_vtol_back_trans_deceleration() const { return _param_back_trans_dec_mss.get(); }
-	float		get_vtol_reverse_delay() const { return _param_reverse_delay.get(); }
+	float		get_vtol_back_trans_deceleration() const { return _param_back_trans_dec_mss; }
+	float		get_vtol_reverse_delay() const { return _param_reverse_delay; }
 
 	bool		force_vtol();
 
@@ -371,12 +371,13 @@ private:
 		(ParamFloat<px4::params::MIS_LTRMIN_ALT>) _param_loiter_min_alt,
 		(ParamFloat<px4::params::MIS_TAKEOFF_ALT>) _param_takeoff_min_alt,
 		(ParamFloat<px4::params::MIS_YAW_TMT>) _param_yaw_timeout,
-		(ParamFloat<px4::params::MIS_YAW_ERR>) _param_yaw_err,
-
-		// VTOL parameters TODO: get these out of navigator
-		(ParamFloat<px4::params::VT_B_DEC_MSS>) _param_back_trans_dec_mss,
-		(ParamFloat<px4::params::VT_B_REV_DEL>) _param_reverse_delay
+		(ParamFloat<px4::params::MIS_YAW_ERR>) _param_yaw_err
 	)
+
+	param_t _handle_back_trans_dec_mss{PARAM_INVALID};
+	param_t _handle_reverse_delay{PARAM_INVALID};
+	float _param_back_trans_dec_mss{0.f};
+	float _param_reverse_delay{0.f};
 
 	float _mission_cruising_speed_mc{-1.0f};
 	float _mission_cruising_speed_fw{-1.0f};
