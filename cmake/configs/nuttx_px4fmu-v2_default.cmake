dc62454f0a0e09b0600885e51017a545a1c668e4
Beat Küng <beat-kueng@gmx.net>
2018-10-24 08:56:17 +0200

    px4fmu-v2_default: disable sf1xx distance sensor
    Due to limited flash space

--- cmake/configs/nuttx_px4fmu-v2_default.cmake
+++ cmake/configs/nuttx_px4fmu-v2_default.cmake
@@ -35,7 +35,7 @@ set(config_module_list
 	drivers/distance_sensor/ll40ls
 	#drivers/distance_sensor/mb12xx
 	drivers/distance_sensor/sf0x
-	drivers/distance_sensor/sf1xx
+	#drivers/distance_sensor/sf1xx
 	#drivers/distance_sensor/srf02
 	#drivers/distance_sensor/teraranger
 	#drivers/distance_sensor/tfmini
