0b84e8c4f7e4351af568e91bd8a9a238fb99f160
Martina <martina@rivizzigno.it>
2018-10-30 19:12:59 +0100

    rostest_avoidance_run: source gazebo setup.sh to remove gzserver core dumped

--- test/rostest_avoidance_run.sh
+++ test/rostest_avoidance_run.sh
@@ -12,6 +12,8 @@ catkin init
 catkin build local_planner --cmake-args -DCMAKE_BUILD_TYPE=Release
 
 source ${PX4_SRC_DIR}/catkin_ws/devel/setup.bash
+source /usr/share/gazebo/setup.sh
+
 export CATKIN_SETUP_UTIL_ARGS=--extend
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:${PX4_SRC_DIR}/catkin_ws/src/avoidance/sim/models
 
