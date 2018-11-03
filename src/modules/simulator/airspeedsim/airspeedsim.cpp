eb5d9332c6efdc354da8e535ea8f37616fd8a029
Daniel Agar <daniel@agar.ca>
2018-11-03 15:22:40 -0400

    delete unused IOCTL AIRSPEEDIOCGSCALE

--- src/modules/simulator/airspeedsim/airspeedsim.cpp
+++ src/modules/simulator/airspeedsim/airspeedsim.cpp
@@ -257,13 +257,6 @@ AirspeedSim::ioctl(cdev::file_t *filp, int cmd, unsigned long arg)
 			return OK;
 		}
 
-	case AIRSPEEDIOCGSCALE: {
-			struct airspeed_scale *s = (struct airspeed_scale *)arg;
-			s->offset_pa = _diff_pres_offset;
-			s->scale = 1.0f;
-			return OK;
-		}
-
 	default:
 		/* give it to the superclass */
 		//return I2C::ioctl(filp, cmd, arg);	XXX SIM should be super class
