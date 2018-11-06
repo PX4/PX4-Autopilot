be7416c823500627a525b12de9581f0cca253390
Simone Guscetti <simone@yuneecresearch.com>
2018-11-06 15:21:31 +0100

    FlightTask Auto: Adapt to new message and rename variable

--- src/lib/FlightTasks/tasks/Auto/FlightTaskAuto.cpp
+++ src/lib/FlightTasks/tasks/Auto/FlightTaskAuto.cpp
@@ -224,7 +224,7 @@ bool FlightTaskAuto::_evaluateTriplets()
 	if (triplet_update || (_current_state != previous_state)) {
 		_updateInternalWaypoints();
 		_updateAvoidanceWaypoints();
-		_deploy_gear = _sub_triplet_setpoint->get().current.deploy_gear;
+		_mission_gear = _sub_triplet_setpoint->get().current.landing_gear;
 	}
 
 	if (MPC_OBS_AVOID.get() && _sub_vehicle_status->get().is_rotary_wing) {
