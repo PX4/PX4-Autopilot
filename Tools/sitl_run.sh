23acb77e2d52121fc109dfd09852f74b801bad4c
Julian Oes <julian@oes.ch>
2018-11-13 15:24:39 +0100

    Tools: add PX4_SIM_SPEED_FACTOR as env variable
    This adds the env variable PX4_SIM_SPEED_FACTOR to set the speed factor
    to run SITL at.

--- Tools/sitl_run.sh
+++ Tools/sitl_run.sh
@@ -66,10 +66,18 @@ done
 
 SIM_PID=0
 
+# Allow speed factor to bet set from environment.
+if [[ -n "$PX4_SIM_SPEED_FACTOR" ]];
+then
+    speed_factor=$PX4_SIM_SPEED_FACTOR
+else
+    speed_factor=1
+fi
+
 if [ "$program" == "jmavsim" ] && [ ! -n "$no_sim" ]
 then
 	# Start Java simulator
-	"$src_path"/Tools/jmavsim_run.sh -r 250 -f 1 &
+	"$src_path"/Tools/jmavsim_run.sh -r 250 -f $speed_factor &
 	SIM_PID=`echo $!`
 elif [ "$program" == "gazebo" ] && [ ! -n "$no_sim" ]
 then
