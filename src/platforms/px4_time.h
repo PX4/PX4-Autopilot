4a0180815ccacecb614aad374d0bdc6e00bb11e1
Julian Oes <julian@oes.ch>
2018-11-15 11:36:33 +0100

    platforms: fix linking issues for NuttX build

--- src/platforms/px4_time.h
+++ src/platforms/px4_time.h
@@ -21,7 +21,7 @@ __EXPORT unsigned int sleep(unsigned int sec);
 
 __END_DECLS
 
-#else
+#elif defined(__PX4_POSIX)
 
 __BEGIN_DECLS
 __EXPORT int px4_clock_gettime(clockid_t clk_id, struct timespec *tp);
@@ -33,5 +33,12 @@ __EXPORT int px4_pthread_cond_timedwait(pthread_cond_t *cond,
 					pthread_mutex_t *mutex,
 					const struct timespec *abstime);
 __END_DECLS
+
+#elif defined(__PX4_NUTTX)
+
+#define px4_clock_gettime system_clock_gettime
+#define px4_clock_settime system_clock_settime
+#define px4_usleep system_usleep
+#define px4_sleep system_sleep
 
 #endif
