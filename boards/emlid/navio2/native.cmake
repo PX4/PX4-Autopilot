409453d31d71b72535bef98faa8ff4b9e83508c5
Daniel Agar <daniel@agar.ca>
2018-11-24 12:12:58 -0500

    move dyn_hello to examples and restore testing

--- boards/emlid/navio2/native.cmake
+++ boards/emlid/navio2/native.cmake
@@ -83,6 +83,7 @@ px4_add_board(
 
 	EXAMPLES
 		bottle_drop # OBC challenge
+		dyn_hello # dynamically loading modules example
 		fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
 		hello
 		#hwtest # Hardware test
