1c50ca2d0b423346b1caf4568f5bee16315dcdab
David Sidrane <david_s5@nscdg.com>
2018-11-27 10:13:43 -0800

    Update Nuttx and NuttX-apps to 7.28+

--- platforms/nuttx/cmake/px4_impl_os.cmake
+++ platforms/nuttx/cmake/px4_impl_os.cmake
@@ -53,7 +53,9 @@ function(px4_os_add_flags)
 
 	include_directories(BEFORE SYSTEM
 		${PX4_BINARY_DIR}/NuttX/nuttx/include
+
 		${PX4_BINARY_DIR}/NuttX/nuttx/include/cxx
+		${PX4_SOURCE_DIR}/platforms/nuttx/NuttX/include/cxx
 	)
 
 	include_directories(
@@ -63,6 +65,12 @@ function(px4_os_add_flags)
 
 		${PX4_BINARY_DIR}/NuttX/apps/include
 		)
+
+	# NuttX's disables inline below C99 (comiler.h), but __STDC_VERSION__ isn't set for C++
+	add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-D__STDC_VERSION__=199901L>)
+
+	# prevent using the toolchain's std c++ library
+	add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-nostdinc++>)
 
 	add_definitions(
 		-D__PX4_NUTTX
