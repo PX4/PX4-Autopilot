14b83f7bfc2e014afb8a83d92817f59d8dfc3bfc
Matthias Grob <maetugr@gmail.com>
2018-12-05 11:37:39 +0100

    mavlink: add orbit to the normal messages
    It is only actually sent out when the orbit is running.

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -1777,6 +1777,7 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 		configure_stream_local("VFR_HUD", 4.0f);
 		configure_stream_local("VISION_POSITION_ESTIMATE", 1.0f);
 		configure_stream_local("WIND_COV", 1.0f);
+		configure_stream_local("ORBIT_EXECUTION_STATUS", 10.f);
 		break;
 
 	case MAVLINK_MODE_ONBOARD:
@@ -1819,6 +1820,7 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 		configure_stream_local("VFR_HUD", 10.0f);
 		configure_stream_local("VISION_POSITION_ESTIMATE", 10.0f);
 		configure_stream_local("WIND_COV", 10.0f);
+		configure_stream_local("ORBIT_EXECUTION_STATUS", 10.f);
 		break;
 
 	case MAVLINK_MODE_OSD:
@@ -1884,6 +1886,7 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 		configure_stream_local("VFR_HUD", 20.0f);
 		configure_stream_local("VISION_POSITION_ESTIMATE", 10.0f);
 		configure_stream_local("WIND_COV", 10.0f);
+		configure_stream_local("ORBIT_EXECUTION_STATUS", 10.f);
 		break;
 
 	case MAVLINK_MODE_IRIDIUM:
