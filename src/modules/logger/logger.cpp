fced1ce33ba8319ed0b2ac3ece74e89aa1bb30b0
Beat Küng <beat-kueng@gmx.net>
2018-12-07 10:51:26 +0100

    logger: remove some unused logged topics in SITL

--- src/modules/logger/logger.cpp
+++ src/modules/logger/logger.cpp
@@ -666,15 +666,12 @@ void Logger::add_default_topics()
 	add_topic("wind_estimate", 200);
 
 #ifdef CONFIG_ARCH_BOARD_PX4_SITL
-	add_topic("actuator_armed");
 	add_topic("actuator_controls_virtual_fw");
 	add_topic("actuator_controls_virtual_mc");
-	add_topic("commander_state");
 	add_topic("fw_virtual_attitude_setpoint");
 	add_topic("mc_virtual_attitude_setpoint");
 	add_topic("multirotor_motor_limits");
 	add_topic("position_controller_status");
-	add_topic("position_controller_landingstatus");
 	add_topic("offboard_control_mode");
 	add_topic("time_offset");
 	add_topic("vehicle_attitude_groundtruth", 10);
