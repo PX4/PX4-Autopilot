e49ab7f002bb3983c1bbfe23a1326945ed1d9339
Martina <martina@rivizzigno.it>
2018-12-10 16:36:57 +0100

    simulator_mavlink: add new field (horizontal, vertical fov and quaterion orientation) to distance_sensor

--- src/modules/simulator/simulator_mavlink.cpp
+++ src/modules/simulator/simulator_mavlink.cpp
@@ -1237,6 +1237,13 @@ int Simulator::publish_distance_topic(const mavlink_distance_sensor_t *dist_mavl
 	dist.variance = dist_mavlink->covariance * 1e-4f; // cm^2 to m^2
 	dist.signal_quality = -1;
 
+	dist.h_fov = dist_mavlink->h_fov;
+	dist.v_fov = dist_mavlink->v_fov;
+	dist.q[0] = dist_mavlink->q[0];
+	dist.q[1] = dist_mavlink->q[1];
+	dist.q[2] = dist_mavlink->q[2];
+	dist.q[3] = dist_mavlink->q[3];
+
 	int dist_multi;
 	orb_publish_auto(ORB_ID(distance_sensor), &_dist_pub, &dist, &dist_multi, ORB_PRIO_HIGH);
 
