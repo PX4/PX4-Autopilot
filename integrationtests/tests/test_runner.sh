fa1bbe87c33e41090beae288c348734486271410
PhiAbs <philipp@yuneecresearch.com>
2018-12-17 14:40:33 +0100

    tests run now for all tests not only for the newest

--- integrationtests/tests/test_runner.sh
+++ integrationtests/tests/test_runner.sh
@@ -7,6 +7,7 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 px4_dir=$( dirname $( dirname ${script_dir} ) )
 rootfs_test=${px4_dir}/build/px4_sitl_default/tmp/rootfs_test
 mkdir -p ${rootfs_test}
+rm -rf ${rootfs_test}/log/*  # delete the content of the log folder
 pushd ${rootfs_test} > /dev/null
 source "${px4_dir}/Tools/setup_gazebo.bash" "${px4_dir}" "${px4_dir}/build/px4_sitl_default"
 ${script_dir}/run_tests.py ${px4_dir} ${maneuver}
