a40871307dbec7334aaf8f0b7a6288de25bd4b2a
斯东Stone <bys_1123@163.com>
2018-12-17 11:56:46 +0800

    Fix px4_fmu-v2/v3 serial port mapping

--- boards/px4/fmu-v3/default.cmake
+++ boards/px4/fmu-v3/default.cmake
@@ -14,10 +14,10 @@ px4_add_board(
 	UAVCAN_INTERFACES 2
 
 	SERIAL_PORTS
-		GPS1:/dev/ttyS0
+		GPS1:/dev/ttyS3
 		TEL1:/dev/ttyS1
 		TEL2:/dev/ttyS2
-		TEL4:/dev/ttyS3
+		TEL4:/dev/ttyS6
 
 	DRIVERS
 		barometer # all available barometer drivers
