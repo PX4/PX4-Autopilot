a744338f2c4653084b86e119fb85cc0a8b8a2932
bresch <brescianimathieu@gmail.com>
2019-01-08 11:29:55 +0100

    LNDMC - Add landed shortcut if drone is already in landed state and throttle is low to avoid detecting takeoff due to measurement and estimation inaccuracies

--- src/modules/land_detector/MulticopterLandDetector.cpp
+++ src/modules/land_detector/MulticopterLandDetector.cpp
@@ -249,8 +249,8 @@ bool MulticopterLandDetector::_get_maybe_landed_state()
 
 bool MulticopterLandDetector::_get_landed_state()
 {
-	// When not armed, consider to be landed
-	if (!_arming.armed) {
+	// if disarmed or if already landed and that the drone has low thrust, it has to be on the ground
+	if (!_arming.armed || (_state == LandDetectionState::LANDED && _has_low_thrust())) {
 		return true;
 	}
 
