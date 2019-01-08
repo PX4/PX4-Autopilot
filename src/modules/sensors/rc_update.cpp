9dbf52e07548f8030dee059939b0af1a4b418f13
bresch <brescianimathieu@gmail.com>
2019-01-08 14:16:13 +0100

    AMS - Remove double ams_switch

--- src/modules/sensors/rc_update.cpp
+++ src/modules/sensors/rc_update.cpp
@@ -444,8 +444,6 @@ RCUpdate::rc_poll(const ParameterHandles &parameter_handles)
 					     _parameters.rc_stab_th, _parameters.rc_stab_inv);
 			manual.man_switch = get_rc_sw2pos_position(rc_channels_s::RC_CHANNELS_FUNCTION_MAN,
 					    _parameters.rc_man_th, _parameters.rc_man_inv);
-			manual.ams_switch = get_rc_sw2pos_position(rc_channels_s::RC_CHANNELS_FUNCTION_AMS,
-					    _parameters.rc_ams_th, _parameters.rc_ams_inv);
 
 			/* publish manual_control_setpoint topic */
 			orb_publish_auto(ORB_ID(manual_control_setpoint), &_manual_control_pub, &manual, &instance,
