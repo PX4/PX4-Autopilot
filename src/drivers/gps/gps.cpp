03d466f02d2ed5e341223ebef9cbd29e22af0713
Daniel Agar <daniel@agar.ca>
2019-01-18 11:44:02 -0500

    GPS increase task stack 1530 -> 1600 bytes

--- src/drivers/gps/gps.cpp
+++ src/drivers/gps/gps.cpp
@@ -998,7 +998,7 @@ int GPS::task_spawn(int argc, char *argv[], Instance instance)
 	}
 
 	int task_id = px4_task_spawn_cmd("gps", SCHED_DEFAULT,
-				   SCHED_PRIORITY_SLOW_DRIVER, 1530,
+				   SCHED_PRIORITY_SLOW_DRIVER, 1600,
 				   entry_point, (char *const *)argv);
 
 	if (task_id < 0) {
