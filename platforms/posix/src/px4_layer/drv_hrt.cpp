c6718a5bca0094f2a7c5199ed39a503b2599baf4
Lorenz Meier <lorenz@px4.io>
2019-01-20 17:28:55 +0100

    Posix: Add initial stub for atomic HRT The actual implementation is not atomic, as the value on the application layer would be limited.

--- platforms/posix/src/px4_layer/drv_hrt.cpp
+++ platforms/posix/src/px4_layer/drv_hrt.cpp
@@ -212,6 +212,20 @@ hrt_abstime hrt_elapsed_time(const volatile hrt_abstime *then)
 }
 
 /*
+ * Compute the delta between a timestamp taken in the past
+ * and now.
+ *
+ * This function is safe to use even if the timestamp is updated
+ * by an interrupt during execution.
+ */
+hrt_abstime hrt_elapsed_time_atomic(const volatile hrt_abstime *then)
+{
+	// This is not atomic as the value on the application layer of POSIX is limited.
+	hrt_abstime delta = hrt_absolute_time() - *then;
+	return delta;
+}
+
+/*
  * Store the absolute time in an interrupt-safe fashion.
  *
  * This function ensures that the timestamp cannot be seen half-written by an interrupt handler.
