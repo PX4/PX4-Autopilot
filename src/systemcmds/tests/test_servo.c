dd855dab5c94a4080655cff4bde728b153b64e37
mcsauder <mcsauder@gmail.com>
2019-01-22 05:49:24 -0700

    Standardized/updated copyright file headers in the src/systemcmds/tests/ directory.

--- src/systemcmds/tests/test_servo.c
+++ src/systemcmds/tests/test_servo.c
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2012, 2013, 2017 PX4 Development Team. All rights reserved.
+ *  Copyright (C) 2012-2019 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -34,7 +34,6 @@
 /**
  * @file test_servo.c
  * Tests the servo outputs
- *
  */
 
 #include <px4_time.h>
--- src/systemcmds/tests/tests_main.c
+++ src/systemcmds/tests/tests_main.c
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2012-2015 PX4 Development Team. All rights reserved.
+ *  Copyright (C) 2012-2019 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -50,21 +50,12 @@
 // Not using Eigen at the moment
 #define TESTS_EIGEN_DISABLE
 
-#include "tests_main.h"
-
-/****************************************************************************
- * Private Function Prototypes
- ****************************************************************************/
 
 static int test_help(int argc, char *argv[]);
 static int test_runner(unsigned option);
 
 static int test_all(int argc, char *argv[]);
 static int test_jig(int argc, char *argv[]);
-
-/****************************************************************************
- * Private Data
- ****************************************************************************/
 
 const struct {
 	const char 	*name;
