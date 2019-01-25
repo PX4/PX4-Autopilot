9a13ba414582cc8c9d59c0781b4d6f46389e4d82
David Sidrane <david_s5@nscdg.com>
2019-01-25 15:16:21 -0800

    pmw3901:Bus defines

--- src/drivers/pmw3901/pmw3901.cpp
+++ src/drivers/pmw3901/pmw3901.cpp
@@ -79,24 +79,26 @@
 #include <board_config.h>
 
 /* Configuration Constants */
-// #if defined PX4_SPI_BUS_EXPANSION    // crazyflie
-// #define PMW3901_BUS PX4_SPI_BUS_EXPANSION
-// #elif defined PX4_SPI_BUS_EXTERNAL1    // fmu-v5
-// #define PMW3901_BUS PX4_SPI_BUS_EXTERNAL1
-// #else
-// #error "add the required spi bus from board_config.h here"
-// #endif
 
-// #if defined PX4_SPIDEV_EXPANSION_2   // crazyflie flow deck
-// #define PMW3901_SPIDEV PX4_SPIDEV_EXPANSION_2
-// #elif defined PX4_SPIDEV_EXTERNAL1_1   // fmu-v5 ext CS1
-// #define PMW3901_SPIDEV PX4_SPIDEV_EXTERNAL1_1
-// #else
-// #error "add the required spi dev from board_config.h here"
-// #endif
+#if defined PX4_SPI_BUS_EXPANSION   // crazyflie
+# define PMW3901_BUS PX4_SPI_BUS_EXPANSION
+#elif defined PX4_SPI_BUS_EXTERNAL1   // fmu-v5
+# define PMW3901_BUS PX4_SPI_BUS_EXTERNAL1
+#elif defined PX4_SPI_BUS_EXTERNAL    // fmu-v4 extspi
+# define PMW3901_BUS PX4_SPI_BUS_EXTERNAL
+#else
+# error "add the required spi bus from board_config.h here"
+#endif
 
-#define PMW3901_BUS PX4_SPI_BUS_EXTERNAL
-#define PMW3901_SPIDEV PX4_SPIDEV_EXTERNAL
+#if defined PX4_SPIDEV_EXPANSION_2    // crazyflie flow deck
+# define PMW3901_SPIDEV PX4_SPIDEV_EXPANSION_2
+#elif defined PX4_SPIDEV_EXTERNAL1_1    // fmu-v5 ext CS1
+# define PMW3901_SPIDEV PX4_SPIDEV_EXTERNAL1_1
+#elif defined PX4_SPIDEV_EXTERNAL   // fmu-v4 extspi
+# define PMW3901_SPIDEV PX4_SPIDEV_EXTERNAL
+#else
+# error "add the required spi dev from board_config.h here"
+#endif
 
 #define PMW3901_SPI_BUS_SPEED (2000000L) // 2MHz
 
