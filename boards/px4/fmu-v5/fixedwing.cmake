12ef9555aa2e740f5d57a9efe92e3c62b2c68e2b
Daniel Agar <daniel@agar.ca>
2019-01-27 11:38:22 -0500

    create example vehicle type build configs for fmu-v2 and fmu-v5

--- boards/px4/fmu-v5/fixedwing.cmake
+++ boards/px4/fmu-v5/fixedwing.cmake
@@ -3,12 +3,11 @@ px4_add_board(
 	PLATFORM nuttx
 	VENDOR px4
 	MODEL fmu-v5
-	LABEL default
+	LABEL fixedwing
 	TOOLCHAIN arm-none-eabi
 	ARCHITECTURE cortex-m7
 	ROMFSROOT px4fmu_common
 	IO px4_io-v2_default
-	TESTING
 	UAVCAN_INTERFACES 2
 
 	SERIAL_PORTS
@@ -24,44 +23,25 @@ px4_add_board(
 		differential_pressure # all available differential pressure drivers
 		distance_sensor # all available distance sensor drivers
 		gps
-		#heater
 		imu/adis16448
-		#imu # all available imu drivers
 		imu/bmi055
 		imu/mpu6000
-		imu/mpu9250
-		imu/icm20948
-		irlock
-		lights/blinkm
-		lights/oreoled
-		lights/pca8574
 		lights/rgbled
 		lights/rgbled_ncp5623c
 		lights/rgbled_pwm
 		magnetometer # all available magnetometer drivers
-		#md25
-		mkblctrl
-		pca9685
-		pmw3901
-		#protocol_splitter
 		pwm_input
 		pwm_out_sim
-		px4flow
 		px4fmu
 		px4io
 		rc_input
-		roboclaw
 		stm32
 		stm32/adc
 		stm32/tone_alarm
-		tap_esc
 		telemetry # all available telemetry drivers
-		test_ppm
-		tone_alarm
 		uavcan
 
 	MODULES
-		attitude_estimator_q
 		camera_feedback
 		commander
 		dataman
@@ -69,20 +49,13 @@ px4_add_board(
 		events
 		fw_att_control
 		fw_pos_control_l1
-		gnd_att_control
-		gnd_pos_control
 		land_detector
-		landing_target_estimator
 		load_mon
-		local_position_estimator
 		logger
 		mavlink
-		mc_att_control
-		mc_pos_control
 		navigator
 		sensors
 		vmount
-		vtol_att_control
 		wind_estimator
 
 	SYSTEMCMDS
@@ -104,23 +77,9 @@ px4_add_board(
 		reflect
 		sd_bench
 		shutdown
-		tests # tests and test runner
 		top
 		topic_listener
 		tune_control
 		usb_connected
 		ver
-
-	EXAMPLES
-		bottle_drop # OBC challenge
-		fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
-		hello
-		hwtest # Hardware test
-		#matlab_csv_serial
-		position_estimator_inav
-		px4_mavlink_debug # Tutorial code from https://px4.io/dev/debug_values
-		px4_simple_app # Tutorial code from https://px4.io/dev/px4_simple_app
-		rover_steering_control # Rover example app
-		segway
-		uuv_example_app
 	)
