b86eb679c571079c4216bc898fdaeb9d130ded5b
Martina Rivizzigno <martina@rivizzigno.it>
2019-01-31 19:42:38 +0100

    update ros-kinetic container to tag 2019-01-31. Remove xvfb since it runs from the container entrypoint.

--- test/rostest_avoidance_run.sh
+++ test/rostest_avoidance_run.sh
@@ -3,8 +3,6 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 PX4_SRC_DIR=${DIR}/..
 
-Xvfb :1 -screen 0 1600x1200x24+32 &
-
 source /opt/ros/kinetic/setup.bash
 mkdir -p ${PX4_SRC_DIR}/catkin_ws/src
 cd ${PX4_SRC_DIR}/catkin_ws/
