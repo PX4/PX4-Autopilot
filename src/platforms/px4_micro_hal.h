0c839c1c31b3303531a4e8826ccde486b37e1646
David Sidrane <david_s5@nscdg.com>
2019-02-06 04:03:07 -0800

    micro_hal:F7 non up_progmem functions

--- src/platforms/px4_micro_hal.h
+++ src/platforms/px4_micro_hal.h
@@ -70,7 +70,10 @@ __BEGIN_DECLS
 #    if defined(CONFIG_ARCH_CHIP_STM32F7)
 #      define PX4_SOC_ARCH_ID             PX4_SOC_ARCH_ID_STM32F7
 #      include <chip.h>
-#include <chip/stm32_flash.h>
+#      include <chip/stm32_flash.h>
+void stm32_flash_lock(void);
+void stm32_flash_unlock(void);
+int stm32_flash_writeprotect(size_t page, bool enabled);
 #      include <up_internal.h> //include up_systemreset() which is included on stm32.h
 #      include <stm32_bbsram.h>
 #      define PX4_BBSRAM_SIZE STM32F7_BBSRAM_SIZE
