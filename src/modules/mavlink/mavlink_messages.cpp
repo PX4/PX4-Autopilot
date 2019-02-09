c4f43fe66e3fe3892f60dfa3a57b781dbd82580d
Daniel Agar <daniel@agar.ca>
2019-02-09 10:55:55 -0500

    update mavlink to latest with addr of packed fix
     - also requires minor changes in ODOMETRY
       (https://mavlink.io/en/messages/common.html#ODOMETRY)

--- src/modules/mavlink/mavlink_messages.cpp
+++ src/modules/mavlink/mavlink_messages.cpp
@@ -2361,11 +2361,15 @@ protected:
 			msg.yawspeed = odom.yawspeed;
 
 			// get the covariance matrix size
+
+			// pose_covariance
 			static constexpr size_t POS_URT_SIZE = sizeof(odom.pose_covariance) / sizeof(odom.pose_covariance[0]);
-			static constexpr size_t VEL_URT_SIZE = sizeof(odom.velocity_covariance) / sizeof(odom.velocity_covariance[0]);
 			static_assert(POS_URT_SIZE == (sizeof(msg.pose_covariance) / sizeof(msg.pose_covariance[0])),
 				      "Odometry Pose Covariance matrix URT array size mismatch");
-			static_assert(VEL_URT_SIZE == (sizeof(msg.twist_covariance) / sizeof(msg.twist_covariance[0])),
+
+			// velocity_covariance
+			static constexpr size_t VEL_URT_SIZE = sizeof(odom.velocity_covariance) / sizeof(odom.velocity_covariance[0]);
+			static_assert(VEL_URT_SIZE == (sizeof(msg.velocity_covariance) / sizeof(msg.velocity_covariance[0])),
 				      "Odometry Velocity Covariance matrix URT array size mismatch");
 
 			// copy pose covariances
@@ -2376,7 +2380,7 @@ protected:
 			// copy velocity covariances
 			//TODO: Apply rotation matrix to transform from body-fixed NED to earth-fixed NED frame
 			for (size_t i = 0; i < VEL_URT_SIZE; i++) {
-				msg.twist_covariance[i] = odom.velocity_covariance[i];
+				msg.velocity_covariance[i] = odom.velocity_covariance[i];
 			}
 
 			mavlink_msg_odometry_send_struct(_mavlink->get_channel(), &msg);
