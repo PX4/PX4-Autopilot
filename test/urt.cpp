707967d117ab15bb8deac5ef3556df9795ce2884
TSC21 <n.marques21@hotmail.com>
2019-02-17 18:18:26 +0000

    add test to .urt()

---
+++ test/urt.cpp
@@ -0,0 +1,24 @@
+#include "test_macros.hpp"
+#include "float.h"
+#include <matrix/math.hpp>
+
+using namespace matrix;
+
+int main()
+{
+    float data[9] = {1, 2, 3,
+                     4, 5, 6,
+                     7, 8, 10
+                    };
+    float urt[6] = {1, 2, 3, 5, 6, 10};
+
+    SquareMatrix<float, 3> A(data);
+
+    for(int i=0; i<sizeof(urt); i++) {
+        TEST(fabs(urt[i] - A.urt().data()[i]) < FLT_EPSILON);
+    }
+
+    return 0;
+}
+
+/* vim: set et fenc=utf-8 ff=unix sts=0 sw=4 ts=4 : */
