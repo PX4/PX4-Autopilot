1b7630e77ebe41559181bf1dd1badb36dcdf437f
Daniel Agar <daniel@agar.ca>
2019-02-22 10:50:44 -0500

    lis3mdl move to px4 work queue

--- src/drivers/magnetometer/lis3mdl/lis3mdl.h
+++ src/drivers/magnetometer/lis3mdl/lis3mdl.h
@@ -48,13 +48,10 @@
 
 #include <lib/conversion/rotation.h>
 #include <systemlib/err.h>
+#include <px4_work_queue/ScheduledWorkItem.hpp>
 
 #include <perf/perf_counter.h>
 #include <px4_defines.h>
-
-#ifndef CONFIG_SCHED_WORKQUEUE
-# error This requires CONFIG_SCHED_WORKQUEUE.
-#endif
 
 /**
  * LIS3MDL internal constants and data structures.
@@ -111,7 +108,7 @@ enum OPERATING_MODE {
 };
 
 
-class LIS3MDL : public device::CDev
+class LIS3MDL : public device::CDev, public px4::ScheduledWorkItem
 {
 public:
 	LIS3MDL(device::Device *interface, const char *path, enum Rotation rotation);
@@ -143,7 +140,6 @@ protected:
 	Device *_interface;
 
 private:
-	work_s _work;
 
 	ringbuffer::RingBuffer *_reports;
 
@@ -165,7 +161,7 @@ private:
 	enum OPERATING_MODE _mode;
 	enum Rotation _rotation;
 
-	unsigned int _measure_ticks;
+	unsigned int _measure_interval;
 
 	int _class_instance;
 	int _orb_class_instance;
@@ -227,15 +223,7 @@ private:
 	 * and measurement to provide the most recent measurement possible
 	 * at the next interval.
 	 */
-	void cycle();
-
-	/**
-	 * @brief Static trampoline from the workq context; because we don't have a
-	 *         generic workq wrapper yet.
-	 *
-	 * @param arg Instance pointer for the driver that is polling.
-	 */
-	static void cycle_trampoline(void *arg);
+	void Run() override;
 
 	/**
 	 * Issue a measurement command.
