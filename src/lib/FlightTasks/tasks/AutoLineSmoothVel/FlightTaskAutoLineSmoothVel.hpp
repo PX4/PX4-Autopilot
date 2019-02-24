7283b77975130e6f17d7666be72d1272318d99db
Daniel Agar <daniel@agar.ca>
2019-02-24 12:02:36 -0500

    FlightTasks AutoLineSmoothVel initialize member
     - fixes Coverity CID 328492

--- src/lib/FlightTasks/tasks/AutoLineSmoothVel/FlightTaskAutoLineSmoothVel.hpp
+++ src/lib/FlightTasks/tasks/AutoLineSmoothVel/FlightTaskAutoLineSmoothVel.hpp
@@ -75,6 +75,6 @@ protected:
 	void _prepareSetpoints(); /**< Generate velocity target points for the trajectory generator. */
 	void _generateTrajectory();
 	VelocitySmoothing _trajectory[3]; ///< Trajectories in x, y and z directions
-	float _yaw_sp_prev;
+	float _yaw_sp_prev{NAN};
 	uint8_t _reset_counter{0}; /**< counter for estimator resets in xy-direction */
 };
