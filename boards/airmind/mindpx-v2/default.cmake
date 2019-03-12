8f03b714aa129da952ac895bff8d9f6843f68b2e
Hamish Willee <hamishwillee@gmail.com>
2019-03-13 09:33:27 +1100

    Correct links to example docs

--- boards/airmind/mindpx-v2/default.cmake
+++ boards/airmind/mindpx-v2/default.cmake
@@ -106,8 +106,8 @@ px4_add_board(
 		hwtest # Hardware test
 		#matlab_csv_serial
 		position_estimator_inav
-		px4_mavlink_debug # Tutorial code from https://px4.io/dev/debug_values
-		px4_simple_app # Tutorial code from https://px4.io/dev/px4_simple_app
+		px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
+		px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
 		rover_steering_control # Rover example app
 		segway
 		uuv_example_app
--- boards/intel/aerofc-v1/rtps.cmake
+++ boards/intel/aerofc-v1/rtps.cmake
@@ -90,8 +90,8 @@ px4_add_board(
 		#fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
 		#hwtest # Hardware test
 		#matlab_csv_serial
-		#px4_mavlink_debug # Tutorial code from https://px4.io/dev/debug_values
-		#px4_simple_app # Tutorial code from https://px4.io/dev/px4_simple_app
+		#px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
+		#px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
 		#rover_steering_control # Rover example app
 		#segway
 		#uuv_example_app
