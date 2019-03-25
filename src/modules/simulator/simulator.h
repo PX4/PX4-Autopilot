7023d124f2fc431876724157a08334be192e212c
bresch <brescianimathieu@gmail.com>
2019-03-25 13:29:13 +0100

    Parameter update - Rename variables in modules/simulator using parameter_update.py script

--- src/modules/simulator/simulator.h
+++ src/modules/simulator/simulator.h
@@ -383,10 +383,10 @@ private:
 	vehicle_status_s _vehicle_status {};
 
 	DEFINE_PARAMETERS(
-		(ParamFloat<px4::params::SIM_BAT_DRAIN>) _battery_drain_interval_s, ///< battery drain interval
-		(ParamInt<px4::params::MAV_TYPE>) _param_system_type,
-		(ParamInt<px4::params::MAV_SYS_ID>) _param_system_id,
-		(ParamInt<px4::params::MAV_COMP_ID>) _param_component_id
+		(ParamFloat<px4::params::SIM_BAT_DRAIN>) _param_sim_bat_drain, ///< battery drain interval
+		(ParamInt<px4::params::MAV_TYPE>) _param_mav_type,
+		(ParamInt<px4::params::MAV_SYS_ID>) _param_mav_sys_id,
+		(ParamInt<px4::params::MAV_COMP_ID>) _param_mav_comp_id
 	)
 
 #endif
