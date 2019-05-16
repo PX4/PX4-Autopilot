9e8575b71b8e9b99aff8e8275515eca6686bbfcd
Martina Rivizzigno <martina@rivizzigno.it>
2019-05-16 09:12:18 +0200

    do not overwrite with obstacle avoidance yaw setpoints if external yaw handling is enabled

--- src/lib/FlightTasks/tasks/Auto/FlightTaskAuto.cpp
+++ src/lib/FlightTasks/tasks/Auto/FlightTaskAuto.cpp
@@ -278,7 +278,8 @@ bool FlightTaskAuto::_evaluateTriplets()
 		_obstacle_avoidance.updateAvoidanceDesiredWaypoints(_triplet_target, _yaw_setpoint, _yawspeed_setpoint,
 				_triplet_next_wp,
 				_sub_triplet_setpoint->get().next.yaw,
-				_sub_triplet_setpoint->get().next.yawspeed_valid ? _sub_triplet_setpoint->get().next.yawspeed : NAN);
+				_sub_triplet_setpoint->get().next.yawspeed_valid ? _sub_triplet_setpoint->get().next.yawspeed : NAN,
+				_ext_yaw_handler != nullptr && _ext_yaw_handler->is_active());
 		_obstacle_avoidance.checkAvoidanceProgress(_position, _triplet_prev_wp, _target_acceptance_radius, _closest_pt);
 	}
 
