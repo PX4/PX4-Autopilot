b85b02469a0f554684544af170a2ae21d44001af
Julian Oes <julian@oes.ch>
2019-05-20 16:01:09 +0200

    hysteresis: remove dependency/side effect on time

--- src/lib/hysteresis/hysteresis_test.cpp
+++ src/lib/hysteresis/hysteresis_test.cpp
@@ -37,16 +37,11 @@
  */
 
 #include <gtest/gtest.h>
-#include <unistd.h>
 
 #include "hysteresis.h"
 
 
-#if defined(__PX4_DARWIN) ||  defined(__PX4_CYGWIN)
-static const int f = 10;
-#else
-static const int f = 1;
-#endif
+static constexpr hrt_abstime SOME_START_TIME = 1558359616134000llu;
 
 
 TEST(Hysteresis, InitFalse)
@@ -63,138 +58,147 @@ TEST(Hysteresis, InitTrue)
 
 TEST(Hysteresis, Zero)
 {
+	hrt_abstime time_us = SOME_START_TIME;
+
 	// Default is 0 hysteresis.
 	systemlib::Hysteresis hysteresis(false);
 	EXPECT_FALSE(hysteresis.get_state());
 
 	// Change and see result immediately.
-	hysteresis.set_state_and_update(true);
+	hysteresis.set_state_and_update(true, time_us);
 	EXPECT_TRUE(hysteresis.get_state());
-	hysteresis.set_state_and_update(false);
+	hysteresis.set_state_and_update(false, time_us);
 	EXPECT_FALSE(hysteresis.get_state());
-	hysteresis.set_state_and_update(true);
+	hysteresis.set_state_and_update(true, time_us);
 	EXPECT_TRUE(hysteresis.get_state());
 
+	time_us += 1000;
 	// A wait won't change anything.
-	px4_usleep(1000 * f);
-	hysteresis.update();
+	hysteresis.update(time_us);
 	EXPECT_TRUE(hysteresis.get_state());
 }
 
 TEST(Hysteresis, ChangeAfterTime)
 {
+	hrt_abstime time_us = SOME_START_TIME;
 
 	systemlib::Hysteresis hysteresis(false);
-	hysteresis.set_hysteresis_time_from(false, 5000 * f);
-	hysteresis.set_hysteresis_time_from(true, 3000 * f);
+	hysteresis.set_hysteresis_time_from(false, 5000);
+	hysteresis.set_hysteresis_time_from(true, 3000);
 
 	// Change to true.
-	hysteresis.set_state_and_update(true);
+	hysteresis.set_state_and_update(true, time_us);
 	EXPECT_FALSE(hysteresis.get_state());
-	px4_usleep(4000 * f);
-	hysteresis.update();
+	time_us += 4000;
+	hysteresis.update(time_us);
 	EXPECT_FALSE(hysteresis.get_state());
-	px4_usleep(2000 * f);
-	hysteresis.update();
+	time_us += 2000;
+	hysteresis.update(time_us);
 	EXPECT_TRUE(hysteresis.get_state());
 
 	// Change back to false.
-	hysteresis.set_state_and_update(false);
+	hysteresis.set_state_and_update(false, time_us);
 	EXPECT_TRUE(hysteresis.get_state());
-	px4_usleep(1000 * f);
-	hysteresis.update();
+	time_us += 1000;
+	hysteresis.update(time_us);
 	EXPECT_TRUE(hysteresis.get_state());
-	px4_usleep(3000 * f);
-	hysteresis.update();
+	time_us += 3000;
+	hysteresis.update(time_us);
 	EXPECT_FALSE(hysteresis.get_state());
 }
 
 TEST(Hysteresis, HysteresisChanged)
 {
+	hrt_abstime time_us = SOME_START_TIME;
+
 	systemlib::Hysteresis hysteresis(false);
-	hysteresis.set_hysteresis_time_from(true, 2000 * f);
-	hysteresis.set_hysteresis_time_from(false, 5000 * f);
+	hysteresis.set_hysteresis_time_from(true, 2000);
+	hysteresis.set_hysteresis_time_from(false, 5000);
 
 	// Change to true.
-	hysteresis.set_state_and_update(true);
+	hysteresis.set_state_and_update(true, time_us);
 	EXPECT_FALSE(hysteresis.get_state());
-	px4_usleep(3000 * f);
-	hysteresis.update();
+	time_us += 3000;
+	hysteresis.update(time_us);
 	EXPECT_FALSE(hysteresis.get_state());
-	px4_usleep(3000 * f);
-	hysteresis.update();
+	time_us += 3000;
+	hysteresis.update(time_us);
 	EXPECT_TRUE(hysteresis.get_state());
 
 	// Change hysteresis time.
-	hysteresis.set_hysteresis_time_from(true, 10000 * f);
+	hysteresis.set_hysteresis_time_from(true, 10000);
 
 	// Change back to false.
-	hysteresis.set_state_and_update(false);
+	hysteresis.set_state_and_update(false, time_us);
 	EXPECT_TRUE(hysteresis.get_state());
-	px4_usleep(7000 * f);
-	hysteresis.update();
+	time_us += 7000;
+	hysteresis.update(time_us);
 	EXPECT_TRUE(hysteresis.get_state());
-	px4_usleep(5000 * f);
-	hysteresis.update();
+	time_us += 5000;
+	hysteresis.update(time_us);
 	EXPECT_FALSE(hysteresis.get_state());
 }
 
 TEST(Hysteresis, ChangeAfterMultipleSets)
 {
+	hrt_abstime time_us = SOME_START_TIME;
+
 	systemlib::Hysteresis hysteresis(false);
-	hysteresis.set_hysteresis_time_from(true, 5000 * f);
-	hysteresis.set_hysteresis_time_from(false, 5000 * f);
+	hysteresis.set_hysteresis_time_from(true, 5000);
+	hysteresis.set_hysteresis_time_from(false, 5000);
 
 	// Change to true.
-	hysteresis.set_state_and_update(true);
+	hysteresis.set_state_and_update(true, time_us);
 	EXPECT_FALSE(hysteresis.get_state());
-	px4_usleep(3000 * f);
-	hysteresis.set_state_and_update(true);
+	time_us += 3000;
+	hysteresis.set_state_and_update(true, time_us);
 	EXPECT_FALSE(hysteresis.get_state());
-	px4_usleep(3000 * f);
-	hysteresis.set_state_and_update(true);
+	time_us += 3000;
+	hysteresis.set_state_and_update(true, time_us);
 	EXPECT_TRUE(hysteresis.get_state());
 
 	// Change to false.
-	hysteresis.set_state_and_update(false);
+	hysteresis.set_state_and_update(false, time_us);
 	EXPECT_TRUE(hysteresis.get_state());
-	px4_usleep(3000 * f);
-	hysteresis.set_state_and_update(false);
+	time_us += 3000;
+	hysteresis.set_state_and_update(false, time_us);
 	EXPECT_TRUE(hysteresis.get_state());
-	px4_usleep(3000 * f);
-	hysteresis.set_state_and_update(false);
+	time_us += 3000;
+	hysteresis.set_state_and_update(false, time_us);
 	EXPECT_FALSE(hysteresis.get_state());
 }
 
 TEST(Hysteresis, TakeChangeBack)
 {
+	hrt_abstime time_us = SOME_START_TIME;
+
 	systemlib::Hysteresis hysteresis(false);
-	hysteresis.set_hysteresis_time_from(false, 5000 * f);
+	hysteresis.set_hysteresis_time_from(false, 5000);
 
 	// Change to true.
-	hysteresis.set_state_and_update(true);
+	hysteresis.set_state_and_update(true, time_us);
 	EXPECT_FALSE(hysteresis.get_state());
-	px4_usleep(3000 * f);
-	hysteresis.update();
+	time_us += 3000;
+	hysteresis.update(time_us);
 	EXPECT_FALSE(hysteresis.get_state());
 	// Change your mind to false.
-	hysteresis.set_state_and_update(false);
+	hysteresis.set_state_and_update(false, time_us);
 	EXPECT_FALSE(hysteresis.get_state());
-	px4_usleep(6000 * f);
-	hysteresis.update();
+	time_us += 6000;
+	hysteresis.update(time_us);
 	EXPECT_FALSE(hysteresis.get_state());
 
 	// And true again
-	hysteresis.set_state_and_update(true);
+	hysteresis.set_state_and_update(true, time_us);
 	EXPECT_FALSE(hysteresis.get_state());
-	px4_usleep(3000 * f);
-	hysteresis.update();
+	time_us += 3000;
+	hysteresis.update(time_us);
 	EXPECT_FALSE(hysteresis.get_state());
-	px4_usleep(3000 * f);
-	hysteresis.update();
+	time_us += 3000;
+	hysteresis.update(time_us);
 	EXPECT_TRUE(hysteresis.get_state());
 
 	// The other directory is immediate.
-	hysteresis.set_state_and_update(false);
+	hysteresis.set_state_and_update(false, time_us);
 	EXPECT_FALSE(hysteresis.get_state());
 }
