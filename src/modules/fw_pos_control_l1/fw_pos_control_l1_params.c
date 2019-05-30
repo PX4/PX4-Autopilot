fe41318da616e0a55c0855123d820311a98aa52c
Daniel Agar <daniel@agar.ca>
2019-05-30 16:47:25 -0400

    fw_pos_control_l1 replace FW_LND_THRTC_SC param description < character
     - fixes #12124

--- src/modules/fw_pos_control_l1/fw_pos_control_l1_params.c
+++ src/modules/fw_pos_control_l1/fw_pos_control_l1_params.c
@@ -396,7 +396,7 @@ PARAM_DEFINE_FLOAT(FW_LND_AIRSPD_SC, 1.3f);
 /**
  * Throttle time constant factor for landing
  *
- * Set this parameter to <1.0 to make the TECS throttle loop react faster during
+ * Set this parameter to less than 1.0 to make the TECS throttle loop react faster during
  * landing than during normal flight (i.e. giving efficiency and low motor wear at
  * high altitudes but control accuracy during landing). During landing, the TECS
  * throttle time constant (FW_T_THRO_CONST) is multiplied by this value.
