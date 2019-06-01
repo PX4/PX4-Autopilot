f4090efd3b2c836756dbeb8956967591594b75cd
Daniel Agar <daniel@agar.ca>
2019-05-31 21:30:37 -0400

    NuttX preparation for SPI DMA
     - fix board_dma_alloc
     - SPI always use SPI_LOCK()
     - NuttX stm32f7 hack stm32_dmacapable to skip buffer end DCACHE alignment check

--- src/lib/drivers/device/nuttx/SPI.hpp
+++ src/lib/drivers/device/nuttx/SPI.hpp
@@ -32,7 +32,7 @@
  ****************************************************************************/
 
 /**
- * @file spi.h
+ * @file SPI.hpp
  *
  * Base class for devices connected via SPI.
  */
@@ -61,22 +61,8 @@ protected:
 	 * @param mode		SPI clock/data mode
 	 * @param frequency	SPI clock frequency
 	 */
-	SPI(const char *name,
-	    const char *devname,
-	    int bus,
-	    uint32_t device,
-	    enum spi_mode_e mode,
-	    uint32_t frequency);
+	SPI(const char *name, const char *devname, int bus, uint32_t device, enum spi_mode_e mode, uint32_t frequency);
 	virtual ~SPI();
-
-	/**
-	 * Locking modes supported by the driver.
-	 */
-	enum LockMode {
-		LOCK_PREEMPTION,	/**< the default; lock against all forms of preemption. */
-		LOCK_THREADS,		/**< lock only against other threads, using SPI_LOCK */
-		LOCK_NONE		/**< perform no locking, only safe if the bus is entirely private */
-	};
 
 	virtual int	init();
 
@@ -137,33 +123,19 @@ protected:
 	 * @param frequency	Frequency to set (Hz)
 	 */
 	void		set_frequency(uint32_t frequency) { _frequency = frequency; }
-
-	/**
-	 * Set the SPI bus locking mode
-	 *
-	 * This set the SPI locking mode. For devices competing with NuttX SPI
-	 * drivers on a bus the right lock mode is LOCK_THREADS.
-	 *
-	 * @param mode	Locking mode
-	 */
-	void		set_lockmode(enum LockMode mode) { locking_mode = mode; }
-
-	LockMode	locking_mode;	/**< selected locking mode */
+	uint32_t	get_frequency() { return _frequency; }
 
 private:
-	uint32_t			_device;
+	uint32_t		_device;
 	enum spi_mode_e		_mode;
 	uint32_t		_frequency;
-	struct spi_dev_s	*_dev;
+	struct spi_dev_s	*_dev {nullptr};
 
 	/* this class does not allow copying */
 	SPI(const SPI &);
 	SPI operator=(const SPI &);
 
 protected:
-	int	_transfer(uint8_t *send, uint8_t *recv, unsigned len);
-
-	int	_transferhword(uint16_t *send, uint16_t *recv, unsigned len);
 
 	bool	external() { return px4_spi_bus_external(get_device_bus()); }
 
