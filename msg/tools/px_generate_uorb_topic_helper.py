3d9f83a4f2751cec6d7c4252f1d694d9cab00535
TSC21 <n.marques21@hotmail.com>
2019-06-02 16:12:50 +0100

    microRTPS_bridge: structural change to make possible to generate pub/sub code for multi-topics/aliased msgs

--- msg/tools/px_generate_uorb_topic_helper.py
+++ msg/tools/px_generate_uorb_topic_helper.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #############################################################################
 #
-#   Copyright (C) 2013-2018 PX4 Pro Development Team. All rights reserved.
+#   Copyright (C) 2013-2019 PX4 Pro Development Team. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
