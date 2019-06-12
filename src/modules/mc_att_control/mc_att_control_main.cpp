048cca7bc40d3274d9298c96a27f19bcd4304622
Matthias Grob <maetugr@gmail.com>
2019-06-12 19:59:29 +0200

    vtol_att_control: apply multicopter takeoff hotfix also for vtol (#12250)
    Please see reference:
    https://github.com/PX4/Firmware/issues/12171

--- src/modules/mc_att_control/mc_att_control_main.cpp
+++ src/modules/mc_att_control/mc_att_control_main.cpp
@@ -358,7 +358,7 @@ MulticopterAttitudeControl::control_attitude()
 {
 	_v_att_sp_sub.update(&_v_att_sp);
 
-	// reinitialize the setpoint while not armed to make sure no value from the last flight is still kept
+	// reinitialize the setpoint while not armed to make sure no value from the last mode or flight is still kept
 	if (!_v_control_mode.flag_armed) {
 		Quatf().copyTo(_v_att_sp.q_d);
 		Vector3f().copyTo(_v_att_sp.thrust_body);
