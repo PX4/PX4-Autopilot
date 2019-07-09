7c37babdef660ecb1dbb574162bf5d83ae27d217
RomanBapst <bapstroman@gmail.com>
2019-07-09 15:31:57 +0200

    fixed handling of THR_MLD_FAC param - fixed bug where THR_MDL_FAC could not be set to zero from a non-zero value - constrain THR_MDL_FAC between 0 and 1 in mixer library
    Signed-off-by: RomanBapst <bapstroman@gmail.com>

--- src/lib/mixer/mixer.h
+++ src/lib/mixer/mixer.h
@@ -128,6 +128,7 @@
 #pragma once
 
 #include <stdint.h>
+#include <lib/mathlib/mathlib.h>
 
 /** simple channel scaler */
 struct mixer_scaler_s {
@@ -707,7 +708,10 @@ public:
 	 *
 	 * @param[in]  val   The value
 	 */
-	void			set_thrust_factor(float val) override { _thrust_factor = val; }
+	void			set_thrust_factor(float val) override
+	{
+		_thrust_factor = math::constrain(val, 0.0f, 1.0f);
+	}
 
 	void 			set_airmode(Airmode airmode) override;
 
