d4cd1d0d2e17d1dd07272fb47f37875d7d429a0d
Daniel Agar <daniel@agar.ca>
2019-07-10 19:08:12 -0400

    NuttX stm32f7 fully re-enable dcache with write back (#12435)
     - fixes https://github.com/PX4/Firmware/issues/12216
     - includes latest PX4/NuttX and apps update 7.29+

--- boards/px4/fmu-v5x/src/init.c
+++ boards/px4/fmu-v5x/src/init.c
@@ -196,32 +196,6 @@ __EXPORT void board_on_reset(int status)
 	}
 }
 
-/****************************************************************************
- * Name: board_app_finalinitialize
- *
- * Description:
- *   Perform application specific initialization.  This function is never
- *   called directly from application code, but only indirectly via the
- *   (non-standard) boardctl() interface using the command
- *   BOARDIOC_FINALINIT.
- *
- * Input Parameters:
- *   arg - The argument has no meaning.
- *
- * Returned Value:
- *   Zero (OK) is returned on success; a negated errno value is returned on
- *   any failure to indicate the nature of the failure.
- *
- ****************************************************************************/
-
-#ifdef CONFIG_BOARDCTL_FINALINIT
-int board_app_finalinitialize(uintptr_t arg)
-{
-	board_configure_dcache(1);
-	return 0;
-}
-#endif
-
 /************************************************************************************
  * Name: stm32_boardinitialize
  *
@@ -285,9 +259,6 @@ stm32_boardinitialize(void)
 __EXPORT int board_app_initialize(uintptr_t arg)
 {
 	/* Power on Interfaces */
-
-	board_configure_dcache(0);
-
 	VDD_3V3_SD_CARD_EN(true);
 	VDD_5V_PERIPH_EN(true);
 	VDD_5V_HIPOWER_EN(true);
