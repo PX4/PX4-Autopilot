4bcf796a89a6c7139d0ae326199a8248974a2ac1
Azarakhsh Keipour <keipour@gmail.com>
2019-07-28 15:21:59 -0400

    Added mixer and gazebo model for the tilted-arm hexarotor

--- platforms/posix/cmake/sitl_target.cmake
+++ platforms/posix/cmake/sitl_target.cmake
@@ -63,7 +63,7 @@ set(debuggers none ide gdb lldb ddd valgrind callgrind)
 set(models none shell
 	if750a iris iris_opt_flow iris_vision iris_rplidar iris_irlock iris_obs_avoid iris_rtps solo typhoon_h480
 	plane
-	standard_vtol tailsitter tiltrotor
+	standard_vtol tailsitter tiltrotor hexa_x_tilt
 	hippocampus rover)
 set(all_posix_vmd_make_targets)
 foreach(viewer ${viewers})
