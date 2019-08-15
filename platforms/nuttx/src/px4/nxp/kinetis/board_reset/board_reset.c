a703ee23ab97ac531d7180249e2aeccb1ca5414b
Beat Küng <beat-kueng@gmx.net>
2019-08-15 19:59:32 +0200

    Upgrade NuttX to 8.1+
    Nuttx with PX4 contrib of bbsram fix
    
    Nuttx 8.31+ with BBSRAM disable dcache

--- platforms/nuttx/src/px4/nxp/kinetis/board_reset/board_reset.c
+++ platforms/nuttx/src/px4/nxp/kinetis/board_reset/board_reset.c
@@ -42,6 +42,39 @@
 #include <nuttx/board.h>
 
 
+#ifdef CONFIG_BOARDCTL_RESET
+
+/****************************************************************************
+ * Public functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Name: board_reset
+ *
+ * Description:
+ *   Reset board.  Support for this function is required by board-level
+ *   logic if CONFIG_BOARDCTL_RESET is selected.
+ *
+ * Input Parameters:
+ *   status - Status information provided with the reset event.  This
+ *            meaning of this status information is board-specific.  If not
+ *            used by a board, the value zero may be provided in calls to
+ *            board_reset().
+ *
+ * Returned Value:
+ *   If this function returns, then it was not possible to power-off the
+ *   board due to some constraints.  The return value int this case is a
+ *   board-specific reason for the failure to shutdown.
+ *
+ ****************************************************************************/
+
+int board_reset(int status)
+{
+	up_systemreset();
+	return 0;
+}
+
+#endif /* CONFIG_BOARDCTL_RESET */
 
 
 int board_set_bootload_mode(board_reset_e mode)
