63140f1d61b74085f2dea3d2141222e38ec66332
Julian Kent <jkflying@gmail.com>
2019-08-21 10:41:28 +0200

    Do a single setup of uORB/Parameters instead of once every test

--- src/lib/CollisionPrevention/CollisionPreventionTest.cpp
+++ src/lib/CollisionPrevention/CollisionPreventionTest.cpp
@@ -39,16 +39,9 @@
 class CollisionPreventionTest : public ::testing::Test
 {
 public:
-	void SetUp() override
-	{
-		uORB::Manager::initialize();
-		param_init();
-	}
-
 	void TearDown() override
 	{
 		param_reset_all();
-		uORB::Manager::terminate();
 	}
 };
 
