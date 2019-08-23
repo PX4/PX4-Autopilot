7398526889c2a4eda1e4f8f9e52c6174c92f0505
Daniel Agar <daniel@agar.ca>
2019-08-23 16:07:08 -0400

    uvify core add dmesg

--- boards/uvify/core/src/board_config.h
+++ boards/uvify/core/src/board_config.h
@@ -338,6 +338,10 @@
 #define BOARD_DMA_ALLOC_POOL_SIZE    5120
 
 #define BOARD_HAS_ON_RESET 1
+
+#define BOARD_ENABLE_CONSOLE_BUFFER
+#define BOARD_CONSOLE_BUFFER_SIZE (1024*3)
+
 __BEGIN_DECLS
 
 /****************************************************************************************************
