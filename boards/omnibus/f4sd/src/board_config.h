474840c49a705e35e45fb9e350b08da00e3b22dc
Beat Küng <beat-kueng@gmx.net>
2019-08-31 12:03:41 +0200

    omnibusf4sd: add dshot timer config

--- boards/omnibus/f4sd/src/board_config.h
+++ boards/omnibus/f4sd/src/board_config.h
@@ -267,6 +267,8 @@
 #define BOARD_ENABLE_CONSOLE_BUFFER
 #define BOARD_CONSOLE_BUFFER_SIZE (1024*3)
 
+#define BOARD_DSHOT_MOTOR_ASSIGNMENT {2, 3, 1, 0};
+
 __BEGIN_DECLS
 
 /****************************************************************************************************
