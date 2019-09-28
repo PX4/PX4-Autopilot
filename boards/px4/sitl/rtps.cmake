c309745bed16261b04da0175fbd61607d22c02cd
Daniel Agar <daniel@agar.ca>
2019-09-28 14:35:17 -0400

    delete obsolete segway example

--- boards/px4/sitl/rtps.cmake
+++ boards/px4/sitl/rtps.cmake
@@ -80,7 +80,6 @@ px4_add_board(
 		px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
 		px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
 		rover_steering_control # Rover example app
-		segway
 	)
 
 set(config_sitl_viewer jmavsim CACHE STRING "viewer for sitl")
