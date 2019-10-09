6d98f4ca4237f553bf0fd5fc45be54253ddd0666
Matthias Grob <maetugr@gmail.com>
2019-10-09 13:56:59 +0200

    PositionControl: rename and comment update return value

--- src/modules/mc_pos_control/PositionControl/PositionControl.cpp
+++ src/modules/mc_pos_control/PositionControl/PositionControl.cpp
@@ -112,10 +112,12 @@ bool PositionControl::update(const float dt)
 	_yawspeed_sp = PX4_ISFINITE(_yawspeed_sp) ? _yawspeed_sp : 0.f;
 	_yaw_sp = PX4_ISFINITE(_yaw_sp) ? _yaw_sp : _yaw;
 
-	const bool valid_acceleration_setpoint = PX4_ISFINITE(_acc_sp(0))
-			&& PX4_ISFINITE(_acc_sp(1))
-			&& PX4_ISFINITE(_acc_sp(2));
-	return valid_acceleration_setpoint;
+	// if the acceleration setpoint is valid there was a setpoint state pair
+	// for each dimension that can get controlled
+	const bool controller_output_is_valid = PX4_ISFINITE(_acc_sp(0))
+						&& PX4_ISFINITE(_acc_sp(1))
+						&& PX4_ISFINITE(_acc_sp(2));
+	return controller_output_is_valid;
 }
 
 void PositionControl::_positionControl()
