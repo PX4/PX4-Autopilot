d927c33bb1f1e377193545848aa01e872c8ccb12
Julien Lecoeur <julien.lecoeur@gmail.com>
2019-10-17 12:25:58 +0200

    logger: log angular acceleration, torque and thrust setpoints for system identification

--- src/modules/logger/logged_topics.cpp
+++ src/modules/logger/logged_topics.cpp
@@ -217,6 +217,9 @@ void LoggedTopics::add_system_identification_topics()
 	add_topic("actuator_controls_0");
 	add_topic("actuator_controls_1");
 	add_topic("sensor_combined");
+	add_topic("vehicle_angular_acceleration");
+	add_topic("vehicle_angular_acceleration_setpoint");
+	add_topic("vehicle_torque_setpoint");
 }
 
 int LoggedTopics::add_topics_from_file(const char *fname)
