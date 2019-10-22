ed692c5c724b3103ad6823d02b746dac03c1ccca
Matthias Grob <maetugr@gmail.com>
2019-10-22 19:11:47 +0200

    PositionControl: add getter for output attitude setpoint

--- src/modules/mc_pos_control/PositionControl/PositionControl.cpp
+++ src/modules/mc_pos_control/PositionControl/PositionControl.cpp
@@ -365,6 +365,12 @@ void PositionControl::getLocalPositionSetpoint(vehicle_local_position_setpoint_s
 	_thr_sp.copyTo(local_position_setpoint.thrust);
 }
 
+void PositionControl::getAttitudeSetpoint(vehicle_attitude_setpoint_s &attitude_setpoint)
+{
+	attitude_setpoint = ControlMath::thrustToAttitude(_thr_sp, _yaw_sp);
+	attitude_setpoint.yaw_sp_move_rate = _yawspeed_sp;
+}
+
 void PositionControl::updateParams()
 {
 	ModuleParams::updateParams();
