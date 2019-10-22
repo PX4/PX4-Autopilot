79334958a9e01def456886d0feeb8934007c9247
Matthias Grob <maetugr@gmail.com>
2019-10-22 17:00:36 +0200

    WeatherVane: only update with last row of rotation matrix

--- src/modules/mc_pos_control/mc_pos_control_main.cpp
+++ src/modules/mc_pos_control/mc_pos_control_main.cpp
@@ -555,7 +555,7 @@ MulticopterPositionControl::Run()
 				}
 			}
 
-			_wv_controller->update(matrix::Quatf(_att_sp.q_d), _states.yaw);
+			_wv_controller->update(Quatf(_att_sp.q_d).dcm_z(), _states.yaw);
 		}
 
 		// an update is necessary here because otherwise the takeoff state doesn't get skiped with non-altitude-controlled modes
