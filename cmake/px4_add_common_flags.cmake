b646f9bfbc5e7a0c4cc32b2a3ae2693391955ef5
Beat Küng <beat-kueng@gmx.net>
2019-10-25 10:58:46 +0200

    includes: remove unused global include directories

--- cmake/px4_add_common_flags.cmake
+++ cmake/px4_add_common_flags.cmake
@@ -169,9 +169,7 @@ function(px4_add_common_flags)
 
 	include_directories(
 		${PX4_BINARY_DIR}
-		${PX4_BINARY_DIR}/src
 		${PX4_BINARY_DIR}/src/lib
-		${PX4_BINARY_DIR}/src/modules
 
 		${PX4_SOURCE_DIR}/platforms/${PX4_PLATFORM}/src/px4/${PX4_CHIP_MANUFACTURER}/${PX4_CHIP}/include
 		${PX4_SOURCE_DIR}/platforms/${PX4_PLATFORM}/src/px4/common/include
@@ -182,7 +180,6 @@ function(px4_add_common_flags)
 		${PX4_SOURCE_DIR}/src/lib/DriverFramework/framework/include
 		${PX4_SOURCE_DIR}/src/lib/matrix
 		${PX4_SOURCE_DIR}/src/modules
-		${PX4_SOURCE_DIR}/src/platforms
 		)
 
 	add_definitions(
