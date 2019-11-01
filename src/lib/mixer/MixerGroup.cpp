15f0a0352b6c24323ed97f5cf311711d57855091
Julien Lecoeur <julien.lecoeur@gmail.com>
2019-11-01 13:46:40 +0100

    Add mixer type AllocatedActuatorMixer (A:)
    The mixer is added with the following character string in a *.mix file:
    `A: X`
    with X a number between 0 and 15
    This indicates that the actuator number X will be written to the output.
    
    Ideally, this mixer would use the topic `vehicle_actuator_setpoint`, but for now it receives info on topics `actuator_controls_4` and `actuator_controls_5`.
    
    romf_pruner: do not prune A mixer

--- src/lib/mixer/MixerGroup.cpp
+++ src/lib/mixer/MixerGroup.cpp
@@ -39,6 +39,7 @@
 
 #include "MixerGroup.hpp"
 
+#include "AllocatedActuatorMixer/AllocatedActuatorMixer.hpp"
 #include "HelicopterMixer/HelicopterMixer.hpp"
 #include "MultirotorMixer/MultirotorMixer.hpp"
 #include "NullMixer/NullMixer.hpp"
@@ -190,6 +191,10 @@ MixerGroup::load_from_buf(Mixer::ControlCallback control_cb, uintptr_t cb_handle
 		switch (*p) {
 		case 'Z':
 			m = NullMixer::from_text(p, resid);
+			break;
+
+		case 'A':
+			m = AllocatedActuatorMixer::from_text(control_cb, cb_handle, p, resid);
 			break;
 
 		case 'M':
