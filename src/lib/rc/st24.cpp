0d400fc18f0bca81bfa9d628adeb3b4a96f052ad
Liio Chen <liio@qq.com>
2019-11-12 11:34:51 +0800

    Fix reset funtion style
    Fix reset function  style check error

--- src/lib/rc/st24.cpp
+++ src/lib/rc/st24.cpp
@@ -70,8 +70,8 @@ static ReceiverFcPacket &_rxpacket = rc_decode_buf._strxpacket;
 
 void st24_reset(void)
 {
-    _decode_state = ST24_DECODE_STATE_UNSYNCED;
-    _rxlen = 0;
+	_decode_state = ST24_DECODE_STATE_UNSYNCED;
+	_rxlen = 0;
 }
 
 uint8_t st24_common_crc8(uint8_t *ptr, uint8_t len)
