6bd4273b9cc4fbd51a1b5e5a063d18e78fb580c7
JaeyoungLim <jalim@student.ethz.ch>
2019-11-13 15:19:02 +0100

    mavlink: handle cellular_status messages for logging

--- src/modules/logger/logger.cpp
+++ src/modules/logger/logger.cpp
@@ -637,6 +637,7 @@ void Logger::add_sensor_comparison_topics()
 
 void Logger::add_vision_and_avoidance_topics()
 {
+	add_topic("cellular_status", 200);
 	add_topic("collision_constraints");
 	add_topic("obstacle_distance_fused");
 	add_topic("onboard_computer_status", 200);
