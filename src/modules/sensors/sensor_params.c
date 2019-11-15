00d00fdca90b405c722b9e39b62889fb24bc45f7
Mohammed Kabir <kabir@uasys.io>
2019-11-15 00:58:54 -0500

    drivers: heater: fix module build and startup
    - Startup was broken due to unnecessary cyclic check probably introduced during transition to work_queues
    - Module never used other than on Teal One which had a hacky heater input GPIO, this enables usage on general boards
    
    drivers: heater: reduce verbosity and simplify commandline options
    
    - We prefer the linux way of only reporting errors and staying quiet when everything is functioning as designed
    - Most of the commandline options just read out the values of the system parameters, and one can just check the parameter values directly.
    
    sensor_params: make thermal control parameters system parameters
    
    heater_params: make thermal control parameters system parameters
    
    drivers: heater: remove pin control hacks
    
    - px4_arch_configgpio(GPIO_HEATER_OUTPUT) directly inits the heater pin to OFF, and as a PUSHPULL (TTL totem pole) OUTPUT
    
    drivers: heater: set default device ID to 0

--- src/modules/sensors/sensor_params.c
+++ src/modules/sensors/sensor_params.c
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2012-2017 PX4 Development Team. All rights reserved.
+ *   Copyright (c) 2012-2019 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -207,6 +207,8 @@ PARAM_DEFINE_FLOAT(SENS_BOARD_Z_OFF, 0.0f);
  *
  * @value -1 Thermal control unavailable
  * @value 0 Thermal control off
+ * @value 1 Thermal control enabled
+ * @category system
  * @group Sensors
  */
 PARAM_DEFINE_INT32(SENS_EN_THERMAL, -1);
