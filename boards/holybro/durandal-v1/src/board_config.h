241f40f956a1c2c8b1974e364f451a1696393b4e
Beat Küng <beat-kueng@gmx.net>
2019-11-19 11:43:36 +0100

    durandal: update default voltage & current dividers
    Measured with the Pixhawk 4 power module (PM07).

--- boards/holybro/durandal-v1/src/board_config.h
+++ boards/holybro/durandal-v1/src/board_config.h
@@ -256,8 +256,8 @@
 /* Define Battery 1 Voltage Divider and A per V
  */
 
-#define BOARD_BATTERY1_V_DIV         (18.1f)     /* measured with the provided PM board */
-#define BOARD_BATTERY1_A_PER_V       (36.367515152f)
+#define BOARD_BATTERY1_V_DIV         (1.148f)     /* measured with the provided PM board (PM07) */
+#define BOARD_BATTERY1_A_PER_V       (2.255f)
 
 /* HW has to large of R termination on ADC todo:change when HW value is chosen */
 
