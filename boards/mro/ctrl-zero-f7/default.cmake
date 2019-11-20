9dc460d51d728f79689ad1ba837629a5201b16c7
Daniel Agar <daniel@agar.ca>
2019-11-19 20:22:50 -0500

    delete unused md25 driver

--- boards/mro/ctrl-zero-f7/default.cmake
+++ boards/mro/ctrl-zero-f7/default.cmake
@@ -37,7 +37,6 @@ px4_add_board(
 		lights/rgbled
 		lights/rgbled_ncp5623c
 		magnetometer # all available magnetometer drivers
-		#md25
 		mkblctrl
 		#optical_flow # all available optical flow drivers
 		#osd
