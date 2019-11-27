cbfd8013e74d23bbed4bef6cc7a3c158cafc1bc0
iyer.suchet@gmail.com <iyer.suchet@gmail.com>
2019-11-27 15:02:07 +0530

    changed operating mode to Single-shot

--- src/drivers/power_monitor/ads1115/ads1115.cpp
+++ src/drivers/power_monitor/ads1115/ads1115.cpp
@@ -45,6 +45,7 @@
 
 #include <uORB/uORB.h>
 #include <uORB/topics/power_monitor.h> // power_monitor struct msg
+#include <uORB/topics/battery_status.h>
 
 #include <float.h>
 #include <px4_platform_common/getopt.h> // Reading inputs from NuttX shell
@@ -55,7 +56,7 @@
 #define ADS1115_BUS_DEFAULT     PX4_I2C_BUS_EXPANSION
 #define ADS1115_BASEADDR        (0x48)
 
-#define ADS1115_CONVERSION_INTERVAL     (8) // 1/Data Rate
+#define ADS1115_CONVERSION_INTERVAL     (8000) // 1/Data Rate in microseconds
 
 #define ADS1115_WRITE_ADDR      (0x90) // 1001 000(0)
 #define ADS1115_READ_ADDR       (0x91) // 1001 000(1)
@@ -63,12 +64,14 @@
 #define ADS1115_CONFIG_REG      (0x01)
 #define ADS1115_CONVERSION_REG  (0x00)
 
-#define ADS1115_CHANNEL_ONE     (0x42A3) // 0 100 001 0 101 0 0 0 11
-#define ADS1115_CHANNEL_TWO     (0x52A3) // 0 101 001 0 101 0 0 0 11
-#define ADS1115_CHANNEL_THREE 	(0x62A3) // 0 110 001 0 101 0 0 0 11
-#define ADS1115_CHANNEL_FOUR	(0x72A3) // 0 111 001 0 101 0 0 0 11
+#define ADS1115_CHANNEL_ONE     (0xC383) // 1 100 001 1 100 0 0 0 11
+#define ADS1115_CHANNEL_TWO     (0xD383) // 1 101 001 1 100 0 0 0 11
+#define ADS1115_CHANNEL_THREE 	(0xE383) // 1 110 001 1 100 0 0 0 11
+#define ADS1115_CHANNEL_FOUR	(0xF383) // 1 111 001 1 100 0 0 0 11
 
 #define swap16(w)       __builtin_bswap16((w))
+
+#define v_offset                (0.364) // Value measured without source
 
 class ADS1115 : public device::I2C, px4::ScheduledWorkItem
 {
@@ -89,6 +92,7 @@ protected:
 
 private:
 	orb_advert_t _power_monitor_topic{nullptr};
+    orb_advert_t _battery_status_topic{nullptr};
 
 	float _measure_interval{ADS1115_CONVERSION_INTERVAL};
 
@@ -101,7 +105,8 @@ private:
 	double getWireTemp();
 	double getBattTemp();
 
-    double avging();
+    double v_avging();
+    double i_avging();
 
     void Run() override;
 };
@@ -163,7 +168,7 @@ int ADS1115::probe()
 
     if (!readConfigReg(&configRegVal))
     {
-        if (0xA342 != configRegVal)
+        if (0x8343 != configRegVal)
         {
             PX4_WARN("ADS1115::readConfigReg() failed");
             return PX4_ERROR;
@@ -174,7 +179,7 @@ int ADS1115::probe()
 
     if (!readConvReg(&convRegVal))
     {
-        PX4_INFO("First Conv Reg read called");
+        PX4_INFO("%u", convRegVal);
     }
 
     PX4_INFO("ADS1115::probe() success");
@@ -185,7 +190,7 @@ int ADS1115::init()
 {
     if (I2C::init() != PX4_OK)
     {
-        PX4_WARN("!2C::init() failed, in ADS1115::init() if.1");
+        PX4_WARN("I2C::init() failed, in ADS1115::init() if.1");
         return PX4_ERROR;
     }
     PX4_INFO("ADS1115::init() success");
@@ -200,22 +205,106 @@ void ADS1115::print_info()
     PX4_INFO("Driver information: ADS1115 Power Monitor Breakout Board");
 }
 
-double ADS1115::avging()
-{
-    int n = 5;
-    uint32_t sum = 0;
-    uint32_t average = 0;
-    uint16_t convRegVal;
+// double ADS1115::getCurrent()
+// {
+//     writeConfigReg(ADS1115_CHANNEL_ONE);
 
-    for (int i = 0; i < n; i++)
-    {
-        readConvReg(&convRegVal);
-        sum = sum + swap16(convRegVal);
-        ScheduleDelayed(ADS1115_CONVERSION_INTERVAL);
-    }
-    average = sum/n;
-    return average;
-}
+//     uint16_t convRegVal = 0;
+//     uint16_t confRegVal = 0;
+//     double current = 0;
+
+//     while (!readConfigReg(&confRegVal) | (0xC383 == confRegVal))
+//     {
+//         readConvReg(&convRegVal);
+//         current = (swap16(convRegVal))*0.025;
+//         break;
+//     }
+//     return current;
+// }
+
+// double ADS1115::v_avging()
+// {
+//     int n =5;
+//     uint32_t sum = 0;
+//     uint32_t average = 0;
+//     uint16_t convRegVal = 0;
+
+//     writeConfigReg(ADS1115_CHANNEL_TWO);
+
+//     for (int i = 0; i < n; i++)
+//     {
+
+//         ScheduleDelayed(ADS1115_CONVERSION_INTERVAL);
+//         if (!readConvReg(&convRegVal))
+//         {
+//             sum = sum + swap16(convRegVal);
+//         } else
+//         {
+//             PX4_ERR("Voltage Averaging failed");
+//         }
+
+//     }
+//     average = sum/n;
+//     return average;
+// }
+
+// double ADS1115::i_avging()
+// {
+//     int n =5;
+//     uint32_t sum = 0;
+//     uint32_t average = 0;
+//     uint16_t convRegVal = 0;
+
+//     writeConfigReg(ADS1115_CHANNEL_ONE);
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (!readConvReg(&convRegVal))
+//         {
+//             sum = sum + swap16(convRegVal);
+//             ScheduleDelayed(ADS1115_CONVERSION_INTERVAL);
+//         } else
+//         {
+//             PX4_ERR("Current averaging failed");
+//         }
+//     }
+//     average = sum/n;
+//     return average;
+// }
+
+// double ADS1115::v_avging()
+// {
+//     int n = 5;
+//     uint32_t sum = 0;
+//     uint32_t average = 0;
+//     uint16_t convRegVal;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         readConvReg(&convRegVal);
+//         sum = sum + swap16(convRegVal);
+//         ScheduleDelayed(ADS1115_CONVERSION_INTERVAL);
+//     }
+//     average = sum/n;
+//     return average;
+// }
+
+// double ADS1115::i_avging()
+// {
+//     int n = 5;
+//     uint32_t sum = 0;
+//     uint32_t average = 0;
+//     uint16_t convRegVal;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         readConvReg(&convRegVal);
+//         sum = sum + swap16(convRegVal);
+//         ScheduleDelayed(ADS1115_CONVERSION_INTERVAL);
+//     }
+//     average = sum/n;
+//     return average;
+// }
 
 double ADS1115::getCurrent()
 {
@@ -227,7 +316,7 @@ double ADS1115::getCurrent()
     if (!readConvReg(&convRegVal))
     {
         current = (swap16(convRegVal))*(0.025);
-        //current = (avging())*0.025;
+        // current = (i_avging())*0.025; // ((avging())*(0.125/1000)/50)/0.1;
     } else
     {
         PX4_ERR("Current Reading Failed");
@@ -244,8 +333,8 @@ double ADS1115::getVoltage()
 
     if (!readConvReg(&convRegVal))
     {
-        //voltage = (swap16(convRegVal))*(0.125/0.0323);
-        voltage = (((avging())*0.125)/1000)/0.0323;
+        voltage = ((swap16(convRegVal))*(0.004))-v_offset;
+        // voltage = ((v_avging())*0.004); // (((avging())*0.125)/1000)/0.0323;
     } else
     {
         PX4_ERR("Voltage Reading Failed");
@@ -253,39 +342,39 @@ double ADS1115::getVoltage()
     return voltage;
 }
 
-double ADS1115::getWireTemp()
-{
-    double wiretemp = 0;
-    uint16_t convRegVal = 0;
+// double ADS1115::getWireTemp()
+// {
+//     double wiretemp = 0;
+//     uint16_t convRegVal = 0;
 
-    writeConfigReg(ADS1115_CHANNEL_THREE);
+//     writeConfigReg(ADS1115_CHANNEL_THREE);
 
-    if (!readConvReg(&convRegVal))
-    {
-        wiretemp = swap16(convRegVal);
-    } else
-    {
-        PX4_ERR("Wire Temperature Reading Failed");
-    }
-    return wiretemp;
-}
+//     if (!readConvReg(&convRegVal))
+//     {
+//         wiretemp = swap16(convRegVal);
+//     } else
+//     {
+//         PX4_ERR("Wire Temperature Reading Failed");
+//     }
+//     return wiretemp;
+// }
 
-double ADS1115::getBattTemp()
-{
-    double battTemp = 0;
-    uint16_t convRegVal = 0;
+// double ADS1115::getBattTemp()
+// {
+//     double battTemp = 0;
+//     uint16_t convRegVal = 0;
 
-    writeConfigReg(ADS1115_CHANNEL_FOUR);
+//     writeConfigReg(ADS1115_CHANNEL_FOUR);
 
-    if (!readConvReg(&convRegVal))
-    {
-        battTemp = swap16(convRegVal);
-    } else
-    {
-        PX4_ERR("Battery Temperature Reading Failed");
-    }
-    return battTemp;
-}
+//     if (!readConvReg(&convRegVal))
+//     {
+//         battTemp = swap16(convRegVal);
+//     } else
+//     {
+//         PX4_ERR("Battery Temperature Reading Failed");
+//     }
+//     return battTemp;
+// }
 
 void ADS1115::start()
 {
@@ -305,16 +394,23 @@ void ADS1115::Run()
 	struct power_monitor_s report;
 	report.timestamp = hrt_absolute_time();
 	report.voltage_v = getVoltage();
-    //   ScheduleDelayed(ADS1115_CONVERSION_INTERVAL);
-	report.current_a = getCurrent();
-    //    ScheduleDelayed(ADS1115_CONVERSION_INTERVAL);
-	// report.wiretemp_c = getWireTemp();
-    //    ScheduleDelayed(ADS1115_CONVERSION_INTERVAL);
-	// report.batttemp_c = getBattTemp();
+	//report.current_a = getCurrent();
+	report.wiretemp_c = 0;
+	report.batttemp_c = 0;
+
+    struct battery_status_s battreport;
+    battreport.timestamp = hrt_absolute_time();
+    battreport.voltage_v = getVoltage();
+    battreport.voltage_filtered_v = getVoltage();
+    //battreport.current_a = getCurrent();
 
 	int instance;
 	orb_publish_auto(ORB_ID(power_monitor), &_power_monitor_topic,
         &report, &instance, ORB_PRIO_DEFAULT);
+
+    int instance1;
+    orb_publish_auto(ORB_ID(battery_status), &_battery_status_topic,
+        &battreport, &instance1, ORB_PRIO_DEFAULT);
 
     ScheduleDelayed(100);
 }
@@ -369,7 +465,7 @@ namespace ads1115 // COMPLETE SH_MEASURE()
 		{
 			g_dev->stop();
             PX4_INFO("ADS1115 Driver sh_stop() success");
-			delete g_dev;
+			// delete g_dev;
 			g_dev = nullptr;
 			return PX4_OK;
 		} else
