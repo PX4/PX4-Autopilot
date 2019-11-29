fec89b64a0d59a7fe0c808d965d31158edcc123d
Julien Lecoeur <julien.lecoeur@gmail.com>
2019-11-29 20:29:52 +0100

    control_allocation: make allocation incremental

--- src/modules/control_allocator/ControlAllocation/ControlAllocationSequentialDesaturation.cpp
+++ src/modules/control_allocator/ControlAllocation/ControlAllocationSequentialDesaturation.cpp
@@ -48,7 +48,7 @@ ControlAllocationSequentialDesaturation::allocate()
 	updatePseudoInverse();
 
 	// Allocate
-	_actuator_sp = _mix * _control_sp;
+	_actuator_sp += _mix * (_control_sp - _control_allocated);
 
 	// go through control axes from lowest to highest priority and unsaturate the actuators
 	for (unsigned i = 0; i < NUM_AXES; i++) {
