ef442fab924bf5fd149811ac859c270532531598
kritz <kritz@ethz.ch>
2019-12-05 11:39:21 +0100

    Getter function for the diag elements of slice (#112)

--- matrix/Slice.hpp
+++ matrix/Slice.hpp
@@ -119,6 +119,16 @@ public:
         }
     }
 
+    Vector<Type, P<Q?P:Q> diag()
+    {
+        Slice<Type, P, Q, M, N>& self = *this;
+        Vector<Type,P<Q?P:Q> res;
+        for (size_t j = 0; j < (P<Q?P:Q); j++) {
+            res(j) = self(j,j);
+        }
+        return res;
+    }
+
     Type norm_squared()
     {
         Slice<Type, P, Q, M, N>& self = *this;
