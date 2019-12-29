cb491f898b89625f3f13081e3f689f0232a3051b
Daniel Agar <daniel@agar.ca>
2019-12-29 12:23:55 -0500

    Revert "sf0x: move to PX4Rangefinder and cleanup"
    This reverts commit b8856a27c12f030ae2a388f12dd954c3b4f1fd29.

--- src/drivers/distance_sensor/sf0x/sf0x_main.cpp
+++ src/drivers/distance_sensor/sf0x/sf0x_main.cpp
@@ -34,15 +34,34 @@
 #include "SF0X.hpp"
 
 #include <px4_platform_common/getopt.h>
-#include <px4_platform_common/module.h>
 
+/*
+ * Driver 'main' command.
+ */
+extern "C" __EXPORT int sf0x_main(int argc, char *argv[]);
+
+/**
+ * Local functions in support of the shell command.
+ */
 namespace sf0x
 {
 
-SF0X *g_dev{nullptr};
+SF0X	*g_dev;
 
-static int start(const char *port, uint8_t rotation)
+int	start(const char *port, uint8_t rotation);
+int	stop();
+int	test();
+int	reset();
+int	info();
+
+/**
+ * Start the driver.
+ */
+int
+start(const char *port, uint8_t rotation)
 {
+	int fd;
+
 	if (g_dev != nullptr) {
 		PX4_WARN("already started");
 		return -1;
@@ -52,19 +71,41 @@ static int start(const char *port, uint8_t rotation)
 	g_dev = new SF0X(port, rotation);
 
 	if (g_dev == nullptr) {
-		return -1;
+		goto fail;
 	}
 
-	if (g_dev->init() != PX4_OK) {
-		delete g_dev;
-		g_dev = nullptr;
-		return -1;
+	if (OK != g_dev->init()) {
+		goto fail;
+	}
+
+	/* set the poll rate to default, starts automatic data collection */
+	fd = open(RANGE_FINDER0_DEVICE_PATH, 0);
+
+	if (fd < 0) {
+		PX4_ERR("device open fail (%i)", errno);
+		goto fail;
+	}
+
+	if (ioctl(fd, SENSORIOCSPOLLRATE, SENSOR_POLLRATE_DEFAULT) < 0) {
+		goto fail;
 	}
 
 	return 0;
+
+fail:
+
+	if (g_dev != nullptr) {
+		delete g_dev;
+		g_dev = nullptr;
+	}
+
+	return -1;
 }
 
-static int stop()
+/**
+ * Stop the driver
+ */
+int stop()
 {
 	if (g_dev != nullptr) {
 		delete g_dev;
@@ -77,53 +118,124 @@ static int stop()
 	return 0;
 }
 
-static int status()
+/**
+ * Perform some basic functional tests on the driver;
+ * make sure we can collect data from the sensor in polled
+ * and automatic modes.
+ */
+int
+test()
+{
+	struct distance_sensor_s report;
+	ssize_t sz;
+
+	int fd = open(RANGE_FINDER0_DEVICE_PATH, O_RDONLY);
+
+	if (fd < 0) {
+		PX4_ERR("%s open failed (try 'sf0x start' if the driver is not running", RANGE_FINDER0_DEVICE_PATH);
+		return -1;
+	}
+
+	/* do a simple demand read */
+	sz = read(fd, &report, sizeof(report));
+
+	if (sz != sizeof(report)) {
+		PX4_ERR("immediate read failed");
+		return -1;
+	}
+
+	print_message(report);
+
+	/* start the sensor polling at 2 Hz rate */
+	if (OK != ioctl(fd, SENSORIOCSPOLLRATE, 2)) {
+		PX4_ERR("failed to set 2Hz poll rate");
+		return -1;
+	}
+
+	/* read the sensor 5x and report each value */
+	for (unsigned i = 0; i < 5; i++) {
+		struct pollfd fds;
+
+		/* wait for data to be ready */
+		fds.fd = fd;
+		fds.events = POLLIN;
+		int ret = poll(&fds, 1, 2000);
+
+		if (ret != 1) {
+			PX4_ERR("timed out");
+			break;
+		}
+
+		/* now go get it */
+		sz = read(fd, &report, sizeof(report));
+
+		if (sz != sizeof(report)) {
+			PX4_ERR("read failed: got %zi vs exp. %zu", sz, sizeof(report));
+			break;
+		}
+
+		print_message(report);
+	}
+
+	/* reset the sensor polling to the default rate */
+	if (OK != ioctl(fd, SENSORIOCSPOLLRATE, SENSOR_POLLRATE_DEFAULT)) {
+		PX4_ERR("ioctl SENSORIOCSPOLLRATE failed");
+		return -1;
+	}
+
+	return 0;
+}
+
+/**
+ * Reset the driver.
+ */
+int
+reset()
+{
+	int fd = open(RANGE_FINDER0_DEVICE_PATH, O_RDONLY);
+
+	if (fd < 0) {
+		PX4_ERR("open failed (%i)", errno);
+		return -1;
+	}
+
+	if (ioctl(fd, SENSORIOCRESET, 0) < 0) {
+		PX4_ERR("driver reset failed");
+		return -1;
+	}
+
+	if (ioctl(fd, SENSORIOCSPOLLRATE, SENSOR_POLLRATE_DEFAULT) < 0) {
+		PX4_ERR("driver poll restart failed");
+		return -1;
+	}
+
+	return 0;
+}
+
+/**
+ * Print a little info about the driver.
+ */
+int
+info()
 {
 	if (g_dev == nullptr) {
 		PX4_ERR("driver not running");
 		return -1;
 	}
 
+	printf("state @ %p\n", g_dev);
 	g_dev->print_info();
 
 	return 0;
 }
 
-static int usage()
-{
-	PRINT_MODULE_DESCRIPTION(
-		R"DESCR_STR(
-### Description
-
-Serial bus driver for the LightWare SF02/F, SF10/a, SF10/b, SF10/c, SF11/c Laser rangefinders.
-
-Most boards are configured to enable/start the driver on a specified UART using the SENS_SF0X_CFG parameter.
-
-Setup/usage information: https://docs.px4.io/en/sensor/sfxx_lidar.html
-
-### Examples
-
-Attempt to start driver on a specified serial device.
-$ sf0x start -d /dev/ttyS1
-Stop driver
-$ sf0x stop
-)DESCR_STR");
-
-	PRINT_MODULE_USAGE_NAME("sf0x", "driver");
-	PRINT_MODULE_USAGE_SUBCATEGORY("distance_sensor");
-	PRINT_MODULE_USAGE_COMMAND_DESCR("start", "Start driver");
-	PRINT_MODULE_USAGE_PARAM_STRING('d', nullptr, nullptr, "Serial device", false);
-	PRINT_MODULE_USAGE_PARAM_INT('R', 25, 1, 25, "Sensor rotation - downward facing by default", true);
-	PRINT_MODULE_USAGE_COMMAND_DESCR("stop", "Stop driver");
-	return PX4_OK;
-}
-
 } // namespace
 
-extern "C" __EXPORT int sf0x_main(int argc, char *argv[])
+int
+sf0x_main(int argc, char *argv[])
 {
 	uint8_t rotation = distance_sensor_s::ROTATION_DOWNWARD_FACING;
-	const char *device_path = nullptr;
+	const char *device_path = SF0X_DEFAULT_PORT;
 	int ch;
 	int myoptind = 1;
 	const char *myoptarg = nullptr;
@@ -139,26 +251,51 @@ extern "C" __EXPORT int sf0x_main(int argc, char *argv[])
 			break;
 
 		default:
-			sf0x::usage();
+			PX4_WARN("Unknown option!");
 			return -1;
 		}
 	}
 
 	if (myoptind >= argc) {
-		sf0x::usage();
-		return -1;
+		goto out_error;
 	}
 
+	/*
+	 * Start/load the driver.
+	 */
 	if (!strcmp(argv[myoptind], "start")) {
 		return sf0x::start(device_path, rotation);
-
-	} else if (!strcmp(argv[myoptind], "stop")) {
-		return sf0x::stop();
-
-	} else if (!strcmp(argv[myoptind], "status")) {
-		return sf0x::status();
 	}
 
-	sf0x::usage();
+	/*
+	 * Stop the driver
+	 */
+	if (!strcmp(argv[myoptind], "stop")) {
+		return sf0x::stop();
+	}
+
+	/*
+	 * Test the driver/device.
+	 */
+	if (!strcmp(argv[myoptind], "test")) {
+		return sf0x::test();
+	}
+
+	/*
+	 * Reset the driver.
+	 */
+	if (!strcmp(argv[myoptind], "reset")) {
+		return sf0x::reset();
+	}
+
+	/*
+	 * Print driver information.
+	 */
+	if (!strcmp(argv[myoptind], "info") || !strcmp(argv[myoptind], "status")) {
+		return sf0x::info();
+	}
+
+out_error:
+	PX4_ERR("unrecognized command, try 'start', 'test', 'reset' or 'info'");
 	return -1;
 }
