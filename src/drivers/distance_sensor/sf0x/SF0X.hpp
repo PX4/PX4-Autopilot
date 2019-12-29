b7badcfc9344ab3f13c1e37ef6d74cc51062e4dc
Daniel Agar <daniel@agar.ca>
2019-12-29 12:23:55 -0500

    sf0x: move to PX4Rangefinder and cleanup

--- src/drivers/distance_sensor/sf0x/SF0X.hpp
+++ src/drivers/distance_sensor/sf0x/SF0X.hpp
@@ -43,111 +43,47 @@
 
 #include <px4_platform_common/px4_config.h>
 #include <px4_platform_common/px4_work_queue/ScheduledWorkItem.hpp>
-
-#include <sys/types.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <semaphore.h>
-#include <string.h>
-#include <fcntl.h>
-#include <poll.h>
-#include <errno.h>
-#include <stdio.h>
-#include <math.h>
-#include <unistd.h>
-#include <termios.h>
-
-#include <perf/perf_counter.h>
-
+#include <lib/drivers/rangefinder/PX4Rangefinder.hpp>
 #include <drivers/drv_hrt.h>
-#include <drivers/drv_range_finder.h>
-#include <drivers/device/device.h>
-#include <drivers/device/ringbuffer.h>
 #include <lib/parameters/param.h>
-
-#include <uORB/uORB.h>
-#include <uORB/topics/distance_sensor.h>
+#include <lib/perf/perf_counter.h>
 
 #include "sf0x_parser.h"
 
-/* Configuration Constants */
-
-#define SF0X_TAKE_RANGE_REG		'd'
-
-// designated SERIAL4/5 on Pixhawk
-#define SF0X_DEFAULT_PORT		"/dev/ttyS6"
-
-class SF0X : public cdev::CDev, public px4::ScheduledWorkItem
+class SF0X : public px4::ScheduledWorkItem
 {
 public:
-	SF0X(const char *port = SF0X_DEFAULT_PORT, uint8_t rotation = distance_sensor_s::ROTATION_DOWNWARD_FACING);
-	virtual ~SF0X();
+	SF0X(const char *port, uint8_t rotation = distance_sensor_s::ROTATION_DOWNWARD_FACING);
+	~SF0X() override;
 
-	virtual int 			init() override;
-
-	virtual ssize_t			read(device::file_t *filp, char *buffer, size_t buflen) override;
-	virtual int			ioctl(device::file_t *filp, int cmd, unsigned long arg) override;
-
-	/**
-	* Diagnostics - print some basic information about the driver.
-	*/
+	int 			init();
 	void				print_info();
 
 private:
-	char 				_port[20];
-	uint8_t _rotation;
-	float				_min_distance;
-	float				_max_distance;
-	int         		        _conversion_interval;
-	ringbuffer::RingBuffer		*_reports;
-	int				_measure_interval;
-	bool				_collect_phase;
-	int				_fd;
-	char				_linebuf[10];
-	unsigned			_linebuf_index;
-	enum SF0X_PARSE_STATE		_parse_state;
-	hrt_abstime			_last_read;
 
-	int				_class_instance;
-	int				_orb_class_instance;
+	void				start();
+	void				stop();
+	void				Run() override;
+	int				measure();
+	int				collect();
 
-	orb_advert_t			_distance_sensor_topic;
+
+	PX4Rangefinder                  _px4_rangefinder;
+
+	char 				_port[20] {};
+	int         		        _interval{100000};
+	bool				_collect_phase{false};
+	int				_fd{-1};
+	char				_linebuf[10] {};
+	unsigned			_linebuf_index{0};
+	enum SF0X_PARSE_STATE		_parse_state {SF0X_PARSE_STATE0_UNSYNC};
+	hrt_abstime			_last_read{0};
 
 	unsigned			_consecutive_fail_count;
 
 	perf_counter_t			_sample_perf;
 	perf_counter_t			_comms_errors;
 
-	/**
-	* Initialise the automatic measurement state machine and start it.
-	*
-	* @note This function is called at open and error time.  It might make sense
-	*       to make it more aggressive about resetting the bus in case of errors.
-	*/
-	void				start();
 
-	/**
-	* Stop the automatic measurement state machine.
-	*/
-	void				stop();
-
-	/**
-	* Set the min and max distance thresholds if you want the end points of the sensors
-	* range to be brought in at all, otherwise it will use the defaults SF0X_MIN_DISTANCE
-	* and SF0X_MAX_DISTANCE
-	*/
-	void				set_minimum_distance(float min);
-	void				set_maximum_distance(float max);
-	float				get_minimum_distance();
-	float				get_maximum_distance();
-
-	/**
-	* Perform a poll cycle; collect from the previous measurement
-	* and start a new one.
-	*/
-	void				Run() override;
-	int				measure();
-	int				collect();
 
 };
