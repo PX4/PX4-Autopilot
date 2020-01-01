7775dfbc564d643fb678ea71030b02a366b16fe2
Daniel Agar <daniel@agar.ca>
2020-01-01 15:32:48 -0500

    mavsdk_test_runner.py remove verbose gazebo output

--- test/mavsdk_tests/mavsdk_test_runner.py
+++ test/mavsdk_tests/mavsdk_test_runner.py
@@ -117,8 +117,7 @@ class GzserverRunner(Runner):
                     workspace_dir + "/Tools/sitl_gazebo/models",
                     "PX4_SIM_SPEED_FACTOR": str(speed_factor)}
         self.cmd = "gzserver"
-        self.args = ["--verbose",
-                     workspace_dir + "/Tools/sitl_gazebo/worlds/iris.world"]
+        self.args = [workspace_dir + "/Tools/sitl_gazebo/worlds/iris.world"]
         self.log_prefix = "gzserver"
 
 
@@ -133,7 +132,6 @@ class GzclientRunner(Runner):
                     workspace_dir + "/Tools/sitl_gazebo/models",
                     "DISPLAY": ":0"}
         self.cmd = "gzclient"
-        self.args = ["--verbose"]
         self.log_prefix = "gzclient"
 
 
