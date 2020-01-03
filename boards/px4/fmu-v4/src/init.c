d1e690a036cd68ae1495cb97b7a081f87f5d7976
Beat Küng <beat-kueng@gmx.net>
2020-01-03 10:38:34 +0100

    fmu-v4: use hw description methods for timer configuration

--- boards/px4/fmu-v4/src/init.c
+++ boards/px4/fmu-v4/src/init.c
@@ -76,6 +76,9 @@
 #include <px4_platform_common/init.h>
 #include <px4_platform/board_dma_alloc.h>
 
+#include <drivers/drv_pwm_output.h>
+#include <px4_arch/io_timer.h>
+
 /****************************************************************************
  * Pre-Processor Definitions
  ****************************************************************************/
@@ -141,12 +144,9 @@ __EXPORT void board_peripheral_reset(int ms)
 __EXPORT void board_on_reset(int status)
 {
 	// Configure the GPIO pins to outputs and keep them low.
-	stm32_configgpio(GPIO_GPIO0_OUTPUT);
-	stm32_configgpio(GPIO_GPIO1_OUTPUT);
-	stm32_configgpio(GPIO_GPIO2_OUTPUT);
-	stm32_configgpio(GPIO_GPIO3_OUTPUT);
-	stm32_configgpio(GPIO_GPIO4_OUTPUT);
-	stm32_configgpio(GPIO_GPIO5_OUTPUT);
+	for (int i = 0; i < DIRECT_PWM_OUTPUT_CHANNELS; ++i) {
+		px4_arch_configgpio(io_timer_channel_get_gpio_output(i));
+	}
 
 	/**
 	 * On resets invoked from system (not boot) insure we establish a low
