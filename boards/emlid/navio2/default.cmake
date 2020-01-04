3bc113a9ced07bb11583da29dbc3adde8ca5bc5c
Daniel Agar <daniel@agar.ca>
2020-01-04 13:03:42 -0500

    ST LSM9DS1 9DOF IMU driver
     - this is a replacement for df_lsm9ds1_wrapper on the Emlid Navio2 once DriverFramework is removed

--- boards/emlid/navio2/default.cmake
+++ boards/emlid/navio2/default.cmake
@@ -19,10 +19,12 @@ px4_add_board(
 		gps
 		#imu # all available imu drivers
 		imu/mpu9250
+		imu/st/lsm9ds1
 		linux_pwm_out
 		linux_sbus
 		#magnetometer # all available magnetometer drivers
 		magnetometer/hmc5883
+		magnetometer/lsm9ds1_mag
 		pwm_out_sim
 		#telemetry # all available telemetry drivers
 	DF_DRIVERS # NOTE: DriverFramework is migrating to intree PX4 drivers
