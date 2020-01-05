1bcd120da0a8f835ad73eb622225a412eee366ea
Daniel Agar <daniel@agar.ca>
2020-01-05 14:03:57 -0500

    cmake merge linux cross compile targets (cross + native) and cleanup
     - consolidate cross and native variants (only a single default is needed)
       - beaglebone_blue_cross + beaglebone_blue_native => beaglebone_blue_default
       - emlid_navio2_cross + emlid_navio2_native => emlid_navio2_default
       - px4_raspberrypi_cross + px4_raspberrypi_native => px4_raspberrypi_default
     - move upload helpers to cmake/upload.cmake

--- boards/emlid/navio2/cmake/upload.cmake
+++ boards/emlid/navio2/cmake/upload.cmake
@@ -1,6 +1,6 @@
 ############################################################################
 #
-#   Copyright (c) 2018 PX4 Development Team. All rights reserved.
+#   Copyright (c) 2020 PX4 Development Team. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -30,3 +30,18 @@
 # POSSIBILITY OF SUCH DAMAGE.
 #
 ############################################################################
+
+if($ENV{AUTOPILOT_HOST})
+	set(AUTOPILOT_HOST $ENV{AUTOPILOT_HOST})
+else()
+	set(AUTOPILOT_HOST "navio")
+endif()
+
+add_custom_target(upload
+	COMMAND rsync -arh --progress
+			${CMAKE_RUNTIME_OUTPUT_DIRECTORY} ${PX4_SOURCE_DIR}/posix-configs/rpi/*.config ${PX4_SOURCE_DIR}/ROMFS # source
+			pi@${AUTOPILOT_HOST}:/home/pi/px4 # destination
+	DEPENDS px4
+	COMMENT "uploading px4"
+	USES_TERMINAL
+)
--- boards/parrot/bebop/cmake/upload.cmake
+++ boards/parrot/bebop/cmake/upload.cmake
@@ -1,6 +1,6 @@
 ############################################################################
 #
-#   Copyright (c) 2018 PX4 Development Team. All rights reserved.
+#   Copyright (c) 2020 PX4 Development Team. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -30,3 +30,12 @@
 # POSSIBILITY OF SUCH DAMAGE.
 #
 ############################################################################
+
+add_custom_target(upload
+	COMMAND ${PX4_BOARD_DIR}/scripts/adb_upload_to_bebop.sh
+			${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
+			/data/ftp/internal_000/px4
+	DEPENDS px4 ${PX4_BOARD_DIR}/scripts/adb_upload_to_bebop.sh
+	COMMENT "uploading px4"
+	USES_TERMINAL
+)
