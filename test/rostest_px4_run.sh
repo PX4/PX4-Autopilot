aadc74470b5d8bb1e9152d18abba21a181aca374
TSC21 <n.marques21@hotmail.com>
2020-01-13 11:36:30 +0000

    rostests: add PYTHONPATH export path so to find Python 2 packages

--- test/rostest_px4_run.sh
+++ test/rostest_px4_run.sh
@@ -6,6 +6,9 @@ PX4_SRC_DIR=${DIR}/..
 source /opt/ros/${ROS_DISTRO:-kinetic}/setup.bash
 source ${PX4_SRC_DIR}/Tools/setup_gazebo.bash ${PX4_SRC_DIR} ${PX4_SRC_DIR}/build/px4_sitl_default
 
+# Make sure that the PYTHONPATH points to Python 2 packages dir
+export PYTHONPATH=$PYTHONPATH:/usr/lib/python2.7/dist-packages
+
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:${PX4_SRC_DIR}:${PX4_SRC_DIR}/Tools/sitl_gazebo
 
 export ROS_LOG_DIR="$HOME/.ros/ros_logs"
