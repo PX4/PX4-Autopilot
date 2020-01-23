7915d09b01b31dea8222724dabf135bf02661cb2
SalimTerryLi <lhf2613@gmail.com>
2020-01-23 23:39:26 +0800

    this commit works

--- src/drivers/adc/ads1115/ADS1115.cpp
+++ src/drivers/adc/ads1115/ADS1115.cpp
@@ -5,6 +5,8 @@
 #include "ADS1115.h"
 #include <cassert>
 
+#include <stdio.h>
+
 ADS1115::ADS1115(I2C_Interface *interface) : _interface(interface)
 {
 
@@ -12,8 +14,8 @@ ADS1115::ADS1115(I2C_Interface *interface) : _interface(interface)
 
 int ADS1115::init()
 {
-	uint8_t config[2] = {0x00};
-	config[0] = CONFIG_HIGH_OS_START_SINGLE | CONFIG_HIGH_MUX_P0NG | CONFIG_HIGH_PGA_6144 | CONFIG_HIGH_MODE_SS;
+	uint8_t config[2] = {};
+	config[0] = CONFIG_HIGH_OS_NOACT | CONFIG_HIGH_MUX_P0NG | CONFIG_HIGH_PGA_6144 | CONFIG_HIGH_MODE_SS;
 	config[1] = CONFIG_LOW_DR_128SPS | CONFIG_LOW_COMP_MODE_TRADITIONAL | CONFIG_LOW_COMP_POL_RESET |
 		    CONFIG_LOW_COMP_LAT_NONE | CONFIG_LOW_COMP_QU_DISABLE;
 	_interface->writeReg(ADDRESSPOINTER_REG_CONFIG, config, 2);
@@ -22,7 +24,7 @@ int ADS1115::init()
 
 int ADS1115::setChannel(ADS1115::ChannelSelection ch)
 {
-	uint8_t buf[1] = {0x00};
+	uint8_t buf[2] = {};
 	uint8_t next_mux_reg = CONFIG_HIGH_MUX_P0NG;
 
 	switch (ch) {
@@ -48,7 +50,9 @@ int ADS1115::setChannel(ADS1115::ChannelSelection ch)
 	}
 
 	buf[0] = CONFIG_HIGH_OS_START_SINGLE | next_mux_reg | CONFIG_HIGH_PGA_6144 | CONFIG_HIGH_MODE_SS;
-	_interface->writeReg(ADDRESSPOINTER_REG_CONFIG, buf, 1);
+	buf[1] = CONFIG_LOW_DR_128SPS | CONFIG_LOW_COMP_MODE_TRADITIONAL | CONFIG_LOW_COMP_POL_RESET |
+		 CONFIG_LOW_COMP_LAT_NONE | CONFIG_LOW_COMP_QU_DISABLE;
+	_interface->writeReg(ADDRESSPOINTER_REG_CONFIG, buf, 2);    // must write whole register to take effect
 	return 0;
 }
 
@@ -56,14 +60,13 @@ bool ADS1115::isSampleReady()
 {
 	uint8_t buf[1] = {0x00};
 	_interface->readReg(ADDRESSPOINTER_REG_CONFIG, buf, 1); // Pull config register
-	return !(buf[0] & (uint8_t) 0x80);
+	return (buf[0] & (uint8_t) 0x80);
 }
 
 ADS1115::ChannelSelection ADS1115::getMeasurement(int16_t *value)
 {
 	uint8_t buf[2] = {0x00};
 	_interface->readReg(ADDRESSPOINTER_REG_CONFIG, buf, 1); // Pull config register
-
 	ChannelSelection channel;
 
 	switch ((buf[0] & (uint8_t) 0x70) >> 4) {
@@ -105,7 +108,6 @@ ADS1115::ChannelSelection ADS1115::cycleMeasure(int16_t *value)
 {
 	uint8_t buf[2] = {0x00};
 	_interface->readReg(ADDRESSPOINTER_REG_CONFIG, buf, 1); // Pull config register
-
 	ChannelSelection channel;
 	uint8_t next_mux_reg = CONFIG_HIGH_MUX_P0NG;
 
@@ -146,7 +148,8 @@ ADS1115::ChannelSelection ADS1115::cycleMeasure(int16_t *value)
 	}
 
 	buf[0] = CONFIG_HIGH_OS_START_SINGLE | next_mux_reg | CONFIG_HIGH_PGA_6144 | CONFIG_HIGH_MODE_SS;
-	_interface->writeReg(ADDRESSPOINTER_REG_CONFIG, buf, 1);
-
+	buf[1] = CONFIG_LOW_DR_128SPS | CONFIG_LOW_COMP_MODE_TRADITIONAL | CONFIG_LOW_COMP_POL_RESET |
+		 CONFIG_LOW_COMP_LAT_NONE | CONFIG_LOW_COMP_QU_DISABLE;
+	_interface->writeReg(ADDRESSPOINTER_REG_CONFIG, buf, 2);    // must write whole register to take effect
 	return channel;
 }
--- src/drivers/adc/ads1115/ads1115_main.cpp
+++ src/drivers/adc/ads1115/ads1115_main.cpp
@@ -1,33 +1,83 @@
 #include "ADS1115.h"
 #include <drivers/device/i2c.h>
+#include <px4_platform_common/module.h>
+#include <px4_platform_common/px4_work_queue/ScheduledWorkItem.hpp>
+#include <lib/perf/perf_counter.h>
+#include <uORB/Publication.hpp>
+#include <uORB/topics/adc_report.h>
+#include <drivers/drv_hrt.h>
+#include <px4_platform_common/getopt.h>
+
+#define ADS1115_DEFAULT_PATH "/dev/ads1115"
+
+using namespace time_literals;
 
 /* PX4 universal low-level driver wrapper for ADS1115 */
-class ADS1115_Drv : public device::I2C, public I2C_Interface
+class ADS1115_Drv : public device::I2C, public I2C_Interface, public ModuleBase<ADS1115_Drv>,
+	public px4::ScheduledWorkItem
 {
 public:
-	ADS1115_Drv(uint8_t bus, uint8_t address, const char *devname);
-	~ADS1115_Drv() override = default;
+	ADS1115_Drv(uint8_t bus, uint8_t address);
+	~ADS1115_Drv() override;
 	int init() override;
 
 	void readReg(uint8_t addr, uint8_t *buf, size_t len) override;
 
 	void writeReg(uint8_t addr, uint8_t *buf, size_t len) override;
 
+	/** @see ModuleBase */
+	static int task_spawn(int argc, char *argv[]);
+
+	/** @see ModuleBase */
+	static int custom_command(int argc, char *argv[]);
+
+	/** @see ModuleBase */
+	static int print_usage(const char *reason = nullptr);
+
 protected:
 	ADS1115 *_ads1115 = nullptr;
 
+	adc_report_s _adc_report = {};
+
 private:
+	void Run() override;
+
+	uORB::Publication<adc_report_s>		_to_adc_report{ORB_ID(adc_report)};
+
+	static const hrt_abstime	SAMPLE_INTERVAL{50_ms};
+
+	perf_counter_t			_cycle_perf;
+
+	int     _channel_cycle_count = 0;
 
 };
 
-ADS1115_Drv::ADS1115_Drv(uint8_t bus, uint8_t address, const char *devname) :
-	I2C("ads1115", devname, bus, address, 400000)
+ADS1115_Drv::ADS1115_Drv(uint8_t bus, uint8_t address) :
+	I2C("ADS1115", ADS1115_DEFAULT_PATH, bus, address, 400000),
+	ScheduledWorkItem(MODULE_NAME, px4::device_bus_to_wq(this->get_device_id())),
+	_cycle_perf(perf_alloc(PC_ELAPSED, MODULE_NAME": single-sample"))
 {
 	_ads1115 = new ADS1115(this);
+
+	// TODO fill a vaild dev type
+	//this->_device_id.devid_s.devtype=0;
+
+	_adc_report.device_id = this->get_device_id();
 
 	if (_ads1115 == nullptr) {
 		PX4_ERR("ads1115 logical driver alloc failed");
 	}
+}
+
+ADS1115_Drv::~ADS1115_Drv()
+{
+	ScheduleClear();
+
+	if (_ads1115 != nullptr) {
+		delete _ads1115;
+	}
+
+	perf_free(_cycle_perf);
 }
 
 int ADS1115_Drv::init()
@@ -50,6 +100,10 @@ int ADS1115_Drv::init()
 		return ret;
 	}
 
+	_ads1115->setChannel(ADS1115::A0);  // prepare for the first measure.
+
+	ScheduleOnInterval(SAMPLE_INTERVAL / 4, SAMPLE_INTERVAL / 4);
+
 	return PX4_OK;
 }
 
@@ -66,7 +120,150 @@ void ADS1115_Drv::writeReg(uint8_t addr, uint8_t *buf, size_t len)
 	transfer(buffer, len + 1, nullptr, 0);
 }
 
+void ADS1115_Drv::Run()
+{
+	if (should_exit()) {
+		PX4_INFO("ADS1115 stopping.");
+		ScheduleClear();
+		exit_and_cleanup();
+		return;
+	}
+
+	lock();
+	perf_begin(_cycle_perf);
+
+	_adc_report.timestamp = hrt_absolute_time();
+
+	if (_ads1115->isSampleReady()) { // whether ADS1115 is ready to be read or not
+		int16_t buf;
+		ADS1115::ChannelSelection ch = _ads1115->cycleMeasure(&buf);
+		++_channel_cycle_count;
+
+		switch (ch) {
+		case ADS1115::A0:
+			_adc_report.channel_id[0] = 0;
+			_adc_report.raw_data[0] = buf;
+			_adc_report.resolution[0] = 32768;
+			_adc_report.v_ref[0] = 6.144f;
+			break;
+
+		case ADS1115::A1:
+			_adc_report.channel_id[1] = 1;
+			_adc_report.raw_data[1] = buf;
+			_adc_report.resolution[1] = 32768;
+			_adc_report.v_ref[1] = 6.144f;
+			break;
+
+		case ADS1115::A2:
+			_adc_report.channel_id[2] = 2;
+			_adc_report.raw_data[2] = buf;
+			_adc_report.resolution[2] = 32768;
+			_adc_report.v_ref[2] = 6.144f;
+			break;
+
+		case ADS1115::A3:
+			_adc_report.channel_id[3] = 3;
+			_adc_report.raw_data[3] = buf;
+			_adc_report.resolution[3] = 32768;
+			_adc_report.v_ref[3] = 6.144f;
+			break;
+
+		default:
+			PX4_DEBUG("ADS1115: undefined behaviour");
+			_ads1115->setChannel(ADS1115::A0);
+			--_channel_cycle_count;
+			break;
+		}
+
+		if (_channel_cycle_count == 4) { // ADS1115 has 4 channels
+			_channel_cycle_count = 0;
+			_to_adc_report.publish(_adc_report);
+		}
+
+	} else {
+		PX4_WARN("ADS1115 not ready!");
+	}
+
+	perf_end(_cycle_perf);
+	unlock();
+}
+
+int ADS1115_Drv::task_spawn(int argc, char **argv)
+{
+	ADS1115_Drv *instance;
+
+	uint8_t i2c_bus = 1;
+	uint8_t i2c_addr = 0x48;
+
+	int myoptind = 1;
+	int ch;
+	const char *myoptarg = nullptr;
+
+	while ((ch = px4_getopt(argc, argv, "a:b:", &myoptind, &myoptarg)) != EOF) {
+		switch (ch) {
+		case 'b':
+			i2c_bus = atoi(myoptarg);
+			break;
+
+		case 'a':
+			i2c_addr = atoi(myoptarg);
+			break;
+
+		default:
+			print_usage("unknown parameters");
+			return -EINVAL;
+		}
+	}
+
+	instance = new ADS1115_Drv(i2c_bus, i2c_addr);
+
+	if (instance) {
+		_object.store(instance);
+		_task_id = task_id_is_work_queue;
+
+		if (instance->init() == PX4_OK) {
+			return PX4_OK;
+		}
+
+	} else {
+		PX4_ERR("alloc failed");
+	}
+
+	delete instance;
+	_object.store(nullptr);
+	_task_id = -1;
+
+	return PX4_ERROR;
+}
+
+int ADS1115_Drv::custom_command(int argc, char **argv)
+{
+	return PX4_OK;
+}
+
+int ADS1115_Drv::print_usage(const char *reason)
+{
+	if (reason) {
+		PX4_WARN("%s\n", reason);
+	}
+
+	PRINT_MODULE_DESCRIPTION(
+		R"DESCR_STR(
+### Description
+ADS1115 driver.
+
+)DESCR_STR");
+
+    PRINT_MODULE_USAGE_NAME("ads1115", "driver");
+    PRINT_MODULE_USAGE_COMMAND("start");
+    PRINT_MODULE_USAGE_PARAM_INT('a',72,0,255,"device address on this bus",true);
+    PRINT_MODULE_USAGE_PARAM_INT('b',1,0,255,"bus that ADS1115 is connected to",true);
+    PRINT_MODULE_USAGE_DEFAULT_COMMANDS();
+
+    return 0;
+}
+
 extern "C" int ads1115_main(int argc, char *argv[])
 {
-	return 0;
+	return ADS1115_Drv::main(argc,argv);
 }
