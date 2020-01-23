1d89351149cb5fda71ca4caecd9728e0491281e0
Matthias Grob <maetugr@gmail.com>
2020-01-23 22:09:45 +0100

    Commander: clarify the rc override message
    Message says novice understanable what happened.

--- src/modules/commander/Commander.cpp
+++ src/modules/commander/Commander.cpp
@@ -1769,7 +1769,7 @@ Commander::run()
 
 				// revert to position control in any case
 				main_state_transition(status, commander_state_s::MAIN_STATE_POSCTL, status_flags, &_internal_state);
-				mavlink_log_info(&mavlink_log_pub, "Autonomy off! Returned control to pilot");
+				mavlink_log_info(&mavlink_log_pub, "Pilot took over control using sticks");
 			}
 		}
 
