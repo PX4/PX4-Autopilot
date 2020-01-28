1552e09bbcc9cde3794c7f5415b6fbf67ca29f4c
Julian Oes <julian@oes.ch>
2020-01-28 16:37:21 +0100

    mavsdk_tests: enable VTOL, fix TEST_CASE name
    We can't use commas in the TEST_CASE name. With the comma, the test did
    not run and displayed:
    
    Filters: Takeoff, transition and RTL
    No test cases matched 'Takeoff'
    No test cases matched 'transition and RTL'
    ==========================================
    No tests ran

--- test/mavsdk_tests/mavsdk_test_runner.py
+++ test/mavsdk_tests/mavsdk_test_runner.py
@@ -17,11 +17,11 @@ test_matrix = [
         "test_filter": "[multicopter]",
         "timeout_min": 20,
     },
-    #{
-    #    "model": "standard_vtol",
-    #    "test_filter": "[vtol]",
-    #    "timeout_min": 20,
-    #},
+    {
+        "model": "standard_vtol",
+        "test_filter": "[vtol]",
+        "timeout_min": 20,
+    },
     # {
     #     "model": "standard_plane",
     #     "test_filter": "[plane]",
