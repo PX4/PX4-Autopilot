32150652dc0e6c21c8e1a3d404ffc2ab2b5241cb
stmoon <munhoney@gmail.com>
2020-02-01 13:53:03 +0900

    ros2_distro = "", if ros2_distro is not found finally

--- msg/tools/generate_microRTPS_bridge.py
+++ msg/tools/generate_microRTPS_bridge.py
@@ -257,6 +257,8 @@ if ros2_distro is None :
         rosversion_out = rosversion_out.rstrip()
         if rosversion_out not in ["<unknown>", "kinetic", "lunar", "melodic"]:
             ros2_distro = rosversion_out
+        else :
+            ros2_distro = ""
     except OSError as e:
         if e.errno == errno.ENOENT:
             if args.ros2_distro != None:
