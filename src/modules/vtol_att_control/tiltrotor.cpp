d4e43d2d8de93e45fab24ed67648cadfaa5c9b8a
Silvan <silvan@hotmail.com>
2020-02-09 20:50:26 +0100

    VTOL tiltrotor: introduce spin up time with spin up tilt angle
    As some tiltrotor systems need a certain tilt angle of their motors in oder to spin up freely,
    this commit introduces an additional parameter VT_TILT_SPINUP and sets the motor tilt to
    this value if disarmed or within 1s since arming.
    
    The spinup consists of 2 phases:
        1st phase fixed tilt (duration hardcoded to 1s)
        2nd phase transition from spinup tilt to multicopter tilt (hardcoded to 0.5s)
    
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- src/modules/vtol_att_control/tiltrotor.cpp
+++ src/modules/vtol_att_control/tiltrotor.cpp
@@ -43,6 +43,7 @@
 #include "vtol_att_control_main.h"
 
 using namespace matrix;
+using namespace time_literals;
 
 #define ARSP_YAW_CTRL_DISABLE 7.0f	// airspeed at which we stop controlling yaw during a front transition
 
@@ -61,6 +62,7 @@ Tiltrotor::Tiltrotor(VtolAttitudeControl *attc) :
 	_params_handles_tiltrotor.tilt_mc = param_find("VT_TILT_MC");
 	_params_handles_tiltrotor.tilt_transition = param_find("VT_TILT_TRANS");
 	_params_handles_tiltrotor.tilt_fw = param_find("VT_TILT_FW");
+	_params_handles_tiltrotor.tilt_spinup = param_find("VT_TILT_SPINUP");
 	_params_handles_tiltrotor.front_trans_dur_p2 = param_find("VT_TRANS_P2_DUR");
 }
 
@@ -80,6 +82,10 @@ Tiltrotor::parameters_update()
 	/* vtol tilt mechanism position in fw mode */
 	param_get(_params_handles_tiltrotor.tilt_fw, &v);
 	_params_tiltrotor.tilt_fw = v;
+
+	/* vtol tilt mechanism position during motor spinup */
+	param_get(_params_handles_tiltrotor.tilt_spinup, &v);
+	_params_tiltrotor.tilt_spinup = v;
 
 	/* vtol front transition phase 2 duration */
 	param_get(_params_handles_tiltrotor.front_trans_dur_p2, &v);
@@ -209,9 +215,46 @@ void Tiltrotor::update_mc_state()
 {
 	VtolType::update_mc_state();
 
-	_tilt_control = VtolType::pusher_assist();
+	/*Motor spin up: define the first second after arming as motor spin up time, during which
+	* the tilt is set to the value of VT_TILT_SPINUP. This allowes the user to set a spin up
+	* tilt angle in case the propellers don't spin up smootly in full upright (MC mode) position.
+	*/
 
-	_v_att_sp->thrust_body[2] = Tiltrotor::thrust_compensation_for_tilt();
+	const int spin_up_duration_p1 = 1000_ms; // duration of 1st phase of spinup (at fixed tilt)
+	const int spin_up_duration_p2 = 700_ms; // duration of 2nd phase of spinup (transition from spinup tilt to mc tilt)
+
+	// reset this timestamp while disarmed
+	if (!_v_control_mode->flag_armed) {
+		_last_timestamp_disarmed = hrt_absolute_time();
+		_tilt_motors_for_startup = true;
+
+	} else if (_tilt_motors_for_startup) {
+		// leave motors tilted forward after arming to allow them to spin up easier
+		if (hrt_absolute_time() - _last_timestamp_disarmed > (spin_up_duration_p1 + spin_up_duration_p2)) {
+			_tilt_motors_for_startup = false;
+		}
+	}
+
+	if (_tilt_motors_for_startup) {
+		if (hrt_absolute_time() - _last_timestamp_disarmed < spin_up_duration_p1) {
+			_tilt_control = _params_tiltrotor.tilt_spinup;
+
+		} else {
+			// duration phase 2: begin to adapt tilt to multicopter tilt
+			float delta_tilt = (_params_tiltrotor.tilt_mc - _params_tiltrotor.tilt_spinup);
+			_tilt_control = _params_tiltrotor.tilt_spinup + delta_tilt / spin_up_duration_p2 * (hrt_absolute_time() -
+					(_last_timestamp_disarmed + spin_up_duration_p1));
+		}
+
+		_mc_yaw_weight = 0.0f; //disable yaw control during spinup
+
+	} else {
+		// normal operation
+		_tilt_control = VtolType::pusher_assist();
+		_mc_yaw_weight = 1.0f;
+		_v_att_sp->thrust_body[2] = Tiltrotor::thrust_compensation_for_tilt();
+	}
+
 }
 
 void Tiltrotor::update_fw_state()
