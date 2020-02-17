1b017f635610fc3227253d403d084164548b0235
Ivan Biriuk <ivan.biriuk@uvify.com>
2020-02-17 16:09:30 +0900

    docker fix

--- Tools/docker_run.sh
+++ Tools/docker_run.sh
@@ -4,7 +4,7 @@ if [ -z ${PX4_DOCKER_REPO+x} ]; then
 	echo "guessing PX4_DOCKER_REPO based on input";
 	if [[ $@ =~ .*px4_fmu.* ]]; then
 		# nuttx-px4fmu-v{1,2,3,4,5}
-		PX4_DOCKER_REPO="px4io/px4-dev-nuttx:2019-11-25"
+		PX4_DOCKER_REPO="px4io/px4-dev-nuttx:latest"
 	elif [[ $@ =~ .*ocpoc.* ]] || [[ $@ =~ .*navio2.* ]] || [[ $@ =~ .*raspberry.* ]] || [[ $@ =~ .*beaglebone.* ]]; then
 		# aerotenna_ocpoc_default, beaglebone_blue_default, emlid_navio2_default, px4_raspberrypi_default
 		PX4_DOCKER_REPO="px4io/px4-dev-armhf:2019-11-25"
@@ -27,7 +27,7 @@ fi
 
 # otherwise default to nuttx
 if [ -z ${PX4_DOCKER_REPO+x} ]; then
-	PX4_DOCKER_REPO="px4io/px4-dev-nuttx:2019-11-25"
+	PX4_DOCKER_REPO="px4io/px4-dev-nuttx:latest"
 fi
 
 # docker hygiene
