4df8f35aeb00e560ffc5400283f7e0f82767b6f3
SalimTerryLi <lhf2613@gmail.com>
2020-03-05 03:20:00 +0800

    add magic value of osc freq

--- src/drivers/pca9685_pwm_out/PCA9685.h
+++ src/drivers/pca9685_pwm_out/PCA9685.h
@@ -84,7 +84,10 @@ namespace drv_pca9685_pwm
 
 #define PCA9685_PWM_CHANNEL_COUNT 16
 #define PCA9685_PWM_RES 4096        //Resolution 4096=12bit
-#define PCA9685_CLOCK_INT 25000000.0 //25MHz internal clock
+/* This should be 25000000 ideally,
+ * but it seems most chips have its oscillator working at a higher frequency
+ * Reference: https://github.com/adafruit/Adafruit-PWM-Servo-Driver-Library/blob/6664ce936210eea53259b814062009d9569a4213/Adafruit_PWMServoDriver.h#L66 */
+#define PCA9685_CLOCK_INT 26075000.0 //25MHz internal clock
 #ifndef PCA9685_CLOCL_EXT
 #define PCA9685_CLOCK_FREQ PCA9685_CLOCK_INT   // use int clk
 #else
