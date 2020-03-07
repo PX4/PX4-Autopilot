29227b84923bee755ca8354c4e58993c7f22fb9e
Igor Mišić <igy1000mb@gmail.com>
2020-03-07 12:25:34 +0100

    spracing-h7extreme: init commit

---
+++ boards/spracing/h7extreme/bootloader.cmake
@@ -0,0 +1,20 @@
+
+px4_add_board(
+	PLATFORM nuttx
+	VENDOR spracing
+	MODEL h7extreme
+	LABEL bootloader
+	TOOLCHAIN arm-none-eabi
+    ARCHITECTURE cortex-m7
+
+
+	DRIVERS
+#	bootloader
+
+	MODULES
+
+	SYSTEMCMDS
+
+	EXAMPLES
+
+	)
