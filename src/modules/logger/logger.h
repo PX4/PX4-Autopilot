b1efe0a828dd8fa613328007c519e5b193794418
bazooka joe <bazookajoe1900@gmail.com>
2020-03-09 03:03:08 -0700

    logger - using ModuleParams
    Fixed parameters type

--- src/modules/logger/logger.h
+++ src/modules/logger/logger.h
@@ -351,8 +351,8 @@ private:
 		(ParamInt<px4::params::SDLOG_DIRS_MAX>) _param_sdlog_dirs_max,
 		(ParamInt<px4::params::SDLOG_PROFILE>) _param_sdlog_profile,
 		(ParamInt<px4::params::SDLOG_MISSION>) _param_sdlog_mission,
-		(ParamInt<px4::params::SDLOG_BOOT_BAT>) _param_sdlog_boot_bat,
-		(ParamInt<px4::params::SDLOG_UUID>) _param_sdlog_uuid
+		(ParamBool<px4::params::SDLOG_BOOT_BAT>) _param_sdlog_boot_bat,
+		(ParamBool<px4::params::SDLOG_UUID>) _param_sdlog_uuid
 	)
 };
 
