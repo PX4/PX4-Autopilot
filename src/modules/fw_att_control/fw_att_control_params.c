8e1ca6f61363becf37446e505049d10f9110b7a2
Roman Dvořák <romandvorak@mlab.cz>
2020-03-10 12:48:18 +0100

    Update src/modules/fw_att_control/fw_att_control_params.c
    Co-Authored-By: Mathieu Bresciani <brescianimathieu@gmail.com>

--- src/modules/fw_att_control/fw_att_control_params.c
+++ src/modules/fw_att_control/fw_att_control_params.c
@@ -543,7 +543,7 @@ PARAM_DEFINE_INT32(FW_ARSP_MODE, 0);
  * This enables a logic that automatically adjusts the output of the rate controller to take
  * into account the real torque produced by an aerodynamic control surface given
  * the current deviation from the trim airspeed (FW_AIRSPD_TRIM).
- 
+ *
  * Enable when using aerodynamic control surfaces (e.g.: plane)
  * Disable when using rotor wings (e.g.: autogyro)
  *
