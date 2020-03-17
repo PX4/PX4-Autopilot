bda76fd53626dcfda7366709c3563bedcc63fbb5
RomanBapst <bapstroman@gmail.com>
2020-03-17 10:22:30 +0300

    better comments
    Signed-off-by: RomanBapst <bapstroman@gmail.com>

--- src/lib/flight_tasks/tasks/Transition/FlightTaskTransition.cpp
+++ src/lib/flight_tasks/tasks/Transition/FlightTaskTransition.cpp
@@ -60,8 +60,10 @@ void FlightTaskTransition::checkSetpoints(vehicle_local_position_setpoint_s &set
 
 bool FlightTaskTransition::update()
 {
-	// level wings during the transition, altitude should be controlled
 	_acceleration_setpoint.xy() = matrix::Vector2f(0.f, 0.f);
+	// demand zero vertical velocity and level attitude
+	// tailsitters will override attitude and thrust setpoint
+	// tiltrotors and standard vtol will overrride roll and pitch setpoint but keep vertical thrust setpoint
 	_position_setpoint *= NAN;
 	_velocity_setpoint(2) = 0.0f;
 	_position_setpoint(2) = NAN;
