d839688f52ed8c814d3dcc44550c605349fa1b86
baumanta <tanja@auterion.com>
2020-03-24 09:56:31 +0100

    add library for current- or thrust-based mag compensation

--- src/lib/mag_compensation/mag_compensation_params.c
+++ src/lib/mag_compensation/mag_compensation_params.c
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2016 PX4 Development Team. All rights reserved.
+ *   Copyright (c) 2019 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,72 +30,14 @@
  * POSSIBILITY OF SUCH DAMAGE.
  *
  ****************************************************************************/
-
-#pragma once
-
 /**
- * @file parameters.h
+ * Type of magnetometer compensation
  *
- * defines the list of parameters that are used within the sensors module
+ * @value 0 Disabled
+ * @value 1 Throttle-based compensation
+ * @value 2 Current-based compensation
  *
- * @author Beat Kueng <beat-kueng@gmx.net>
+ * @category system
+ * @group Sensor Calibration
  */
-
-#include <lib/parameters/param.h>
-
-namespace sensors
-{
-
-struct Parameters {
-	float diff_pres_offset_pa;
-#ifdef ADC_AIRSPEED_VOLTAGE_CHANNEL
-	float diff_pres_analog_scale;
-#endif /* ADC_AIRSPEED_VOLTAGE_CHANNEL */
-
-	int32_t board_rotation;
-
-	float board_offset[3];
-
-	//parameters for current/throttle-based mag compensation
-	float mag_comp_paramX[4];
-	float mag_comp_paramY[4];
-	float mag_comp_paramZ[4];
-
-	int32_t air_cmodel;
-	float air_tube_length;
-	float air_tube_diameter_mm;
-};
-
-struct ParameterHandles {
-	param_t diff_pres_offset_pa;
-#ifdef ADC_AIRSPEED_VOLTAGE_CHANNEL
-	param_t diff_pres_analog_scale;
-#endif /* ADC_AIRSPEED_VOLTAGE_CHANNEL */
-
-	param_t board_rotation;
-
-	param_t board_offset[3];
-
-	param_t mag_comp_paramX[4];
-	param_t mag_comp_paramY[4];
-	param_t mag_comp_paramZ[4];
-
-	param_t air_cmodel;
-	param_t air_tube_length;
-	param_t air_tube_diameter_mm;
-
-};
-
-/**
- * initialize ParameterHandles struct
- */
-void initialize_parameter_handles(ParameterHandles &parameter_handles);
-
-
-/**
- * Read out the parameters using the handles into the parameters struct.
- * @return 0 on success, <0 on error
- */
-void update_parameters(const ParameterHandles &parameter_handles, Parameters &parameters);
-
-} /* namespace sensors */
+PARAM_DEFINE_INT32(CAL_MAG_COMP_TYP, 0);
