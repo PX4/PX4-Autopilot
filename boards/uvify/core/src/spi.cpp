f75196d2bf3b148b03103a192f71de6f1ab56452
Hyon Lim <lim@uvify.com>
2020-03-26 02:27:14 +0900

    Board support: UVify Core. As Core is not using SPI4 as external, SPI4 external interface related code removed. Also, PMW3901 flow sensor is added on SPI2 bus.

--- boards/uvify/core/src/spi.cpp
+++ boards/uvify/core/src/spi.cpp
@@ -46,14 +46,13 @@ constexpr px4_spi_bus_t px4_spi_buses[SPI_BUS_MAX_BUS_ITEMS] = {
 		initSPIDevice(SPIDEV_FLASH(0), SPI::CS{GPIO::PortD, GPIO::Pin10}),
 		initSPIDevice(DRV_BARO_DEVTYPE_MS5611, SPI::CS{GPIO::PortD, GPIO::Pin7}),
 		initSPIDevice(DRV_BARO_DEVTYPE_MS5607, SPI::CS{GPIO::PortD, GPIO::Pin7}),
-	}),
-	initSPIBusExternal(SPI::Bus::SPI4, {
-		SPI::CS{GPIO::PortA, GPIO::Pin8},
-	}),
+		initSPIDevice(DRV_FLOW_DEVTYPE_PMW3901, SPI::CS{GPIO::PortB, GPIO::Pin12})
+	})
 };
 
 static constexpr bool unused = validateSPIConfig(px4_spi_buses);
 
+#if defined(BOARD_HAS_BUS_MANIFEST)
 __EXPORT bool board_has_bus(enum board_bus_types type, uint32_t bus)
 {
 	bool rv = true;
@@ -73,4 +72,4 @@ __EXPORT bool board_has_bus(enum board_bus_types type, uint32_t bus)
 
 	return rv;
 }
-
+#endif
