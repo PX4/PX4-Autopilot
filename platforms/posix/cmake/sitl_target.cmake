667daaee952cd6a57bd0f450457fa62fba8dac76
JaeyoungLim <gokoreas@gmail.com>
2020-03-29 13:55:18 +0200

    Add world targets and update submodules
    Add world names as make targets and include the worlds by updating the submodules

--- platforms/posix/cmake/sitl_target.cmake
+++ platforms/posix/cmake/sitl_target.cmake
@@ -66,7 +66,7 @@ set(models none shell
 	standard_vtol tailsitter tiltrotor
 	rover boat
 	uuv_hippocampus)
-set(worlds none empty warehouse sonoma_raceway)
+set(worlds none empty baylands ksql_airport mcmillan_airfield sonoma_raceway warehouse)
 set(all_posix_vmd_make_targets)
 foreach(viewer ${viewers})
 	foreach(debugger ${debuggers})
