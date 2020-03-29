0a7cde48197f2cfd6c1dcd14f70de0b11599585c
Matthias Grob <maetugr@gmail.com>
2020-03-29 20:25:36 +0200

    arch.sh: use binary repository for gazebo
    This makes installation a lot faster and less error prone.

--- Tools/setup/arch.sh
+++ Tools/setup/arch.sh
@@ -136,14 +136,18 @@ if [[ $INSTALL_SIM == "true" ]]; then
 			opencv \
 			protobuf \
 			vtk \
-			yay \
 			;
 
-		# enable multicore gazebo compilation
-		sudo sed -i '/MAKEFLAGS=/c\MAKEFLAGS="-j'$(($(nproc)+2))'"' /etc/makepkg.conf
+		# add community binary repository for gazebo and ROS
+		# https://wiki.archlinux.org/index.php/Unofficial_user_repositories#oscloud
+		if ! grep -q oscloud /etc/pacman.conf; then
+			echo "# ROS gazebo repository for PX4
+[oscloud]
+SigLevel = Never
+Server = http://repo.oscloud.info/" | sudo tee -a /etc/pacman.conf > /dev/null
+		fi
 
-		# install gazebo from AUR
-		yay -S gazebo --noconfirm
+		sudo pacman -Sy --noconfirm --needed gazebo
 
 		if sudo dmidecode -t system | grep -q "Manufacturer: VMware, Inc." ; then
 			# fix VMWare 3D graphics acceleration for gazebo
