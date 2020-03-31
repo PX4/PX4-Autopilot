baa50a19853e02160a4dcc4a62b3bf07e3a98d30
Daniel Agar <daniel@agar.ca>
2020-03-31 13:23:52 -0400

    boards: omnibus/f4sd add board_dma_map.h

--- boards/omnibus/f4sd/default.cmake
+++ boards/omnibus/f4sd/default.cmake
@@ -20,11 +20,8 @@ px4_add_board(
 		#distance_sensor # all available distance sensor drivers
 		dshot
 		gps
-		#heater
-		#imu # all available imu drivers
-		#imu/mpu6000
-		imu/invensense/mpu6000
 		imu/invensense/icm20602
+		imu/invensense/mpu6000
 		#irlock
 		#lights/blinkm
 		lights/rgbled
