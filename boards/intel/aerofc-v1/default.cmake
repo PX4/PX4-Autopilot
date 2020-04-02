06478d99972548d4c4c2f370f3608bd031833026
Daniel Agar <daniel@agar.ca>
2020-04-01 20:28:26 -0400

    delete obsolete SENSORIOCSPOLLRATE/SENSORIOCRESET and systemcmds/config

--- boards/intel/aerofc-v1/default.cmake
+++ boards/intel/aerofc-v1/default.cmake
@@ -62,7 +62,6 @@ px4_add_board(
 		#vtol_att_control
 	SYSTEMCMDS
 		bl_update
-		config
 		#dmesg
 		dumpfile
 		esc_calib
--- boards/px4/fmu-v5/rover.cmake
+++ boards/px4/fmu-v5/rover.cmake
@@ -65,7 +65,6 @@ px4_add_board(
 		vmount
 	SYSTEMCMDS
 		bl_update
-		config
 		dmesg
 		dumpfile
 		esc_calib
