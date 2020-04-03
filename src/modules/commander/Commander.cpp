aa6f9280e12948a8050dc0e61ae0290e4d69e853
Beat Küng <beat-kueng@gmx.net>
2020-04-03 11:48:43 +0200

    fix commander: set _have_taken_off_since_arming when !landed upon arming
    If arming and already !landed, _have_taken_off_since_arming will not be set
    and thus auto-disarm after 10s will be triggered (with default config).
    
    This can only happen due to quick state changes, as land detector generally
    sets landed=true if !armed.

--- src/modules/commander/Commander.cpp
+++ src/modules/commander/Commander.cpp
@@ -2189,13 +2189,23 @@ Commander::run()
 		if (_was_armed != armed.armed) {
 			_status_changed = true;
 
-			if (!armed.armed) { // increase the flight uuid upon disarming
+			if (armed.armed) {
+				if (!_land_detector.landed) { // check if takeoff already detected upon arming
+					_have_taken_off_since_arming = true;
+				}
+
+			} else { // increase the flight uuid upon disarming
 				const int32_t flight_uuid = _param_flight_uuid.get() + 1;
 				_param_flight_uuid.set(flight_uuid);
 				_param_flight_uuid.commit_no_notification();
 
 				_last_disarmed_timestamp = hrt_absolute_time();
 			}
+		}
+
+		if (!armed.armed) {
+			/* Reset the flag if disarmed. */
+			_have_taken_off_since_arming = false;
 		}
 
 		_was_armed = armed.armed;
@@ -2358,11 +2368,6 @@ Commander::run()
 		}
 
 		_status_changed = false;
-
-		if (!armed.armed) {
-			/* Reset the flag if disarmed. */
-			_have_taken_off_since_arming = false;
-		}
 
 		arm_auth_update(now, params_updated || param_init_forced);
 
