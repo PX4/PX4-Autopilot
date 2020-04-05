ad559a66a10e25ec21243f2819c5b167cb5fb07c
Daniel Agar <daniel@agar.ca>
2020-04-05 10:20:19 -0400

    examples: add simple work_item example module

--- boards/px4/fmu-v4/stackcheck.cmake
+++ boards/px4/fmu-v4/stackcheck.cmake
@@ -122,4 +122,5 @@ px4_add_board(
 		#px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
 		#rover_steering_control # Rover example app
 		#uuv_example_app
+		#work_item
 	)
--- boards/px4/sitl/rtps.cmake
+++ boards/px4/sitl/rtps.cmake
@@ -84,6 +84,7 @@ px4_add_board(
 		px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
 		rover_steering_control # Rover example app
 		uuv_example_app
+		work_item
 	)
 
 set(config_sitl_viewer jmavsim CACHE STRING "viewer for sitl")
