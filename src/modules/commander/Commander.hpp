d47cdbeee9dd80ad8ff2b67ca530d6212b3443f9
Beat Küng <beat-kueng@gmx.net>
2020-04-07 13:12:13 +0200

    commander: set home position on takeoff
    instead of when arming and on takeoff.
    
    It is set on the first takeoff or if there was a disarm event between the
    last and the current takeoff.

--- src/modules/commander/Commander.hpp
+++ src/modules/commander/Commander.hpp
@@ -365,6 +365,7 @@ private:
 	bool		_was_armed{false};
 	bool		_failsafe_old{false};	///< check which state machines for changes, clear "changed" flag
 	bool		_have_taken_off_since_arming{false};
+	bool		_should_set_home_on_takeoff{true};
 	bool		_flight_termination_printed{false};
 
 	main_state_t	_main_state_pre_offboard{commander_state_s::MAIN_STATE_MANUAL};
