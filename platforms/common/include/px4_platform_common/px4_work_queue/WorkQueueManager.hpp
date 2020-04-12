83f6f8b317b6ad14a30241279aa199a8b106a717
Daniel Agar <daniel@agar.ca>
2020-04-11 21:16:10 -0400

    px4_fmu-v5_stackcheck: update stack sizes and add to Jenkins
     - increase stack sizes to run cleanly under stackcheck
         - this is likely overkill for most boards, but using stackcheck to set our minimum ensures we have a very safe margin on regular builds and it's something we can currently afford
     - remove holybro_durandal-v1_stackcheck from test rack (there's only one unit)

--- platforms/common/include/px4_platform_common/px4_work_queue/WorkQueueManager.hpp
+++ platforms/common/include/px4_platform_common/px4_work_queue/WorkQueueManager.hpp
@@ -48,21 +48,21 @@ struct wq_config_t {
 
 namespace wq_configurations
 {
-static constexpr wq_config_t rate_ctrl{"wq:rate_ctrl", 1600, 0}; // PX4 inner loop highest priority
+static constexpr wq_config_t rate_ctrl{"wq:rate_ctrl", 1664, 0}; // PX4 inner loop highest priority
 
-static constexpr wq_config_t SPI0{"wq:SPI0", 2368, -1};
-static constexpr wq_config_t SPI1{"wq:SPI1", 2368, -2};
-static constexpr wq_config_t SPI2{"wq:SPI2", 2368, -3};
-static constexpr wq_config_t SPI3{"wq:SPI3", 2368, -4};
-static constexpr wq_config_t SPI4{"wq:SPI4", 2368, -5};
-static constexpr wq_config_t SPI5{"wq:SPI5", 2368, -6};
-static constexpr wq_config_t SPI6{"wq:SPI6", 2368, -7};
+static constexpr wq_config_t SPI0{"wq:SPI0", 2496, -1};
+static constexpr wq_config_t SPI1{"wq:SPI1", 2496, -2};
+static constexpr wq_config_t SPI2{"wq:SPI2", 2496, -3};
+static constexpr wq_config_t SPI3{"wq:SPI3", 2496, -4};
+static constexpr wq_config_t SPI4{"wq:SPI4", 2496, -5};
+static constexpr wq_config_t SPI5{"wq:SPI5", 2496, -6};
+static constexpr wq_config_t SPI6{"wq:SPI6", 2496, -7};
 
-static constexpr wq_config_t I2C0{"wq:I2C0", 1400, -8};
-static constexpr wq_config_t I2C1{"wq:I2C1", 1400, -9};
-static constexpr wq_config_t I2C2{"wq:I2C2", 1400, -10};
-static constexpr wq_config_t I2C3{"wq:I2C3", 1400, -11};
-static constexpr wq_config_t I2C4{"wq:I2C4", 1400, -12};
+static constexpr wq_config_t I2C0{"wq:I2C0", 1472, -8};
+static constexpr wq_config_t I2C1{"wq:I2C1", 1472, -9};
+static constexpr wq_config_t I2C2{"wq:I2C2", 1472, -10};
+static constexpr wq_config_t I2C3{"wq:I2C3", 1472, -11};
+static constexpr wq_config_t I2C4{"wq:I2C4", 1472, -12};
 
 // PX4 att/pos controllers, highest priority after sensors.
 static constexpr wq_config_t att_pos_ctrl{"wq:att_pos_ctrl", 7200, -13};
