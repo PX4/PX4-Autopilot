6776a023f0b464e09aacaf63562e707a2890893b
Avionics Anonymous <avionicsanonymous@gmail.com>
2020-04-16 20:36:11 -0400

    Format improvements

--- src/drivers/uavcan/sensors/rangefinder.cpp
+++ src/drivers/uavcan/sensors/rangefinder.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2014, 2015 PX4 Development Team. All rights reserved.
+ *   Copyright (c) 2020 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -66,7 +66,7 @@ int UavcanRangefinderBridge::init()
 
 void
 UavcanRangefinderBridge::range_sub_cb(const
-		uavcan::ReceivedDataStructure<uavcan::equipment::range_sensor::Measurement> &msg)
+				      uavcan::ReceivedDataStructure<uavcan::equipment::range_sensor::Measurement> &msg)
 {
 	distance_sensor_s report{};
 
