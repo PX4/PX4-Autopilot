cb5203f1ca804d348591dada760b814ed82eaac2
Igor Mišić <igy1000mb@gmail.com>
2020-04-19 18:48:23 +0200

    Disable unused modules.

--- boards/spracing/h7extreme/default.cmake
+++ boards/spracing/h7extreme/default.cmake
@@ -9,17 +9,17 @@ px4_add_board(
 	ROMFSROOT px4fmu_common
 	#BUILD_BOOTLOADER
 	#IO px4_io-v2_default
-	TESTING
+#	TESTING
 #	UAVCAN_INTERFACES 2  - No H7 or FD can support in UAVCAN
 	SERIAL_PORTS
-		GPS1:/dev/ttyS0
-		TEL1:/dev/ttyS1
+#		GPS1:/dev/ttyS0
+		RC:/dev/ttyS1
 		TEL2:/dev/ttyS2
 		TEL4:/dev/ttyS3
 	DRIVERS
 		adc
 		barometer # all available barometer drivers
-		batt_smbus
+		#batt_smbus
 		camera_capture
 		camera_trigger
 		differential_pressure # all available differential pressure drivers
@@ -34,42 +34,43 @@ px4_add_board(
 		#imu/bmi088
 		imu/mpu6000
 		#imu/mpu9250
-		irlock
-		lights/blinkm
+		#irlock
+		#lights/blinkm
 		#lights/rgbled
 		#lights/rgbled_ncp5623c
-		magnetometer # all available magnetometer drivers
-		mkblctrl
+		#magnetometer # all available magnetometer drivers
+		#mkblctrl
 		#optical_flow # all available optical flow drivers
 		osd
-		pca9685
+		#pca9685
 		#power_monitor/ina226
 		#protocol_splitter
 #		pwm_input  - Need to create arch/stm32 arch/stm32h7 arch/kinetis and reloacate
 #					   all arch dependant code there
 		pwm_out_sim
 		px4fmu
-		roboclaw
-		tap_esc
+		#roboclaw
+		#tap_esc
+		rc_input
 		telemetry # all available telemetry drivers
-		test_ppm
+		#test_ppm
 		tone_alarm
 #		uavcan - No H7 or FD can support in UAVCAN yet
 	MODULES
-		airspeed_selector
+		#airspeed_selector
 		attitude_estimator_q
 		battery_status
-		camera_feedback
+		#camera_feedback
 		commander
 		dataman
-		ekf2
+		#ekf2
 		events
-		fw_att_control
-		fw_pos_control_l1
+		#fw_att_control
+		#fw_pos_control_l1
 		land_detector
-		landing_target_estimator
+		#landing_target_estimator
 		load_mon
-		local_position_estimator
+		#local_position_estimator
 		logger
 		mavlink
 		mc_att_control
@@ -77,12 +78,12 @@ px4_add_board(
 		mc_rate_control
 		navigator
 		rc_update
-		rover_pos_control
+		#rover_pos_control
 		sensors
-		sih
-		temperature_compensation
-		vmount
-		vtol_att_control
+		#sih
+		#temperature_compensation
+		#vmount
+		#vtol_att_control
 	SYSTEMCMDS
 		#bl_update
 		config
@@ -93,7 +94,7 @@ px4_add_board(
 		i2cdetect
 		led_control
 		mixer
-		motor_ramp
+		#motor_ramp
 		motor_test
 		#mtd
 		nshterm
@@ -113,12 +114,12 @@ px4_add_board(
 		work_queue
 	EXAMPLES
 		#bottle_drop # OBC challenge
-		fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
-		hello
-		hwtest # Hardware test
+		#fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
+		#hello
+		#hwtest # Hardware test
 		#matlab_csv_serial
-		px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
-		px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
-		rover_steering_control # Rover example app
-		uuv_example_app
+		#px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
+		#px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
+		#rover_steering_control # Rover example app
+		#uuv_example_app
 	)
