92ad432d2933067b9ca36b9eaf78e1861fb8ba01
梁财杰 <caijie@px4.cuav.net>
2020-04-20 14:08:19 +0800

    Add CAN rgb led

---
+++ src/drivers/uavcan/sensors/rgb.cpp
@@ -0,0 +1,126 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2014, 2015 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/**
+ * @file rgb.cpp
+ *
+ * @author CUAVcaijie <caijie@cuav.net>
+ */
+
+#include "rgb.hpp"
+#include <systemlib/err.h>
+
+
+UavcanUavcanRgb::UavcanUavcanRgb(uavcan::INode &node) :
+	_rgb_pub(node),
+	_timer(node)
+{
+
+}
+
+int
+UavcanUavcanRgb::init()
+{
+	/*
+	 * Setup timer and call back function for periodic updates
+	 */
+	if (!_timer.isRunning()) {
+		_timer.setCallback(TimerCbBinder(this, &UavcanUavcanRgb::periodic_update));
+		_timer.startPeriodic(uavcan::MonotonicDuration::fromMSec(1000 / MAX_RATE_HZ));
+	}
+
+	return 0;
+}
+
+void
+UavcanUavcanRgb::periodic_update(const uavcan::TimerEvent &)
+{
+	LedControlData led_control_data;
+
+	if (_led_controller.update(led_control_data) == 1) {
+
+		float _brightness;
+		uint8_t _r = 0, _g = 0, _b = 0;
+
+		switch (led_control_data.leds[0].color) {
+		case led_control_s::COLOR_RED:
+			_r = 255; _g = 0; _b = 0;
+			break;
+
+		case led_control_s::COLOR_GREEN:
+			_r = 0; _g = 255; _b = 0;
+			break;
+
+		case led_control_s::COLOR_BLUE:
+			_r = 0; _g = 0; _b = 255;
+			break;
+
+		case led_control_s::COLOR_AMBER: //make it the same as yellow
+		case led_control_s::COLOR_YELLOW:
+			_r = 255; _g = 255; _b = 0;
+			break;
+
+		case led_control_s::COLOR_PURPLE:
+			_r = 255; _g = 0; _b = 255;
+			break;
+
+		case led_control_s::COLOR_CYAN:
+			_r = 0; _g = 255; _b = 255;
+			break;
+
+		case led_control_s::COLOR_WHITE:
+			_r = 255; _g = 255; _b = 255;
+			break;
+
+		default:
+			_r = 0; _g = 0; _b = 0;
+			break;
+		}
+
+		_brightness = (float)led_control_data.leds[0].brightness / 255.f;
+		_r = _r * _brightness;
+		_g = _g * _brightness;
+		_b = _b * _brightness;
+
+		uavcan::equipment::indication::LightsCommand msg;
+		uavcan::equipment::indication::SingleLightCommand cmd;
+
+		cmd.light_id = 0;
+		cmd.color.red = _r >> 3;
+		cmd.color.green = _g >> 2;
+		cmd.color.blue = _b >> 3;
+		msg.commands.push_back(cmd);
+
+		(void)_rgb_pub.broadcast(msg);
+	}
+}
--- src/drivers/uavcan/uavcan_main.hpp
+++ src/drivers/uavcan/uavcan_main.hpp
@@ -52,6 +52,7 @@
 #include "actuators/esc.hpp"
 #include "actuators/hardpoint.hpp"
 #include "sensors/sensor_bridge.hpp"
+#include "sensors/rgb.hpp"
 
 #include <uavcan/helpers/heap_based_pool_allocator.hpp>
 #include <uavcan/protocol/global_time_sync_master.hpp>
@@ -200,6 +201,7 @@ private:
 	UavcanEscController		_esc_controller;
 	UavcanMixingInterface 		_mixing_interface{_node_mutex, _esc_controller};
 	UavcanHardpointController	_hardpoint_controller;
+	UavcanUavcanRgb			_rgb_controller;
 	uavcan::GlobalTimeSyncMaster	_time_sync_master;
 	uavcan::GlobalTimeSyncSlave	_time_sync_slave;
 	uavcan::NodeStatusMonitor	_node_status_monitor;
