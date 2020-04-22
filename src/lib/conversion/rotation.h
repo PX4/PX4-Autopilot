d41208735daba6c70bca84757730386172a0b301
Jin Chengde <jinchengde@gmail.com>
2020-04-22 15:03:55 +0800

    add new rotation (#14512)
    * add new rotation  ROLL_270_YAW_180
    
    fix compile error
    
    fix rotation
    
    modify roatation
    
    * modify enum to 41

--- src/lib/conversion/rotation.h
+++ src/lib/conversion/rotation.h
@@ -85,6 +85,7 @@ enum Rotation {
 	ROTATION_PITCH_45 = 33,
 	ROTATION_PITCH_315 = 34,
 	ROTATION_ROLL_90_YAW_270 = 35,
+	ROTATION_ROLL_270_YAW_180 = 41,
 	ROTATION_MAX
 };
 
@@ -131,6 +132,7 @@ const rot_lookup_t rot_lookup[] = {
 	{ 0,   45,   0 },
 	{ 0,  315,   0 },
 	{ 90,   0, 270 },
+	{270,   0, 180 },
 };
 
 /**
