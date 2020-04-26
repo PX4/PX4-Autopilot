8b6d665a1331f94091caf2f262e09d508eb8975c
kamilritz <kritz@ethz.ch>
2020-04-26 17:54:38 +0200

    Avoid subtraction of two uint

--- EKF/RingBuffer.h
+++ EKF/RingBuffer.h
@@ -127,7 +127,7 @@ public:
 			int index = (_head - i);
 			index = index < 0 ? _size + index : index;
 
-			if (timestamp >= _buffer[index].time_us && timestamp - _buffer[index].time_us < (uint64_t)1e5) {
+			if (timestamp >= _buffer[index].time_us && timestamp < _buffer[index].time_us + (uint64_t)1e5) {
 				*sample = _buffer[index];
 
 				// Now we can set the tail to the item which
