a03568a8165c1a15b9178bf56f041e6c4268045f
Jakub Kákona <kaklik@mlab.cz>
2020-04-29 08:46:59 +0200

    FlightGear simulator support (#14539)
    - Add flightgear_bridge submodule.
    - Add traget px4_sitl_nolockstep with disabled  LOCKSTEP simulation.
    - Add flightgear viewer targets and startup scripts
    - Add a few possible vehicles plane (rascal), autogyro (tf-g1), and rover (tf-r1))
    
    Co-authored-by: Vit Hanousek <vithanousek@seznam.cz>

---
+++ boards/px4/sitl/nolockstep.cmake
@@ -0,0 +1,91 @@
+
+px4_add_board(
+	PLATFORM posix
+	VENDOR px4
+	MODEL sitl
+	LABEL nolockstep
+	TESTING
+	DRIVERS
+		#barometer # all available barometer drivers
+		#batt_smbus
+		camera_capture
+		camera_trigger
+		#differential_pressure # all available differential pressure drivers
+		#distance_sensor # all available distance sensor drivers
+		gps
+		#imu # all available imu drivers
+		#magnetometer # all available magnetometer drivers
+		pwm_out_sim
+		#telemetry # all available telemetry drivers
+		tone_alarm
+		#uavcan
+	MODULES
+		airspeed_selector
+		attitude_estimator_q
+		camera_feedback
+		commander
+		dataman
+		ekf2
+		events
+		fw_att_control
+		fw_pos_control_l1
+		land_detector
+		landing_target_estimator
+		#load_mon
+		local_position_estimator
+		logger
+		mavlink
+		mc_att_control
+		mc_hover_thrust_estimator
+		mc_pos_control
+		mc_rate_control
+		navigator
+		rc_update
+		replay
+		rover_pos_control
+		sensors
+		#sih
+		simulator
+		temperature_compensation
+		vmount
+		vtol_att_control
+		uuv_att_control
+
+	SYSTEMCMDS
+		#config
+		#dumpfile
+		dyn
+		esc_calib
+		led_control
+		mixer
+		motor_ramp
+		motor_test
+		#mtd
+		#nshterm
+		param
+		perf
+		pwm
+		reboot
+		sd_bench
+		shutdown
+		tests # tests and test runner
+		#top
+		topic_listener
+		tune_control
+		ver
+		work_queue
+	EXAMPLES
+		dyn_hello # dynamically loading modules example
+		fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
+		hello
+		#hwtest # Hardware test
+		#matlab_csv_serial
+		px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
+		px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
+		rover_steering_control # Rover example app
+		uuv_example_app
+	)
+
+message(STATUS "Building without lockstep")
+set(ENABLE_LOCKSTEP_SCHEDULER no)
+
