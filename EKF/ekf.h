2c16133dacae0528bb0bd1a18e0e4fab7c46d794
Paul Riseborough <priseborough@users.noreply.github.com>
2020-04-30 17:50:26 +1000

    EKF: Prevent yaw reset to GPS caused by loss of GPS data (#805)

--- EKF/ekf.h
+++ EKF/ekf.h
@@ -335,6 +335,7 @@ private:
 	bool _tas_data_ready{false};	///< true when new true airspeed data has fallen behind the fusion time horizon and is available to be fused
 	bool _flow_for_terrain_data_ready{false}; /// same flag as "_flow_data_ready" but used for separate terrain estimator
 
+	uint64_t _time_prev_gps_us{0};	///< time stamp of previous GPS data retrieved from the buffer (uSec)
 	uint64_t _time_last_aiding{0};	///< amount of time we have been doing inertial only deadreckoning (uSec)
 	bool _using_synthetic_position{false};	///< true if we are using a synthetic position to constrain drift
 
