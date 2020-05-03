88001e4c74ed9de01140e2cd33e982bc6a3aa5ad
David Jablonski <dayjaby@gmail.com>
2020-05-03 11:55:20 +0200

    implemented MAV_CMD_REQUEST_MESSAGE for STORAGE_INFORMATION

--- src/modules/mavlink/mavlink_stream.h
+++ src/modules/mavlink/mavlink_stream.h
@@ -96,8 +96,12 @@ public:
 	 * This function is called in response to a MAV_CMD_REQUEST_MESSAGE command. The default implementation is to
 	 * just reset the counter to immediately send one message.
 	 */
-	virtual void request_message(float param1 = 0.0, float param2 = 0.0, float param3 = 0.0, float param4 = 0.0,
-				     float param5 = 0.0, float param6 = 0.0, float param7 = 0.0) { reset_last_sent(); }
+	virtual bool request_message(float param1 = 0.0, float param2 = 0.0, float param3 = 0.0, float param4 = 0.0,
+				     float param5 = 0.0, float param6 = 0.0, float param7 = 0.0)
+	{
+		reset_last_sent();
+		return true;
+	}
 
 	/**
 	 * Get the average message size
