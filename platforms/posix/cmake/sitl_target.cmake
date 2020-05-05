d9fcc18812168eff282918c026194ccfa60e81d6
JaeyoungLim <jaeyoung@auterion.com>
2020-05-05 11:07:02 +0200

    Add aion robotics r1 rover sitl target (#14652)
    This adds a sitl target and aiframe configs for the r1_rover, which is a differential rover example for SITL rover. The model is based on the aion robotics r1 rover

--- platforms/posix/cmake/sitl_target.cmake
+++ platforms/posix/cmake/sitl_target.cmake
@@ -79,7 +79,7 @@ set(models none shell
 	if750a iris iris_opt_flow iris_opt_flow_mockup iris_vision iris_rplidar iris_irlock iris_obs_avoid iris_rtps px4vision solo typhoon_h480
 	plane plane_cam plane_catapult plane_lidar
 	standard_vtol tailsitter tiltrotor
-	rover boat
+	rover r1_rover boat
 	uuv_hippocampus)
 set(worlds none empty baylands ksql_airport mcmillan_airfield sonoma_raceway warehouse)
 set(all_posix_vmd_make_targets)
