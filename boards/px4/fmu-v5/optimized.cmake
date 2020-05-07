8ae60c0d541fdfd97b859beaf962ec7fe8450107
Daniel Agar <daniel@agar.ca>
2020-05-07 14:41:54 -0400

    boards: px4_fmu-v5_optimized don't build all mags

--- boards/px4/fmu-v5/optimized.cmake
+++ boards/px4/fmu-v5/optimized.cmake
@@ -39,7 +39,8 @@ px4_add_board(
 		lights/rgbled
 		lights/rgbled_ncp5623c
 		#lights/rgbled_pwm
-		magnetometer # all available magnetometer drivers
+		#magnetometer # all available magnetometer drivers
+		magnetometer/ist8310
 		optical_flow # all available optical flow drivers
 		#pwm_input
 		pwm_out_sim
