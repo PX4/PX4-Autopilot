37d9cef262deb2a5bea9ddc6421a79f2ccceaf38
Mathieu Bresciani <brescianimathieu@gmail.com>
2020-05-19 10:02:40 +0200

    ekf: disable xy accel bias learning before takeoff (#818)
    * ekf: disable xy accel bias learning before takeoff
    
    As those biases are usually poorly observable before takeoff because
    they are almost perpendicular to the gravity vector, learning is often
    driven by noise and numerical issues. This results in incorrect bias
    learning before takeoff when the drone is static on ground for a long
    period of time.
    
    * ekf: update unit test and change indicator

--- EKF/covariance.cpp
+++ EKF/covariance.cpp
@@ -163,9 +163,8 @@ void Ekf::predictCovariance()
 	for (unsigned stateIndex = 13; stateIndex <= 15; stateIndex++) {
 		const unsigned index = stateIndex - 13;
 
-		// TODO: When on ground, only consider an accel bias observable if aligned with the gravity vector
-		// const bool is_bias_observable = (fabsf(_R_to_earth(2, index)) > 0.8f) || _control_status.flags.in_air;
-		const bool is_bias_observable = true;
+		// When on ground, only consider an accel bias observable if aligned with the gravity vector
+		const bool is_bias_observable = (fabsf(_R_to_earth(2, index)) > 0.8f) || _control_status.flags.in_air;
 		const bool do_inhibit_axis = do_inhibit_all_axes || !is_bias_observable;
 
 		if (do_inhibit_axis) {
