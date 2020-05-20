4c870a3e4bd59bb171f869b3e610c4957317ad7c
kritz <kritz@ethz.ch>
2020-05-20 08:24:23 +0200

    EKF: fix initialization of local position validity 2 (#820)
    The `_deadreckon_time_exceeded` flag is used in
    `local_position_is_valid()`. This means that
    `_params.valid_timeout_max` after startup, in my observed case 5
    seconds, the local position switche from valid to invalid and then after
    a while back to valid again.
    
    With this fix, the local position is flagged invalid from boot and gets validated after the first aiding event.
    
    Co-authored-by: Julian Oes <julian@oes.ch>

--- EKF/ekf_helper.cpp
+++ EKF/ekf_helper.cpp
@@ -1215,8 +1215,8 @@ void Ekf::update_deadreckoning_status()
 		_time_last_aiding = _time_last_imu - _params.no_aid_timeout_max;
 	}
 
-	// report if we have been deadreckoning for too long
-	_deadreckon_time_exceeded = isTimedOut(_time_last_aiding, (uint64_t)_params.valid_timeout_max);
+	// report if we have been deadreckoning for too long, initial state is deadreckoning until aiding is present
+	_deadreckon_time_exceeded = (_time_last_aiding == 0) || isTimedOut(_time_last_aiding, (uint64_t)_params.valid_timeout_max);
 }
 
 // calculate the inverse rotation matrix from a quaternion rotation
