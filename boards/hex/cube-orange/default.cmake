5c8112da4c2231a47afbce74f17cb2dfb7a14696
Jacob Dahl <dahl.jakejacob@gmail.com>
2020-05-20 17:10:26 -0600

    added back DRV_MAG_DEVTYPE_LIS2MDL and added icm20948 to cube orange cmake

--- boards/hex/cube-orange/default.cmake
+++ boards/hex/cube-orange/default.cmake
@@ -31,9 +31,9 @@ px4_add_board(
 		imu/adis16477
 		imu/adis16497
 		imu/bmi088
-		imu/icm20948 # TODO: driver is busted right now?
 		imu/invensense/icm20602
 		imu/invensense/icm20649
+		imu/invensense/icm20948
 		irlock
 		lights/blinkm
 		lights/rgbled
