ab9d5498bccf1abffc714154244a4d0ffb113345
Silvan Fuhrer <silvan@auterion.com>
2020-05-26 16:40:42 +0200

    mavlink_mission: MAV_CMD_NAV_LOITER_TIME: set force_heading corresponding to param2
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- src/modules/mavlink/mavlink_mission.cpp
+++ src/modules/mavlink/mavlink_mission.cpp
@@ -1329,6 +1329,7 @@ MavlinkMissionManager::parse_mavlink_mission_item(const mavlink_mission_item_t *
 		case MAV_CMD_NAV_LOITER_TIME:
 			mission_item->nav_cmd = NAV_CMD_LOITER_TIME_LIMIT;
 			mission_item->time_inside = mavlink_mission_item->param1;
+			mission_item->force_heading = (mavlink_mission_item->param2 > 0);
 			mission_item->loiter_radius = mavlink_mission_item->param3;
 			mission_item->loiter_exit_xtrack = (mavlink_mission_item->param4 > 0);
 			// Yaw is only valid for multicopter but we set it always because
