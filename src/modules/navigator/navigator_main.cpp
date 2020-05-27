e52cc88484ec6fd488f6499dca201d7bcf10f84f
Pandacoolcool <1308455330@qq.com>
2020-05-27 20:49:04 +0800

    navigator_main.cpp: don't reset wv enable status (#14901)
    * navigator_main.cpp: don't reset wv enable status
    
    * fix review

--- src/modules/navigator/navigator_main.cpp
+++ src/modules/navigator/navigator_main.cpp
@@ -891,7 +891,7 @@ Navigator::reset_position_setpoint(position_setpoint_s &sp)
 	sp.cruising_throttle = get_cruising_throttle();
 	sp.valid = false;
 	sp.type = position_setpoint_s::SETPOINT_TYPE_IDLE;
-	sp.disable_weather_vane = true;
+	sp.disable_weather_vane = false;
 }
 
 float
