e9411017fd4e4685d4160c513b908de3b77a5f96
Julian Oes <julian@oes.ch>
2020-05-28 14:12:04 +0200

    mavlink: use send() instead of using reset flag
    I think we can just directly use send() and don't need to use the
    workaround of resetting the send flag to trigger one send.
    
    Also, we don't need to override it if we don't need to check any params.

--- src/modules/mavlink/streams/flight_information.h
+++ src/modules/mavlink/streams/flight_information.h
@@ -37,12 +37,6 @@ public:
 	{
 		return MAVLINK_MSG_ID_FLIGHT_INFORMATION_LEN + MAVLINK_NUM_NON_PAYLOAD_BYTES;
 	}
-
-	bool request_message(float param2, float param3, float param4,
-			     float param5, float param6, float param7) override
-	{
-		return send(hrt_absolute_time());
-	}
 private:
 	uORB::Subscription _armed_sub{ORB_ID(actuator_armed)};
 
