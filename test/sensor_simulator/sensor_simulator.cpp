6126c190b221fd6890bb9ea3214b63a83f2d23f4
bresch <brescianimathieu@gmail.com>
2020-06-03 11:09:18 +0200

    sensor_simulator: update EKF at IMU rate
    This is how it is also done in ekf2_main. Otherwise, this leads to
    multiple integration of the same IMU data due to asynchronous sensor
    updates triggering a prediction step between IMU updates.
    
    Fix unit tests that broke because of this fix

--- test/sensor_simulator/sensor_simulator.cpp
+++ test/sensor_simulator/sensor_simulator.cpp
@@ -146,9 +146,13 @@ void SensorSimulator::runMicroseconds(uint32_t duration)
 
 	for(;_time < start_time + (uint64_t)duration; _time+=1000)
 	{
+		bool update_imu = _imu.should_send(_time);
 		updateSensors();
 
-		_ekf->update();
+		if (update_imu) {
+			// Update at IMU rate
+			_ekf->update();
+		}
 	}
 }
 
