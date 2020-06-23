55d06241b35505f371aa1d41a4332cdf1edc9cb5
Beat Küng <beat-kueng@gmx.net>
2020-06-23 08:51:32 +0200

    fix unit tests: do not stub pthread_cond_wait
    and fix the tests instead.
    Some unit tests depend on pthread_cond_wait to work as expected.

--- platforms/posix/src/px4/common/test_stubs/stub_parameter.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <pthread.h>
-
-#include <functional>
-
-std::function<int(pthread_cond_t *, pthread_mutex_t *)> stub_pthread_cond_wait_callback =
-[](pthread_cond_t *, pthread_mutex_t *) {return 0;};
