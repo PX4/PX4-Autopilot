d085309fd393b0742c5e47590425ee860d783bf1
Daniel Agar <daniel@agar.ca>
2020-07-06 10:28:00 -0400

    load_mon: decrease warning threshold for stack check builds
     - enabling stack check increases stack usage and will assert if there's
    any overflow

--- src/modules/load_mon/load_mon.cpp
+++ src/modules/load_mon/load_mon.cpp
@@ -55,7 +55,13 @@
 #  error load_mon support requires CONFIG_SCHED_INSTRUMENTATION
 #endif
 
-#define STACK_LOW_WARNING_THRESHOLD 300 ///< if free stack space falls below this, print a warning
+// if free stack space falls below this, print a warning
+#if defined(CONFIG_ARMV7M_STACKCHECK)
+static constexpr unsigned STACK_LOW_WARNING_THRESHOLD = 100;
+#else
+static constexpr unsigned STACK_LOW_WARNING_THRESHOLD = 300;
+#endif
+
 #define FDS_LOW_WARNING_THRESHOLD 3 ///< if free file descriptors fall below this, print a warning
 
 namespace load_mon
