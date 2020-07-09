fa0ca7e65cf62e42f9e8730d23bf8bfc4258120f
JaeyoungLim <gokoreas@gmail.com>
2020-07-09 17:23:22 +0200

    Update mavlink with cellular status message fixes
    This Commit includes a update of the mavlink submodule
    
    since the CELLULAR_STATUS message was updated, the necessary changes were done together
    Add enums

--- src/modules/mavlink/mavlink_receiver.cpp
+++ src/modules/mavlink/mavlink_receiver.cpp
@@ -2317,12 +2317,12 @@ MavlinkReceiver::handle_message_cellular_status(mavlink_message_t *msg)
 
 	cellular_status.timestamp = hrt_absolute_time();
 	cellular_status.status = status.status;
+	cellular_status.failure_reason = status.failure_reason;
 	cellular_status.type = status.type;
 	cellular_status.quality = status.quality;
 	cellular_status.mcc = status.mcc;
 	cellular_status.mnc = status.mnc;
 	cellular_status.lac = status.lac;
-	cellular_status.cid = status.cid;
 
 	_cellular_status_pub.publish(cellular_status);
 }
