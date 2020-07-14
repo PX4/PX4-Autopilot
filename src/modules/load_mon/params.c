ad0bcda4b69cc133ad628bb3ce577ea7df98cccf
Daniel Agar <daniel@agar.ca>
2020-07-14 17:38:32 -0400

    load_mon updates
     - increase rate
     - cpu load calculation grab timestamp atomically
     - only check one task per cycle (but cycle at a higher rate)
     - decrease available FD threshold
     - minor cleanup

--- src/modules/load_mon/params.c
+++ src/modules/load_mon/params.c
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2013-2017 PX4 Development Team. All rights reserved.
+ *   Copyright (c) 2013-2020 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
