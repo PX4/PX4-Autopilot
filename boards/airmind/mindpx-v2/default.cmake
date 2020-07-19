a9920fd61f01d9d52b0c069b518b17ba4d6be050
rolandash <ning.roland@gmail.com>
2020-07-19 12:17:43 +0800

    remove useless line.

--- boards/airmind/mindpx-v2/default.cmake
+++ boards/airmind/mindpx-v2/default.cmake
@@ -36,7 +36,6 @@ px4_add_board(
 		magnetometer # all available magnetometer drivers
 		mkblctrl
 		optical_flow # all available optical flow drivers
-		#optical_flow/px4flow
 		#osd
 		pca9685
 		power_monitor/ina226
