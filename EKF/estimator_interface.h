7609bc69b2e2de6278d57ce41180796c9c6fc879
kamilritz <kritz@ethz.ch>
2020-07-27 21:18:04 +0200

    drag innov member variables to Vector2f

--- EKF/estimator_interface.h
+++ EKF/estimator_interface.h
@@ -480,18 +480,18 @@ protected:
 	// innovation consistency check monitoring ratios
 	float _yaw_test_ratio{};		// yaw innovation consistency check ratio
 	Vector3f _mag_test_ratio;		// magnetometer XYZ innovation consistency check ratios
-	Vector2f _gps_vel_test_ratio;	// GPS velocity innovation consistency check ratios
-	Vector2f _gps_pos_test_ratio;	// GPS position innovation consistency check ratios
+	Vector2f _gps_vel_test_ratio;		// GPS velocity innovation consistency check ratios
+	Vector2f _gps_pos_test_ratio;		// GPS position innovation consistency check ratios
 	Vector2f _ev_vel_test_ratio;		// EV velocity innovation consistency check ratios
 	Vector2f _ev_pos_test_ratio ;		// EV position innovation consistency check ratios
 	Vector2f _aux_vel_test_ratio;		// Auxiliray horizontal velocity innovation consistency check ratio
-	Vector2f _baro_hgt_test_ratio;	// baro height innovation consistency check ratios
-	Vector2f _rng_hgt_test_ratio;	// range finder height innovation consistency check ratios
+	Vector2f _baro_hgt_test_ratio;		// baro height innovation consistency check ratios
+	Vector2f _rng_hgt_test_ratio;		// range finder height innovation consistency check ratios
 	float _optflow_test_ratio{};		// Optical flow innovation consistency check ratio
 	float _tas_test_ratio{};		// tas innovation consistency check ratio
 	float _hagl_test_ratio{};		// height above terrain measurement innovation consistency check ratio
 	float _beta_test_ratio{};		// sideslip innovation consistency check ratio
-	float _drag_test_ratio[2] {};	// drag innovation consistency check ratio
+	Vector2f _drag_test_ratio;		// drag innovation consistency check ratio
 	innovation_fault_status_u _innov_check_fail_status{};
 
 	bool _is_dead_reckoning{false};		// true if we are no longer fusing measurements that constrain horizontal velocity drift
