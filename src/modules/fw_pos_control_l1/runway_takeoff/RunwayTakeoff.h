560c22e612fcd78cfc47bb7fedb94e6d96ffbe15
Daniel Agar <daniel@agar.ca>
2020-08-07 21:26:56 -0400

    fw_pos_control_l1: pass time through from run

--- src/modules/fw_pos_control_l1/runway_takeoff/RunwayTakeoff.h
+++ src/modules/fw_pos_control_l1/runway_takeoff/RunwayTakeoff.h
@@ -68,8 +68,9 @@ public:
 	RunwayTakeoff(ModuleParams *parent);
 	~RunwayTakeoff() = default;
 
-	void init(float yaw, double current_lat, double current_lon);
-	void update(float airspeed, float alt_agl, double current_lat, double current_lon, orb_advert_t *mavlink_log_pub);
+	void init(const hrt_abstime &now, float yaw, double current_lat, double current_lon);
+	void update(const hrt_abstime &now, float airspeed, float alt_agl, double current_lat, double current_lon,
+		    orb_advert_t *mavlink_log_pub);
 
 	RunwayTakeoffState getState() { return _state; }
 	bool isInitialized() { return _initialized; }
@@ -83,7 +84,7 @@ public:
 	float getPitch(float tecsPitch);
 	float getRoll(float navigatorRoll);
 	float getYaw(float navigatorYaw);
-	float getThrottle(float tecsThrottle);
+	float getThrottle(const hrt_abstime &now, float tecsThrottle);
 	bool resetIntegrators();
 	float getMinPitch(float sp_min, float climbout_min, float min);
 	float getMaxPitch(float max);
@@ -93,11 +94,11 @@ public:
 
 private:
 	/** state variables **/
-	RunwayTakeoffState _state;
-	bool _initialized;
-	hrt_abstime _initialized_time;
-	float _init_yaw;
-	bool _climbout;
+	RunwayTakeoffState _state{THROTTLE_RAMP};
+	bool _initialized{false};
+	hrt_abstime _initialized_time{0};
+	float _init_yaw{0.f};
+	bool _climbout{false};
 	matrix::Vector2f _start_wp;
 
 	DEFINE_PARAMETERS(
