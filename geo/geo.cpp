b31be423a5e24a9f56173c02e31f7fd6a56bf9bc
kamilritz <kritz@ethz.ch>
2020-08-10 18:40:32 +0200

    Geo: Fix double correcting for negative distance

--- geo/geo.cpp
+++ geo/geo.cpp
@@ -289,13 +289,8 @@ void create_waypoint_from_line_and_dist(double lat_A, double lon_A, double lat_B
 		*lat_target = lat_A;
 		*lon_target = lon_A;
 
-	} else if (dist >= FLT_EPSILON) {
-		float heading = get_bearing_to_next_waypoint(lat_A, lon_A, lat_B, lon_B);
-		waypoint_from_heading_and_distance(lat_A, lon_A, heading, dist, lat_target, lon_target);
-
 	} else {
 		float heading = get_bearing_to_next_waypoint(lat_A, lon_A, lat_B, lon_B);
-		heading = wrap_2pi(heading + M_PI_F);
 		waypoint_from_heading_and_distance(lat_A, lon_A, heading, dist, lat_target, lon_target);
 	}
 }
