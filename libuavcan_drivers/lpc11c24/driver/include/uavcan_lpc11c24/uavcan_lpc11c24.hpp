04377cc2704eb7e2fae36fffa5e67063cc40b4c0
Daniel Agar <daniel@agar.ca>
2020-08-12 14:32:34 -0400

    PX4 branch remove redundant confusing libuavcan_drivers

--- libuavcan_drivers/lpc11c24/driver/include/uavcan_lpc11c24/uavcan_lpc11c24.hpp
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (C) 2014 Pavel Kirienko <pavel.kirienko@gmail.com>
- */
-
-#pragma once
-
-#include <uavcan/uavcan.hpp>
-#include <uavcan_lpc11c24/can.hpp>
-#include <uavcan_lpc11c24/clock.hpp>
