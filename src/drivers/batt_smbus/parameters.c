ef2eec4b538b356a9825a5f1fc3d294010cd2e0b
bazooka joe <bazookajoe1900@gmail.com>
2020-08-14 13:41:59 -0700

    SMbus battery driver - a lot of updates and optimizations
    - added support for BQ40Z80 based battery
    - added performance counter for interface errors
    - added SMART_BATTERY_INFO mavlink message
    - general code cleanups and optimization
    - fixed: void flooding the log in case of interface error
    - fixed: using _batt_startup_capacity instead of _batt_capacity for discharged_mah
    - update: read manufacture_date
    - update: get _cell_count from parameter and not const 4
    - update: avoid re-reading data that has already been read and stored on class already
    - currently the battery type defined by BAT_SMBUS_MODEL parameter and not by auto detection

--- src/drivers/batt_smbus/parameters.c
+++ src/drivers/batt_smbus/parameters.c
@@ -32,7 +32,7 @@
  ****************************************************************************/
 
 /**
- * SMBUS Smart battery driver (BQ40Z50)
+ * SMBUS Smart battery driver BQ40Z50 and BQ40Z80
  *
  * @reboot_required true
  *
@@ -49,3 +49,16 @@ PARAM_DEFINE_INT32(SENS_EN_BATT, 0);
  * @group Sensors
  */
 PARAM_DEFINE_FLOAT(BAT_C_MULT, 1.0f);
+
+/**
+ * Battery device model
+ *
+ * @reboot_required true
+ * @min 0
+ * @max 2
+ * @group Sensors
+ * @value 0 AutoDetect
+ * @value 1 BQ40Z50 based
+ * @value 2 BQ40Z80 based
+ */
+PARAM_DEFINE_INT32(BAT_SMBUS_MODEL, 0);
