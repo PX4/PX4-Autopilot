e53a2c0992fba8d10b60667eb82f56d553f32502
SalimTerryLi <lhf2613@gmail.com>
2020-08-14 20:43:19 +0800

    ADS1115 included and V_DIV settings

--- boards/px4/airpi2/src/board_config.h
+++ boards/px4/airpi2/src/board_config.h
@@ -51,6 +51,7 @@
 #define ADC_BATTERY_CURRENT_CHANNEL	-1
 #define ADC_AIRSPEED_VOLTAGE_CHANNEL 2
 
+#define BOARD_BATTERY1_V_DIV 5.7f	// 1K + 4.7K
 #define ADC_DP_V_DIV 1.0f
 
 #include <system_config.h>
