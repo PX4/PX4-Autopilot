eb1c470e7ac5f66178734c2fd0d66dbe16261ceb
SalimTerryLi <lhf2613@gmail.com>
2020-08-26 20:08:37 +0800

    another IMU solution

--- boards/scumaker/airpi2/src/spi.cpp
+++ boards/scumaker/airpi2/src/spi.cpp
@@ -37,5 +37,6 @@
 constexpr px4_spi_bus_t px4_spi_buses[SPI_BUS_MAX_BUS_ITEMS] = {
 	initSPIBus(0, {
 		initSPIDevice(DRV_IMU_DEVTYPE_ICM42688P, 0),
+		initSPIDevice(DRV_IMU_DEVTYPE_ICM42605, 0),
 	}),
 };
