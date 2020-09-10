ec5f11fa53831f44aae8611bdeac120a24b396e3
Beat Küng <beat-kueng@gmx.net>
2020-09-10 13:33:29 +0200

    posix: enable ROMFSROOT
    This will copy the ROMFS into the build directory under etc/, and thus
    needs a change of the PX4 startup parameters and mixer paths.

--- Tools/sitl_run.sh
+++ Tools/sitl_run.sh
@@ -156,7 +156,7 @@ set +e
 if [[ ${model} == test_* ]] || [[ ${model} == *_generated ]]; then
 	sitl_command="\"$sitl_bin\" $no_pxh \"$src_path\"/ROMFS/px4fmu_test -s \"${src_path}\"/posix-configs/SITL/init/test/${model} -t \"$src_path\"/test_data"
 else
-	sitl_command="\"$sitl_bin\" $no_pxh \"$src_path\"/ROMFS/px4fmu_common -s etc/init.d-posix/rcS -t \"$src_path\"/test_data"
+	sitl_command="\"$sitl_bin\" $no_pxh \"$build_path\"/etc -s etc/init.d-posix/rcS -t \"$src_path\"/test_data"
 fi
 
 echo SITL COMMAND: $sitl_command
