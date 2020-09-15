5e4e4a4573b47fead6252b44d01333b37a907d6b
Daniel Agar <daniel@agar.ca>
2020-09-15 16:49:13 -0400

    boards: aerotenna ocpoc fix package (new ROMFSROOT was missing)

--- boards/aerotenna/ocpoc/default.cmake
+++ boards/aerotenna/ocpoc/default.cmake
@@ -8,6 +8,7 @@ px4_add_board(
 	LABEL default
 	PLATFORM posix
 	ARCHITECTURE cortex-a9
+	ROMFSROOT px4fmu_common
 	TOOLCHAIN arm-linux-gnueabihf
 	TESTING
 	SERIAL_PORTS
@@ -61,9 +62,9 @@ px4_add_board(
 		rc_update
 		rover_pos_control
 		sensors
-		temperature_compensation
 		sih
 		#simulator
+		temperature_compensation
 		vmount
 		vtol_att_control
 	SYSTEMCMDS
