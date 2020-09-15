ingridariel-nascimento/Firmware

sensors: add SENS_MAG_MODE to optionally publish all magnetometers

src/modules/sensors/vehicle_magnetometer/VehicleMagnetometer.hpp
@@ -46,6 +46,7 @@
 #include <px4_platform_common/px4_config.h>
 #include <px4_platform_common/px4_work_queue/ScheduledWorkItem.hpp>
 #include <uORB/Publication.hpp>
+#include <uORB/PublicationMulti.hpp>
 #include <uORB/Subscription.hpp>
 #include <uORB/SubscriptionCallback.hpp>
 #include <uORB/topics/actuator_controls.h>
@@ -75,6 +76,8 @@ private:
 
 	void ParametersUpdate(bool force = false);
 
+	void Publish(uint8_t instance, bool multi = false);
+
 	/**
 	 * Calculates the magnitude in Gauss of the largest difference between the primary and any other magnetometers
 	 */
@@ -83,7 +86,14 @@ private:
 	static constexpr int MAX_SENSOR_COUNT = 4;
 
 	uORB::Publication<sensor_preflight_mag_s> _sensor_preflight_mag_pub{ORB_ID(sensor_preflight_mag)};
+
 	uORB::Publication<vehicle_magnetometer_s> _vehicle_magnetometer_pub{ORB_ID(vehicle_magnetometer)};
+	uORB::PublicationMulti<vehicle_magnetometer_s> _vehicle_magnetometer_multi_pub[MAX_SENSOR_COUNT] {
+		{ORB_ID(vehicle_magnetometer)},
+		{ORB_ID(vehicle_magnetometer)},
+		{ORB_ID(vehicle_magnetometer)},
+		{ORB_ID(vehicle_magnetometer)},
+	};
 
 	uORB::Subscription _actuator_controls_0_sub{ORB_ID(actuator_controls_0)};
 	uORB::Subscription _battery_status_sub{ORB_ID(battery_status), 0};
@@ -110,16 +120,16 @@ private:
 
 	perf_counter_t _cycle_perf{perf_alloc(PC_ELAPSED, MODULE_NAME": cycle")};
 
-	hrt_abstime _last_publication_timestamp{0};
 	hrt_abstime _last_error_message{0};
 	orb_advert_t _mavlink_log_pub{nullptr};
 
 	DataValidatorGroup _voter{1};
 	unsigned _last_failover_count{0};
 
-	uint64_t _mag_timestamp_sum{0};
-	matrix::Vector3f _mag_sum{};
-	int _mag_sum_count{0};
+	uint64_t _timestamp_sample_sum[MAX_SENSOR_COUNT] {0};
+	matrix::Vector3f _mag_sum[MAX_SENSOR_COUNT] {};
+	int _mag_sum_count[MAX_SENSOR_COUNT] {};
+	hrt_abstime _last_publication_timestamp[MAX_SENSOR_COUNT] {};
 
 	sensor_mag_s _last_data[MAX_SENSOR_COUNT] {};
 	bool _advertised[MAX_SENSOR_COUNT] {};
@@ -134,6 +144,7 @@ private:
 
 	DEFINE_PARAMETERS(
 		(ParamInt<px4::params::CAL_MAG_COMP_TYP>) _param_mag_comp_typ,
+		(ParamInt<px4::params::SENS_MAG_MODE>) _param_sens_mag_mode,
 		(ParamFloat<px4::params::SENS_MAG_RATE>) _param_sens_mag_rate
 	)
 };

PX4/PX4-Autopilot

src/modules/sensors/vehicle_magnetometer/VehicleMagnetometer.hpp
@@ -46,6 +46,7 @@
 #include <px4_platform_common/px4_config.h>
 #include <px4_platform_common/px4_work_queue/ScheduledWorkItem.hpp>
 #include <uORB/Publication.hpp>
+#include <uORB/PublicationMulti.hpp>
 #include <uORB/Subscription.hpp>
 #include <uORB/SubscriptionCallback.hpp>
 #include <uORB/topics/actuator_controls.h>
@@ -75,6 +76,8 @@ private:
 
 	void ParametersUpdate(bool force = false);
 
+	void Publish(uint8_t instance, bool multi = false);
+
 	/**
 	 * Calculates the magnitude in Gauss of the largest difference between the primary and any other magnetometers
 	 */
@@ -83,7 +86,14 @@ private:
 	static constexpr int MAX_SENSOR_COUNT = 4;
 
 	uORB::Publication<sensor_preflight_mag_s> _sensor_preflight_mag_pub{ORB_ID(sensor_preflight_mag)};
+
 	uORB::Publication<vehicle_magnetometer_s> _vehicle_magnetometer_pub{ORB_ID(vehicle_magnetometer)};
+	uORB::PublicationMulti<vehicle_magnetometer_s> _vehicle_magnetometer_multi_pub[MAX_SENSOR_COUNT] {
+		{ORB_ID(vehicle_magnetometer)},
+		{ORB_ID(vehicle_magnetometer)},
+		{ORB_ID(vehicle_magnetometer)},
+		{ORB_ID(vehicle_magnetometer)},
+	};
 
 	uORB::Subscription _actuator_controls_0_sub{ORB_ID(actuator_controls_0)};
 	uORB::Subscription _battery_status_sub{ORB_ID(battery_status), 0};
@@ -110,16 +120,16 @@ private:
 
 	perf_counter_t _cycle_perf{perf_alloc(PC_ELAPSED, MODULE_NAME": cycle")};
 
-	hrt_abstime _last_publication_timestamp{0};
 	hrt_abstime _last_error_message{0};
 	orb_advert_t _mavlink_log_pub{nullptr};
 
 	DataValidatorGroup _voter{1};
 	unsigned _last_failover_count{0};
 
-	uint64_t _mag_timestamp_sum{0};
-	matrix::Vector3f _mag_sum{};
-	int _mag_sum_count{0};
+	uint64_t _timestamp_sample_sum[MAX_SENSOR_COUNT] {0};
+	matrix::Vector3f _mag_sum[MAX_SENSOR_COUNT] {};
+	int _mag_sum_count[MAX_SENSOR_COUNT] {};
+	hrt_abstime _last_publication_timestamp[MAX_SENSOR_COUNT] {};
 
 	sensor_mag_s _last_data[MAX_SENSOR_COUNT] {};
 	bool _advertised[MAX_SENSOR_COUNT] {};
@@ -134,6 +144,7 @@ private:
 
 	DEFINE_PARAMETERS(
 		(ParamInt<px4::params::CAL_MAG_COMP_TYP>) _param_mag_comp_typ,
+		(ParamBool<px4::params::SENS_MAG_MODE>) _param_sens_mag_mode,
 		(ParamFloat<px4::params::SENS_MAG_RATE>) _param_sens_mag_rate
 	)
 };