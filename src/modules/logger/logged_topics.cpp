861e06dfd7e317a426cb0868e5824b9d92987dc8
Daniel Agar <daniel@agar.ca>
2020-09-25 11:36:58 -0400

    mavlink: handle receiving GENERATOR_STATUS
     - only published (ORB_ID(generator_status)) and logged for now

--- src/modules/logger/logged_topics.cpp
+++ src/modules/logger/logged_topics.cpp
@@ -63,6 +63,7 @@ void LoggedTopics::add_default_topics()
 	add_topic("estimator_sensor_bias", 1000);
 	add_topic("estimator_states", 1000);
 	add_topic("estimator_status", 200);
+	add_topic("generator_status");
 	add_topic("home_position");
 	add_topic("hover_thrust_estimate", 100);
 	add_topic("input_rc", 500);
@@ -79,9 +80,9 @@ void LoggedTopics::add_default_topics()
 	add_topic("safety");
 	add_topic("sensor_combined");
 	add_topic("sensor_correction");
-	add_topic("sensors_status_imu", 200);
 	add_topic("sensor_preflight_mag", 500);
 	add_topic("sensor_selection");
+	add_topic("sensors_status_imu", 200);
 	add_topic("system_power", 500);
 	add_topic("tecs_status", 200);
 	add_topic("test_motor", 500);
