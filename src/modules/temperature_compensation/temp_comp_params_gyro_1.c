eecf2e7a1e97166358b2acfd9ebabe42ea54c15e
Daniel Agar <daniel@agar.ca>
2020-10-08 19:01:44 -0400

    sensors: allow up to 4 accels, gyros, and baros and add configurable rotations for accel & gyro

---
+++ src/modules/temperature_compensation/temp_comp_params_gyro_1.c
@@ -0,0 +1,162 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2017-2020 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/* Gyro 1 */
+
+/**
+ * ID of Gyro that the calibration is for.
+ *
+ * @group Thermal Compensation
+ * @category system
+ */
+PARAM_DEFINE_INT32(TC_G1_ID, 0);
+
+/**
+ * Gyro rate offset temperature ^3 polynomial coefficient - X axis.
+ *
+ * @group Thermal Compensation
+ * @category system
+ */
+PARAM_DEFINE_FLOAT(TC_G1_X3_0, 0.0f);
+
+/**
+ * Gyro rate offset temperature ^3 polynomial coefficient - Y axis.
+ *
+ * @group Thermal Compensation
+ * @category system
+ */
+PARAM_DEFINE_FLOAT(TC_G1_X3_1, 0.0f);
+
+/**
+ * Gyro rate offset temperature ^3 polynomial coefficient - Z axis.
+ *
+ * @group Thermal Compensation
+ * @category system
+ */
+PARAM_DEFINE_FLOAT(TC_G1_X3_2, 0.0f);
+
+/**
+ * Gyro rate offset temperature ^2 polynomial coefficient - X axis.
+ *
+ * @group Thermal Compensation
+ * @category system
+ */
+PARAM_DEFINE_FLOAT(TC_G1_X2_0, 0.0f);
+
+/**
+ * Gyro rate offset temperature ^2 polynomial coefficient - Y axis.
+ *
+ * @group Thermal Compensation
+ * @category system
+ */
+PARAM_DEFINE_FLOAT(TC_G1_X2_1, 0.0f);
+
+/**
+ * Gyro rate offset temperature ^2 polynomial coefficient - Z axis.
+ *
+ * @group Thermal Compensation
+ * @category system
+ */
+PARAM_DEFINE_FLOAT(TC_G1_X2_2, 0.0f);
+
+/**
+ * Gyro rate offset temperature ^1 polynomial coefficient - X axis.
+ *
+ * @group Thermal Compensation
+ * @category system
+ */
+PARAM_DEFINE_FLOAT(TC_G1_X1_0, 0.0f);
+
+/**
+ * Gyro rate offset temperature ^1 polynomial coefficient - Y axis.
+ *
+ * @group Thermal Compensation
+ * @category system
+ */
+PARAM_DEFINE_FLOAT(TC_G1_X1_1, 0.0f);
+
+/**
+ * Gyro rate offset temperature ^1 polynomial coefficient - Z axis.
+ *
+ * @group Thermal Compensation
+ * @category system
+ */
+PARAM_DEFINE_FLOAT(TC_G1_X1_2, 0.0f);
+
+/**
+ * Gyro rate offset temperature ^0 polynomial coefficient - X axis.
+ *
+ * @group Thermal Compensation
+ * @category system
+ */
+PARAM_DEFINE_FLOAT(TC_G1_X0_0, 0.0f);
+
+/**
+ * Gyro rate offset temperature ^0 polynomial coefficient - Y axis.
+ *
+ * @group Thermal Compensation
+ * @category system
+ */
+PARAM_DEFINE_FLOAT(TC_G1_X0_1, 0.0f);
+
+/**
+ * Gyro rate offset temperature ^0 polynomial coefficient - Z axis.
+ *
+ * @group Thermal Compensation
+ * @category system
+ */
+PARAM_DEFINE_FLOAT(TC_G1_X0_2, 0.0f);
+
+/**
+ * Gyro calibration reference temperature.
+ *
+ * @group Thermal Compensation
+ * @category system
+ */
+PARAM_DEFINE_FLOAT(TC_G1_TREF, 25.0f);
+
+/**
+ * Gyro calibration minimum temperature.
+ *
+ * @group Thermal Compensation
+ * @category system
+ */
+PARAM_DEFINE_FLOAT(TC_G1_TMIN, 0.0f);
+
+/**
+ * Gyro calibration maximum temperature.
+ *
+ * @group Thermal Compensation
+ * @category system
+ */
+PARAM_DEFINE_FLOAT(TC_G1_TMAX, 100.0f);
