85e8c4801b75b56b451f9cf1d0aa094cb87c4fd6
JaeyoungLim <jaeyoung@auterion.com>
2020-10-09 16:20:01 +0200

    Add techpod SITL Gazebo target (#15919)
    * Add techpod SITL target
    
    This adds a SITL target forthe techpod fixedwing model
    
    * Update sitl_gazebo submoudle
    
    This submodule update includes the techpod UAV model

--- platforms/posix/cmake/sitl_target.cmake
+++ platforms/posix/cmake/sitl_target.cmake
@@ -93,7 +93,7 @@ set(viewers none jmavsim gazebo)
 set(debuggers none ide gdb lldb ddd valgrind callgrind)
 set(models none shell
 	if750a iris iris_dual_gps iris_opt_flow iris_opt_flow_mockup iris_vision iris_rplidar iris_irlock iris_obs_avoid iris_rtps px4vision solo typhoon_h480
-	plane plane_cam plane_catapult plane_lidar
+	plane plane_cam plane_catapult plane_lidar techpod
 	standard_vtol tailsitter tiltrotor
 	rover r1_rover boat cloudship
 	uuv_hippocampus)
