261b14040e9a0753e4f123a75ff4b4fef1c2ebb8
Daniel Agar <daniel@agar.ca>
2020-10-10 13:50:56 -0400

    mavlink: move ORBIT_EXECUTION_STATUS stream to separate file

--- src/modules/mavlink/mavlink_messages.cpp
+++ src/modules/mavlink/mavlink_messages.cpp
@@ -80,7 +80,6 @@
 #include <uORB/topics/mavlink_log.h>
 #include <uORB/topics/mount_orientation.h>
 #include <uORB/topics/optical_flow.h>
-#include <uORB/topics/orbit_status.h>
 #include <uORB/topics/position_controller_status.h>
 #include <uORB/topics/position_setpoint_triplet.h>
 #include <uORB/topics/sensor_baro.h>
@@ -121,6 +120,7 @@ using matrix::wrap_2pi;
 #include "streams/FLIGHT_INFORMATION.hpp"
 #include "streams/HIGH_LATENCY2.hpp"
 #include "streams/OBSTACLE_DISTANCE.hpp"
+#include "streams/ORBIT_EXECUTION_STATUS.hpp"
 #include "streams/PROTOCOL_VERSION.hpp"
 #include "streams/RAW_RPM.hpp"
 #include "streams/STORAGE_INFORMATION.hpp"
@@ -5093,71 +5093,6 @@ protected:
 	}
 };
 
-class MavlinkStreamOrbitStatus : public MavlinkStream
-{
-public:
-	const char *get_name() const override
-	{
-		return MavlinkStreamOrbitStatus::get_name_static();
-	}
-
-	static constexpr const char *get_name_static()
-	{
-		return "ORBIT_EXECUTION_STATUS";
-	}
-
-	static constexpr uint16_t get_id_static()
-	{
-		return MAVLINK_MSG_ID_ORBIT_EXECUTION_STATUS;
-	}
-
-	uint16_t get_id() override
-	{
-		return get_id_static();
-	}
-
-	static MavlinkStream *new_instance(Mavlink *mavlink)
-	{
-		return new MavlinkStreamOrbitStatus(mavlink);
-	}
-
-	unsigned get_size() override
-	{
-		return _orb_status_sub.advertised() ? MAVLINK_MSG_ID_ORBIT_EXECUTION_STATUS_LEN + MAVLINK_NUM_NON_PAYLOAD_BYTES : 0;
-	}
-
-private:
-	uORB::Subscription _orb_status_sub{ORB_ID(orbit_status)};
-
-	/* do not allow top copying this class */
-	MavlinkStreamOrbitStatus(MavlinkStreamOrbitStatus &);
-	MavlinkStreamOrbitStatus &operator = (const MavlinkStreamOrbitStatus &);
-
-protected:
-	explicit MavlinkStreamOrbitStatus(Mavlink *mavlink) : MavlinkStream(mavlink)
-	{}
-
-	bool send() override
-	{
-		orbit_status_s _orbit_status;
-
-		if (_orb_status_sub.update(&_orbit_status)) {
-			mavlink_orbit_execution_status_t _msg_orbit_execution_status{};
-
-			_msg_orbit_execution_status.time_usec = _orbit_status.timestamp;
-			_msg_orbit_execution_status.radius = _orbit_status.radius;
-			_msg_orbit_execution_status.frame = _orbit_status.frame;
-			_msg_orbit_execution_status.x = _orbit_status.x * 1e7;
-			_msg_orbit_execution_status.y = _orbit_status.y * 1e7;
-			_msg_orbit_execution_status.z = _orbit_status.z;
-
-			mavlink_msg_orbit_execution_status_send_struct(_mavlink->get_channel(), &_msg_orbit_execution_status);
-		}
-
-		return true;
-	}
-};
-
 static const StreamListItem streams_list[] = {
 	create_stream_list_item<MavlinkStreamHeartbeat>(),
 	create_stream_list_item<MavlinkStreamStatustext>(),
@@ -5221,7 +5156,9 @@ static const StreamListItem streams_list[] = {
 #endif // HIGH_LATENCY2_HPP
 	create_stream_list_item<MavlinkStreamGroundTruth>(),
 	create_stream_list_item<MavlinkStreamPing>(),
+#if defined(ORBIT_EXECUTION_STATUS_HPP)
 	create_stream_list_item<MavlinkStreamOrbitStatus>(),
+#endif // ORBIT_EXECUTION_STATUS_HPP
 #if defined(OBSTACLE_DISTANCE_HPP)
 	create_stream_list_item<MavlinkStreamObstacleDistance>(),
 #endif // OBSTACLE_DISTANCE_HPP
