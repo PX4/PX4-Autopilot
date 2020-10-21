d970b514fee72d225b3b3bde82033c6b2e9caf4e
Daniel Agar <daniel@agar.ca>
2020-10-21 13:15:04 -0400

    EKF: add fault status bit for clipping

--- EKF/common.h
+++ EKF/common.h
@@ -401,6 +401,7 @@ union fault_status_u {
 		bool bad_pos_E: 1;	///< 13 - true if fusion of the East position has encountered a numerical error
 		bool bad_pos_D: 1;	///< 14 - true if fusion of the Down position has encountered a numerical error
 		bool bad_acc_bias: 1;	///< 15 - true if bad delta velocity bias estimates have been detected
+		bool bad_acc_clipping: 1; ///< 16 - true if delta velocity data contains clipping (asymmetric railing)
 	} flags;
 	uint16_t value;
 
