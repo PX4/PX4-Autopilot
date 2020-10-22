d99e8f3879021d7c5f64f5fc264e30c1634b5ac7
Daniel Agar <daniel@agar.ca>
2020-10-22 10:44:18 -0400

    logger: double actuator_controls_0 and vehicle_attitude_setpoint default log rate

--- src/modules/logger/logged_topics.cpp
+++ src/modules/logger/logged_topics.cpp
@@ -45,7 +45,7 @@ using namespace px4::logger;
 void LoggedTopics::add_default_topics()
 {
 	add_topic("actuator_armed");
-	add_topic("actuator_controls_0", 100);
+	add_topic("actuator_controls_0", 50);
 	add_topic("actuator_controls_1", 100);
 	add_topic("airspeed", 1000);
 	add_topic("airspeed_validated", 200);
@@ -93,7 +93,7 @@ void LoggedTopics::add_default_topics()
 	add_topic("vehicle_air_data", 200);
 	add_topic("vehicle_angular_velocity", 20);
 	add_topic("vehicle_attitude", 50);
-	add_topic("vehicle_attitude_setpoint", 100);
+	add_topic("vehicle_attitude_setpoint", 50);
 	add_topic("vehicle_command");
 	add_topic("vehicle_constraints", 1000);
 	add_topic("vehicle_control_mode");
