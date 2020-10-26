3e869dc85ab1814b9c26dcb21721ccd822450ae6
SalimTerryLi <lhf2613@gmail.com>
2020-10-26 13:43:46 +0800

    fix

--- src/modules/load_mon/LoadMon.cpp
+++ src/modules/load_mon/LoadMon.cpp
@@ -168,51 +168,52 @@ void LoadMon::cpuload()
 	int32_t kb_main_buffers = -1;
 	int parsedCount = 0;
 
-	if (_proc_fd == nullptr) {
-		PX4_ERR("Could not open /proc/meminfo");
-	}
+	if (_proc_fd != nullptr) {
+		while (fgets(line, sizeof(line), _proc_fd)) {
+			if (sscanf(line, "MemTotal: %d kB", &kb_main_total) == 1) {
+				++parsedCount;
+				continue;
+			}
 
-	while (fgets(line, sizeof(line), _proc_fd)) {
-		if (sscanf(line, "MemTotal: %d kB", &kb_main_total) == 1) {
-			++parsedCount;
-			continue;
+			if (sscanf(line, "MemFree: %d kB", &kb_main_free) == 1) {
+				++parsedCount;
+				continue;
+			}
+
+			if (sscanf(line, "Cached: %d kB", &kb_page_cache) == 1) {
+				++parsedCount;
+				continue;
+			}
+
+			if (sscanf(line, "SReclaimable: %d kB", &kb_slab_reclaimable) == 1) {
+				++parsedCount;
+				continue;
+			}
+
+			if (sscanf(line, "Buffers: %d kB", &kb_main_buffers) == 1) {
+				++parsedCount;
+				continue;
+			}
 		}
 
-		if (sscanf(line, "MemFree: %d kB", &kb_main_free) == 1) {
-			++parsedCount;
-			continue;
+		fseek(_proc_fd, 0, SEEK_END);
+
+		if (parsedCount == 5) {
+			int32_t kb_main_cached = kb_page_cache + kb_slab_reclaimable;
+			int32_t mem_used = kb_main_total - kb_main_free - kb_main_cached - kb_main_buffers;
+
+			if (mem_used < 0) {
+				mem_used = kb_main_total - kb_main_free;
+			}
+
+			cpuload.ram_usage = (float)mem_used / kb_main_total;
+
+		} else {
+			PX4_ERR("Could not parse /proc/meminfo");
+			cpuload.ram_usage = -1;
 		}
-
-		if (sscanf(line, "Cached: %d kB", &kb_page_cache) == 1) {
-			++parsedCount;
-			continue;
-		}
-
-		if (sscanf(line, "SReclaimable: %d kB", &kb_slab_reclaimable) == 1) {
-			++parsedCount;
-			continue;
-		}
-
-		if (sscanf(line, "Buffers: %d kB", &kb_main_buffers) == 1) {
-			++parsedCount;
-			continue;
-		}
-	}
-
-	fseek(_proc_fd, 0, SEEK_END);
-
-	if (parsedCount == 5) {
-		int32_t kb_main_cached = kb_page_cache + kb_slab_reclaimable;
-		int32_t mem_used = kb_main_total - kb_main_free - kb_main_cached - kb_main_buffers;
-
-		if (mem_used < 0) {
-			mem_used = kb_main_total - kb_main_free;
-		}
-
-		cpuload.ram_usage = (float)mem_used / kb_main_total;
 
 	} else {
-		PX4_ERR("Could not parse /proc/meminfo");
 		cpuload.ram_usage = -1;
 	}
 
