47170096ba3789fcc8632e40d5f4c326a7746980
SalimTerryLi <lhf2613@gmail.com>
2020-10-26 13:35:06 +0800

    open _proc_fd in Run()

--- src/modules/load_mon/LoadMon.cpp
+++ src/modules/load_mon/LoadMon.cpp
@@ -86,6 +86,17 @@ void LoadMon::start()
 
 void LoadMon::Run()
 {
+#if defined (__PX4_LINUX)
+
+	if (_proc_fd == nullptr) {	// init fd
+		_proc_fd = fopen("/proc/meminfo", "r");
+
+		if (_proc_fd == nullptr) {
+			PX4_ERR("Failed to open /proc/meminfo");
+		}
+	}
+
+#endif
 	perf_begin(_cycle_perf);
 
 	cpuload();
@@ -100,6 +111,9 @@ void LoadMon::Run()
 
 	if (should_exit()) {
 		ScheduleClear();
+#if defined (__PX4_LINUX)
+		fclose(_proc_fd);
+#endif
 		exit_and_cleanup();
 	}
 
@@ -153,17 +167,12 @@ void LoadMon::cpuload()
 	int32_t kb_slab_reclaimable = -1;
 	int32_t kb_main_buffers = -1;
 	int parsedCount = 0;
-	FILE *meminfo = fopen("/proc/meminfo", "r");
 
-	if (meminfo == nullptr) {
+	if (_proc_fd == nullptr) {
 		PX4_ERR("Could not open /proc/meminfo");
 	}
 
-	while (fgets(line, sizeof(line), meminfo)) {
-		if (parsedCount == 5) {
-			break;
-		}
-
+	while (fgets(line, sizeof(line), _proc_fd)) {
 		if (sscanf(line, "MemTotal: %d kB", &kb_main_total) == 1) {
 			++parsedCount;
 			continue;
@@ -190,7 +199,7 @@ void LoadMon::cpuload()
 		}
 	}
 
-	fclose(meminfo);
+	fseek(_proc_fd, 0, SEEK_END);
 
 	if (parsedCount == 5) {
 		int32_t kb_main_cached = kb_page_cache + kb_slab_reclaimable;
--- src/modules/load_mon/LoadMon.hpp
+++ src/modules/load_mon/LoadMon.hpp
@@ -93,6 +93,7 @@ private:
 	uORB::Publication<cpuload_s> _cpuload_pub {ORB_ID(cpuload)};
 
 #if defined(__PX4_LINUX)
+	FILE *_proc_fd = nullptr;
 	/* calculate usage directly from clock ticks on Linux */
 	clock_t _last_total_time_stamp{};
 	clock_t _last_spent_time_stamp{};
