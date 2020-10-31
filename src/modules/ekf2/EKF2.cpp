96292195e14225f823dad88c1da70cd7da0de1c7
Daniel Agar <daniel@agar.ca>
2020-10-31 15:36:41 -0400

    ekf2: move parameter update to beginning of cycle

--- src/modules/ekf2/EKF2.cpp
+++ src/modules/ekf2/EKF2.cpp
@@ -251,6 +251,16 @@ void EKF2::Run()
 		}
 	}
 
+	// check for parameter updates
+	if (_parameter_update_sub.updated()) {
+		// clear update
+		parameter_update_s pupdate;
+		_parameter_update_sub.copy(&pupdate);
+
+		// update parameters from storage
+		updateParams();
+	}
+
 	bool updated = false;
 	imuSample imu_sample_new {};
 
@@ -297,17 +307,6 @@ void EKF2::Run()
 	}
 
 	if (updated) {
-
-		// check for parameter updates
-		if (_parameter_update_sub.updated()) {
-			// clear update
-			parameter_update_s pupdate;
-			_parameter_update_sub.copy(&pupdate);
-
-			// update parameters from storage
-			updateParams();
-		}
-
 		const hrt_abstime now = imu_sample_new.time_us;
 
 		// ekf2_timestamps (using 0.1 ms relative timestamps)
