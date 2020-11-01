a17860db4ad5c59e4f271d01259149bc5975ca6e
Daniel Agar <daniel@agar.ca>
2020-11-01 09:52:15 -0500

    px4_work_queue: reduce INS (ekf2) stack by 1200 bytes

--- platforms/common/include/px4_platform_common/px4_work_queue/WorkQueueManager.hpp
+++ platforms/common/include/px4_platform_common/px4_work_queue/WorkQueueManager.hpp
@@ -67,10 +67,10 @@ static constexpr wq_config_t I2C4{"wq:I2C4", 1472, -12};
 // PX4 att/pos controllers, highest priority after sensors.
 static constexpr wq_config_t nav_and_controllers{"wq:nav_and_controllers", 1728, -13};
 
-static constexpr wq_config_t INS0{"wq:INS0", 7200, -14};
-static constexpr wq_config_t INS1{"wq:INS1", 7200, -15};
-static constexpr wq_config_t INS2{"wq:INS2", 7200, -16};
-static constexpr wq_config_t INS3{"wq:INS3", 7200, -17};
+static constexpr wq_config_t INS0{"wq:INS0", 6000, -14};
+static constexpr wq_config_t INS1{"wq:INS1", 6000, -15};
+static constexpr wq_config_t INS2{"wq:INS2", 6000, -16};
+static constexpr wq_config_t INS3{"wq:INS3", 6000, -17};
 
 static constexpr wq_config_t hp_default{"wq:hp_default", 1900, -18};
 
