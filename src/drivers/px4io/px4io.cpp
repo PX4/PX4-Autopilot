PX4/PX4-Autopilot

commit 8ea5fd20c1c268c6baf145606186592bf5bd3699

px4io driver: Add support for circuit breakers

src/drivers/px4io/px4io.cpp
@@ -72,6 +72,7 @@
 #include <systemlib/systemlib.h>
 #include <systemlib/scheduling_priorities.h>
 #include <systemlib/param/param.h>
+#include <systemlib/circuit_breaker.h>
 
 #include <uORB/topics/actuator_controls.h>
 #include <uORB/topics/actuator_outputs.h>
@@ -1007,6 +1008,19 @@ PX4IO::task_main()
 						if (pret != OK) {
 							log("failsafe upload failed, FS: %d us", (int)failsafe_thr);
 						}
+					}
+				}
+
+				int32_t safety_param_val;
+				param_t safety_param = param_find("RC_FAILS_THR");
+
+				if (safety_param != PARAM_INVALID) {
+
+					param_get(safety_param, &safety_param_val);
+
+					if (safety_param_val == PX4IO_FORCE_SAFETY_MAGIC) {
+						/* disable IO safety if circuit breaker asked for it */
+						(void)io_reg_set(PX4IO_PAGE_SETUP, PX4IO_P_SETUP_FORCE_SAFETY_OFF, safety_param_val);
 					}
 				}
 

commit a150d4c2d2d35afcc0734f2122bd2ac15efc37e6

px4io: circuit breaker set -> safety has no influence
The IO firmware does not support completely disabling the IO safety.
#1399 and
8ea5fd20c1c268c6baf145606186592bf5bd3699 introduced just setting
safety off once when the circuit breaker gets set and on boot but
the button is still active and can turn safety on again
and even disarm the vehicle.

I'm adapting the implementation of the SafetyButton driver on the FMU
that was introduced in
35fc51de920c6ca01806a044d3d37b01f8a5758a

src/drivers/px4io/px4io.cpp
@@ -422,6 +422,8 @@ private:
 	/* do not allow to copy this class due to ptr data members */
 	PX4IO(const PX4IO &);
 	PX4IO operator=(const PX4IO &);
+
+	bool _safety_disabled{false}; ///< circuit breaker to disable the safety button
 };
 
 namespace
@@ -821,8 +823,10 @@ PX4IO::init()
 
 	}
 
+	_safety_disabled = circuit_breaker_enabled("CBRK_IO_SAFETY", CBRK_IO_SAFETY_KEY);
+
 	/* set safety to off if circuit breaker enabled */
-	if (circuit_breaker_enabled("CBRK_IO_SAFETY", CBRK_IO_SAFETY_KEY)) {
+	if (_safety_disabled) {
 		(void)io_reg_set(PX4IO_PAGE_SETUP, PX4IO_P_SETUP_FORCE_SAFETY_OFF, PX4IO_FORCE_SAFETY_MAGIC);
 	}
 
@@ -1002,9 +1006,11 @@ PX4IO::task_main()
 				}
 
 				/* Check if the IO safety circuit breaker has been updated */
-				bool circuit_breaker_io_safety_enabled = circuit_breaker_enabled("CBRK_IO_SAFETY", CBRK_IO_SAFETY_KEY);
-				/* Bypass IO safety switch logic by setting FORCE_SAFETY_OFF */
-				(void)io_reg_set(PX4IO_PAGE_SETUP, PX4IO_P_SETUP_FORCE_SAFETY_OFF, circuit_breaker_io_safety_enabled);
+				_safety_disabled = circuit_breaker_enabled("CBRK_IO_SAFETY", CBRK_IO_SAFETY_KEY);
+
+				if (_safety_disabled) {
+					(void)io_reg_set(PX4IO_PAGE_SETUP, PX4IO_P_SETUP_FORCE_SAFETY_OFF, PX4IO_FORCE_SAFETY_MAGIC);
+				}
 
 				/* Check if the flight termination circuit breaker has been updated */
 				_cb_flighttermination = circuit_breaker_enabled("CBRK_FLIGHTTERM", CBRK_FLIGHTTERM_KEY);
@@ -1636,7 +1642,7 @@ PX4IO::io_handle_status(uint16_t status)
 	/**
 	 * Get and handle the safety status
 	 */
-	const bool safety_off = status & PX4IO_P_STATUS_FLAGS_SAFETY_OFF;
+	const bool safety_off = (status & PX4IO_P_STATUS_FLAGS_SAFETY_OFF) || _safety_disabled;
 	const bool override_enabled = status & PX4IO_P_STATUS_FLAGS_OVERRIDE;
 
 	// publish immediately on change, otherwise at 1 Hz