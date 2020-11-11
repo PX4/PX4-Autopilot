843171700837dc90ee82465a4d90c7161f757352
Matthias Grob <maetugr@gmail.com>
2020-11-11 20:00:52 +0100

    FlightTasks: remove FlightTaskManualPositionSmooth
    alias first smooth position control version from 2017.
    
    RIP it served well.

--- src/modules/mc_pos_control/MulticopterPositionControl.cpp
+++ src/modules/mc_pos_control/MulticopterPositionControl.cpp
@@ -593,10 +593,6 @@ void MulticopterPositionControl::start_flight_task()
 			error =  _flight_tasks.switchTask(FlightTaskIndex::ManualPosition);
 			break;
 
-		case 1:
-			error =  _flight_tasks.switchTask(FlightTaskIndex::ManualPositionSmooth);
-			break;
-
 		case 3:
 			error =  _flight_tasks.switchTask(FlightTaskIndex::ManualPositionSmoothVel);
 			break;
@@ -629,10 +625,6 @@ void MulticopterPositionControl::start_flight_task()
 		switch (_param_mpc_pos_mode.get()) {
 		case 0:
 			error =  _flight_tasks.switchTask(FlightTaskIndex::ManualAltitude);
-			break;
-
-		case 1:
-			error =  _flight_tasks.switchTask(FlightTaskIndex::ManualAltitudeSmooth);
 			break;
 
 		case 3:
