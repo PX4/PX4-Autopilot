1719ff9892f3c3d034f2b44e94d15527ab09cec6
Julian Oes <julian@oes.ch>
2020-12-07 10:34:32 +1300

    simulator: fix after px4_task_spawn_cmd change
    argv[0] now contains the task name, so everything moves by one.

--- src/modules/simulator/simulator.cpp
+++ src/modules/simulator/simulator.cpp
@@ -69,14 +69,14 @@ int Simulator::start(int argc, char *argv[])
 	_instance = new Simulator();
 
 	if (_instance) {
-		if (argc == 4 && strcmp(argv[2], "-u") == 0) {
+		if (argc == 5 && strcmp(argv[3], "-u") == 0) {
 			_instance->set_ip(InternetProtocol::UDP);
-			_instance->set_port(atoi(argv[3]));
+			_instance->set_port(atoi(argv[4]));
 		}
 
-		if (argc == 4 && strcmp(argv[2], "-c") == 0) {
+		if (argc == 5 && strcmp(argv[3], "-c") == 0) {
 			_instance->set_ip(InternetProtocol::TCP);
-			_instance->set_port(atoi(argv[3]));
+			_instance->set_port(atoi(argv[4]));
 		}
 
 		_instance->run();
