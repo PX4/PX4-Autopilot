0b76add4dcd09aa00cc65345063cb4c645f3be77
flbernier <florian.bernier@drotek.com>
2020-12-09 12:04:34 +0100

    Enable dshot driver for fmu-v4pro

--- boards/px4/fmu-v4pro/default.cmake
+++ boards/px4/fmu-v4pro/default.cmake
@@ -25,7 +25,7 @@ px4_add_board(
 		camera_trigger
 		differential_pressure # all available differential pressure drivers
 		distance_sensor # all available distance sensor drivers
-		#dshot
+		dshot
 		gps
 		#heater
 		#imu # all available imu drivers
