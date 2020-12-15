1a78ff822ce6fb73c8eea39d6391570bc9999d32
bresch <brescianimathieu@gmail.com>
2020-12-15 16:15:24 +0100

    HTE: only run in air
    the _landed flag only is not enough to tell if the drone is still touching
    the ground, so an additional check based on the altitude AGL is required.
    
    to have this in_air detection correctly updated, the module needs to be
    scheduled on the vehicle_local_position message as no setpoint is
    produced in non-assisted modes.

--- src/modules/mc_hover_thrust_estimator/MulticopterHoverThrustEstimator.hpp
+++ src/modules/mc_hover_thrust_estimator/MulticopterHoverThrustEstimator.hpp
@@ -95,12 +95,12 @@ private:
 
 	uORB::Publication<hover_thrust_estimate_s> _hover_thrust_ekf_pub{ORB_ID(hover_thrust_estimate)};
 
-	uORB::SubscriptionCallbackWorkItem _vehicle_local_position_setpoint_sub{this, ORB_ID(vehicle_local_position_setpoint)};
+	uORB::SubscriptionCallbackWorkItem _vehicle_local_position_sub{this, ORB_ID(vehicle_local_position)};
 
 	uORB::Subscription _parameter_update_sub{ORB_ID(parameter_update)};
 	uORB::Subscription _vehicle_land_detected_sub{ORB_ID(vehicle_land_detected)};
 	uORB::Subscription _vehicle_status_sub{ORB_ID(vehicle_status)};
-	uORB::Subscription _vehicle_local_pos_sub{ORB_ID(vehicle_local_position)};
+	uORB::Subscription _vehicle_local_position_setpoint_sub{ORB_ID(vehicle_local_position_setpoint)};
 
 	hrt_abstime _timestamp_last{0};
 
