cdca7e537b4fde27029ffcbbc7a3eafb15cd3070
Daniel Agar <daniel@agar.ca>
2021-01-06 21:50:52 -0500

    log all vehicle_magnetometer by default

--- src/modules/logger/logged_topics.cpp
+++ src/modules/logger/logged_topics.cpp
@@ -143,6 +143,7 @@ void LoggedTopics::add_default_topics()
 	add_topic_multi("sensor_mag", 1000, 4);
 	add_topic_multi("vehicle_imu", 500, 4);
 	add_topic_multi("vehicle_imu_status", 1000, 4);
+	add_topic_multi("vehicle_magnetometer", 500, 4);
 
 #ifdef CONFIG_ARCH_BOARD_PX4_SITL
 	add_topic("actuator_controls_virtual_fw");
