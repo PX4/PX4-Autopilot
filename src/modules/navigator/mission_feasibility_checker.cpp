d4e4499ea113d1ea550ebba6d085652cb268b540
Lorenz Meier <lorenz@px4.io>
2021-01-10 21:13:58 +0100

    Update mission_feasibility_checker.cpp
    Fix typo

--- src/modules/navigator/mission_feasibility_checker.cpp
+++ src/modules/navigator/mission_feasibility_checker.cpp
@@ -346,7 +346,7 @@ MissionFeasibilityChecker::checkTakeoff(const mission_s &mission, float home_alt
 			// get the parameter for min take off altitude in meters
 			float takeoff_alt_min = _navigator->get_takeoff_min_alt();
 
-			// check if it is set to something meaning full and if the take off altitude is set above the parameter
+			// check if it is set to something meaningful and if the take off altitude is set above the parameter
 			if (takeoff_alt_min > 0 && takeoff_alt < takeoff_alt_min) {
 				mavlink_log_critical(_navigator->get_mavlink_log_pub(), "Mission rejected: Takeoff altitude too low!");
 				return false;
