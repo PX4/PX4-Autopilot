e34868ffad94f3c3d4ffa06c9ec51354f6c5780f
JacobCrabill <jacob@flyvoly.com>
2021-01-13 14:11:54 -0800

    distance_sensors: Fix assignment of serial bus ID

--- src/drivers/distance_sensor/cm8jl65/CM8JL65.cpp
+++ src/drivers/distance_sensor/cm8jl65/CM8JL65.cpp
@@ -100,7 +100,7 @@ CM8JL65::CM8JL65(const char *port, uint8_t rotation) :
 	device::Device::DeviceId device_id;
 	device_id.devid_s.bus_type = device::Device::DeviceBusType_SERIAL;
 
-	uint8_t bus_num = atoi(&_port[sizeof(_port) - 2]);
+	uint8_t bus_num = atoi(&_port[strlen(_port) - 1]); // Assuming '/dev/ttySx'
 
 	if (bus_num < 10) {
 		device_id.devid_s.bus = bus_num;
