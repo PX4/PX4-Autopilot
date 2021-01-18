761cd7d761c5ea7f4fc7ea41b3c5060dd2a9edcc
bresch <brescianimathieu@gmail.com>
2021-01-18 15:38:16 +0100

    mag_calibration: log mag_worker_data

--- src/modules/logger/logged_topics.cpp
+++ src/modules/logger/logged_topics.cpp
@@ -65,6 +65,7 @@ void LoggedTopics::add_default_topics()
 	add_topic("home_position");
 	add_topic("hover_thrust_estimate", 100);
 	add_topic("input_rc", 500);
+	add_topic("mag_worker_data");
 	add_topic("manual_control_setpoint", 200);
 	add_topic("manual_control_switches");
 	add_topic("mission");
