0649c5e4e4ad3aac815bbc583bb534e54f0b60e0
David Sidrane <David.Sidrane@NscDg.com>
2021-01-22 09:14:27 -0800

    px4_fmu-v6x:Support RC03 HW

--- boards/px4/fmu-v6x/src/i2c.cpp
+++ boards/px4/fmu-v6x/src/i2c.cpp
@@ -36,6 +36,6 @@
 constexpr px4_i2c_bus_t px4_i2c_buses[I2C_BUS_MAX_BUS_ITEMS] = {
 	initI2CBusExternal(1),
 	initI2CBusExternal(2),
-	initI2CBusInternal(3),
-	initI2CBusExternal(4),
+	initI2CBusExternal(3),
+	initI2CBusInternal(4),
 };
