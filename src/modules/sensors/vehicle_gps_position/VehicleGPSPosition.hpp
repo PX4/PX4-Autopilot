19610b35762c262f1263c2c138d559700a390116
bresch <brescianimathieu@gmail.com>
2021-01-28 16:06:44 +0100

    gps blend: move blending logic to class

--- src/modules/sensors/vehicle_gps_position/VehicleGPSPosition.hpp
+++ src/modules/sensors/vehicle_gps_position/VehicleGPSPosition.hpp
@@ -47,6 +47,8 @@
 #include <uORB/topics/sensor_gps.h>
 #include <uORB/topics/vehicle_gps_position.h>
 
+#include "gps_blending.hpp"
+
 using namespace time_literals;
 
 namespace sensors
@@ -64,49 +66,18 @@ public:
 	void PrintStatus();
 
 private:
-	// define max number of GPS receivers supported and 0 base instance used to access virtual 'blended' GPS solution
-	static constexpr int GPS_MAX_RECEIVERS = 2;
-	static constexpr int GPS_BLENDED_INSTANCE = GPS_MAX_RECEIVERS;
-
 	void Run() override;
 
 	void ParametersUpdate(bool force = false);
 	void Publish(const sensor_gps_s &gps, uint8_t selected);
-
-	/*
-	 * Update the internal state estimate for a blended GPS solution that is a weighted average of the phsyical
-	 * receiver solutions. This internal state cannot be used directly by estimators because if physical receivers
-	 * have significant position differences, variation in receiver estimated accuracy will cause undesirable
-	 * variation in the position solution.
-	*/
-	bool blend_gps_data();
-
-	/*
-	 * Calculate internal states used to blend GPS data from multiple receivers using weightings calculated
-	 * by calc_blend_weights()
-	 */
-	sensor_gps_s gps_blend_states(float blend_weights[GPS_MAX_RECEIVERS]) const;
-
-	/*
-	 * The location in gps_blended_state will move around as the relative accuracy changes.
-	 * To mitigate this effect a low-pass filtered offset from each GPS location to the blended location is
-	 * calculated.
-	*/
-	void update_gps_offsets(const sensor_gps_s &gps_blended_state);
-
-	/*
-	 Calculate GPS output that is a blend of the offset corrected physical receiver data
-	*/
-	void calc_gps_blend_output(sensor_gps_s &gps_blended_state, float blend_weights[GPS_MAX_RECEIVERS]) const;
 
 	// defines used to specify the mask position for use of different accuracy metrics in the GPS blending algorithm
 	static constexpr uint8_t BLEND_MASK_USE_SPD_ACC  = 1;
 	static constexpr uint8_t BLEND_MASK_USE_HPOS_ACC = 2;
 	static constexpr uint8_t BLEND_MASK_USE_VPOS_ACC = 4;
 
-	// Set the GPS timeout to 2s, after which a receiver will be ignored
-	static constexpr hrt_abstime GPS_TIMEOUT_US = 2_s;
-	static constexpr float GPS_TIMEOUT_S = (GPS_TIMEOUT_US / 1e6f);
+	// define max number of GPS receivers supported
+	static constexpr int GPS_MAX_RECEIVERS = 2;
 
 	uORB::Publication<vehicle_gps_position_s> _vehicle_gps_position_pub{ORB_ID(vehicle_gps_position)};
 
@@ -119,16 +90,7 @@ private:
 
 	perf_counter_t _cycle_perf{perf_alloc(PC_ELAPSED, MODULE_NAME": cycle")};
 
-	sensor_gps_s _gps_state[GPS_MAX_RECEIVERS] {}; ///< internal state data for the physical GPS
-
-	matrix::Vector2f _NE_pos_offset_m[GPS_MAX_RECEIVERS] {}; ///< Filtered North,East position offset from GPS instance to blended solution in _output_state.location (m)
-	float _hgt_offset_mm[GPS_MAX_RECEIVERS] {};	///< Filtered height offset from GPS instance relative to blended solution in _output_state.location (mm)
-
-	uint64_t _time_prev_us[GPS_MAX_RECEIVERS] {};	///< the previous value of time_us for that GPS instance - used to detect new data.
-	uint8_t _gps_time_ref_index{0};			///< index of the receiver that is used as the timing reference for the blending update
-	uint8_t _gps_newest_index{0};			///< index of the physical receiver with the newest data
-	uint8_t _gps_slowest_index{0};			///< index of the physical receiver with the slowest update rate
-	float _gps_dt[GPS_MAX_RECEIVERS] {};		///< average time step in seconds.
+	GpsBlending<GPS_MAX_RECEIVERS> _gps_blending;
 
 	DEFINE_PARAMETERS(
 		(ParamInt<px4::params::SENS_GPS_MASK>) _param_sens_gps_mask,
