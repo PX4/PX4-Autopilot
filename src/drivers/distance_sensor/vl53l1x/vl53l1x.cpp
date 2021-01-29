8e3b9f3a60fe11d22ae2a2fbd5736590dd092b55
dirksavage88 <dirksavage88@gmail.com>
2021-01-28 21:41:49 -0600

    lowered timing budget and inter measurement period, limited to 5 ROI's
    Signed-off-by: dirksavage88 <dirksavage88@gmail.com>

--- src/drivers/distance_sensor/vl53l1x/vl53l1x.cpp
+++ src/drivers/distance_sensor/vl53l1x/vl53l1x.cpp
@@ -36,8 +36,8 @@
 
 #include "vl53l1x.hpp"
 
-#define VL53L1X_SAMPLE_RATE                                20  // ms, default
-#define VL53L1X_INTER_MEAS_MS				   22  //ms
+#define VL53L1X_SAMPLE_RATE                                15  // ms, default
+#define VL53L1X_INTER_MEAS_MS				   18  //ms
 /* ST */
 const uint8_t VL51L1X_DEFAULT_CONFIGURATION[] = {
 	0x00, /* 0x2d : set bit 2 and 5 to 1 for fast plus mode (1MHz I2C), else don't touch */
@@ -240,8 +240,13 @@ void VL53L1X::RunImpl()
 {
 	uint8_t dataReady = 0;
 
+<<<<<<< HEAD
 	uint8_t roiCenter[] = {239, 215, 191, 167, 151};
         static uint8_t zone;
+=======
+	uint8_t roiCenter[] = {239, 215, 191, 167, 151};
+        static uint8_t zone = 0;
+>>>>>>> lowered timing budget and inter measurement period, limited to 5 ROI's
 
 	VL53L1X_CheckForDataReady(&dataReady);
 
@@ -259,8 +264,16 @@ void VL53L1X::RunImpl()
 	if(zone >= sizeof(roiCenter)){
 	        zone = 0;
 	}
+	VL53L1X_SetROICenter(roiCenter[zone]);
+
         //increment
 	zone++;
+
+        //reset counter check
+	if(zone >= sizeof(roiCenter) - 1){
+	        zone = 0;
+	}
+
 }
 
 int VL53L1X::init()
