PX4/PX4-Autopilot

commit 081506bdc887bdfad950718ac432430a5c6a2549

PX Uploader: Disable windowed mode The windowed mode has been added for Windows targets originally. It gets very often incorrectly detected and slows down flashing considerably. This even applies to serial links. We are disabling it now in most circumstances.

Tools/px_uploader.py
@@ -344,8 +344,9 @@ class uploader(object):
         try:
             self.__getSync(False)
         except:
-            # if it fails we are on a real Serial Port
-            self.ackWindowedMode = True
+            # if it fails we are on a real serial port - only leave this enabled on Windows
+            if _platform.system() == 'Windows':
+                self.ackWindowedMode = True
 
         self.port.baudrate = self.baudrate_bootloader
 

commit 239e8c16bec2fa87a6f5906dc9045bb4bd571d5b

px_uploader:Fixes breakage for real serial ports caused by 081506b
   The code was pitching an exception. It was invaild. That then left
   the baud rate AFU

Tools/px_uploader.py
@@ -345,10 +345,10 @@ class uploader(object):
             self.__getSync(False)
         except:
             # if it fails we are on a real serial port - only leave this enabled on Windows
-            if _platform.system() == 'Windows':
+            if sys.platform.startswith('win'):
                 self.ackWindowedMode = True
-
-        self.port.baudrate = self.baudrate_bootloader
+        finally:
+            self.port.baudrate = self.baudrate_bootloader
 
     # send the GET_DEVICE command and wait for an info parameter
     def __getInfo(self, param):