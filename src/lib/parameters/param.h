934268b84b0848fb0ce2ec9b4d5d44af594e80bd
Daniel Agar <daniel@agar.ca>
2021-02-08 11:32:13 -0500

    parameters: merge generated files ito single static constexpr header
     - store parameter type and if volatile separately (saves kilobytes of flash)
     - use Bitset for tracking active and changed parameters
     - use atomic for autosave_enabled flag
     - compile at ${MAX_CUSTOM_OPT_LEVEL} (-O2 on non flash constrained boards)

--- src/lib/parameters/param.h
+++ src/lib/parameters/param.h
@@ -435,25 +435,7 @@ union param_value_u {
  * instead.
  */
 struct param_info_s {
-	const char	*name
-
-// GCC 4.8 and higher don't implement proper alignment of static data on
-// 64-bit. This means that the 24-byte param_info_s variables are
-// 16 byte aligned by GCC and that messes up the assumption that
-// sequential items in the __param segment can be addressed as an array.
-// The assumption is that the address of the second parameter is at
-// &param[0]+sizeof(param[0]). When compiled with clang it is
-// true, with gcc is is not true.
-// See https://llvm.org/bugs/show_bug.cgi?format=multiple&id=18006
-// The following hack is for GCC >=4.8 only. Clang works fine without
-// this.
-#ifdef __PX4_POSIX
-	__attribute__((aligned(16)));
-#else
-	;
-#endif
-	param_type_t	type;
-	uint16_t		volatile_param: 1;
+	const char *name;
 	union param_value_u val;
 };
 
