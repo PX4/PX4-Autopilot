f05264a779ce076766323d5f05e27e7c7a74cba9
David Sidrane <David.Sidrane@NscDg.com>
2021-02-10 15:37:18 -0800

    uav_can-gps-v1:Use common uavcan board identity

--- boards/cuav/can-gps-v1/default.cmake
+++ boards/cuav/can-gps-v1/default.cmake
@@ -1,22 +1,5 @@
+include (${CMAKE_CURRENT_LIST_DIR}/uavcan_board_identity)
 
-
-# UAVCAN boot loadable Module ID
-set(uavcanblid_sw_version_major 0)
-set(uavcanblid_sw_version_minor 1)
-add_definitions(
-	-DAPP_VERSION_MAJOR=${uavcanblid_sw_version_major}
-	-DAPP_VERSION_MINOR=${uavcanblid_sw_version_minor}
-)
-
-set(uavcanblid_hw_version_major 3)
-set(uavcanblid_hw_version_minor 233)
-set(uavcanblid_name "\"org.cuav.can-gps-v1\"")
-
-add_definitions(
-	-DHW_UAVCAN_NAME=${uavcanblid_name}
-	-DHW_VERSION_MAJOR=${uavcanblid_hw_version_major}
-	-DHW_VERSION_MINOR=${uavcanblid_hw_version_minor}
-)
 add_definitions(-DUSE_S_RGB_LED_DMA)
 
 px4_add_board(
