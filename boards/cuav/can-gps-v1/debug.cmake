c481b6922115bc5eeb54fcadf392d6b79112234e
David Sidrane <David.Sidrane@NscDg.com>
2021-02-12 03:10:24 -0800

    ROMFS:Bake in UAVCAN FW with builds that end in _uavcan
       The configuration are mostly for testing now.

---
+++ boards/cuav/can-gps-v1/debug.cmake
@@ -0,0 +1,53 @@
+
+
+# UAVCAN boot loadable Module ID
+set(uavcanblid_sw_version_major 0)
+set(uavcanblid_sw_version_minor 1)
+add_definitions(
+	-DAPP_VERSION_MAJOR=${uavcanblid_sw_version_major}
+	-DAPP_VERSION_MINOR=${uavcanblid_sw_version_minor}
+)
+
+set(uavcanblid_hw_version_major 1)
+set(uavcanblid_hw_version_minor 0)
+set(uavcanblid_name "\"org.cuav.can-gps-v1\"")
+
+add_definitions(
+	-DHW_UAVCAN_NAME=${uavcanblid_name}
+	-DHW_VERSION_MAJOR=${uavcanblid_hw_version_major}
+	-DHW_VERSION_MINOR=${uavcanblid_hw_version_minor}
+)
+add_definitions(-DUSE_S_RGB_LED_DMA)
+
+px4_add_board(
+	PLATFORM nuttx
+	VENDOR cuav
+	MODEL can-gps-v1
+	LABEL debug
+	TOOLCHAIN arm-none-eabi
+	ARCHITECTURE cortex-m4
+	ROMFSROOT cannode
+	UAVCAN_INTERFACES 1
+	DRIVERS
+		barometer/ms5611
+		bootloaders
+		gps
+		lights/neopixel
+		magnetometer/rm3100
+		safety_button
+		tone_alarm
+		uavcannode
+	MODULES
+		load_mon
+	SYSTEMCMDS
+		i2cdetect
+		led_control
+		param
+		perf
+		reboot
+		top
+		topic_listener
+		tune_control
+		ver
+		work_queue
+)
