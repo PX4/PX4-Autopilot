c485846318e2e6fcc30c9b543baf0e6bd2323fee
Lorenz Meier <lorenz@px4.io>
2021-02-13 22:12:07 +0100

    Change wording in GPS failure handling This matches better the different platforms that are using this functionality.

--- src/modules/navigator/gpsfailure.cpp
+++ src/modules/navigator/gpsfailure.cpp
@@ -153,7 +153,7 @@ GpsFailure::advance_gpsf()
 	switch (_gpsf_state) {
 	case GPSF_STATE_NONE:
 		_gpsf_state = GPSF_STATE_LOITER;
-		mavlink_log_critical(_navigator->get_mavlink_log_pub(), "Global position failure: fixed bank loiter");
+		mavlink_log_critical(_navigator->get_mavlink_log_pub(), "Global position failure: loitering");
 		break;
 
 	case GPSF_STATE_LOITER:
