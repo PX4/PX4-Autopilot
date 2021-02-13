4d55473da50d71374cb0a03d2a557ccd9c4c8217
Lorenz Meier <lorenz@px4.io>
2021-02-13 23:22:19 +0100

    Sensors: New parameter for baro priority This allows to give external barometers priority if they are present.

--- src/modules/sensors/sensor_params.c
+++ src/modules/sensors/sensor_params.c
@@ -221,3 +221,15 @@ PARAM_DEFINE_INT32(SENS_EXT_I2C_PRB, 1);
  * @group Sensors
  */
 PARAM_DEFINE_INT32(SENS_IMU_MODE, 1);
+
+/**
+ * Enable internal barometers
+ *
+ * For systems with an external barometer, this should be set to false to make sure that the external is used.
+ *
+ * @boolean
+ * @reboot_required true
+ * @category system
+ * @group Sensors
+ */
+PARAM_DEFINE_INT32(SENS_INT_BARO_EN, 1);
