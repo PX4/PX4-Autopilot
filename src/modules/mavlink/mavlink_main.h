0e9fdb0006c6b19c01d3fd5df7093c4443ee92f1
Nico van Duijn <nico@auterion.com>
2021-02-17 10:37:41 +0100

    v5x: use low bandwidth mavlink mode
    This changes the default mavlink message set from onboard to
    onboard_low_bandwidth, which drastically reduces the bandwidth required to get a
    usable connection.

--- src/modules/mavlink/mavlink_main.h
+++ src/modules/mavlink/mavlink_main.h
@@ -192,6 +192,7 @@ public:
 		MAVLINK_MODE_EXTVISION,
 		MAVLINK_MODE_EXTVISIONMIN,
 		MAVLINK_MODE_GIMBAL,
+		MAVLINK_MODE_ONBOARD_LOW_BANDWIDTH,
 		MAVLINK_MODE_COUNT
 	};
 
@@ -242,6 +243,9 @@ public:
 
 		case MAVLINK_MODE_GIMBAL:
 			return "Gimbal";
+
+		case MAVLINK_MODE_ONBOARD_LOW_BANDWIDTH:
+			return "OnboardLowBandwidth";
 
 		default:
 			return "Unknown";
