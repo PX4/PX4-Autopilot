5826f7570f3bbb244ac67e73802f45a7a783c8db
Ryan Johnston <31726584+ryanjAA@users.noreply.github.com>
2021-02-17 23:22:10 -0600

    Code Style fix

--- boards/mro/ctrl-zero-h7/src/init.c
+++ boards/mro/ctrl-zero-h7/src/init.c
@@ -165,7 +165,7 @@ __EXPORT int board_app_initialize(uintptr_t arg)
 	}
 
 
-/* initial LED state */
+	/* initial LED state */
 	drv_led_start();
 	led_off(LED_RED);
 	led_on(LED_GREEN); // Indicate Power.
