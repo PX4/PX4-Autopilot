9e95391b42cd146fe39eacc0d14e04642c2e2b68
Daniel Agar <daniel@agar.ca>
2021-02-17 21:24:42 -0500

    boards: new uavcan board variants remove delete mkblctrl driver

--- boards/px4/fmu-v5/uavcan.cmake
+++ boards/px4/fmu-v5/uavcan.cmake
@@ -43,7 +43,6 @@ px4_add_board(
 		lights/rgbled_ncp5623c
 		lights/rgbled_pwm
 		magnetometer # all available magnetometer drivers
-		mkblctrl
 		optical_flow # all available optical flow drivers
 		#osd
 		pca9685
