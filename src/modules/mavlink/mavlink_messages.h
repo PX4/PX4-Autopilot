993d1034aea74774f4590544a90f25eaa7c19866
Daniel Agar <daniel@agar.ca>
2021-02-20 20:25:33 -0500

    mavlink: move HEARTBEAT to separate stream header

--- src/modules/mavlink/mavlink_messages.h
+++ src/modules/mavlink/mavlink_messages.h
@@ -43,6 +43,8 @@
 
 #include "mavlink_stream.h"
 
+#include <commander/px4_custom_mode.h>
+
 class StreamListItem
 {
 
@@ -72,7 +74,6 @@ MavlinkStream *create_mavlink_stream(const char *stream_name, Mavlink *mavlink);
 
 MavlinkStream *create_mavlink_stream(const uint16_t msg_id, Mavlink *mavlink);
 
-void get_mavlink_navigation_mode(const struct vehicle_status_s *const status, uint8_t *mavlink_base_mode,
-				 union px4_custom_mode *custom_mode);
+union px4_custom_mode get_px4_custom_mode(uint8_t nav_state);
 
 #endif /* MAVLINK_MESSAGES_H_ */
