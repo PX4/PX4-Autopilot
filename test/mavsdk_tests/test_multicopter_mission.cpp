82200fab9e19d80b3182452048de6e29e87546e1
Daniel Agar <daniel@agar.ca>
2021-02-21 12:53:26 -0500

    mavsdk_tests: relax MC "Fly straight" speed threshold

--- test/mavsdk_tests/test_multicopter_mission.cpp
+++ test/mavsdk_tests/test_multicopter_mission.cpp
@@ -91,7 +91,7 @@ TEST_CASE("Fly straight Multicopter Mission", "[multicopter]")
 	mission_options.rtl_at_end = false;
 	mission_options.fly_through = true;
 	tester.prepare_straight_mission(mission_options);
-	tester.check_mission_item_speed_above(2, 4.5);
+	tester.check_mission_item_speed_above(2, 4.0);
 	tester.check_tracks_mission(5.f);
 	tester.arm();
 	tester.execute_mission();
