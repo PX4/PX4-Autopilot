11a327a0f98891af730f0d180a1c6d870dd7bd1a
Paul Riseborough <gncsolns@gmail.com>
2021-02-23 17:01:57 +1100

    EKF: Add external monitoring of warning and information events

--- EKF/covariance.cpp
+++ EKF/covariance.cpp
@@ -971,6 +971,7 @@ void Ekf::fixCovarianceErrors(bool force_symmetry)
 
 			_time_acc_bias_check = _time_last_imu;
 			_fault_status.flags.bad_acc_bias = false;
+			_warning_events.flags.invalid_accel_bias_cov_reset = true;
 			ECL_WARN_TIMESTAMPED("invalid accel bias - covariance reset");
 
 		} else if (force_symmetry) {
