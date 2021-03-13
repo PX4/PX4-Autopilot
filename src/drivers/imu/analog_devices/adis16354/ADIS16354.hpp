ba94744a52783134eb7dae79c2dea2ec0ed6425b
Pskoog3 <54239292+Pskoog3@users.noreply.github.com>
2021-03-13 16:23:06 +0500

    Update ADIS16354.hpp

--- src/drivers/imu/analog_devices/adis16354/ADIS16354.hpp
+++ src/drivers/imu/analog_devices/adis16354/ADIS16354.hpp
@@ -123,10 +123,10 @@ private:
 	uint8_t _checked_register{0};
 	static constexpr uint8_t size_register_cfg{3};
 	register_config_t _register_cfg[size_register_cfg] {
-	  // Register                	| Set bits, Clear bits
-	    { Register::SMPL_PRD,   	SMPL_PRD_BIT::Multiplier, SMPL_PRD_BIT::Time_base }, // Multiplier = 1, Time_base = 0
-	    { Register::SENS_AVG,   	SENS_AVG_BIT::Measurement_range_set | SENS_AVG_BIT::Number_of_filter_taps, SENS_AVG_BIT::Measurement_range_clear },
-	                                // Measurement_range_set = 1, Number_of_filter_taps = 1, Measurement_range_clear = 0, 0000 0100 0000 0010 (0x0402)
-	    { Register::GPIO_CTRL,   	GPIO_CTRL_BIT::GPIO2_DIRECTION | GPIO_CTRL_BIT::GPIO1_DIRECTION, 0}, // GPIO2 = 1 (output), GPIO1 = 1 (output)
+	  // Register                		| Set bits, Clear bits
+	  	{ Register::SMPL_PRD,   	SMPL_PRD_BIT::Multiplier, SMPL_PRD_BIT::Time_base }, // Multiplier = 1, Time_base = 0
+		{ Register::SENS_AVG,   	SENS_AVG_BIT::Measurement_range_set | SENS_AVG_BIT::Number_of_filter_taps, SENS_AVG_BIT::Measurement_range_clear },
+		// Measurement_range_set = 1, Number_of_filter_taps = 1, Measurement_range_clear = 0, 0000 0100 0000 0010 (0x0402)
+		{ Register::GPIO_CTRL,   	GPIO_CTRL_BIT::GPIO2_DIRECTION | GPIO_CTRL_BIT::GPIO1_DIRECTION, 0}, // GPIO2 = 1 (output), GPIO1 = 1 (output)
 	};
 };
