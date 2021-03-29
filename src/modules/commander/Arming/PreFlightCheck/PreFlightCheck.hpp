55eb6988de492d1a9c49e95c0372a9c4e5bf0fd4
Michael Schaeuble <michael@auterion.com>
2021-03-29 13:20:06 +0200

    PreFlightCheck: Check if SD card is present only once and store the result
    statfs accesses the file-system and can be blocking for an extended period. Since the SD card check is part of the preflight checks in the main thread of commander, it could block its execution and cause various issues. The SD card is only mounted in rcS during boot so the state will not change after the first check.

--- src/modules/commander/Arming/PreFlightCheck/PreFlightCheck.hpp
+++ src/modules/commander/Arming/PreFlightCheck/PreFlightCheck.hpp
@@ -119,5 +119,5 @@ private:
 	static bool manualControlCheck(orb_advert_t *mavlink_log_pub, const bool report_fail);
 	static bool airframeCheck(orb_advert_t *mavlink_log_pub, const vehicle_status_s &status);
 	static bool cpuResourceCheck(orb_advert_t *mavlink_log_pub, const bool report_fail);
-	static bool sdcardCheck(orb_advert_t *mavlink_log_pub, const bool report_fail);
+	static bool sdcardCheck(orb_advert_t *mavlink_log_pub, bool &sd_card_detected_once, const bool report_fail);
 };
