97415754d4200b903e6d08b468d480a0b18026e4
Daniel Agar <daniel@agar.ca>
2021-04-13 17:35:21 -0400

    IMU_GYRO_RATEMAX: enforce reasonable constraint and update metadata

--- src/modules/sensors/vehicle_angular_velocity/imu_gyro_parameters.c
+++ src/modules/sensors/vehicle_angular_velocity/imu_gyro_parameters.c
@@ -83,14 +83,15 @@ PARAM_DEFINE_FLOAT(IMU_GYRO_NF_BW, 20.0f);
 PARAM_DEFINE_FLOAT(IMU_GYRO_CUTOFF, 30.0f);
 
 /**
-* Gyro control data maximum publication rate
+* Gyro control data maximum publication rate (inner loop rate)
 *
-* This is the maximum rate the gyro control data (sensor_gyro) will be allowed to publish at.
-* Set to 0 to disable and publish at the native sensor sample rate.
+* The maximum rate the gyro control data (vehicle_angular_velocity) will be
+* allowed to publish at. This is the loop rate for the rate controller and outputs.
 *
-* @min 0
+* Note: sensor data is always read and filtered at the full raw rate (eg commonly 8 kHz) regardless of this setting.
+*
+* @min 100
 * @max 2000
-* @value 0 0 (driver minimum)
 * @value 100 100 Hz
 * @value 250 250 Hz
 * @value 400 400 Hz
