259ce26c13e947c7314ca396e5066f9b4ee10338
dirksavage88 <dirksavage88@gmail.com>
2021-04-16 13:39:08 -0500

    Changed hex values to decimal in uavcan_board_identity, manually added drivers__device to cannode src CMakeLists
    Signed-off-by: dirksavage88 <dirksavage88@gmail.com>

--- boards/mro/cannode/default.cmake
+++ boards/mro/cannode/default.cmake
@@ -13,11 +13,12 @@ px4_add_board(
 	UAVCAN_INTERFACES 1
 	UAVCAN_TIMER_OVERRIDE 3
 	DRIVERS
-		barometer/dps310
+		#barometer/dps310
 		bootloaders
 		#gps
-		lights/rgbled_ncp5623c
-		magnetometer/rm3100
+		#lights/rgbled_ncp5623c
+		#magnetometer/rm3100
+		distance_sensor/vl53l1x
 		uavcannode
 	MODULES
 	SYSTEMCMDS
