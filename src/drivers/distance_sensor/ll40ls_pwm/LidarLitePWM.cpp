e67df00ea7000dcbc1c5efb21d741096de0d84d8
David Sidrane <David.Sidrane@NscDg.com>
2021-04-21 09:29:36 -0700

    distance_sensor: Use inttypes

--- src/drivers/distance_sensor/ll40ls_pwm/LidarLitePWM.cpp
+++ src/drivers/distance_sensor/ll40ls_pwm/LidarLitePWM.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2014-2019 PX4 Development Team. All rights reserved.
+ *   Copyright (c) 2014-2019, 2021 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -145,7 +145,7 @@ LidarLitePWM::print_info()
 	perf_print_counter(_comms_errors);
 	perf_print_counter(_sensor_resets);
 	perf_print_counter(_sensor_zero_resets);
-	printf("poll interval:  %u \n", get_measure_interval());
+	printf("poll interval:  %" PRIu32 " \n", get_measure_interval());
 }
 
 #endif
