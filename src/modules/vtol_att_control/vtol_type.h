ed369f5ea058a83e7c18269c020b96a928c28544
David Sidrane <David.Sidrane@NscDg.com>
2021-04-22 11:38:38 -0700

    vtol_att_control:Fix type

--- src/modules/vtol_att_control/vtol_type.h
+++ src/modules/vtol_att_control/vtol_type.h
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2015 PX4 Development Team. All rights reserved.
+ *   Copyright (c) 2015, 2021 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -76,7 +76,7 @@ struct Params {
 	float dec_to_pitch_i;
 	float back_trans_dec_sp;
 	bool vt_mc_on_fmu;
-	int vt_forward_thrust_enable_mode;
+	int32_t vt_forward_thrust_enable_mode;
 	float mpc_land_alt1;
 	float mpc_land_alt2;
 };
