85e4172c9b7b51ce632208b90b4ea112df3e0a54
Jukka Laitinen <jukkax@ssrc.tii.ae>
2021-04-30 09:58:07 +0300

    Use atomic<int> type for atomic bool on RISC-V
    The atomic<bool> is not quaranteed to be lock-free, use int instead just in
    case.
    
    Signed-off-by: Jukka Laitinen <jukkax@ssrc.tii.ae>

--- platforms/common/include/px4_platform_common/atomic.h
+++ platforms/common/include/px4_platform_common/atomic.h
@@ -66,7 +66,7 @@ class atomic
 {
 public:
 
-#ifdef __PX4_NUTTX
+#if defined(__PX4_NUTTX)
 	// Ensure that all operations are lock-free, so that 'atomic' can be used from
 	// IRQ handlers. This might not be required everywhere though.
 	static_assert(__atomic_always_lock_free(sizeof(T), 0), "atomic is not lock-free for the given type T");
@@ -183,7 +183,17 @@ private:
 
 using atomic_int = atomic<int>;
 using atomic_int32_t = atomic<int32_t>;
+
+/* On riscv64-unknown-elf atomic<bool> is  not quaranteed to be lock-free
+ * It is unclear whether it is really required.
+ * An optimal solution could be atomic_flag, but it doesn't seem to be available
+ * Just use atomic ints for now, to be safe
+*/
+#if !defined(CONFIG_ARCH_RISCV)
 using atomic_bool = atomic<bool>;
+#else
+using atomic_bool = atomic<int>;
+#endif
 
 } /* namespace px4 */
 
