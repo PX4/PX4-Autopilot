3d22a3efecb08ac3b3136c504ed7b1f494dba8c7
Jukka Laitinen <jukkax@ssrc.tii.ae>
2021-04-30 11:00:22 +0300

    Add mpfs version driver (stub)
    Signed-off-by: Jukka Laitinen <jukkax@ssrc.tii.ae>

---
+++ platforms/nuttx/src/px4/microchip/mpfs/version/board_mcu_version.c
@@ -0,0 +1,139 @@
+/****************************************************************************
+ *
+ *   Copyright (C) 2021 Technology Innovation Institute. All rights reserved.
+ *   Author: @author Jukka Laitinen <jukkax@ssrc.tii.ae>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/**
+ * @file usr_mcu_version.c
+ * Implementation of generic user-space version API
+ */
+
+#include <px4_platform_common/px4_config.h>
+#include <px4_platform_common/defines.h>
+
+#include "board_config.h"
+
+static int hw_version = HW_INFO_INIT_VER;
+static int hw_revision = HW_INFO_INIT_REV;
+static char hw_info[] = HW_INFO_INIT;
+
+__EXPORT const char *board_get_hw_type_name(void)
+{
+	return (const char *) hw_info;
+}
+
+__EXPORT int board_get_hw_version(void)
+{
+	return  hw_version;
+}
+
+__EXPORT int board_get_hw_revision()
+{
+	return  hw_revision;
+}
+
+__EXPORT void board_get_uuid32(uuid_uint32_t uuid_words)
+{
+	/* TODO: This is a stub for userspace build. Use some proper interface
+	 * to fetch the uuid32 from the kernel
+	 */
+	uint32_t chip_uuid[PX4_CPU_UUID_WORD32_LENGTH];
+	memset((uint8_t *)chip_uuid, 0, PX4_CPU_UUID_WORD32_LENGTH * 4);
+
+	for (unsigned i = 0; i < PX4_CPU_UUID_WORD32_LENGTH; i++) {
+		uuid_words[i] = chip_uuid[i];
+	}
+}
+
+int board_mcu_version(char *rev, const char **revstr, const char **errata)
+{
+	/* TODO: This is a stub for userspace build. Use some proper interface
+	 * to fetch the version from the kernel
+	 */
+	const char *str = "PolarFire MPFS250T";
+	*rev = '1';
+	*revstr = str;
+	*errata = 0;
+	return 0;
+}
+
+int board_get_px4_guid(px4_guid_t px4_guid)
+{
+	/* TODO: This is a stub for userspace build. Use some proper interface
+	 * to fetch the guid from the kernel
+	 */
+	uint8_t  *pb = (uint8_t *) &px4_guid[0];
+	memset(pb, 0, PX4_GUID_BYTE_LENGTH);
+
+	return PX4_GUID_BYTE_LENGTH;
+}
+
+int board_get_px4_guid_formated(char *format_buffer, int size)
+{
+	px4_guid_t px4_guid;
+	board_get_px4_guid(px4_guid);
+	int offset  = 0;
+
+	/* size should be 2 per byte + 1 for termination
+	 * So it needs to be odd
+	 */
+	size = size & 1 ? size : size - 1;
+
+	/* Discard from MSD */
+	for (unsigned i = PX4_GUID_BYTE_LENGTH - size / 2; offset < size && i < PX4_GUID_BYTE_LENGTH; i++) {
+		offset += snprintf(&format_buffer[offset], size - offset, "%02x", px4_guid[i]);
+	}
+
+	return offset;
+}
+
+/************************************************************************************
+  * Name: board_determine_hw_info
+ *
+ * Description:
+ *	Uses the HW revision and version detection
+ *
+ * Input Parameters:
+ *   None
+ *
+ * Returned Value:
+ *   0  - on success or negated errono
+ *   1) The values for integer value of this boards hardware revision is set
+ *   2) The integer value of this boards hardware version is set.
+ *   3) hw_info is populated
+ *
+ ************************************************************************************/
+
+int board_determine_hw_info(void)
+{
+	return OK;
+}
