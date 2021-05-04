008fa9c996667168f8400fdc2c3407773e1cf112
Merlinlll <1061886245@qq.com>
2021-05-04 16:21:50 +0800

    TRY

--- src/drivers/adc/ADC.cpp
+++ src/drivers/adc/ADC.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************
- *
+ *AAa
  *   Copyright (C) 2012-2020 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
