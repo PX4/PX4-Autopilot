4a93f7cea1800946cca5fa7024b3c93a471f99b8
AlexKlimaj <alex@arkelectron.com>
2021-05-16 11:21:44 -0600

    Add ARK CAN GPS Board Config

---
+++ boards/ark/can-gps/canbootloader.cmake
@@ -0,0 +1,13 @@
+include (${CMAKE_CURRENT_LIST_DIR}/uavcan_board_identity)
+
+px4_add_board(
+	PLATFORM nuttx
+	VENDOR ark
+	MODEL can-gps
+	LABEL canbootloader
+	TOOLCHAIN arm-none-eabi
+	ARCHITECTURE cortex-m4
+	CONSTRAINED_MEMORY
+	DRIVERS
+		bootloaders
+)
