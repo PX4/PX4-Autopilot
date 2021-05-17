39b9bb6b1d6e9f0ce3a5d4c1ee4a8f2e5cbf3336
Daniel Agar <daniel@agar.ca>
2021-05-17 09:59:21 -0400

    IMU_GYRO_CUTOFF and IMU_DGYRO_CUTOFF increase default slightly

--- src/modules/sensors/vehicle_angular_velocity/imu_gyro_parameters.c
+++ src/modules/sensors/vehicle_angular_velocity/imu_gyro_parameters.c
@@ -80,7 +80,7 @@ PARAM_DEFINE_FLOAT(IMU_GYRO_NF_BW, 20.0f);
 * @reboot_required true
 * @group Sensors
 */
-PARAM_DEFINE_FLOAT(IMU_GYRO_CUTOFF, 30.0f);
+PARAM_DEFINE_FLOAT(IMU_GYRO_CUTOFF, 40.0f);
 
 /**
 * Gyro control data maximum publication rate (inner loop rate)
@@ -122,7 +122,7 @@ PARAM_DEFINE_INT32(IMU_GYRO_RATEMAX, 400);
 * @reboot_required true
 * @group Sensors
 */
-PARAM_DEFINE_FLOAT(IMU_DGYRO_CUTOFF, 20.0f);
+PARAM_DEFINE_FLOAT(IMU_DGYRO_CUTOFF, 30.0f);
 
 /**
 * IMU gyro dynamic notch filtering
