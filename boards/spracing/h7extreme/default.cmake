c3884b5bc1949a2bbdf61c40deef9d0799035152
Daniel Agar <daniel@agar.ca>
2021-05-17 20:26:25 -0400

    fake_imu (formally fake_gyro) updates for testing gyro filtering
     - fake_imu now publishes sine sweeps over 10 seconds
     - accel is also published so that fake IMU can be selected when the only option

--- boards/spracing/h7extreme/default.cmake
+++ boards/spracing/h7extreme/default.cmake
@@ -105,7 +105,7 @@ px4_add_board(
 		work_queue
 	EXAMPLES
 		fake_gps
-		#fake_gyro
+		#fake_imu
 		#fake_magnetometer
 		#fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
 		#hello
