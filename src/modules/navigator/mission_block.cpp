1b671877708098b7c803b940f4a52402bcc64db9
Julian Oes <julian@oes.ch>
2021-05-28 14:58:08 +0200

    navigator: protect against 0 acceptance radius
    This seems to slip in e.g. as part of the VTOL_LAND command.

--- src/modules/navigator/mission_block.cpp
+++ src/modules/navigator/mission_block.cpp
@@ -283,7 +283,7 @@ MissionBlock::is_mission_item_reached()
 			// We use the acceptance radius of the mission item if it has been set (not NAN)
 			// but only for multicopter.
 			if (_navigator->get_vstatus()->vehicle_type == vehicle_status_s::VEHICLE_TYPE_ROTARY_WING
-			    && PX4_ISFINITE(_mission_item.acceptance_radius)) {
+			    && PX4_ISFINITE(_mission_item.acceptance_radius) && _mission_item.acceptance_radius > FLT_EPSILON) {
 				acceptance_radius = _mission_item.acceptance_radius;
 			}
 
