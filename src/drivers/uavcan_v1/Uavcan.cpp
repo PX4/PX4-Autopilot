910ce6fed10edef41af321f857b1f8a44d129bb4
JacobCrabill <jacob@flyvoly.com>
2021-06-07 18:34:44 -0700

    uavcan_v1: Fix comment about port ID params

--- src/drivers/uavcan_v1/Uavcan.cpp
+++ src/drivers/uavcan_v1/Uavcan.cpp
@@ -184,7 +184,7 @@ void UavcanNode::Run()
 
 		for (auto &publisher : _publishers) {
 			// Have the publisher update its associated port-id parameter
-			// Setting to 0 disable publication
+			// Setting to -1 disable publication
 			publisher->updateParam();
 		}
 
