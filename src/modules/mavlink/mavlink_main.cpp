389a096acead50857110dc84ddc97a84b31819e7
Jaeyoung-Lim <jalim@ethz.ch>
2021-06-24 13:15:21 +0200

    Add SENSOR_AIRFLOW_ANGLES mavlink stream to config mode

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -1740,6 +1740,7 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 		configure_stream_local("SCALED_IMU3", 25.0f);
 		configure_stream_local("SERVO_OUTPUT_RAW_0", 20.0f);
 		configure_stream_local("SERVO_OUTPUT_RAW_1", 20.0f);
+		configure_stream_local("SENSOR_AIRFLOW_ANGLES", 20.0f);
 		configure_stream_local("SYS_STATUS", 1.0f);
 		configure_stream_local("SYSTEM_TIME", 1.0f);
 		configure_stream_local("UTM_GLOBAL_POSITION", 1.0f);
