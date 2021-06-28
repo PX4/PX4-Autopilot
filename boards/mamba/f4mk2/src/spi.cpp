8bb6a56cd4c0d264183f5709c762fd96c642efb3
achim <lumserei@web.de>
2021-06-29 00:21:41 +0200

    Update spi.cpp
    format correction

--- boards/mamba/f4mk2/src/spi.cpp
+++ boards/mamba/f4mk2/src/spi.cpp
@@ -40,12 +40,12 @@ constexpr px4_spi_bus_t px4_spi_buses[SPI_BUS_MAX_BUS_ITEMS] = {
 		initSPIDevice(DRV_IMU_DEVTYPE_MPU6000, SPI::CS{GPIO::PortA, GPIO::Pin4}),
 		initSPIDevice(DRV_IMU_DEVTYPE_ICM20602, SPI::CS{GPIO::PortA, GPIO::Pin4}),
 	}),
-    initSPIBus(SPI::Bus::SPI2, {
+    	initSPIBus(SPI::Bus::SPI2, {
         initSPIDevice(DRV_OSD_DEVTYPE_ATXXXX, SPI::CS{GPIO::PortB, GPIO::Pin12}),
-    }),
-    initSPIBus(SPI::Bus::SPI3, {
+    	}),
+    	initSPIBus(SPI::Bus::SPI3, {
         initSPIDevice(SPIDEV_FLASH(0), SPI::CS{GPIO::PortB, GPIO::Pin3}),
-    }),
+    	}),
 };
 
 static constexpr bool unused = validateSPIConfig(px4_spi_buses);
