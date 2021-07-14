6f6d3f98a646c5d3df72d2a17f769faebec8c807
Daniel Agar <daniel@agar.ca>
2021-07-14 17:15:47 -0400

    wind_estimator: move from ecl and consolidate airspeed selector

--- src/lib/wind_estimator/WindEstimator.hpp
+++ src/lib/wind_estimator/WindEstimator.hpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2018 PX4 Development Team. All rights reserved.
+ *   Copyright (c) 2018-2021 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -38,7 +38,6 @@
 
 #pragma once
 
-#include <ecl.h>
 #include <mathlib/mathlib.h>
 #include <matrix/math.hpp>
 
--- src/modules/airspeed_selector/airspeed_selector_main.cpp
+++ src/modules/airspeed_selector/airspeed_selector_main.cpp
@@ -31,8 +31,10 @@
  *
  ****************************************************************************/
 
+#include "AirspeedValidator.hpp"
+
 #include <drivers/drv_hrt.h>
-#include <ecl/airdata/WindEstimator.hpp>
+#include <lib/wind_estimator/WindEstimator.hpp>
 #include <matrix/math.hpp>
 #include <parameters/param.h>
 #include <perf/perf_counter.h>
@@ -40,8 +42,7 @@
 #include <px4_platform_common/module_params.h>
 #include <px4_platform_common/px4_work_queue/ScheduledWorkItem.hpp>
 #include <lib/airspeed/airspeed.h>
-#include <AirspeedValidator.hpp>
-#include <systemlib/mavlink_log.h>
+#include <lib/systemlib/mavlink_log.h>
 
 #include <uORB/Subscription.hpp>
 #include <uORB/SubscriptionMultiArray.hpp>
