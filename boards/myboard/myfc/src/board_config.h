2f8aed982ea34f0c41483822ac7a24376369a663
vxj9800 <vatsalasitkumar.joshi@mavs.uta.edu>
2021-07-15 15:26:38 -0500

    Add board_config.h to provide PX4 side of the configuration information

---
+++ boards/myboard/myfc/src/board_config.h
@@ -0,0 +1,23 @@
+/**
+ * @file board_config.h
+ *
+ * myboardmyfc internal definitions
+ */
+
+#pragma once
+
+/****************************************************************************************************
+ * Included Files
+ ****************************************************************************************************/
+
+#include <px4_platform_common/px4_config.h>
+#include <nuttx/compiler.h>
+#include <stdint.h>
+
+#ifndef __ASSEMBLY__
+
+#include <px4_platform_common/board_common.h>
+
+#endif /* __ASSEMBLY__ */
+
+__END_DECLS
