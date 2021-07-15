6490f37108e0472953fe67b22c615d2af1b709d9
vxj9800 <vatsalasitkumar.joshi@mavs.uta.edu>
2021-07-15 13:29:56 -0500

    Copy clocking definitions from nuttx's pi-pico board's board.h file so that libarch.a can be compiled

--- boards/myboard/myfc/nuttx-config/include/board.h
+++ boards/myboard/myfc/nuttx-config/include/board.h
@@ -10,4 +10,27 @@
 # include <stdint.h>
 #endif
 
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+/* Clocking *****************************************************************/
+
+#define MHZ                     1000000
+
+#define BOARD_XOSC_FREQ         (12 * MHZ)
+#define BOARD_PLL_SYS_FREQ      (125 * MHZ)
+#define BOARD_PLL_USB_FREQ      (48 * MHZ)
+
+#define BOARD_REF_FREQ          (12 * MHZ)
+#define BOARD_SYS_FREQ          (125 * MHZ)
+#define BOARD_PERI_FREQ         (125 * MHZ)
+#define BOARD_USB_FREQ          (48 * MHZ)
+#define BOARD_ADC_FREQ          (48 * MHZ)
+#define BOARD_RTC_FREQ          46875
+
+#define BOARD_UART_BASEFREQ     BOARD_PERI_FREQ
+
+#define BOARD_TICK_CLOCK        (1 * MHZ)
+
 #endif  /* __CONFIG_MYBOARDMYFC_INCLUDE_BOARD_H */
