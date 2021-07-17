12fb5b7f60ec32f07025baec5783bdcea736ba64
Daniel Agar <daniel@agar.ca>
2021-07-16 21:08:08 -0400

    Tools/HIL: fix terminal colors (ANSI escape codes)

--- Tools/HIL/run_tests.py
+++ Tools/HIL/run_tests.py
@@ -9,9 +9,9 @@ import unittest
 import os
 import sys
 
-COLOR_YELLOW = "\x1b[31m"
+COLOR_RED    = "\x1b[31m"
 COLOR_GREEN  = "\x1b[32m"
-COLOR_RED    = "\x1b[33m"
+COLOR_YELLOW = "\x1b[33m"
 COLOR_WHITE  = "\x1b[37m"
 COLOR_RESET  = "\x1b[0m"
 
