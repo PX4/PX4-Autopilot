1af2ecaff6c22957cc41428199685f17c7c22c2f
Daniel Agar <daniel@agar.ca>
2021-08-18 19:31:24 -0400

    boards: px4_fmu-v5_test disable attitude_estimator_q and local_position_estimator to save flash

--- boards/px4/fmu-v5/test.cmake
+++ boards/px4/fmu-v5/test.cmake
@@ -54,7 +54,7 @@ px4_add_board(
 		uavcan
 	MODULES
 		airspeed_selector
-		attitude_estimator_q
+		#attitude_estimator_q
 		battery_status
 		camera_feedback
 		commander
@@ -70,7 +70,7 @@ px4_add_board(
 		land_detector
 		landing_target_estimator
 		load_mon
-		local_position_estimator
+		#local_position_estimator
 		logger
 		mavlink
 		mc_att_control
