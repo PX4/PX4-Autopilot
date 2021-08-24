69021dbafafea4282b32bb3852e42808c2694a11
Jukka Laitinen <jukkax@ssrc.tii.ae>
2021-08-24 13:50:42 +0300

    Increase navigator stack for 64-bit targets
    Signed-off-by: Jukka Laitinen <jukkax@ssrc.tii.ae>

--- src/modules/navigator/navigator_main.cpp
+++ src/modules/navigator/navigator_main.cpp
@@ -857,7 +857,7 @@ int Navigator::task_spawn(int argc, char *argv[])
 	_task_id = px4_task_spawn_cmd("navigator",
 				      SCHED_DEFAULT,
 				      SCHED_PRIORITY_NAVIGATION,
-				      1800,
+				      PX4_STACK_ADJUSTED(1800),
 				      (px4_main_t)&run_trampoline,
 				      (char *const *)argv);
 
