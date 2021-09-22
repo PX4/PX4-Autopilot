3a148bc7772da69286d8e9b41c77f246efb3b526
Igor Mišić <igy1000mb@gmail.com>
2021-09-22 10:57:25 +0200

    camera_trigger: remove camera trigger secondary
    camera_trigger: publish trigger when capture feedback is not enabled

--- src/modules/logger/logged_topics.cpp
+++ src/modules/logger/logged_topics.cpp
@@ -56,7 +56,6 @@ void LoggedTopics::add_default_topics()
 	add_topic("airspeed_validated", 200);
 	add_topic("camera_capture");
 	add_topic("camera_trigger");
-	add_topic("camera_trigger_secondary");
 	add_topic("cellular_status", 200);
 	add_topic("commander_state");
 	add_topic("cpuload");
