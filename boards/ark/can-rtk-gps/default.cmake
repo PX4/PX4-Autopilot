8b44058b993ad3f5eb593021b4650a6b804b8cd0
alexklimaj <alex@arkelectron.com>
2021-09-30 11:32:00 -0600

    Add ARK RTK GPS

---
+++ boards/ark/can-rtk-gps/default.cmake
@@ -0,0 +1,36 @@
+include (${CMAKE_CURRENT_LIST_DIR}/uavcan_board_identity)
+
+px4_add_board(
+	PLATFORM nuttx
+	TOOLCHAIN arm-none-eabi
+	ARCHITECTURE cortex-m4
+	#CONSTRAINED_FLASH
+	CONSTRAINED_MEMORY
+	EXTERNAL_METADATA
+	ROMFSROOT cannode
+	UAVCAN_INTERFACES 1
+	DRIVERS
+		barometer/bmp388
+		bootloaders
+		gps
+		imu/invensense/icm42688p
+		magnetometer/bosch/bmm150
+		safety_button
+		tone_alarm
+		uavcannode
+	MODULES
+		#ekf2
+		#load_mon
+		#sensors
+	SYSTEMCMDS
+		#mft
+		#mtd
+		param
+		#perf
+		#reboot
+		#system_time
+		#top
+		#topic_listener
+		#ver
+		#work_queue
+)
