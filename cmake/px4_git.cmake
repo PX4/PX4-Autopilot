a0a0a3e98353b163f15870c14b0f54f5eff276b2
Daniel Agar <daniel@agar.ca>
2021-10-01 14:51:23 -0400

    cmake: git check submodules updates

--- cmake/px4_git.cmake
+++ cmake/px4_git.cmake
@@ -62,30 +62,120 @@ function(px4_add_git_submodule)
 		REQUIRED TARGET PATH
 		ARGN ${ARGN})
 
-	set(REL_PATH)
+	option(GIT_SUBMODULE "Check submodules during build" ON)
 
-	if(IS_ABSOLUTE ${PATH})
-		file(RELATIVE_PATH REL_PATH ${PX4_SOURCE_DIR} ${PATH})
-	else()
-		file(RELATIVE_PATH REL_PATH ${PX4_SOURCE_DIR} ${CMAKE_CURRENT_SOURCE_DIR}/${PATH})
+	# ENV GIT_SUBMODULES_ARE_EVIL
+
+	# ENV TERM_PROGRAM="vscode"
+	# ENV VSCODE_GIT_ASKPASS_MAIN
+	# ENV VSCODE_GIT_ASKPASS_NODE
+	# ENV VSCODE_GIT_IPC_HANDLE
+	# ENV VSCODE_IPC_HOOK_CLI
+
+	# if CI or vscode then force submodule update?
+
+	if(DEFINED ENV{VSCODE_IPC_HOOK_CLI})
+		#message(STATUS "VSCODE_IPC_HOOK_CLI: $ENV{VSCODE_IPC_HOOK_CLI}")
+
+		#message(STATUS "TERM_PROGRAM: $ENV{TERM_PROGRAM}")
+
+		#message(STATUS "VSCODE_GIT_ASKPASS_MAIN: $ENV{VSCODE_GIT_ASKPASS_MAIN}")
+		#message(STATUS "VSCODE_GIT_ASKPASS_NODE: $ENV{VSCODE_GIT_ASKPASS_NODE}")
+		#message(STATUS "VSCODE_GIT_IPC_HANDLE: $ENV{VSCODE_GIT_IPC_HANDLE}")
 	endif()
 
-	execute_process(
-		COMMAND Tools/check_submodules.sh ${REL_PATH}
-		WORKING_DIRECTORY ${PX4_SOURCE_DIR}
-		)
+	if(GIT_FOUND AND GIT_SUBMODULE AND NOT DEFINED ENV{GIT_SUBMODULES_ARE_EVIL})
 
-	string(REPLACE "/" "_" NAME ${PATH})
-	string(REPLACE "." "_" NAME ${NAME})
+		set(REL_PATH)
 
-	add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/git_init_${NAME}.stamp
-		COMMAND Tools/check_submodules.sh ${REL_PATH}
-		COMMAND ${CMAKE_COMMAND} -E touch ${CMAKE_CURRENT_BINARY_DIR}/git_init_${NAME}.stamp
-		DEPENDS ${PX4_SOURCE_DIR}/.gitmodules ${PATH}/.git
-		COMMENT "git submodule ${REL_PATH}"
-		WORKING_DIRECTORY ${PX4_SOURCE_DIR}
-		USES_TERMINAL
-		)
+		if(IS_ABSOLUTE ${PATH})
+			file(RELATIVE_PATH REL_PATH ${PX4_SOURCE_DIR} ${PATH})
+		else()
+			file(RELATIVE_PATH REL_PATH ${PX4_SOURCE_DIR} ${CMAKE_CURRENT_SOURCE_DIR}/${PATH})
+		endif()
 
-	add_custom_target(${TARGET} DEPENDS git_init_${NAME}.stamp)
+		if(NOT EXISTS ${PATH}/.git)
+			execute_process(
+				COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive --jobs=4 -- ${REL_PATH}
+				OUTPUT_VARIABLE GIT_SUBMODULE_UPDATE
+				RESULT_VARIABLE GIT_SUBMODULE_UPDATE_RESULT
+				WORKING_DIRECTORY ${PX4_SOURCE_DIR}
+			)
+			if(NOT GIT_SUBMODULE_UPDATE_RESULT EQUAL "0")
+				message(FATAL_ERROR "git submodule update --init --recursive -- ${REL_PATH} failed: ${GIT_SUBMODULE_UPDATE}")
+			endif()
+
+		else()
+			execute_process(
+				COMMAND ${GIT_EXECUTABLE} submodule status -- ${REL_PATH}
+				OUTPUT_VARIABLE GIT_SUBMODULE_STATUS
+				RESULT_VARIABLE GIT_SUBMODULE_STATUS_RESULT
+				WORKING_DIRECTORY ${PX4_SOURCE_DIR}
+			)
+			#message(STATUS "git submodule status ${REL_PATH} result ${GIT_SUBMODULE_STATUS_RESULT} : ${GIT_SUBMODULE_STATUS}")
+
+			if(NOT GIT_SUBMODULE_STATUS_RESULT EQUAL "0")
+				message(FATAL_ERROR "git submodule status -- ${REL_PATH} failed: ${GIT_SUBMODULE_STATUS}")
+			else()
+				# submodule status characters
+				string(FIND "${GIT_SUBMODULE_STATUS}" "+" submodule_plus)
+				string(FIND "${GIT_SUBMODULE_STATUS}" "-" submodule_minus)
+				string(FIND "${GIT_SUBMODULE_STATUS}" "U" submodule_merge_conflicts)
+
+				# + commit does not match the SHA-1 found in the index
+				if("${submodule_plus}" EQUAL 0)
+					# GIT_SHA_HEAD: submodule git SHA on current HEAD
+					execute_process(
+						COMMAND ${GIT_EXECUTABLE} ls-tree -d HEAD ${REL_PATH} | awk '{print $3;}
+						OUTPUT_VARIABLE GIT_SHA_HEAD
+						WORKING_DIRECTORY ${PX4_SOURCE_DIR}
+					)
+
+					# GIT_SHA_PREV_HEAD: submodule git SHA on previous HEAD
+					execute_process(
+						COMMAND ${GIT_EXECUTABLE} ls-tree -d HEAD@{1} ${REL_PATH} | awk '{print $3;}
+						OUTPUT_VARIABLE GIT_SHA_PREV_HEAD
+						WORKING_DIRECTORY ${PX4_SOURCE_DIR}
+					)
+
+					#message(STATUS "GIT_SHA_HEAD: ${GIT_SHA_HEAD}")
+					#message(STATUS "GIT_SHA_PREV_HEAD: ${GIT_SHA_PREV_HEAD}")
+
+					# TODO: check if submodule is dirty?
+
+					if(${GIT_SHA_HEAD} MATCHES ${GIT_SHA_PREV_HEAD})
+						#message(STATUS "${rel_path} GIT SHA matched previous")
+						# submodule hasn't changed, assume it's safe to update
+						execute_process(
+							COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive --jobs=4 -- ${REL_PATH}
+							OUTPUT_VARIABLE GIT_SUBMODULE_UPDATE
+							WORKING_DIRECTORY ${PX4_SOURCE_DIR}
+						)
+					else()
+						message(NOTICE "submodule ${REL_PATH} commit does not match the SHA-1 found in the index")
+					endif()
+				endif()
+
+				# - uninitialized
+				if("${submodule_minus}" EQUAL 0)
+					execute_process(
+						COMMAND ${GIT_EXECUTABLE} submodule update --init --recursive --jobs=4 -- ${REL_PATH}
+						OUTPUT_VARIABLE GIT_SUBMODULE_UPDATE
+						WORKING_DIRECTORY ${PX4_SOURCE_DIR}
+					)
+				endif()
+
+				# U merge conflicts
+				if("${submodule_merge_conflicts}" EQUAL 0)
+					message(NOTICE "submodule ${REL_PATH} has merge conflicts")
+				endif()
+			endif()
+		endif()
+
+
+		add_custom_target(${TARGET})
+
+		set_property(DIRECTORY APPEND PROPERTY CMAKE_CONFIGURE_DEPENDS ${PATH}/.git)
+
+	endif()
 endfunction()
