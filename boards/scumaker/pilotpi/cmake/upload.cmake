c91ce177a4faf4d7b36a26266688847a838da162
SalimTerryLi <lhf2613@gmail.com>
2021-10-11 12:15:06 +0800

    pilotpi: fix upload cmake

--- boards/scumaker/pilotpi/cmake/upload.cmake
+++ boards/scumaker/pilotpi/cmake/upload.cmake
@@ -46,7 +46,7 @@ endif()
 add_custom_target(upload
 	COMMAND rsync -arh --progress
 			${CMAKE_RUNTIME_OUTPUT_DIRECTORY} ${PX4_SOURCE_DIR}/posix-configs/rpi/pilotpi*.config ${PX4_BINARY_DIR}/etc # source
-			\$\{AUTOPILOT_USER\}@\$\{AUTOPILOT_HOST\}:/home/\$\{AUTOPILOT_USER\}/px4 # destination
+			"${AUTOPILOT_USER}@${AUTOPILOT_HOST}:/home/${AUTOPILOT_USER}/px4" # destination
 	DEPENDS px4
 	COMMENT "uploading px4"
 	USES_TERMINAL
