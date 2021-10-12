ee52b6caae62b753a909c168a133f0bfbeec0fc4
Silvan Fuhrer <silvan@auterion.com>
2021-10-12 23:38:15 +0200

    AirspeedValidator: check_airspeed_innovation() check absolute innovations
    Do no longer use tas_innovation from wind estimator and test ratio, but calculate
    the innovation  based on wind estimate, TAS measurement (including currently applied scale)
    and ground velocity. Use innovations directly to trigger failure.
    
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- src/modules/airspeed_selector/AirspeedValidator.hpp
+++ src/modules/airspeed_selector/AirspeedValidator.hpp
@@ -178,7 +178,7 @@ private:
 	void update_CAS_TAS(float air_pressure_pa, float air_temperature_celsius);
 	void check_airspeed_data_stuck(uint64_t timestamp);
 	void check_airspeed_innovation(uint64_t timestamp, float estimator_status_vel_test_ratio,
-				       float estimator_status_mag_test_ratio);
+				       float estimator_status_mag_test_ratio, matrix::Vector3f vI);
 	void check_load_factor(float accel_z);
 	void update_airspeed_valid_status(const uint64_t timestamp);
 	void reset();
