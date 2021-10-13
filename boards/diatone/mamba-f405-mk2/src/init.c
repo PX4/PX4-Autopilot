ea62697d43d2ec2ec3137277091a55328dcbcd94
achim <lumserei@web.de>
2021-10-13 13:09:29 +0200

    Update init.c
    corrected names

--- boards/diatone/mamba-f405-mk2/src/init.c
+++ boards/diatone/mamba-f405-mk2/src/init.c
@@ -34,7 +34,7 @@
 /**
  * @file init.c
  *
- * omnibusf4sd-specific early startup code.  This file implements the
+ * mambaf405mk2-specific early startup code.  This file implements the
  * board_app_initialize() function that is called early by nsh during startup.
  *
  * Code here is run before the rcS script is invoked; it should start required
