917282eeec6b8876e333b30c899eb76ca99ff47d
alexklimaj <alex@arkelectron.com>
2021-10-19 10:56:25 -0600

    Add ARK RTK GPS Debug, turn on UART RX DMA

--- boards/ark/can-rtk-gps/nuttx-config/include/board_dma_map.h
+++ boards/ark/can-rtk-gps/nuttx-config/include/board_dma_map.h
@@ -39,5 +39,8 @@
 
 //  DMA2 Channel/Stream Selections
 //--------------------------------------------//---------------------------//----------------
-#define DMACHAN_SPI1_RX    DMAMAP_SPI1_RX_2   // DMA2, Stream 2, Channel 3
-#define DMACHAN_SPI1_TX    DMAMAP_SPI1_TX_1   // DMA2, Stream 5, Channel 3
+#define DMACHAN_SPI1_RX    DMAMAP_SPI1_RX_1   // DMA2, Stream 0, Channel 3
+#define DMACHAN_SPI1_TX    DMAMAP_SPI1_TX_1   // DMA2, Stream 3, Channel 3
+#define DMACHAN_USART1_RX  DMAMAP_USART1_RX_1 // DMA2, Stream 2, Channel 4
+#define DMAMAP_USART1_RX   DMAMAP_USART1_RX_1
+//#define DMACHAN_USART1_TX  DMAMAP_USART1_TX   // DMA2, Stream 7, Channel 4
