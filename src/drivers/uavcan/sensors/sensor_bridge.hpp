9ecaf94f48ff0f3072546835ca13e4b33eccca49
Daniel Agar <daniel@agar.ca>
2021-10-19 19:07:12 -0400

    drivers/uavcan: add new UAVCAN_SUB_* parameters to enable subscriptions
     - only GPS and mag are enabled by default

--- src/drivers/uavcan/sensors/sensor_bridge.hpp
+++ src/drivers/uavcan/sensors/sensor_bridge.hpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2014, 2015 PX4 Development Team. All rights reserved.
+ *   Copyright (c) 2014-2021 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
