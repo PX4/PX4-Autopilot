2716ce7a56589ca504d9e354c74ccd7efb4bef9b
Matthias Grob <maetugr@gmail.com>
2021-10-21 14:04:53 +0200

    pre-commit hook: clear output with error, file name, diff, instructions

--- Tools/astyle/check_code_style.sh
+++ Tools/astyle/check_code_style.sh
@@ -8,16 +8,16 @@ if [ -f "$FILE" ]; then
 	if [ -n "$CHECK_FAILED" ]; then
 		${DIR}/fix_code_style.sh --quiet < $FILE > $FILE.pretty
 
+		echo -e 'Formatting issue found in' $FILE
 		echo
-		git --no-pager diff --no-index --minimal --histogram --color=always $FILE $FILE.pretty
+		git --no-pager diff --no-index --minimal --histogram --color=always $FILE $FILE.pretty | grep -vE -e "^.{,4}diff.*\.pretty.{,3}$" -e "^.{,4}--- a/.*$" -e "^.{,4}\+\+\+ b/.*$" -e "^.{,5}@@ .* @@.*$" -e "^.{,4}index .{10}\.\."
 		rm -f $FILE.pretty
 		echo
 
 		if [[ $PX4_ASTYLE_FIX -eq 1 ]]; then
 			${DIR}/fix_code_style.sh $FILE
 		else
-			# Provide instructions
-			echo $FILE 'bad formatting, please run "make format" or "./Tools/astyle/fix_code_style.sh' $FILE'"'
+			echo 'to fix automatically run "make format" or "./Tools/astyle/fix_code_style.sh' $FILE'"'
 			exit 1
 		fi
 	fi
