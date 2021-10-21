7ff254b798a61b1e1bbf67b580da06faefdbcd31
Matthias Grob <maetugr@gmail.com>
2021-10-21 14:02:51 +0200

    pre-commit hook: don't unstage the entire file containing a style issue

--- Tools/astyle/check_code_style.sh
+++ Tools/astyle/check_code_style.sh
@@ -16,8 +16,6 @@ if [ -f "$FILE" ]; then
 		if [[ $PX4_ASTYLE_FIX -eq 1 ]]; then
 			${DIR}/fix_code_style.sh $FILE
 		else
-			# Make sure this file is not staged for comitting
-			git reset $FILE
 			# Provide instructions
 			echo $FILE 'bad formatting, please run "make format" or "./Tools/astyle/fix_code_style.sh' $FILE'"'
 			exit 1
