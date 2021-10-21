e40aaf97106d39dcbd9b96cd8a0505146006d076
achim <lumserei@web.de>
2021-10-21 19:22:38 +0200

    Add files via upload
    the path to the files doesn´t match yet

---
+++ boards/mro/ctrl-zero-classic/src/hw_description_stm_common.h
@@ -0,0 +1,315 @@
+/****************************************************************************
+ *
+ *   Copyright (C) 2019 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *	notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *	notice, this list of conditions and the following disclaimer in
+ *	the documentation and/or other materials provided with the
+ *	distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *	used to endorse or promote products derived from this software
+ *	without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#pragma once
+
+#include <stdint.h>
+
+#include <stm32_gpio.h>
+#include <stm32_tim.h>
+#include <stm32_dma.h>
+
+#include <px4_platform_common/constexpr_util.h>
+
+/*
+ * DMA
+ */
+
+struct DMA {
+	enum Index { Invalid = 0, Index1 = 1, Index2 };
+	Index index;
+	enum Stream { Stream0, Stream1, Stream2, Stream3, Stream4, Stream5, Stream6, Stream7 };
+	Stream stream;
+	enum Channel { Channel0, Channel1, Channel2, Channel3, Channel4, Channel5, Channel6, Channel7 };
+	Channel channel;
+};
+
+static inline constexpr uint32_t getDMABaseRegister(const DMA &dma)
+{
+	switch (dma.index) {
+	case DMA::Index1: return STM32_DMA1_BASE;
+
+	case DMA::Index2: return STM32_DMA2_BASE;
+
+	case DMA::Invalid:
+		constexpr_assert(false, "Trying to get DMA base register for invalid config");
+		break;
+	}
+
+	return 0;
+}
+
+
+/*
+ * Timers
+ */
+
+namespace Timer
+{
+enum Timer {
+	Timer1 = 1,
+	Timer2,
+	Timer3,
+	Timer4,
+	Timer5,
+	Timer6,
+	Timer7,
+	Timer8,
+	Timer9,
+	Timer10,
+	Timer11,
+	Timer12,
+	Timer13,
+	Timer14,
+	Timer15,
+};
+enum Channel {
+	Channel1 = 1,
+	Channel2,
+	Channel3,
+	Channel4,
+};
+struct TimerChannel {
+	Timer timer;
+	Channel channel;
+};
+}
+
+static inline constexpr uint32_t timerBaseRegister(Timer::Timer timer)
+{
+	switch (timer) {
+	case Timer::Timer1: return STM32_TIM1_BASE;
+
+	case Timer::Timer2: return STM32_TIM2_BASE;
+
+	case Timer::Timer3: return STM32_TIM3_BASE;
+
+	case Timer::Timer4: return STM32_TIM4_BASE;
+
+	case Timer::Timer5: return STM32_TIM5_BASE;
+
+	case Timer::Timer6: return STM32_TIM6_BASE;
+
+	case Timer::Timer7: return STM32_TIM7_BASE;
+
+	case Timer::Timer8: return STM32_TIM8_BASE;
+
+#ifdef STM32_TIM9_BASE // STM32F1 only goes up to TIM8
+
+	case Timer::Timer9: return STM32_TIM9_BASE;
+#endif
+
+#ifdef STM32_TIM10_BASE
+
+	case Timer::Timer10: return STM32_TIM10_BASE;
+#endif
+
+#ifdef STM32_TIM11_BASE
+
+	case Timer::Timer11: return STM32_TIM11_BASE;
+#endif
+
+#ifdef STM32_TIM12_BASE
+
+	case Timer::Timer12: return STM32_TIM12_BASE;
+#endif
+
+#ifdef STM32_TIM13_BASE
+
+	case Timer::Timer13: return STM32_TIM13_BASE;
+#endif
+
+#ifdef STM32_TIM14_BASE
+
+	case Timer::Timer14: return STM32_TIM14_BASE;
+#endif
+
+#ifdef STM32_TIM15_BASE
+
+	case Timer::Timer15: return STM32_TIM15_BASE;
+#endif	
+
+	default: break;
+	}
+
+	return 0;
+}
+
+
+/*
+ * GPIO
+ */
+
+namespace GPIO
+{
+enum Port {
+	PortInvalid = 0,
+	PortA,
+	PortB,
+	PortC,
+	PortD,
+	PortE,
+	PortF,
+	PortG,
+	PortH,
+	PortI,
+	PortJ,
+	PortK,
+};
+enum Pin {
+	Pin0 = 0,
+	Pin1,
+	Pin2,
+	Pin3,
+	Pin4,
+	Pin5,
+	Pin6,
+	Pin7,
+	Pin8,
+	Pin9,
+	Pin10,
+	Pin11,
+	Pin12,
+	Pin13,
+	Pin14,
+	Pin15,
+};
+struct GPIOPin {
+	Port port;
+	Pin pin;
+};
+}
+
+static inline constexpr uint32_t getGPIOPort(GPIO::Port port)
+{
+	switch (port) {
+	case GPIO::PortA: return GPIO_PORTA;
+
+	case GPIO::PortB: return GPIO_PORTB;
+
+	case GPIO::PortC: return GPIO_PORTC;
+
+	case GPIO::PortD: return GPIO_PORTD;
+
+	case GPIO::PortE: return GPIO_PORTE;
+#ifdef GPIO_PORTF
+
+	case GPIO::PortF: return GPIO_PORTF;
+#endif
+#ifdef GPIO_PORTG
+
+	case GPIO::PortG: return GPIO_PORTG;
+#endif
+#ifdef GPIO_PORTH
+
+	case GPIO::PortH: return GPIO_PORTH;
+#endif
+#ifdef GPIO_PORTI
+
+	case GPIO::PortI: return GPIO_PORTI;
+#endif
+#ifdef GPIO_PORTJ
+
+	case GPIO::PortJ: return GPIO_PORTJ;
+#endif
+#ifdef GPIO_PORTK
+
+	case GPIO::PortK: return GPIO_PORTK;
+#endif
+
+	default: break;
+	}
+
+	return 0;
+}
+
+static inline constexpr uint32_t getGPIOPin(GPIO::Pin pin)
+{
+	switch (pin) {
+	case GPIO::Pin0: return GPIO_PIN0;
+
+	case GPIO::Pin1: return GPIO_PIN1;
+
+	case GPIO::Pin2: return GPIO_PIN2;
+
+	case GPIO::Pin3: return GPIO_PIN3;
+
+	case GPIO::Pin4: return GPIO_PIN4;
+
+	case GPIO::Pin5: return GPIO_PIN5;
+
+	case GPIO::Pin6: return GPIO_PIN6;
+
+	case GPIO::Pin7: return GPIO_PIN7;
+
+	case GPIO::Pin8: return GPIO_PIN8;
+
+	case GPIO::Pin9: return GPIO_PIN9;
+
+	case GPIO::Pin10: return GPIO_PIN10;
+
+	case GPIO::Pin11: return GPIO_PIN11;
+
+	case GPIO::Pin12: return GPIO_PIN12;
+
+	case GPIO::Pin13: return GPIO_PIN13;
+
+	case GPIO::Pin14: return GPIO_PIN14;
+
+	case GPIO::Pin15: return GPIO_PIN15;
+	}
+
+	return 0;
+}
+
+namespace SPI
+{
+
+enum class Bus {
+	SPI1 = 1,
+	SPI2,
+	SPI3,
+	SPI4,
+	SPI5,
+	SPI6,
+};
+
+using CS = GPIO::GPIOPin; ///< chip-select pin
+using DRDY = GPIO::GPIOPin; ///< data ready pin
+
+struct bus_device_external_cfg_t {
+	CS cs_gpio;
+	DRDY drdy_gpio;
+};
+
+} // namespace SPI
---
+++ boards/mro/ctrl-zero-classic/src/io_timer_hw_description.h
@@ -0,0 +1,289 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2019 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+#pragma once
+
+#include <px4_arch/io_timer.h>
+#include <px4_arch/hw_description.h>
+#include <px4_platform_common/constexpr_util.h>
+#include <px4_platform_common/px4_config.h>
+#include <px4_platform/io_timer_init.h>
+#include <stm32_tim.h>
+
+static inline constexpr timer_io_channels_t initIOTimerGPIOInOut(Timer::TimerChannel timer, GPIO::GPIOPin pin)
+{
+	timer_io_channels_t ret{};
+	uint32_t gpio_af = 0;
+
+	switch (timer.timer) {
+	case Timer::Timer1:
+	case Timer::Timer2:
+		gpio_af = GPIO_AF1;
+		break;
+
+	case Timer::Timer3:
+	case Timer::Timer4:
+	case Timer::Timer5:
+	case Timer::Timer12:
+		gpio_af = GPIO_AF2;
+		break;
+
+	case Timer::Timer6:
+	case Timer::Timer7:
+	case Timer::Timer8:
+	case Timer::Timer9:
+	case Timer::Timer10:
+	case Timer::Timer11:
+		gpio_af = GPIO_AF3;
+		break;
+
+	case Timer::Timer15:
+		gpio_af = GPIO_AF4;
+		break;	
+
+	case Timer::Timer13:
+	case Timer::Timer14:
+		gpio_af = GPIO_AF9;
+		break;
+	}
+
+	uint32_t pin_port = getGPIOPort(pin.port) | getGPIOPin(pin.pin);
+	ret.gpio_in = gpio_af | (GPIO_ALT | GPIO_SPEED_50MHz | GPIO_FLOAT) | pin_port;
+	ret.gpio_out = gpio_af | (GPIO_ALT | GPIO_SPEED_50MHz | GPIO_PUSHPULL) | pin_port;
+	return ret;
+}
+
+
+
+static inline constexpr timer_io_channels_t initIOTimerChannel(const io_timers_t io_timers_conf[MAX_IO_TIMERS],
+		Timer::TimerChannel timer, GPIO::GPIOPin pin)
+{
+	timer_io_channels_t ret = initIOTimerGPIOInOut(timer, pin);
+
+	switch (timer.channel) {
+	case Timer::Channel1:
+		ret.ccr_offset = STM32_GTIM_CCR1_OFFSET;
+		ret.masks = GTIM_SR_CC1IF | GTIM_SR_CC1OF;
+		ret.timer_channel = 1;
+		break;
+
+	case Timer::Channel2:
+		ret.ccr_offset = STM32_GTIM_CCR2_OFFSET;
+		ret.masks = GTIM_SR_CC2IF | GTIM_SR_CC2OF;
+		ret.timer_channel = 2;
+		break;
+
+	case Timer::Channel3:
+		ret.ccr_offset = STM32_GTIM_CCR3_OFFSET;
+		ret.masks = GTIM_SR_CC3IF | GTIM_SR_CC3OF;
+		ret.timer_channel = 3;
+		break;
+
+	case Timer::Channel4:
+		ret.ccr_offset = STM32_GTIM_CCR4_OFFSET;
+		ret.masks = GTIM_SR_CC4IF | GTIM_SR_CC4OF;
+		ret.timer_channel = 4;
+		break;
+	}
+
+	// find timer index
+	ret.timer_index = 0xff;
+	const uint32_t timer_base = timerBaseRegister(timer.timer);
+
+	for (int i = 0; i < MAX_IO_TIMERS; ++i) {
+		if (io_timers_conf[i].base == timer_base) {
+			ret.timer_index = i;
+			break;
+		}
+	}
+
+	constexpr_assert(ret.timer_index != 0xff, "Timer not found");
+
+	return ret;
+}
+
+static inline constexpr io_timers_t initIOTimer(Timer::Timer timer, DMA dshot_dma = {})
+{
+	bool nuttx_config_timer_enabled = false;
+	io_timers_t ret{};
+
+	switch (timer) {
+	case Timer::Timer1:
+		ret.base = STM32_TIM1_BASE;
+		ret.clock_register = STM32_RCC_APB2ENR;
+		ret.clock_bit = RCC_APB2ENR_TIM1EN;
+		ret.clock_freq = STM32_APB2_TIM1_CLKIN;
+		ret.vectorno =  STM32_IRQ_TIMCC;
+#ifdef CONFIG_STM32_TIM1
+		nuttx_config_timer_enabled = true;
+#endif
+		break;
+
+	case Timer::Timer2:
+		ret.base = STM32_TIM2_BASE;
+		ret.clock_register = STM32_RCC_APB1LENR;
+		ret.clock_bit = RCC_APB1LENR_TIM2EN;
+		ret.clock_freq = STM32_APB1_TIM2_CLKIN;
+		ret.vectorno =  STM32_IRQ_TIM2;
+#ifdef CONFIG_STM32_TIM2
+		nuttx_config_timer_enabled = true;
+#endif
+		break;
+
+	case Timer::Timer3:
+		ret.base = STM32_TIM3_BASE;
+		ret.clock_register = STM32_RCC_APB1LENR;
+		ret.clock_bit = RCC_APB1LENR_TIM3EN;
+		ret.clock_freq = STM32_APB1_TIM3_CLKIN;
+		ret.vectorno =  STM32_IRQ_TIM3;
+#ifdef CONFIG_STM32_TIM3
+		nuttx_config_timer_enabled = true;
+#endif
+		break;
+
+	case Timer::Timer4:
+		ret.base = STM32_TIM4_BASE;
+		ret.clock_register = STM32_RCC_APB1LENR;
+		ret.clock_bit = RCC_APB1LENR_TIM4EN;
+		ret.clock_freq = STM32_APB1_TIM4_CLKIN;
+		ret.vectorno =  STM32_IRQ_TIM4;
+#ifdef CONFIG_STM32_TIM4
+		nuttx_config_timer_enabled = true;
+#endif
+		break;
+
+	case Timer::Timer5:
+		ret.base = STM32_TIM5_BASE;
+		ret.clock_register = STM32_RCC_APB1LENR;
+		ret.clock_bit = RCC_APB1LENR_TIM5EN;
+		ret.clock_freq = STM32_APB1_TIM5_CLKIN;
+		ret.vectorno =  STM32_IRQ_TIM5;
+#ifdef CONFIG_STM32_TIM5
+		nuttx_config_timer_enabled = true;
+#endif
+		break;
+
+	case Timer::Timer6:
+		ret.base = STM32_TIM6_BASE;
+		ret.clock_register = STM32_RCC_APB1LENR;
+		ret.clock_bit = RCC_APB1LENR_TIM6EN;
+		ret.clock_freq = STM32_APB1_TIM6_CLKIN;
+		ret.vectorno =  STM32_IRQ_TIM6;
+#ifdef CONFIG_STM32_TIM6
+		nuttx_config_timer_enabled = true;
+#endif
+		break;
+
+	case Timer::Timer7:
+		ret.base = STM32_TIM7_BASE;
+		ret.clock_register = STM32_RCC_APB1LENR;
+		ret.clock_bit = RCC_APB1LENR_TIM7EN;
+		ret.clock_freq = STM32_APB1_TIM7_CLKIN;
+		ret.vectorno =  STM32_IRQ_TIM7;
+#ifdef CONFIG_STM32_TIM7
+		nuttx_config_timer_enabled = true;
+#endif
+		break;
+
+	case Timer::Timer8:
+		ret.base = STM32_TIM8_BASE;
+		ret.clock_register = STM32_RCC_APB2ENR;
+		ret.clock_bit = RCC_APB2ENR_TIM8EN;
+		ret.clock_freq = STM32_APB2_TIM8_CLKIN;
+		ret.vectorno =  STM32_IRQ_TIM8CC;
+#ifdef CONFIG_STM32_TIM8
+		nuttx_config_timer_enabled = true;
+#endif
+		break;
+
+	case Timer::Timer9:
+	case Timer::Timer10:
+	case Timer::Timer11:
+		constexpr_assert(false, "Invalid Timer");
+		break;
+
+	case Timer::Timer12:
+		ret.base = STM32_TIM12_BASE;
+		ret.clock_register = STM32_RCC_APB1LENR;
+		ret.clock_bit = RCC_APB1LENR_TIM12EN;
+		ret.clock_freq = STM32_APB1_TIM12_CLKIN;
+		ret.vectorno =  STM32_IRQ_TIM12;
+#ifdef CONFIG_STM32_TIM12
+		nuttx_config_timer_enabled = true;
+#endif
+		break;
+
+	case Timer::Timer13:
+		ret.base = STM32_TIM13_BASE;
+		ret.clock_register = STM32_RCC_APB1LENR;
+		ret.clock_bit = RCC_APB1LENR_TIM13EN;
+		ret.clock_freq = STM32_APB1_TIM13_CLKIN;
+		ret.vectorno =  STM32_IRQ_TIM13;
+#ifdef CONFIG_STM32_TIM13
+		nuttx_config_timer_enabled = true;
+#endif
+		break;
+
+	case Timer::Timer14:
+		ret.base = STM32_TIM14_BASE;
+		ret.clock_register = STM32_RCC_APB1LENR;
+		ret.clock_bit = RCC_APB1LENR_TIM14EN;
+		ret.clock_freq = STM32_APB1_TIM14_CLKIN;
+		ret.vectorno =  STM32_IRQ_TIM14;
+#ifdef CONFIG_STM32_TIM14
+		nuttx_config_timer_enabled = true;
+#endif
+		break;
+
+	case Timer::Timer15:
+		ret.base = STM32_TIM15_BASE;
+		ret.clock_register = STM32_RCC_APB2ENR;
+		ret.clock_bit = RCC_APB2LENR_TIM15EN;
+		ret.clock_freq = STM32_APB2_TIM15_CLKIN;
+		ret.vectorno =  STM32_IRQ_TIM15;
+#ifdef CONFIG_STM32_TIM15
+		nuttx_config_timer_enabled = true;
+#endif
+		break;		
+	}
+
+	// This is not strictly required, but for consistency let's make sure NuttX timers are disabled
+	constexpr_assert(!nuttx_config_timer_enabled, "IO Timer requires NuttX timer config to be disabled (STM32_TIMx)");
+
+	// DShot
+	if (dshot_dma.index != DMA::Invalid) {
+		ret.dshot.dma_base = getDMABaseRegister(dshot_dma);
+		ret.dshot.dmamap = getTimerUpdateDMAMap(timer, dshot_dma);
+	}
+
+	return ret;
+}
