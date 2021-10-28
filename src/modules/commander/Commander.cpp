3947ad3a311461d04ecd9996fb0534b64c357583
Thomas Debrunner <thomas.debrunner@auterion.com>
2021-10-28 15:43:54 +0200

    commander: Only run estimator  navigation checks when armed

--- src/modules/commander/Commander.cpp
+++ src/modules/commander/Commander.cpp
@@ -3912,8 +3912,7 @@ void Commander::estimator_check()
 
 		if (run_quality_checks && _status.vehicle_type == vehicle_status_s::VEHICLE_TYPE_ROTARY_WING) {
 
-			if (_status.arming_state == vehicle_status_s::ARMING_STATE_STANDBY
-			    || _status.arming_state == vehicle_status_s::ARMING_STATE_INIT) {
+			if (_status.arming_state != vehicle_status_s::ARMING_STATE_ARMED) {
 				_nav_test_failed = false;
 				_nav_test_passed = false;
 
