7014113b2c35c51c1313c3e7bd69061ae1b26c8f
vvladic <viktor.vladic@gmail.com>
2021-11-01 13:23:30 +0100

    Changes for pull request  #18544

--- boards/matek/h743-slim/src/spi.cpp
+++ boards/matek/h743-slim/src/spi.cpp
@@ -48,7 +48,7 @@ constexpr px4_spi_bus_t px4_spi_buses[SPI_BUS_MAX_BUS_ITEMS] = {
 		initSPIConfigExternal(SPI::CS{GPIO::PortE, GPIO::Pin2}),
 	}),
 	initSPIBus(SPI::Bus::SPI4, {
-		initSPIDevice(DRV_IMU_DEVTYPE_ICM20602, SPI::CS{GPIO::PortE, GPIO::Pin11}, SPI::DRDY{GPIO::PortE, GPIO::Pin15}),
+		initSPIDevice(DRV_IMU_DEVTYPE_ICM20602, SPI::CS{GPIO::PortE, GPIO::Pin11}),
 	}),
 };
 
