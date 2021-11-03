2aed49bcc8606edc1d50a705aa1e16c2d75e7e84
CUAVmengxiao <mengxiao@cuav.net>
2021-11-03 15:16:11 +0800

    barometer: Add ICP10100 and ICP1011

---
+++ src/drivers/barometer/invensense/icp10111/ICP10111.hpp
@@ -0,0 +1,98 @@
+/****************************************************************************
+ *
+ *   Copyright (C) 2021 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#pragma once
+
+#include "Inven_Sense_ICP10111_registers.hpp"
+
+#include <drivers/drv_hrt.h>
+#include <lib/drivers/device/i2c.h>
+#include <lib/drivers/barometer/PX4Barometer.hpp>
+#include <lib/perf/perf_counter.h>
+#include <px4_platform_common/i2c_spi_buses.h>
+
+using namespace Inven_Sense_ICP10111;
+
+class ICP10111 : public device::I2C, public I2CSPIDriver<ICP10111>
+{
+public:
+	ICP10111(const I2CSPIDriverConfig &config);
+	~ICP10111() override;
+
+	static void print_usage();
+
+	void RunImpl();
+
+	int init() override;
+	void print_status() override;
+
+private:
+	int probe() override;
+
+	bool Reset();
+
+	bool Measure();
+
+	int8_t cal_crc(uint8_t seed, uint8_t data);
+	int read_measure_results(uint8_t *buf, uint8_t len);
+	int read_response(Cmd cmd, uint8_t *buf, uint8_t len);
+	int send_command(Cmd cmd);
+	int send_command(Cmd cmd, uint8_t *data, uint8_t len);
+
+	PX4Barometer _px4_baro;
+
+	perf_counter_t _reset_perf{perf_alloc(PC_COUNT, MODULE_NAME": reset")};
+	perf_counter_t _sample_perf{perf_alloc(PC_ELAPSED, MODULE_NAME": read")};
+	perf_counter_t _bad_transfer_perf{perf_alloc(PC_COUNT, MODULE_NAME": bad transfer")};
+
+	hrt_abstime _reset_timestamp{0};
+	int _failure_count{0};
+
+	unsigned _measure_interval{0};
+	int16_t _scal[4];
+
+	enum class STATE : uint8_t {
+		RESET,
+		WAIT_FOR_RESET,
+		READ_OTP,
+		MEASURE,
+		READ
+	} _state{STATE::RESET};
+
+	enum class MODE : uint8_t {
+		FAST,
+		NORMAL,
+		ACCURATE,
+		VERY_ACCURATE
+	} _mode{MODE::VERY_ACCURATE};
+};
---
+++ src/drivers/barometer/invensense/icp10111/Inven_Sense_ICP10111_registers.hpp
@@ -0,0 +1,62 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2021 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/**
+ * @file icp10111_registers.hpp
+ *
+ * icp10111 registers.
+ *
+ */
+
+#pragma once
+
+#include <cstdint>
+
+namespace Inven_Sense_ICP10111
+{
+static constexpr uint32_t I2C_SPEED = 100 * 1000; // 100 kHz I2C serial interface
+static constexpr uint8_t I2C_ADDRESS_DEFAULT = 0x63;
+
+static constexpr uint8_t Product_ID = 0x08;
+
+enum class Cmd : uint16_t {
+	READ_ID		= 0xefc8,
+	SET_ADDR 	= 0xc595,
+	READ_OTP 	= 0xc7f7,
+	MEAS_LP 	= 0x609c,
+	MEAS_N 		= 0x6825,
+	MEAS_LN 	= 0x70df,
+	MEAS_ULN  	= 0x7866,
+	SOFT_RESET 	= 0x805d
+};
+} // namespace Inven_Sense_ICP10111
