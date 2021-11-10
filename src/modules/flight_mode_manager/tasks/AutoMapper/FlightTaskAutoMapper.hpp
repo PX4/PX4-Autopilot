59a395f6a0e3f241b45ba640bf47db526a679ac7
Matthias Grob <maetugr@gmail.com>
2021-11-10 21:40:28 +0100

    FlightTaskAutoMapper: move remaining members into FlightTaskAutoLineSmooth

--- src/modules/flight_mode_manager/tasks/AutoMapper/FlightTaskAutoMapper.hpp
+++ src/modules/flight_mode_manager/tasks/AutoMapper/FlightTaskAutoMapper.hpp
@@ -41,14 +41,11 @@
 #pragma once
 
 #include "FlightTaskAuto.hpp"
-#include "Sticks.hpp"
-#include "StickAccelerationXY.hpp"
-#include "StickYaw.hpp"
 
 class FlightTaskAutoMapper : public FlightTaskAuto
 {
 public:
-	FlightTaskAutoMapper();
+	FlightTaskAutoMapper() = default;
 	virtual ~FlightTaskAutoMapper() = default;
 
 protected:
@@ -65,13 +62,4 @@ protected:
 					_param_mpc_tko_ramp_t, // time constant for smooth takeoff ramp
 					(ParamFloat<px4::params::MPC_MAN_Y_MAX>) _param_mpc_man_y_max
 				       );
-
-protected:
-	Sticks _sticks;
-	StickAccelerationXY _stick_acceleration_xy;
-	StickYaw _stick_yaw;
-	matrix::Vector3f _land_position;
-	float _land_heading;
-	WaypointType _type_previous{WaypointType::idle}; /**< Previous type of current target triplet. */
-
 };
