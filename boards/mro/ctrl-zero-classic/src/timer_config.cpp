a5379cd92fbcfcb545f2580c3528414ae4187386
achim <lumserei@web.de>
2021-11-26 17:59:06 +0100

    Update timer_config.cpp
    10 pwm outs of the board can be used 
    pwm 10 and 12 are currently not supported by PX4/nuttx cause io timer 15 is missing

--- boards/mro/ctrl-zero-classic/src/timer_config.cpp
+++ boards/mro/ctrl-zero-classic/src/timer_config.cpp
@@ -38,7 +38,7 @@ constexpr io_timers_t io_timers[MAX_IO_TIMERS] = {
 	initIOTimer(Timer::Timer4, DMA{DMA::Index1}),
 	initIOTimer(Timer::Timer2, DMA{DMA::Index1}),
 //	initIOTimer(Timer::Timer15, DMA{DMA::Index2}),
-//	initIOTimer(Timer::Timer8, DMA{DMA::Index1}),
+	initIOTimer(Timer::Timer8, DMA{DMA::Index1}),
 };
 
 constexpr timer_io_channels_t timer_io_channels[MAX_TIMER_IO_CHANNELS] = {
@@ -52,7 +52,7 @@ constexpr timer_io_channels_t timer_io_channels[MAX_TIMER_IO_CHANNELS] = {
 	initIOTimerChannel(io_timers, {Timer::Timer2, Timer::Channel1}, {GPIO::PortA, GPIO::Pin15}),
     	initIOTimerChannel(io_timers, {Timer::Timer2, Timer::Channel4}, {GPIO::PortB, GPIO::Pin11}),
 //    	initIOTimerChannel(io_timers, {Timer::Timer15, Timer::Channel1}, {GPIO::PortE, GPIO::Pin5}),
-//    	initIOTimerChannel(io_timers, {Timer::Timer8, Timer::Channel1}, {GPIO::PortC, GPIO::Pin6}),
+    	initIOTimerChannel(io_timers, {Timer::Timer8, Timer::Channel1}, {GPIO::PortC, GPIO::Pin6}),
 //    	initIOTimerChannel(io_timers, {Timer::Timer4, Timer::Channel4}, {GPIO::PortD, GPIO::Pin15}),
 };
 
