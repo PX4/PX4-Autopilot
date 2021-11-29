4becd6e4c7d6aa07d1529fb40b1c59d9af7dec42
Beat Küng <beat-kueng@gmx.net>
2021-11-29 13:51:12 +0100

    rover: publish vehicle_thrust_setpoint & vehicle_torque_setpoint

--- src/modules/rover_pos_control/RoverPositionControl.hpp
+++ src/modules/rover_pos_control/RoverPositionControl.hpp
@@ -73,6 +73,8 @@
 #include <uORB/topics/vehicle_local_position.h>
 #include <uORB/topics/vehicle_local_position_setpoint.h>
 #include <uORB/topics/actuator_controls.h>
+#include <uORB/topics/vehicle_thrust_setpoint.h>
+#include <uORB/topics/vehicle_torque_setpoint.h>
 
 using matrix::Dcmf;
 
@@ -126,6 +128,8 @@ private:
 	actuator_controls_s				_act_controls{};		/**< direct control of actuators */
 	vehicle_attitude_s				_vehicle_att{};
 	vehicle_local_position_setpoint_s       _trajectory_setpoint{};
+	uORB::Publication<vehicle_thrust_setpoint_s>	_vehicle_thrust_setpoint_pub{ORB_ID(vehicle_thrust_setpoint)};
+	uORB::Publication<vehicle_torque_setpoint_s>	_vehicle_torque_setpoint_pub{ORB_ID(vehicle_torque_setpoint)};
 
 	uORB::SubscriptionData<vehicle_acceleration_s>		_vehicle_acceleration_sub{ORB_ID(vehicle_acceleration)};
 
