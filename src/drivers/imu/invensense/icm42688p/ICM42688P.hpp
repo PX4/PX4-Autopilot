9ab633c18f131fbfec3407d0e57f6d7b05a3fa28
Daniel Agar <daniel@agar.ca>
2021-12-05 15:35:58 -0500

    drivers/imu/invensense: check register bank during probe if necessary
     - the WHO_AM_I is only accessible in register bank 0, which might not
    be currently selected if the sensor didn't have a clean reset

--- src/drivers/imu/invensense/icm42688p/ICM42688P.hpp
+++ src/drivers/imu/invensense/icm42688p/ICM42688P.hpp
@@ -114,7 +114,7 @@ private:
 	void ConfigureSampleRate(int sample_rate);
 	void ConfigureFIFOWatermark(uint8_t samples);
 
-	void SelectRegisterBank(enum REG_BANK_SEL_BIT bank);
+	void SelectRegisterBank(enum REG_BANK_SEL_BIT bank, bool force = false);
 	void SelectRegisterBank(Register::BANK_0 reg) { SelectRegisterBank(REG_BANK_SEL_BIT::USER_BANK_0); }
 	void SelectRegisterBank(Register::BANK_1 reg) { SelectRegisterBank(REG_BANK_SEL_BIT::USER_BANK_1); }
 	void SelectRegisterBank(Register::BANK_2 reg) { SelectRegisterBank(REG_BANK_SEL_BIT::USER_BANK_2); }
