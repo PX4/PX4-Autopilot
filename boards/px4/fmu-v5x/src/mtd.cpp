3463b725a5e33903d7a9b36308807d5677805b92
Igor Mišić <igy1000mb@gmail.com>
2021-12-15 23:35:50 +0100

    px4_manifest: fix px4_mft_s to accept multiple manifests

--- boards/px4/fmu-v5x/src/mtd.cpp
+++ boards/px4/fmu-v5x/src/mtd.cpp
@@ -105,7 +105,6 @@ static const px4_mtd_manifest_t board_mtd_config = {
 	.nconfigs   = 3,
 	.entries = {
 		&fmum_fram,
-		&base_eeprom,
 		&imu_eeprom
 	}
 };
@@ -117,7 +116,9 @@ static const px4_mft_entry_s mtd_mft = {
 
 static const px4_mft_s mft = {
 	.nmft = 1,
-	.mfts = &mtd_mft
+	.mfts = {
+		&mtd_mft
+	}
 };
 
 const px4_mft_s *board_get_manifest(void)
