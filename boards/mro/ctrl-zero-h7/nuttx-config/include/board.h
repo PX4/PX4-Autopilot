0f4e1dd9f9ae641506fd62c4d9b3e8015fa17af9
Daniel Agar <daniel@agar.ca>
2021-12-23 15:35:33 -0500

    boards: mro ctrl-zero-h7 pin fixes

--- boards/mro/ctrl-zero-h7/nuttx-config/include/board.h
+++ boards/mro/ctrl-zero-h7/nuttx-config/include/board.h
@@ -221,32 +221,32 @@
 
 
 /* UART/USART */
-#define GPIO_USART2_RX   GPIO_USART2_RX_2       /* PD6 */
-#define GPIO_USART2_TX   GPIO_USART2_TX_2       /* PD5 */
-#define GPIO_USART2_RTS  GPIO_USART2_RTS_2      /* PD4 */
-#define GPIO_USART2_CTS  GPIO_USART2_CTS_NSS_2  /* PD3 */
+#define GPIO_USART2_TX   GPIO_USART2_TX_2      /* PD5  */
+#define GPIO_USART2_RX   GPIO_USART2_RX_2      /* PD6  */
+#define GPIO_USART2_CTS  GPIO_USART2_CTS_NSS_2 /* PD3  */
+#define GPIO_USART2_RTS  GPIO_USART2_RTS_2     /* PD4  */
 
-#define GPIO_USART3_RX   GPIO_USART3_RX_3       /* PD9 */
-#define GPIO_USART3_TX   GPIO_USART3_TX_3       /* PD8 */
-#define GPIO_USART3_RTS  GPIO_USART3_RTS_2      /* PD12 */
-#define GPIO_USART3_CTS  GPIO_USART3_CTS_NSS_2  /* PD11 */
+#define GPIO_USART3_TX   GPIO_USART3_TX_3      /* PD8  */
+#define GPIO_USART3_RX   GPIO_USART3_RX_3      /* PD9  */
+#define GPIO_USART3_CTS  GPIO_USART3_CTS_NSS_2 /* PD11 */
+#define GPIO_USART3_RTS  GPIO_USART3_RTS_2     /* PD12 */
 
-#define GPIO_UART4_RX    GPIO_UART4_RX_1        /* PA1 */
-#define GPIO_UART4_TX    GPIO_UART4_TX_1        /* PA0 */
+#define GPIO_UART4_TX    GPIO_UART4_TX_2       /* PA0  */
+#define GPIO_UART4_RX    GPIO_UART4_RX_2       /* PA1  */
 
-#define GPIO_USART6_RX   GPIO_USART6_RX_1       /* PC7 */
-#define GPIO_USART6_TX   GPIO_USART6_TX_2       /* PG14 */
+#define GPIO_USART6_TX   0                     /* USART6 is RX-only */
+#define GPIO_USART6_RX   GPIO_USART6_RX_1      /* PC7  */
 
-#define GPIO_UART7_RX    GPIO_UART7_RX_1        /* PE7 */
-#define GPIO_UART7_TX    GPIO_UART7_TX_1        /* PE8 */
+#define GPIO_UART7_TX    GPIO_UART7_TX_3       /* PE8  */
+#define GPIO_UART7_RX    GPIO_UART7_RX_3       /* PE7  */
 
-#define GPIO_UART8_RX    GPIO_UART8_RX_1        /* PE0 */
-#define GPIO_UART8_TX    GPIO_UART8_TX_1        /* PE1 */
+#define GPIO_UART8_TX    GPIO_UART8_TX_1       /* PE1  */
+#define GPIO_UART8_RX    GPIO_UART8_RX_1       /* PE0  */
 
 
 /* CAN */
-#define GPIO_CAN1_RX     GPIO_CAN1_RX_3     /* PD0  */
-#define GPIO_CAN1_TX     GPIO_CAN1_TX_3     /* PD1 */
+#define GPIO_CAN1_RX     GPIO_CAN1_RX_3        /* PD0  */
+#define GPIO_CAN1_TX     GPIO_CAN1_TX_3        /* PD1  */
 
 
 /* SPI */
@@ -262,7 +262,7 @@
 
 #define GPIO_SPI5_SCK    ADJ_SLEW_RATE(GPIO_SPI5_SCK_1) /* PF7  */
 #define GPIO_SPI5_MISO   GPIO_SPI5_MISO_1               /* PF8  */
-#define GPIO_SPI5_MOSI   GPIO_SPI5_MOSI_1               /* PF9  */
+#define GPIO_SPI5_MOSI   GPIO_SPI5_MOSI_2               /* PF9  */
 
 
 /* I2C */
