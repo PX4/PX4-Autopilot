f3635d20453ca98ee9243924aaaa27e5437af2c8
Silvan Fuhrer <silvan@auterion.com>
2022-01-10 14:03:52 +0100

    ControlAllocation: update normalization scale only if matrix updated is forced
    The forced flag is used to distinguish between updates due to a configuration
    (parameter) change (only enabled when disarmed), and matrix updates due to motor
    tilt change. Only update the normalization scale if the forced flag is true, and
    use a tilt angle of vertical position for it to have the scales tilt-invariant.
    
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- src/modules/control_allocator/ControlAllocation/ControlAllocation.cpp
+++ src/modules/control_allocator/ControlAllocation/ControlAllocation.cpp
@@ -51,7 +51,8 @@ ControlAllocation::ControlAllocation()
 void
 ControlAllocation::setEffectivenessMatrix(
 	const matrix::Matrix<float, ControlAllocation::NUM_AXES, ControlAllocation::NUM_ACTUATORS> &effectiveness,
-	const ActuatorVector &actuator_trim, const ActuatorVector &linearization_point, int num_actuators)
+	const ActuatorVector &actuator_trim, const ActuatorVector &linearization_point, int num_actuators,
+	bool update_normalization_scale)
 {
 	_effectiveness = effectiveness;
 	ActuatorVector linearization_point_clipped = linearization_point;
