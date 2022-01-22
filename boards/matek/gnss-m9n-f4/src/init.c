cb06f82f0ff1f9107895dbbf116b7249233899ad
David Sidrane <David.Sidrane@NscDg.com>
2022-01-21 16:03:45 -0800

    gnss-m9n-f4:Board support clean up
       SD is on SPI3 - correct pin mapping
       Fix DMA Mapping for all SPI and RX DMA on U[S]ART RX
       Fix Memory MAP SRAM size
       Removed unused GPIO
       Used proper I2C definitions
       Ensure Watchdog is configured for debugging
       Fixed FLASH param definitions
       Removed unedded SPI init
    
    matek_gnss-m9n-f4:Correct Board ID and Size
    
    Build order SJF
    
    Added Support for F40x

--- boards/matek/gnss-m9n-f4/src/init.c
+++ boards/matek/gnss-m9n-f4/src/init.c
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2012-2021 PX4 Development Team. All rights reserved.
+ *   Copyright (c) 2012-2022 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -60,7 +60,6 @@
 #include <nuttx/i2c/i2c_master.h>
 #include <nuttx/mmcsd.h>
 #include <nuttx/analog/adc.h>
-#include <nuttx/mm/gran.h>
 
 #include <stm32.h>
 #include "board_config.h"
@@ -69,79 +68,19 @@
 #include <arch/board/board.h>
 
 #include <drivers/drv_hrt.h>
-#include <drivers/drv_board_led.h>
+#include <drivers/drv_watchdog.h>
+
+#include "led.h"
 
 #include <systemlib/px4_macros.h>
 
-#include <px4_arch/io_timer.h>
 #include <px4_platform_common/init.h>
-#include <px4_platform/board_dma_alloc.h>
-#include <px4_platform/gpio.h>
 
+#include <px4_platform/gpio.h>
+#include <px4_platform/board_dma_alloc.h>
 # if defined(FLASH_BASED_PARAMS)
 #  include <parameters/flashparams/flashfs.h>
 #endif
-
-/****************************************************************************
- * Pre-Processor Definitions
- ****************************************************************************/
-
-/*
- * Ideally we'd be able to get these from arm_internal.h,
- * but since we want to be able to disable the NuttX use
- * of leds for system indication at will and there is no
- * separate switch, we need to build independent of the
- * CONFIG_ARCH_LEDS configuration switch.
- */
-__BEGIN_DECLS
-extern void led_init(void);
-extern void led_on(int led);
-extern void led_off(int led);
-__END_DECLS
-
-/****************************************************************************
- * Protected Functions
- ****************************************************************************/
-/****************************************************************************
- * Public Functions
- ****************************************************************************/
-/************************************************************************************
- * Name: board_peripheral_reset
- *
- * Description:
- *
- ************************************************************************************/
-__EXPORT void board_peripheral_reset(int ms)
-{
-	UNUSED(ms);
-}
-
-/************************************************************************************
- * Name: board_on_reset
- *
- * Description:
- * Optionally provided function called on entry to board_system_reset
- * It should perform any house keeping prior to the rest.
- *
- * status - 1 if resetting to boot loader
- *          0 if just resetting
- *
- ************************************************************************************/
-__EXPORT void board_on_reset(int status)
-{
-	/* configure the GPIO pins to outputs and keep them low */
-	/*for (int i = 0; i < DIRECT_PWM_OUTPUT_CHANNELS; ++i) {
-		px4_arch_configgpio(io_timer_channel_get_gpio_output(i));
-	}*/
-
-	/* On resets invoked from system (not boot) insure we establish a low
-	 * output state (discharge the pins) on PWM pins before they become inputs.
-	 */
-
-	if (status >= 0) {
-		up_mdelay(400);
-	}
-}
 
 /************************************************************************************
  * Name: stm32_boardinitialize
@@ -156,6 +95,12 @@ __EXPORT void board_on_reset(int status)
 __EXPORT void
 stm32_boardinitialize(void)
 {
+	watchdog_init();
+
+
+	/* configure LEDs */
+
+	board_autoled_initialize();
 
 	/* configure pins */
 	const uint32_t gpio[] = PX4_GPIO_INIT_LIST;
@@ -163,7 +108,9 @@ stm32_boardinitialize(void)
 
 	/* configure SPI all interfaces GPIO */
 
-	stm32_spiinitialize();
+	/* configure USB interfaces */
+
+	stm32_usbinitialize();
 
 }
 
@@ -192,19 +139,11 @@ stm32_boardinitialize(void)
  *
  ****************************************************************************/
 
-static struct spi_dev_s *spi1;
-static struct spi_dev_s *spi2;
 static struct spi_dev_s *spi3;
 
 __EXPORT int board_app_initialize(uintptr_t arg)
 {
 	px4_platform_init();
-
-	/* configure the DMA allocator */
-
-	if (board_dma_alloc_init() < 0) {
-		syslog(LOG_ERR, "DMA alloc FAILED\n");
-	}
 
 #if defined(SERIAL_HAVE_RXDMA)
 
@@ -214,63 +153,36 @@ __EXPORT int board_app_initialize(uintptr_t arg)
 	hrt_call_every(&serial_dma_call, 1000, 1000, (hrt_callout)stm32_serial_dma_poll, NULL);
 #endif
 
-	/* initial LED state */
-	drv_led_start();
-	led_off(LED_BLUE);
+	/* configure the DMA allocator */
 
-	/*if (board_hardfault_init(2, true) != 0) {
-		led_on(LED_BLUE);
-	}*/
-
-	/* Configure SPI-based devices */
-
-	spi1 = stm32_spibus_initialize(1);
-
-	if (!spi1) {
-		syslog(LOG_ERR, "[boot] FAILED to initialize SPI port 1\n");
-		led_on(LED_BLUE);
-		return -ENODEV;
+	if (board_dma_alloc_init() < 0) {
+		syslog(LOG_ERR, "DMA alloc FAILED\n");
 	}
 
-	/* SPI1: ICM20602 IMU */
 
-	SPI_SETFREQUENCY(spi1, 10000000);
-	SPI_SETBITS(spi1, 8);
-	SPI_SETMODE(spi1, SPIDEV_MODE3);
-	up_udelay(20);
-
-	/* SPI2: RM3100 Magnetometer */
-
-	spi2 = stm32_spibus_initialize(2);
-
-	if (!spi2) {
-		syslog(LOG_ERR, "[boot] FAILED to initialize SPI port %d\n", 2);
-		led_on(LED_BLUE);
-		return -ENODEV;
-	}
-
-	up_udelay(20);
+	stm32_spiinitialize();
 
 
-	/* SPI3: SD card */
-
-	spi3 = stm32_spibus_initialize(3);
+	spi3 = stm32_spibus_initialize(CONFIG_NSH_MMCSDSPIPORTNO);
 
 	if (!spi3) {
 		syslog(LOG_ERR, "[boot] FAILED to initialize SPI port 3\n");
-		led_on(LED_BLUE);
+		rgb_led(255, 0, 0, 0);
 		return -ENODEV;
 	}
 
-	SPI_SETFREQUENCY(spi3, 10 * 1000 * 1000);
-	SPI_SETBITS(spi3, 8);
-	SPI_SETMODE(spi3, SPIDEV_MODE3);
-	up_udelay(20);
+	int ret = mmcsd_spislotinitialize(CONFIG_NSH_MMCSDMINOR, CONFIG_NSH_MMCSDSLOTNO, spi3);
+
+	if (ret < 0) {
+		syslog(LOG_ERR, "[boot] Failed to bind SPI port 0 to SD slot 0\n");
+		rgb_led(255, 0, 0, 0);
+		return ret;
+	}
 
 #if defined(FLASH_BASED_PARAMS)
 	static sector_descriptor_t params_sector_map[] = {
-		{1, 16 * 1024, 0x08008000},
-		{2, 16 * 1024, 0x0800C000},
+		{2, 16 * 1024, 0x08008000},
+		{3, 16 * 1024, 0x0800C000},
 		{0, 0, 0},
 	};
 
@@ -279,15 +191,13 @@ __EXPORT int board_app_initialize(uintptr_t arg)
 
 	if (result != OK) {
 		syslog(LOG_ERR, "[boot] FAILED to init params in FLASH %d\n", result);
-		led_on(LED_AMBER);
+		rgb_led(255, 0, 0, 0);
 		return -ENODEV;
 	}
 
-#endif
+#endif // FLASH_BASED_PARAMS
 
-	/* Configure the HW based on the manifest */
-
-	px4_platform_configure();
+	rgb_led(0, 255, 0, 0);
 
 	return OK;
 }
