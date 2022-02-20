1298b0fac4baef962e2fc76a6d3063c5b9e2b907
Roman Dvořák <romandvorak@mlab.cz>
2022-02-20 23:50:35 +0100

    Update src/modules/logger/logged_topics.cpp
    Co-authored-by: Daniel Agar <daniel@agar.ca>

--- src/modules/logger/logged_topics.cpp
+++ src/modules/logger/logged_topics.cpp
@@ -122,7 +122,7 @@ void LoggedTopics::add_default_topics()
 	add_topic_multi("control_allocator_status", 200, 2);
 	add_optional_topic_multi("rate_ctrl_status", 200, 2);
 	add_topic_multi("sensor_hygrometer", 500, 4);
-	add_topic_multi("rpm", 200, 8);
+	add_topic_multi("rpm", 200);
 	add_optional_topic_multi("telemetry_status", 1000, 4);
 
 	// EKF multi topics (currently max 9 estimators)
