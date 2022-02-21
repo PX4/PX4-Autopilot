0e5f95d388ab6657a4bcb10edb4dcf98fa1f5659
Charles Cross <charles@missionrobotics.us>
2022-02-21 07:53:20 -0800

    Applies formatting

--- platforms/nuttx/src/px4/stm/stm32_common/io_pins/io_timer.c
+++ platforms/nuttx/src/px4/stm/stm32_common/io_pins/io_timer.c
@@ -645,11 +645,11 @@ int io_timer_init_timer(unsigned timer, io_timer_channel_mode_t mode)
 		rDCR(timer) = 0;
 
 		if ((io_timers[timer].base == STM32_TIM1_BASE)
-			|| (io_timers[timer].base == STM32_TIM8_BASE)
-			#ifdef STM32_TIM15_BASE
-			|| (io_timers[timer].base == STM32_TIM15_BASE)
-			#endif
-		) {
+		    || (io_timers[timer].base == STM32_TIM8_BASE)
+#ifdef STM32_TIM15_BASE
+		    || (io_timers[timer].base == STM32_TIM15_BASE)
+#endif
+		   ) {
 
 			/* master output enable = on */
 
