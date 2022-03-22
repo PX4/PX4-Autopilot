84e796c3855bb4dc995528f62dd430c52b4cccae
Daniel Agar <daniel@agar.ca>
2022-03-22 16:35:09 -0400

    drives/gps: add new sensor_gnsss_relative msg
     - for ublox this corresponds to NAV_RELPOSNED

--- src/modules/logger/logged_topics.cpp
+++ src/modules/logger/logged_topics.cpp
@@ -176,6 +176,7 @@ void LoggedTopics::add_default_topics()
 	add_optional_topic_multi("sensor_accel", 1000, 4);
 	add_optional_topic_multi("sensor_baro", 1000, 4);
 	add_topic_multi("sensor_gps", 1000, 2);
+	add_topic_multi("sensor_gnss_relative", 1000, 1);
 	add_optional_topic("pps_capture", 1000);
 	add_optional_topic_multi("sensor_gyro", 1000, 4);
 	add_optional_topic_multi("sensor_mag", 1000, 4);
