e2524d8dd511cc0f548a80fb2f16c938b027bf88
Julian Oes <julian@oes.ch>
2022-03-25 08:20:13 +1300

    flight_modes: use precision landing when landing
    This also enables precision landing in landing mode, not just during
    RTL.
    
    This required an additional fix during the horizontal approach. It seems
    like the z setpoint is not set when landing, so we can't blindly use
    that setpoint but rather need to keep the altitude.

--- src/modules/flight_mode_manager/FlightModeManager.cpp
+++ src/modules/flight_mode_manager/FlightModeManager.cpp
@@ -166,7 +166,8 @@ void FlightModeManager::start_flight_task()
 
 		// Take-over landing from navigator if precision landing is enabled
 
-	} else if (_vehicle_status_sub.get().nav_state == vehicle_status_s::NAVIGATION_STATE_AUTO_RTL
+	} else if ((_vehicle_status_sub.get().nav_state == vehicle_status_s::NAVIGATION_STATE_AUTO_RTL ||
+		    _vehicle_status_sub.get().nav_state == vehicle_status_s::NAVIGATION_STATE_AUTO_LAND)
 		   && _param_rtl_pld_md.get() > 0) {
 		should_disable_task = false;
 
