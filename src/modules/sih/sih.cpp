6221d01835e2ea728915270233b31135bcd0decf
Thomas Debrunner <thomas.debrunner@auterion.com>
2022-04-01 12:20:28 +0200

    sih: increase stack size

--- src/modules/sih/sih.cpp
+++ src/modules/sih/sih.cpp
@@ -757,7 +757,7 @@ int Sih::task_spawn(int argc, char *argv[])
 	_task_id = px4_task_spawn_cmd("sih",
 				      SCHED_DEFAULT,
 				      SCHED_PRIORITY_MAX,
-				      1024,
+				      1250,
 				      (px4_main_t)&run_trampoline,
 				      (char *const *)argv);
 
