f1851048196bc77ddf1af3b67f954b2450118458
Thomas Stastny <thomas.stastny@auterion.com>
2022-04-04 17:10:27 +0200

    Update src/lib/mathlib/math/test/second_order_reference_model_test.cpp
    Co-authored-by: Mathieu Bresciani <brescianimathieu@gmail.com>

--- src/lib/mathlib/math/test/second_order_reference_model_test.cpp
+++ src/lib/mathlib/math/test/second_order_reference_model_test.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2019 ECL Development Team. All rights reserved.
+ *   Copyright (c) 2022 ECL Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
