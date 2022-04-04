17501e9c4a1fb59df39683bd3752a5120d5f696d
David Sidrane <David.Sidrane@NscDg.com>
2022-04-04 11:56:21 -0700

    px4io:Removed dma_poll call back

--- src/modules/px4iofirmware/px4io.cpp
+++ src/modules/px4iofirmware/px4io.cpp
@@ -66,8 +66,6 @@
 
 struct sys_state_s system_state;
 
-static struct hrt_call serial_dma_call;
-
 /*
  * a set of debug buffers to allow us to send debug information from ISRs
  */
@@ -290,9 +288,6 @@ extern "C" __EXPORT int user_start(int argc, char *argv[])
 	 * Poll at 1ms intervals for received bytes that have not triggered
 	 * a DMA event.
 	 */
-#ifdef CONFIG_ARCH_DMA
-	hrt_call_every(&serial_dma_call, 1000, 1000, (hrt_callout)stm32_serial_dma_poll, NULL);
-#endif
 
 	/* print some startup info */
 	syslog(LOG_INFO, "\nPX4IO: starting\n");
