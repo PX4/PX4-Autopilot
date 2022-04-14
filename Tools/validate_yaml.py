d5fa8b617b4e7151c945e30c37e6962f238c42bc
Kalyan Sriram <kalyan@coderkalyan.com>
2022-04-13 20:51:07 -0700

    tools/validate_yaml: code cleanup

--- Tools/validate_yaml.py
+++ Tools/validate_yaml.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python3
-""" Script to validate YAML file(s) against a YAML schema file """
-
-from __future__ import print_function
+"""Script to validate YAML file(s) against a YAML schema file."""
 
 import argparse
+import logging
+import pprint
 import sys
 
 try:
@@ -27,42 +27,52 @@ except ImportError as e:
     sys.exit(1)
 
 
-parser = argparse.ArgumentParser(description='Validate YAML file(s) against a schema')
-
-parser.add_argument('yaml_file', nargs='+', help='YAML config file(s)')
-parser.add_argument('--schema-file', type=str, action='store',
-                    help='YAML schema file', required=True)
-parser.add_argument('-v', '--verbose', dest='verbose', action='store_true',
-                    help='Verbose Output')
-
-args = parser.parse_args()
-schema_file = args.schema_file
-yaml_files = args.yaml_file
-verbose = args.verbose
-
 def load_yaml_file(file_name):
-    with open(file_name, 'r') as stream:
-        try:
-            return yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
-            raise
+    """Open file, safely loads it as a yaml and returns the resulting dict."""
+    with open(file_name, encoding="utf-8") as stream:
+        return yaml.safe_load(stream)
 
-# load the schema
-schema = load_yaml_file(schema_file)
-validator = cerberus.Validator(schema)
 
-# validate yaml files
-for yaml_file in yaml_files:
-    if verbose: print("Validating {:}".format(yaml_file))
-    document = load_yaml_file(yaml_file)
-    # ignore top-level entries prefixed with __
-    for key in list(document.keys()):
-        if key.startswith('__'): del document[key]
+def main():
+    parser = argparse.ArgumentParser(
+        description='Validate YAML file(s) against a schema')
 
-    if not validator.validate(document):
-        print("Validation Errors:")
-        print(validator.errors)
-        print("")
-        raise Exception("Validation of {:} failed".format(yaml_file))
+    parser.add_argument('yaml_file', nargs='+', help='YAML config file(s)')
+    parser.add_argument('--schema-file', type=str, action='store',
+                        help='YAML schema file', required=True)
+    parser.add_argument('-v', '--verbose', dest='verbose', action='store_true',
+                        help='Verbose Output')
 
+    args = parser.parse_args()
+
+    schema_file = args.schema_file
+    yaml_files = args.yaml_file
+    if args.verbose:
+        logging.basicConfig(level=logging.INFO)
+    else:
+        logging.basicConfig(level=logging.ERROR)
+
+    # load the schema
+    schema = load_yaml_file(schema_file)
+    validator = cerberus.Validator(schema)
+
+    # validate the specified yaml files
+    for yaml_file in yaml_files:
+        logging.info("Validating %s", yaml_file)
+
+        document = load_yaml_file(yaml_file)
+
+        # ignore top-level entries prefixed with __
+        for key in list(document.keys()):
+            if key.startswith('__'):
+                del document[key]
+
+        if not validator.validate(document):
+            logging.error("Found validation errors with %s:", yaml_file)
+            logging.error(pprint.pformat(validator.errors))
+
+            raise Exception(f"Validation of {yaml_file} failed")
+
+
+if __name__ == "__main__":
+    main()
