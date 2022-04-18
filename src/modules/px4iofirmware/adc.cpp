3cdeeb8d64bcee615f64a9a2dfc500937a894389
Daniel Agar <daniel@agar.ca>
2022-04-17 20:44:30 -0400

    px4iofirmware: convert most files to c++

--- src/modules/px4iofirmware/adc.cpp
+++ src/modules/px4iofirmware/adc.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2012 PX4 Development Team. All rights reserved.
+ *   Copyright (c) 2012-2022 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -32,7 +32,7 @@
  ****************************************************************************/
 
 /**
- * @file adc.c
+ * @file adc.cpp
  *
  * Simple ADC support for PX4IO on STM32.
  */
