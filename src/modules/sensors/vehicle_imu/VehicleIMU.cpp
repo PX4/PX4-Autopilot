beaebe4d0b8b21c98dc1bda1ad531d91a9a52156
Daniel Agar <daniel@agar.ca>
2022-06-04 11:15:03 -0400

    sensors/vehicle_imu: don't bother checking IMU_GYRO_RATEMAX

--- src/modules/sensors/vehicle_imu/VehicleIMU.cpp
+++ src/modules/sensors/vehicle_imu/VehicleIMU.cpp
@@ -146,8 +146,7 @@ bool VehicleIMU::ParametersUpdate(bool force)
 		}
 
 		// constrain IMU integration time 1-20 milliseconds (50-1000 Hz)
-		int32_t imu_integration_rate_hz = constrain(_param_imu_integ_rate.get(),
-						  (int32_t)50, math::min(_param_imu_gyro_ratemax.get(), (int32_t) 1000));
+		const int32_t imu_integration_rate_hz = constrain(_param_imu_integ_rate.get(), (int32_t)50, (int32_t)1000);
 
 		if (imu_integration_rate_hz != _param_imu_integ_rate.get()) {
 			PX4_WARN("IMU_INTEG_RATE updated %" PRId32 " -> %" PRIu32, _param_imu_integ_rate.get(), imu_integration_rate_hz);
