0922f003f586ac32d3d0ce08d0a2ad76218b8542
Igor Mišić <igy1000mb@gmail.com>
2022-06-14 18:55:05 +0200

    uavcan: don't print an error if there is no UAVCAN device on the CAN bus

--- src/drivers/uavcan/uavcan_main.cpp
+++ src/drivers/uavcan/uavcan_main.cpp
@@ -1200,7 +1200,8 @@ UavcanNode::param_count(uavcan::NodeID node_id)
 	req.index = 0;
 	int call_res = _param_getset_client.call(node_id, req);
 
-	if (call_res < 0) {
+	// -ErrInvalidParam is returned when no UAVCAN device is connected to the CAN bus
+	if ((call_res < 0) && (-uavcan::ErrInvalidParam != call_res)) {
 		PX4_ERR("couldn't start parameter count: %d", call_res);
 
 	} else {
