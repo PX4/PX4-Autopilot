554572011d8ee044ef2ba03ecb93b9dc9d6735a5
Marvin Harms <harmsm@ethz.ch>
2022-06-22 09:05:20 +0200

    TESTING CODE PUSH

--- src/modules/fw_dyn_soar_control/FixedwingPositionINDIControl.cpp
+++ src/modules/fw_dyn_soar_control/FixedwingPositionINDIControl.cpp
@@ -409,8 +409,8 @@ FixedwingPositionINDIControl::_read_trajectory_coeffs_csv(char *filename)
     // =======================================================================
     bool error = false;
 
-    char home_dir[200] = "/home/marvin/Documents/master_thesis_ADS/PX4/Git/ethzasl_fw_px4/src/modules/fw_dyn_soar_control/trajectories/";
-    //char home_dir[200] = PX4_ROOTFSDIR"/fs/microsd/trajectories/";
+    //char home_dir[200] = "/home/marvin/Documents/master_thesis_ADS/PX4/Git/ethzasl_fw_px4/src/modules/fw_dyn_soar_control/trajectories/";
+    char home_dir[200] = PX4_ROOTFSDIR"/fs/microsd/trajectories/";
     //PX4_ERR(home_dir);
     strcat(home_dir,filename);
     FILE* fp = fopen(home_dir, "r");
