f0a301912a72842b934f77dacd11ab2706f73a0a
Igor Mišić <igy1000mb@gmail.com>
2022-06-30 15:01:24 +0200

    Remove logging rate limit for pps_capture
    In case of a signal issue on the PPS GPIO it is helpful to have logging
    data with higher rates.

--- src/modules/logger/logged_topics.cpp
+++ src/modules/logger/logged_topics.cpp
@@ -178,7 +178,6 @@ void LoggedTopics::add_default_topics()
 	add_optional_topic_multi("sensor_baro", 1000, 4);
 	add_topic_multi("sensor_gps", 1000, 2);
 	add_topic_multi("sensor_gnss_relative", 1000, 1);
-	add_optional_topic("pps_capture", 1000);
 	add_optional_topic_multi("sensor_gyro", 1000, 4);
 	add_optional_topic_multi("sensor_mag", 1000, 4);
 	add_optional_topic_multi("sensor_optical_flow", 1000, 2);
@@ -188,6 +187,7 @@ void LoggedTopics::add_default_topics()
 	add_optional_topic_multi("vehicle_magnetometer", 500, 4);
 	add_optional_topic("vehicle_optical_flow", 500);
 	//add_optional_topic("vehicle_optical_flow_vel", 100);
+	add_optional_topic("pps_capture");
 
 	// SYS_CTRL_ALLOC: additional dynamic control allocation logging when enabled
 	int32_t sys_ctrl_alloc = 0;
