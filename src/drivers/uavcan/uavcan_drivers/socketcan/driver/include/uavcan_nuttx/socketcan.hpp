089fbdccc99c53ad926df7aed969d85f82f3130e
Peter van der Perk <peter.vanderperk@nxp.com>
2022-07-19 14:46:54 +0200

    UAVCANNODE NuttX SocketCAN driver Change init mode for CAN driver so it get executed in a single context

--- src/drivers/uavcan/uavcan_drivers/socketcan/driver/include/uavcan_nuttx/socketcan.hpp
+++ src/drivers/uavcan/uavcan_drivers/socketcan/driver/include/uavcan_nuttx/socketcan.hpp
@@ -1,6 +1,36 @@
-/*
- * Copyright (C) 2014 Pavel Kirienko <pavel.kirienko@gmail.com>
- */
+/****************************************************************************
+ *
+ *   Copyright (C) 2014 Pavel Kirienko <pavel.kirienko@gmail.com>
+ *   NuttX SocketCAN port Copyright (C) 2022 NXP Semiconductors
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
 
 #pragma once
 
@@ -8,32 +38,85 @@
 #include <uavcan_nuttx/clock.hpp>
 #include <uavcan/driver/can.hpp>
 
+#include <sys/time.h>
+#include <sys/socket.h>
+
+#include <nuttx/can.h>
+#include <netpacket/can.h>
+
 namespace uavcan_socketcan
 {
+
+class CanIface : public uavcan::ICanIface
+	, uavcan::Noncopyable
+{
+	int               _fd{-1};
+	bool              _can_fd{false};
+
+	//// Send msg structure
+	struct iovec       _send_iov {};
+	struct canfd_frame _send_frame {};
+	struct msghdr      _send_msg {};
+	struct cmsghdr     *_send_cmsg {};
+	struct timeval     *_send_tv {};  /* TX deadline timestamp */
+	uint8_t            _send_control[sizeof(struct cmsghdr) + sizeof(struct timeval)] {};
+
+	//// Receive msg structure
+	struct iovec       _recv_iov {};
+	struct canfd_frame _recv_frame {};
+	struct msghdr      _recv_msg {};
+	struct cmsghdr     *_recv_cmsg {};
+	uint8_t            _recv_control[sizeof(struct cmsghdr) + sizeof(struct timeval)] {};
+
+	SystemClock clock;
+
+public:
+	uavcan::uint32_t socketInit(const char *can_iface_name);
+
+	uavcan::int16_t send(const uavcan::CanFrame &frame,
+			     uavcan::MonotonicTime tx_deadline,
+			     uavcan::CanIOFlags flags) override;
+
+	uavcan::int16_t receive(uavcan::CanFrame &out_frame,
+				uavcan::MonotonicTime &out_ts_monotonic,
+				uavcan::UtcTime &out_ts_utc,
+				uavcan::CanIOFlags &out_flags) override;
+
+	uavcan::int16_t configureFilters(const uavcan::CanFilterConfig *filter_configs,
+					 uavcan::uint16_t num_configs) override;
+
+	uavcan::uint64_t getErrorCount() const override;
+
+	uavcan::uint16_t getNumFilters() const override;
+
+	int getFD();
+};
 
 /**
  * This class implements CAN driver interface for libuavcan.
  * No configuration needed other than CAN baudrate.
- * This class is a singleton.
  */
 class CanDriver
 	: public uavcan::ICanDriver
-	, public uavcan::ICanIface
 	, uavcan::Noncopyable
 {
 	BusEvent update_event_;
-	static CanDriver self;
+	CanIface if_[UAVCAN_SOCKETCAN_NUM_IFACES];
 	SystemClock clock;
+	struct pollfd pfds[UAVCAN_SOCKETCAN_NUM_IFACES];
 
 public:
 	CanDriver() : update_event_(*this)
 	{}
 
-	/**
-	 * Returns the singleton reference.
-	 * No other copies can be created.
-	 */
-	static CanDriver &instance() { return self; }
+	uavcan::int32_t initIface(uint32_t index, const char *name)
+	{
+		if (index > (UAVCAN_SOCKETCAN_NUM_IFACES - 1)) {
+			return -1;
+		}
+
+		return if_[index].socketInit(name);
+	}
 
 	/**
 	 * Attempts to detect bit rate of the CAN bus.
@@ -50,15 +133,6 @@ public:
 	 */
 	int init(uavcan::uint32_t bitrate);
 
-	bool hasReadyRx() const;
-	bool hasEmptyTx() const;
-
-	/**
-	 * This method will return true only if there was any CAN bus activity since previous call of this method.
-	 * This is intended to be used for LED iface activity indicators.
-	 */
-	bool hadActivity();
-
 	/**
 	 * Returns the number of times the RX queue was overrun.
 	 */
@@ -71,25 +145,9 @@ public:
 	 */
 	bool isInBusOffState() const;
 
-	uavcan::int16_t send(const uavcan::CanFrame &frame,
-			     uavcan::MonotonicTime tx_deadline,
-			     uavcan::CanIOFlags flags) override;
-
-	uavcan::int16_t receive(uavcan::CanFrame &out_frame,
-				uavcan::MonotonicTime &out_ts_monotonic,
-				uavcan::UtcTime &out_ts_utc,
-				uavcan::CanIOFlags &out_flags) override;
-
 	uavcan::int16_t select(uavcan::CanSelectMasks &inout_masks,
 			       const uavcan::CanFrame * (&)[uavcan::MaxCanIfaces],
 			       uavcan::MonotonicTime blocking_deadline) override;
-
-	uavcan::int16_t configureFilters(const uavcan::CanFilterConfig *filter_configs,
-					 uavcan::uint16_t num_configs) override;
-
-	uavcan::uint64_t getErrorCount() const override;
-
-	uavcan::uint16_t getNumFilters() const override;
 
 	uavcan::ICanIface *getIface(uavcan::uint8_t iface_index) override;
 
@@ -122,6 +180,10 @@ public:
 	 */
 	int init(uavcan::uint32_t bitrate)
 	{
+		driver.initIface(0, "can0");
+#if UAVCAN_SOCKETCAN_NUM_IFACES > 1
+		driver.initIface(1, "can1");
+#endif
 		return driver.init(bitrate);
 	}
 
