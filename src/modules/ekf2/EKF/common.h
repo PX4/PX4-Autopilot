b9da9cb91dc279c1b60d9d09c66fed04b59eb33a
bresch <brescianimathieu@gmail.com>
2022-08-05 11:02:47 +0200

    ekf2: add common height fusion timeout parameter

--- src/modules/ekf2/EKF/common.h
+++ src/modules/ekf2/EKF/common.h
@@ -396,6 +396,7 @@ struct parameters {
 
 	const unsigned reset_timeout_max{7000000};      ///< maximum time we allow horizontal inertial dead reckoning before attempting to reset the states to the measurement or change _control_status if the data is unavailable (uSec)
 	const unsigned no_aid_timeout_max{1000000};     ///< maximum lapsed time from last fusion of a measurement that constrains horizontal velocity drift before the EKF will determine that the sensor is no longer contributing to aiding (uSec)
+	const unsigned hgt_fusion_timeout_max{5'000'000}; ///< maximum time we allow height fusion to fail before attempting a reset or stopping the fusion aiding (uSec)
 
 	int32_t valid_timeout_max{5000000};     ///< amount of time spent inertial dead reckoning before the estimator reports the state estimates as invalid (uSec)
 
