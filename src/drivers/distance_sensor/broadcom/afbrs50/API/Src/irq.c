1b1e439cf24596fc755176ee99eb98d82fb506f3
alexklimaj <alex@arkelectron.com>
2022-08-07 18:23:28 -0600

    Switch to async AFBR measurement calls and use schedule.

--- src/drivers/distance_sensor/broadcom/afbrs50/API/Src/irq.c
+++ src/drivers/distance_sensor/broadcom/afbrs50/API/Src/irq.c
@@ -2,6 +2,7 @@
 #include <nuttx/irq.h>
 
 static volatile irqstate_t irqstate_flags;
+static volatile size_t _lock_count = 0;
 
 /*!***************************************************************************
 * @brief Enable IRQ Interrupts
@@ -10,7 +11,13 @@ static volatile irqstate_t irqstate_flags;
 *****************************************************************************/
 void IRQ_UNLOCK(void)
 {
-	leave_critical_section(irqstate_flags);
+	if (_lock_count > 0) {
+		_lock_count--;
+
+		if (_lock_count == 0) {
+			leave_critical_section(irqstate_flags);
+		}
+	}
 }
 
 /*!***************************************************************************
@@ -20,5 +27,9 @@ void IRQ_UNLOCK(void)
 *****************************************************************************/
 void IRQ_LOCK(void)
 {
-	irqstate_flags = enter_critical_section();
+	if (_lock_count == 0) {
+		irqstate_flags = enter_critical_section();
+	}
+
+	_lock_count++;
 }
