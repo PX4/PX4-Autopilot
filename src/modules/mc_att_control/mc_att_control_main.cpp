562647dec615a28977072d5b99b94bf85aa5f7d2
Matthias Grob <maetugr@gmail.com>
2022-08-10 10:54:28 +0200

    mc_att_control_main: use gradual() for throttle curve rescale without hover thrust

--- src/modules/mc_att_control/mc_att_control_main.cpp
+++ src/modules/mc_att_control/mc_att_control_main.cpp
@@ -101,7 +101,7 @@ MulticopterAttitudeControl::throttle_curve(float throttle_stick_input)
 	// throttle_stick_input is in range [0, 1]
 	switch (_param_mpc_thr_curve.get()) {
 	case 1: // no rescaling to hover throttle
-		return throttle_min + throttle_stick_input * (_param_mpc_thr_max.get() - throttle_min);
+		return math::gradual(throttle_stick_input, 0.f, 1.f, throttle_min, _param_mpc_thr_max.get());
 
 	default: // 0 or other: rescale to hover throttle at 0.5 stick
 		return math::gradual3(throttle_stick_input,
