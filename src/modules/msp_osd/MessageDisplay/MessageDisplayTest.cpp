cc15eff832fe7e2296bfc9cc86e1d6e4e88e8ee8
Daniel M. Sahu <danielmohansahu@gmail.com>
2022-08-16 10:30:49 -0400

    Switched to non-functional unit gtests; fleshing out tests.

--- src/modules/msp_osd/MessageDisplay/MessageDisplayTest.cpp
+++ src/modules/msp_osd/MessageDisplay/MessageDisplayTest.cpp
@@ -33,17 +33,66 @@
 
 #include <gtest/gtest.h>
 
+#include "MessageDisplay.hpp"
+
+// default parameters
+#define PERIOD 125000
+#define DWELL  500000
+
+// use base namespace
+using namespace msp_osd;
+
+// core test setup class
 class MessageDisplayTest : public ::testing::Test
 {
 public:
+  // test copy of the message display object
+  std::unique_ptr<MessageDisplay> md_;
+
+  // setup a new test
 	void SetUp() override
 	{
+    // construct new default display
+    md_ = std::make_unique<MessageDisplay>(PERIOD, DWELL);
     return;
 	}
 };
 
-TEST_F(MessageDisplayTest, testMessageDisplayInitialization)
+// check basic construction
+TEST_F(MessageDisplayTest, testMessageDisplayConstruction)
 {
-  ASSERT_TRUE(false);
+  // make sure we have an object!
+  ASSERT_TRUE(static_cast<bool>(md_));
+
+  // verify default message
+  char message[FULL_MSG_LENGTH];
+  md_->get(message, 0);
+  EXPECT_STREQ(message, "INITIALIZING");
+}
+
+// check setting and getting for a warning message
+TEST_F(MessageDisplayTest, testMessageDisplayWarning)
+{
+  // make sure we have an object!
+  ASSERT_TRUE(static_cast<bool>(md_));
+
+  // set just a warning message, but a very long one
+  md_->set(MessageDisplayType::WARNING, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+
+  // the ground truth message is the following:
+  const char ground_truth[MSG_BUFFER_SIZE] = "???|???|??   WARN: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat";
+
+  // walk through the message as it scrolls
+  char message[FULL_MSG_LENGTH]; // output message
+  char correct[FULL_MSG_LENGTH]; // correct value of output message
+  for (size_t i = 0; i != 10; ++i)
+  {
+    // update message while incrementing time
+    md_->get(message, DWELL + PERIOD * i);
+
+    // get substring that we should be seeing
+    strncpy(correct, &ground_truth[i], FULL_MSG_LENGTH);
+    EXPECT_STREQ(message, correct);
+  }
 }
 
