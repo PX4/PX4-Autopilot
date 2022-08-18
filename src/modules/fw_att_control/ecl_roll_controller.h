9c2994189568f8f580af66c84110f56976d2dcf9
Silvan Fuhrer <silvan@auterion.com>
2022-08-18 09:20:52 +0200

    FW Attitude Control: add @brief, @param and @return for control_attitude()
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- src/modules/fw_att_control/ecl_roll_controller.h
+++ src/modules/fw_att_control/ecl_roll_controller.h
@@ -58,6 +58,13 @@ public:
 	ECL_RollController() = default;
 	~ECL_RollController() = default;
 
+	/**
+	 * @brief Calculates both euler and body roll rate setpoints.
+	 *
+	 * @param dt Time step [s]
+	 * @param ctrl_data Various control inputs (attitude, body rates, attitdue stepoints, euler rate setpoints, current speeed)
+	 * @return Roll body rate setpoint [rad/s]
+	 */
 	float control_attitude(const float dt, const ECL_ControlData &ctl_data) override;
 };
 
