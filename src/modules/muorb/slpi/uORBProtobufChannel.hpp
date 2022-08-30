314e25421c3f8f3a053ba92f9e5507ffdd1e60b3
Eric Katzfey <eric.katzfey@modalai.com>
2022-08-30 09:37:23 -0700

    Fixed formatting

--- src/modules/muorb/slpi/uORBProtobufChannel.hpp
+++ src/modules/muorb/slpi/uORBProtobufChannel.hpp
@@ -41,27 +41,27 @@
 // TODO: This has to be defined in the slpi_proc build and in the PX4 build.
 // Make it accessible from one file to both builds.
 typedef struct {
-    int (*advertise_func_ptr)(const char *topic_name);
-    int (*subscribe_func_ptr)(const char *topic_name);
-    int (*unsubscribe_func_ptr)(const char *topic_name);
-    int (*topic_data_func_ptr)(const char *name, const uint8_t *data, int data_len_in_bytes);
-    // device::SPI::_config_spi_bus_func_t config_spi_bus;
-    // device::SPI::_spi_transfer_func_t spi_transfer;
-    int (*_config_spi_bus_func_t)();
-    int (*_spi_transfer_func_t)(int, const uint8_t*, uint8_t*, const unsigned);
-    // device::I2C::_config_i2c_bus_func_t config_i2c_bus;
-    // device::I2C::_set_i2c_address_func_t set_i2c_address;
-    // device::I2C::_i2c_transfer_func_t i2c_transfer;
-    int (*_config_i2c_bus_func_t)(uint8_t, uint8_t, uint32_t);
-    int (*_set_i2c_address_func_t)(int, uint8_t);
-    int (*_i2c_transfer_func_t)(int, const uint8_t*, const unsigned, uint8_t*, const unsigned);
-    // open_uart_func_t open_uart_func;
-    // write_uart_func_t write_uart_func;
-    // read_uart_func_t read_uart_func;
+	int (*advertise_func_ptr)(const char *topic_name);
+	int (*subscribe_func_ptr)(const char *topic_name);
+	int (*unsubscribe_func_ptr)(const char *topic_name);
+	int (*topic_data_func_ptr)(const char *name, const uint8_t *data, int data_len_in_bytes);
+	// device::SPI::_config_spi_bus_func_t config_spi_bus;
+	// device::SPI::_spi_transfer_func_t spi_transfer;
+	int (*_config_spi_bus_func_t)();
+	int (*_spi_transfer_func_t)(int, const uint8_t *, uint8_t *, const unsigned);
+	// device::I2C::_config_i2c_bus_func_t config_i2c_bus;
+	// device::I2C::_set_i2c_address_func_t set_i2c_address;
+	// device::I2C::_i2c_transfer_func_t i2c_transfer;
+	int (*_config_i2c_bus_func_t)(uint8_t, uint8_t, uint32_t);
+	int (*_set_i2c_address_func_t)(int, uint8_t);
+	int (*_i2c_transfer_func_t)(int, const uint8_t *, const unsigned, uint8_t *, const unsigned);
+	// open_uart_func_t open_uart_func;
+	// write_uart_func_t write_uart_func;
+	// read_uart_func_t read_uart_func;
 	int (*open_uart_func_t)(uint8_t, uint32_t);
-	int (*write_uart_func_t)(int, const void*, size_t);
-	int (*read_uart_func_t)(int,  void*, size_t);
-    int (*register_interrupt_callback)(int (*)(int, void*, void*), void* arg);
+	int (*write_uart_func_t)(int, const void *, size_t);
+	int (*read_uart_func_t)(int,  void *, size_t);
+	int (*register_interrupt_callback)(int (*)(int, void *, void *), void *arg);
 } fc_func_ptrs;
 
 extern "C" {
