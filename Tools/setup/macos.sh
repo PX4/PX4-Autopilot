0966ff16926c04480d158848a7f61e18c04b1309
Julian Oes <julian@oes.ch>
2022-09-06 16:40:27 +1200

    setup: attempt to fix macOS CI
    Somehow the wheel install of pymavlink fails without future, let's try
    to work around that.

--- Tools/setup/macos.sh
+++ Tools/setup/macos.sh
@@ -47,6 +47,8 @@ fi
 
 # Python dependencies
 echo "Installing PX4 Python3 dependencies"
+# We need to have future to install pymavlink later.
+python3 -m pip install future
 python3 -m pip install --user -r ${DIR}/requirements.txt
 
 # Optional, but recommended additional simulation tools:
