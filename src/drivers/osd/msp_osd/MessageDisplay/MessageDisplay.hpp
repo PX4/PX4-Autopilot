8b616bba556db6b1804848f6b38280535dd0e4e4
Daniel M. Sahu <danielmohansahu@gmail.com>
2022-09-16 12:02:41 -0400

    MSP OSD Revision

--- src/drivers/osd/msp_osd/MessageDisplay/MessageDisplay.hpp
+++ src/drivers/osd/msp_osd/MessageDisplay/MessageDisplay.hpp
@@ -31,16 +31,74 @@
  *
  ****************************************************************************/
 
+/* Class for displaying messages to a limited screen.
+ */
+
 #pragma once
 
-class MspV1
-{
-public:
-	MspV1(int fd);
-	int GetMessageSize(int message_type);
-	bool Send(const uint8_t message_id, const void *payload);
+#include <string.h>
+#include <stdint.h>
 
-private:
-	int _fd{-1};
+namespace msp_osd
+{
+
+// character size limitations
+#define MSG_BUFFER_SIZE 250
+
+// size of available characters, accounting for null terminator
+//  note: the craft_name seems to think it has 15 chars. From testing
+//        that seems incorrect
+#define FULL_MSG_LENGTH 12
+#define FULL_MSG_BUFFER 13
+
+// supported message types
+enum MessageDisplayType {
+	WARNING,
+	FLIGHT_MODE,
+	ARMING,
+	STATUS,
+	HEADING
 };
 
+// display information
+class MessageDisplay
+{
+	// working information
+	char warning_msg[MSG_BUFFER_SIZE] {""};
+	char flight_mode_msg[MSG_BUFFER_SIZE] {"???"};
+	char arming_msg[MSG_BUFFER_SIZE] {"????"};
+	char heading_msg[MSG_BUFFER_SIZE] {"??"};
+	// currently unused:
+	char status_msg[MSG_BUFFER_SIZE] {""};
+
+	// the full message and the part we're currently displaying
+	char full_message[MSG_BUFFER_SIZE] {"INITIALIZING"};
+
+	// current index we're displaying
+	uint16_t index{0};
+
+	// last update timestamp
+	uint64_t last_update_{0};
+	bool updated_{false};
+
+	// dwell duration update period (us)
+	uint64_t period_{125'000};
+	uint64_t dwell_{500'000};
+
+public:
+	MessageDisplay() = default;
+	MessageDisplay(const uint64_t period, const uint64_t dwell) : period_(period), dwell_(dwell) {}
+
+	// set the given string
+	void set(const MessageDisplayType mode, const char *string);
+
+	// get the latest subsection of the message
+	void get(char *string, const uint32_t current_time);
+
+	// update local parameters
+	void set_period(const uint64_t period) { period_ = period; }
+	void set_dwell(const uint64_t dwell) { dwell_ = dwell; }
+};
+
+
+} // namespace msp_osd
