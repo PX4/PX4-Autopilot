5fcd4ac55393db391eacc6fb1de5c110b7010822
David Sidrane <David.Sidrane@NscDg.com>
2022-10-11 00:51:34 -0700

    px4_fmu-v6:Add Revision 1 to manifest to note I2C4 is only internal

--- boards/px4/fmu-v6c/src/manifest.c
+++ boards/px4/fmu-v6c/src/manifest.c
@@ -97,8 +97,10 @@ static const px4_hw_mft_item_t hw_mft_list_v0610[] = {
 
 static px4_hw_mft_list_entry_t mft_lists[] = {
 //  ver_rev
-	{V6C00, hw_mft_list_v0600, arraySize(hw_mft_list_v0600)},
-	{V6C10, hw_mft_list_v0610, arraySize(hw_mft_list_v0610)}, // No PX4IO
+	{V6C00, hw_mft_list_v0600, arraySize(hw_mft_list_v0600)}, // Rev 0
+	{V6C01, hw_mft_list_v0600, arraySize(hw_mft_list_v0600)}, // Rev 1
+	{V6C10, hw_mft_list_v0610, arraySize(hw_mft_list_v0610)}, // Rev 0 No PX4IO
+	{V6C11, hw_mft_list_v0610, arraySize(hw_mft_list_v0610)}, // Rev 1 No PX4IO
 };
 
 /************************************************************************************
