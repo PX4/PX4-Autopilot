b707540dec6e3ddd0f95bab54e9d56f803f1e277
Silvan Fuhrer <silvan@auterion.com>
2022-11-02 15:10:57 +0100

    FW Position control: remove param FW_CLMBOUT_DIFF and instead use hardcoded 10m for clearing aborted flag
    Use kClearanceAltitudeBuffer for it, which is also used to ensure that during takeoff an
    altitude setpoint above the clearance altitdue is set.
    
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- src/modules/fw_pos_control_l1/fw_pos_control_l1_params.c
+++ src/modules/fw_pos_control_l1/fw_pos_control_l1_params.c
@@ -357,23 +357,6 @@ PARAM_DEFINE_FLOAT(FW_THR_MIN, 0.0f);
 PARAM_DEFINE_FLOAT(FW_THR_IDLE, 0.0f);
 
 /**
- * Climbout Altitude difference
- *
- * If the altitude error exceeds this parameter, the system will climb out
- * with maximum throttle and minimum airspeed until it is closer than this
- * distance to the desired altitude. Mostly used for takeoff waypoints / modes.
- * Set to 0 to disable climbout mode (not recommended).
- *
- * @unit m
- * @min 0.0
- * @max 150.0
- * @decimal 1
- * @increment 0.5
- * @group FW L1 Control
- */
-PARAM_DEFINE_FLOAT(FW_CLMBOUT_DIFF, 10.0f);
-
-/**
  * Maximum landing slope angle
  *
  * Typically the desired landing slope angle when landing configuration (flaps, airspeed) is enabled.
