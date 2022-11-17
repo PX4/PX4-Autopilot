966560edc097ba7654afc459f854a2ebfaaadb7a
Jukka Laitinen <jukkax@ssrc.tii.ae>
2022-11-17 11:44:53 +0400

    Fix overflows in abstime_to_ts
    Signed-off-by: Jukka Laitinen <jukkax@ssrc.tii.ae>

--- src/drivers/drv_hrt.h
+++ src/drivers/drv_hrt.h
@@ -149,8 +149,8 @@ static inline hrt_abstime ts_to_abstime(const struct timespec *ts)
  */
 static inline void abstime_to_ts(struct timespec *ts, hrt_abstime abstime)
 {
-	ts->tv_sec = (time_t)abstime / 1000000;
-	abstime -= (hrt_abstime)(ts->tv_sec * 1000000);
+	ts->tv_sec = (typeof(ts->tv_sec))(abstime / 1000000);
+	abstime -= (hrt_abstime)(ts->tv_sec) * 1000000;
 	ts->tv_nsec = (typeof(ts->tv_nsec))(abstime * 1000);
 }
 
