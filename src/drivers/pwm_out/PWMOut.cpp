ab4868377ef3f22a970256edf2df23c94d128310
Beat Küng <beat-kueng@gmx.net>
2022-11-22 11:56:36 +0100

    mixer_module: remove unused limit_callbacks_to_primary argument

--- src/drivers/pwm_out/PWMOut.cpp
+++ src/drivers/pwm_out/PWMOut.cpp
@@ -187,7 +187,7 @@ void PWMOut::Run()
 	}
 
 	// check at end of cycle (updateSubscriptions() can potentially change to a different WorkQueue thread)
-	_mixing_output.updateSubscriptions(true, true);
+	_mixing_output.updateSubscriptions(true);
 
 	perf_end(_cycle_perf);
 }
