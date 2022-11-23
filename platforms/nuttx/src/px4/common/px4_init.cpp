1a938570f748c6a9e680f66283e6dcf4d1525315
Ville Juven <ville.juven@unikie.com>
2022-11-23 13:22:08 +0200

    px4_init: Fix linker defined symbols after NuttX update
    For some reason, the type of the linker defined symbols was changed.
    The reasoning was "they typically represent pointers" which is absolutely
    not true at all. They typically define memory locations, but they can
    be whatever. It is just a way to provide symbols from the linker to
    the executable, which is done via relocation symbols.

--- platforms/nuttx/src/px4/common/px4_init.cpp
+++ platforms/nuttx/src/px4/common/px4_init.cpp
@@ -68,10 +68,10 @@
 
 #if !defined(CONFIG_BUILD_FLAT)
 typedef CODE void (*initializer_t)(void);
-extern initializer_t _sinit;
-extern initializer_t _einit;
-extern uint32_t _stext;
-extern uint32_t _etext;
+extern initializer_t _sinit[];
+extern initializer_t _einit[];
+extern uint8_t _stext[];
+extern uint8_t _etext[];
 
 static void cxx_initialize(void)
 {
@@ -79,7 +79,7 @@ static void cxx_initialize(void)
 
 	/* Visit each entry in the initialization table */
 
-	for (initp = &_sinit; initp != &_einit; initp++) {
+	for (initp = _sinit; initp != _einit; initp++) {
 		initializer_t initializer = *initp;
 
 		/* Make sure that the address is non-NULL and lies in the text
@@ -87,8 +87,8 @@ static void cxx_initialize(void)
 		* NULL values or counts in the initialization table.
 		*/
 
-		if ((FAR void *)initializer >= (FAR void *)&_stext &&
-		    (FAR void *)initializer < (FAR void *)&_etext) {
+		if ((FAR void *)initializer >= (FAR void *)_stext &&
+		    (FAR void *)initializer < (FAR void *)_etext) {
 			initializer();
 		}
 	}
