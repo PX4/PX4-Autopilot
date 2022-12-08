3736416d15e52a46857316c7e1208a453c7c4082
Jari Nippula <jarix@ssrc.tii.ae>
2022-12-08 14:19:47 +0200

    Parametrization of PX4_SITL simulator mavlink communication method
    TCP (server) can be selected by selecting rcS.sim_tcp_server startup file in
    px4 command line with '-s' parameter.
    
    To use legacy UDP protocol the original rcS startup file is to be used
    for '-s' param.

--- packaging/entrypoint.sh
+++ packaging/entrypoint.sh
@@ -2,4 +2,8 @@
 
 source /opt/ros/galactic/setup.bash
 
-px4 -d "/px4_sitl_etc" -w sitl_${PX4_SIM_MODEL} -s /px4_sitl_etc/init.d-posix/rcS
+if [ "$1" == "sim_tcp_server" ]; then
+    px4 -d "/px4_sitl_etc" -w sitl_${PX4_SIM_MODEL} -s /px4_sitl_etc/init.d-posix/rcS.sim_tcp_server
+else
+    px4 -d "/px4_sitl_etc" -w sitl_${PX4_SIM_MODEL} -s /px4_sitl_etc/init.d-posix/rcS
+fi
