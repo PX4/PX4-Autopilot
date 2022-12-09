8114aad98313a19945253c9d050c54cb2f5e3eff
Daniel Agar <daniel@agar.ca>
2022-12-08 23:03:44 -0500

    initial minimal PX4_ROS2 platform and px4_ros2_default build (#20689)
     - new ROS2 platform in PX4 intended for creating configs that build and run entirely in ROS2
     - PX4_CONFIG defaults to px4_ros2_default if no config specified and in a colcon workspace with ROS_VERSION=2
     - currently doesn't do much other than allow you to build px4 msgs interface package

---
+++ platforms/ros2/include/uORB/Publication.hpp
@@ -0,0 +1,148 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2020 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/**
+ * @file Publication.hpp
+ *
+ */
+
+#pragma once
+
+#include "rclcpp/rclcpp.hpp"
+
+#include "uORB.h"
+
+namespace uORB
+{
+
+template <typename U> class DefaultQueueSize
+{
+private:
+	template <typename T>
+	static constexpr uint8_t get_queue_size(decltype(T::ORB_QUEUE_LENGTH) *)
+	{
+		return T::ORB_QUEUE_LENGTH;
+	}
+
+	template <typename T> static constexpr uint8_t get_queue_size(...)
+	{
+		return 1;
+	}
+
+public:
+	static constexpr unsigned value = get_queue_size<U>(nullptr);
+};
+
+/**
+ * uORB publication wrapper class
+ */
+template<typename T, uint8_t ORB_QSIZE = DefaultQueueSize<T>::value>
+class Publication
+{
+public:
+
+	/**
+	 * Constructor
+	 *
+	 * @param meta The uORB metadata (usually from the ORB_ID() macro) for the topic.
+	 */
+	Publication(rclcpp::Node *node, ORB_ID id) : _node(*node), _id(id)
+	{
+		_publisher = _node.create_publisher<T>(::get_topic_string(_id), ORB_QSIZE); // TODO
+	}
+
+	bool advertised() { return (_publisher != nullptr); }
+
+	bool advertise()
+	{
+		if (!advertised()) {
+			_publisher = _node.create_publisher<T>(get_topic_string(_id), ORB_QSIZE);
+		}
+
+		return advertised();
+	}
+
+	/**
+	 * Publish the struct
+	 * @param data The uORB message struct we are updating.
+	 */
+	bool publish(const T &data)
+	{
+		if (!advertised()) {
+			advertise();
+		}
+
+		_publisher->publish(data);
+
+		return true;
+	}
+
+private:
+	typename rclcpp::Publisher<T>::SharedPtr _publisher{nullptr};
+
+	rclcpp::Node &_node;
+
+	const ORB_ID _id;
+};
+
+/**
+ * The publication class with data embedded.
+ */
+template<typename T>
+class PublicationData : public Publication<T>
+{
+public:
+	/**
+	 * Constructor
+	 *
+	 * @param meta The uORB metadata (usually from the ORB_ID() macro) for the topic.
+	 */
+	PublicationData(ORB_ID id) : Publication<T>(id) {}
+	//PublicationData(const orb_metadata *meta) : Publication<T>(meta) {}
+
+	T	&get() { return _data; }
+	void	set(const T &data) { _data = data; }
+
+	// Publishes the embedded struct.
+	bool	update() { return Publication<T>::publish(_data); }
+	bool	update(const T &data)
+	{
+		_data = data;
+		return Publication<T>::publish(_data);
+	}
+
+private:
+	T _data{};
+};
+
+} // namespace uORB
