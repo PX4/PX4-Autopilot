6309223e065a609ae6754fe0f2249baa44d394a7
alexklimaj <alex@arkelectron.com>
2022-12-09 12:34:02 -0700

    Increase SPI stacks by 16 bytes

--- platforms/common/include/px4_platform_common/px4_work_queue/WorkQueueManager.hpp
+++ platforms/common/include/px4_platform_common/px4_work_queue/WorkQueueManager.hpp
@@ -50,13 +50,13 @@ namespace wq_configurations
 {
 static constexpr wq_config_t rate_ctrl{"wq:rate_ctrl", 3150, 0}; // PX4 inner loop highest priority
 
-static constexpr wq_config_t SPI0{"wq:SPI0", 2336, -1};
-static constexpr wq_config_t SPI1{"wq:SPI1", 2336, -2};
-static constexpr wq_config_t SPI2{"wq:SPI2", 2336, -3};
-static constexpr wq_config_t SPI3{"wq:SPI3", 2336, -4};
-static constexpr wq_config_t SPI4{"wq:SPI4", 2336, -5};
-static constexpr wq_config_t SPI5{"wq:SPI5", 2336, -6};
-static constexpr wq_config_t SPI6{"wq:SPI6", 2336, -7};
+static constexpr wq_config_t SPI0{"wq:SPI0", 2352, -1};
+static constexpr wq_config_t SPI1{"wq:SPI1", 2352, -2};
+static constexpr wq_config_t SPI2{"wq:SPI2", 2352, -3};
+static constexpr wq_config_t SPI3{"wq:SPI3", 2352, -4};
+static constexpr wq_config_t SPI4{"wq:SPI4", 2352, -5};
+static constexpr wq_config_t SPI5{"wq:SPI5", 2352, -6};
+static constexpr wq_config_t SPI6{"wq:SPI6", 2352, -7};
 
 static constexpr wq_config_t I2C0{"wq:I2C0", 2336, -8};
 static constexpr wq_config_t I2C1{"wq:I2C1", 2336, -9};
