b66bf90a8281e8cf7599e0499064e3c7b52b6396
Eric Katzfey <eric.katzfey@modalai.com>
2023-01-13 15:20:13 -0800

    Fixed formatting and i2c config definition

--- platforms/qurt/include/px4_arch/i2c_hw_description.h
+++ platforms/qurt/include/px4_arch/i2c_hw_description.h
@@ -37,16 +37,16 @@
 
 static inline constexpr px4_i2c_bus_t initI2CBusInternal(int bus)
 {
-        px4_i2c_bus_t ret{};
-        ret.bus = bus;
-        ret.is_external = false;
-        return ret;
+	px4_i2c_bus_t ret{};
+	ret.bus = bus;
+	ret.is_external = false;
+	return ret;
 }
 
 static inline constexpr px4_i2c_bus_t initI2CBusExternal(int bus)
 {
-        px4_i2c_bus_t ret{};
-        ret.bus = bus;
-        ret.is_external = true;
-        return ret;
+	px4_i2c_bus_t ret{};
+	ret.bus = bus;
+	ret.is_external = true;
+	return ret;
 }
