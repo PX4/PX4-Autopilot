cdd750b1fa19123e98975e6e9308bb18c690f34e
Daniel Agar <daniel@agar.ca>
2023-01-21 11:38:39 -0500

    HIL/test_airframes.sh allow 'gps' and 'mavlink' status to fail
     - we're only looking for debug info if/when things go wrong later

--- Tools/HIL/test_airframes.sh
+++ Tools/HIL/test_airframes.sh
@@ -54,10 +54,10 @@ do
 	${DIR}/run_nsh_cmd.py --device ${SERIAL_DEVICE} --cmd 'ps'
 	${DIR}/run_nsh_cmd.py --device ${SERIAL_DEVICE} --cmd 'work_queue status'
 
-	${DIR}/run_nsh_cmd.py --device ${SERIAL_DEVICE} --cmd 'mavlink status'
+	${DIR}/run_nsh_cmd.py --device ${SERIAL_DEVICE} --cmd 'mavlink status' || true
 	${DIR}/run_nsh_cmd.py --device ${SERIAL_DEVICE} --cmd 'mavlink stop-all'
 
-	${DIR}/run_nsh_cmd.py --device ${SERIAL_DEVICE} --cmd 'gps status'
+	${DIR}/run_nsh_cmd.py --device ${SERIAL_DEVICE} --cmd 'gps status' || true
 	${DIR}/run_nsh_cmd.py --device ${SERIAL_DEVICE} --cmd 'gps stop'
 
 done
