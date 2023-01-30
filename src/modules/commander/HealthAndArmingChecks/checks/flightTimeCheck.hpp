05d31f63aff71776ab9ab2da545c16950965bdae
Silvan Fuhrer <silvan@auterion.com>
2023-01-30 17:28:54 +0100

    Commander: add max flight time warning (starting at 90%)
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- src/modules/commander/HealthAndArmingChecks/checks/flightTimeCheck.hpp
+++ src/modules/commander/HealthAndArmingChecks/checks/flightTimeCheck.hpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2022 PX4 Development Team. All rights reserved.
+ *   Copyright (c) 2022-2023 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -44,6 +44,8 @@ public:
 	void checkAndReport(const Context &context, Report &reporter) override;
 
 private:
+	int _last_flight_time_warning_sec{-1};
+
 	DEFINE_PARAMETERS_CUSTOM_PARENT(HealthAndArmingCheckBase,
 					(ParamInt<px4::params::COM_FLT_TIME_MAX>) _param_com_flt_time_max
 				       )
