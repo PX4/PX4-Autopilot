fcecf06e64de39f5e7888da9a3b8fe448504a056
DavidPaddy97 <96058684+DavidPaddy97@users.noreply.github.com>
2023-02-01 16:52:58 +0000

    Adding GPS_RTCM_DATA stream (#37)
    This is to inform SI2 when RTCM is being received.

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -1569,6 +1569,7 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 		configure_stream_local("GPS2_RAW", unlimited_rate);
 		configure_stream_local("GPS_GLOBAL_ORIGIN", 1.0f);
 		configure_stream_local("GPS_RAW_INT", unlimited_rate);
+		configure_stream_local("GPS_RTCM_DATA", unlimited_rate);
 		configure_stream_local("GPS_STATUS", 1.0f);
 		configure_stream_local("HOME_POSITION", 0.5f);
 		configure_stream_local("HYGROMETER_SENSOR", 1.0f);
