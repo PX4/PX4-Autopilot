xushuai5522/PX4

[TECS]: WIP Add Stall prevention function into TECS for testing. TODO remove testing code from airspeed selector to forse airspeedless in FW mode. TODO test stall prevention better

src/modules/fw_pos_control_l1/FixedwingPositionControl.cpp
@@ -481,6 +481,10 @@ FixedwingPositionControl::tecs_status_publish(float alt_sp, float equivalent_air
 	case TECS::ECL_TECS_MODE_CLIMBOUT:
 		t.mode = tecs_status_s::TECS_MODE_CLIMBOUT;
 		break;
+
+	case TECS::ECL_TECS_MODE_STALL_PREVENTION:
+		t.mode = tecs_status_s::TECS_MODE_STALL;
+		break;
 	}
 
 	t.altitude_sp = alt_sp;

PX4/PX4-Autopilot

src/modules/fw_pos_control_l1/FixedwingPositionControl.cpp
@@ -481,6 +481,10 @@ FixedwingPositionControl::tecs_status_publish(float alt_sp, float equivalent_air
 	case TECS::ECL_TECS_MODE_CLIMBOUT:
 		t.mode = tecs_status_s::TECS_MODE_CLIMBOUT;
 		break;
+
+	case TECS::ECL_TECS_MODE_STALL_PREVENTION:
+		t.mode = tecs_status_s::TECS_MODE_STALL_PREVENTION;
+		break;
 	}
 
 	t.altitude_sp = alt_sp;