ea6814d25878f27168a262045e9a091d4ed655b2
Alejandro Hernández Cordero <ahcorde@gmail.com>
2023-02-22 19:32:25 +0100

    Simulation Gazebo: Use Gazebo Airpressure sensor (#21176)
    * Simulation Gazebo: Use Gazebo Airpressure sensor
    
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>
    
    * Fixed build
    
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>
    
    * Added feedback
    
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>
    
    * make linters happy
    
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>
    
    ---------
    
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- src/modules/simulation/gz_bridge/GZBridge.hpp
+++ src/modules/simulation/gz_bridge/GZBridge.hpp
@@ -54,13 +54,14 @@
 #include <uORB/topics/vehicle_attitude.h>
 #include <uORB/topics/vehicle_global_position.h>
 #include <uORB/topics/vehicle_local_position.h>
+#include <uORB/topics/sensor_baro.h>
 
 #include <gz/math.hh>
 #include <gz/msgs.hh>
 #include <gz/transport.hh>
 
-// #include <gz/msgs/fluid_pressure.pb.h>
 #include <gz/msgs/imu.pb.h>
+#include <gz/msgs/fluid_pressure.pb.h>
 
 using namespace time_literals;
 
@@ -94,7 +95,8 @@ private:
 
 	void clockCallback(const gz::msgs::Clock &clock);
 
-	//void airpressureCallback(const gz::msgs::FluidPressure &air_pressure);
+	// void airspeedCallback(const gz::msgs::AirSpeedSensor &air_pressure);
+	void barometerCallback(const gz::msgs::FluidPressure &air_pressure);
 	void imuCallback(const gz::msgs::IMU &imu);
 	void poseInfoCallback(const gz::msgs::Pose_V &pose);
 
@@ -106,6 +108,7 @@ private:
 	uORB::Publication<vehicle_attitude_s>         _attitude_ground_truth_pub{ORB_ID(vehicle_attitude_groundtruth)};
 	uORB::Publication<vehicle_global_position_s>  _gpos_ground_truth_pub{ORB_ID(vehicle_global_position_groundtruth)};
 	uORB::Publication<vehicle_local_position_s>   _lpos_ground_truth_pub{ORB_ID(vehicle_local_position_groundtruth)};
+	uORB::PublicationMulti<sensor_baro_s> _sensor_baro_pub{ORB_ID(sensor_baro)};
 
 	uORB::PublicationMulti<sensor_accel_s> _sensor_accel_pub{ORB_ID(sensor_accel)};
 	uORB::PublicationMulti<sensor_gyro_s>  _sensor_gyro_pub{ORB_ID(sensor_gyro)};
@@ -128,6 +131,8 @@ private:
 	const std::string _model_name;
 	const std::string _model_sim;
 	const std::string _model_pose;
+
+	float _temperature{288.15};  // 15 degrees
 
 	gz::transport::Node _node;
 
