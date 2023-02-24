4492a35dd762c3db7b1479bbc134029a9f054945
Matthias Grob <maetugr@gmail.com>
2023-02-24 10:22:44 +0100

    StickAccelerationXY: improve comments

--- src/modules/flight_mode_manager/tasks/Utility/StickAccelerationXY.hpp
+++ src/modules/flight_mode_manager/tasks/Utility/StickAccelerationXY.hpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2020 PX4 Development Team. All rights reserved.
+ *   Copyright (c) 2020-2023 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -33,7 +33,7 @@
 
 /**
  * @file StickAccelerationXY.hpp
- * @brief Generate horizontal position, velocity and acceleration from stick input
+ * @brief Generate horizontal position, velocity and acceleration setpoints from stick input
  * @author Matthias Grob <maetugr@gmail.com>
  */
 
