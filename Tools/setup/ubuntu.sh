cee73d9492db3f16fdcd2058f623f8bb363fe8ee
Hamish Willee <hamishwillee@gmail.com>
2023-03-01 13:14:52 +1100

    Apply suggestions from code review

--- Tools/setup/ubuntu.sh
+++ Tools/setup/ubuntu.sh
@@ -221,8 +221,8 @@ if [[ $INSTALL_SIM == "true" ]]; then
 
 	# Gazebo / Gazebo classic installation
 	if [[ "${UBUNTU_RELEASE}" == "22.04" ]]; then
-		echo "The new Gazebo (Garden) will be installed"
-		echo "Previuos Gazebo versions will be removed"
+		echo "Gazebo (Garden) will be installed"
+		echo "Earlier versions will be removed"
 		# Add Gazebo binary repository
 		sudo wget https://packages.osrfoundation.org/gazebo.gpg -O /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
 		echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
