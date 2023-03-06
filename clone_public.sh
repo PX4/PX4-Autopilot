ac9227d1507431efd357c8168ae3bbe2a391ed2d
Jari Nippula <jarix@ssrc.tii.ae>
2023-03-06 10:04:33 +0200

    Add pf_crypto submodule

--- clone_public.sh
+++ clone_public.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 
 while read -r repo
 do
-  [[ "${repo}" == *saluki-v? ]] || [[ "${repo}" == *pfsoc_crypto ]] && continue
+  [[ "${repo}" == *saluki-v? ]] || \
+  [[ "${repo}" == *pfsoc_crypto ]]  || \
+  [[ "${repo}" == *pf_crypto ]] && continue
   git submodule update --init --recursive "${repo}"
 done <<< "$(git submodule status | awk '{print $2}')"
