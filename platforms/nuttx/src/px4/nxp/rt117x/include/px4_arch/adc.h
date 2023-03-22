98c1ed7a62f178ba1e504f73316d30a4bdd762fe
David Sidrane <David.Sidrane@NscDg.com>
2023-03-22 07:07:53 -0700

    imxrt:ADC & LPADC bifurcation and restructuring

--- platforms/nuttx/src/px4/nxp/rt117x/include/px4_arch/adc.h
+++ platforms/nuttx/src/px4/nxp/rt117x/include/px4_arch/adc.h
@@ -32,4 +32,14 @@
  ****************************************************************************/
 #pragma once
 
-#include "../../../imxrt/include/px4_arch/adc.h"
+#include <board_config.h>
+
+#if !defined(HW_REV_VER_ADC_BASE)
+#  define HW_REV_VER_ADC_BASE IMXRT_LPADC1_BASE
+#endif
+
+#if !defined(SYSTEM_ADC_BASE)
+#  define SYSTEM_ADC_BASE IMXRT_LPADC1_BASE
+#endif
+
+#include <px4_platform/adc.h>
