0d023b1fcfadaf7c303a13ff6b5453b2c2b0c692
Matthias Grob <maetugr@gmail.com>
2023-03-27 16:43:09 +0200

    control_allocator: add coaxial helicopter effectiveness
    It's now just a copy of the helicopter such that changes get well
    visible in the history.

--- src/modules/control_allocator/ControlAllocator.hpp
+++ src/modules/control_allocator/ControlAllocator.hpp
@@ -53,6 +53,7 @@
 #include <ActuatorEffectivenessCustom.hpp>
 #include <ActuatorEffectivenessUUV.hpp>
 #include <ActuatorEffectivenessHelicopter.hpp>
+#include <ActuatorEffectivenessHelicopterCoaxial.hpp>
 
 #include <ControlAllocation.hpp>
 #include <ControlAllocationPseudoInverse.hpp>
@@ -157,6 +158,7 @@ private:
 		CUSTOM = 9,
 		HELICOPTER_TAIL_ESC = 10,
 		HELICOPTER_TAIL_SERVO = 11,
+		HELICOPTER_COAXIAL = 12,
 	};
 
 	enum class FailureMode {
