87a58fab0573f4dfc6e5beaea6f6724b8af6e0d3
Silvan Fuhrer <silvan@auterion.com>
2023-04-04 10:53:17 +0200

    TECS: rename _reference_model to _altitude_reference_model
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- src/lib/tecs/TECS.hpp
+++ src/lib/tecs/TECS.hpp
@@ -642,7 +642,7 @@ public:
 				.alt_rate = altitude_rate};
 
 		// reset altitude reference model.
-		_reference_model.initialize(init_state);
+		_altitude_reference_model.initialize(init_state);
 	}
 
 	float get_pitch_setpoint() {return _control.getPitchSetpoint();}
@@ -652,9 +652,9 @@ public:
 	ECL_TECS_MODE tecs_mode() { return _tecs_mode; }
 
 private:
-	TECSControl 		_control;				///< Control submodule.
-	TECSAirspeedFilter 	_airspeed_filter;			///< Airspeed filter submodule.
-	TECSAltitudeReferenceModel 	_reference_model;			///< Setpoint reference model submodule.
+	TECSControl 			_control;			///< Control submodule.
+	TECSAirspeedFilter 		_airspeed_filter;		///< Airspeed filter submodule.
+	TECSAltitudeReferenceModel 	_altitude_reference_model;	///< Setpoint reference model submodule.
 
 	enum ECL_TECS_MODE _tecs_mode {ECL_TECS_MODE_NORMAL};		///< Current activated mode.
 
