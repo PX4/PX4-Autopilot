6b4f0566f82339f68ad28c0138b36105f732aa3b
AlexKlimaj <alex@arkelectron.com>
2023-04-05 11:21:24 -0600

    Update AFBR to 1.4.4. Remove changing measurement modes. Add parameters. Default to Long range mode"

--- src/drivers/distance_sensor/broadcom/afbrs50/Inc/platform/argus_nvm.h
+++ src/drivers/distance_sensor/broadcom/afbrs50/Inc/platform/argus_nvm.h
@@ -1,11 +1,11 @@
 /*************************************************************************//**
  * @file
- * @brief    	This file is part of the AFBR-S50 API.
- * @details		This file provides an interface for the optional non-volatile memory.
+ * @brief       This file is part of the AFBR-S50 API.
+ * @details     This file provides an interface for the optional non-volatile memory.
  *
  * @copyright
  *
- * Copyright (c) 2021, Broadcom Inc
+ * Copyright (c) 2023, Broadcom Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -36,99 +36,113 @@
 
 #ifndef ARGUS_NVM_H
 #define ARGUS_NVM_H
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 /*!***************************************************************************
- * @defgroup	argus_nvm NVM: Non-Volatile Memory Layer
- * @ingroup		argus_platform
+ * @defgroup    argus_nvm NVM: Non-Volatile Memory Layer
+ * @ingroup     argus_hal
  *
- * @brief		Non-Volatile Memory Layer
+ * @brief       Non-Volatile Memory Layer
  *
- * @details		This module provides functionality to access the non-volatile
- * 				memory (e.g. flash) on the underlying platform.
+ * @details     This module provides functionality to access the non-volatile
+ *              memory (e.g. flash) on the underlying platform.
  *
- * 				This module is optional and only required if calibration data
- * 				needs to be stored within the API.
+ *              This module is optional and only required if calibration data
+ *              needs to be stored within the API.
  *
- * @note		The implementation of this module is optional for the correct
- * 				execution of the API. If not implemented, a weak implementation
- * 				within the API will be used that disables the NVM feature.
+ * @note        The implementation of this module is optional for the correct
+ *              execution of the API. If not implemented, a weak implementation
+ *              within the API will be used that disables the NVM feature.
  *
- * @addtogroup 	argus_nvm
+ * @addtogroup  argus_nvm
  * @{
  *****************************************************************************/
 
-#include "argus.h"
+#include "api/argus_def.h"
+
+/*! The NVM block size in the non-volatile memory. */
+#define ARGUS_NVM_BLOCK_SIZE 0x300 // 768 bytes
 
 /*!***************************************************************************
- * @brief	Initializes the non-volatile memory unit and reserves a chunk of memory.
+ * @brief   Write a block of data to the non-volatile memory.
  *
- * @details The function is called upon API initialization sequence. If available,
- * 			the non-volatile memory module reserves a chunk of memory with the
- * 			provides number of bytes (size) and returns with #STATUS_OK.
+ * @details The function is called whenever the API wants to write data into
+ *          non-volatile memory, e.g. flash. Later, the API reads the written
+ *          data via the #NVM_ReadBlock function.
  *
- * 			If not implemented, the function should return #ERROR_NOT_IMPLEMENTED
- * 			in oder to inform the API to not use the NVM module.
+ *          The data shall be written to a specified memory block that is
+ *          uniquely dedicated to each individual device. The /p id parameter
+ *          is passed to the function that identifies the device. The /p id
+ *          is composed of the device ID and module type, i.e. it is unique
+ *          among all devices. If only a single device is used anyway, the
+ *          /p id parameter can be ignored.
  *
- * 			After initialization, the API calls the #NVM_Write and #NVM_Read
- * 			methods to write within the reserved chunk of memory.
+ *          If no NVM module is available, the function can return with error
+ *          #ERROR_NOT_IMPLEMENTED and the API ignores the NVM.
  *
- * @note	The implementation of this function is optional for the correct
- * 			execution of the API. If not implemented, a weak implementation
- * 			within the API will be used that disables the NVM feature.
+ *          If write fails, e.g. due to lack of memory, a negative status
+ *          must be returned, e.g. #ERROR_NVM_OUT_OF_RANGE.
  *
- * @param	size The required size of NVM to store all parameters.
- * @return 	Returns the \link #status_t status\endlink (#STATUS_OK on success).
+ *          The block size is fixed for a single device. The actual block size
+ *          is defined with #ARGUS_NVM_BLOCK_SIZE.
+ *
+ * @note    The implementation of this function is optional for the correct
+ *          execution of the API. If not implemented, a weak implementation
+ *          within the API will be used that disables the NVM feature.
+ *
+ * @param   id The 32-bit ID number to identify the corresponding memory block.
+ * @param   block_size The number of bytes to be written. Note that this value
+ *                     is fixed, i.e. the API always writes the same data size.
+ *                     The size is defined here: #ARGUS_NVM_BLOCK_SIZE.
+ * @param   buf The pointer to the data buffer of size /p block_size that needs
+ *              to be written to the NVM.
+ * @return  Returns the \link #status_t status\endlink (#STATUS_OK on success).
  *****************************************************************************/
-status_t NVM_Init(uint32_t size);
+status_t NVM_WriteBlock(uint32_t id, uint32_t block_size, uint8_t const *buf);
 
 /*!***************************************************************************
- * @brief	Write a block of data to the non-volatile memory.
+ * @brief   Reads a block of data from the non-volatile memory.
  *
- * @details	The function is called whenever the API wants to write data into
- * 			the previously reserved (#NVM_Init) memory block. The data shall
- * 			be written at a given offset and with a given size.
+ * @details The function is called whenever the API wants to read data from
+ *          non-volatile memory, e.g. flash. The data will be previously
+ *          stored using the #NVM_WriteBlock function. Otherwise, the function
+ *          must return a corresponding error code, namely #ERROR_NVM_EMPTY.
  *
- *			If no NVM module is available, the function can return with error
- *			#ERROR_NOT_IMPLEMENTED.
+ *          The data shall be read from a specified memory block that is
+ *          uniquely dedicated to each individual device. The /p id parameter
+ *          is passed to the function that identifies the device. The /p id
+ *          is composed of the device ID and module type, i.e. it is unique
+ *          among all devices. If only a single device is used anyway, the
+ *          /p id parameter can be ignored.
  *
- * @note	The implementation of this function is optional for the correct
- * 			execution of the API. If not implemented, a weak implementation
- * 			within the API will be used that disables the NVM feature.
+ *          If no NVM module is available, the function can return with error
+ *          #ERROR_NOT_IMPLEMENTED and the API ignores the NVM.
  *
- * @param	offset The index offset where the first byte needs to be written.
- * @param	size The number of bytes to be written.
- * @param	buf The pointer to the data buffer with the data to be written.
- * @return 	Returns the \link #status_t status\endlink (#STATUS_OK on success).
+ *          If read fails, e.g. if data has not been written previously,
+ *          a negative status must be returned, e.g. #ERROR_NVM_EMPTY if no
+ *          data has been written yet or any other negative error else-wise.
+ *
+ *          The block size is fixed for a single device. The actual block size
+ *          is defined with #ARGUS_NVM_BLOCK_SIZE.
+ *
+ * @note    The implementation of this function is optional for the correct
+ *          execution of the API. If not implemented, a weak implementation
+ *          within the API will be used that disables the NVM feature.
+ *
+ * @param   id The 32-bit ID number to identify the corresponding memory block.
+ * @param   block_size The number of bytes to be read. Note that this value
+ *                     is fixed, i.e. the API always reads the same data size.
+ *                     The size is defined here: #ARGUS_NVM_BLOCK_SIZE.
+ * @param   buf The pointer to the data buffer of size /p block_size to copy
+ *              the data to.
+ * @return  Returns the \link #status_t status\endlink (#STATUS_OK on success).
  *****************************************************************************/
-status_t NVM_Write(uint32_t offset, uint32_t size, uint8_t const *buf);
-
-/*!***************************************************************************
- * @brief	Reads a block of data from the non-volatile memory.
- *
- * @details	The function is called whenever the API wants to read data from
- * 			the previously reserved (#NVM_Init) memory block. The data shall
- * 			be read at a given offset and with a given size.
- *
- *			If no NVM module is available, the function can return with error
- *			#ERROR_NOT_IMPLEMENTED.
- *
- * @note	The implementation of this function is optional for the correct
- * 			execution of the API. If not implemented, a weak implementation
- * 			within the API will be used that disables the NVM feature.
- *
- * @param	offset The index offset where the first byte needs to be read.
- * @param	size The number of bytes to be read.
- * @param	buf The pointer to the data buffer to copy the data to.
- * @return 	Returns the \link #status_t status\endlink (#STATUS_OK on success).
- *****************************************************************************/
-status_t NVM_Read(uint32_t offset, uint32_t size, uint8_t *buf);
-#ifdef __cplusplus
-}
-#endif
+status_t NVM_ReadBlock(uint32_t id, uint32_t block_size, uint8_t *buf);
 
 /*! @} */
+#ifdef __cplusplus
+} // extern "C"
+#endif
 #endif // ARGUS_NVM_H
