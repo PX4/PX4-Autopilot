f8445a25e0caf378860e829bce615ced5591ccd3
Thomas Stastny <thomas.stastny@auterion.com>
2023-04-19 11:31:34 +0200

    drivers/gps: increase stack to handle gps dump config for RTCM output (PPK)

--- src/drivers/gps/gps.cpp
+++ src/drivers/gps/gps.cpp
@@ -108,7 +108,7 @@ struct GPS_Sat_Info {
 	satellite_info_s _data;
 };
 
-static constexpr int TASK_STACK_SIZE = PX4_STACK_ADJUSTED(1990);
+static constexpr int TASK_STACK_SIZE = PX4_STACK_ADJUSTED(2040);
 
 
 class GPS : public ModuleBase<GPS>, public device::Device
