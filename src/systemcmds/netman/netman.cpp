94da8aefa381d5b7b748f1c0989525044ff088ff
Ramon Roche <mrpollo@gmail.com>
2023-05-31 12:42:54 -0700

    Update netman.cpp

--- src/systemcmds/netman/netman.cpp
+++ src/systemcmds/netman/netman.cpp
@@ -407,15 +407,16 @@ static void usage(const char *reason)
   system rebooted.
   
   ### update
-  `netman update` is run automatically by the startup script.  the `update` option will check for the existence of
-  `net.cfg` in the root of the SD Card. It will saves the network settings
-  from `net.cfg` in non-volatile memory, delete the file and reboot the system.
+  `netman update` is run automatically by the startup script. When run,
+  the `update` option will check for the existence of `net.cfg` in the
+  root of the SD Card. It will saves the network settings from `net.cfg`
+  in non-volatile memory, delete the file and reboot the system.
 
   ### save
-  The `save` option will save settings from non-volatile memory to a file named `net.cfg` on the SD Card filesystem for editing.
-  Use this to edit the settings. 
-  save does not immediately apply the network settings; it depends on the
-  user rebooting; Where the `update` command is run in the start-up sequence and saves the settings.
+  The `save` option will save settings from non-volatile memory to a file named
+  `net.cfg` on the SD Card filesystem for editing. Use this to edit the settings. 
+  Save does not immediately apply the network settings; it depends on the user
+  rebooting; Where the `update` command is run by the start-up script and also saves the settings.
   
     
   ### show
