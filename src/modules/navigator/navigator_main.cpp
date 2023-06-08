bbd9c4c70aaa779bfa0f68c5a38a9115a474eee0
Silvan Fuhrer <silvan@auterion.com>
2023-06-08 10:36:19 +0200

    Navigator: move disable camera trigger on mission inactivation to seperate function
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- src/modules/navigator/navigator_main.cpp
+++ src/modules/navigator/navigator_main.cpp
@@ -1527,6 +1527,18 @@ void Navigator::mode_completed(uint8_t nav_state, uint8_t result)
 	_mode_completed_pub.publish(mode_completed);
 }
 
+
+void Navigator::disable_camera_trigger()
+{
+	// Disable camera trigger
+	vehicle_command_s cmd {};
+	cmd.command = vehicle_command_s::VEHICLE_CMD_DO_TRIGGER_CONTROL;
+	// Pause trigger
+	cmd.param1 = -1.0f;
+	cmd.param3 = 1.0f;
+	publish_vehicle_cmd(&cmd);
+}
+
 int Navigator::print_usage(const char *reason)
 {
 	if (reason) {
