89de325e4b1b52a6e1ade157bd8a0d72d197d00c
David Sidrane <David.Sidrane@NscDg.com>
2023-06-16 23:34:21 -0700

    nxp_fmurt1170:Bring in ROMAPI

--- boards/nxp/fmurt1170-v1/src/init.c
+++ boards/nxp/fmurt1170-v1/src/init.c
@@ -47,6 +47,7 @@
 
 #include "board_config.h"
 
+#include <barriers.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
@@ -64,8 +65,9 @@
 #include <nuttx/mm/gran.h>
 
 #include "arm_internal.h"
-#include "arm_internal.h"
 #include "imxrt_flexspi_nor_boot.h"
+#include "imxrt_flexspi_nor_flash.h"
+#include "imxrt_romapi.h"
 #include "imxrt_iomuxc.h"
 #include <chip.h>
 #include "board_config.h"
@@ -149,7 +151,39 @@ __EXPORT void board_on_reset(int status)
 	}
 }
 
+#if defined(CONFIG_BOARD_BOOTLOADER_FIXUP)
+/****************************************************************************
+ * Name: imxrt_octl_flash_initialize
+ *
+ * Description:
+ *
+ ****************************************************************************/
 
+locate_code(".ramfunc")
+void imxrt_octl_flash_initialize(void)
+{
+	const uint32_t instance =  1;
+	serial_nor_config_option_t option_1_8bit = {
+		.option0.U = 0xC0403007,
+		.option1.U = 0U,
+	};
+
+	struct flexspi_nor_config_s norConfig;
+
+	ROM_API_Init();
+
+
+	int32_t status = ROM_FLEXSPI_NorFlash_GetConfig(instance, &norConfig, &option_1_8bit);
+
+	if (status == OK) {
+		status = ROM_FLEXSPI_NorFlash_Init(instance, &norConfig);
+	}
+
+	ARM_DSB();
+	ARM_ISB();
+	ARM_DMB();
+}
+#endif
 /****************************************************************************
  * Name: imxrt_ocram_initialize
  *
@@ -206,10 +240,10 @@ __EXPORT void imxrt_ocram_initialize(void)
 
 __EXPORT void imxrt_boardinitialize(void)
 {
-
-#if !defined(CONFIG_BOOT_RUNFROMISRAM)
-	imxrt_ocram_initialize();
+#if defined(CONFIG_BOARD_BOOTLOADER_FIXUP)
+	imxrt_octl_flash_initialize();
 #endif
+
 
 	board_on_reset(-1); /* Reset PWM first thing */
 
@@ -253,6 +287,7 @@ __EXPORT void imxrt_boardinitialize(void)
  *   any failure to indicate the nature of the failure.
  *
  ****************************************************************************/
+volatile bool g_debug_loop_on_fault = true;
 
 __EXPORT int board_app_initialize(uintptr_t arg)
 {
@@ -264,6 +299,13 @@ __EXPORT int board_app_initialize(uintptr_t arg)
 
 
 	int ret = OK;
+
+	ret = imxrt_flexspi_fram_initialize();
+
+	if (ret < 0) {
+		syslog(LOG_ERR,
+		       "ERROR: imxrt_flexspi_nor_initialize failed: %d\n", ret);
+	}
 
 	/* Need hrt running before using the ADC */
 
@@ -298,7 +340,7 @@ __EXPORT int board_app_initialize(uintptr_t arg)
 		syslog(LOG_ERR, "[boot] DMA alloc FAILED\n");
 	}
 
-#if defined(SERIAL_HAVE_RXDMA)
+#if defined(SERIAL_HAVE_RXDMA) // Needed???
 	// set up the serial DMA polling at 1ms intervals for received bytes that have not triggered a DMA event.
 	static struct hrt_call serial_dma_call;
 	hrt_call_every(&serial_dma_call, 1000, 1000, (hrt_callout)imxrt_serial_dma_poll, NULL);
@@ -319,13 +361,5 @@ __EXPORT int board_app_initialize(uintptr_t arg)
 	}
 
 #endif
-
-	ret = imxrt_flexspi_fram_initialize();
-
-	if (ret < 0) {
-		syslog(LOG_ERR,
-		       "ERROR: imxrt_flexspi_nor_initialize failed: %d\n", ret);
-	}
-
 	return ret;
 }
