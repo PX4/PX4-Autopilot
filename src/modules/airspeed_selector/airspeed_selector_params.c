29714a0fbaa1e78309239f2cb4d307d8bdc14843
Silvan Fuhrer <silvan@auterion.com>
2023-06-26 18:35:50 +0200

    AirspeedValidator: make wind estimate more dynamic
    Increase wind process noise default (ASPD_WIND_NSD) and gate
    (ASPD_TAS_GATE) to be able to catch rapid wind increases with
    the internal wind estimator of the airspeed validator.
    
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- src/modules/airspeed_selector/airspeed_selector_params.c
+++ src/modules/airspeed_selector/airspeed_selector_params.c
@@ -11,7 +11,7 @@
  * @decimal 2
  * @group Airspeed Validator
  */
-PARAM_DEFINE_FLOAT(ASPD_WIND_NSD, 1.e-2f);
+PARAM_DEFINE_FLOAT(ASPD_WIND_NSD, 1.e-1f);
 
 /**
  * Wind estimator true airspeed scale process noise spectral density
@@ -63,7 +63,7 @@ PARAM_DEFINE_FLOAT(ASPD_BETA_NOISE, 0.15f);
  * @unit SD
  * @group Airspeed Validator
  */
-PARAM_DEFINE_INT32(ASPD_TAS_GATE, 3);
+PARAM_DEFINE_INT32(ASPD_TAS_GATE, 4);
 
 /**
  * Gate size for sideslip angle fusion
