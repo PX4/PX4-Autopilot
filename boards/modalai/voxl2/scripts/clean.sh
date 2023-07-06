34c00c659db6249bb7d0540bb3a979126a10025b
Eric Katzfey <eric.katzfey@modalai.com>
2023-07-06 09:05:26 -0700

    Changed the clean convenience script to remove the build directory

--- boards/modalai/voxl2/scripts/clean.sh
+++ boards/modalai/voxl2/scripts/clean.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 # Clean out the build artifacts
-source /home/build-env.sh
-make clean
+# source /home/build-env.sh
+# make clean
+
+# This gets rid of everything and starts fresh
+sudo rm -fR build
