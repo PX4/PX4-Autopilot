41a57bc30a40c42990995d5b4e8fe72389f66902
Eric Katzfey <eric.katzfey@modalai.com>
2023-07-17 11:40:23 -0700

    Changed default setting of navigation mode to manual so that it doesn't need to be commanded in the startup script

--- src/modules/commander/UserModeIntention.hpp
+++ src/modules/commander/UserModeIntention.hpp
@@ -73,8 +73,8 @@ private:
 	const vehicle_status_s &_vehicle_status;
 	const HealthAndArmingChecks &_health_and_arming_checks;
 
-	uint8_t _user_intented_nav_state{vehicle_status_s::NAVIGATION_STATE_AUTO_LOITER}; ///< Current user intended mode
-	uint8_t _nav_state_after_disarming{vehicle_status_s::NAVIGATION_STATE_AUTO_LOITER}; ///< Mode that is switched into after landing/disarming
+	uint8_t _user_intented_nav_state{vehicle_status_s::NAVIGATION_STATE_MANUAL}; ///< Current user intended mode
+	uint8_t _nav_state_after_disarming{vehicle_status_s::NAVIGATION_STATE_MANUAL}; ///< Mode that is switched into after landing/disarming
 
 	bool _ever_had_mode_change{false}; ///< true if there was ever a mode change call (also if the same mode as already set)
 	bool _had_mode_change{false}; ///< true if there was a mode change call since the last getHadModeChangeAndClear()
