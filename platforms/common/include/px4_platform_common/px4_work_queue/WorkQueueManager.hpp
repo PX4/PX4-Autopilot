tiiuae/px4-firmware

Increase navigator and wq stacks
These give warnings, which we want to remove

Signed-off-by: Jukka Laitinen <jukkax@ssrc.tii.ae>

platforms/common/include/px4_platform_common/px4_work_queue/WorkQueueManager.hpp
@@ -48,7 +48,7 @@ struct wq_config_t {
 
 namespace wq_configurations
 {
-static constexpr wq_config_t rate_ctrl{"wq:rate_ctrl", 1952, 0}; // PX4 inner loop highest priority
+static constexpr wq_config_t rate_ctrl{"wq:rate_ctrl", 2052, 0}; // PX4 inner loop highest priority
 static constexpr wq_config_t ctrl_alloc{"wq:ctrl_alloc", 9500, 0}; // PX4 control allocation, same priority as rate_ctrl
 
 static constexpr wq_config_t SPI0{"wq:SPI0", 2336, -1};
@@ -77,15 +77,15 @@ static constexpr wq_config_t hp_default{"wq:hp_default", 1900, -18};
 
 static constexpr wq_config_t uavcan{"wq:uavcan", 3624, -19};
 
-static constexpr wq_config_t UART0{"wq:UART0", 1632, -21};
-static constexpr wq_config_t UART1{"wq:UART1", 1632, -22};
-static constexpr wq_config_t UART2{"wq:UART2", 1632, -23};
-static constexpr wq_config_t UART3{"wq:UART3", 1632, -24};
-static constexpr wq_config_t UART4{"wq:UART4", 1632, -25};
-static constexpr wq_config_t UART5{"wq:UART5", 1632, -26};
-static constexpr wq_config_t UART6{"wq:UART6", 1632, -27};
-static constexpr wq_config_t UART7{"wq:UART7", 1632, -28};
-static constexpr wq_config_t UART8{"wq:UART8", 1632, -29};
+static constexpr wq_config_t UART0{"wq:UART0", 1732, -21};
+static constexpr wq_config_t UART1{"wq:UART1", 1732, -22};
+static constexpr wq_config_t UART2{"wq:UART2", 1732, -23};
+static constexpr wq_config_t UART3{"wq:UART3", 1732, -24};
+static constexpr wq_config_t UART4{"wq:UART4", 1732, -25};
+static constexpr wq_config_t UART5{"wq:UART5", 1732, -26};
+static constexpr wq_config_t UART6{"wq:UART6", 1732, -27};
+static constexpr wq_config_t UART7{"wq:UART7", 1732, -28};
+static constexpr wq_config_t UART8{"wq:UART8", 1732, -29};
 static constexpr wq_config_t UART_UNKNOWN{"wq:UART_UNKNOWN", 1632, -30};
 
 static constexpr wq_config_t lp_default{"wq:lp_default", 1920, -50};

PX4/PX4-Autopilot

platforms/common/include/px4_platform_common/px4_work_queue/WorkQueueManager.hpp
@@ -76,18 +76,18 @@ static constexpr wq_config_t hp_default{"wq:hp_default", 1900, -18};
 
 static constexpr wq_config_t uavcan{"wq:uavcan", 3624, -19};
 
-static constexpr wq_config_t ttyS0{"wq:ttyS0", 1728, -21};
-static constexpr wq_config_t ttyS1{"wq:ttyS1", 1728, -22};
-static constexpr wq_config_t ttyS2{"wq:ttyS2", 1728, -23};
-static constexpr wq_config_t ttyS3{"wq:ttyS3", 1728, -24};
-static constexpr wq_config_t ttyS4{"wq:ttyS4", 1728, -25};
-static constexpr wq_config_t ttyS5{"wq:ttyS5", 1728, -26};
-static constexpr wq_config_t ttyS6{"wq:ttyS6", 1728, -27};
-static constexpr wq_config_t ttyS7{"wq:ttyS7", 1728, -28};
-static constexpr wq_config_t ttyS8{"wq:ttyS8", 1728, -29};
-static constexpr wq_config_t ttyS9{"wq:ttyS9", 1728, -30};
-static constexpr wq_config_t ttyACM0{"wq:ttyACM0", 1728, -31};
-static constexpr wq_config_t ttyUnknown{"wq:ttyUnknown", 1728, -32};
+static constexpr wq_config_t ttyS0{"wq:ttyS0", 1732, -21};
+static constexpr wq_config_t ttyS1{"wq:ttyS1", 1732, -22};
+static constexpr wq_config_t ttyS2{"wq:ttyS2", 1732, -23};
+static constexpr wq_config_t ttyS3{"wq:ttyS3", 1732, -24};
+static constexpr wq_config_t ttyS4{"wq:ttyS4", 1732, -25};
+static constexpr wq_config_t ttyS5{"wq:ttyS5", 1732, -26};
+static constexpr wq_config_t ttyS6{"wq:ttyS6", 1732, -27};
+static constexpr wq_config_t ttyS7{"wq:ttyS7", 1732, -28};
+static constexpr wq_config_t ttyS8{"wq:ttyS8", 1732, -29};
+static constexpr wq_config_t ttyS9{"wq:ttyS9", 1732, -30};
+static constexpr wq_config_t ttyACM0{"wq:ttyACM0", 1732, -31};
+static constexpr wq_config_t ttyUnknown{"wq:ttyUnknown", 1732, -32};
 
 static constexpr wq_config_t lp_default{"wq:lp_default", 1920, -50};
 