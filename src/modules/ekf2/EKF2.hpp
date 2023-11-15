429401ccc7afb570eb7261e8b65073fd22e5aff6
Daniel Agar <daniel@agar.ca>
2023-11-15 15:55:27 -0500

    ekf2: purge remaining EKF2_AID_MASK references

--- src/modules/ekf2/EKF2.hpp
+++ src/modules/ekf2/EKF2.hpp
@@ -156,19 +156,6 @@ public:
 	int instance() const { return _instance; }
 
 private:
-	//TODO: remove after 1.14 release
-	enum SensorFusionMask : uint16_t {
-		// Bit locations for fusion_mode
-		DEPRECATED_USE_GPS = (1 << 0),  ///< set to true to use GPS data (DEPRECATED, use gnss_ctrl)
-		DEPRECATED_USE_OPT_FLOW     = (1 << 1), ///< set to true to use optical flow data
-		DEPRECATED_INHIBIT_ACC_BIAS = (1 << 2), ///< set to true to inhibit estimation of accelerometer delta velocity bias
-		DEPRECATED_USE_EXT_VIS_POS = (1 << 3), ///< set to true to use external vision position data
-		DEPRECATED_USE_EXT_VIS_YAW = (1 << 4), ///< set to true to use external vision quaternion data for yaw
-		DEPRECATED_USE_DRAG         = (1 << 5), ///< set to true to use the multi-rotor drag model to estimate wind
-		DEPRECATED_ROTATE_EXT_VIS  = (1 << 6), ///< set to true to if the EV observations are in a non NED reference frame and need to be rotated before being used
-		DEPRECATED_USE_GPS_YAW     = (1 << 7), ///< set to true to use GPS yaw data if available (DEPRECATED, use gnss_ctrl)
-		DEPRECATED_USE_EXT_VIS_VEL = (1 << 8), ///< set to true to use external vision velocity data
-	};
 
 	static constexpr uint8_t MAX_NUM_IMUS = 4;
 	static constexpr uint8_t MAX_NUM_MAGS = 4;
@@ -642,9 +629,6 @@ private:
 		(ParamExtInt<px4::params::EKF2_SYNT_MAG_Z>) _param_ekf2_synthetic_mag_z,
 #endif // CONFIG_EKF2_MAGNETOMETER
 
-		// measurement source control
-		(ParamInt<px4::params::EKF2_AID_MASK>)
-		_param_ekf2_aid_mask,		///< bitmasked integer that selects which of the GPS and optical flow aiding sources will be used
 		(ParamExtInt<px4::params::EKF2_HGT_REF>) _param_ekf2_hgt_ref,    ///< selects the primary source for height data
 
 		(ParamExtInt<px4::params::EKF2_NOAID_TOUT>)
