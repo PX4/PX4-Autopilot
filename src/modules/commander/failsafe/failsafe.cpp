8e1c3e8093e3da0dbe9a276eb01d2672d7cd190b
Silvan Fuhrer <silvan@auterion.com>
2023-11-16 14:33:44 +0100

    Geofence: rework messaging and notification
    - do reporting of breaching in-air only in geofenceCheck
    - remove geofence_violation_reason_t
    - replace geofence_breached field in GeofenceResult.msg with 3 fields
    (one for each GF type: max dist, max alt, custom geofence)
    - the warning message after breaching a GF is only done by Commander,
    and it's specific to the GF type failure
    
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- src/modules/commander/failsafe/failsafe.cpp
+++ src/modules/commander/failsafe/failsafe.cpp
@@ -442,7 +442,7 @@ void Failsafe::checkStateAndMode(const hrt_abstime &time_us, const State &state,
 		CHECK_FAILSAFE(status_flags, local_position_accuracy_low, ActionOptions(Action::RTL));
 	}
 
-	CHECK_FAILSAFE(status_flags, primary_geofence_breached, fromGfActParam(_param_gf_action.get()).cannotBeDeferred());
+	CHECK_FAILSAFE(status_flags, geofence_breached, fromGfActParam(_param_gf_action.get()).cannotBeDeferred());
 
 	// Battery
 	CHECK_FAILSAFE(status_flags, battery_low_remaining_time,
