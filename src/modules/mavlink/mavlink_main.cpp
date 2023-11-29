12e420deb4196fd3d20b11a7f84e265f6575fb9c
Jacob Dahl <dahl.jakejacob@gmail.com>
2023-11-29 13:10:22 -0900

    added 3 retries for opening serial port in mavlink, removed sleep before sercon

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -2214,11 +2214,23 @@ Mavlink::task_main(int argc, char *argv[])
 
 	/* open the UART device after setting the instance, as it might block */
 	if (get_protocol() == Protocol::SERIAL) {
-		_uart_fd = mavlink_open_uart(_baudrate, _device_name, _flow_control);
 
-		if (_uart_fd < 0) {
-			PX4_ERR("could not open %s", _device_name);
-			return PX4_ERROR;
+		// NOTE: we attempt to open the port multiple times due to sercon returning before
+		// the port is ready to be opened. This avoids needing to sleep() after sercon_main.
+		int attempts = 0;
+		static const int max_attempts = 3;
+
+		while (_uart_fd < 0) {
+			_uart_fd = mavlink_open_uart(_baudrate, _device_name, _flow_control);
+			attempts++;
+
+			if (_uart_fd < 0 && attempts < max_attempts) {
+				PX4_ERR("could not open %s, retrying", _device_name);
+				px4_usleep(1_s);
+			} else if (_uart_fd < 0) {
+				PX4_ERR("failed to open %s after %d attempts, exiting!", _device_name, attempts);
+				return PX4_ERROR;
+			}
 		}
 	}
 
