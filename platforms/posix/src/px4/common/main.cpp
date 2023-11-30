5a7f7569441c2cf4aace6610b438cb9fe1168d43
johntgz <john_tanguanzhong@hotmail.com>
2023-11-30 15:09:51 +0800

    added checking of path and initialized default default working_directory only if PX4_SOURCE_DIR is defined

--- platforms/posix/src/px4/common/main.cpp
+++ platforms/posix/src/px4/common/main.cpp
@@ -279,15 +279,14 @@ int main(int argc, char **argv)
 		if (test_data_path.empty() && dir_exists(PX4_SOURCE_DIR"/test_data")) {
 			test_data_path = PX4_SOURCE_DIR"/test_data";
 		}
+		if (working_directory.empty() && dir_exists(PX4_SOURCE_DIR)){
+			working_directory = PX4_SOURCE_DIR"/build/px4_sitl_default/rootfs";
+		}
 
 #endif // PX4_SOURCE_DIR
 
 		if (commands_file.empty()) {
 			commands_file = "etc/init.d-posix/rcS";
-		}
-
-		if (working_directory.empty()){
-			working_directory = PX4_SOURCE_DIR"/build/px4_sitl_default/rootfs";
 		}
 
 		// change the CWD befre setting up links and other directories
