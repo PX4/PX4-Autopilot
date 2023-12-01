d5a6474d32403810a303598af339c307ae652456
Eric Katzfey <eric.katzfey@modalai.com>
2023-12-01 11:29:07 -0800

    Added apps_sbus driver to support M0065 SBUS on apps processor for M0052

--- boards/modalai/voxl2/scripts/install-voxl.sh
+++ boards/modalai/voxl2/scripts/install-voxl.sh
@@ -127,6 +127,7 @@ adb shell "cd /usr/bin; /bin/ln -f -s px4 px4-rc_controller"
 adb shell "cd /usr/bin; /bin/ln -f -s px4 px4-uart_esc_driver"
 adb shell "cd /usr/bin; /bin/ln -f -s px4 px4-flight_mode_manager"
 adb shell "cd /usr/bin; /bin/ln -f -s px4 px4-imu_server"
+adb shell "cd /usr/bin; /bin/ln -f -s px4 px4-apps_sbus"
 
 # Make sure any required directories exist
 adb shell "/bin/mkdir -p /data/px4/param"
