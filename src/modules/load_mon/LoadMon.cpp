c4098abd041c28f371e491867d1e43723d7af396
Zachary Lowell <lowellzachary@gmail.com>
2023-12-04 12:43:49 -0600

    Added second instance of cpu load so that both DSP and Apps can report and log it.

--- src/modules/load_mon/LoadMon.cpp
+++ src/modules/load_mon/LoadMon.cpp
@@ -224,14 +224,17 @@ void LoadMon::cpuload()
 	}
 
 	cpuload.load = interval_spent_time / interval;
+	strncpy(cpuload.platform, "POSIX", sizeof(cpuload.platform));
 #elif defined(__PX4_NUTTX)
 	// get ram usage
 	struct mallinfo mem = mallinfo();
 	cpuload.ram_usage = (float)mem.uordblks / mem.arena;
 	cpuload.load = 1.f - interval_idletime / interval;
+	strncpy(cpuload.platform, "NUTTX", sizeof(cpuload.platform));
 #elif defined(__PX4_QURT)
 	cpuload.ram_usage = 0.0f;
 	cpuload.load = px4muorb_get_cpu_load() / 100.0f;
+	strncpy(cpuload.platform, "QURT", sizeof(cpuload.platform));
 #endif
 	cpuload.timestamp = hrt_absolute_time();
 
