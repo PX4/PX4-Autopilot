97423136d147330aba92e7c36ee02adcaa0fcbdc
bresch <brescianimathieu@gmail.com>
2023-12-05 11:32:34 +0100

    ekf2-AGP: scope control enum

--- src/modules/ekf2/EKF/aux_global_position.hpp
+++ src/modules/ekf2/EKF/aux_global_position.hpp
@@ -88,7 +88,7 @@ private:
 	RingBuffer<AuxGlobalPositionSample> _aux_global_position_buffer{20}; // TODO: size with _obs_buffer_length and actual publication rate
 	uint64_t _time_last_buffer_push{0};
 
-	enum Ctrl : uint8_t {
+	enum class Ctrl : uint8_t {
 		HPOS  = (1<<0),
 		VPOS  = (1<<1)
 	};
