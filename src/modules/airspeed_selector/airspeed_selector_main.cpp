7353d66e742846be10cdefa9882711059b5a1681
Silvan Fuhrer <silvan@auterion.com>
2023-12-06 15:08:54 +0100

    AirspeedSelector: remove option to disable airspeed sensor through ASPD_PRIMARY
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- src/modules/airspeed_selector/airspeed_selector_main.cpp
+++ src/modules/airspeed_selector/airspeed_selector_main.cpp
@@ -600,9 +600,8 @@ void AirspeedModule::select_airspeed_and_publish()
 	}
 
 	// check if airspeed based on ground-wind speed is valid and can be published
-	if (_param_airspeed_primary_index.get() > airspeed_index::DISABLED_INDEX &&
-	    (_valid_airspeed_index < airspeed_index::FIRST_SENSOR_INDEX
-	     || _param_airspeed_primary_index.get() == airspeed_index::GROUND_MINUS_WIND_INDEX)) {
+	if (_valid_airspeed_index < airspeed_index::FIRST_SENSOR_INDEX
+	    || _param_airspeed_primary_index.get() == airspeed_index::GROUND_MINUS_WIND_INDEX) {
 
 		// _vehicle_local_position_valid determines if ground-wind estimate is valid
 		if (_vehicle_local_position_valid &&
