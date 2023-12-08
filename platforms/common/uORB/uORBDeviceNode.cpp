ce67fee6b969bbff3a87f1f7fff354476203b35d
Daniel Agar <daniel@agar.ca>
2023-12-07 21:32:07 -0500

    uORB::DeviceNode mark advertised on write
     - this is a workaround if multiple publishers for the same topic exists and one unadvertises (eg uORB::Publication destruction)

--- platforms/common/uORB/uORBDeviceNode.cpp
+++ platforms/common/uORB/uORBDeviceNode.cpp
@@ -219,13 +219,15 @@ uORB::DeviceNode::write(cdev::file_t *filp, const char *buffer, size_t buflen)
 
 	memcpy(_data + (_meta->o_size * (generation % _queue_size)), buffer, _meta->o_size);
 
+	/* Mark at least one data has been published */
+	_data_valid = true;
+
+	mark_as_advertised();
+
 	// callbacks
 	for (auto item : _callbacks) {
 		item->call();
 	}
-
-	/* Mark at least one data has been published */
-	_data_valid = true;
 
 	ATOMIC_LEAVE;
 
