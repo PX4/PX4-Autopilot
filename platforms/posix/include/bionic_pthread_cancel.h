eda20b3d902f8cb5e10c1da074bd45e6d6a963cd
AuroraRAS <chplee@gmail.com>
2024-01-02 22:37:40 +0900

    Make code conform to standards.
    Make the code conform to the project's astyle standards, again.
    
    Signed-off-by: AuroraRAS <chplee@gmail.com>

--- platforms/posix/include/bionic_pthread_cancel.h
+++ platforms/posix/include/bionic_pthread_cancel.h
@@ -12,20 +12,20 @@ namespace
 {
 static void handler_pthread_cancel(int sig)
 {
-    pthread_exit(0);
+	pthread_exit(0);
 }
 int pthread_cancel(pthread_t thread)
 {
-    return pthread_kill(thread, SIGCANCEL);
+	return pthread_kill(thread, SIGCANCEL);
 }
 void register_handler_pthread_cancel()
 {
-    struct sigaction actions;
-    memset(&actions, 0, sizeof(actions));
-    sigemptyset(&actions.sa_mask);
-    actions.sa_flags = 0;
-    actions.sa_handler = handler_pthread_cancel;
-    sigaction(SIGCANCEL,&actions,NULL);
+	struct sigaction actions;
+	memset(&actions, 0, sizeof(actions));
+	sigemptyset(&actions.sa_mask);
+	actions.sa_flags = 0;
+	actions.sa_handler = handler_pthread_cancel;
+	sigaction(SIGCANCEL,&actions,NULL);
 }
 }
 
