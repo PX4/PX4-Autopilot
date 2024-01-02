42c3d8a1cb1431cf8dad2d0950b93655d137075e
AuroraRAS <chplee@gmail.com>
2024-01-02 21:58:58 +0900

    Make code conform to standards.
    Make the code conform to the project's astyle standards.
    
    Signed-off-by: AuroraRAS <chplee@gmail.com>

--- platforms/posix/include/bionic_pthread_cancel.h
+++ platforms/posix/include/bionic_pthread_cancel.h
@@ -8,26 +8,25 @@
 /* The signal used for asynchronous cancelation.  */
 #define SIGCANCEL       __SIGRTMIN
 
-namespace {
-    static void handler_pthread_cancel(int sig)
-    {
-        pthread_exit(0);
-    }
-
-    int pthread_cancel(pthread_t thread)
-    {
-        return pthread_kill(thread, SIGCANCEL);
-    }
-
-    void register_handler_pthread_cancel()
-    {
-        struct sigaction actions;
-        memset(&actions, 0, sizeof(actions));
-        sigemptyset(&actions.sa_mask);
-        actions.sa_flags = 0;
-        actions.sa_handler = handler_pthread_cancel;
-        sigaction(SIGCANCEL,&actions,NULL);
-    }
+namespace
+{
+static void handler_pthread_cancel(int sig)
+{
+    pthread_exit(0);
+}
+int pthread_cancel(pthread_t thread)
+{
+    return pthread_kill(thread, SIGCANCEL);
+}
+void register_handler_pthread_cancel()
+{
+    struct sigaction actions;
+    memset(&actions, 0, sizeof(actions));
+    sigemptyset(&actions.sa_mask);
+    actions.sa_flags = 0;
+    actions.sa_handler = handler_pthread_cancel;
+    sigaction(SIGCANCEL,&actions,NULL);
+}
 }
 
 #endif
