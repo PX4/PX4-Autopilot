22eb911160a7540044f6145e747137a9a3faf1fb
Eric Katzfey <eric.katzfey@modalai.com>
2024-01-04 10:36:01 -0800

    Added ARM toolchain path to environment

--- boards/modalai/voxl2/scripts/build-apps.sh
+++ boards/modalai/voxl2/scripts/build-apps.sh
@@ -4,6 +4,8 @@ echo "*** Starting apps processor build ***"
 
 source /home/build-env.sh
 
+export PATH=/home/4.1.0.4/tools/linaro64/bin:$PATH
+
 make modalai_voxl2
 
 cat build/modalai_voxl2_default/src/lib/version/build_git_version.h
