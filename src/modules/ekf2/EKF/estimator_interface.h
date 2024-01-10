d45c3d34078d3f716b0ac3592cc3c4fc4ea03583
Daniel Agar <daniel@agar.ca>
2024-01-10 09:46:04 -0500

    ekf2: verbose (DEBUG_BUILD) print status with Matrix improved print
     * ekf2: verbose print status
     * matrix/Matrix improve print output
     * bold diagonal elements, print ring buffer entry size
     * print in scientific notation when >= 10 to respect max size
    
    ---------
    
    Co-authored-by: bresch <brescianimathieu@gmail.com>

--- src/modules/ekf2/EKF/estimator_interface.h
+++ src/modules/ekf2/EKF/estimator_interface.h
@@ -312,8 +312,6 @@ public:
 	const MapProjection &global_origin() const { return _pos_ref; }
 	float getEkfGlobalOriginAltitude() const { return PX4_ISFINITE(_gps_alt_ref) ? _gps_alt_ref : 0.f; }
 
-	void print_status();
-
 	OutputPredictor &output_predictor() { return _output_predictor; };
 
 protected:
@@ -479,7 +477,7 @@ protected:
 	warning_event_status_u _warning_events{};
 	information_event_status_u _information_events{};
 
-private:
+	unsigned _min_obs_interval_us{0}; // minimum time interval between observations that will guarantee data is not lost (usec)
 
 #if defined(CONFIG_EKF2_DRAG_FUSION)
 	void setDragData(const imuSample &imu);
@@ -492,7 +490,5 @@ private:
 	void printBufferAllocationFailed(const char *buffer_name);
 
 	ImuDownSampler _imu_down_sampler{_params.filter_update_interval_us};
-
-	unsigned _min_obs_interval_us{0}; // minimum time interval between observations that will guarantee data is not lost (usec)
 };
 #endif // !EKF_ESTIMATOR_INTERFACE_H
