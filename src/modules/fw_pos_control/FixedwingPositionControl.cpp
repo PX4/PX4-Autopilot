a38abdbf9d5590a8572651ab1d0baecccd2cb7bb
Silvan Fuhrer <silvan@auterion.com>
2024-01-12 10:25:53 +0100

    TECS throttle gains default reduction and transition values from previous PX4 version (#22548)
    * TECS: reduce default of FW_T_I_GAIN_THR
    
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>
    
    * TECS: reduce default of FW_T_THR_DAMP
    
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>
    
    * TECS: improve param descriptions and meta data of some params
    
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>
    
    * FW Pos C: rename FW_T_THR_DAMP to FW_T_THR_DAMPING
    
    Rename wihtout param translation as the interpretation of this param has changed
    recently (a bug was fixed that caused the damping to have a much lower effect than
    it should). We want to avoid that users keep the previously tuned value (with the bug)
    and do not update the tuning when using the fixed version (v1.14.0 and later).
    
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>
    
    * FW Pos C: rename FW_T_I_GAIN_THR to FW_T_THR_INTEG
    
    Rename wihtout param translation as the interpretation of this param has changed
    recently (a bug was fixed that caused the damping to have a much lower effect than
    it should). We want to avoid that users keep the previously tuned value (with the bug)
    and do not update the tuning when using the fixed version (v1.14.0 and later).
    
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>
    
    ---------
    
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- src/modules/fw_pos_control/FixedwingPositionControl.cpp
+++ src/modules/fw_pos_control/FixedwingPositionControl.cpp
@@ -129,8 +129,8 @@ FixedwingPositionControl::parameters_update()
 	_tecs.set_speed_weight(_param_fw_t_spdweight.get());
 	_tecs.set_equivalent_airspeed_trim(_performance_model.getCalibratedTrimAirspeed());
 	_tecs.set_equivalent_airspeed_min(_performance_model.getMinimumCalibratedAirspeed());
-	_tecs.set_throttle_damp(_param_fw_t_thr_damp.get());
-	_tecs.set_integrator_gain_throttle(_param_fw_t_I_gain_thr.get());
+	_tecs.set_throttle_damp(_param_fw_t_thr_damping.get());
+	_tecs.set_integrator_gain_throttle(_param_fw_t_thr_integ.get());
 	_tecs.set_integrator_gain_pitch(_param_fw_t_I_gain_pit.get());
 	_tecs.set_throttle_slewrate(_param_fw_thr_slew_max.get());
 	_tecs.set_vertical_accel_limit(_param_fw_t_vert_acc.get());
