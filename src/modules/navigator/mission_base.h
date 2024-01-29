a1a8531ecec2a534ec8986fc8ffcb77a5eea9653
Konrad <konrad@auterion.com>
2024-01-29 12:47:57 +0100

    rtl_direct_mission_land: add time estimation for RTL mission land

--- src/modules/navigator/mission_base.h
+++ src/modules/navigator/mission_base.h
@@ -315,6 +315,9 @@ protected:
 	float _mission_init_climb_altitude_amsl{NAN}; 		/**< altitude AMSL the vehicle will climb to when mission starts */
 	int _inactivation_index{-1}; // index of mission item at which the mission was paused. Used to resume survey missions at previous waypoint to not lose images.
 
+	int32_t _load_mission_index{-1}; /**< Mission inted of loaded mission items in dataman cache*/
+	int32_t _dataman_cache_size_signed; /**< Size of the dataman cache. A negativ value indicates that previous mission items should be loaded, a positiv value the next mission items*/
+
 	DatamanCache _dataman_cache{"mission_dm_cache_miss", 10}; /**< Dataman cache of mission items*/
 	DatamanClient	&_dataman_client = _dataman_cache.client(); /**< Dataman client*/
 
@@ -334,7 +337,7 @@ private:
 	 * @brief Update Dataman cache
 	 *
 	 */
-	void updateDatamanCache();
+	virtual void updateDatamanCache();
 	/**
 	 * @brief Update mission subscription
 	 *
@@ -446,9 +449,6 @@ private:
 	bool checkMissionDataChanged(mission_s new_mission);
 
 	bool canRunMissionFeasibility();
-
-	int32_t _load_mission_index{-1}; /**< Mission inted of loaded mission items in dataman cache*/
-	int32_t _dataman_cache_size_signed; /**< Size of the dataman cache. A negativ value indicates that previous mission items should be loaded, a positiv value the next mission items*/
 
 	bool _align_heading_necessary{false}; // if true, heading of vehicle needs to be aligned with heading of next waypoint. Used to create new mission items for heading alignment.
 
