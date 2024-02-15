eaa8fa9bf3d1786aea651d6261fa5bf50bd567b6
bresch <brescianimathieu@gmail.com>
2024-02-15 12:59:21 +0100

    ekf2: fix unit tests failing due to mag fusion changes

--- src/modules/ekf2/test/test_EKF_basics.cpp
+++ src/modules/ekf2/test/test_EKF_basics.cpp
@@ -107,7 +107,8 @@ TEST_F(EkfBasicsTest, initialControlMode)
 	EXPECT_EQ(0, (int) _ekf->control_status_flags().opt_flow);
 	EXPECT_EQ(1, (int) _ekf->control_status_flags().mag_hdg);
 	EXPECT_EQ(0, (int) _ekf->control_status_flags().mag_3D);
-	EXPECT_EQ(0, (int) _ekf->control_status_flags().mag_dec);
+	EXPECT_EQ(1, (int) _ekf->control_status_flags().mag);
+	EXPECT_EQ(1, (int) _ekf->control_status_flags().mag_dec);
 	EXPECT_EQ(0, (int) _ekf->control_status_flags().in_air);
 	EXPECT_EQ(0, (int) _ekf->control_status_flags().wind);
 	EXPECT_EQ(1, (int) _ekf->control_status_flags().baro_hgt);
@@ -161,7 +162,8 @@ TEST_F(EkfBasicsTest, gpsFusion)
 	EXPECT_EQ(0, (int) _ekf->control_status_flags().opt_flow);
 	EXPECT_EQ(1, (int) _ekf->control_status_flags().mag_hdg);
 	EXPECT_EQ(0, (int) _ekf->control_status_flags().mag_3D);
-	EXPECT_EQ(0, (int) _ekf->control_status_flags().mag_dec);
+	EXPECT_EQ(1, (int) _ekf->control_status_flags().mag);
+	EXPECT_EQ(1, (int) _ekf->control_status_flags().mag_dec);
 	EXPECT_EQ(0, (int) _ekf->control_status_flags().in_air);
 	EXPECT_EQ(0, (int) _ekf->control_status_flags().wind);
 	EXPECT_EQ(1, (int) _ekf->control_status_flags().baro_hgt);
