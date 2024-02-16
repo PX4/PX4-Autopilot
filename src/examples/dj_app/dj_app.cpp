25c690eb8a455734398eb066737d510bf52b720b
DJ <minerva01st@gmail.com>
2024-02-16 20:53:13 +0900

    Add dj_app

---
+++ src/examples/dj_app/dj_app.cpp
@@ -0,0 +1,245 @@
+#include "dj_app.hpp"
+#include <stdio.h>
+
+RS485::RS485() :
+	OutputModuleInterface(MODULE_NAME, px4::wq_configurations::ttyS3)
+{
+	PX4_INFO("Instance has constructed!\n");
+}
+
+RS485::~RS485()
+{
+	close(_rs485_fd);	// rs485 디스크립터를 닫는다.
+	PX4_INFO("Instance has destructed...\n");
+}
+
+int RS485::task_spawn(int argc, char *argv[])
+{
+	RS485 *instance = new RS485();
+
+	if (!instance) {
+		PX4_ERR("alloc failed");
+		return -1;
+	}
+
+	_object.store(instance);
+	_task_id = task_id_is_work_queue;
+	instance->ScheduleNow();
+	return 0;
+}
+
+int RS485::custom_command(int argc, char *argv[])
+{
+	return print_usage("unknown command");
+}
+
+int RS485::print_usage(const char *reason)
+{
+	if (reason) {
+		PX4_WARN("%s\n", reason);
+	}
+
+	PRINT_MODULE_USAGE_NAME("dj_app", "example");
+	PRINT_MODULE_USAGE_COMMAND("start");
+	PRINT_MODULE_USAGE_DEFAULT_COMMANDS();
+	return 0;
+}
+
+int RS485::print_status()
+{
+	_mixing_output.printStatus();
+	return 0;
+}
+
+bool RS485::updateOutputs(bool stop_motors, uint16_t outputs[MAX_ACTUATORS],
+			   unsigned num_outputs, unsigned num_control_groups_updated)
+{
+	return 0;
+}
+
+void RS485::Run()
+{
+	if (should_exit()) {
+		ScheduleClear();
+		_mixing_output.unregister();
+
+		exit_and_cleanup();
+		return;
+	}
+
+	_mixing_output.update();
+}
+
+ssize_t RS485::initializeRS485()
+{
+	_rs485_fd = open(_port_name, O_RDWR | O_NOCTTY);	// rs485 디스크립터를 연다.
+	if (_rs485_fd < 0) return ERROR;
+
+	ssize_t ret = 0;
+	struct termios rs485_config {};			// termios 구조체 초기화
+	ret = tcgetattr(_rs485_fd, &rs485_config);	// 현재 터미널 설정 termios 구조체에 가져옴
+	if (ret < 0) return ERROR;
+
+	rs485_config.c_cflag &= ~PARENB;	// Parity bit None
+	rs485_config.c_cflag &= ~CSTOPB;	// Stop bit 1로 설정
+	rs485_config.c_cflag &= ~CSIZE;		// Data bits 설정 초기화
+	rs485_config.c_cflag |= CS8;		// Data bits 8로 설정
+	rs485_config.c_cflag &= ~CRTSCTS;	// Flow Conotrol 끄기
+	rs485_config.c_cflag |= CREAD | CLOCAL;
+	rs485_config.c_oflag &= ~OPOST;
+	rs485_config.c_oflag &= ~ONLCR;
+	cfsetispeed(&rs485_config, B115200);
+	cfsetospeed(&rs485_config, B115200);
+
+	tcflush(_rs485_fd, TCIFLUSH);
+	ret = tcsetattr(_rs485_fd, TCSANOW, &rs485_config);	// termios 구조체 설정을 디스크립터에 저장
+	if (ret < 0) return ERROR;
+
+	PX4_INFO("Successfully connected!\n");
+	return OK;
+}
+
+ssize_t RS485::setMotorSpeed(uint16_t rpm, bool side)
+{
+	if (_motor_mode != Mode::Velocity) setMotorMode(Mode::Velocity);
+	if (side == 0) setRTUPacket(0x06, 0x2088, (uint8_t*)&rpm, sizeof(rpm));
+	if (side == 1) setRTUPacket(0x06, 0x2089, (uint8_t*)&rpm, sizeof(rpm));
+	calculateCRC((uint8_t*)&_rtu, sizeof(_rtu) - 2);
+	return writeData();
+}
+
+ssize_t RS485::readEncoder()
+{
+	uint16_t register_number = 0x0001;
+
+	if (_motor_mode != Mode::Velocity) setMotorMode(Mode::Velocity);
+	setRTUPacket(0x03, 0x20AB, (uint8_t*)&register_number, sizeof(register_number));
+	calculateCRC((uint8_t*)&_rtu, sizeof(_rtu) - 2);
+	speed_left = readData();
+	setRTUPacket(0x03, 0x20AC, (uint8_t*)&register_number, sizeof(register_number));
+	calculateCRC((uint8_t*)&_rtu, sizeof(_rtu) - 2);
+	speed_right = readData();
+
+	setMotorMode(Mode::AbsolutePosition);
+	setRTUPacket(0x03, 0x20A7, (uint8_t*)&register_number, sizeof(register_number));
+	calculateCRC((uint8_t*)&_rtu, sizeof(_rtu) - 2);
+	position_left = readData();
+	setRTUPacket(0x03, 0x20A8, (uint8_t*)&register_number, sizeof(register_number));
+	calculateCRC((uint8_t*)&_rtu, sizeof(_rtu) - 2);
+	position_left = (position_left << 8) | readData();
+
+	setRTUPacket(0x03, 0x20A9, (uint8_t*)&register_number, sizeof(register_number));
+	calculateCRC((uint8_t*)&_rtu, sizeof(_rtu) - 2);
+	position_right = readData();
+	setRTUPacket(0x03, 0x20AA, (uint8_t*)&register_number, sizeof(register_number));
+	calculateCRC((uint8_t*)&_rtu, sizeof(_rtu) - 2);
+	position_right = (position_right << 8) | readData();
+
+	wheel_encoders_s wheel_encoders{};
+	wheel_encoders.wheel_speed[0] = speed_right;
+	wheel_encoders.wheel_speed[1] = speed_left;
+	wheel_encoders.wheel_angle[0] = position_right;
+	wheel_encoders.wheel_angle[1] = position_left;
+	wheel_encoders.timestamp = hrt_absolute_time();
+	_wheel_encoders_pub.publish(wheel_encoders);
+
+	printf("speed_right : %.2x\n", speed_right);
+	printf("speed_left : %.2x\n", speed_left);
+	printf("position_right : %.4lx\n", position_right);
+	printf("position_left : %.4lx\n", position_left);
+
+
+	return OK;
+}
+
+ssize_t RS485::setMotorMode(Mode mode)
+{
+	setRTUPacket(0x06, 0x200D, (uint8_t*)&mode, sizeof(mode));
+	calculateCRC((uint8_t*)&_rtu, sizeof(_rtu) - 2);
+	_motor_mode = mode;
+	return writeData();
+}
+
+uint16_t RS485::calculateCRC(const uint8_t *data, size_t data_length)
+{
+	const uint16_t polynomial = 0xA001;
+	uint16_t crc = 0xFFFF;
+
+	for (size_t i = 0; i < data_length; ++i)
+	{
+		crc ^= data[i];
+		for (int j = 0; j < 8; ++j)
+        	{
+			uint16_t LSB = crc & 0x0001;
+			crc >>= 1;
+			if (LSB) crc ^= polynomial;
+        	}
+	}
+	_rtu._crc_high = crc & 0xFF;
+	_rtu._crc_low = crc >> 8;
+	return crc;
+}
+
+void RS485::setRTUPacket(uint8_t function_code, uint16_t register_address, uint8_t* data, size_t data_length)
+{
+	_rtu._function_code = function_code;
+	_rtu._register_address_high = register_address >> 8;
+	_rtu._register_address_low = register_address & 0xFF;
+	for (size_t i = 0; i < data_length; i++)
+		_rtu._data[i] = data[data_length - 1 - i];
+}
+
+void RS485::printRTUPacket(void)
+{
+	PX4_INFO("%.2x %.2x %.2x %.2x %.2x %.2x %.2x %.2x\n",
+	_rtu._device_address, _rtu._function_code, _rtu._register_address_high, _rtu._register_address_low
+	, _rtu._data[0], _rtu._data[1], _rtu._crc_high, _rtu._crc_low);
+}
+
+ssize_t RS485::writeData()
+{
+	ssize_t ret = write(_rs485_fd, &_rtu, sizeof(_rtu));
+	printRTUPacket();
+	usleep(4000);
+	return ret;
+}
+
+uint16_t RS485::readData()
+{
+	uint16_t buf;
+	write(_rs485_fd, &_rtu, sizeof(_rtu));
+	printRTUPacket();
+	read(_rs485_fd, &buf, 2);
+	return buf;
+}
+
+extern "C" __EXPORT int dj_app_main(int argc, char *argv[])
+{
+	// PX4_INFO("DJ has started!\n");
+
+	// int quit;
+	// RS485 rs485;
+	// rs485.initializeRS485();
+	// while(quit)
+	// {
+	// 	scanf("%d", &quit);
+	// 	if (quit == 1)
+	// 	{
+	// 		rs485.setMotorSpeed(100, 0);
+	// 		rs485.setMotorSpeed(100, 1);
+	// 	}
+	// 	else if (quit == 2)
+	// 	{
+	// 		rs485.setMotorSpeed(0, 0);
+	// 		rs485.setMotorSpeed(0, 1);
+	// 	}
+	// 	else if (quit == 3)
+	// 	{
+	// 		rs485.readEncoder();
+	// 	}
+
+	// }
+	// return OK;
+	printf("Hello");
+	return RS485::main(argc, argv);
+}
---
+++ src/examples/dj_app/dj_app.hpp
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <lib/mixer_module/mixer_module.hpp>
+#include <px4_platform_common/module.h>
+#include <px4_platform_common/log.h>
+#include <termios.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <uORB/Publication.hpp>
+#include <uORB/topics/wheel_encoders.h>
+
+class RS485 : public ModuleBase<RS485>, public OutputModuleInterface
+{
+public:
+	RS485();
+	~RS485() override;
+
+	static int task_spawn(int argc, char *argv[]);
+	static int custom_command(int argc, char *argv[]);
+	static int print_usage(const char *reason = nullptr);
+	int print_status() override;
+
+	void Run() override;
+
+	bool updateOutputs(bool stop_motors, uint16_t outputs[MAX_ACTUATORS],
+			   unsigned num_outputs, unsigned num_control_groups_updated) override;
+
+	enum class Mode : uint16_t
+	{
+		None = 0,
+		RelativePosition = 1,
+		AbsolutePosition = 2,
+		Velocity = 3,
+		Torque = 4
+	};
+
+	ssize_t initializeRS485();	// RS485 통신 초기화
+	ssize_t setMotorSpeed(uint16_t rpm, bool side);	// rpm과 왼,오른쪽을 매개변수로 받아 모터 속도 설정
+	ssize_t readEncoder();
+
+private:
+	MixingOutput _mixing_output{"DJ_APP", MAX_ACTUATORS, *this, MixingOutput::SchedulingPolicy::Auto, true};
+
+	typedef struct RTUPacket	// Modbus RTU 통신에서 주고받는 패킷을 구조체로 추상화
+	{
+		const uint8_t _device_address = 0x01;
+		uint8_t _function_code;
+		uint8_t _register_address_high;
+		uint8_t _register_address_low;
+		uint8_t _data[2];
+		uint8_t _crc_high;
+		uint8_t _crc_low;
+	}RTU;
+	RTU _rtu;
+
+	const char* _port_name = "/dev/ttyS3";	// 디바이스 포트 이름(pixhawk UART 포트이름이 dev/ttyS3)
+	const int _baudrate = B115200;		// baudrate(모터드라이브의 default가 115200)
+	int _rs485_fd = 0;			// rs485 통신 디스크립터
+
+	uint16_t speed_left = 0, speed_right = 0;
+	uint32_t position_left = 0, position_right = 0;
+
+	uORB::PublicationData<wheel_encoders_s> _wheel_encoders_pub{ORB_ID(wheel_encoders)};
+
+	Mode _motor_mode = Mode::None;
+
+	ssize_t setMotorMode(Mode mode);	// 모터드라이버 모드 설정
+	uint16_t calculateCRC(const uint8_t *data, size_t data_length);	// CRC 계산
+	void setRTUPacket(uint8_t function_code, uint16_t register_address, uint8_t* data, size_t data_length);	// 원하는 값으로 패킷 설정
+	void printRTUPacket(void);	// 현재 패킷에 저장된 데이터 출력(디버깅용)
+	ssize_t writeData();	// 데이터 쓰기
+	uint16_t readData();	// 데이터 읽기
+};
