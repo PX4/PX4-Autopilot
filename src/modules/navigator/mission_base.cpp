572a723ce069bcec79057bbedeb87ac660fd25da
Drone-Lab <2334088143@qq.com>
2024-03-18 18:18:00 +0800

    make format

--- src/modules/navigator/mission_base.cpp
+++ src/modules/navigator/mission_base.cpp
@@ -1382,13 +1382,22 @@ void MissionBase::updateMissionAltAfterHomeChanged()
 	if (_navigator->get_home_position()->update_count > _home_update_counter) {
 		float new_alt = get_absolute_altitude_for_item(_mission_item);
 		float altitude_diff = new_alt - _navigator->get_position_setpoint_triplet()->current.alt;
-		if (_navigator->get_position_setpoint_triplet()->previous.valid && PX4_ISFINITE(_navigator->get_position_setpoint_triplet()->previous.alt)) {
-		    _navigator->get_position_setpoint_triplet()->previous.alt = _navigator->get_position_setpoint_triplet()->previous.alt + altitude_diff;
-		    }
-		_navigator->get_position_setpoint_triplet()->current.alt = _navigator->get_position_setpoint_triplet()->current.alt + altitude_diff;
-		if (_navigator->get_position_setpoint_triplet()->next.valid && PX4_ISFINITE(_navigator->get_position_setpoint_triplet()->next.alt)) {
-		    _navigator->get_position_setpoint_triplet()->next.alt = _navigator->get_position_setpoint_triplet()->next.alt + altitude_diff;
-		    }
+
+		if (_navigator->get_position_setpoint_triplet()->previous.valid
+		    && PX4_ISFINITE(_navigator->get_position_setpoint_triplet()->previous.alt)) {
+			_navigator->get_position_setpoint_triplet()->previous.alt = _navigator->get_position_setpoint_triplet()->previous.alt +
+					altitude_diff;
+		}
+
+		_navigator->get_position_setpoint_triplet()->current.alt = _navigator->get_position_setpoint_triplet()->current.alt +
+				altitude_diff;
+
+		if (_navigator->get_position_setpoint_triplet()->next.valid
+		    && PX4_ISFINITE(_navigator->get_position_setpoint_triplet()->next.alt)) {
+			_navigator->get_position_setpoint_triplet()->next.alt = _navigator->get_position_setpoint_triplet()->next.alt +
+					altitude_diff;
+		}
+
 		_navigator->set_position_setpoint_triplet_updated();
 		_home_update_counter = _navigator->get_home_position()->update_count;
 	}
