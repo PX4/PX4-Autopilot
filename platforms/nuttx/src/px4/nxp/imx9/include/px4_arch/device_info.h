7d23d9e70ef1adcb9759829f4a049a9a49d501ab
Jukka Laitinen <jukkax@ssrc.tii.ae>
2024-03-22 09:22:52 +0200

    platforms/nuttx/src/px4/nxp: Add imx9 base files
    Add basic imx9 platform support and stubs for adc, board_critmon, board_reset, hrt, tone_alarm and version
    
    Signed-off-by: Jukka Laitinen <jukkax@ssrc.tii.ae>

---
+++ platforms/nuttx/src/px4/nxp/imx9/include/px4_arch/device_info.h
@@ -0,0 +1,69 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2023 Technology Innovation Institute. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *	notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *	notice, this list of conditions and the following disclaimer in
+ *	the documentation and/or other materials provided with the
+ *	distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *	used to endorse or promote products derived from this software
+ *	without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#pragma once
+
+#include <image_toc.h>
+
+typedef struct __attribute__((__packed__))
+{
+	/* Magic, matches characters "DNFO" when valid */
+
+	uint32_t magic;
+
+	/* Anti-rollback counters for different payloads */
+
+	uint16_t arb[IMAGE_NUM_TYPES];
+
+	/* Feature flags. RD certificate feature bits are ORd to this.
+	 * Passed to payload OSs for enabling / disabling features
+	 */
+
+	uint32_t features;
+
+	/* Device MAC addresses */
+
+	uint8_t mac[4][6];
+
+	/* Bootloader version info */
+
+	char bl_version[32];
+
+	/* FPGA version info */
+
+	uint32_t fpga_version;
+
+	/* Reserved for future use */
+
+	uint8_t reserved[424];
+} devinfo_t;
