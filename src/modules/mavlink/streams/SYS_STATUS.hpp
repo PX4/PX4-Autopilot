60142574ce9771ab598c29323800ba483a7179a7
DavidPaddy97 <96058684+DavidPaddy97@users.noreply.github.com>
2024-04-04 16:39:18 +0100

    Passing valid_rc_setpoint_count to SI2 instead of rc_channels signal_lost (#68)

--- src/modules/mavlink/streams/SYS_STATUS.hpp
+++ src/modules/mavlink/streams/SYS_STATUS.hpp
@@ -143,7 +143,8 @@ private:
 				status.rc_signal_lost;    // No manual_control_setpoint messages arriving ( can come from RC or MAV )
 			msg.errors_count2 =
 				sees_manual_control_data.valid_mavlink_setpoint_count;    // Number of Mavlink connections providing setpoints (implying joystick connected). Should be < 2.
-			msg.errors_count3 = rc_channels.signal_lost;           // No messages from RC Tx received
+			msg.errors_count3 =
+				sees_manual_control_data.valid_rc_setpoint_count;           // Number of RC connections providing setpoints (implying RC Connected). Should be < 2.
 			msg.errors_count4 =
 				sees_manual_control_data.sees_desired_control_source;       // Desired type of manual control source, RC (1) or Mavlink (2)
 
