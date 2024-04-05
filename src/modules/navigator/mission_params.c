64505b4b9c776656c2f8382834d8b2a9ccbe3a3a
Silvan Fuhrer <silvan@auterion.com>
2024-04-05 16:39:12 +0200

    Mission params: update description of MIS_TKO_LAND_REQ
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- src/modules/navigator/mission_params.c
+++ src/modules/navigator/mission_params.c
@@ -68,7 +68,7 @@ PARAM_DEFINE_FLOAT(MIS_TAKEOFF_ALT, 2.5f);
  * @value 2 Require a landing
  * @value 3 Require a takeoff and a landing
  * @value 4 Require both a takeoff and a landing, or neither
- * @value 5 Same as previous, but require a landing if in air and no valid VTOL landing approach is present
+ * @value 5 Same as previous when landed, in-air require landing only if no valid VTOL approach is present
  * @group Mission
  */
 PARAM_DEFINE_INT32(MIS_TKO_LAND_REQ, 0);
