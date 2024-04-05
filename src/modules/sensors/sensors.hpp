f712820124a67e9c7ceb78751e64be10f48c6715
Ilia-Loginov <ilia.loginov@tii.ae>
2024-04-05 11:11:51 +0400

    Component: Introduced failure_detector_hitl module, integrating three classes for handling failure injection messages(FailureDetectorHITL), simulating sensors that have ceased to update (FakeStuckSensor), and a component managing a collection of these simulated sensors (FakeSensors).
    This module is a combination of three classes: FailureDetectorHITL for detecting failure injection messages,
    FakeStuckSensor for simulating sensors whose readings have become stuck, and FakeSensors, a component that aggregates a collection of such simulated sensors.
    
    Additionally, the handling of the sensors module has been extended to support disabling sensors during flight.
    
    Designed exclusively for operation in HITL (Hardware-In-The-Loop) mode, this module does not interfere with normal operational mode.
    
    Reported-by: Ilia Loginov ilia.loginov@tii.ae
    
    On branch FEATURE_FAILURE_INJECTION_HITL
    Changes to be committed:
    	modified:   src/modules/sensors/CMakeLists.txt
    	new file:   src/modules/sensors/failure_detector_HITL/CMakeLists.txt
    	new file:   src/modules/sensors/failure_detector_HITL/failure_detector_HITL.cpp
    	new file:   src/modules/sensors/failure_detector_HITL/failure_detector_HITL.hpp
    	modified:   src/modules/sensors/sensors.cpp
    	modified:   src/modules/sensors/sensors.hpp

--- src/modules/sensors/sensors.hpp
+++ src/modules/sensors/sensors.hpp
@@ -51,6 +51,7 @@
 #include <uORB/SubscriptionCallback.hpp>
 #include "voted_sensors_update.h"
 #include "vehicle_imu/VehicleIMU.hpp"
+#include "failure_detector_HITL/failure_detector_HITL.hpp"
 
 #if defined(CONFIG_SENSORS_VEHICLE_ACCELERATION)
 # include "vehicle_acceleration/VehicleAcceleration.hpp"
@@ -250,6 +251,9 @@ private:
 	VehicleOpticalFlow *_vehicle_optical_flow {nullptr};
 	uint8_t _n_optical_flow{0};
 #endif // CONFIG_SENSORS_VEHICLE_OPTICAL_FLOW
+
+	FailureDetectorHITL _failureDetector;
+	FakeSensors _fakeSensors;
 
 	DEFINE_PARAMETERS(
 #if defined(CONFIG_SENSORS_VEHICLE_AIR_DATA)
