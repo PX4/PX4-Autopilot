7684d3e9b1962da829c612df61d7e63f2e4e00b2
Eric Katzfey <eric.katzfey@modalai.com>
2024-04-09 14:14:57 -0700

    Added missing installation script line for new module

--- boards/modalai/voxl2/scripts/install-voxl.sh
+++ boards/modalai/voxl2/scripts/install-voxl.sh
@@ -128,6 +128,7 @@ adb shell "cd /usr/bin; /bin/ln -f -s px4 px4-uart_esc_driver"
 adb shell "cd /usr/bin; /bin/ln -f -s px4 px4-flight_mode_manager"
 adb shell "cd /usr/bin; /bin/ln -f -s px4 px4-imu_server"
 adb shell "cd /usr/bin; /bin/ln -f -s px4 px4-apps_sbus"
+adb shell "cd /usr/bin; /bin/ln -f -s px4 px4-voxl_save_cal_params"
 
 # Make sure any required directories exist
 adb shell "/bin/mkdir -p /data/px4/param"
