2fca12f49e94c3128961dbc5bdf4279c0572f7dc
Beniamino Pozzan <beniamino.pozzan@gmail.com>
2024-04-14 17:35:26 +0100

    logger: add timesync_status to default logged topics
    Signed-off-by: Beniamino Pozzan <beniamino.pozzan@gmail.com>

--- src/modules/logger/logged_topics.cpp
+++ src/modules/logger/logged_topics.cpp
@@ -145,6 +145,7 @@ void LoggedTopics::add_default_topics()
 	add_optional_topic_multi("rate_ctrl_status", 200, 2);
 	add_optional_topic_multi("sensor_hygrometer", 500, 4);
 	add_optional_topic_multi("rpm", 200);
+	add_topic_multi("timesync_status", 1000, 3);
 	add_optional_topic_multi("telemetry_status", 1000, 4);
 
 	// EKF multi topics (currently max 9 estimators)
