b23afc55c8e50b920db8d64f91d5ce4a732a6014
Julian Oes <julian@oes.ch>
2024-05-03 17:52:22 +1200

    uavcan: implement OpenDroneID BasicID
    Signed-off-by: Julian Oes <julian@oes.ch>

--- src/drivers/uavcan/remoteid.hpp
+++ src/drivers/uavcan/remoteid.hpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (C) 2020 PX4 Development Team. All rights reserved.
+ *   Copyright (C) 2024 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -33,46 +33,41 @@
 
 #pragma once
 
-#include <uORB/topics/actuator_armed.h>
+#include <uORB/topics/vehicle_status.h>
+#include <uORB/Subscription.hpp>
 
 #include <uavcan/uavcan.hpp>
-#include <uavcan/equipment/indication/LightsCommand.hpp>
+#include <dronecan/remoteid/BasicID.hpp>
 
-#include <lib/led/led.h>
 #include <px4_platform_common/module_params.h>
 
-class UavcanRGBController : public ModuleParams
+class UavcanRemoteIDController : public ModuleParams
 {
 public:
-	UavcanRGBController(uavcan::INode &node);
-	~UavcanRGBController() = default;
+	UavcanRemoteIDController(uavcan::INode &node);
+	~UavcanRemoteIDController() = default;
 
-	// setup periodic updater
 	int init();
 
 private:
-	// Max update rate to avoid excessive bus traffic
-	static constexpr unsigned MAX_RATE_HZ = 20;
+	typedef uavcan::MethodBinder<UavcanRemoteIDController *, void (UavcanRemoteIDController::*)(const uavcan::TimerEvent &)>
+	TimerCbBinder;
+
+	static constexpr unsigned MAX_RATE_HZ = 1;
+	uavcan::TimerEventForwarder<TimerCbBinder> _timer;
 
 	void periodic_update(const uavcan::TimerEvent &);
 
-	uavcan::equipment::indication::RGB565 brightness_to_rgb565(uint8_t brightness);
-
-	typedef uavcan::MethodBinder<UavcanRGBController *, void (UavcanRGBController::*)(const uavcan::TimerEvent &)>
-	TimerCbBinder;
-
 	uavcan::INode &_node;
-	uavcan::Publisher<uavcan::equipment::indication::LightsCommand> _uavcan_pub_lights_cmd;
-	uavcan::TimerEventForwarder<TimerCbBinder> _timer;
 
-	uORB::Subscription _armed_sub{ORB_ID(actuator_armed)};
+	uORB::SubscriptionData<vehicle_status_s> _vehicle_status_sub{ORB_ID(vehicle_status)};
 
-	LedController _led_controller;
+	uavcan::Publisher<dronecan::remoteid::BasicID> _uavcan_pub_remoteid_basicid;
 
-	DEFINE_PARAMETERS(
-		(ParamInt<px4::params::UAVCAN_LGT_ANTCL>) _param_mode_anti_col,
-		(ParamInt<px4::params::UAVCAN_LGT_STROB>) _param_mode_strobe,
-		(ParamInt<px4::params::UAVCAN_LGT_NAV>) _param_mode_nav,
-		(ParamInt<px4::params::UAVCAN_LGT_LAND>) _param_mode_land
-	)
+	//DEFINE_PARAMETERS(
+	//	(ParamInt<px4::params::UAVCAN_LGT_ANTCL>) _param_mode_anti_col,
+	//	(ParamInt<px4::params::UAVCAN_LGT_STROB>) _param_mode_strobe,
+	//	(ParamInt<px4::params::UAVCAN_LGT_NAV>) _param_mode_nav,
+	//	(ParamInt<px4::params::UAVCAN_LGT_LAND>) _param_mode_land
+	//)
 };
