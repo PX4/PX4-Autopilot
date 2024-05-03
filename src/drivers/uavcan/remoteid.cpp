83e629b245f9b0eec1510f98a1dc155c34be4fa4
Julian Oes <julian@oes.ch>
2024-05-03 17:52:22 +1200

    uavcan: implement OpenDroneID BasicID
    Signed-off-by: Julian Oes <julian@oes.ch>

--- src/drivers/uavcan/remoteid.cpp
+++ src/drivers/uavcan/remoteid.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (C) 2020 PX4 Development Team. All rights reserved.
+ *   Copyright (C) 2024 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -31,48 +31,43 @@
  *
  ****************************************************************************/
 
-#pragma once
+#include "remoteid.hpp"
+#include <lib/open_drone_id/open_drone_id_translations.hpp>
 
-#include <uORB/topics/actuator_armed.h>
-
-#include <uavcan/uavcan.hpp>
-#include <uavcan/equipment/indication/LightsCommand.hpp>
-
-#include <lib/led/led.h>
-#include <px4_platform_common/module_params.h>
-
-class UavcanRGBController : public ModuleParams
+UavcanRemoteIDController::UavcanRemoteIDController(uavcan::INode &node) :
+	ModuleParams(nullptr),
+	_timer(node),
+	_node(node),
+	_uavcan_pub_remoteid_basicid(node)
 {
-public:
-	UavcanRGBController(uavcan::INode &node);
-	~UavcanRGBController() = default;
+}
 
-	// setup periodic updater
-	int init();
+int UavcanRemoteIDController::init()
+{
+	// Setup timer and call back function for periodic updates
+	_timer.setCallback(TimerCbBinder(this, &UavcanRemoteIDController::periodic_update));
+	_timer.startPeriodic(uavcan::MonotonicDuration::fromMSec(1000 / MAX_RATE_HZ));
+	return 0;
+}
 
-private:
-	// Max update rate to avoid excessive bus traffic
-	static constexpr unsigned MAX_RATE_HZ = 20;
+void UavcanRemoteIDController::periodic_update(const uavcan::TimerEvent &)
+{
+	if (!_vehicle_status_sub.update()) {
+		return;
+	}
 
-	void periodic_update(const uavcan::TimerEvent &);
+	dronecan::remoteid::BasicID basic_id {};
+	// basic_id.id_or_mac // supposedly only used for drone ID data from other UAs
+	basic_id.id_type = dronecan::remoteid::BasicID::ODID_ID_TYPE_SERIAL_NUMBER;
+	basic_id.ua_type = static_cast<uint8_t>(open_drone_id_translations::odidTypeForMavType(
+			_vehicle_status_sub.get().system_type));
 
-	uavcan::equipment::indication::RGB565 brightness_to_rgb565(uint8_t brightness);
+	// uas_id: UAS (Unmanned Aircraft System) ID following the format specified by id_type
+	// TODO: MAV_ODID_ID_TYPE_SERIAL_NUMBER needs to be ANSI/CTA-2063 format
 
-	typedef uavcan::MethodBinder<UavcanRGBController *, void (UavcanRGBController::*)(const uavcan::TimerEvent &)>
-	TimerCbBinder;
+	char uas_id[20] = {};
+	board_get_px4_guid_formated((char *)(uas_id), sizeof(uas_id));
+	basic_id.uas_id = uas_id;
 
-	uavcan::INode &_node;
-	uavcan::Publisher<uavcan::equipment::indication::LightsCommand> _uavcan_pub_lights_cmd;
-	uavcan::TimerEventForwarder<TimerCbBinder> _timer;
-
-	uORB::Subscription _armed_sub{ORB_ID(actuator_armed)};
-
-	LedController _led_controller;
-
-	DEFINE_PARAMETERS(
-		(ParamInt<px4::params::UAVCAN_LGT_ANTCL>) _param_mode_anti_col,
-		(ParamInt<px4::params::UAVCAN_LGT_STROB>) _param_mode_strobe,
-		(ParamInt<px4::params::UAVCAN_LGT_NAV>) _param_mode_nav,
-		(ParamInt<px4::params::UAVCAN_LGT_LAND>) _param_mode_land
-	)
-};
+	_uavcan_pub_remoteid_basicid.broadcast(basic_id);
+}
