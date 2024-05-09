7721b353486291491e5e8242628ba5814a2b70a3
Julian Oes <julian@oes.ch>
2024-05-09 14:30:55 +1200

    mavlink: extract OpenDroneID function to lib
    This extracts the function mapping from MAV_TYPE to MAV_ODID_UA_TYPE to
    the library, so that it can be re-used later by the remote ID
    implementation over DroneCAN.
    
    Signed-off-by: Julian Oes <julian@oes.ch>

--- src/modules/mavlink/streams/OPEN_DRONE_ID_BASIC_ID.hpp
+++ src/modules/mavlink/streams/OPEN_DRONE_ID_BASIC_ID.hpp
@@ -35,6 +35,7 @@
 #define OPEN_DRONE_ID_BASIC_ID_HPP
 
 #include <uORB/topics/vehicle_status.h>
+#include <lib/open_drone_id/open_drone_id_translations.hpp>
 
 class MavlinkStreamOpenDroneIdBasicId : public MavlinkStream
 {
@@ -57,98 +58,6 @@ private:
 
 	uORB::Subscription _vehicle_status_sub{ORB_ID(vehicle_status)};
 
-	MAV_ODID_UA_TYPE odidTypeForMavType(uint8_t system_type)
-	{
-		switch (system_type) {
-		case MAV_TYPE_GENERIC: return MAV_ODID_UA_TYPE_OTHER;
-
-		case MAV_TYPE_FIXED_WING: return MAV_ODID_UA_TYPE_AEROPLANE;
-
-		case MAV_TYPE_QUADROTOR: return MAV_ODID_UA_TYPE_HELICOPTER_OR_MULTIROTOR;
-
-		case MAV_TYPE_COAXIAL: return MAV_ODID_UA_TYPE_HELICOPTER_OR_MULTIROTOR;
-
-		case MAV_TYPE_HELICOPTER: return MAV_ODID_UA_TYPE_HELICOPTER_OR_MULTIROTOR;
-
-		case MAV_TYPE_ANTENNA_TRACKER: return MAV_ODID_UA_TYPE_NONE;
-
-		case MAV_TYPE_GCS: return MAV_ODID_UA_TYPE_NONE;
-
-		case MAV_TYPE_AIRSHIP: return MAV_ODID_UA_TYPE_AIRSHIP;
-
-		case MAV_TYPE_FREE_BALLOON: return MAV_ODID_UA_TYPE_FREE_BALLOON;
-
-		case MAV_TYPE_ROCKET: return MAV_ODID_UA_TYPE_ROCKET;
-
-		case MAV_TYPE_GROUND_ROVER: return MAV_ODID_UA_TYPE_GROUND_OBSTACLE;
-
-		case MAV_TYPE_SURFACE_BOAT: return MAV_ODID_UA_TYPE_OTHER;
-
-		case MAV_TYPE_SUBMARINE: return MAV_ODID_UA_TYPE_OTHER;
-
-		case MAV_TYPE_HEXAROTOR: return MAV_ODID_UA_TYPE_HELICOPTER_OR_MULTIROTOR;
-
-		case MAV_TYPE_OCTOROTOR: return MAV_ODID_UA_TYPE_HELICOPTER_OR_MULTIROTOR;
-
-		case MAV_TYPE_TRICOPTER: return MAV_ODID_UA_TYPE_HELICOPTER_OR_MULTIROTOR;
-
-		case MAV_TYPE_FLAPPING_WING: return MAV_ODID_UA_TYPE_ORNITHOPTER;
-
-		case MAV_TYPE_KITE: return MAV_ODID_UA_TYPE_KITE;
-
-		case MAV_TYPE_ONBOARD_CONTROLLER: return MAV_ODID_UA_TYPE_NONE;
-
-		case MAV_TYPE_VTOL_TAILSITTER_DUOROTOR: return MAV_ODID_UA_TYPE_HYBRID_LIFT;
-
-		case MAV_TYPE_VTOL_TAILSITTER_QUADROTOR: return MAV_ODID_UA_TYPE_HYBRID_LIFT;
-
-		case MAV_TYPE_VTOL_TILTROTOR: return MAV_ODID_UA_TYPE_HYBRID_LIFT;
-
-		case MAV_TYPE_VTOL_FIXEDROTOR: return MAV_ODID_UA_TYPE_HYBRID_LIFT;
-
-		case MAV_TYPE_VTOL_TAILSITTER: return MAV_ODID_UA_TYPE_HYBRID_LIFT;
-
-		case MAV_TYPE_VTOL_TILTWING: return MAV_ODID_UA_TYPE_HYBRID_LIFT;
-
-		case MAV_TYPE_VTOL_RESERVED5: return MAV_ODID_UA_TYPE_HYBRID_LIFT;
-
-		case MAV_TYPE_GIMBAL: return MAV_ODID_UA_TYPE_NONE;
-
-		case MAV_TYPE_ADSB: return MAV_ODID_UA_TYPE_NONE;
-
-		case MAV_TYPE_PARAFOIL: return MAV_ODID_UA_TYPE_FREE_FALL_PARACHUTE;
-
-		case MAV_TYPE_DODECAROTOR: return MAV_ODID_UA_TYPE_HELICOPTER_OR_MULTIROTOR;
-
-		case MAV_TYPE_CAMERA: return MAV_ODID_UA_TYPE_NONE;
-
-		case MAV_TYPE_CHARGING_STATION: return MAV_ODID_UA_TYPE_NONE;
-
-		case MAV_TYPE_FLARM: return MAV_ODID_UA_TYPE_NONE;
-
-		case MAV_TYPE_SERVO: return MAV_ODID_UA_TYPE_NONE;
-
-		case MAV_TYPE_ODID: return MAV_ODID_UA_TYPE_NONE;
-
-		case MAV_TYPE_DECAROTOR: return MAV_ODID_UA_TYPE_NONE;
-
-		case MAV_TYPE_BATTERY: return MAV_ODID_UA_TYPE_NONE;
-
-		case MAV_TYPE_PARACHUTE: return MAV_ODID_UA_TYPE_FREE_FALL_PARACHUTE;
-
-		case MAV_TYPE_LOG: return MAV_ODID_UA_TYPE_NONE;
-
-		case MAV_TYPE_OSD: return MAV_ODID_UA_TYPE_NONE;
-
-		case MAV_TYPE_IMU: return MAV_ODID_UA_TYPE_NONE;
-
-		case MAV_TYPE_GPS: return MAV_ODID_UA_TYPE_NONE;
-
-		case MAV_TYPE_WINCH: return MAV_ODID_UA_TYPE_NONE;
-
-		default: return MAV_ODID_UA_TYPE_OTHER;
-		}
-	}
 
 
 	bool send() override
@@ -167,7 +76,7 @@ private:
 			msg.id_type = MAV_ODID_ID_TYPE_SERIAL_NUMBER;
 
 			// ua_type: MAV_ODID_UA_TYPE
-			msg.ua_type = odidTypeForMavType(vehicle_status.system_type);
+			msg.ua_type = open_drone_id_translations::odidTypeForMavType(vehicle_status.system_type);
 
 			// uas_id: UAS (Unmanned Aircraft System) ID following the format specified by id_type
 			// TODO: MAV_ODID_ID_TYPE_SERIAL_NUMBER needs to be ANSI/CTA-2063 format
