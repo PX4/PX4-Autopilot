PX4/PX4-Autopilot

commit 4f64acb3524d39967bd7f516ae2ebfe4be5246bc

Docs for camera_feedback module (#23103)
* Docs for camera_feedback module

* Update src/modules/camera_feedback/CameraFeedback.cpp

* Update src/modules/camera_feedback/CameraFeedback.cpp

src/modules/camera_feedback/CameraFeedback.cpp
@@ -194,6 +194,24 @@ CameraFeedback::print_usage(const char *reason)
 		R"DESCR_STR(
 ### Description
 
+The camera_feedback module publishes `CameraCapture` UORB topics when image capture has been triggered.
+
+If camera capture is enabled, then trigger information from the camera capture pin is published; 
+otherwise trigger information at the point the camera was commanded to trigger is published
+(from the `camera_trigger` module).
+
+The `CAMERA_IMAGE_CAPTURED` message is then emitted (by streaming code) following `CameraCapture` updates.
+`CameraCapture` topics are also logged and can be used for geotagging.
+
+### Implementation
+
+`CameraTrigger` topics are published by the `camera_trigger` module (`feedback` field set `false`) when image capture is triggered,
+and may also be published by the  `camera_capture` driver (with `feedback` field set `true`) if the camera capture pin is activated.
+
+The `camera_feedback` module subscribes to `CameraTrigger`.
+It discards topics from the `camera_trigger` module if camera capture is enabled.
+For the topics that are not discarded it creates a `CameraCapture` topic with the timestamp information
+from the `CameraTrigger` and position information from the vehicle.
 
 )DESCR_STR");
 

commit 25212ab46b0cad8a97a99234245704e27598e7de

CameraFeedback: shorten line length such that documentation parser works
This broke in 4f64acb3524d39967bd7f516ae2ebfe4be5246bc and was also flagged by CI in the pr and since then.

src/modules/camera_feedback/CameraFeedback.cpp
@@ -196,7 +196,7 @@ CameraFeedback::print_usage(const char *reason)
 
 The camera_feedback module publishes `CameraCapture` UORB topics when image capture has been triggered.
 
-If camera capture is enabled, then trigger information from the camera capture pin is published; 
+If camera capture is enabled, then trigger information from the camera capture pin is published;
 otherwise trigger information at the point the camera was commanded to trigger is published
 (from the `camera_trigger` module).
 
@@ -205,8 +205,9 @@ The `CAMERA_IMAGE_CAPTURED` message is then emitted (by streaming code) followin
 
 ### Implementation
 
-`CameraTrigger` topics are published by the `camera_trigger` module (`feedback` field set `false`) when image capture is triggered,
-and may also be published by the  `camera_capture` driver (with `feedback` field set `true`) if the camera capture pin is activated.
+`CameraTrigger` topics are published by the `camera_trigger` module (`feedback` field set `false`)
+when image capture is triggered, and may also be published by the  `camera_capture` driver
+(with `feedback` field set `true`) if the camera capture pin is activated.
 
 The `camera_feedback` module subscribes to `CameraTrigger`.
 It discards topics from the `camera_trigger` module if camera capture is enabled.