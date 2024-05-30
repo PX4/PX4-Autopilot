902265f902e1fad883a33d85c35a33296c304ae8
Eric Katzfey <eric.katzfey@modalai.com>
2024-05-30 11:21:49 -0700

    Fixed a small memory leak in uORBManager.cpp

--- platforms/common/uORB/uORBManager.cpp
+++ platforms/common/uORB/uORBManager.cpp
@@ -621,7 +621,11 @@ int16_t uORB::Manager::process_remote_topic(const char *topic_name)
 			if (node) {
 				PX4_DEBUG("Marking DeviceNode(%s) as advertised in process_remote_topic", topic_name);
 				node->mark_as_advertised();
-				_remote_topics.insert(topic_name);
+
+				if (_remote_topics.find(topic_name) == false) {
+					_remote_topics.insert(topic_name);
+				}
+
 				return 0;
 			}
 		}
