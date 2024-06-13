5fbc34027619d9fc6f38084438ec64c3c6a5768f
Nuno Marques <n.marques21@hotmail.com>
2024-06-13 09:10:53 -0700

    src: drivers: uavcan: move UAVCAN_ECU_MAXF and UAVCAN_ECU_FUELT fetch to init and make reboot required

--- src/drivers/uavcan/sensors/fuel_tank_status.cpp
+++ src/drivers/uavcan/sensors/fuel_tank_status.cpp
@@ -57,6 +57,12 @@ int UavcanFuelTankStatusBridge::init()
 		return res;
 	}
 
+	// Fetch maximum fuel capacity (in liters)
+	param_get(param_find("UAVCAN_ECU_MAXF"), &_max_fuel_capacity);
+
+	// Fetching fuel type
+	param_get(param_find("UAVCAN_ECU_FUELT"), &_fuel_type);
+
 	return 0;
 }
 
@@ -65,16 +71,7 @@ void UavcanFuelTankStatusBridge::fuel_tank_status_sub_cb(const
 {
 	auto report = ::fuel_tank_status_s();
 	report.timestamp = hrt_absolute_time();
-
-	// Fetching maximum fuel capacity (in liters) from a parameter
-	param_get(param_find("UAVCAN_ECU_MAXF"), &_max_fuel_capacity);
-
-	_max_fuel_capacity *= 1000.0f; // convert to ml
-	report.maximum_fuel_capacity = _max_fuel_capacity;
-
-	// Fetching fuel type
-	param_get(param_find("UAVCAN_ECU_FUELT"), &_fuel_type);
-
+	report.maximum_fuel_capacity = _max_fuel_capacity * 1000.0f; // convert to ml
 	report.fuel_type = static_cast<uint8_t>(_fuel_type);
 	report.consumed_fuel = NAN; // only the remaining fuel is measured
 	report.fuel_consumption_rate = msg.fuel_consumption_rate_cm3pm / 60.0f; // convert to ml/s
