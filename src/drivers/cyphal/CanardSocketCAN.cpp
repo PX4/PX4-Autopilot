43add7c9fd42897fc907a9349dd7932f30b25bbd
PonomarevDA <ponomarevda96@gmail.com>
2024-08-05 22:44:02 +0300

    remove CONFIG_NET_CAN_RAW_TX_DEADLINE from fmu-v6c and fmu-v6x and add ifndef CONFIG_NET_CAN_RAW_TX_DEADLINE guards in cyphal socketcan driver

--- src/drivers/cyphal/CanardSocketCAN.cpp
+++ src/drivers/cyphal/CanardSocketCAN.cpp
@@ -90,12 +90,12 @@ int CanardSocketCAN::init()
 	/* NuttX Feature: Enable TX deadline when sending CAN frames
 	 * When a deadline occurs the driver will remove the CAN frame
 	 */
-
+#ifdef CONFIG_NET_CAN_RAW_TX_DEADLINE
 	if (setsockopt(_fd, SOL_CAN_RAW, CAN_RAW_TX_DEADLINE, &on, sizeof(on)) < 0) {
 		PX4_ERR("CAN_RAW_TX_DEADLINE is disabled");
 		return -1;
 	}
-
+#endif
 	if (can_fd) {
 		if (setsockopt(_fd, SOL_CAN_RAW, CAN_RAW_FD_FRAMES, &on, sizeof(on)) < 0) {
 			PX4_ERR("no CAN FD support");
@@ -174,7 +174,11 @@ int16_t CanardSocketCAN::transmit(const CanardTxQueueItem &txf, int timeout_ms)
 	_send_tv->tv_usec = deadline_systick % 1000000ULL;
 	_send_tv->tv_sec = (deadline_systick - _send_tv->tv_usec) / 1000000ULL;
 
+#ifdef CONFIG_NET_CAN_RAW_TX_DEADLINE
+	return sendmsg(_fd, &_send_msg, 0);
+#else
 	return sendmsg(_fd, &_send_msg, MSG_DONTWAIT);
+#endif
 }
 
 int16_t CanardSocketCAN::receive(CanardRxFrame *rxf)
