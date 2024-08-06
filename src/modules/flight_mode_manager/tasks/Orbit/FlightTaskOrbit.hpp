c7a910e553ee87c42983b05edb9f25dfe635c693
Mathieu Bresciani <brescianimathieu@gmail.com>
2024-08-06 16:12:10 +0200

    Update src/modules/flight_mode_manager/tasks/Orbit/FlightTaskOrbit.hpp

--- src/modules/flight_mode_manager/tasks/Orbit/FlightTaskOrbit.hpp
+++ src/modules/flight_mode_manager/tasks/Orbit/FlightTaskOrbit.hpp
@@ -44,7 +44,6 @@
 #include "FlightTaskManualAltitudeSmoothVel.hpp"
 #include <uORB/PublicationMulti.hpp>
 #include <uORB/topics/orbit_status.h>
-//#include <uORB/topics/vehicle_command.h>
 #include <lib/slew_rate/SlewRateYaw.hpp>
 #include <lib/motion_planning/PositionSmoothing.hpp>
 #include <lib/motion_planning/VelocitySmoothing.hpp>
