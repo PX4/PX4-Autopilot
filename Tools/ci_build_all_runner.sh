10000905b23ba2d0c360a281e9048cf68c4c9470
Ramon Roche <mrpollo@gmail.com>
2024-08-09 09:12:02 -0700

    tools: build tool fail

--- Tools/ci_build_all_runner.sh
+++ Tools/ci_build_all_runner.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # This script is meant to be used by the build_all.yml workflow in a github runner
 # Please only modify if you know what you are doing
+set -e
 
 targets=$1
 for target in ${targets//,/ }
