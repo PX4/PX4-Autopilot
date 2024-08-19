e29a36adb46aed60e1166860ebb490400d962d96
Claudio Chies <61051109+Claudio-Chies@users.noreply.github.com>
2024-08-19 08:01:43 +0200

    Landing horizontal velocity compensation / unsteady landing (#23546)
    * initial working
    
    * implemented feedback

--- src/modules/navigator/land.cpp
+++ src/modules/navigator/land.cpp
@@ -57,8 +57,13 @@ Land::on_activation()
 
 	/* convert mission item to current setpoint */
 	struct position_setpoint_triplet_s *pos_sp_triplet = _navigator->get_position_setpoint_triplet();
-	pos_sp_triplet->previous.valid = false;
+
+	if (_navigator->get_vstatus()->vehicle_type == vehicle_status_s::VEHICLE_TYPE_ROTARY_WING) {
+		_navigator->calculate_breaking_stop(_mission_item.lat, _mission_item.lon);
+	}
+
 	mission_item_to_position_setpoint(_mission_item, &pos_sp_triplet->current);
+	pos_sp_triplet->previous.valid = false;
 	pos_sp_triplet->next.valid = false;
 
 	_navigator->set_position_setpoint_triplet_updated();
