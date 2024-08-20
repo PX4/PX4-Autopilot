f252e20eaec78972ff0723af26c922c3015aa6a6
Jaeyoung Lim <jalim@ethz.ch>
2024-08-21 01:36:08 +0200

    Revert "Update GZBridge to be able to use gazebo airspeed. Add quadtailsitter. (#23455)" (#23583)
    This reverts commit 7e45f4915208b2d02e05e5741ff7456cf92120f9.
    
    Co-authored-by: jmackay2 <1.732mackay@gmail.com>

--- src/modules/simulation/gz_bridge/GZBridge.cpp
+++ src/modules/simulation/gz_bridge/GZBridge.cpp
@@ -203,15 +203,17 @@ int GZBridge::init()
 		PX4_WARN("failed to subscribe to %s", laser_scan_topic.c_str());
 	}
 
-	// Airspeed: /world/$WORLD/model/$MODEL/link/base_link/sensor/airspeed_sensor/air_speed
-	std::string airspeed_topic = "/world/" + _world_name + "/model/" + _model_name +
-				     "/link/base_link/sensor/airspeed_sensor/air_speed";
+#if 0
+	// Airspeed: /world/$WORLD/model/$MODEL/link/airspeed_link/sensor/air_speed/air_speed
+	std::string airpressure_topic = "/world/" + _world_name + "/model/" + _model_name +
+					"/link/airspeed_link/sensor/air_speed/air_speed";
 
-	if (!_node.Subscribe(airspeed_topic, &GZBridge::airspeedCallback, this)) {
-		PX4_ERR("failed to subscribe to %s", airspeed_topic.c_str());
+	if (!_node.Subscribe(airpressure_topic, &GZBridge::airspeedCallback, this)) {
+		PX4_ERR("failed to subscribe to %s", airpressure_topic.c_str());
 		return PX4_ERROR;
 	}
 
+#endif
 	// Air pressure: /world/$WORLD/model/$MODEL/link/base_link/sensor/air_pressure_sensor/air_pressure
 	std::string air_pressure_topic = "/world/" + _world_name + "/model/" + _model_name +
 					 "/link/base_link/sensor/air_pressure_sensor/air_pressure";
@@ -424,7 +426,8 @@ void GZBridge::barometerCallback(const gz::msgs::FluidPressure &air_pressure)
 	pthread_mutex_unlock(&_node_mutex);
 }
 
-void GZBridge::airspeedCallback(const gz::msgs::AirSpeed &air_speed)
+#if 0
+void GZBridge::airspeedCallback(const gz::msgs::AirSpeedSensor &air_speed)
 {
 	if (hrt_absolute_time() == 0) {
 		return;
@@ -449,6 +452,7 @@ void GZBridge::airspeedCallback(const gz::msgs::AirSpeed &air_speed)
 
 	pthread_mutex_unlock(&_node_mutex);
 }
+#endif
 
 void GZBridge::imuCallback(const gz::msgs::IMU &imu)
 {
