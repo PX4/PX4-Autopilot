08de04d9c1308cf213bd549a74dba952c0cb5269
RomanBapst <bapstroman@gmail.com>
2024-08-29 12:53:06 +0200

    fixed bug
    Signed-off-by: RomanBapst <bapstroman@gmail.com>

--- src/modules/navigator/mission.cpp
+++ src/modules/navigator/mission.cpp
@@ -212,7 +212,7 @@ void Mission::setActiveMissionItems()
 		// prevent fixed wing lateral guidance from loitering at a waypoint as part of a mission landing if the altitude
 		// is not achieved.
 		if (_vehicle_status_sub.get().vehicle_type == vehicle_status_s::VEHICLE_TYPE_FIXED_WING && isLanding() &&
-		    && _mission_item.nav_cmd == NAV_CMD_WAYPOINT) {
+		    _mission_item.nav_cmd == NAV_CMD_WAYPOINT) {
 			pos_sp_triplet->current.alt_acceptance_radius = FLT_MAX;
 		}
 
