baa6a418c48666a460d24f507b20e7a1b71182ef
chfriedrich98 <125505139+chfriedrich98@users.noreply.github.com>
2024-08-29 15:27:08 +0200

    differential: update module (#23629)
    Improve the slow down effect and add support for speed change in mission mode.
    Seperate code related to turning setpoints into motor commands into its own folder and refactor code.

--- src/modules/rover_differential/RoverDifferential.hpp
+++ src/modules/rover_differential/RoverDifferential.hpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2023-2024 PX4 Development Team. All rights reserved.
+ *   Copyright (c) 2024 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -42,23 +42,21 @@
 
 // uORB includes
 #include <uORB/Publication.hpp>
-#include <uORB/PublicationMulti.hpp>
 #include <uORB/Subscription.hpp>
 #include <uORB/topics/manual_control_setpoint.h>
 #include <uORB/topics/parameter_update.h>
 #include <uORB/topics/vehicle_status.h>
-#include <uORB/topics/actuator_motors.h>
 #include <uORB/topics/vehicle_angular_velocity.h>
 #include <uORB/topics/vehicle_attitude.h>
 #include <uORB/topics/vehicle_local_position.h>
-#include <uORB/topics/rover_differential_status.h>
+#include <uORB/topics/rover_differential_setpoint.h>
 
 // Standard libraries
-#include <lib/pid/pid.h>
 #include <matrix/matrix/math.hpp>
 
 // Local includes
 #include "RoverDifferentialGuidance/RoverDifferentialGuidance.hpp"
+#include "RoverDifferentialControl/RoverDifferentialControl.hpp"
 
 using namespace time_literals;
 
@@ -80,20 +78,16 @@ public:
 
 	bool init();
 
-	/**
-	 * @brief Computes motor commands for differential drive.
-	 *
-	 * @param forward_speed Linear velocity along the x-axis.
-	 * @param speed_diff Speed difference between left and right wheels.
-	 * @return matrix::Vector2f Motor velocities for the right and left motors.
-	 */
-	matrix::Vector2f computeMotorCommands(float forward_speed, const float speed_diff);
-
 protected:
 	void updateParams() override;
 
 private:
 	void Run() override;
+
+	/**
+	 * @brief Update uORB subscriptions.
+	 */
+	void updateSubscriptions();
 
 	// uORB Subscriptions
 	uORB::Subscription _manual_control_setpoint_sub{ORB_ID(manual_control_setpoint)};
@@ -104,33 +98,22 @@ private:
 	uORB::Subscription _vehicle_status_sub{ORB_ID(vehicle_status)};
 
 	// uORB Publications
-	uORB::PublicationMulti<actuator_motors_s> _actuator_motors_pub{ORB_ID(actuator_motors)};
-	uORB::Publication<rover_differential_status_s> _rover_differential_status_pub{ORB_ID(rover_differential_status)};
+	uORB::Publication<rover_differential_setpoint_s> _rover_differential_setpoint_pub{ORB_ID(rover_differential_setpoint)};
 
 	// Instances
 	RoverDifferentialGuidance _rover_differential_guidance{this};
+	RoverDifferentialControl _rover_differential_control{this};
 
 	// Variables
-	float _vehicle_body_yaw_rate{0.f};
+	matrix::Quatf _vehicle_attitude_quaternion{};
+	float _vehicle_yaw_rate{0.f};
 	float _vehicle_forward_speed{0.f};
 	float _vehicle_yaw{0.f};
 	float _max_yaw_rate{0.f};
 	int _nav_state{0};
-	matrix::Quatf _vehicle_attitude_quaternion{};
-	hrt_abstime _timestamp{0};
-	PID_t _pid_yaw_rate; // The PID controller for yaw rate
-	RoverDifferentialGuidance::differential_setpoint _differential_setpoint;
-
-	// Constants
-	static constexpr float YAW_RATE_ERROR_THRESHOLD = 0.1f; // [rad/s] Error threshold for the closed loop yaw rate control
 
 	DEFINE_PARAMETERS(
 		(ParamFloat<px4::params::RD_MAN_YAW_SCALE>) _param_rd_man_yaw_scale,
-		(ParamFloat<px4::params::RD_WHEEL_TRACK>) _param_rd_wheel_track,
-		(ParamFloat<px4::params::RD_YAW_RATE_P>) _param_rd_p_gain_yaw_rate,
-		(ParamFloat<px4::params::RD_YAW_RATE_I>) _param_rd_i_gain_yaw_rate,
-		(ParamFloat<px4::params::RD_MAX_SPEED>) _param_rd_max_speed,
-		(ParamFloat<px4::params::RD_MAX_YAW_RATE>) _param_rd_max_yaw_rate,
-		(ParamInt<px4::params::CA_R_REV>) _param_r_rev
+		(ParamFloat<px4::params::RD_MAX_YAW_RATE>) _param_rd_max_yaw_rate
 	)
 };
