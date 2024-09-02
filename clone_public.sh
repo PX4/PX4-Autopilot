0f27550c9ff31c27c381059700ec2f95b640ec80
Jari Nippula <jarix@ssrc.tii.ae>
2024-09-02 15:43:49 +0300

    add sec-udp-proxy

--- clone_public.sh
+++ clone_public.sh
@@ -15,6 +15,7 @@ do
   [[ "${repo}" == *saluki_packaging ]] || \
   [[ "${repo}" == *rust_px4_nuttx ]] || \
   [[ "${repo}" == *rust_module_example ]] || \
+  [[ "${repo}" == *secure_udp_proxy ]] || \
   [[ "${repo}" == *process ]] && continue
   git submodule update --init --recursive "${repo}"
 done <<< "$(git submodule status | awk '{print $2}')"
