bc26ff93d9320ec29076d9a864379364ab3d601b
Silvan Fuhrer <silvan@auterion.com>
2024-09-03 18:20:04 +0200

    Manual control: rename SOURCE_RC_STICK_GESTURE to SOURCE_MANUAL_CONTROL_GESTURE
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- src/modules/manual_control/ManualControl.cpp
+++ src/modules/manual_control/ManualControl.cpp
@@ -356,7 +356,7 @@ void ManualControl::processStickArming(const manual_control_setpoint_s &input)
 	_stick_arm_hysteresis.set_state_and_update(left_stick_lower_right && right_stick_centered, input.timestamp);
 
 	if (_param_man_arm_gesture.get() && !previous_stick_arm_hysteresis && _stick_arm_hysteresis.get_state()) {
-		sendActionRequest(action_request_s::ACTION_ARM, action_request_s::SOURCE_RC_STICK_GESTURE);
+		sendActionRequest(action_request_s::ACTION_ARM, action_request_s::SOURCE_MANUAL_CONTROL_GESTURE);
 	}
 
 	// Disarm gesture
@@ -366,7 +366,7 @@ void ManualControl::processStickArming(const manual_control_setpoint_s &input)
 	_stick_disarm_hysteresis.set_state_and_update(left_stick_lower_left && right_stick_centered, input.timestamp);
 
 	if (_param_man_arm_gesture.get() && !previous_stick_disarm_hysteresis && _stick_disarm_hysteresis.get_state()) {
-		sendActionRequest(action_request_s::ACTION_DISARM, action_request_s::SOURCE_RC_STICK_GESTURE);
+		sendActionRequest(action_request_s::ACTION_DISARM, action_request_s::SOURCE_MANUAL_CONTROL_GESTURE);
 	}
 
 	// Kill gesture
@@ -377,7 +377,7 @@ void ManualControl::processStickArming(const manual_control_setpoint_s &input)
 		_stick_kill_hysteresis.set_state_and_update(left_stick_lower_left && right_stick_lower_right, input.timestamp);
 
 		if (!previous_stick_kill_hysteresis && _stick_kill_hysteresis.get_state()) {
-			sendActionRequest(action_request_s::ACTION_KILL, action_request_s::SOURCE_RC_STICK_GESTURE);
+			sendActionRequest(action_request_s::ACTION_KILL, action_request_s::SOURCE_MANUAL_CONTROL_GESTURE);
 		}
 	}
 }
