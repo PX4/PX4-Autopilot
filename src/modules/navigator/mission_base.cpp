147472e86581b2c89e7f6b8c27082b4634165048
RomanBapst <bapstroman@gmail.com>
2024-09-10 14:59:35 +0300

    mission base: avoid unnecessary sd card readings
    Signed-off-by: RomanBapst <bapstroman@gmail.com>

--- src/modules/navigator/mission_base.cpp
+++ src/modules/navigator/mission_base.cpp
@@ -370,7 +370,7 @@ MissionBase::on_active()
 bool
 MissionBase::isLanding()
 {
-	if (hasMissionLandStart()) {
+	if (hasMissionLandStart() && (_mission.current_seq > _mission.land_start_index)) {
 		static constexpr size_t max_num_next_items{1u};
 		int32_t next_mission_items_index[max_num_next_items];
 		size_t num_found_items;
