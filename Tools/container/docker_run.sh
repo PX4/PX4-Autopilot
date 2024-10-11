ae27182ea8404b78c36d8f64200f92f0a5762396
Ramon Roche <mrpollo@gmail.com>
2024-10-11 13:44:08 -0700

    container: runner with default tag

--- Tools/container/docker_run.sh
+++ Tools/container/docker_run.sh
@@ -3,7 +3,7 @@
 if [[ -z "${DOCKER_TAG}" ]]; then
   # The default tag name should be hardcoded
   # to whatever we are currently using in CI on this branch
-  TAG_NAME="latest"
+  TAG_NAME="2024-10-11"
 else
   TAG_NAME="${DOCKER_TAG}"
 fi
@@ -17,15 +17,23 @@ SRC_DIR=${SRC_DIR%/*/*}
 echo "[docker_run.sh] Project Root: $SRC_DIR"
 echo "[docker_run.sh] ⭐️ Running: '$PX4_DOCKER_REPO'"
 
-docker run -it --rm \
-  --env=LOCAL_USER_ID="$(id -u)" \
+docker run -it --rm -w "${SRC_DIR}" \
   --env=AWS_ACCESS_KEY_ID \
-  --env=AWS_SECRET_ACCESS_KEY \
-  --env=BRANCH_NAME \
-  --env=CCACHE_DIR="${CCACHE_DIR}" \
-  --env=CI \
-  --publish 14556:14556/udp \
+	--env=AWS_SECRET_ACCESS_KEY \
+	--env=BRANCH_NAME \
+	--env=CCACHE_DIR="${CCACHE_DIR}" \
+	--env=CI \
+	--env=CODECOV_TOKEN \
+	--env=COVERALLS_REPO_TOKEN \
+	--env=LOCAL_USER_ID="$(id -u)" \
+	--env=PX4_ASAN \
+	--env=PX4_MSAN \
+	--env=PX4_TSAN \
+	--env=PX4_UBSAN \
+	--env=TRAVIS_BRANCH \
+	--env=TRAVIS_BUILD_ID \
+	--publish 14556:14556/udp \
   --volume=/tmp/.X11-unix:/tmp/.X11-unix \
   --volume=/tmp:/tmp:rw \
-  --volume=${SRC_DIR}:/px4:rw \
+  --volume=${SRC_DIR}:${SRC_DIR}:rw \
   ${PX4_DOCKER_REPO} /bin/bash -c "$1 $2 $3"
