460adde4c533b59c6f800c2ecf2fa2cb37c1daa0
alexklimaj <alex@arkelectron.com>
2024-10-18 18:43:58 -0600

    Disable CAN2

--- boards/ark/fpv/src/can.c
+++ boards/ark/fpv/src/can.c
@@ -49,9 +49,7 @@ uint16_t board_get_can_interfaces(void)
 {
 	uint16_t enabled_interfaces = 0x3;
 
-	if (!PX4_MFT_HW_SUPPORTED(PX4_MFT_CAN2)) {
-		enabled_interfaces &= ~(1 << 1);
-	}
+	enabled_interfaces &= ~(1 << 1);
 
 	return enabled_interfaces;
 }
