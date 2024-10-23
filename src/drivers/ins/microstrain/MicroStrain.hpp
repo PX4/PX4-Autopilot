7d1f847775a10ac92d2036addb7fa9e0109f12bc
jjohn <jjohn2@wpi.edu>
2024-10-23 09:25:24 -0400

    Cleanup + ConfigureIMUMessage

--- src/drivers/ins/microstrain/MicroStrain.hpp
+++ src/drivers/ins/microstrain/MicroStrain.hpp
@@ -33,10 +33,8 @@
 
 #pragma once
 
-#include <px4_platform_common/defines.h>
 #include <px4_platform_common/module.h>
 #include <px4_platform_common/module_params.h>
-#include <px4_platform_common/posix.h>
 #include <px4_platform_common/px4_work_queue/ScheduledWorkItem.hpp>
 #include <px4_platform_common/getopt.h>
 
@@ -47,49 +45,27 @@
 #include <lib/drivers/magnetometer/PX4Magnetometer.hpp>
 
 #include <uORB/Publication.hpp>
-#include <uORB/Subscription.hpp>
 #include <uORB/SubscriptionCallback.hpp>
-#include <uORB/topics/orb_test.h>
 #include <uORB/topics/parameter_update.h>
-#include <uORB/topics/debug_array.h>
 #include <uORB/topics/sensor_accel.h>
-#include <uORB/topics/vehicle_status.h>
+#include <uORB/topics/sensor_mag.h>
+#include <uORB/topics/sensor_gyro.h>
 #include <uORB/topics/sensor_baro.h>
-#include <uORB/topics/sensor_gps.h>
-#include <uORB/topics/sensor_selection.h>
-#include <uORB/topics/vehicle_global_position.h>
-#include <uORB/topics/vehicle_status.h>
-#include <uORB/topics/vehicle_local_position.h>
-#include <uORB/topics/vehicle_angular_velocity.h>
-#include <uORB/topics/vehicle_attitude.h>
-#include <uORB/topics/vehicle_air_data.h>
-#include <uORB/topics/distance_sensor.h>
-#include <uORB/topics/estimator_status.h>
-#include <uORB/topics/sensor_selection.h>
-#include <uORB/topics/vehicle_odometry.h>
-#include <uORB/topics/vehicle_global_position.h>
-
-
 
 #include "mip_sdk/src/mip/mip_all.h"
-#include "mip_sdk/src/mip/mip_interface.h"
-#include "mip_sdk/src/mip/utils/serial_port.h"
 
-#include "mip_sdk/src/mip/definitions/commands_aiding.h"
-
-#include <containers/Array.hpp>
+#include "modal_io_serial.hpp"
 
 using namespace mip::C;
 
 using namespace time_literals;
 
-#include "modal_io_serial.hpp"
 
-class MicrosStrain : public ModuleBase<MicrosStrain>, public ModuleParams, public px4::ScheduledWorkItem
+class MicroStrain : public ModuleBase<MicroStrain>, public ModuleParams, public px4::ScheduledWorkItem
 {
 public:
-	MicrosStrain(const char *device, int32_t rotation);
-	~MicrosStrain() override;
+	MicroStrain(const char *device, int32_t rotation);
+	~MicroStrain() override;
 
 	/* Callbacks */
 
@@ -110,11 +86,6 @@ public:
 
 	bool init();
 
-	uint32_t _debug_rx_bytes[4] {0};
-	uint32_t _debug_tx_bytes{0};
-
-	uint64_t _delay_offset{0};
-
 private:
 	/** @see ModuleBase */
 	void Run() override;
@@ -124,9 +95,15 @@ private:
 
 	void set_sensor_rate(mip_descriptor_rate *sensor_descriptors, uint16_t len);
 
-	int connect_at_baud(int32_t baud);
+	mip_cmd_result get_supported_descriptors();
 
-	void apply_mag_cal();
+	bool supports_descriptor(uint8_t descriptor_set, uint8_t field_descriptor);
+
+	mip_cmd_result get_base_rate(uint8_t descriptor_set, uint16_t *base_rate);
+
+	mip_cmd_result configure_imu_message_format();
+
+	int connect_at_baud(int32_t baud);
 
 	enum Rotation rotation = ROTATION_NONE;
 	uint32_t dev_id{0};
@@ -137,23 +114,11 @@ private:
 	PX4Magnetometer _px4_mag{0};
 	sensor_baro_s _sensor_baro{0};
 
-	uORB::Publication<vehicle_local_position_s> _vehicle_local_position_pub{ORB_ID(vehicle_local_position)};
-	uORB::Publication<vehicle_angular_velocity_s> _vehicle_angular_velocity_pub{ORB_ID(vehicle_angular_velocity)};
-	uORB::Publication<vehicle_attitude_s> _vehicle_attitude_pub{ORB_ID(vehicle_attitude)};
-	uORB::Publication<vehicle_global_position_s> _global_position_pub{ORB_ID(vehicle_global_position)};
-	uORB::Publication<vehicle_odometry_s> _vehicle_odometry_pub{ORB_ID(vehicle_odometry)};
-	uORB::Publication<debug_array_s> _debug_array_pub{ORB_ID(debug_array)};
-
-	// Needed for health checks
-	uORB::Publication<estimator_status_s> _estimator_status_pub{ORB_ID(estimator_status)};
-
 	// Must publish to prevent sensor stale failure (sensors module)
 	uORB::PublicationMulti<sensor_baro_s> _sensor_baro_pub{ORB_ID(sensor_baro)};
-	uORB::Publication<sensor_selection_s> _sensor_selection_pub{ORB_ID(sensor_selection)};
 
 	// Subscriptions
-	uORB::SubscriptionInterval         _parameter_update_sub{ORB_ID(parameter_update), 1_s}; // subscription limited to 1 Hz updates
-	uORB::Subscription                 _sensor_gps_sub{ORB_ID(sensor_gps)};
+	uORB::SubscriptionInterval _parameter_update_sub{ORB_ID(parameter_update), 1_s}; // subscription limited to 1 Hz updates
 
 	// Performance (perf) counters
 	perf_counter_t	_loop_perf{perf_alloc(PC_ELAPSED, MODULE_NAME": cycle")};
@@ -162,6 +127,13 @@ private:
 	uint8_t parse_buffer[2048];
 	bool _is_initialized{false};
 	bool _is_init_failed{false};
+
+	int ms_schedule_rate_us{0};
+
+	uint16_t _supported_descriptors[1024] = {0};
+	uint8_t _supported_desc_len = 0;
+	uint16_t _supported_descriptor_sets[1000] = {0};
+	uint8_t _supported_desc_set_len = 0;
 
 	/******************************/
 	mip::C::mip_interface device;
@@ -174,11 +146,9 @@ private:
 
 	// Parameters
 	DEFINE_PARAMETERS(
-		(ParamInt<px4::params::MS_SCHEDULE>) _param_ms_schedule,
-		(ParamInt<px4::params::SENSOR_DELAY>) _param_sensor_delay,
-		(ParamInt<px4::params::PR_UPDATE_RATE>) _param_pr_update_rate,
-		(ParamInt<px4::params::SEC_UPDATE_RATE>) _param_sec_update_rate
-
+		(ParamInt<px4::params::MS_IMU_RATE_HZ>) _param_ms_imu_rate_hz,
+		(ParamInt<px4::params::MS_MAG_RATE_HZ>) _param_ms_mag_rate_hz,
+		(ParamInt<px4::params::MS_BARO_RATE_HZ>) _param_ms_baro_rate_hz
 	)
 
 };
