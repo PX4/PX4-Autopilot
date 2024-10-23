b6eb8dbfd947e393b426ffc63b9774dddc848935
Silvan Fuhrer <silvan@auterion.com>
2024-10-23 15:45:27 +0200

    Navigator: land: set lat/lon fields of triplet to NAN if global position is not valid
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- src/modules/navigator/mission_block.cpp
+++ src/modules/navigator/mission_block.cpp
@@ -832,8 +832,15 @@ MissionBlock::set_land_item(struct mission_item_s *item)
 	item->nav_cmd = NAV_CMD_LAND;
 
 	// set land item to current position
-	item->lat = _navigator->get_global_position()->lat;
-	item->lon = _navigator->get_global_position()->lon;
+	if (_navigator->get_local_position()->xy_global) {
+		item->lat = _navigator->get_global_position()->lat;
+		item->lon = _navigator->get_global_position()->lon;
+
+	} else {
+		item->lat = (double)NAN;
+		item->lon = (double)NAN;
+	}
+
 	item->yaw = NAN;
 
 	item->altitude = 0;
