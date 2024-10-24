1543bc0c06c2efa40f8fce97b3b4ba54a3d00bb1
Matthias Grob <maetugr@gmail.com>
2024-10-24 20:08:38 +0200

    navigator: refactor naming of break instead of brake functions

--- src/modules/navigator/loiter.cpp
+++ src/modules/navigator/loiter.cpp
@@ -109,7 +109,7 @@ Loiter::set_loiter_position()
 			setLoiterItemFromCurrentPositionSetpoint(&_mission_item);
 
 		} else if (_navigator->get_vstatus()->vehicle_type == vehicle_status_s::VEHICLE_TYPE_ROTARY_WING) {
-			setLoiterItemFromCurrentPositionWithBreaking(&_mission_item);
+			setLoiterItemFromCurrentPositionWithBraking(&_mission_item);
 
 		} else {
 			setLoiterItemFromCurrentPosition(&_mission_item);
