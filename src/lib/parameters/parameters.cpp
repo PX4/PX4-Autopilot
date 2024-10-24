tiiuae/px4-firmware

nuttx/config_build_kernel: Do not use user malloc etc for kernel modules
malloc/realloc/free/mallinfo etc don't work correctly when executing NuttX
kernel code, as they are used for user heap.

For kernel modules, use the kernel versions instead.

src/lib/parameters/parameters.cpp
@@ -81,6 +81,10 @@ using namespace time_literals;
 #include "parameters_ioctl.h"
 #endif
 
+#if defined(__PX4_NUTTX)
+#include <kmalloc.h>
+#endif
+
 #if defined(FLASH_BASED_PARAMS)
 #include "flashparams/flashparams.h"
 #else

PX4/PX4-Autopilot

src/lib/parameters/parameters.cpp
@@ -81,6 +81,10 @@ using namespace time_literals;
 #include "parameters_ioctl.h"
 #endif
 
+#if defined(__PX4_NUTTX)
+#include <klibc_glue.h>
+#endif
+
 #if defined(FLASH_BASED_PARAMS)
 #include "flashparams/flashparams.h"
 #else