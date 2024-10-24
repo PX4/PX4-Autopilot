tiiuae/px4-firmware

src/drivers/uavcan: Use UAVCAN_ENABLE param for correct uavcan dynamic node server initialization

src/drivers/uavcan/uavcan_main.cpp
@@ -561,11 +561,12 @@ UavcanNode::init(uavcan::NodeID node_id, UAVCAN_DRIVER::BusEvent &bus_events)
 	_param_opcode_client.setCallback(ExecuteOpcodeCallback(this, &UavcanNode::cb_opcode));
 	_param_restartnode_client.setCallback(RestartNodeCallback(this, &UavcanNode::cb_restart));
 
+	int32_t uavcan_enable = 1;
+	(void)param_get(param_find("UAVCAN_ENABLE"), &uavcan_enable);
+	int32_t uavcan_dynamic_node_server_enable = 1;
+	(void)param_get(param_find("UAVCAN_DNS"), &uavcan_dynamic_node_server_enable);
 
-	int32_t uavcan_dyna_node_server = 1;
-	(void)param_get(param_find("UAVCAN_DNS"), &uavcan_dyna_node_server);
-
-	if (uavcan_dyna_node_server == 1) {
+	if (uavcan_enable > 1 && uavcan_dynamic_node_server == 1) {
 		_servers = new UavcanServers(_node, _node_info_retriever);
 
 		if (_servers) {

PX4/PX4-Autopilot

src/drivers/uavcan/uavcan_main.cpp
@@ -576,11 +576,12 @@ UavcanNode::init(uavcan::NodeID node_id, UAVCAN_DRIVER::BusEvent &bus_events)
 	_param_opcode_client.setCallback(ExecuteOpcodeCallback(this, &UavcanNode::cb_opcode));
 	_param_restartnode_client.setCallback(RestartNodeCallback(this, &UavcanNode::cb_restart));
 
+	int32_t uavcan_enable = 1;
+	(void)param_get(param_find("UAVCAN_ENABLE"), &uavcan_enable);
+	int32_t uavcan_dynamic_node_server_enable = 1;
+	(void)param_get(param_find("UAVCAN_DNS"), &uavcan_dynamic_node_server_enable);
 
-	int32_t uavcan_dyna_node_server = 1;
-	(void)param_get(param_find("UAVCAN_DNS"), &uavcan_dyna_node_server);
-
-	if (uavcan_dyna_node_server == 1) {
+	if (uavcan_enable > 1 && uavcan_dynamic_node_server_enable == 1) {
 		_servers = new UavcanServers(_node, _node_info_retriever);
 
 		if (_servers) {