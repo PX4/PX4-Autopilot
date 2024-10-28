c8c9ba7ee3d5b86bcfd07f87fbb410ecaf0b6c4a
chfriedrich98 <chfriedrich@student.ethz.ch>
2024-10-28 16:33:38 +0100

    mecanum: fix clang tidy check

--- src/modules/rover_mecanum/RoverMecanum.cpp
+++ src/modules/rover_mecanum/RoverMecanum.cpp
@@ -161,13 +161,13 @@ void RoverMecanum::Run()
 				rover_mecanum_setpoint.yaw_setpoint = NAN;
 
 				// Reset cruise control if the manual input changes
-				if (_yaw_ctl == true && (!(fabsf(manual_control_setpoint.throttle - _prev_throttle) > STICK_DEADZONE)
-							 || !(fabsf(manual_control_setpoint.roll - _prev_roll) > STICK_DEADZONE))) {
+				if (_yaw_ctl && (!(fabsf(manual_control_setpoint.throttle - _prev_throttle) > STICK_DEADZONE)
+						 || !(fabsf(manual_control_setpoint.roll - _prev_roll) > STICK_DEADZONE))) {
 					_yaw_ctl = false;
 					_prev_throttle = manual_control_setpoint.throttle;
 					_prev_roll = manual_control_setpoint.roll;
 
-				} else if (_yaw_ctl == false) {
+				} else if (!_yaw_ctl) {
 					_prev_throttle = manual_control_setpoint.throttle;
 					_prev_roll = manual_control_setpoint.roll;
 				}
