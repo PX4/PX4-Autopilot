7660084d350bd508406d6907a92686dab74ba179
Ville Juven <ville.juven@unikie.com>
2024-10-29 11:31:49 +0200

    board_reset.cpp: Support CONFIG_SMP=y
    Execute the reboot command (jump to __start) on all CPUs. The boot CPU i.e.
    CPU0 will wait for the other CPUs to reboot first, otherwise when CPU0
    does the .data/.bss section initialization it will pull the rug from
    under the other CPUs feet by modifying global data unexpectedly.

--- platforms/nuttx/src/px4/microchip/mpfs/board_reset/board_reset.cpp
+++ platforms/nuttx/src/px4/microchip/mpfs/board_reset/board_reset.cpp
@@ -39,10 +39,40 @@
 
 #include <px4_platform_common/px4_config.h>
 #include <errno.h>
+#include <nuttx/atomic.h>
 #include <nuttx/board.h>
+#include <nuttx/sched.h>
 #include <hardware/mpfs_wdog.h>
 
 #include "riscv_internal.h"
+
+#ifdef CONFIG_SMP
+/* The CPU mask for all (valid) CPUs */
+
+#define ALL_CPUS ((1 << CONFIG_SMP_NCPUS) - 1)
+
+/* PX4 has -nostdinc++ set, which confuses the logic in <nuttx/atomic.h>
+ * making it pull the C-versions of atomic_fetch_add/atomic_load which use
+ * __auto_type, which should match the counters here.
+ */
+
+#define __auto_type int *
+
+/* With SMP, first we need to wait for all CPUs to pause (g_cpus_paused).
+ * Then the CPUs can be reset, but CPU0 must be reset last (g_cpus_ready).
+ * Otherwise a race can occur on g_cpus_paused as CPU0 clears .bss.
+ *
+ * Note: We cannot use locks that allow synchronization (like semaphores) here
+ * because we want the system reset to happen as fast as possible.
+ */
+
+static int g_cpus_paused;
+static int g_cpus_ready;
+
+/* Handle for nxsched_smp_call_async */
+
+static struct smp_call_data_s g_reboot_data;
+#endif
 
 extern "C" void __start(void);
 
@@ -64,19 +94,53 @@ static void board_reset_enter_bootloader_and_continue_boot()
 	for (; ;);
 }
 
-static void board_reset_enter_app(uintptr_t hartid)
+static int board_reset_enter_app(FAR void *arg)
 {
-	register long r0 asm("a0") = (long)(hartid);
+	uintptr_t hartid = riscv_mhartid();
+
+	/* Mask local interrupts */
+
+	up_irq_save();
+
+#ifdef CONFIG_SMP
+	/* Notify that this CPU is paused */
+
+	atomic_fetch_add(&g_cpus_paused, 1);
+
+	/* Wait for ALL CPUs to be ready */
+
+	while (atomic_load(&g_cpus_paused) < CONFIG_SMP_NCPUS);
+
+	/* Notify that this CPU has now ready */
+
+	atomic_fetch_add(&g_cpus_ready, 1);
+
+	/* CPU0 must then wait for other CPUs to start */
+
+	if (this_cpu() == 0) {
+		while (atomic_load(&g_cpus_ready) < CONFIG_SMP_NCPUS);
+	}
+
+#endif
+
+	/* Make sure the CPU cache is flushed so data is not lost */
+
+	up_flush_dcache_all();
 
 	asm volatile
 	(
-		"tail __start\n" :: "r"(r0) : "memory"
+		"fence\n"
+		"mv   a0, %0\n"
+		"tail __start\n" :: "r"(hartid) : "memory"
 	);
+
+	/* Never reached */
+
+	return 0;
 }
 
 int board_reset(int status)
 {
-	uintptr_t hartid = riscv_mhartid();
 #if defined(BOARD_HAS_ON_RESET)
 	board_on_reset(status);
 #endif
@@ -88,9 +152,32 @@ int board_reset(int status)
 		board_reset_enter_bootloader_and_continue_boot();
 	}
 
+#ifdef CONFIG_SMP
+	g_reboot_data.func = board_reset_enter_app;
+	g_reboot_data.arg  = NULL;
+	g_cpus_paused      = 0;
+	g_cpus_ready       = 0;
+
+	/* Reset the other CPUs via SMP call.
+	 *
+	 * The SMP call in fact does run the callback on this CPU as well if
+	 * requested to do so, but it does it BEFORE dispatching the request to the
+	 * other CPUs. This is why we need to remove ourself from the CPU set.
+	 */
+
+	cpu_set_t cpuset = ALL_CPUS;
+	CPU_CLR(this_cpu(), &cpuset);
+
+	/* We cannot wait in smp_call because the target CPUs are reset, thus they
+	 * will never acknowledge that they have run the handler!
+	 */
+
+	nxsched_smp_call_async(cpuset, &g_reboot_data);
+#endif
+
 	/* Just reboot via reset vector */
 
-	board_reset_enter_app(hartid);
+	board_reset_enter_app(NULL);
 
 	return 0;
 }
