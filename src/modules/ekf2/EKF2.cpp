3f084c94bd4bdb6023975ba36861266aa6eb903c
Andrew Brahim <35986980+dirksavage88@users.noreply.github.com>
2024-11-05 11:11:27 -0500

    Update src/modules/ekf2/EKF2.cpp
    Co-authored-by: Mathieu Bresciani <brescianimathieu@gmail.com>

--- src/modules/ekf2/EKF2.cpp
+++ src/modules/ekf2/EKF2.cpp
@@ -2410,7 +2410,7 @@ void EKF2::UpdateGpsSample(ekf2_timestamps_s &ekf2_timestamps)
 				// Apply offset
 				float raw_yaw_offset = matrix::wrap_pi(math::radians(_param_ekf2_gps_yaw_off.get()));
 				vehicle_gps_position.heading_offset = raw_yaw_offset;
-				vehicle_gps_position.heading -= raw_yaw_offset;
+				vehicle_gps_position.heading = matrix::wrap_pi(vehicle_gps_position.heading - raw_yaw_offset);
 			}
 		}
 
