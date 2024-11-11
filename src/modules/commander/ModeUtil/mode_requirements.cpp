c719a3266b6b513b55d2ce1e45d814341b51857a
RomanBapst <bapstroman@gmail.com>
2024-11-11 10:34:15 +0300

    mode_requirements: require only relaxed local position for auto land, in order to enable auto land with optical flow
    Signed-off-by: RomanBapst <bapstroman@gmail.com>

--- src/modules/commander/ModeUtil/mode_requirements.cpp
+++ src/modules/commander/ModeUtil/mode_requirements.cpp
@@ -140,7 +140,7 @@ void getModeRequirements(uint8_t vehicle_type, failsafe_flags_s &flags)
 	setRequirement(vehicle_status_s::NAVIGATION_STATE_AUTO_LAND, flags.mode_req_angular_velocity);
 	setRequirement(vehicle_status_s::NAVIGATION_STATE_AUTO_LAND, flags.mode_req_attitude);
 	setRequirement(vehicle_status_s::NAVIGATION_STATE_AUTO_LAND, flags.mode_req_local_alt);
-	setRequirement(vehicle_status_s::NAVIGATION_STATE_AUTO_LAND, flags.mode_req_local_position);
+	setRequirement(vehicle_status_s::NAVIGATION_STATE_AUTO_LAND, flags.mode_req_local_position_relaxed);
 	setRequirement(vehicle_status_s::NAVIGATION_STATE_AUTO_LAND, flags.mode_req_prevent_arming);
 
 	// NAVIGATION_STATE_AUTO_FOLLOW_TARGET
