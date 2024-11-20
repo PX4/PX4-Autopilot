ba0a761ca068169b88a774fa83a3b0b284dc4d8b
somebody-once-told-me <155648712+somebody-once-told-me@users.noreply.github.com>
2024-11-20 03:52:36 -0500

    Centralize yaw margin (#23982)

--- src/modules/control_allocator/ControlAllocation/ControlAllocationSequentialDesaturation.hpp
+++ src/modules/control_allocator/ControlAllocation/ControlAllocationSequentialDesaturation.hpp
@@ -57,6 +57,10 @@ public:
 	void allocate() override;
 
 	void updateParameters() override;
+
+	// This is the minimum actuator yaw granted when the controller is saturated.
+	// In the yaw-only case where outputs are saturated, thrust is reduced by up to this amount.
+	static constexpr float MINIMUM_YAW_MARGIN{0.15f};
 private:
 
 	/**
