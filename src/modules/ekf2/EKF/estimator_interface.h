62a8df77574f2167645446bd81ac8174424c0077
bresch <brescianimathieu@gmail.com>
2024-11-22 12:03:11 +0100

    ekf2: do not inline getPosition

--- src/modules/ekf2/EKF/estimator_interface.h
+++ src/modules/ekf2/EKF/estimator_interface.h
@@ -242,25 +242,7 @@ public:
 	Vector3f getVelocity() const { return _output_predictor.getVelocity(); }
 	const Vector3f &getVelocityDerivative() const { return _output_predictor.getVelocityDerivative(); }
 	float getVerticalPositionDerivative() const { return _output_predictor.getVerticalPositionDerivative(); }
-	Vector3f getPosition() const
-	{
-		LatLonAlt lla = _output_predictor.getLatLonAlt();
-		float x;
-		float y;
-
-		if (_local_origin_lat_lon.isInitialized()) {
-			_local_origin_lat_lon.project(lla.latitude_deg(), lla.longitude_deg(), x, y);
-
-		} else {
-			MapProjection zero_ref;
-			zero_ref.initReference(0.0, 0.0);
-			zero_ref.project(lla.latitude_deg(), lla.longitude_deg(), x, y);
-		}
-
-		const float z = -(lla.altitude() - getEkfGlobalOriginAltitude());
-
-		return Vector3f(x, y, z);
-	}
+	Vector3f getPosition() const;
 	LatLonAlt getLatLonAlt() const { return _output_predictor.getLatLonAlt(); }
 	const Vector3f &getOutputTrackingError() const { return _output_predictor.getOutputTrackingError(); }
 
