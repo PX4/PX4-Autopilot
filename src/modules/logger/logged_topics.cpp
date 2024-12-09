26719da92c51768b2571873ab4fdccbf0ca7fbe7
Knut Hjorth <knut.hjorth@aviant.no>
2024-12-09 13:39:41 +0100

    logger: log GNSS data at max rate
    The data is typically received at 5 Hz, and we want to log everything
    for troubleshooting and analysis.

--- src/modules/logger/logged_topics.cpp
+++ src/modules/logger/logged_topics.cpp
@@ -180,7 +180,7 @@ void LoggedTopics::add_default_topics()
 	add_topic_multi("optical_flow", 1000, 1);
 	add_optional_topic_multi("sensor_accel", 1000, 4);
 	add_optional_topic_multi("sensor_baro", 1000, 4);
-	add_topic_multi("sensor_gps", 1000, 2);
+	add_topic_multi("sensor_gps", 0, 2);
 	add_topic_multi("sensor_gnss_relative", 1000, 1);
 	add_optional_topic("pps_capture", 1000);
 	add_optional_topic_multi("sensor_gyro", 1000, 4);
