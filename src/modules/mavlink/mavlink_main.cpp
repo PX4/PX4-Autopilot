5a5523eada8b6f90c8b01b508c2a1d5273170aa6
Balduin <balduin@auterion.com>
2024-12-12 16:18:24 +0100

    format

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -672,6 +672,7 @@ Mavlink::set_hil_enabled(bool hil_enabled)
 			configure_stream("HIL_STATE_QUATERNION", 25.0f); // ground truth to display the SIH
 			// stream attitude also as quaternion for correct display
 			configure_stream("ATTITUDE_QUATERNION", 20.0f);
+
 		} else {
 			configure_stream("HIL_STATE_QUATERNION", 0.0f);
 		}
