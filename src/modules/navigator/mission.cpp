b9b82ecce7b69a9d3ad993ebbda8a0dd89fe08e7
RomanBapst <bapstroman@gmail.com>
2024-12-20 09:47:24 +0300

    added check for vtol
    Signed-off-by: RomanBapst <bapstroman@gmail.com>

--- src/modules/navigator/mission.cpp
+++ src/modules/navigator/mission.cpp
@@ -215,7 +215,7 @@ void Mission::setActiveMissionItems()
 						   && isLanding() &&
 						   _mission_item.nav_cmd == NAV_CMD_WAYPOINT;
 		const bool mc_landing_after_transition = _vehicle_status_sub.get().vehicle_type ==
-				vehicle_status_s::VEHICLE_TYPE_ROTARY_WING &&
+				vehicle_status_s::VEHICLE_TYPE_ROTARY_WING && _vehicle_status_sub.get().is_vtol &&
 				new_work_item_type == WorkItemType::WORK_ITEM_TYPE_MOVE_TO_LAND;
 
 		if (fw_on_mission_landing || mc_landing_after_transition) {
