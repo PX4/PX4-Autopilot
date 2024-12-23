196f26d4d45e47e03a0e02e7f5c134314b7a4cc7
Master Chief <master-chief@the-void.com>
2024-12-23 11:55:59 -0800

    update comments to match yaw-only effect

--- src/lib/mixer_module/params.c
+++ src/lib/mixer_module/params.c
@@ -18,16 +18,18 @@
 PARAM_DEFINE_INT32(MC_AIRMODE, 0);
 
 /**
- * Multicopter Reduce Thrust
+ * Multicopter Throttle Reduction for Increased Yaw Actuation
  *
  * The default behavior for multicopters is to trade altitude (z-thrust) for attitude automatically.
+ * This is reasonable for pitch and roll, because it prevents a worsening problem (increasing
+ * throttle requirements). But this is not always reasonable for yaw actuation.
  *
  * This gives an alternative to the default behavior where z-thrust is never sacrificed for
- * attitude. The idea is to leave it up to the pilot to sacrifice altitude for attitude.
+ * yaw actuation. The idea is to leave it up to the pilot to sacrifice altitude for yaw actuation.
  *
- * If false, Z-thrust is never sacrificed for attitude (roll, pitch, yaw).
- * If true, default behavior. Z-thrust is reduced to make room for roll, pitch, and yaw
- * 	setpoints).
+ * If false, Z-thrust is never sacrificed for yaw actuation.
+ * If true, default behavior. Z-thrust is reduced to make room yaw actuation
+ * 	when saturated.
  *
  * @boolean
  * @group Mixer Output
