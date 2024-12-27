2b6e594ab574a3a9c013c213976dc66f067a8322
fbaklanov <fedor.baklanov@euler-nav.com>
2024-12-27 20:37:22 +0100

    Process data buffer (3)

--- src/drivers/ins/eulernav_bahrs/eulernav_driver.cpp
+++ src/drivers/ins/eulernav_bahrs/eulernav_driver.cpp
@@ -1,6 +1,5 @@
 #include "eulernav_driver.h"
 #include <px4_platform_common/getopt.h>
-#include "CSerialProtocol.h"
 
 EulerNavDriver::EulerNavDriver(const char* device_name)
 	: _serial_port{device_name, 115200, ByteSize::EightBits, Parity::None, StopBits::One, FlowControl::Disabled}
@@ -123,6 +122,7 @@ void EulerNavDriver::run()
 void EulerNavDriver::processDataBuffer()
 {
 	static_assert(MIN_MESSAGE_LENGTH >= (sizeof(CSerialProtocol::SMessageHeader) + sizeof(CSerialProtocol::CrcType_t)));
+	using EMessageIds = CSerialProtocol::EMessageIds;
 
 	// We start from the first element and search for a message header. We remove
 	// bytes from the buffer until a header is found. Then we parse the found message
@@ -164,14 +164,47 @@ void EulerNavDriver::processDataBuffer()
 
 		if (false == error)
 		{
-			switch (static_cast<CSerialProtocol::EMessageIds>(message_code))
+			const auto message_id{static_cast<EMessageIds>(message_code)};
+			const auto message_length{getMessageLength(message_id)};
+
+			if (message_length > 0)
 			{
-			case CSerialProtocol::EMessageIds::eInertialData:
-				break;
-			case CSerialProtocol::EMessageIds::eNavigationData:
-				break;
-			default:
-				break;
+				const auto bytes_to_retrieve{message_length - sizeof(CSerialProtocol::SMessageHeader)};
+
+				if ((bytes_to_retrieve > 0) &&
+					(_data_buffer.space_used() >= bytes_to_retrieve) &&
+					(bytes_to_retrieve + sizeof(CSerialProtocol::SMessageHeader)) <= _message_storage.capacity())
+				{
+					uint8_t* bytes = reinterpret_cast<uint8_t*>(_message_storage.begin());
+					bytes[0] = CSerialProtocol::uMarker1_;
+					bytes[1] = CSerialProtocol::uMarker2_;
+					bytes[2] = reinterpret_cast<uint8_t*>(&protocol_ver)[0];
+					bytes[3] = reinterpret_cast<uint8_t*>(&protocol_ver)[1];
+					bytes[4] = message_code;
+
+					if (bytes_to_retrieve != _data_buffer.pop_front(bytes + 5, bytes_to_retrieve))
+					{
+						error = true;
+					}
+
+					if (false == error)
+					{
+						uint32_t message_length_in_words{message_length / sizeof(uint32_t)};
+						const uint32_t actual_crc{crc32(_message_storage.begin(), message_length_in_words - 1)};
+						const uint32_t expected_crc = _message_storage[message_length_in_words - 1];
+
+						if (expected_crc != actual_crc)
+						{
+							error = true;
+						}
+
+					}
+
+					if (false == error)
+					{
+						// decode message
+					}
+				}
 			}
 		}
 	}
@@ -200,4 +233,47 @@ bool EulerNavDriver::retrieveProtocolVersionAndMessageType(Ringbuffer& buffer, u
 	}
 
 	return status;
+}
+
+int EulerNavDriver::getMessageLength(CSerialProtocol::EMessageIds messsage_id)
+{
+	int message_length{-1};
+
+	switch (messsage_id)
+	{
+	case CSerialProtocol::EMessageIds::eInertialData:
+		message_length = sizeof(CSerialProtocol::SInertialDataMessage);
+		break;
+	case CSerialProtocol::EMessageIds::eNavigationData:
+		message_length = sizeof(CSerialProtocol::SNavigationDataMessage);
+		break;
+	default:
+		break;
+	}
+
+	return message_length;
+}
+
+uint32_t EulerNavDriver::crc32(const uint32_t* buffer, size_t length)
+{
+  uint32_t crc = 0xFFFFFFFF;
+
+  for (size_t i = 0; i < length; ++i)
+  {
+    crc = crc ^ buffer[i];
+
+    for (uint8_t j = 0; j < 32; j++)
+    {
+      if (crc & 0x80000000)
+      {
+        crc = (crc << 1) ^ 0x04C11DB7;
+      }
+      else
+      {
+        crc = (crc << 1);
+      }
+    }
+  }
+
+  return crc;
 }
