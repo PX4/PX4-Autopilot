82f0b22e086dbe7aa6c430c50e5c2d25b4c1ab89
Balduin <balduin@auterion.com>
2025-01-06 16:17:28 +0100

    sign errors after all

--- src/modules/simulation/simulator_sih/sih.cpp
+++ src/modules/simulation/simulator_sih/sih.cpp
@@ -357,10 +357,10 @@ void Sih::generate_fw_aerodynamics(const float roll_cmd, const float pitch_cmd, 
 	const Vector3f v_B = _q_E.rotateVectorInverse(_v_E);
 	const float &alt = _lla.altitude();
 
-	_wing_l.update_aero(v_B, _w_B, alt, -roll_cmd * FLAP_MAX);
-	_wing_r.update_aero(v_B, _w_B, alt, roll_cmd * FLAP_MAX);
+	_wing_l.update_aero(v_B, _w_B, alt, roll_cmd * FLAP_MAX);
+	_wing_r.update_aero(v_B, _w_B, alt, -roll_cmd * FLAP_MAX);
 
-	_tailplane.update_aero(v_B, _w_B, alt, -pitch_cmd * FLAP_MAX, _T_MAX * thrust);
+	_tailplane.update_aero(v_B, _w_B, alt, pitch_cmd * FLAP_MAX, _T_MAX * thrust);
 	_fin.update_aero(v_B, _w_B, alt, yaw_cmd * FLAP_MAX, _T_MAX * thrust);
 	_fuselage.update_aero(v_B, _w_B, alt);
 
