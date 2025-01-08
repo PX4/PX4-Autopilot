d8c9ffeea4a214d86a4efb68e14b68f72e82b8f9
RomanBapst <bapstroman@gmail.com>
2025-01-08 15:34:43 +0300

    added messages for lateral and longitudinal control setpoints and corresponding limits
    Signed-off-by: RomanBapst <bapstroman@gmail.com>

--- src/modules/logger/logged_topics.cpp
+++ src/modules/logger/logged_topics.cpp
@@ -147,6 +147,10 @@ void LoggedTopics::add_default_topics()
 	add_topic("vehicle_status");
 	add_optional_topic("vtol_vehicle_status", 200);
 	add_topic("wind", 1000);
+	add_topic("fw_lateral_control_setpoint");
+	add_topic("fw_lateral_control_status");
+	add_topic("fw_longitudinal_control_setpoint");
+	add_topic("fw_longitudinal_control_status");
 
 	// multi topics
 	add_optional_topic_multi("actuator_outputs", 100, 3);
