ff07c1595445cad5d3835c70de0521bcf4d6ed61
Silvan Fuhrer <silvan@auterion.com>
2025-02-14 10:35:27 +0100

    msg_translation: Add vehicle_status_v1 translation
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>

--- msg/translation_node/translations/all_translations.h
+++ msg/translation_node/translations/all_translations.h
@@ -9,3 +9,5 @@
 //#include "example_translation_direct_v1.h"
 //#include "example_translation_multi_v2.h"
 //#include "example_translation_service_v1.h"
+
+#include "translation_vehicle_status_v1.h"
