3c129aefa15edc9fcdea3fdeaa5cbce9c5abdab3
Silvan Fuhrer <silvan@auterion.com>
2025-02-18 17:15:32 +0100

    Add IC engine control module (#24055)
    Internal combustion engine control module.
    New actuator functions and RPM based start/restart logic.
    Not enabled by default. 
    
    ---------
    
    Signed-off-by: Silvan Fuhrer <silvan@auterion.com>
    Co-authored-by: Pernilla <pernilla@auterion.com>

--- src/modules/logger/logged_topics.cpp
+++ src/modules/logger/logged_topics.cpp
@@ -79,6 +79,7 @@ void LoggedTopics::add_default_topics()
 	add_topic("home_position");
 	add_topic("hover_thrust_estimate", 100);
 	add_topic("input_rc", 500);
+	add_optional_topic("internal_combustion_engine_control", 10);
 	add_optional_topic("internal_combustion_engine_status", 10);
 	add_optional_topic("iridiumsbd_status", 1000);
 	add_optional_topic("irlock_report", 1000);
