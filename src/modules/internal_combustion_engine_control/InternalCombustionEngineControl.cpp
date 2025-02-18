b4cb27bbb1b380e0ccff3dd2ed7a8b9a76182b99
Hamish Willee <hamishwillee@gmail.com>
2025-02-19 10:39:40 +1100

    Update InternalCombustionEngineControl.cpp

--- src/modules/internal_combustion_engine_control/InternalCombustionEngineControl.cpp
+++ src/modules/internal_combustion_engine_control/InternalCombustionEngineControl.cpp
@@ -358,10 +358,6 @@ int InternalCombustionEngineControl::print_usage(const char *reason)
 		
 The module controls internal combustion engine (ICE) features including:
 ignition (on/off), throttle and choke level, starter engine delay, and user request.
-The module publishes [InternalCombustionEngineControl.msg](../msg_docs/InternalCombustionEngineControl.md).
-		
-The architecture is as shown below:
-![Architecture](../../assets/diagrams/ice_control_diagram.png)
 
 ### Enabling
 
@@ -374,15 +370,23 @@ CONFIG_DRIVERS_RPM_CAPTURE=y
 ```
 
 Additionally, to enable the module:
+
 - Set [ICE_EN](../advanced_config/parameter_reference.md#ICE_EN)
-to true and adjust the other module parameters ICE_ according to your needs.
+to true and adjust the other `ICE_` module parameters according to your needs.
 - Set [RPM_CAP_ENABLE](../advanced_config/parameter_reference.md#RPM_CAP_ENABLE) to true.
+
+The module outputs control signals for ignition, throttle, and choke,
+and takes inputs from an RPM sensor.
+These must be mapped to AUX outputs/inputs in the [Actuator configuration](../config/actuators.md),
+similar to the setup shown below.
+
+![Actuator setup for ICE](../../assets/hardware/ice/ice_actuator_setup.png)
 
 ### Implementation
 
 The ICE is implemented with a (4) state machine:
 
-![Architecture](../../assets/diagrams/ice_control_state_machine.png)
+![Architecture](../../assets/hardware/ice/ice_control_state_machine.png)
 
 The state machine:
 		
@@ -390,6 +394,12 @@ The state machine:
 - Allows for user inputs from:
   - AUX{N}
   - Arming state in [VehicleStatus.msg](../msg_docs/VehicleStatus.md)
+
+The module publishes [InternalCombustionEngineControl.msg](../msg_docs/InternalCombustionEngineControl.md).
+		
+The architecture is as shown below:
+
+![Architecture](../../assets/hardware/ice/ice_control_diagram.png)
 		
 <a id="internal_combustion_engine_control_usage"></a>
 )DESCR_STR");
