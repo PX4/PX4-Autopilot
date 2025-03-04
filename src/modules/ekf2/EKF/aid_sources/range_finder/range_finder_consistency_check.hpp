adad0282bcd8d87aeb2874f2bb7721ce62915ccf
Marco Hauswirth <marco.hauswirth@auterion.com>
2025-03-04 09:46:26 +0100

    change consistency to only be valid if running for a second

--- src/modules/ekf2/EKF/aid_sources/range_finder/range_finder_consistency_check.hpp
+++ src/modules/ekf2/EKF/aid_sources/range_finder/range_finder_consistency_check.hpp
@@ -58,8 +58,8 @@ public:
 	float getTestRatioLpf() const { return _initialized ? _test_ratio_lpf.getState() : 0.f; }
 	float getInnov() const { return _initialized ? _innov : 0.f; }
 	float getInnovVar() const { return _initialized ? _innov_var : 0.f; }
-	bool isKinematicallyConsistent() const { return _state == KinematicState::CONSISTENT; }
-	bool isNotKinematicallyInconsistent() const { return _state != KinematicState::INCONSISTENT; }
+	bool isKinematicallyConsistent() const { return _state == KinematicState::CONSISTENT && _t_since_first_sample > _t_to_init; }
+	bool isNotKinematicallyInconsistent() const { return _state != KinematicState::INCONSISTENT && _t_since_first_sample > _t_to_init; }
 	void setGate(const float gate) { _gate = gate; }
 	void run(float z, float z_var, float vz, float vz_var,
 		 float dist_bottom, float dist_bottom_var, uint64_t time_us);
@@ -93,6 +93,7 @@ private:
 	KinematicState _state{KinematicState::UNKNOWN};
 	float _t_since_first_sample{0.f};
 	uint8_t _last_posD_reset_count{0};
+	static constexpr float _t_to_init{1.f};
 };
 
 namespace RangeFilter
