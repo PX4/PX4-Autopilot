c9efb84a21d17e38cb2b5d0dd48ca38ad42fb292
Ramon Roche <mrpollo@gmail.com>
2025-03-04 08:08:10 -0800

    docker: update docker entry

--- Tools/setup/docker-entrypoint.sh
+++ Tools/setup/docker-entrypoint.sh
@@ -2,26 +2,26 @@
 
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
-FILE_DESCRIPTOR="${GREEN}[docker-entrypoint.sh]${NC}"
+SCRIPTID="${GREEN}[docker-entrypoint.sh]${NC}"
 
-echo -e "$FILE_DESCRIPTOR Starting"
+echo -e "$SCRIPTID Starting"
 
 # Start virtual X server in the background
 # - DISPLAY default is :99, set in dockerfile
 # - Users can override with `-e DISPLAY=` in `docker run` command to avoid
 #   running Xvfb and attach their screen
 if [[ -x "$(command -v Xvfb)" && "$DISPLAY" == ":99" ]]; then
-	echo -e "$FILE_DESCRIPTOR Starting Xvfb"
+	echo -e "$SCRIPTID Starting Xvfb"
 	Xvfb :99 -screen 0 1600x1200x24+32 &
 fi
 
 # Check if the ROS_DISTRO is passed and use it
 # to source the ROS environment
 if [ -n "${ROS_DISTRO}" ]; then
-	echo -e "$FILE_DESCRIPTOR ROS: ${ROS_DISTRO}"
+	echo -e "$SCRIPTID ROS: ${ROS_DISTRO}"
 	source "/opt/ros/$ROS_DISTRO/setup.bash"
 fi
 
-echo -e "$FILE_DESCRIPTOR ($( uname -m ))"
+echo -e "$SCRIPTID ($( uname -m ))"
 
 exec "$@"
