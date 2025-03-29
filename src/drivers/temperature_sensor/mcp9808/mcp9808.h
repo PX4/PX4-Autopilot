5483d901f91f202e52f2aa1fba00e06fbf6fff47
TedObrien <tedobr1998@gmail.com>
2025-03-29 11:07:30 +0000

    MPC9808: Run ScheduleOnInterval() at desired publishing rate and remove elapsed time check and timestamp_sample field from message as no longer needed.
    MCP9808: Replaced PX4_INFO with PX4_DEBUG
    
    MCP9808: Update date in headers
    
    MCP9808: Define functions before variables
    
    MCP9808: Increase logging interval for sensor_temp
    
    MCP9808: Removed extra space
    
    MCP9808: Remove this->

--- src/drivers/temperature_sensor/mcp9808/mcp9808.h
+++ src/drivers/temperature_sensor/mcp9808/mcp9808.h
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2024 PX4 Development Team. All rights reserved.
+ *   Copyright (c) 2025 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -66,11 +66,10 @@ protected:
 
 private:
 	uORB::PublicationMulti<sensor_temp_s> _sensor_temp_pub{ORB_ID(sensor_temp)};
-	perf_counter_t _cycle_perf;
-	perf_counter_t _comms_errors;
-	sensor_temp_s _sensor_temp{};
 	int read_reg(uint8_t address, uint16_t &data);
 	int write_reg(uint8_t address, uint16_t value);
 	float read_temperature();
-	hrt_abstime measurement_time = 0;
+	sensor_temp_s _sensor_temp{};
+	perf_counter_t _cycle_perf;
+	perf_counter_t _comms_errors;
 };
--- src/modules/logger/logged_topics.cpp
+++ src/modules/logger/logged_topics.cpp
@@ -123,7 +123,7 @@ void LoggedTopics::add_default_topics()
 	add_optional_topic("sensor_gyro_fft", 50);
 	add_topic("sensor_selection");
 	add_topic("sensors_status_imu", 200);
-	add_optional_topic("sensor_temp", 10);
+	add_optional_topic("sensor_temp", 100);
 	add_optional_topic("spoilers_setpoint", 1000);
 	add_topic("system_power", 500);
 	add_optional_topic("takeoff_status", 1000);
