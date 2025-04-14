63a73d30381d400eefd5b8d4902cc8b0eec21bb9
Dawid Rudy <dawid@auterion.com>
2025-04-14 11:09:33 +0200

    fix: Rescaling cases order

--- src/modules/mc_att_control/mc_att_control_main.cpp
+++ src/modules/mc_att_control/mc_att_control_main.cpp
@@ -112,15 +112,15 @@ MulticopterAttitudeControl::throttle_curve(float throttle_stick_input)
 
 	// throttle_stick_input is in range [-1, 1]
 	switch (_param_mpc_thr_curve.get()) {
-	case 1: // rescale to HTE value
+	case 1: // no rescaling to hover throttle
+		thrust = math::interpolate(throttle_stick_input, -1.f, 1.f,
+					   _manual_throttle_minimum.getState(), _param_mpc_thr_max.get());
+		break;
+
+	case 2: // rescale to HTE value
 		thrust = math::interpolateNXY(throttle_stick_input,
 		{-1.f, 0.f, 1.f},
 		{_manual_throttle_minimum.getState(), _hover_thrust_slew_rate.getState(), _param_mpc_thr_max.get()});
-		break;
-
-	case 2: // no rescaling to hover throttle
-		thrust = math::interpolate(throttle_stick_input, -1.f, 1.f,
-					   _manual_throttle_minimum.getState(), _param_mpc_thr_max.get());
 		break;
 
 	default: // 0 or other: rescale to hover throttle at 0 stick input
