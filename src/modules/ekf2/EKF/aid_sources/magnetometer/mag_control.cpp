8b2b19e7da0d8df15e756fdc4a0c58861e7c77ad
bresch <brescianimathieu@gmail.com>
2025-04-14 14:29:25 +0200

    ekf2-mag: never skip post-takeoff mag yaw reset

--- src/modules/ekf2/EKF/aid_sources/magnetometer/mag_control.cpp
+++ src/modules/ekf2/EKF/aid_sources/magnetometer/mag_control.cpp
@@ -210,9 +210,11 @@ void Ekf::controlMagFusion(const imuSample &imu_sample)
 
 			if (continuing_conditions_passing && _control_status.flags.yaw_align) {
 
-				if (checkHaglYawResetReq() || (wmm_updated && no_ne_aiding_or_not_moving)) {
+				if ((checkHaglYawResetReq() && (_control_status.flags.mag_hdg || _control_status.flags.mag_3D))
+				    || (wmm_updated && no_ne_aiding_or_not_moving)) {
 					ECL_INFO("reset to %s", AID_SRC_NAME);
-					resetMagStates(_mag_lpf.getState(), _control_status.flags.mag_hdg || _control_status.flags.mag_3D);
+					const bool reset_heading = _control_status.flags.mag_hdg || _control_status.flags.mag_3D;
+					resetMagStates(_mag_lpf.getState(), reset_heading);
 					aid_src.time_last_fuse = imu_sample.time_us;
 
 				} else {
@@ -446,7 +448,7 @@ void Ekf::resetMagStates(const Vector3f &mag, bool reset_heading)
 	}
 
 	// record the start time for the magnetic field alignment
-	if (_control_status.flags.in_air) {
+	if (_control_status.flags.in_air && reset_heading) {
 		_control_status.flags.mag_aligned_in_flight = true;
 		_flt_mag_align_start_time = _time_delayed_us;
 	}
