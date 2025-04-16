2cbf10e1ed5ab37a9739f8526c367f7b80dfe9f2
Matthias Grob <maetugr@gmail.com>
2025-04-16 11:21:09 +0200

    ControlAllocationSequentialDesaturationTest: fix include instead of duplicating definitions

--- src/lib/control_allocation/control_allocation/ControlAllocationSequentialDesaturationTest.cpp
+++ src/lib/control_allocation/control_allocation/ControlAllocationSequentialDesaturationTest.cpp
@@ -40,7 +40,7 @@
 
 #include <gtest/gtest.h>
 #include <ControlAllocationSequentialDesaturation.hpp>
-#include <actuator_effectiveness/ActuatorEffectivenessRotors.hpp>
+#include <ActuatorEffectivenessRotors.hpp>
 
 using namespace matrix;
 
