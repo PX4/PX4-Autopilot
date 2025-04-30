39f47b47bad86c4ae4e6e1ff1c6541e772cd2c5b
Hamish Willee <hamishwillee@gmail.com>
2025-04-30 13:49:01 +1000

    Run make format

--- src/modules/mavlink/mavlink_receiver.cpp
+++ src/modules/mavlink/mavlink_receiver.cpp
@@ -606,7 +606,7 @@ void MavlinkReceiver::handle_message_command_both(mavlink_message_t *msg, const 
 			result = vehicle_command_ack_s::VEHICLE_CMD_RESULT_DENIED;
 			send_ack = true;
 		}
-		
+
 	} else if (cmd_mavlink.command == MAV_CMD_DO_SET_MODE) {
 		_cmd_pub.publish(vehicle_command);
 
