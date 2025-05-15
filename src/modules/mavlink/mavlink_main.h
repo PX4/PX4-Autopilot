a3902157fb081d0573813405789fbcf6958caf62
Matthias Grob <maetugr@gmail.com>
2025-05-15 18:50:54 +0200

    mavlink: adhere to parameter naming convention

--- src/modules/mavlink/mavlink_main.h
+++ src/modules/mavlink/mavlink_main.h
@@ -503,7 +503,7 @@ public:
 	bool hash_check_enabled() const { return _param_mav_hash_chk_en.get(); }
 	bool forward_heartbeats_enabled() const { return _param_mav_hb_forw_en.get(); }
 
-	bool failure_injection_enabled() const { return _param_sys_failure_injection_enabled.get(); }
+	bool failure_injection_enabled() const { return _param_sys_failure_en.get(); }
 
 	struct ping_statistics_s {
 		uint64_t last_ping_time;
@@ -668,7 +668,7 @@ private:
 		(ParamBool<px4::params::MAV_HB_FORW_EN>) _param_mav_hb_forw_en,
 		(ParamInt<px4::params::MAV_RADIO_TOUT>)      _param_mav_radio_timeout,
 		(ParamInt<px4::params::SYS_HITL>) _param_sys_hitl,
-		(ParamBool<px4::params::SYS_FAILURE_EN>) _param_sys_failure_injection_enabled
+		(ParamBool<px4::params::SYS_FAILURE_EN>) _param_sys_failure_en
 	)
 
 	perf_counter_t _loop_perf{perf_alloc(PC_ELAPSED, MODULE_NAME": tx run elapsed")};                      /**< loop performance counter */
