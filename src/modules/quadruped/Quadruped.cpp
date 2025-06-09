685a659ee41166dff7121b24abe4c99a63091b3b
KrishnakumarSanthanam <129371088+KrishnakumarSanthanam@users.noreply.github.com>
2025-06-09 18:58:06 +1000

    Fix code format

--- src/modules/quadruped/Quadruped.cpp
+++ src/modules/quadruped/Quadruped.cpp
@@ -9,35 +9,37 @@ using namespace time_literals;
 class Quadruped : public ModuleBase<Quadruped>, public ModuleParams, public px4::ScheduledWorkItem
 {
 public:
-        Quadruped() : ModuleParams(nullptr), ScheduledWorkItem(MODULE_NAME, px4::wq_configurations::nav_and_controllers)
-        {
-        }
+	Quadruped() : ModuleParams(nullptr), ScheduledWorkItem(MODULE_NAME, px4::wq_configurations::nav_and_controllers)
+	{
+	}
 
-        ~Quadruped() override { }
+	~Quadruped() override { }
 
-        bool init() {
-                ScheduleOnInterval(100_ms); // 10 Hz
-                return true;
-        }
+	bool init()
+	{
+		ScheduleOnInterval(100_ms); // 10 Hz
+		return true;
+	}
 
-        void Run() override {
-                if (should_exit()) {
-                        ScheduleClear();
-                        exit_and_cleanup();
-                        return;
-                }
+	void Run() override
+	{
+		if (should_exit()) {
+			ScheduleClear();
+			exit_and_cleanup();
+			return;
+		}
 
-                quadruped_leg_command_s cmd{};
-                cmd.timestamp = hrt_absolute_time();
-                // currently send zeros
-                _cmd_pub.publish(cmd);
-        }
+		quadruped_leg_command_s cmd{};
+		cmd.timestamp = hrt_absolute_time();
+		// currently send zeros
+		_cmd_pub.publish(cmd);
+	}
 
 private:
-        uORB::Publication<quadruped_leg_command_s> _cmd_pub{ORB_ID(quadruped_leg_command)};
+	uORB::Publication<quadruped_leg_command_s> _cmd_pub{ORB_ID(quadruped_leg_command)};
 };
 
 int Quadruped_main(int argc, char *argv[])
 {
-        return Quadruped::main(argc, argv);
+	return Quadruped::main(argc, argv);
 }
