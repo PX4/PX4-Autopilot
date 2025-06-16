a18d91e23dd8686fdb971e55f723f53cd43ad3e4
chfriedrich98 <chfriedrich@student.ethz.ch>
2025-06-16 09:07:09 +0200

    ackermann: rename DriveModes to AckermannDriveModes

--- src/modules/rover_ackermann/AckermannDriveModes/AckermannOffboardMode/AckermannOffboardMode.hpp
+++ src/modules/rover_ackermann/AckermannDriveModes/AckermannOffboardMode/AckermannOffboardMode.hpp
@@ -37,60 +37,36 @@
 #include <px4_platform_common/module_params.h>
 
 // Libraries
-#include <lib/rover_control/RoverControl.hpp>
 #include <math.h>
+#include <matrix/matrix/math.hpp>
 
 // uORB includes
 #include <uORB/Subscription.hpp>
 #include <uORB/Publication.hpp>
-#include <uORB/topics/manual_control_setpoint.h>
-#include <uORB/topics/vehicle_status.h>
-#include <uORB/topics/vehicle_attitude.h>
-#include <uORB/topics/vehicle_local_position.h>
-#include <uORB/topics/rover_throttle_setpoint.h>
-#include <uORB/topics/rover_steering_setpoint.h>
-#include <uORB/topics/rover_rate_setpoint.h>
-#include <uORB/topics/rover_attitude_setpoint.h>
 #include <uORB/topics/rover_velocity_setpoint.h>
 #include <uORB/topics/rover_position_setpoint.h>
+#include <uORB/topics/offboard_control_mode.h>
+#include <uORB/topics/trajectory_setpoint.h>
+
+using namespace matrix;
 
 /**
  * @brief Class for ackermann manual mode.
  */
-class ManualMode : public ModuleParams
+class AckermannOffboardMode : public ModuleParams
 {
 public:
 	/**
-	 * @brief Constructor for ManualMode.
+	 * @brief Constructor for AckermannOffboardMode.
 	 * @param parent The parent ModuleParams object.
 	 */
-	ManualMode(ModuleParams *parent);
-	~ManualMode() = default;
+	AckermannOffboardMode(ModuleParams *parent);
+	~AckermannOffboardMode() = default;
 
 	/**
-	 * @brief Publish roverThrottleSetpoint and roverSteeringSetpoint from manualControlSetpoint.
+	 * @brief Generate and publish roverSetpoints from trajectorySetpoint.
 	 */
-	void manual();
-
-	/**
-	 * @brief Generate and publish roverThrottleSetpoint and RoverRateSetpoint from manualControlSetpoint.
-	 */
-	void acro();
-
-	/**
-	 * @brief Generate and publish roverThrottleSetpoint and RoverAttitudeSetpoint from manualControlSetpoint.
-	 */
-	void stab();
-
-	/**
-	 * @brief Generate and publish roverVelocitySetpoint from manualControlSetpoint.
-	 */
-	void position();
-
-	/**
-	 * @brief Reset manual mode variables.
-	 */
-	void reset();
+	void offboardControl();
 
 protected:
 	/**
@@ -100,33 +76,10 @@ protected:
 
 private:
 	// uORB subscriptions
-	uORB::Subscription _vehicle_attitude_sub{ORB_ID(vehicle_attitude)};
-	uORB::Subscription _manual_control_setpoint_sub{ORB_ID(manual_control_setpoint)};
-	uORB::Subscription _vehicle_local_position_sub{ORB_ID(vehicle_local_position)};
+	uORB::Subscription _trajectory_setpoint_sub{ORB_ID(trajectory_setpoint)};
+	uORB::Subscription _offboard_control_mode_sub{ORB_ID(offboard_control_mode)};
 
 	// uORB publications
-	uORB::Publication<rover_throttle_setpoint_s> _rover_throttle_setpoint_pub{ORB_ID(rover_throttle_setpoint)};
-	uORB::Publication<rover_steering_setpoint_s> _rover_steering_setpoint_pub{ORB_ID(rover_steering_setpoint)};
-	uORB::Publication<rover_rate_setpoint_s>     _rover_rate_setpoint_pub{ORB_ID(rover_rate_setpoint)};
-	uORB::Publication<rover_attitude_setpoint_s> _rover_attitude_setpoint_pub{ORB_ID(rover_attitude_setpoint)};
 	uORB::Publication<rover_velocity_setpoint_s> _rover_velocity_setpoint_pub{ORB_ID(rover_velocity_setpoint)};
 	uORB::Publication<rover_position_setpoint_s> _rover_position_setpoint_pub{ORB_ID(rover_position_setpoint)};
-
-	// Variables
-	MapProjection _global_ned_proj_ref{}; // Transform global to NED coordinates
-	Quatf _vehicle_attitude_quaternion{};
-	Vector2f _pos_ctl_course_direction{NAN, NAN};
-	Vector2f _pos_ctl_start_position_ned{NAN, NAN};
-	Vector2f _curr_pos_ned{NAN, NAN};
-	float _stab_yaw_setpoint{NAN};
-	float _vehicle_yaw{NAN};
-	float _max_yaw_rate{NAN};
-
-	DEFINE_PARAMETERS(
-		(ParamFloat<px4::params::RO_YAW_RATE_LIM>)  _param_ro_yaw_rate_limit,
-		(ParamFloat<px4::params::RO_YAW_P>)         _param_ro_yaw_p,
-		(ParamFloat<px4::params::RO_YAW_STICK_DZ>)  _param_ro_yaw_stick_dz,
-		(ParamFloat<px4::params::PP_LOOKAHD_MAX>)   _param_pp_lookahd_max,
-		(ParamFloat<px4::params::RO_SPEED_LIM>)     _param_ro_speed_limit
-	)
 };
