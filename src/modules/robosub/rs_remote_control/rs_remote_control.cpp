a8f6fead905e5bd309f30747bee0af636dd1b381
MightyPiggie <degamendekoe@gmail.com>
2025-06-17 08:59:14 +0200

    Changed settings for the dvl fusion

--- src/modules/robosub/rs_remote_control/rs_remote_control.cpp
+++ src/modules/robosub/rs_remote_control/rs_remote_control.cpp
@@ -244,7 +244,7 @@ _loop_perf(perf_alloc(PC_ELAPSED, MODULE_NAME": cycle"))
 				{
 					range = 1.0f;
 				}
-				// range = 0.2f;
+				range = 1.0f;
 
 				// Normalize the rc data to a value between -1 and 1
 				normalized[0] = (rc_data.values[1] - 1500) / 400.0f;
@@ -260,11 +260,11 @@ _loop_perf(perf_alloc(PC_ELAPSED, MODULE_NAME": cycle"))
 
 
 
-				robosub_motor_control.actuator_test(MOTOR_FORWARDS1, 	normalized[0], 0, false);
-				robosub_motor_control.actuator_test(MOTOR_FORWARDS2, 	normalized[0], 0, false);
+				robosub_motor_control.actuator_test(MOTOR_FORWARDS1, 	normalized[0]/2, 0, false);
+				robosub_motor_control.actuator_test(MOTOR_FORWARDS2, 	normalized[0]/2, 0, false);
 				robosub_motor_control.actuator_test(MOTOR_UP1, 		-normalized[1], 0, false);
-				robosub_motor_control.actuator_test(MOTOR_UP2, 		(normalized[1] * 0.5f), 0, false);
-				robosub_motor_control.actuator_test(MOTOR_UP3, 		(-normalized[1] * 0.5f), 0, false);
+				robosub_motor_control.actuator_test(MOTOR_UP2, 		(normalized[1]), 0, false);
+				robosub_motor_control.actuator_test(MOTOR_UP3, 		(-normalized[1]), 0, false);
 				if(normalized[2] > 0.1f || normalized[2] < -0.1f)
 				{
 					robosub_motor_control.actuator_test(MOTOR_SIDE1, -normalized[2], 0, false);
