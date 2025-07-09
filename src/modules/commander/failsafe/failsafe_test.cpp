8465ee92c38f99d7ef854723d0c95be0b68380a6
Beat Küng <beat-kueng@gmx.net>
2025-07-09 13:53:36 +0200

    commander: do not trigger obsolete failsafe when deactivating failsafe deferring
    Previously, when deferring was active and e.g. RC loss was triggered, and
    RC regained, the action was not cleared, as the RC loss action only clears
    on mode switch/disarm (when set to RTL for example).
    When deferring was then disabled, the RC loss failsafe would still trigger.
    
    This changes the behavior to immediately remove those actions when
    deferring is active.
    
    It also ensures to reset the Hold delay when deferring is disabled and no
    failsafe is being deferred.

--- src/modules/commander/failsafe/failsafe_test.cpp
+++ src/modules/commander/failsafe/failsafe_test.cpp
@@ -370,6 +370,46 @@ TEST_F(FailsafeTest, defer)
 	ASSERT_FALSE(failsafe.failsafeDeferred());
 }
 
+TEST_F(FailsafeTest, defer_and_clear)
+{
+	FailsafeTester failsafe(nullptr);
+
+	failsafe_flags_s failsafe_flags{};
+	FailsafeBase::State state{};
+	state.armed = true;
+	state.user_intended_mode = vehicle_status_s::NAVIGATION_STATE_POSCTL;
+	state.vehicle_type = vehicle_status_s::VEHICLE_TYPE_ROTARY_WING;
+	hrt_abstime time = 3847124342;
+
+	uint8_t updated_user_intented_mode = failsafe.update(time, state, false, false, failsafe_flags);
+
+	failsafe.deferFailsafes(true, -1);
+	ASSERT_TRUE(failsafe.getDeferFailsafes());
+	ASSERT_FALSE(failsafe.failsafeDeferred());
+	// Manual control lost -> deferred
+	time += 10_ms;
+	failsafe_flags.manual_control_signal_lost = true;
+	updated_user_intented_mode = failsafe.update(time, state, false, false, failsafe_flags);
+	ASSERT_EQ(failsafe.selectedAction(), FailsafeBase::Action::None);
+	ASSERT_TRUE(failsafe.failsafeDeferred());
+
+	// Clear flag (the failsafe action only clears on mode switch, but we still expect it to clear as it's being deferred)
+	failsafe_flags.manual_control_signal_lost = false;
+	time += 5_s;
+	updated_user_intented_mode = failsafe.update(time, state, false, false, failsafe_flags);
+	ASSERT_EQ(failsafe.selectedAction(), FailsafeBase::Action::None);
+	ASSERT_FALSE(failsafe.failsafeDeferred());
+
+	// Wait a bit, don't defer anymore -> no failsafe triggered
+	time += 1_s;
+	failsafe.deferFailsafes(false, 0);
+	updated_user_intented_mode = failsafe.update(time, state, false, false, failsafe_flags);
+	ASSERT_EQ(updated_user_intented_mode, state.user_intended_mode);
+	ASSERT_EQ(failsafe.selectedAction(), FailsafeBase::Action::None);
+	ASSERT_FALSE(failsafe.getDeferFailsafes());
+	ASSERT_FALSE(failsafe.failsafeDeferred());
+}
+
 TEST_F(FailsafeTest, skip_failsafe)
 {
 	FailsafeTester failsafe(nullptr);
