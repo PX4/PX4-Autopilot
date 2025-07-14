27ffd68b78db1df7adc49ca8dc26e1e952c423cf
vidmantas zemleris <vidma@kensu.io>
2025-07-14 23:11:24 +0300

    Changes after Nuttx upgrade

--- src/modules/load_mon/LoadMon.cpp
+++ src/modules/load_mon/LoadMon.cpp
@@ -278,16 +278,7 @@ void LoadMon::stack_usage()
 
 #if CONFIG_NFILE_DESCRIPTORS_PER_BLOCK > 0
 		unsigned int tcb_num_used_fds = 0; // number of used file descriptors
-		struct filelist *filelist = &system_load.tasks[_stack_task_index].tcb->group->tg_filelist;
-
-		for (int fdr = 0; fdr < filelist->fl_rows; fdr++) {
-			for (int fdc = 0; fdc < CONFIG_NFILE_DESCRIPTORS_PER_BLOCK; fdc++) {
-				if (filelist->fl_files[fdr][fdc].f_inode) {
-					++tcb_num_used_fds;
-				}
-			}
-		}
-
+		tcb_num_used_fds += (&system_load.tasks[_stack_task_index].tcb->group->tg_fdlist)->fl_rows;
 #endif // CONFIG_NFILE_DESCRIPTORS_PER_BLOCK
 	}
 
