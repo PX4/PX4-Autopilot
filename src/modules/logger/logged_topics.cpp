a5fa2f5b2868db59af70f7f2f50cd28bf71965bb
Rohan Inamdar <145878828+rohan908@users.noreply.github.com>
2025-07-15 11:34:44 -0400

    removed all edits related to urob topics for enabling indi and actuator effectivness publication

--- src/modules/logger/logged_topics.cpp
+++ src/modules/logger/logged_topics.cpp
@@ -214,7 +214,6 @@ void LoggedTopics::add_default_topics()
 	add_topic("actuator_servos", 100);
 	add_topic_multi("vehicle_thrust_setpoint", 20, 2);
 	add_topic_multi("vehicle_torque_setpoint", 20, 2);
-	add_topic_multi("actuator_effectiveness_matrix", 20, 2);
 
 	// SYS_HITL: default ground truth logging for simulation
 	int32_t sys_hitl = 0;
