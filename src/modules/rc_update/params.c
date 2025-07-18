e3d8625e7f06fd1ca5b32aa9b156e4b5241d6d8e
Matthias Grob <maetugr@gmail.com>
2025-07-18 19:36:51 +0200

    Add RC termination switch

--- src/modules/rc_update/params.c
+++ src/modules/rc_update/params.c
@@ -1442,6 +1442,34 @@ PARAM_DEFINE_INT32(RC_MAP_OFFB_SW, 0);
 PARAM_DEFINE_INT32(RC_MAP_KILL_SW, 0);
 
 /**
+ * Termination switch channel
+ *
+ * @min 0
+ * @max 18
+ * @group Radio Switches
+ * @value 0 Unassigned
+ * @value 1 Channel 1
+ * @value 2 Channel 2
+ * @value 3 Channel 3
+ * @value 4 Channel 4
+ * @value 5 Channel 5
+ * @value 6 Channel 6
+ * @value 7 Channel 7
+ * @value 8 Channel 8
+ * @value 9 Channel 9
+ * @value 10 Channel 10
+ * @value 11 Channel 11
+ * @value 12 Channel 12
+ * @value 13 Channel 13
+ * @value 14 Channel 14
+ * @value 15 Channel 15
+ * @value 16 Channel 16
+ * @value 17 Channel 17
+ * @value 18 Channel 18
+ */
+PARAM_DEFINE_INT32(RC_MAP_TERM_SW, 0);
+
+/**
  * Arm switch channel.
  *
  * Use it to arm/disarm via switch instead of default throttle stick. If this is
