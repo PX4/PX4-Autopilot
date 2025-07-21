6e7888b29bcce25906a2797f4748c15a3d4a69df
bresch <brescianimathieu@gmail.com>
2025-07-21 14:19:11 +0200

    AGP: format enum names

--- src/modules/ekf2/EKF/aid_sources/aux_global_position/aux_global_position.cpp
+++ src/modules/ekf2/EKF/aid_sources/aux_global_position/aux_global_position.cpp
@@ -69,7 +69,7 @@ void AuxGlobalPosition::update(Ekf &ekf, const estimator::imuSample &imu_delayed
 
 	if (_aux_global_position_buffer.pop_first_older_than(imu_delayed.time_us, &sample)) {
 
-		if (!(_param_ekf2_agp_ctrl.get() & static_cast<int32_t>(Ctrl::HPOS))) {
+		if (!(_param_ekf2_agp_ctrl.get() & static_cast<int32_t>(Ctrl::kHPos))) {
 			return;
 		}
 
@@ -96,12 +96,12 @@ void AuxGlobalPosition::update(Ekf &ekf, const estimator::imuSample &imu_delayed
 						   && ekf.global_origin_valid();
 
 		switch (_state) {
-		case State::stopped:
+		case State::kStopped:
 
 		/* FALLTHROUGH */
-		case State::starting:
+		case State::kStarting:
 			if (starting_conditions) {
-				_state = State::starting;
+				_state = State::kStarting;
 
 				if (ekf.global_origin_valid()) {
 					const bool fused = ekf.fuseHorizontalPosition(aid_src);
@@ -116,7 +116,7 @@ void AuxGlobalPosition::update(Ekf &ekf, const estimator::imuSample &imu_delayed
 					if (fused || reset) {
 						ekf.enableControlStatusAuxGpos();
 						_reset_counters.lat_lon = sample.lat_lon_reset_counter;
-						_state = State::active;
+						_state = State::kActive;
 					}
 
 				} else {
@@ -126,14 +126,14 @@ void AuxGlobalPosition::update(Ekf &ekf, const estimator::imuSample &imu_delayed
 						ekf.resetAidSourceStatusZeroInnovation(aid_src);
 						ekf.enableControlStatusAuxGpos();
 						_reset_counters.lat_lon = sample.lat_lon_reset_counter;
-						_state = State::active;
+						_state = State::kActive;
 					}
 				}
 			}
 
 			break;
 
-		case State::active:
+		case State::kActive:
 			if (continuing_conditions) {
 				ekf.fuseHorizontalPosition(aid_src);
 
@@ -149,13 +149,13 @@ void AuxGlobalPosition::update(Ekf &ekf, const estimator::imuSample &imu_delayed
 
 					} else {
 						ekf.disableControlStatusAuxGpos();
-						_state = State::stopped;
+						_state = State::kStopped;
 					}
 				}
 
 			} else {
 				ekf.disableControlStatusAuxGpos();
-				_state = State::stopped;
+				_state = State::kStopped;
 			}
 
 			break;
@@ -171,9 +171,9 @@ void AuxGlobalPosition::update(Ekf &ekf, const estimator::imuSample &imu_delayed
 		_test_ratio_filtered = math::max(fabsf(aid_src.test_ratio_filtered[0]), fabsf(aid_src.test_ratio_filtered[1]));
 #endif // MODULE_NAME
 
-	} else if ((_state != State::stopped) && isTimedOut(_time_last_buffer_push, imu_delayed.time_us, (uint64_t)5e6)) {
+	} else if ((_state != State::kStopped) && isTimedOut(_time_last_buffer_push, imu_delayed.time_us, (uint64_t)5e6)) {
 		ekf.disableControlStatusAuxGpos();
-		_state = State::stopped;
+		_state = State::kStopped;
 		ECL_WARN("Aux global position data stopped");
 	}
 }
