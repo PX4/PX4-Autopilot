f4fb485b210dba173c2cb71c4530be7ed3a0bee9
Silvan Fuhrer <silvan@auterion.com>
2025-08-08 04:10:32 +0200

    AirspeedSelector: increase default of ASPD_WERR_THR
    The previous default of 0.55m/s was very restrictive, the
    ground-minus-wind airspeed estimate was barely ever valid
    with it.
    
    Signed-off-by: Silvan <silvan@auterion.com>

--- src/modules/airspeed_selector/airspeed_selector_params.c
+++ src/modules/airspeed_selector/airspeed_selector_params.c
@@ -228,18 +228,18 @@ PARAM_DEFINE_FLOAT(ASPD_FS_T_STOP, 1.f);
 PARAM_DEFINE_FLOAT(ASPD_FS_T_START, -1.f);
 
 /**
- * Horizontal wind uncertainty threshold for synthetic airspeed.
+ * Horizontal wind uncertainty threshold for valid ground-minus-wind
  *
- * The synthetic airspeed estimate (from groundspeed and heading) will be declared valid
+ * The airspeed alternative derived from groundspeed and heading will be declared valid
  * as soon and as long the horizontal wind uncertainty is below this value.
  *
  * @unit m/s
- * @min 0.001
+ * @min 0.01
  * @max 5
- * @decimal 3
+ * @decimal 2
  * @group Airspeed Validator
  */
-PARAM_DEFINE_FLOAT(ASPD_WERR_THR, 0.55f);
+PARAM_DEFINE_FLOAT(ASPD_WERR_THR, 2.f);
 
 /**
  * First principle airspeed check time window
