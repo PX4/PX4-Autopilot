4498be207459f784f6605f7860510124b5e405a7
Beat Küng <beat-kueng@gmx.net>
2025-08-19 15:55:04 +0200

    commander: add valid_registrations_mask to ArmingCheckRequest.msg
    This allows external modes to individually check if they are flagged as
    invalid/unresponsive.
    Previously this was done only based on whether or not ArmingCheckRequest
    was received, which does not work when multiple modes are running.

--- msg/translation_node/translations/all_translations.h
+++ msg/translation_node/translations/all_translations.h
@@ -8,6 +8,7 @@
 
 #include "translation_airspeed_validated_v1.h"
 #include "translation_arming_check_reply_v1.h"
+#include "translation_arming_check_request_v1.h"
 #include "translation_battery_status_v1.h"
 #include "translation_event_v1.h"
 #include "translation_home_position_v1.h"
