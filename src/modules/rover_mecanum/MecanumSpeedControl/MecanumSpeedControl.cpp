f858cea222814814d38df7dd6a92c9673e8cd58b
chfriedrich98 <chfriedrich@student.ethz.ch>
2025-08-22 11:29:48 +0200

    rover: constrain update steps

--- src/modules/rover_mecanum/MecanumSpeedControl/MecanumSpeedControl.cpp
+++ src/modules/rover_mecanum/MecanumSpeedControl/MecanumSpeedControl.cpp
@@ -65,7 +65,7 @@ void MecanumSpeedControl::updateSpeedControl()
 {
 	const hrt_abstime timestamp_prev = _timestamp;
 	_timestamp = hrt_absolute_time();
-	const float dt = math::constrain(_timestamp - timestamp_prev, 1_ms, 5000_ms) * 1e-6f;
+	const float dt = math::constrain(_timestamp - timestamp_prev, 1_ms, 10_ms) * 1e-6f;
 
 	updateSubscriptions();
 
