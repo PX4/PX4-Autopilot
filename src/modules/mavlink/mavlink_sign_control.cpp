1d696cba353e05b1aa894795b46b554703db3571
Yulian Oifa <yulian.oifa@mobius-software.com>
2025-08-23 14:25:05 +0300

    Fixing styles

--- src/modules/mavlink/mavlink_sign_control.cpp
+++ src/modules/mavlink/mavlink_sign_control.cpp
@@ -84,9 +84,11 @@ void MavlinkSignControl::start(int _instance_id, mavlink_status_t *_mavlink_stat
 
 			if (bytes_read == 32) {
 				bytes_read = ::read(_fd, &_mavlink_signing.timestamp, 8);
-				if(bytes_read == 8) {
-					if(_mavlink_signing.timestamp !=0 || !is_array_all_zeros(_mavlink_signing.secret_key, 32))
+
+				if (bytes_read == 8) {
+					if (_mavlink_signing.timestamp != 0 || !is_array_all_zeros(_mavlink_signing.secret_key, 32)) {
 						is_signing_initialized = true;
+					}
 				}
 			}
 
@@ -95,7 +97,7 @@ void MavlinkSignControl::start(int _instance_id, mavlink_status_t *_mavlink_stat
 	}
 
 	//lets reset it to nulls if it was not read properly
-	if(!is_signing_initialized) {
+	if (!is_signing_initialized) {
 		for (size_t i = 0; i < 32; ++i) {
 			_mavlink_signing.secret_key[i] = 0;
 		}
@@ -113,6 +115,19 @@ bool MavlinkSignControl::check_for_signing(const mavlink_message_t *msg)
 	if (msg->msgid == MAVLINK_MSG_ID_SETUP_SIGNING) {
 		mavlink_setup_signing_t setup_signing;
 		mavlink_msg_setup_signing_decode(msg, &setup_signing);
+
+		//setup signing provides new key , lets update it
+		//we update it only in case everything was stored properly
+		memcpy(_mavlink_signing.secret_key, setup_signing.secret_key, 32);
+		_mavlink_signing.timestamp = setup_signing.initial_timestamp;
+
+		if (setup_signing.initial_timestamp != 0 || !is_array_all_zeros(setup_signing.secret_key, 32)) {
+			is_signing_initialized = true;
+
+		} else {
+			is_signing_initialized = false;
+		}
+
 		int _fd = ::open(MAVLINK_SECRET_FILE, O_CREAT | O_WRONLY | O_TRUNC, PX4_O_MODE_600);
 
 		if (_fd == -1) {
@@ -125,17 +140,6 @@ bool MavlinkSignControl::check_for_signing(const mavlink_message_t *msg)
 
 			if (bytes_write == 32) {
 				bytes_write = ::write(_fd, &setup_signing.initial_timestamp, 8);
-				if(bytes_write == 8) {
-					//setup signing provides new key , lets update it
-					//we update it only in case everything was stored properly
-					memcpy(_mavlink_signing.secret_key, setup_signing.secret_key, 32);
-					_mavlink_signing.timestamp = setup_signing.initial_timestamp;
-
-					if(setup_signing.initial_timestamp !=0 || !is_array_all_zeros(setup_signing.secret_key, 32))
-						is_signing_initialized = true;
-					else
-						is_signing_initialized = false;
-				}
 			}
 
 			close(_fd);
@@ -155,6 +159,7 @@ void MavlinkSignControl::write_key_and_timestamp()
 		if (errno != ENOENT) {
 			PX4_ERR("failed opening mavlink secret key file for writing: %s (%i)", MAVLINK_SECRET_FILE, errno);
 		}
+
 	} else {
 		ssize_t bytes_write = ::write(_fd, _mavlink_signing.secret_key, 32);
 
@@ -169,8 +174,9 @@ void MavlinkSignControl::write_key_and_timestamp()
 bool MavlinkSignControl::accept_unsigned(int32_t sign_mode, bool is_usb_uart, uint32_t message_id)
 {
 	// if signing is not initilized properly or has all zeroes we will allow any message
-	if(!is_signing_initialized)
+	if (!is_signing_initialized) {
 		return true;
+	}
 
 	// Always accept a few select messages even if unsigned
 	for (unsigned i = 0; i < sizeof(unsigned_messages) / sizeof(unsigned_messages[0]); i++) {
@@ -196,11 +202,13 @@ bool MavlinkSignControl::accept_unsigned(int32_t sign_mode, bool is_usb_uart, ui
 	}
 }
 
-bool MavlinkSignControl::is_array_all_zeros(uint8_t arr[], size_t size) {
-    for (size_t i = 0; i < size; ++i) {
-        if (arr[i] != 0) {
-            return false;
-        }
-    }
-    return true;
+bool MavlinkSignControl::is_array_all_zeros(uint8_t arr[], size_t size)
+{
+	for (size_t i = 0; i < size; ++i) {
+		if (arr[i] != 0) {
+			return false;
+		}
+	}
+
+	return true;
 }
