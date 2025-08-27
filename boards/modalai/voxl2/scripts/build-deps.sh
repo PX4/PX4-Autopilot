3aaad4ff29b234653eaba49ec9b40777c8d5afc0
Eric Katzfey <eric.katzfey@modalai.com>
2025-08-27 09:26:22 -0700

    Added git submodule update for libfc-sensor-api

--- boards/modalai/voxl2/scripts/build-deps.sh
+++ boards/modalai/voxl2/scripts/build-deps.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+git submodule init boards/modalai/voxl2/libfc-sensor-api
+git submodule update boards/modalai/voxl2/libfc-sensor-api
+
 cd boards/modalai/voxl2/libfc-sensor-api
 rm -fR build
 mkdir build
