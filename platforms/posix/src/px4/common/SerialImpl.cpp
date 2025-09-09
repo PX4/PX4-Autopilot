799ef42fb83d376090d5312c9936702161300fca
Alexander Lerach <alexander@auterion.com>
2025-09-09 14:03:17 +0200

    serial: do not print error on EAGAIN

--- platforms/posix/src/px4/common/SerialImpl.cpp
+++ platforms/posix/src/px4/common/SerialImpl.cpp
@@ -337,12 +337,14 @@ ssize_t SerialImpl::write(const void *buffer, size_t buffer_size)
 	}
 
 	int written = ::write(_serial_fd, buffer, buffer_size);
-	::fsync(_serial_fd);
 
 	if (written < 0) {
-		PX4_ERR("%s write error %d", _port, written);
-
+		if (errno != EAGAIN) {
+			PX4_ERR("%s write error %d", _port, written);
+		}
 	}
+
+	::fsync(_serial_fd);
 
 	return written;
 }
