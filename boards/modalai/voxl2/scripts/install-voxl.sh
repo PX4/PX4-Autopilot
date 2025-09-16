e74c282fc3594020236c5306fd82d7e6c0ff098b
Eric Katzfey <eric.katzfey@modalai.com>
2025-09-16 08:44:39 -0700

    Added support for dps368 external barometer on apps processor

--- boards/modalai/voxl2/scripts/install-voxl.sh
+++ boards/modalai/voxl2/scripts/install-voxl.sh
@@ -129,6 +129,7 @@ adb shell "cd /usr/bin; /bin/ln -f -s px4 px4-imu_server"
 adb shell "cd /usr/bin; /bin/ln -f -s px4 px4-apps_sbus"
 adb shell "cd /usr/bin; /bin/ln -f -s px4 px4-vfc_logger"
 adb shell "cd /usr/bin; /bin/ln -f -s px4 px4-mavlink_odometry_bridge"
+adb shell "cd /usr/bin; /bin/ln -f -s px4 px4-dps310"
 
 # Make sure any required directories exist
 adb shell "/bin/mkdir -p /data/px4/param"
