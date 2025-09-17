8a64fca8d3b194e6056bb2b537a7926ae78e15fa
Claudio Chies <claudio.chies@auterion.com>
2025-09-17 08:47:21 +0200

    LOG: increase number of loggable batteries to 3

--- src/modules/logger/logged_topics.cpp
+++ src/modules/logger/logged_topics.cpp
@@ -51,7 +51,7 @@ void LoggedTopics::add_default_topics()
 	add_topic("airspeed", 1000);
 	add_optional_topic("airspeed_validated", 200);
 	add_optional_topic("autotune_attitude_control_status", 100);
-	add_topic_multi("battery_info", 5000, 2);
+	add_topic_multi("battery_info", 5000, 3);
 	add_optional_topic("camera_capture");
 	add_optional_topic("camera_trigger");
 	add_topic("cellular_status", 200);
