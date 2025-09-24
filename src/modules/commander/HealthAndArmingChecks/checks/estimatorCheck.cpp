9868f41f886ceed79d48075ba2bf516ef92845fe
Louis-max-H <louis-max.harter@protonmail.com>
2025-09-24 17:08:10 +0200

    Septentrio GNSS resilience reporting (#25012)
    Co-authored-by: Tory9 <vvpost05@gmail.com>

--- src/modules/commander/HealthAndArmingChecks/checks/estimatorCheck.cpp
+++ src/modules/commander/HealthAndArmingChecks/checks/estimatorCheck.cpp
@@ -594,15 +594,15 @@ void EstimatorChecks::checkEstimatorStatusFlags(const Context &context, Report &
 
 void EstimatorChecks::checkGps(const Context &context, Report &reporter, const sensor_gps_s &vehicle_gps_position) const
 {
-	if (vehicle_gps_position.jamming_state == sensor_gps_s::JAMMING_STATE_CRITICAL) {
+	if (vehicle_gps_position.jamming_state == sensor_gps_s::JAMMING_STATE_DETECTED) {
 		/* EVENT
 		 */
 		reporter.armingCheckFailure(NavModes::None, health_component_t::gps,
 					    events::ID("check_estimator_gps_jamming_critical"),
-					    events::Log::Critical, "GPS reports critical jamming state");
+					    events::Log::Critical, "GPS jamming detected");
 
 		if (reporter.mavlink_log_pub()) {
-			mavlink_log_critical(reporter.mavlink_log_pub(), "GPS reports critical jamming state\t");
+			mavlink_log_critical(reporter.mavlink_log_pub(), "GPS jamming detected\t");
 		}
 	}
 }
