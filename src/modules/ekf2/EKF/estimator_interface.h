a86a75afba8f77c0db094706c22bf83299983bea
Daniel Agar <daniel@agar.ca>
2025-09-25 17:29:01 -0400

    ekf2 log hacks

--- src/modules/ekf2/EKF/estimator_interface.h
+++ src/modules/ekf2/EKF/estimator_interface.h
@@ -44,9 +44,9 @@
 
 #if defined(MODULE_NAME)
 #include <px4_platform_common/log.h>
-# define ECL_INFO PX4_DEBUG
-# define ECL_WARN PX4_DEBUG
-# define ECL_ERR  PX4_DEBUG
+# define ECL_INFO PX4_INFO
+# define ECL_WARN PX4_WARN
+# define ECL_ERR  PX4_ERR
 # define ECL_DEBUG PX4_DEBUG
 #else
 # define ECL_INFO(X, ...) printf(X "\n", ##__VA_ARGS__)
