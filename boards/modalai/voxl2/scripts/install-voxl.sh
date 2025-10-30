04df1c26d46596a870ed0d1eefb23f612f238851
Eric Katzfey <eric.katzfey@modalai.com>
2025-10-30 15:21:18 -0700

    Add start of new sensor_baro_bridge module to start scripts

--- boards/modalai/voxl2/scripts/install-voxl.sh
+++ boards/modalai/voxl2/scripts/install-voxl.sh
@@ -130,6 +130,7 @@ adb shell "cd /usr/bin; /bin/ln -f -s px4 px4-apps_sbus"
 adb shell "cd /usr/bin; /bin/ln -f -s px4 px4-vfc_logger"
 adb shell "cd /usr/bin; /bin/ln -f -s px4 px4-mavlink_odometry_bridge"
 adb shell "cd /usr/bin; /bin/ln -f -s px4 px4-vehicle_air_data_bridge"
+adb shell "cd /usr/bin; /bin/ln -f -s px4 px4-sensor_baro_bridge"
 adb shell "cd /usr/bin; /bin/ln -f -s px4 px4-dps310"
 adb shell "cd /usr/bin; /bin/ln -f -s px4 px4-icp101xx"
 
