3bc1d9604ea1954f4ff7e0474da345a3ddab7d4d
Michael Schaeuble <michael@auterion.com>
2025-11-24 10:29:47 +0100

    mode: control auto set home from an external mode
    The mode executor can run land mode which updates the home position to the landing location. This
    can be not the desirable behavior and the home position should stay at the original location.
    
    A flag is added to the configuration overrides to control if the home position is updated or not.

--- msg/translation_node/translations/all_translations.h
+++ msg/translation_node/translations/all_translations.h
@@ -10,6 +10,7 @@
 #include "translation_arming_check_reply_v1.h"
 #include "translation_arming_check_request_v1.h"
 #include "translation_battery_status_v1.h"
+#include "translation_config_overrides_v1.h"
 #include "translation_event_v1.h"
 #include "translation_home_position_v1.h"
 #include "translation_vehicle_attitude_setpoint_v1.h"
