b7fa4b125a25bded330f9c9d46504d70d17d77f2
Phil-Engljaehringer <philipp.engljahringer@auterion.com>
2025-12-17 17:48:30 +0100

    drivers: MCP23009 & MCP23017 shared code base (#25924)
    * Implemented driver for MCP23017
    
    * fixed compatability with mcp23009. (naming of instantiated GPIO-Devices)
    
    * removed some comments
    
    * removed even more comments
    
    * commented out instatntiation of driver since it will not be used with v6x
    
    * removed last useless comments
    
    * re-activated gpio_mcp23009 driver, removed useless comments and empty lines
    
    * removed empty lines at the end of mcp23017.cpp
    
    * removed empty line
    
    * Implemented driver for MCP23017
    
    * fixed compatability with mcp23009. (naming of instantiated GPIO-Devices)
    
    * removed some comments
    
    * removed even more comments
    
    * commented out instatntiation of driver since it will not be used with v6x
    
    * removed last useless comments
    
    * re-activated gpio_mcp23009 driver, removed useless comments and empty lines
    
    * removed empty lines at the end of mcp23017.cpp
    
    * removed empty line
    
    * basic working implementation
    
    * first improved driver version with shared code base for MCP23009 & MCP23017 (built as state machine with sanity checks)
    
    * removed unused imports
    
    * changed module name from MCP to MCP230XX
    
    * removed debug print statements
    
    * adjusted auto start command of driver
    
    * removed comments
    
    * -added seperate main functions for both derivative modules (mcp23009 and mcp23017)
    
    * compile common functions as shared library in src/lib/drivers
    
    * fixed cleanup of modules
    
    * ->unclean working version with shared common library
    
    * used make format
    
    * working & cleaned version
    
    * -> Added CallbackHandler to be able to use multiple reading GPIO-expanders simultaneously
    -> Removed old mcap23009 calls and pin registrations
    -> Adjusted GpioIn.msg to contain MAX_INSTANCES
    
    * ->removed unused imports
    ->used make format
    
    * Fix: Re-enabled platform_mcp_gpio for accton-godwit, cuav, fmu-v5x
    
    * Fix: enabled platform_mcp_gpio in px4/fmu-v5x
    
    * added depency to fmu-v5x
    
    * Fix: removed new lines
    
    * Fix: fixed linker errors
    
    * removed unused linkage against mcp-library
    
    * Made mcp start calls consistent for fmu-v5x and fmu-v6x
    
    * moved logging of comm errors to read/write function directly
    
    * added perf_count for sanity_check
    
    * removed error message
    
    * ensured member variables follow naming convention
    
    * added retries to probe function
    
    * simplyfied state-logic
    
    * add break to terminate loop early
    
    * ensured clean state when register_gpios() fails
    
    * add registered-flag to pins
    
    * used path from top dir instead of relative path in CMakeLists
    
    * used constexpr to set parameters instead of calculating them at runtime
    
    * style: used make format
    
    * fix: corrected i2c_bus assignment
    
    * style: init input of callbackhandler to 0
    
    * fix: mark pin as registered if successful
    
    * style: made arguments const type
    
    ---------
    
    Co-authored-by: Alexander Lerach <alexander@auterion.com>

--- src/drivers/gpio/mcp23009/MCP23009.hpp
+++ src/drivers/gpio/mcp23009/MCP23009.hpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (c) 2020 PX4 Development Team. All rights reserved.
+ *   Copyright (C) 2025 PX4 Development Team. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -30,41 +30,15 @@
  * POSSIBILITY OF SUCH DAMAGE.
  *
  ****************************************************************************/
-
 #pragma once
-
-#include <stdint.h>
-#include <drivers/device/i2c.h>
-#include <px4_platform_common/i2c_spi_buses.h>
-#include <uORB/topics/gpio_config.h>
-#include <uORB/topics/gpio_in.h>
-#include <uORB/topics/gpio_out.h>
-#include <uORB/topics/gpio_request.h>
-#include <uORB/Publication.hpp>
-#include <uORB/SubscriptionCallback.hpp>
-#include <lib/perf/perf_counter.h>
-#include <drivers/drv_hrt.h>
+#include <lib/drivers/mcp_common/MCP.hpp>
 
 using namespace time_literals;
 
-class MCP23009 : public device::I2C, public I2CSPIDriver<MCP23009>
+class MCP23009 : public MCP230XX
 {
 public:
 	MCP23009(const I2CSPIDriverConfig &config);
-	~MCP23009() override;
-
-	void RunImpl();
-
-	static void print_usage();
-	static I2CSPIDriverBase *instantiate(const I2CSPIDriverConfig &config, int runtime_instance);
-
-protected:
-	int init_uorb();
-	int init(uint8_t direction, uint8_t state, uint8_t pull_up);
-
-	int probe() override;
-	void print_status() override;
-	void exit_and_cleanup() override;
 
 private:
 	enum class
@@ -82,30 +56,9 @@ private:
 		OLAT    = 0x0a
 	};
 
-	enum class
-	PinType : uint8_t {
-		Output,
-		Input,
-		InputPullUp,
-	};
-
-	uORB::SubscriptionCallbackWorkItem _gpio_out_sub{this, ORB_ID(gpio_out)};
-	uORB::SubscriptionCallbackWorkItem _gpio_request_sub{this, ORB_ID(gpio_request)};
-	uORB::SubscriptionCallbackWorkItem _gpio_config_sub{this, ORB_ID(gpio_config)};
-
-	uORB::Publication<gpio_in_s> _to_gpio_in{ORB_ID(gpio_in)};
-
-	perf_counter_t _cycle_perf;
-
-	// buffer often used (write-only!) registers here
-	uint8_t _olat;
-	uint8_t _iodir;
-	uint8_t _gppu;
-
-	int read(uint8_t *mask);
-	int write(uint8_t mask_set, uint8_t mask_clear);
-	int configure(uint8_t mask, PinType type);
-
-	int read_reg(Register address, uint8_t &data);
-	int write_reg(Register address, uint8_t data);
+	int get_olat(int bank, uint8_t *addr) override;
+	int get_gppu(int bank, uint8_t *addr) override;
+	int get_iodir(int bank, uint8_t *addr) override;
+	int get_gpio(int bank, uint8_t *addr) override;
+	int get_probe_reg(uint8_t *addr) override;
 };
