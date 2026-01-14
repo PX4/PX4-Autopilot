f4c812be5eded0f9e8f29a6f83bf1cdce1cc537f
Matthias Grob <maetugr@gmail.com>
2026-01-14 19:27:47 +0100

    rtl: adhere to parameter member naming convention

--- src/modules/navigator/rtl.h
+++ src/modules/navigator/rtl.h
@@ -240,11 +240,11 @@ private:
 
 	DEFINE_PARAMETERS(
 		(ParamInt<px4::params::RTL_TYPE>)          _param_rtl_type,
-		(ParamInt<px4::params::RTL_CONE_ANG>)      _param_rtl_cone_half_angle_deg,
+		(ParamInt<px4::params::RTL_CONE_ANG>)      _param_rtl_cone_ang,
 		(ParamFloat<px4::params::RTL_RETURN_ALT>)  _param_rtl_return_alt,
 		(ParamFloat<px4::params::RTL_MIN_DIST>)    _param_rtl_min_dist,
 		(ParamFloat<px4::params::NAV_ACC_RAD>)     _param_nav_acc_rad,
-		(ParamInt<px4::params::RTL_APPR_FORCE>)    _param_rtl_approach_force
+		(ParamInt<px4::params::RTL_APPR_FORCE>)    _param_rtl_appr_force
 	)
 
 	uORB::SubscriptionInterval _parameter_update_sub{ORB_ID(parameter_update), 1_s};
