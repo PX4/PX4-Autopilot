3f3508f8aab7929246f6ae932490cf5d145795dd
Silvan <silvan@auterion.com>
2026-02-16 17:42:19 +0100

    CA: remove legacy motor stopping logic
    Signed-off-by: Silvan <silvan@auterion.com>

--- src/modules/control_allocator/VehicleActuatorEffectiveness/ActuatorEffectivenessFixedWing.cpp
+++ src/modules/control_allocator/VehicleActuatorEffectiveness/ActuatorEffectivenessFixedWing.cpp
@@ -64,8 +64,6 @@ ActuatorEffectivenessFixedWing::getEffectivenessMatrix(Configuration &configurat
 void ActuatorEffectivenessFixedWing::updateSetpoint(const matrix::Vector<float, NUM_AXES> &control_sp, int matrix_index,
 		ActuatorVector &actuator_sp, const ActuatorVector &actuator_min, const ActuatorVector &actuator_max)
 {
-	stopMaskedMotorsWithZeroThrust(_forwards_motors_mask, actuator_sp);
-
 	// disable selected control surfaces during launch
 	launch_detection_status_s launch_detection_status;
 
