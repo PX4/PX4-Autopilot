01f2f9b6a1e918cb8b38acac5f4e23818afe4980
jonas <jonas.perolini@rigi.tech>
2026-02-18 17:16:09 +0100

    fix potential out of bounds call

--- src/modules/airspeed_selector/AirspeedValidator.cpp
+++ src/modules/airspeed_selector/AirspeedValidator.cpp
@@ -123,7 +123,8 @@ AirspeedValidator::update_CAS_scale_validated(bool gnss_valid, const matrix::Vec
 	}
 
 	const float course_over_ground_rad = matrix::wrap_2pi(atan2f(vI(1), vI(0)));
-	const int segment_index = int(SCALE_CHECK_SAMPLES * course_over_ground_rad / (2.f * M_PI_F));
+	const int segment_index = static_cast<int>(SCALE_CHECK_SAMPLES * course_over_ground_rad / (2.f * M_PI_F))
+				  % SCALE_CHECK_SAMPLES;
 
 	_scale_check_groundspeed(segment_index) = vI.norm();
 	_scale_check_TAS(segment_index) = airspeed_true_raw;
