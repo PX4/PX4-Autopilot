996060f581cdf63c31789e9066c0ce3e5495b130
Eric Katzfey <eric.katzfey@modalai.com>
2026-02-18 16:58:52 -0700

    muorb: Added statistics and status reporting. Improved aggregator send performance.

--- src/modules/muorb/slpi/muorb_main.cpp
+++ src/modules/muorb/slpi/muorb_main.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- * Copyright (C) 2022-2026 ModalAI, Inc. All rights reserved.
+ *   Copyright (c) 2022-2026 ModalAI, Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -31,66 +31,21 @@
  *
  ****************************************************************************/
 
-#pragma once
-
-#include <string>
 #include <string.h>
-#include <drivers/drv_hrt.h>
-#include "uORB/uORBCommunicator.hpp"
+#include "uORBProtobufChannel.hpp"
 
-namespace mUORB
+extern "C" {
+	__EXPORT int muorb_main(int argc, char *argv[]);
+}
+
+int
+muorb_main(int argc, char *argv[])
 {
+	uORB::ProtobufChannel *channel = uORB::ProtobufChannel::GetInstance();
 
-class Aggregator
-{
-public:
-	typedef int (*sendFuncPtr)(const char *, const uint8_t *, int);
+	if (channel) {
+		channel->PrintStatus();
+	}
 
-	void RegisterSendHandler(sendFuncPtr func) { sendFunc = func; }
-
-	void RegisterHandler(uORBCommunicator::IChannelRxHandler *handler) { _RxHandler = handler; }
-
-	int16_t ProcessTransmitTopic(const char *topic, const uint8_t *data, uint32_t length_in_bytes);
-
-	void ProcessReceivedTopic(const char *topic, const uint8_t *data, uint32_t length_in_bytes);
-
-	int16_t SendData();
-
-	hrt_abstime GetLastSendTime() { return _last_send_time; }
-
-private:
-	static const bool debugFlag;
-
-	const std::string topicName = "aggregation";
-
-	// Master flag to enable aggregation
-	const bool aggregationEnabled = true;
-
-	const uint32_t syncFlag = 0x5A01FF00;
-	const uint32_t syncFlagSize = 4;
-	const uint32_t topicNameLengthSize = 4;
-	const uint32_t dataLengthSize = 4;
-	const uint32_t headerSize = syncFlagSize + topicNameLengthSize + dataLengthSize;
-	static const uint32_t numBuffers = 2;
-	static const uint32_t bufferSize = 2048;
-
-	uint32_t bufferId;
-	uint32_t bufferWriteIndex;
-	uint8_t  buffer[numBuffers][bufferSize];
-
-	uORBCommunicator::IChannelRxHandler *_RxHandler;
-
-	sendFuncPtr sendFunc;
-
-	hrt_abstime _last_send_time;
-
-	bool isAggregate(const char *name) { return (strcmp(name, topicName.c_str()) == 0); }
-
-	bool NewRecordOverflows(const char *messageName, int32_t length);
-
-	void MoveToNextBuffer();
-
-	void AddRecordToBuffer(const char *messageName, int32_t length, const uint8_t *data);
-};
-
+	return 0;
 }
