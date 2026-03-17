209fb8a5c601bc3128ebb429811ab94bbf88bf23
Silvan <silvan@auterion.com>
2026-03-17 15:47:24 +0100

    feat(Commander): add preflight check for containing rally point for RTL_TYPE 5
    Signed-off-by: Silvan <silvan@auterion.com>

--- src/modules/navigator/rtl.cpp
+++ src/modules/navigator/rtl.cpp
@@ -441,11 +441,16 @@ PositionYawSetpoint RTL::findClosestSafePoint(float min_dist, uint8_t &safe_poin
 				continue;
 			}
 
+			// Only look at rally points
+			if (mission_safe_point.nav_cmd != NAV_CMD_RALLY_POINT) {
+				continue;
+			}
+
 			// Ignore safepoints which are too close to the homepoint (only if home is an option to return to)
 			const bool far_from_home = get_distance_to_next_waypoint(_home_pos_sub.get().lat, _home_pos_sub.get().lon,
 						   mission_safe_point.lat, mission_safe_point.lon) > MAX_DIST_FROM_HOME_FOR_LAND_APPROACHES;
 
-			if (mission_safe_point.nav_cmd == NAV_CMD_RALLY_POINT && (far_from_home || (_param_rtl_type.get() == 5))) {
+			if (far_from_home || (_param_rtl_type.get() == 5)) {
 				const float dist{get_distance_to_next_waypoint(_global_pos_sub.get().lat, _global_pos_sub.get().lon, mission_safe_point.lat, mission_safe_point.lon)};
 
 				PositionYawSetpoint safepoint_position;
