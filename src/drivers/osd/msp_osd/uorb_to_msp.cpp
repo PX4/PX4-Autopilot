4d1700cd6fa59d17d8e71911b0118c8559c4d364
Naor Waiss <naor@formicrobotics.com>
2026-03-24 15:59:53 +0200

    for gali

--- src/drivers/osd/msp_osd/uorb_to_msp.cpp
+++ src/drivers/osd/msp_osd/uorb_to_msp.cpp
@@ -775,6 +775,8 @@ msp_rendor_formic_vision_quality_t construct_rendor_FORMIC_VISION_QUALITY(const 
 		snprintf(&vision_quality.str[0], sizeof(vision_quality.str), "VQ:N/A");
 	}
 
+	PX4_INFO("Vision quality: %d", vehicle_vision_odometry.quality);
+
 	return vision_quality;
 }
 
