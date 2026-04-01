d69346ce03323175dec488aa17ecc4619b474834
Luka Filipović <filipovicluka3@gmail.com>
2026-04-01 17:33:01 +0200

    feat(motion_planning/TrajectoryConstraints): compute target and waypoint 3D speed

--- src/lib/motion_planning/TrajectoryConstraints.hpp
+++ src/lib/motion_planning/TrajectoryConstraints.hpp
@@ -176,6 +176,84 @@ float computeXYSpeedFromWaypoints(const Vector3f waypoints[N], const VehicleDyna
 	return max_speed;
 }
 
+inline void computeTarget3DSpeedFromWaypoints(const Vector3f &start_position, const Vector3f &target,
+		const Vector3f &next_target, const float &xy_speed_at_next, const float &z_speed_at_next,
+		const VehicleDynamicLimits &config,
+		float &xy_speed_at_target, float &z_speed_at_target)
+{
+	const float distance_target_next_xy = (target - next_target).xy().norm();
+	const float distance_target_next_z = fabsf(target(2) - next_target(2));
+
+	bool start_position_target_overlap = ((start_position - target).xy().norm() < 0.001f)
+					     && (fabsf(start_position(2) - target(2)) < 0.001f);
+
+	const bool z_direction_changed = !(start_position(2) <= target(2) && target(2) <= next_target(2))
+					 && !(start_position(2) >= target(2) && target(2) >= next_target(2));
+	const bool z_direction_up = target(2) > next_target(2);
+
+	// Horizontal speed at target waypoint
+	xy_speed_at_target = computeMaxSpeedFromDistance(config.max_jerk, config.max_acc_xy, distance_target_next_xy,
+			     xy_speed_at_next);
+
+	if (!start_position_target_overlap) {
+		const float alpha = acosf(Vector2f((target - start_position).xy()).unit_or_zero().dot(
+						  Vector2f((target - next_target).xy()).unit_or_zero()));
+		const float safe_alpha = math::constrain(alpha, 0.f, M_PI_F - FLT_EPSILON);
+		xy_speed_at_target = fminf(xy_speed_at_target, computeMaxSpeedInWaypoint(safe_alpha,
+					   config.max_acc_xy_radius_scale * config.max_acc_xy, config.xy_accept_rad));
+
+	}
+
+	xy_speed_at_target = fminf(xy_speed_at_target, config.max_speed_xy);
+
+	// Vertical speed at target waypoint
+	z_speed_at_target = computeMaxSpeedFromDistance(config.max_jerk,
+			    z_direction_up ? config.max_acc_z_up : config.max_acc_z_down, distance_target_next_z,
+			    z_speed_at_next);
+
+	if (!start_position_target_overlap && z_direction_changed) {
+		// Z direction is changing at the target waypoint, we need to come to a full stop
+		z_speed_at_target = 0.f;
+
+	}
+
+	z_speed_at_target = fminf(z_speed_at_target, z_direction_up ? config.max_speed_z_up : config.max_speed_z_down);
+
+	// Clamp the speed in both XY and Z to respect the line target-next_target
+	const Vector3f u_target_to_next{(next_target - target).unit_or_zero()};
+	Vector3f vel_sp_constrained = u_target_to_next * sqrtf(xy_speed_at_target * xy_speed_at_target + z_speed_at_target *
+				      z_speed_at_target);
+	clampToXYNorm(vel_sp_constrained, xy_speed_at_target, 0.5f);
+	clampToZNorm(vel_sp_constrained, z_speed_at_target, 0.5f);
+
+	xy_speed_at_target = Vector2f(vel_sp_constrained).norm();
+	z_speed_at_target = fabsf(vel_sp_constrained(2));
+
+	return;
+}
+
+template <int N>
+void compute3DSpeedFromWaypoints(const Vector3f waypoints[N], const VehicleDynamicLimits &config,
+				 float &xy_speed, float &z_speed)
+{
+	static_assert(N >= 2, "Need at least 2 points to compute speed");
+
+	float xy_speed_at_next = 0;
+	float z_speed_at_next = 0;
+
+	// go backwards through the waypoints to compute the speed at the target waypoint (waypoints[1])
+	for (int i = (N - 2); i >= -1; i--) {
+		computeTarget3DSpeedFromWaypoints(waypoints[max(i, 0)],
+						  waypoints[i + 1],
+						  waypoints[min(i + 2, N - 1)],
+						  xy_speed_at_next, z_speed_at_next, config, xy_speed, z_speed);
+
+		xy_speed_at_next = xy_speed;
+		z_speed_at_next = z_speed;
+	}
+
+	return;
+}
 
 }
 }
