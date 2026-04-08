96a4d3dabc470cfcafa63f2d2b1aad4991110e7a
ttechnick <nick.truttmann@auterion.com>
2026-04-08 10:05:48 +0200

    fix(fw_attitude_control): use euler angles to construct stabilized setpoint

--- src/modules/fw_att_control/FixedwingAttitudeControl.cpp
+++ src/modules/fw_att_control/FixedwingAttitudeControl.cpp
@@ -105,7 +105,7 @@ FixedwingAttitudeControl::vehicle_manual_poll(const float yaw_body)
 				pitch_body = constrain(pitch_body,
 						       -radians(_param_fw_man_p_max.get()), radians(_param_fw_man_p_max.get()));
 
-				const Quatf q_sp_rp = AxisAnglef(roll_body, pitch_body, 0.f);
+				const Quatf q_sp_rp = Eulerf(roll_body, pitch_body, 0.f);
 				const Quatf q_sp_yaw(cosf(yaw_body / 2.f), 0.f, 0.f, sinf(yaw_body / 2.f));
 				Quatf q = q_sp_yaw * q_sp_rp;
 
