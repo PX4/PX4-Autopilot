2d093c978b4018643d868a5cca5a21811d57dc1f
Julian Oes <julian@oes.ch>
2026-04-21 11:13:02 +1200

    feat(mavlink): map @MAV_LOG virtual directory to log root
    Implements the MAVLink FTP "Virtual Directory Entries" convention so
    that clients can access log files without knowing the flight-stack
    storage layout. Requests whose path starts with @MAV_LOG are remapped
    to CONFIG_BOARD_ROOT_PATH "/log", matching the logger's LOG_ROOT.
    
    See https://github.com/mavlink/mavlink-devguide/pull/668.
    
    Signed-off-by: Julian Oes <julian@oes.ch>

--- src/modules/mavlink/mavlink_ftp.cpp
+++ src/modules/mavlink/mavlink_ftp.cpp
@@ -48,6 +48,8 @@
 using namespace time_literals;
 
 constexpr const char MavlinkFTP::_root_dir[];
+constexpr const char MavlinkFTP::_mav_log_prefix[];
+constexpr const char MavlinkFTP::_mav_log_dir[];
 
 MavlinkFTP::MavlinkFTP(Mavlink &mavlink) :
 	_mavlink(mavlink)
@@ -310,6 +312,24 @@ MavlinkFTP::_reply(mavlink_file_transfer_protocol_t *ftp_req)
 }
 void MavlinkFTP::_constructPath(char *dst, int dst_len, const char *path) const
 {
+	// MAVLink FTP virtual directory: paths starting with "@MAV_LOG" (optionally prefixed with '/')
+	// are remapped to the flight-stack log root directory.
+	const char *p = path;
+
+	if (p[0] == '/') {
+		p++;
+	}
+
+	if (strncmp(p, _mav_log_prefix, _mav_log_prefix_len) == 0
+	    && (p[_mav_log_prefix_len] == '\0' || p[_mav_log_prefix_len] == '/')) {
+		strncpy(dst, _mav_log_dir, dst_len);
+		dst[dst_len - 1] = '\0';
+		int used = strlen(dst);
+		strncpy(dst + used, p + _mav_log_prefix_len, dst_len - used);
+		dst[dst_len - 1] = '\0';
+		return;
+	}
+
 	strncpy(dst, _root_dir, dst_len);
 	int root_dir_len = _root_dir_len;
 
