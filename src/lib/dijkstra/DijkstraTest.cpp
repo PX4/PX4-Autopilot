e5b4e9c52af1d9bb6d71ad97b0fa210347567986
RomanBapst <bapstroman@gmail.com>
2026-04-30 14:27:46 +0300

    support symmetric cost in order to save memory
    Signed-off-by: RomanBapst <bapstroman@gmail.com>

--- src/lib/dijkstra/DijkstraTest.cpp
+++ src/lib/dijkstra/DijkstraTest.cpp
@@ -64,7 +64,7 @@ TEST(DijkstraTest, SingleNodeAtGoal)
 	int next[N];
 	bool vis[N];
 
-	ASSERT_TRUE(dijkstra::solveBackward(N, 0, cost, best, next, vis));
+	ASSERT_TRUE(dijkstra::solveBackward(N, 0, cost, false, best, next, vis));
 	EXPECT_FLOAT_EQ(best[0], 0.f);
 	EXPECT_EQ(next[0], -1);
 }
@@ -85,7 +85,7 @@ TEST(DijkstraTest, AsymmetricLineGraphForward)
 	int next[N];
 	bool vis[N];
 
-	ASSERT_TRUE(dijkstra::solveBackward(N, 2, cost, best, next, vis));
+	ASSERT_TRUE(dijkstra::solveBackward(N, 2, cost, false, best, next, vis));
 
 	EXPECT_FLOAT_EQ(best[0], 2.f); // 0 -> 1 -> 2 costs 1 + 1
 	EXPECT_FLOAT_EQ(best[1], 1.f); // 1 -> 2
@@ -112,7 +112,7 @@ TEST(DijkstraTest, AsymmetricLineGraphReverse)
 	int next[N];
 	bool vis[N];
 
-	ASSERT_TRUE(dijkstra::solveBackward(N, 0, cost, best, next, vis));
+	ASSERT_TRUE(dijkstra::solveBackward(N, 0, cost, false, best, next, vis));
 
 	EXPECT_FLOAT_EQ(best[0], 0.f);
 	EXPECT_FLOAT_EQ(best[1], 100.f);
@@ -137,13 +137,13 @@ TEST(DijkstraTest, TriangleWithBlockedEdge)
 	int next[N];
 	bool vis[N];
 
-	ASSERT_TRUE(dijkstra::solveBackward(N, 2, cost, best, next, vis));
+	ASSERT_TRUE(dijkstra::solveBackward(N, 2, cost, false, best, next, vis));
 	EXPECT_FLOAT_EQ(best[0], 0.5f);
 	EXPECT_EQ(next[0], 2);
 
 	// Block the direct edge and re-solve; path must detour through 1.
 	cost[0 * N + 2] = INFINITY;
-	ASSERT_TRUE(dijkstra::solveBackward(N, 2, cost, best, next, vis));
+	ASSERT_TRUE(dijkstra::solveBackward(N, 2, cost, false, best, next, vis));
 	EXPECT_FLOAT_EQ(best[0], 2.f);
 	EXPECT_EQ(next[0], 1);
 }
@@ -163,7 +163,7 @@ TEST(DijkstraTest, GoalUnreachableNoInfiniteLoop)
 	int next[N];
 	bool vis[N];
 
-	ASSERT_TRUE(dijkstra::solveBackward(N, 2, cost, best, next, vis));
+	ASSERT_TRUE(dijkstra::solveBackward(N, 2, cost, false, best, next, vis));
 	EXPECT_FLOAT_EQ(best[2], 0.f);
 	EXPECT_FALSE(best[0] < dijkstra::kUnreachable);
 	EXPECT_FALSE(best[1] < dijkstra::kUnreachable);
@@ -185,7 +185,7 @@ TEST(DijkstraTest, NaNTreatedAsMissingEdge)
 	int next[N];
 	bool vis[N];
 
-	ASSERT_TRUE(dijkstra::solveBackward(N, 2, cost, best, next, vis));
+	ASSERT_TRUE(dijkstra::solveBackward(N, 2, cost, false, best, next, vis));
 	EXPECT_FLOAT_EQ(best[0], 2.f);
 	EXPECT_EQ(next[0], 1);
 }
@@ -198,11 +198,58 @@ TEST(DijkstraTest, RejectsInvalidInputs)
 	int next[N];
 	bool vis[N];
 
-	EXPECT_FALSE(dijkstra::solveBackward(0, 0, cost, best, next, vis));
-	EXPECT_FALSE(dijkstra::solveBackward(N, -1, cost, best, next, vis));
-	EXPECT_FALSE(dijkstra::solveBackward(N, N, cost, best, next, vis));
-	EXPECT_FALSE(dijkstra::solveBackward(N, 0, nullptr, best, next, vis));
-	EXPECT_FALSE(dijkstra::solveBackward(N, 0, cost, nullptr, next, vis));
+	EXPECT_FALSE(dijkstra::solveBackward(0, 0, cost, false, best, next, vis));
+	EXPECT_FALSE(dijkstra::solveBackward(N, -1, cost, false, best, next, vis));
+	EXPECT_FALSE(dijkstra::solveBackward(N, N, cost, false, best, next, vis));
+	EXPECT_FALSE(dijkstra::solveBackward(N, 0, nullptr, false, best, next, vis));
+	EXPECT_FALSE(dijkstra::solveBackward(N, 0, cost, false, nullptr, next, vis));
+}
+
+TEST(DijkstraTest, SymmetricPackedMatchesAsymmetric)
+{
+	// Same undirected weighted graph expressed two ways: full N*N matrix with both
+	// halves filled, and packed upper triangle. Results must match for both layouts.
+	constexpr int N = 5;
+
+	// Edges (a, b, w) with a < b.
+	struct UndirEdge { int a, b; float w; };
+	const UndirEdge edges[] = {
+		{0, 1, 2.f},
+		{0, 2, 4.f},
+		{1, 2, 1.f},
+		{1, 3, 7.f},
+		{2, 3, 3.f},
+		{3, 4, 2.f},
+	};
+
+	float full[N * N];
+
+	for (int i = 0; i < N * N; ++i) { full[i] = INFINITY; }
+
+	constexpr int kPacked = N * (N - 1) / 2;
+	float packed[kPacked];
+
+	for (int i = 0; i < kPacked; ++i) { packed[i] = INFINITY; }
+
+	for (const auto &e : edges) {
+		full[e.a * N + e.b] = e.w;
+		full[e.b * N + e.a] = e.w;
+		packed[e.a * (2 * N - e.a - 1) / 2 + (e.b - e.a - 1)] = e.w;
+	}
+
+	float best_full[N], best_pack[N];
+	int next_full[N], next_pack[N];
+	bool vis[N];
+
+	for (int goal = 0; goal < N; ++goal) {
+		ASSERT_TRUE(dijkstra::solveBackward(N, goal, full, false, best_full, next_full, vis));
+		ASSERT_TRUE(dijkstra::solveBackward(N, goal, packed, true, best_pack, next_pack, vis));
+
+		for (int i = 0; i < N; ++i) {
+			EXPECT_FLOAT_EQ(best_full[i], best_pack[i]) << "goal=" << goal << " i=" << i;
+			EXPECT_EQ(next_full[i], next_pack[i]) << "goal=" << goal << " i=" << i;
+		}
+	}
 }
 
 TEST(DijkstraTest, ForwardWalkReachesGoal)
@@ -223,7 +270,7 @@ TEST(DijkstraTest, ForwardWalkReachesGoal)
 	float best[N];
 	int next[N];
 	bool vis[N];
-	ASSERT_TRUE(dijkstra::solveBackward(N, 4, cost, best, next, vis));
+	ASSERT_TRUE(dijkstra::solveBackward(N, 4, cost, false, best, next, vis));
 
 	for (int start = 0; start < N - 1; ++start) {
 		int u = start;
