763ebb56412880683854832c3282e7b56b01470a
Balduin <balduin@auterion.com>
2026-05-07 17:13:07 +0200

    touching and parallel tests

--- src/lib/geofence/GeofenceUtilsTest.cpp
+++ src/lib/geofence/GeofenceUtilsTest.cpp
@@ -61,6 +61,49 @@ TEST(GeofenceUtilsTest, SegmentsCross)
 	EXPECT_TRUE(geofence_utils::segmentsIntersect(p1, p2, v1, v2));
 }
 
+TEST(GeofenceUtilsTest, SegmentsTouching)
+{
+	// Vertical line
+	Vector2f p1(0.f, 0.f);
+	Vector2f p2(0.f, 2.f);
+
+	// Horizontal line starting on interior of vertical line
+	Vector2f v1(0.f, 1.0f);
+	Vector2f v2(1.f, 1.0f);
+
+	// This asymmetry may seem very weird. It is explained in segmentsIntersect.
+
+	// If vertical line is part of polygon (first arg): No intersection
+	EXPECT_FALSE(geofence_utils::segmentsIntersect(p1, p2, v1, v2));
+
+	// If horizontal line is part of polygon (second arg): intersection
+	EXPECT_TRUE(geofence_utils::segmentsIntersect(v1, v2, p1, p2));
+
+	// Same, but with vertical line slanted for good measure
+	p1(0) = -1.0f;
+	p2(0) = 1.0f;
+
+	EXPECT_FALSE(geofence_utils::segmentsIntersect(p1, p2, v1, v2));
+	EXPECT_TRUE(geofence_utils::segmentsIntersect(v1, v2, p1, p2));
+}
+
+TEST(GeofenceUtilsTest, SegmentsParallel)
+{
+	// Slope = 1/3 for both lines
+	Vector2f p1(0.f, 0.f);
+	Vector2f p2(3.0f, 0.f);
+
+	Vector2f v1(10.f, 10.f);
+	Vector2f v2(40.f, 20.f);
+
+	// Parallel lines never intersect
+	EXPECT_FALSE(geofence_utils::segmentsIntersect(p1, p2, v1, v2));
+
+	// Equal lines also don't (our convention)
+	EXPECT_FALSE(geofence_utils::segmentsIntersect(p1, p2, p1, p2));
+	EXPECT_FALSE(geofence_utils::segmentsIntersect(v1, v2, v1, v2));
+}
+
 TEST(GeofenceUtilsTest, SegmentPolygonIntersectOutside)
 {
 	static constexpr int N = 4;
