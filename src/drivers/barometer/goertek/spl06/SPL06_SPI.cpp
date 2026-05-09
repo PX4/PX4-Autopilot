6c2871f4310ea75575db68ace04e81795a660ae9
NeverSayGoodby <18585840+NSG-666@users.noreply.github.com>
2026-05-09 10:11:58 +0800

    Update SPL06_SPI.cpp
    Correct source code formatting

--- src/drivers/barometer/goertek/spl06/SPL06_SPI.cpp
+++ src/drivers/barometer/goertek/spl06/SPL06_SPI.cpp
@@ -99,15 +99,15 @@ SPL06_SPI::read(uint8_t addr, uint8_t *buf, uint8_t len)
 	uint8_t tx_buf[len + 1] = {(uint8_t)(addr | DIR_READ)}; // GCC support VLA, let's use it
 
 	int res = transfer(tx_buf, tx_buf, (len + 1));
-
+	
 	if (res < 0) {
- 		return res;
- 	}
-
-	for (int i=0;i<len;i++) {
-		buf[i] = tx_buf[1+i];
+		return res;
 	}
-
+	
+	for (int i = 0; i < len; i++) {
+		buf[i] = tx_buf[1 + i];
+	}
+	
 	return res;
 }
 
