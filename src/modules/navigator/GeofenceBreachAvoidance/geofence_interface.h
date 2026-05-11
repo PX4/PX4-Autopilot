c7f88664146c08b21afb1a1f58ba7d4fbdda1f72
Balduin <balduin@auterion.com>
2026-05-11 18:24:14 +0200

    cleanup: remove checkIfLineViolatesAnyFence from GeofenceInterface
    Planner no longer calls this method. all visibility checks go through
    PlannerPolygons directly. Remove the pure virtual from the interface,
    its implementation in Geofence, and the stub in FakeGeofence.

--- src/modules/navigator/GeofenceBreachAvoidance/geofence_interface.h
+++ src/modules/navigator/GeofenceBreachAvoidance/geofence_interface.h
@@ -8,10 +8,6 @@ class GeofenceInterface
 public:
 	virtual ~GeofenceInterface() = default;
 
-	virtual bool checkIfLineViolatesAnyFence(const matrix::Vector2f &start_local,
-			const matrix::Vector2f &end_local,
-			const matrix::Vector2<double> &reference) = 0;
-
 	virtual PolygonInfo getPolygonInfoByIndex(int index) = 0;
 
 	virtual matrix::Vector2<double> getPolygonVertexByIndex(int poly_idx, int vertex_idx) = 0;
