691ab811a974eb119e0110f456decaa41c0a5496
soshnikov936 <soshnikov936@gmail.com>
2026-05-12 22:52:39 +0300

    Update default.cmake

--- boards/px4/blackpill/default.cmake
+++ boards/px4/blackpill/default.cmake
@@ -6,18 +6,21 @@ px4_add_board(
     TOOLCHAIN arm-none-eabi
     ARCHITECTURE stm32f411ceu6
     
+    # Жесткая экономия Flash памяти под 512 КБ
     CONSTRAINED_FLASH
     
     DRIVERS
-        barometer
-        imu
-        gps
+        # Оставляем только драйвер вашего датчика MPU6050
+        imu/mpu6050
+        # Драйвер для приема данных оптического потока от OpenMV
         optical_flow/px4flow
         
     MODULES
         apps
         commander
-        ekf2
+        # Используем ekf2. Если сборка будет ругаться на нехватку RAM,
+        # в будущем заменим на более легкий local_position_estimator
+        ekf2 
         mavlink
         mc_att_control
         mc_pos_control
