2be413c7b2331f81cd5d4aa80691d14a387a36c4
Julian Oes <julian@oes.ch>
2026-05-19 10:35:28 +1200

    Update board_id to 1302

--- boards/cbunmanned/h753-stamp/src/hw_config.h
+++ boards/cbunmanned/h753-stamp/src/hw_config.h
@@ -48,7 +48,7 @@
 #define INTERFACE_USART                1
 #define INTERFACE_USART_CONFIG         "/dev/ttyS0,115200"
 #define BOOT_DELAY_ADDRESS             0x000001a0
-#define BOARD_TYPE                     1156
+#define BOARD_TYPE                     1302
 #define _FLASH_KBYTES                  (*(uint32_t *)0x1FF1E880)
 #define BOARD_FLASH_SECTORS            (14)
 #define BOARD_FLASH_SIZE               (_FLASH_KBYTES * 1024)
