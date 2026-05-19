d25017f87b8ae24e1c1ebb49e3d74c743b69e6d4
Nir Mor <nir.mor@gmail.com>
2026-05-19 07:28:16 +0300

    style(boards/dakefpv): fix indentation to tabs in H743 source files
    Signed-off-by: Nir Mor <nir.mor@gmail.com>

--- boards/dakefpv/h743/src/timer_config.cpp
+++ boards/dakefpv/h743/src/timer_config.cpp
@@ -34,32 +34,32 @@
 #include <px4_arch/io_timer_hw_description.h>
 
 constexpr io_timers_t io_timers[MAX_IO_TIMERS] = {
-        initIOTimer(Timer::Timer2,  DMA{DMA::Index1}),  // M1-M4
-        initIOTimer(Timer::Timer4,  DMA{DMA::Index1}),  // M5-M8
-        initIOTimer(Timer::Timer15),                     // S1-S2
-        initIOTimer(Timer::Timer8,  DMA{DMA::Index1}),  // S3-S4 (HRT is TIM5, so TIM8 is free)
+	initIOTimer(Timer::Timer2,  DMA{DMA::Index1}),  // M1-M4
+	initIOTimer(Timer::Timer4,  DMA{DMA::Index1}),  // M5-M8
+	initIOTimer(Timer::Timer15),                     // S1-S2
+	initIOTimer(Timer::Timer8,  DMA{DMA::Index1}),  // S3-S4 (HRT is TIM5, so TIM8 is free)
 };
 
 constexpr timer_io_channels_t timer_io_channels[MAX_TIMER_IO_CHANNELS] = {
-        initIOTimerChannel(io_timers, {Timer::Timer2,  Timer::Channel1}, {GPIO::PortA, GPIO::Pin0}),  // M1
-        initIOTimerChannel(io_timers, {Timer::Timer2,  Timer::Channel2}, {GPIO::PortA, GPIO::Pin1}),  // M2
-        initIOTimerChannel(io_timers, {Timer::Timer2,  Timer::Channel3}, {GPIO::PortA, GPIO::Pin2}),  // M3
-        initIOTimerChannel(io_timers, {Timer::Timer2,  Timer::Channel4}, {GPIO::PortA, GPIO::Pin3}),  // M4
+	initIOTimerChannel(io_timers, {Timer::Timer2,  Timer::Channel1}, {GPIO::PortA, GPIO::Pin0}),  // M1
+	initIOTimerChannel(io_timers, {Timer::Timer2,  Timer::Channel2}, {GPIO::PortA, GPIO::Pin1}),  // M2
+	initIOTimerChannel(io_timers, {Timer::Timer2,  Timer::Channel3}, {GPIO::PortA, GPIO::Pin2}),  // M3
+	initIOTimerChannel(io_timers, {Timer::Timer2,  Timer::Channel4}, {GPIO::PortA, GPIO::Pin3}),  // M4
 
-        initIOTimerChannel(io_timers, {Timer::Timer4,  Timer::Channel1}, {GPIO::PortD, GPIO::Pin12}), // M5
-        initIOTimerChannel(io_timers, {Timer::Timer4,  Timer::Channel2}, {GPIO::PortD, GPIO::Pin13}), // M6
-        initIOTimerChannel(io_timers, {Timer::Timer4,  Timer::Channel3}, {GPIO::PortD, GPIO::Pin14}), // M7
-        initIOTimerChannel(io_timers, {Timer::Timer4,  Timer::Channel4}, {GPIO::PortD, GPIO::Pin15}), // M8
+	initIOTimerChannel(io_timers, {Timer::Timer4,  Timer::Channel1}, {GPIO::PortD, GPIO::Pin12}), // M5
+	initIOTimerChannel(io_timers, {Timer::Timer4,  Timer::Channel2}, {GPIO::PortD, GPIO::Pin13}), // M6
+	initIOTimerChannel(io_timers, {Timer::Timer4,  Timer::Channel3}, {GPIO::PortD, GPIO::Pin14}), // M7
+	initIOTimerChannel(io_timers, {Timer::Timer4,  Timer::Channel4}, {GPIO::PortD, GPIO::Pin15}), // M8
 
-        initIOTimerChannel(io_timers, {Timer::Timer15, Timer::Channel1}, {GPIO::PortE, GPIO::Pin5}),  // S1
-        initIOTimerChannel(io_timers, {Timer::Timer15, Timer::Channel2}, {GPIO::PortE, GPIO::Pin6}),  // S2
+	initIOTimerChannel(io_timers, {Timer::Timer15, Timer::Channel1}, {GPIO::PortE, GPIO::Pin5}),  // S1
+	initIOTimerChannel(io_timers, {Timer::Timer15, Timer::Channel2}, {GPIO::PortE, GPIO::Pin6}),  // S2
 
-        initIOTimerChannel(io_timers, {Timer::Timer8,  Timer::Channel3}, {GPIO::PortC, GPIO::Pin8}),  // S3
-        initIOTimerChannel(io_timers, {Timer::Timer8,  Timer::Channel4}, {GPIO::PortC, GPIO::Pin9}),  // S4
+	initIOTimerChannel(io_timers, {Timer::Timer8,  Timer::Channel3}, {GPIO::PortC, GPIO::Pin8}),  // S3
+	initIOTimerChannel(io_timers, {Timer::Timer8,  Timer::Channel4}, {GPIO::PortC, GPIO::Pin9}),  // S4
 };
 
 constexpr io_timers_channel_mapping_t io_timers_channel_mapping = initIOTimerChannelMapping(io_timers, timer_io_channels);
 
 constexpr io_timers_t led_pwm_timers[MAX_LED_TIMERS] = {
-        initIOTimer(Timer::Timer1),  // PE9 LED strip pad (TIM1_CH1)
+	initIOTimer(Timer::Timer1),  // PE9 LED strip pad (TIM1_CH1)
 };
