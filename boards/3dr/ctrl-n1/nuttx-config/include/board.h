a3f966a01083dabe8cfc150e0ccafcf4b7e6f87a
Alexis Guijarro <totonzx@gmail.com>
2026-05-19 11:29:35 -0500

    fix(boards/3dr/ctrl-n1): applying copyright changes

--- boards/3dr/ctrl-n1/nuttx-config/include/board.h
+++ boards/3dr/ctrl-n1/nuttx-config/include/board.h
@@ -1,8 +1,7 @@
 /************************************************************************************
  * nuttx-config/include/board.h
  *
- *   Copyright (C) 2020 Gregory Nutt. All rights reserved.
- *   Authors: David Sidrane <david.sidrane@nscdg.com>
+ *   Copyright (C) 2026 Gregory Nutt. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
