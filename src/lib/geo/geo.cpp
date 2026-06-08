018e3c1233b470752d43f32515537fff93ae2b91
PuRe-ToX <ackarkera2@gmail.com>
2026-06-08 09:03:30 -0400

    fix(ci): perfectly align whitespace and tabs

--- src/lib/geo/geo.cpp
+++ src/lib/geo/geo.cpp
@@ -253,15 +253,15 @@ int get_distance_to_line(struct crosstrack_error_s &crosstrack_error, double lat
 	// Only run the math if we aren't sitting exactly ON the start point
 	if (dist_to_start > 0.1f) {
         	float bearing_start = get_bearing_to_next_waypoint(lat_now, lon_now, lat_start, lon_start);
-                float bearing_diff_start = wrap_pi(bearing_track - bearing_start);
+               float bearing_diff_start = wrap_pi(bearing_track - bearing_start);
 
-                // If the angle difference is less than 90 degrees, we are behind the starting line
-        	if (bearing_diff_start < M_PI_2_F && bearing_diff_start > -M_PI_2_F) {
-                	crosstrack_error.past_start = true;
-                	return_value = 0;
-                	return return_value;
-                }
-        }
+               // If the angle difference is less than 90 degrees, we are behind the starting line
+               if (bearing_diff_start < M_PI_2_F && bearing_diff_start > -M_PI_2_F) {
+		       crosstrack_error.past_start = true;
+                       return_value = 0;
+        	       return return_value;
+               }
+       }
 
 	crosstrack_error.distance = (dist_to_end) * sinf(bearing_diff);
 
