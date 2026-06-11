e7bebb19d49a9ee92c336eaca6404c49e1ba647b
Claudio Chies <chiesc@chies.com>
2026-06-11 12:39:23 +0200

    feat(sih): add initial yaw parameter for SIH

--- src/modules/simulation/simulator_sih/sih.cpp
+++ src/modules/simulation/simulator_sih/sih.cpp
@@ -71,7 +71,7 @@ void Sih::run()
 {
 	_px4_accel.set_temperature(T1_C);
 	_px4_gyro.set_temperature(T1_C);
-
+	_q = Quatf(Eulerf(0.f, 0.f, _sih_yaw0.get()));
 	parameters_updated();
 
 	const hrt_abstime task_start = hrt_absolute_time();
@@ -300,7 +300,7 @@ void Sih::parameters_updated()
 		const Dcmf R_E2N = _lla.computeRotEcefToNed();
 		_R_N2E = R_E2N.transpose();
 		_v_E = _R_N2E * _v_N;
-
+		_q = Quatf(Eulerf(0.f, 0.f, _sih_yaw0.get()));
 		_q_E = Quatf(_R_N2E) * _q;
 		_q_E.normalize();
 	}
