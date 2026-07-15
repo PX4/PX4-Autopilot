ad118f4675d18f09b7d7b2b6a871eb27c1f9ad66
Bartok <danielrpike9@gmail.com>
2026-07-15 13:35:01 -0500

    docs(cyphal): fix succes→success in socket/CAN return comments (#27911)
    Comment-only typo on CanardSocketCAN and CanardNuttXCDev.

--- src/drivers/cyphal/CanardSocketCAN.hpp
+++ src/drivers/cyphal/CanardSocketCAN.hpp
@@ -59,7 +59,7 @@ public:
 	/// Creates a SocketCAN socket for corresponding iface can_iface_name
 	/// Also sets up the message structures required for socketcanTransmit & socketcanReceive
 	/// can_fd determines to use CAN FD frame when is 1, and classical CAN frame when is 0
-	/// The return value is 0 on succes and -1 on error
+	/// The return value is 0 on success and -1 on error
 	int init();
 
 	/// Close socket connection
