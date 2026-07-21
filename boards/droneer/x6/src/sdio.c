e1789839ff8c9195f2aae7ca130ff6006a4cf58c
wszy2021 <wangshan13088@qq.com>
2026-07-21 15:15:24 +0800

    update copyright 2026

--- boards/droneer/x6/src/sdio.c
+++ boards/droneer/x6/src/sdio.c
@@ -1,6 +1,6 @@
 /****************************************************************************
  *
- *   Copyright (C) 2014, 2016 Gregory Nutt. All rights reserved.
+ *   Copyright (C) 2014, 2026 Gregory Nutt. All rights reserved.
  *   Author: Gregory Nutt <gnutt@nuttx.org>
  *
  * Redistribution and use in source and binary forms, with or without
