ddb755450b4e05e1abe66ea35df4d33fad8e7c8f
T Sukumar <sukumarthillairajan@gmail.com>
2026-08-07 17:42:46 +0530

    Add new navigation state 'MY_NEW_MODE'

--- src/modules/navigator/navigator_main.cpp
+++ src/modules/navigator/navigator_main.cpp
@@ -1019,6 +1019,7 @@ void Navigator::run()
 		case vehicle_status_s::NAVIGATION_STATE_ACRO:
 		case vehicle_status_s::NAVIGATION_STATE_ALTCTL:
 		case vehicle_status_s::NAVIGATION_STATE_ALTITUDE_CRUISE:
+		case vehicle_status_s::NAVIGATION_STATE_MY_NEW_MODE:
 		case vehicle_status_s::NAVIGATION_STATE_POSCTL:
 		case vehicle_status_s::NAVIGATION_STATE_DESCEND:
 		case vehicle_status_s::NAVIGATION_STATE_TERMINATION:
