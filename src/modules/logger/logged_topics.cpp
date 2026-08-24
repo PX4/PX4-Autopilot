ea48b59309306d2a1a169e25d366d079eb91174f
Elisa Ferrara <elisa.ferrara@auterion.com>
2026-08-24 11:35:03 +0200

    refactor(mavlink): redundancy of logged topic

--- src/modules/logger/logged_topics.cpp
+++ src/modules/logger/logged_topics.cpp
@@ -343,7 +343,6 @@ void LoggedTopics::add_estimator_replay_topics()
 	add_topic("vehicle_status");
 	add_topic("vehicle_visual_odometry");
 	add_topic("ranging_beacon");
-	add_topic("estimator_aid_src_ranging_beacon");
 	add_topic_multi("aux_global_position");
 	add_topic_multi("distance_sensor");
 }
