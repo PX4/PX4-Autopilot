707418ce70eda3e7abf3e10146096923de8bd160
Anas Abdelaziz <abdelazizanas3@gmail.com>
2026-08-31 08:58:27 +0300

    Fix error

--- src/modules/complementary_filter/complementary_filter.cpp
+++ src/modules/complementary_filter/complementary_filter.cpp
@@ -11,7 +11,7 @@ ComplementaryFilter::ComplementaryFilter()
 
     // Initialize the debug message once (name field doesn't change per cycle)
     memset(&_debug_data, 0, sizeof(_debug_data));
-    strncpy(_debug_data.name, "COMP_FILT", sizeof(_debug_data.name) - 1);
+  strlcpy(_debug_data.name, "COMP_FILT", sizeof(_debug_data.name));
     _debug_data.name[sizeof(_debug_data.name) - 1] = '\0';
 }
 
