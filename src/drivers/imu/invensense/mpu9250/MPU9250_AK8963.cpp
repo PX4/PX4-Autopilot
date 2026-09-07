f86c099e72fa329862a9e501dfc25042fe699013
mahima-yoga <mahima@auterion.com>
2026-09-07 10:53:24 +0200

    fix(drivers): correct ADIS16448 and AK8963 mag ranges

--- src/drivers/imu/invensense/mpu9250/MPU9250_AK8963.cpp
+++ src/drivers/imu/invensense/mpu9250/MPU9250_AK8963.cpp
@@ -54,7 +54,7 @@ MPU9250_AK8963::MPU9250_AK8963(MPU9250 &mpu9250, enum Rotation rotation) :
 
 	// in 16-bit sampling mode the mag resolution is 1.5 milli Gauss per bit */
 	_px4_mag.set_scale(1.5e-3f);
-	_px4_mag.set_range(49.12f); // +/-4912 uT
+	_px4_mag.set_range(49.f); // +/-4900 uT
 }
 
 MPU9250_AK8963::~MPU9250_AK8963()
