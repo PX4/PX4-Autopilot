c6c9cb8bf479d6c7edde93b956d71c8219929fb6
danielbuleandra <daniel.buleandra@auterion.com>
2026-09-10 18:27:06 +0200

    fix(drivers/rc/): fix hardfault error caused by strcmp called with nullptr

--- src/drivers/rc/dsm_rc/DsmRc.cpp
+++ src/drivers/rc/dsm_rc/DsmRc.cpp
@@ -95,6 +95,11 @@ int DsmRc::task_spawn(int argc, char *argv[])
 		return -1;
 	}
 
+	if (!device_name) {
+		PX4_ERR("valid device required");
+		return PX4_ERROR;
+	}
+
 	if (board_rc_conflicting(device_name)) {
 		PX4_INFO("unable to start, conflict with PX4IO on %s", device_name);
 		return PX4_ERROR;
