6ab3e98cff75a2eec742538bb26781275416f785
dopelion <wangdongpeng333@sina.com>
2026-09-23 13:55:26 +0800

    Fix ADC_SCALED_VDD_3V3_SENSORS1_CHANNEL definition

--- boards/cyberx/v10/src/board_config.h
+++ boards/cyberx/v10/src/board_config.h
@@ -188,7 +188,7 @@
 	  (1 << ADC_SCALED_V5_CHANNEL)               | \
 	  (1 << ADC_ADC3_6V6_CHANNEL)                | \
 	  (1 << ADC_ADC3_3V3_CHANNEL))               | \
-	  (1 << ADC_SCALED_VDD_3V3_SENSORS1_CHANNEL))
+	 (1 << ADC_SCALED_VDD_3V3_SENSORS1_CHANNEL))
 
 /* HW has to large of R termination on ADC todo:change when HW value is chosen */
 
