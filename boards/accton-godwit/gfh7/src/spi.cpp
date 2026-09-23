8597760b00b713a5202ffdc9edb1b4618622b3b9
RunnyCow <thankyous@gmail.com>
2026-09-23 17:30:52 +0800

    Adjust the spacing.

--- boards/accton-godwit/gfh7/src/spi.cpp
+++ boards/accton-godwit/gfh7/src/spi.cpp
@@ -12,11 +12,11 @@ constexpr px4_spi_bus_t px4_spi_buses[SPI_BUS_MAX_BUS_ITEMS] = {
 	}),
 	initSPIBus(SPI::Bus::SPI2, {
 		initSPIDevice(DRV_IMU_DEVTYPE_ST_LSM6DSV, SPI::CS{GPIO::PortB, GPIO::Pin9},
-			      SPI::DRDY{GPIO::PortD, GPIO::Pin11}),
+		SPI::DRDY{GPIO::PortD, GPIO::Pin11}),
 	}),
 	initSPIBus(SPI::Bus::SPI4, {
 		initSPIDevice(DRV_IMU_DEVTYPE_ICM42688P, SPI::CS{GPIO::PortE, GPIO::Pin4},
-			      SPI::DRDY{GPIO::PortD, GPIO::Pin10}),
+		SPI::DRDY{GPIO::PortD, GPIO::Pin10}),
 	}),
 };
 
